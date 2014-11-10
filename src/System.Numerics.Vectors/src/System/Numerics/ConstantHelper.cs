d6310ac8c94dbb9d104702a34a143d19daf9ee7f
Eric Mellino <erme@microsoft.com>
2014-11-10 13:44:50 -0800

    Fix License wording, extra line break, and delete unused file

--- src/System.Numerics.Vectors/src/System/Numerics/ConstantHelper.cs
+++ src/System.Numerics.Vectors/src/System/Numerics/ConstantHelper.cs
@@ -1,5 +1,5 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under MIT. See LICENSE in the project root for license information.
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // This file is auto-generated, do not make permanent modifications.
 
 using System.Runtime.CompilerServices;
