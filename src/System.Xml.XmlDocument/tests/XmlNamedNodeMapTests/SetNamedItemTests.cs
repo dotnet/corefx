84cce63c358a57fdb8997c83539506e24bb8349e
Joe Tremblay <jos.trem@gmail.com>
2014-11-13 20:00:33 -0600

    XmlAttributeCollection.SetNamedItem(XmlNode) returns null if provided a null parameter

--- src/System.Xml.XmlDocument/tests/XmlNamedNodeMapTests/SetNamedItemTests.cs
+++ src/System.Xml.XmlDocument/tests/XmlNamedNodeMapTests/SetNamedItemTests.cs
@@ -16,15 +16,25 @@ namespace XmlDocumentTests.XmlNamedNodeMapTests
             Assert.Equal(0, namedNodeMap.Count);
 
             var newAttribute = xmlDocument.CreateNode(XmlNodeType.Attribute, "newNode", string.Empty);
-
-            // null value should throw an argument exception
-            Assert.Throws<ArgumentException>((() => namedNodeMap.SetNamedItem(newAttribute as XmlDocument)));
-
             namedNodeMap.SetNamedItem(newAttribute);
 
             Assert.NotNull(newAttribute);
             Assert.Equal(1, namedNodeMap.Count);
             Assert.Equal(newAttribute,namedNodeMap.GetNamedItem("newNode"));
+        }
+
+        [Fact]
+        public static void NamedItemIsNull()
+        {
+            var xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml("<foo />");
+
+            var namedNodeMap = (XmlNamedNodeMap)xmlDocument.FirstChild.Attributes;
+
+            // providing a null parameter does not throw an exception; returns null
+            var oldNode = new object();
+            Assert.DoesNotThrow(() => oldNode = namedNodeMap.SetNamedItem(null));
+            Assert.Null(oldNode);           
         }
 
     }
