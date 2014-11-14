83abca292ed2db9ff18ab9baa19326d1ffb4389d
Andrew Arnott <andarno@microsoft.com>
2014-11-14 07:10:06 -0800

    Fix build break due to using being removed that was actually used.

--- src/System.Reflection.Metadata/src/System/Reflection/Metadata/MetadataStringDecoder.cs
+++ src/System.Reflection.Metadata/src/System/Reflection/Metadata/MetadataStringDecoder.cs
@@ -1,8 +1,9 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Text;
 using System.Diagnostics;
+using System.Reflection.Internal;
+using System.Text;
 
 namespace System.Reflection.Metadata
 {
