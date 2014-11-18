6593bffc45989c9eae2ed99bd6572c4837ebf773
Krzysztof Wicher <kwicher@microsoft.com>
2014-11-18 10:28:58 -0800

    Rename binary of System.Xml.XPath.XDocument tests

--- src/System.Xml.XPath/tests/XPathDocument/XmlReaderXPathTest.cs
+++ src/System.Xml.XPath/tests/XPathDocument/XmlReaderXPathTest.cs
@@ -8,6 +8,6 @@ namespace XPathTests.Common
     public static partial class Utils
     {
         private readonly static ICreateNavigator _navigatorCreator = new CreateNavigatorFromXmlReader();
-        public readonly static string ResourceFilesPath = "System.Xml.XPath.XPathDocument.Tests.TestData.";
+        public readonly static string ResourceFilesPath = "System.Xml.XPath.Tests.TestData.";
     }
 }
