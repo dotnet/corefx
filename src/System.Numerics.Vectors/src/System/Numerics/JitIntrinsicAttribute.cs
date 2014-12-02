89e9baee1ea3884097271d5219f52b06dae7a26e
briangru <grunk@live.com>
2014-12-01 21:46:37 -0800

    Limit application of the JitIntrinsicAttribute
    This attribute should limit where it can be placed to something that the
    JIT will respect.

--- src/System.Numerics.Vectors/src/System/Numerics/JitIntrinsicAttribute.cs
+++ src/System.Numerics.Vectors/src/System/Numerics/JitIntrinsicAttribute.cs
@@ -6,7 +6,7 @@ namespace System.Numerics
     /// <summary>
     /// An attribute that can be attached to JIT Intrinsic methods/properties
     /// </summary>
-    [AttributeUsage(AttributeTargets.All)]
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Property)]
     internal class JitIntrinsicAttribute : Attribute
     {
     }
