11312c0c6d96e0a356d186e8c479cecf725d5666
Andrew Arnott <andarno@microsoft.com>
2014-12-05 20:12:02 -0800

    Fixes thread-safety vulnerability in ImmutableArray<T>.
    Any member that references 'this' more than once is vulnerable to in-place replacements of the value of the struct from another thread. That pretty much defeats the thread-safety guarantee of ImmutableArray<T>. So the fix is to make sure we dereference this (to get the array) exactly once per public entrypoint so that the array instance we access is the same every time.
    
    An example of the problem is we can capture the length of the array, and then copy from the array. But in between those two steps another thread updated the field that contained this struct, so while our method is still running, the instance of the array actually *changed*, so the length of the array changed with it, and we throw an exception because we tried to read beyond the bounds of the array.
    
    It turns out this demonstrates a very compelling reason to prefix *all* instance member access with "this." (not only fields, but properties and methods too!) because it made auditing and fixing this class much easier since I just had to look for any member that referenced 'this' more than once and could use search and replace to change them all to 'self'.
    
    Fixes #191

--- src/System.Collections.Immutable/tests/ImmutableArrayTest.cs
+++ src/System.Collections.Immutable/tests/ImmutableArrayTest.cs
@@ -1169,6 +1169,8 @@ namespace System.Collections.Immutable.Test
             // it *can* throw because the field can be replaced with an array of another length. 
             // In fact, much worse can happen where we corrupt data if we are for example copying data out of the array
             // in (for example) a CopyTo method and we read from the field more than once.
+            // Also noteworthy: this method only tests the thread-safety of the Add method.
+            // While it proves the general point, any method that reads 'this' more than once is vulnerable.
             var array = ImmutableArray.Create<int>();
             Action mutator = () => { for (int i = 0; i < 100; i++) ImmutableInterlocked.InterlockedExchange(ref array, array.Add(1)); };
             Task.WaitAll(Task.Run(mutator), Task.Run(mutator));
