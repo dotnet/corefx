2f1136840d6a8069a15013f6b2a3e27bca86483f
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2014-12-07 15:17:27 +0200

    Introduced empty cache delegate classes to cache delegates

---
+++ src/System.Linq.Parallel/src/System/Linq/DelegateCache.cs
@@ -0,0 +1,14 @@
+namespace System.Linq
+{
+    internal static class DelegateCache
+    {
+    }
+
+    internal static class DelegateCache<T>
+    {
+    }
+
+    internal static class DelegateCache<T1, T2>
+    {
+    }
+}
