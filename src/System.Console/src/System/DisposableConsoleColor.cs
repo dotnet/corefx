91de197554fbb1423f95f684f5efa462e7279cab
Antoine Blanchet <a.blanchet@abc-arbitrage.com>
2014-12-11 13:41:06 +0100

    Implement simple foreground/background color scoping via IDisposable

---
+++ src/System.Console/src/System/DisposableConsoleColor.cs
@@ -0,0 +1,17 @@
+﻿namespace System
+{
+    internal class DisposableConsoleColor : IDisposable
+    {
+        private readonly Action _resetPreviousConsoleSettings;
+
+        public DisposableConsoleColor(Action resetPreviousConsoleSettings )
+        {
+            _resetPreviousConsoleSettings = resetPreviousConsoleSettings;
+        }
+
+        public void Dispose()
+        {
+            _resetPreviousConsoleSettings();
+        }
+    }
+}
