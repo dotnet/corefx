b8f4b2e0b0727a232e88b89fcb5519af022a1a0a
Lakshmi Priya <lasekar@microsoft.com>
2014-12-16 11:58:36 -0800

    Adding more tests to Microsoft.Win32.Primitives.

--- src/Microsoft.Win32.Primitives/tests/Win32Exception.cs
+++ src/Microsoft.Win32.Primitives/tests/Win32Exception.cs
@@ -18,5 +18,15 @@ namespace Microsoft.Win32.Primitives.Tests
         }
 
         public const int E_FAIL = unchecked((int)0x80004005);
+
+        [Fact]
+        public static void InstantiateExceptionWithLongErrorString()
+        {
+            // Ensure Win32Exception supports error strings greater than 256 chars.
+            // ErrorCode 616(ERROR_PWD_TOO_RECENT) has error message string length
+            // greater than 256 and hence using it to validate that we support lengths > 256.
+            Win32Exception ex = new Win32Exception(616);
+            Assert.True(!ex.Message.Equals("Unknown error (0x268)"));
+        }
     }
 }
