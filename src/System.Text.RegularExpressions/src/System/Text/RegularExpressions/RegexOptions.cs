15422e7ac6dcc7981932898b12c7d6c30563eccb
Justin Van Patten <jvp@justinvp.com>
2015-01-02 21:27:50 -0800

    Minor cleanup

--- src/System.Text.RegularExpressions/src/System/Text/RegularExpressions/RegexOptions.cs
+++ src/System.Text.RegularExpressions/src/System/Text/RegularExpressions/RegexOptions.cs
@@ -6,29 +6,20 @@ namespace System.Text.RegularExpressions
     [Flags]
     public enum RegexOptions
     {
-        None = 0x0000,
-
-        IgnoreCase = 0x0001,      // "i"
-
-        Multiline = 0x0002,      // "m"
-
-        ExplicitCapture = 0x0004,      // "n"
-
-        Compiled = 0x0008,      // "c"
-
-        Singleline = 0x0010,      // "s"
-
-        IgnorePatternWhitespace = 0x0020,      // "x"
-
-        RightToLeft = 0x0040,      // "r"
+        None                    = 0x0000,
+        IgnoreCase              = 0x0001, // "i"
+        Multiline               = 0x0002, // "m"
+        ExplicitCapture         = 0x0004, // "n"
+        Compiled                = 0x0008, // "c"
+        Singleline              = 0x0010, // "s"
+        IgnorePatternWhitespace = 0x0020, // "x"
+        RightToLeft             = 0x0040, // "r"
 
 #if DEBUG
-        Debug = 0x0080,      // "d"
+        Debug                   = 0x0080, // "d"
 #endif
 
-        ECMAScript = 0x0100,      // "e"
-
-        CultureInvariant = 0x0200,
+        ECMAScript              = 0x0100, // "e"
+        CultureInvariant        = 0x0200,
     }
 }
-
