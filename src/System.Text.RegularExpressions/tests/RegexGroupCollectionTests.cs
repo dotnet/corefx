bbeadf12c0605531f6c7cdf2b49975d783732653
_aLfa_ <mail@joaodias.eu>
2015-01-22 22:26:26 +0100

    Add a basic test for GetGroup

---
+++ src/System.Text.RegularExpressions/tests/RegexGroupCollectionTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Xunit;
+
+public class RegexGroupCollectionTests
+{
+    [Fact]
+    public static void GetGroupTest()
+    {
+        var caps = new Dictionary<int, int>
+        {
+            { 0, 0 }
+        };
+        var groups = new GroupCollection(null, caps);
+        var group = groups.GetGroup(1);
+        Assert.NotEqual(Group._emptygroup, group);
+    }
+}
