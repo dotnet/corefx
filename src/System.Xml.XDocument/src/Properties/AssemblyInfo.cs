e7bdcbee44c911c06f7e5831af2ffdb8f4fe6c99
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2015-02-02 18:26:59 +0200

    Add a failing test for XObject.SkipNotify()
    #54 (Remove always true "if" and unreachable code in System.Xml.Linq.XObject.SkipNotify method.)
    Tests fails because XObject.Annotations<T>() returns an enumerator object which is always not null.

--- src/System.Xml.XDocument/src/Properties/AssemblyInfo.cs
+++ src/System.Xml.XDocument/src/Properties/AssemblyInfo.cs
@@ -5,6 +5,7 @@ using System;
 using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -23,6 +24,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: InternalsVisibleTo("System.Xml.XDocument.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010007d1fa57c4aed9f0a32e84aa0faefd0de9e8fd6aec8f87fb03766c834c99921eb23be79ad9d5dcc1dd9ad236132102900b723cf980957fc4e177108fc607774f29e8320e92ea05ece4e821c0a5efe8f1645c4c0c93c1ab99285d622caa652c1dfad63d745d6f2de5f17e5eaf0fc4963d261c8a12436518206dc093344d5ad293")]
 
 
 
