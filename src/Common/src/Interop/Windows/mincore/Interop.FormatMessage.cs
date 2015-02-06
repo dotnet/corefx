2f7292ed92e2f6b3fa57d853fb5e6446415a650d
Maxwe11 <turin.dmytro@gmail.com>
2015-02-06 18:44:20 +0200

    Add FormatMessage interop signature

---
+++ src/Common/src/Interop/Windows/mincore/Interop.FormatMessage.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Text;
+using System.Runtime.InteropServices;
+
+internal partial class Interop
+{
+    internal partial class mincore
+    {
+        [DllImport(Libraries.Localization, CharSet = CharSet.Unicode,  EntryPoint="FormatMessageW", SetLastError = true, BestFitMapping = true)]
+        internal static extern int FormatMessage(int dwFlags, IntPtr lpSource_mustBeNull, uint dwMessageId,
+            int dwLanguageId, StringBuilder lpBuffer, int nSize, IntPtr[] arguments);
+
+        internal const int FORMAT_MESSAGE_IGNORE_INSERTS = 0x00000200;
+        internal const int FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000;
+        internal const int FORMAT_MESSAGE_ARGUMENT_ARRAY = 0x00002000;
+
+        internal const int ERROR_INSUFFICIENT_BUFFER = 0x7A;
+    }
+}
