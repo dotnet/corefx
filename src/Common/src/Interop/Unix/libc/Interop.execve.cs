0143f90db19517dc3d3db6bf97aee6e1886851a6
Stephen Toub <stoub@microsoft.com>
2015-02-08 22:26:47 -0500

    Provide null-terminated arrays to execve call
    With this change (and working around some other unrelated errors/asserts which I'll come back to), basic Process.Start functionality works.

--- src/Common/src/Interop/Unix/libc/Interop.execve.cs
+++ src/Common/src/Interop/Unix/libc/Interop.execve.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 internal static partial class Interop
@@ -13,12 +14,13 @@ internal static partial class Interop
             byte** argvPtr = null, envpPtr = null;
             try
             {
-                AllocArray(argv, ref argvPtr);
-                AllocArray(envp, ref envpPtr);
+                AllocNullTerminatedArray(argv, ref argvPtr);
+                AllocNullTerminatedArray(envp, ref envpPtr);
                 return execve(filename, argvPtr, envpPtr);
             }
             finally
             {
+                // Should only execute if execve fails
                 FreeArray(envpPtr, envp.Length);
                 FreeArray(argvPtr, argv.Length);
             }
@@ -27,19 +29,29 @@ internal static partial class Interop
         [DllImport(Libraries.Libc, SetLastError = true)]
         private static extern unsafe int execve(string filename, byte** argv, byte** envp);
 
-        private static unsafe void AllocArray(string[] arr, ref byte** arrPtr)
+        private static unsafe void AllocNullTerminatedArray(string[] arr, ref byte** arrPtr)
         {
-            // Allocate the unmanaged array to hold each string pointer
-            arrPtr = (byte**)Marshal.AllocHGlobal(sizeof(IntPtr) * arr.Length);
+            int arrLength = arr.Length + 1; // +1 is for null termination
+
+            // Allocate the unmanaged array to hold each string pointer.
+            // It needs to have an extra element to null terminate the array.
+            arrPtr = (byte**)Marshal.AllocHGlobal(sizeof(IntPtr) * arrLength);
+            Debug.Assert(arrPtr != null);
 
             // Zero the memory so that if any of the individual string allocations fails,
-            // we can loop through the array to free any that succeeded
-            for (int i = 0; i < arr.Length; i++)
+            // we can loop through the array to free any that succeeded.
+            // The last element will remain null.
+            for (int i = 0; i < arrLength; i++)
+            {
                 arrPtr[i] = null;
-            
+            }
+
             // Now copy each string to unmanaged memory referenced from the array
             for (int i = 0; i < arr.Length; i++)
+            {
                 arrPtr[i] = (byte*)Marshal.StringToHGlobalUni(arr[i]);
+                Debug.Assert(arrPtr[i] != null);
+            }
         }
 
         private static unsafe void FreeArray(byte** arr, int length)
@@ -52,6 +64,7 @@ internal static partial class Interop
                     if (arr[i] != null)
                     {
                         Marshal.FreeHGlobal((IntPtr)arr[i]);
+                        arr[i] = null;
                     }
                 }
 
