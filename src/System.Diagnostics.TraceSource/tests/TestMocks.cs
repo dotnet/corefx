0ff503e7e2560dec68da6e507d8b72a455e3b3de
MarkPflug <mark.pflug@live.com>
2015-03-19 21:33:13 -0700

    Unit tests for System.Diagnostics.TraceSource class .

---
+++ src/System.Diagnostics.TraceSource/tests/TestMocks.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace System.Diagnostics.TraceSourceTests
+{
+    // mock TraceListener to make assertions against TraceSource behavior
+    internal class TestTraceListener : TraceListener
+    {
+        public enum Method
+        {
+            TraceEvent = 0,
+            TraceData,
+            Dispose,
+            Write,
+            WriteLine,
+        }
+
+        int[] _calls;
+
+        /// <summary>
+        /// Gets the number of times any of the public methods on this instance were called.
+        /// </summary>
+        public int GetCallCount(Method group) {
+            return _calls[(int) group]; 
+        }
+
+        private void Call(Method method)
+        {
+            _calls[(int)method]++;
+
+        }
+        
+        public TestTraceListener()
+        {
+            _calls = new int[5];
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Call(Method.Dispose);
+        }
+
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id)
+        {
+            Call(Method.TraceEvent);
+        }
+
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string format, params object[] args)
+        {
+            Call(Method.TraceEvent);
+        }
+
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string message)
+        {
+            Call(Method.TraceEvent);
+        }
+
+        public override void TraceData(TraceEventCache eventCache, string source, TraceEventType eventType, int id, object data)
+        {
+            Call(Method.TraceData);
+        }
+
+        public override void TraceData(TraceEventCache eventCache, string source, TraceEventType eventType, int id, params object[] data)
+        {
+            Call(Method.TraceData);
+        }
+
+        public override void Write(string message)
+        {
+            Call(Method.Write);
+        }
+
+        public override void WriteLine(string message)
+        {
+            Call(Method.WriteLine);
+        }
+    }
+}
---
+++ src/System.Diagnostics.TraceSource/tests/TraceSourceClassTests.cs
@@ -0,0 +1,101 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Xunit;
+
+namespace System.Diagnostics.TraceSourceTests
+{
+    using Method = TestTraceListener.Method;
+
+    public class TraceSourceTests
+    {
+        [Fact]
+        public void DefaultListener()
+        {
+            var trace = new TraceSource("TestTraceSource");
+            Assert.True(trace.Listeners.Count == 1);
+            Assert.IsType<DefaultTraceListener>(trace.Listeners[0]);
+        }
+
+        [Fact]
+        public void DefaultLevel()
+        {
+            var trace = new TraceSource("TestTraceSource");
+            Assert.True(trace.Switch.Level == SourceLevels.Off);
+        }
+
+        [Theory]
+        [InlineData(SourceLevels.Off, TraceEventType.Critical, 0)]
+        [InlineData(SourceLevels.Critical, TraceEventType.Critical, 1)]
+        [InlineData(SourceLevels.Critical, TraceEventType.Error, 0)]
+        [InlineData(SourceLevels.Error, TraceEventType.Error, 1)]
+        [InlineData(SourceLevels.Error, TraceEventType.Warning, 0)]
+        [InlineData(SourceLevels.Warning, TraceEventType.Warning, 1)]
+        [InlineData(SourceLevels.Warning, TraceEventType.Information, 0)]
+        [InlineData(SourceLevels.Information, TraceEventType.Information, 1)]
+        [InlineData(SourceLevels.Information, TraceEventType.Verbose, 0)]
+        [InlineData(SourceLevels.Verbose, TraceEventType.Verbose, 1)]
+        // NOTE: test to cover a TraceEventType value that is not in .net Core
+        [InlineData(SourceLevels.Verbose, (TraceEventType)0x20, 0)]
+        [InlineData(SourceLevels.All, TraceEventType.Critical, 1)]
+        [InlineData(SourceLevels.All, TraceEventType.Verbose, 1)]
+        public void SwitchLevel(SourceLevels sourceLevel, TraceEventType messageLevel, int expected)
+        {
+            var trace = new TraceSource("TestTraceSource");
+            var listener = new TestTraceListener();
+            trace.Listeners.Add(listener);
+            trace.Switch.Level = sourceLevel;
+            trace.TraceEvent(messageLevel, 0);
+            Assert.Equal(expected, listener.GetCallCount(Method.TraceEvent));
+        }
+
+        [Fact]
+        public void TraceEvent()
+        {
+            var trace = new TraceSource("TestTraceSource", SourceLevels.All);
+            var listener = new TestTraceListener();
+            trace.Listeners.Add(listener);
+            trace.TraceEvent(TraceEventType.Verbose, 0);
+            Assert.Equal(1, listener.GetCallCount(Method.TraceEvent));
+        }
+
+
+        [Fact]
+        public void TraceData()
+        {
+            var trace = new TraceSource("TestTraceSource", SourceLevels.All);
+            var listener = new TestTraceListener();
+            trace.Listeners.Add(listener);
+            trace.TraceData(TraceEventType.Verbose, 0, null);
+            Assert.Equal(1, listener.GetCallCount(Method.TraceData));
+        }
+
+        [Fact]
+        public void TraceInformation()
+        {
+            var trace = new TraceSource("TestTraceSource", SourceLevels.All);
+            var listener = new TestTraceListener();
+            trace.Listeners.Add(listener);
+            trace.TraceInformation(null);
+            Assert.Equal(0, listener.GetCallCount(Method.TraceData));
+            Assert.Equal(0, listener.GetCallCount(Method.Write));
+            Assert.Equal(1, listener.GetCallCount(Method.TraceEvent));
+        }
+
+        [Fact]
+        public void Close()
+        {
+            var trace = new TraceSource("T1", SourceLevels.All);
+            trace.Listeners.Clear();
+
+            var listener = new TestTraceListener();
+            trace.Listeners.Add(listener);
+            trace.Close();
+            // NOTE: this assertion fails on .net 4.5
+            // where TraceSource.Close calls TraceListener.Close, not Dispose.
+            Assert.Equal(1, listener.GetCallCount(Method.Dispose));
+            // Assert that writing to a closed TraceSource is not an error.
+            trace.TraceEvent(TraceEventType.Critical, 0);
+        }
+    }
+}
