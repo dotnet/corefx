ad40fe4e8aa3952a15a1830902f0ff684e67c654
Rafid K. Abdullah <rafidka@gmail.com>
2015-03-21 09:05:27 +0000

    Some changes to Linq tests as suggested by the community.
    #1143

--- src/System.Linq/tests/EnumerableTests.cs
+++ src/System.Linq/tests/EnumerableTests.cs
@@ -14,9 +14,82 @@ namespace System.Linq.Tests
         [Fact]
         public void Average()
         {
-            var array = Enumerable.Range(1, 10).ToArray();
-            var min = array.Min();
-            Assert.Equal(min, 1);
+            var one = new[] {0, 1, 2};
+            var two = new[] {0, 1, 2, 3, 4};
+            var fourByThree = new[] {-2, 1, 5};
+            var oneByThree = new[] {-2, 1, 2};
+
+            var oneWithNulls = new int?[] {0, 1, 2, null, null, null, null};
+            var twoWithNulls = new int?[] {0, 1, 2, 3, 4, null, null, null};
+            var fourByThreeWithNulls = new int?[] {-2, 1, 5, null};
+            var oneByThreeWithNulls = new int?[] {-2, 1, 2, null, null};
+
+            Assert.Equal(1, one.Average());
+            Assert.Equal(2, two.Average());
+            Assert.Equal(4.0/3, fourByThree.Average());
+            Assert.Equal(1.0/3, oneByThree.Average());
+
+            // Repeat the tests with some null values and ensure they don't affect the result.
+            Assert.Equal(1, oneWithNulls.Average());
+            Assert.Equal(2, twoWithNulls.Average());
+            Assert.Equal(4.0/3, fourByThreeWithNulls.Average());
+            Assert.Equal(1.0/3, oneByThreeWithNulls.Average());
+
+            // Cast the arrays' elements types to different data types and repeat the tests.
+            // 1. long and long?
+            Assert.Equal(1, one.Select<int, long>(i => i).Average());
+            Assert.Equal(2, two.Select<int, long>(i => i).Average());
+            Assert.Equal(4.0/3, fourByThree.Select<int, long>(i => i).Average());
+            Assert.Equal(1.0/3, oneByThree.Select<int, long>(i => i).Average());
+            Assert.Equal(1, oneWithNulls.Select<int?, long?>(i => i).Average());
+            Assert.Equal(2, twoWithNulls.Select<int?, long?>(i => i).Average());
+            Assert.Equal(4.0/3, fourByThreeWithNulls.Select<int?, long?>(i => i).Average());
+            Assert.Equal(1.0/3, oneByThreeWithNulls.Select<int?, long?>(i => i).Average());
+            // 2. float and float?
+            Assert.Equal(1, one.Select<int, float>(i => i).Average());
+            Assert.Equal(2, two.Select<int, float>(i => i).Average());
+            Assert.Equal(4.0f/3, fourByThree.Select<int, float>(i => i).Average());
+            Assert.Equal(1.0f/3, oneByThree.Select<int, float>(i => i).Average());
+            Assert.Equal(1, oneWithNulls.Select<int?, float?>(i => i).Average());
+            Assert.Equal(2, twoWithNulls.Select<int?, float?>(i => i).Average());
+            Assert.Equal(4.0f/3, fourByThreeWithNulls.Select<int?, float?>(i => i).Average());
+            Assert.Equal(1.0f/3, oneByThreeWithNulls.Select<int?, float?>(i => i).Average());
+            // 3. double and double?
+            Assert.Equal(1, one.Select<int, double>(i => i).Average());
+            Assert.Equal(2, two.Select<int, double>(i => i).Average());
+            Assert.Equal(4.0/3, fourByThree.Select<int, double>(i => i).Average());
+            Assert.Equal(1.0/3, oneByThree.Select<int, double>(i => i).Average());
+            Assert.Equal(1, oneWithNulls.Select<int?, double?>(i => i).Average());
+            Assert.Equal(2, twoWithNulls.Select<int?, double?>(i => i).Average());
+            Assert.Equal(4.0/3, fourByThreeWithNulls.Select<int?, double?>(i => i).Average());
+            Assert.Equal(1.0/3, oneByThreeWithNulls.Select<int?, double?>(i => i).Average());
+            // 4. decimal and decimal?
+            Assert.Equal(1, one.Select<int, decimal>(i => i).Average());
+            Assert.Equal(2, two.Select<int, decimal>(i => i).Average());
+            Assert.Equal(4.0m/3, fourByThree.Select<int, decimal>(i => i).Average());
+            Assert.Equal(1.0m/3, oneByThree.Select<int, decimal>(i => i).Average());
+            Assert.Equal(1, oneWithNulls.Select<int?, decimal?>(i => i).Average());
+            Assert.Equal(2, twoWithNulls.Select<int?, decimal?>(i => i).Average());
+            Assert.Equal(4.0m/3, fourByThreeWithNulls.Select<int?, decimal?>(i => i).Average());
+            Assert.Equal(1.0m/3, oneByThreeWithNulls.Select<int?, decimal?>(i => i).Average());
+
+            // Convert the array to different data structures and repeat the tests to
+            // ensure that the method works with other data structures as well.
+            // 1. List
+            Assert.Equal(1, one.ToList().Average());
+            Assert.Equal(2, two.ToList().Average());
+            Assert.Equal(4.0/3, fourByThree.ToList().Average());
+            Assert.Equal(1.0/3, oneByThree.ToList().Average());
+            // 2. Stack
+            Assert.Equal(1, new Stack<int>(one).Average());
+            Assert.Equal(2, new Stack<int>(two).Average());
+            Assert.Equal(4.0/3, new Stack<int>(fourByThree).Average());
+            Assert.Equal(1.0/3, new Stack<int>(oneByThree).Average());
+            // 3. Queue
+            Assert.Equal(1, new Queue<int>(one).Average());
+            Assert.Equal(2, new Queue<int>(two).Average());
+            Assert.Equal(4.0/3, new Queue<int>(fourByThree).Average());
+            Assert.Equal(1.0/3, new Queue<int>(oneByThree).Average());
         }
 
         [Fact]
@@ -24,77 +97,74 @@ namespace System.Linq.Tests
         {
             var array = Enumerable.Range(1, 10).ToArray();
             Assert.True(array.All(i => i > 0));
-            Assert.False(array.All(i => i > 1));
-            Assert.False(array.All(i => i > 2));
-            Assert.False(array.All(i => i > 3));
-            Assert.False(array.All(i => i > 4));
-            Assert.False(array.All(i => i > 5));
-            Assert.False(array.All(i => i > 6));
-            Assert.False(array.All(i => i > 7));
-            Assert.False(array.All(i => i > 8));
-            Assert.False(array.All(i => i > 9));
-            Assert.False(array.All(i => i > 10));
+            for (var j = 1; j <= 10; j++)
+                Assert.False(array.All(i => i > j));
         }
 
         [Fact]
         public void Any()
         {
             var array = Enumerable.Range(1, 10).ToArray();
-            Assert.True(array.Any(i => i > 0));
-            Assert.True(array.Any(i => i > 1));
-            Assert.True(array.Any(i => i > 2));
-            Assert.True(array.Any(i => i > 3));
-            Assert.True(array.Any(i => i > 4));
-            Assert.True(array.Any(i => i > 5));
-            Assert.True(array.Any(i => i > 6));
-            Assert.True(array.Any(i => i > 7));
-            Assert.True(array.Any(i => i > 8));
-            Assert.True(array.Any(i => i > 9));
+            for (var j = 0; j <= 9; j++)
+                Assert.True(array.Any(i => i > j));
             Assert.False(array.Any(i => i > 10));
+        }
+
+        private void FindCountAndValidate<T>(IEnumerable<T> enumerable, int expectedCount)
+        {
+            Assert.Equal(expectedCount, enumerable.Count());
+            Assert.Equal(expectedCount, enumerable.ToList().Count());
+            Assert.Equal(expectedCount, enumerable.ToArray().Count());
+            Assert.Equal(expectedCount, new Stack<T>(enumerable).Count());
+            Assert.Equal(expectedCount, new Queue<T>(enumerable).Count());
         }
 
         [Fact]
         public void Count()
         {
             const int count = 100;
-            Assert.Equal(Enumerable.Range(1, count).Count(), count);
+            var range = Enumerable.Range(1, count).ToArray();
+            FindCountAndValidate(range, count);
+            FindCountAndValidate(range.Select<int, long>(i => i), count);
+            FindCountAndValidate(range.Select<int, float>(i => i), count);
+            FindCountAndValidate(range.Select<int, double>(i => i), count);
+            FindCountAndValidate(range.Select<int, decimal>(i => i), count);
         }
 
-        [Fact]
-        public void Distinct()
+        private void FindDistinctAndValidate<T>(IEnumerable<T> original)
         {
-            var array = new[] {1, 1, 1, 2, 3, 5, 5, 6, 6, 10};
-            var distinct = array.Distinct().ToArray();
+            // Convert to list to avoid repeated enumerations of the enumerables.
+            var originalList = original.ToList();
+            var distinctList = originalList.Distinct().ToList();
 
             // Ensure the result doesn't contain duplicates; sort it first to make this easier.
             // Notice that we assume the correctness of OrderBy() here, which is tested
             // in another unit.
-            distinct = distinct.OrderBy(i => i).ToArray();
-            for (var i = 0; i < distinct.Length - 1; i++)
+            for (var i = 0; i < distinctList.Count - 1; i++)
             {
-                Assert.False(distinct[i] == distinct[i + 1],
+                Assert.False(distinctList[i].Equals(distinctList[i + 1]),
                     "There are some duplicates in the result of Distinct().");
             }
 
-            // Ensure that every element in 'array' exists in 'distinct'.
-            foreach (var i in array)
+            // Ensure that every element in 'array' exists in 'distinctList'.
+            foreach (var i in originalList)
             {
                 var correct = false;
-                foreach (var j in distinct)
+                foreach (var j in distinctList)
                 {
-                    if (i == j)
+                    if (i.Equals(j))
                         correct = true;
                 }
                 Assert.True(correct, "Some elements in the original enumerable are missing in the result of Distinct().");
             }
 
-            // Ensure that every element in 'distinct' exists in 'array'.
-            foreach (var i in distinct)
+            // Ensure that every element in 'distinctList' exists in 'originalList'.
+            foreach (var i in distinctList)
             {
                 var correct = false;
-                foreach (var j in array)
+                foreach (var j in originalList)
                 {
-                    if (i == j)
+                    if (i.Equals(j))
                         correct = true;
                 }
                 Assert.True(correct, "Some elements in the result of Distinct() don't xist in the given enumerable.");
@@ -102,19 +172,55 @@ namespace System.Linq.Tests
         }
 
         [Fact]
+        public void Distinct()
+        {
+            // Validate an array of different numeric data types.
+            FindDistinctAndValidate(new int[] {1, 1, 1, 2, 3, 5, 5, 6, 6, 10});
+            FindDistinctAndValidate(new long[] {1, 1, 1, 2, 3, 5, 5, 6, 6, 10});
+            FindDistinctAndValidate(new float[] {1, 1, 1, 2, 3, 5, 5, 6, 6, 10});
+            FindDistinctAndValidate(new double[] {1, 1, 1, 2, 3, 5, 5, 6, 6, 10});
+            FindDistinctAndValidate(new decimal[] {1, 1, 1, 2, 3, 5, 5, 6, 6, 10});
+            // Try strings
+            FindDistinctAndValidate(new []
+            {
+                "add",
+                "add",
+                "subtract",
+                "multiply",
+                "divide",
+                "divide2",
+                "subtract",
+                "add",
+                "power",
+                "exponent",
+                "hello",
+                "class",
+                "namespace",
+                "namespace",
+                "namespace",
+            });
+        }
+
+        [Fact]
         public void Min()
         {
-            var array = Enumerable.Range(1, 10).ToArray();
-            var min = array.Min();
-            Assert.Equal(min, 1);
+            var one = Enumerable.Range(1, 10).ToArray();
+            var minusTen = new [] {-1, -10, 10, 200, 1000};
+            var hundred = new [] {3000, 100, 200, 1000};
+            Assert.Equal(1, one.Min());
+            Assert.Equal(-10, minusTen.Min());
+            Assert.Equal(100, hundred.Min());
         }
 
         [Fact]
         public void Max()
         {
-            var array = Enumerable.Range(1, 10).ToArray();
-            var max = array.Max();
-            Assert.Equal(max, 10);
+            var ten = Enumerable.Range(1, 10).ToArray();
+            var minusTen = new[] {-100, -15, -50, -10};
+            var thousand = new[] {-16, 0, 50, 100, 1000};
+            Assert.Equal(10, ten.Max());
+            Assert.Equal(-10, minusTen.Max());
+            Assert.Equal(1000, thousand.Max());
         }
 
         [Fact]
