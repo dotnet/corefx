50508b42515bf5e21ce2bc87d26b226388d34b11
Krzysztof Cwalina <kcwalina@microsoft.com>
2015-04-13 10:59:22 -0700

    Initial commit of web encoders This is the first commit in a series to make web encoders part of core libraries. We have had some discussions with the ASP.NET team, and we concluded that the encoders are quite general purpose, and should be made part of the core.
    This commit includes a subset of Microsoft.Framework.WebEncoders.Core that we deemed general purpose.
    It excludes some APIs related to dependency injection and some ASP.NET specific helpers.
    
    The only changes in the moved code are:
    a) new namespace and assembly name (System.Text.Encodings.Web)
    b) changes to project files required by the CoreFx repo build system
    c) changes to embedded resource paths (as the directory structure has changed)
    
    One caveat, some tests in the test project are disabled. They do pass, but the test harness logger
    fails as it cannot output some exotic characters that are passed to InlineData (and so printed in the log).
    The root cause of this is most probably in XmlEscape routine in
    https://github.com/xunit/xunit/blob/40e153fe983977be8fe009384fae5c377bc557ff/src/common/XmlTestExecutionVisitor.cs.
    I will fix this problem in a subsequent commit.

---
+++ src/System.Text.Encodings.Web/src/System/Text/Encodings/Web/ICodePointFilter.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace System.Text.Encodings.Web
+{
+    /// <summary>
+    /// Represents a filter which allows only certain Unicode code points through.
+    /// </summary>
+    public interface ICodePointFilter
+    {
+        /// <summary>
+        /// Gets an enumeration of all allowed code points.
+        /// </summary>
+        IEnumerable<int> GetAllowedCodePoints();
+    }
+}
