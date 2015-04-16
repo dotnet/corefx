bf092ad5c23cf739cc5232ac4478e2de90e22671
Krzysztof Cwalina <kcwalina@microsoft.com>
2015-04-16 15:03:40 -0700

    Added EncoderExtensions
    This API and tests are simply ported from ASP.NET repo.
    Initially, when I ported the component, I did not include the extensions in the port.
    I want the CoreFx component to be independent from I/O (TextWriter). But there is more work needed,
    to make it trully independent, and I am not sure this will ever happen, so I am including the extensions for now.

---
+++ src/System.Text.Encodings.Web/src/System/Text/Encodings/Web/EncoderExtensions.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+
+namespace System.Text.Encodings.Web
+{
+    /// <summary>
+    /// Helpful extension methods for the encoder classes.
+    /// </summary>
+    public static class EncoderExtensions
+    {
+        /// <summary>
+        /// HTML-encodes a string and writes the result to the supplied output.
+        /// </summary>
+        /// <remarks>
+        /// The encoded value is also safe for inclusion inside an HTML attribute
+        /// as long as the attribute value is surrounded by single or double quotes.
+        /// </remarks>
+        public static void HtmlEncode(this IHtmlEncoder htmlEncoder, string value, TextWriter output)
+        {
+            if (htmlEncoder == null)
+            {
+                throw new ArgumentNullException("htmlEncoder");
+            }
+
+            if (!String.IsNullOrEmpty(value))
+            {
+                htmlEncoder.HtmlEncode(value, 0, value.Length, output);
+            }
+        }
+
+        /// <summary>
+        /// JavaScript-escapes a string and writes the result to the supplied output.
+        /// </summary>
+        public static void JavaScriptStringEncode(this IJavaScriptStringEncoder javaScriptStringEncoder, string value, TextWriter output)
+        {
+            if (javaScriptStringEncoder == null)
+            {
+                throw new ArgumentNullException("javaScriptStringEncoder");
+            }
+
+            if (!String.IsNullOrEmpty(value))
+            {
+                javaScriptStringEncoder.JavaScriptStringEncode(value, 0, value.Length, output);
+            }
+        }
+
+        /// <summary>
+        /// URL-encodes a string and writes the result to the supplied output.
+        /// </summary>
+        /// <remarks>
+        /// The encoded value is safe for use in the segment, query, or
+        /// fragment portion of a URI.
+        /// </remarks>
+        public static void UrlEncode(this IUrlEncoder urlEncoder, string value, TextWriter output)
+        {
+            if (urlEncoder == null)
+            {
+                throw new ArgumentNullException("urlEncoder");
+            }
+
+            if (!String.IsNullOrEmpty(value))
+            {
+                urlEncoder.UrlEncode(value, 0, value.Length, output);
+            }
+        }
+    }
+}
