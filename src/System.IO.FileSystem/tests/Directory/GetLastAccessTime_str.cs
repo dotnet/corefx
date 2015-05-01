86ddbc51aca5f4163a1c3d0183bbf5da3cf6f824
Matt Cohn <macohn@microsoft.com>
2015-05-01 15:42:39 -0700

    Replace Sys.IO.FS GetLastAccessTime and GetLastWriteTime tests

--- src/System.IO.FileSystem/tests/Directory/GetLastAccessTime_str.cs
+++ src/System.IO.FileSystem/tests/Directory/GetLastAccessTime_str.cs
@@ -3,108 +3,15 @@
 
 using System;
 using System.IO;
-using System.Collections;
-using System.Globalization;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Runtime.CompilerServices;
 using Xunit;
 
-public class Directory_Co9027GetLastAccessTime_str
+namespace System.IO.FileSystem.Tests
 {
-    public static String s_strDtTmVer = "2001/02/12 13:19";
-    public static String s_strClassMethod = "Directory.GetLastAccessTime()";
-    public static String s_strTFName = "GetLastAccessTime_str.cs";
-    public static String s_strTFPath = Directory.GetCurrentDirectory();
-
-    [Fact]
-    [OuterLoop]
-    public static void runTest()
+    public class Directory_GetLastAccessTime_str : GetTime_str
     {
-        String strLoc = "Loc_000oo";
-        String strValue = String.Empty;
-        int iCountErrors = 0;
-        int iCountTestcases = 0;
-
-        try
+        protected override DateTime m_Get(string path)
         {
-            String dirName = Path.Combine(TestInfo.CurrentDirectory, "GetLastAccessTime_str_TestDir");
-            DirectoryInfo dir2;
-
-            if (Directory.Exists(dirName))
-                Directory.Delete(dirName, true);
-
-            // [] Create a directory and check the last access time 
-
-            strLoc = "Loc_r8r7j";
-
-            dir2 = Directory.CreateDirectory(dirName);
-            dir2.Refresh();
-            iCountTestcases++;
-            try
-            {
-                if ((DateTime.Now - Directory.GetLastAccessTime(dirName)).Days != 0)
-                {
-                    iCountErrors++;
-                    Console.WriteLine(Directory.GetLastAccessTime(dirName));
-                    Console.WriteLine((DateTime.Now - dir2.LastAccessTime).Days);
-                    printerr("Error_20hjx! Access time cannot be correct");
-                }
-            }
-            catch (Exception exc)
-            {
-                iCountErrors++;
-                printerr("Bug 14952: Error_20fhd! Unexpected exceptiont thrown: " + exc.ToString());
-            }
-
-            // [] access the directory and check the time
-
-            strLoc = "Loc_20yxc";
-
-            //dir2.GetFiles();
-            dir2.Refresh();
-            Task.Delay(1000).Wait();
-            iCountTestcases++;
-            try
-            {
-                if ((DateTime.Now - Directory.GetLastAccessTime(dirName)).Days != 0)
-                {
-                    iCountErrors++;
-                    Console.WriteLine((DateTime.Now - Directory.GetLastAccessTime(dirName)).Days);
-                    printerr("Eror_209x9! LastAccessTime is way off");
-                }
-            }
-            catch (Exception exc)
-            {
-                iCountErrors++;
-                printerr("Error_209jx! Unexpected exception thrown: " + exc.ToString());
-            }
-            dir2.Delete();
-
-            //-----------------------------------------------------------
-
-            if (Directory.Exists(dirName))
-                Directory.Delete(dirName, true);
+            return Directory.GetLastAccessTime(path);
         }
-        catch (Exception exc_general)
-        {
-            ++iCountErrors;
-            printerr("Error Err_8888yyy!  strLoc==" + strLoc + ", exc_general==" + exc_general.ToString());
-        }
-
-        ////  Finish Diagnostics
-
-        if (iCountErrors != 0)
-        {
-            Console.WriteLine("FAiL! " + s_strTFName + " ,iCountErrors==" + iCountErrors.ToString());
-        }
-
-        Assert.Equal(0, iCountErrors);
-    }
-
-    public static void printerr(String err, [CallerMemberName] string memberName = "", [CallerFilePath] string filePath = "", [CallerLineNumber] int lineNumber = 0)
-    {
-        Console.WriteLine("ERROR: ({0}, {1}, {2}) {3}", memberName, filePath, lineNumber, err);
     }
 }
---
+++ src/System.IO.FileSystem/tests/GetTime_str.cs
@@ -0,0 +1,128 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.IO;
+using System.Collections;
+using System.Globalization;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using Xunit;
+
+namespace System.IO.FileSystem.Tests
+{
+    public abstract class GetTime_str : FileSystemTest
+    {
+        private readonly string NonExistantFileName = "nonexistant_file_name";
+        private readonly TimeSpan CreateCheckGap = TimeSpan.FromMinutes(8);
+
+        protected abstract DateTime m_Get(string path);
+
+        [Fact]
+        public void FileNotFound()
+        {
+            // If the directory described in the path parameter does not exist,
+            // this method returns 12:00 midnight, January 1, 1601 A.D. (C.E.)
+            // Coordinated Universal Time (UTC), adjusted to local time.
+            DateTime time = m_Get(NonExistantFileName);
+
+            Assert.Equal(time, new DateTime(1601, 1, 1, 0, 0, 0, DateTimeKind.Utc).ToLocalTime());
+        }
+
+        [Fact]
+        public void PathArgumentWhitespace()
+        {
+            // Check ArgumentException is thrown for only whitespace in path
+            Action<string> test = (s) => Assert.Throws<ArgumentException>(() => m_Get(s));
+
+            test("");
+            test("    ");
+            test("  \t\t  ");
+            test("  \t\t\n\n  ");
+            test("\t\t");
+            test("\n\n");
+            test("\x9");
+            test("\xA");
+            test("\xB");
+            test("\xC");
+            test("\xD");
+            test("\x20");
+            test("\x85");
+            test("\xA0");
+        }
+
+        [Fact]
+        public void PathInvalidCharacters()
+        {
+            // Check ArgumentException is thrown for invalid characters in path
+            Action<string> test = (s) => Assert.Throws<ArgumentException>(() => m_Get(s));
+
+            foreach (char c in Path.GetInvalidPathChars())
+                test(c.ToString());
+            test("?");
+            test("*");
+        }
+
+        [Fact]
+        public void PathArgumentNull()
+        {
+            // Check ArgumentNullException is thrown for null path argument
+            Assert.Throws<ArgumentNullException>(() => m_Get(null));
+        }
+
+        [Fact]
+        public void PathTooLong()
+        {
+            // Check PathTooLongException is thrown for too long a path
+            StringBuilder sb = new StringBuilder(TestDirectory);
+            while (sb.Length < IOInputs.MaxPath + 1)
+            {
+                sb.Append("a");
+            }
+
+            Assert.Throws<PathTooLongException>(() => m_Get(sb.ToString()));
+        }
+
+        [Fact]
+        public void DirectoryTooLong()
+        {
+            // Check PathTooLongException is thrown for too long a directory component of path
+            StringBuilder sb = new StringBuilder();
+            while (sb.Length < IOInputs.MaxDirectory + 1)
+            {
+                sb.Append("a");
+            }
+
+            Assert.Throws<PathTooLongException>(() => m_Get(TestDirectory + Path.DirectorySeparatorChar + sb.ToString()));
+        }
+
+        [Fact]
+        public void PathJustShortEnough()
+        {
+            // Check PathTooLongException is not thrown for a path that is just short enough
+            StringBuilder sb = new StringBuilder(TestDirectory + Path.DirectorySeparatorChar);
+            sb.Append(NonExistantFileName);
+            while (sb.Length < IOInputs.MaxPath)
+            {
+                sb.Append("a");
+            }
+
+            m_Get(sb.ToString());
+        }
+
+        [Fact]
+        public void PositiveTests()
+        {
+            // Create a new file / directory
+            // Verify all of the file times are within CreateCheckGap of DateTime.Now
+            TestOnValidFileAndDirectory((path) => 
+            {
+                DateTime time = m_Get(path);
+                Assert.True(CreateCheckGap > time.Subtract(DateTime.Now).Duration());
+            });
+        }
+    }
+
+}
