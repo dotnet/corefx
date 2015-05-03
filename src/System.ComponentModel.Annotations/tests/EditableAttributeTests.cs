95348700f0d6e7ebf60a29dcb700df7014edd1e6
Geoffrey Huntley <ghuntley@ghuntley.com>
2015-05-04 09:01:53 +1000

    added EditableAttributeTest construction test progresses issue #921

---
+++ src/System.ComponentModel.Annotations/tests/EditableAttributeTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace System.ComponentModel.DataAnnotations
+{
+    public class EditableAttributeTests
+    {
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public static void Can_construct_and_both_AllowEdit_and_AllowInitialValue_are_set(bool value)
+        {
+            var attribute = new EditableAttribute(value);
+            Assert.Equal(value, attribute.AllowEdit);
+            Assert.Equal(value, attribute.AllowInitialValue);
+        }
+    }
+}
