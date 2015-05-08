2bbca8b9efd9368442b6d1557b56cc514e3323e4
Stephen Toub <stoub@microsoft.com>
2015-05-08 16:03:14 -0400

    Add test for System.Threading ETW provider
    This adds a test for the synchronization ETW provider and one of its events.  The other two events are used by .NET Native.

---
+++ src/System.Threading/tests/EtwTests.cs
@@ -0,0 +1,35 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics.Tracing;
+using System.Threading;
+using Xunit;
+
+namespace Test
+{
+    public class EtwTests
+    {
+        [Fact]
+        public void TestEtw()
+        {
+            using (var listener = new TestEventListener("System.Threading.SynchronizationEventSource", EventLevel.Verbose))
+            {
+                const int BarrierPhaseFinishedId = 3;
+                const int ExpectedInvocations = 5;
+                int eventsRaised = 0;
+                int delegateInvocations = 0;
+                listener.RunWithCallback(ev => {
+                        Assert.Equal(expected: BarrierPhaseFinishedId, actual: ev.EventId);
+                        eventsRaised++;
+                    },
+                    () => {
+                        Barrier b = new Barrier(1, _ => delegateInvocations++);
+                        for (int i = 0; i < ExpectedInvocations; i++)
+                            b.SignalAndWait();
+                    });
+                Assert.Equal(ExpectedInvocations, delegateInvocations);
+                Assert.Equal(ExpectedInvocations, eventsRaised);
+            }
+        }
+    }
+}
