dff88a38712d8e25012d83231b64f1321f740890
Stephen Toub <stoub@microsoft.com>
2015-05-13 11:42:32 -0400

    Improve UnmanagedMemoryStream code coverage
    Before:
    - UnmanagedMemoryAccessor: 98.8%
    - UnmanagedMemoryStream: 81.3%
    - Overall (including SR): 89.4%
    
    After:
    - UnmanagedMemoryAccessor: 99.7%
    - UnmanagdMemoryStream: 98.1%
    - Overall (including SR): 96.1%
    
    All that remains are some hard-to-reach error paths, e.g. pointer overflow.

---
+++ src/System.IO.UnmanagedMemoryStream/tests/HGlobalSafeBuffer.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace System.IO.Tests
+{
+    internal sealed class HGlobalSafeBuffer : SafeBuffer
+    {
+        internal HGlobalSafeBuffer(int capacity) : base(true)
+        {
+            SetHandle(Marshal.AllocHGlobal(capacity));
+            Initialize((ulong)capacity);
+        }
+
+        protected override bool ReleaseHandle()
+        {
+            Marshal.FreeHGlobal(handle);
+            return true;
+        }
+    }
+}
