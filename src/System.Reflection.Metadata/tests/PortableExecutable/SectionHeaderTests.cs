d1276f85ff7ad734fb02ffee0ced83592c4a869d
Ben Bader <ben@bendb.com>
2015-05-27 09:40:49 -0700

    Use `\u` escape syntax for non-ASCII text

--- src/System.Reflection.Metadata/tests/PortableExecutable/SectionHeaderTests.cs
+++ src/System.Reflection.Metadata/tests/PortableExecutable/SectionHeaderTests.cs
@@ -21,7 +21,7 @@ namespace System.Reflection.Metadata.Tests.PortableExecutable
         [InlineData(".drectve", 0, 0, 26, 0x12C, 0, 0, 0, 0, SectionCharacteristics.Align1Bytes)]
         [InlineData("", 1, 1, 2, 3, 5, 8, 13, 21, SectionCharacteristics.Align16Bytes)]
         [InlineData("x", 1, 1, 2, 3, 5, 8, 13, 21, SectionCharacteristics.MemSysheap)]
-        [InlineData(".बग", int.MaxValue, int.MinValue, int.MaxValue, int.MinValue, int.MaxValue, int.MaxValue, ushort.MaxValue, ushort.MaxValue, SectionCharacteristics.GPRel)]
+        [InlineData(".\u092c\u0917", int.MaxValue, int.MinValue, int.MaxValue, int.MinValue, int.MaxValue, int.MaxValue, ushort.MaxValue, ushort.MaxValue, SectionCharacteristics.GPRel)]
         [InlineData("nul\u0000nul", 1, 1, 1, 1, 1, 1, 1, 1, SectionCharacteristics.ContainsInitializedData)]
         public void Ctor(
             string name,
