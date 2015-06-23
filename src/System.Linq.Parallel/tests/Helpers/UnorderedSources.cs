1c3ecb33008d93d47b2e9a42480ebac50821b617
Clockwork-Muse <clockwork-muse@outlook.com>
2015-06-23 11:30:38 +0900

    Parcel out PlinqDelegateExceptions.cs contents, remove.
    Update ExchangeTests
    
    Update PlinqModesTests.
    
    Remove file (contents covered more thoroughly by other tests).
    
    Change Union tests to use custom Comparator similar to other set operations.
    
    Add List to sources used for OuterLoop
    
    Change how empty tests are handled.
    
    Add tests for non-comparable types.
    
    Add tests checking for duplicate compared items.
    
    Describe behavior of common range sources
    
    Describe Zip member data functions.
    
    Describe Union member data functions.
    
    Describe OrderBy member data functions
    
    Describe Max/Min member data functions.

--- src/System.Linq.Parallel/tests/Helpers/UnorderedSources.cs
+++ src/System.Linq.Parallel/tests/Helpers/UnorderedSources.cs
@@ -10,6 +10,8 @@ namespace Test
     {
         public const int AdditionalTypeLimit = 1024;
 
+        // Get a set of ranges, of each count in `counts`.
+        // The start of each range is determined by passing the count into the `start` predicate.
         private static IEnumerable<object[]> Ranges(Func<int, int> start, IEnumerable<int> counts)
         {
             foreach (int count in counts)
@@ -22,33 +24,80 @@ namespace Test
             }
         }
 
+        /// <summary>
+        /// Get a set of ranges, starting at `start`, and running for each count in `counts`.
+        /// </summary>
+        /// <param name="start">The starting element of the range.</param>
+        /// <param name="counts">The sizes of ranges to return.</param>
+        /// <returns>Entries for test data.
+        /// The first element is the Labeled{ParallelQuery{int}} range,
+        /// the second element is the count, and the third is the start.</returns>
         public static IEnumerable<object[]> Ranges(int start, IEnumerable<int> counts)
         {
             foreach (object[] parms in Ranges(x => start, counts)) yield return parms;
         }
 
-        // Wrapper for attribute calls
+        /// <summary>
+        /// Get a set of ranges, starting at 0, and running for each count in `counts`.
+        /// </summary>
+        /// <remarks>This version is a wrapper for use from the MemberData attribute.</remarks>
+        /// <param name="counts">The sizes of ranges to return.</param>
+        /// <returns>Entries for test data.
+        /// The first element is the Labeled{ParallelQuery{int}} range,
+        /// and the second element is the count</returns>
         public static IEnumerable<object[]> Ranges(object[] counts)
         {
             foreach (object[] parms in Ranges(counts.Cast<int>())) yield return parms;
         }
 
+        /// <summary>
+        /// Get a set of ranges, starting at `start`, and running for each count in `counts`.
+        /// </summary>
+        /// <remarks>This version is a wrapper for use from the MemberData attribute.</remarks>
+        /// <param name="start">The starting element of the range.</param>
+        /// <param name="counts">The sizes of ranges to return.</param>
+        /// <returns>Entries for test data.
+        /// The first element is the Labeled{ParallelQuery{int}} range,
+        /// the second element is the count, and the third is the start.</returns>
         public static IEnumerable<object[]> Ranges(int start, object[] counts)
         {
             foreach (object[] parms in Ranges(start, counts.Cast<int>())) yield return parms;
         }
 
+        /// <summary>
+        /// Return pairs of ranges, both from 0 to each respective count in `counts`.
+        /// </summary>
+        /// <remarks>This version is a wrapper for use from the MemberData attribute.</remarks>
+        /// <param name="leftCounts">The sizes of left ranges to return.</param>
+        /// <param name="rightCounts">The sizes of right ranges to return.</param>
+        /// <returns>Entries for test data.
+        /// The first element is the left Labeled{ParallelQuery{int}} range, the second element is the left count,
+        /// the third element is the right Labeled{ParallelQuery{int}} range, and the fourth element is the right count, .</returns>
         public static IEnumerable<object[]> BinaryRanges(object[] leftCounts, object[] rightCounts)
         {
             foreach (object[] parms in BinaryRanges(leftCounts.Cast<int>(), rightCounts.Cast<int>())) yield return parms;
         }
 
-        // Simple labeled-range source - just return a set of ranges from 0 to each of the counts.
+        /// <summary>
+        /// Get a set of ranges, starting at 0, and running for each count in `counts`.
+        /// </summary>
+        /// <param name="counts">The sizes of ranges to return.</param>
+        /// <returns>Entries for test data.
+        /// The first element is the Labeled{ParallelQuery{int}} range,
+        /// and the second element is the count</returns>
         public static IEnumerable<object[]> Ranges(IEnumerable<int> counts)
         {
             foreach (object[] parms in Ranges(x => 0, counts)) yield return parms.Take(2).ToArray();
         }
 
+        /// <summary>
+        /// Return pairs of ranges, both from 0 to each respective count in `counts`.
+        /// </summary>
+        /// <param name="leftCounts">The sizes of left ranges to return.</param>
+        /// <param name="rightCounts">The sizes of right ranges to return.</param>
+        /// <returns>Entries for test data.
+        /// The first element is the left Labeled{ParallelQuery{int}} range, the second element is the left count,
+        /// the third element is the right Labeled{ParallelQuery{int}} range, and the fourth element is the right count.</returns>
         public static IEnumerable<object[]> BinaryRanges(IEnumerable<int> leftCounts, IEnumerable<int> rightCounts)
         {
             foreach (object[] left in Ranges(leftCounts))
@@ -60,6 +109,16 @@ namespace Test
             }
         }
 
+        /// <summary>
+        /// Return pairs of ranges, for each respective count in `counts`.
+        /// </summary>
+        /// <param name="leftCounts">The sizes of left ranges to return.</param>
+        /// <param name="rightStart">A predicate to determine the start of the right range, by passing the left and right range size.</param>
+        /// <param name="rightCounts">The sizes of right ranges to return.</param>
+        /// <returns>Entries for test data.
+        /// The first element is the left Labeled{ParallelQuery{int}} range, the second element is the left count,
+        /// the third element is the right Labeled{ParallelQuery{int}} range, the fourth element is the right count,
+        /// and the fifth is the right start..</returns>
         public static IEnumerable<object[]> BinaryRanges(IEnumerable<int> leftCounts, Func<int, int, int> rightStart, IEnumerable<int> rightCounts)
         {
             foreach (object[] left in Ranges(leftCounts))
@@ -71,8 +130,17 @@ namespace Test
             }
         }
 
-        // Allows for a set of output modifiers to be passed.
-        // This is useful for things like showing an average (via the use of `x => (double)SumRange(0, x) / x`)
+        /// <summary>
+        /// Get a set of ranges, starting at 0, and running for each count in `counts`.
+        /// </summary>
+        /// <remarks>
+        /// This is useful for things like showing an average (via the use of `x => (double)SumRange(0, x) / x`)
+        /// </remarks>
+        /// <param name="counts">The sizes of ranges to return.</param>
+        /// <param name="modifiers">A set of modifiers to return as additional parameters.</param>
+        /// <returns>Entries for test data.
+        /// The first element is the Labeled{ParallelQuery{int}} range,
+        /// the second element is the count, and one additional element for each modifier.</returns>
         public static IEnumerable<object[]> Ranges<T>(IEnumerable<int> counts, params Func<int, T>[] modifiers)
         {
             if (modifiers == null || !modifiers.Any())
@@ -89,10 +157,19 @@ namespace Test
             }
         }
 
-        // For each count, each modifier has multiple outputs.
-        // This is useful for things like dealing with `Max(predicate)`,
-        // allowing multiple predicate values for the same source count to be tested.
-        // The number of variations is equal to the longest inner enumeration (all others will cycle).
+        /// <summary>
+        /// Get a set of ranges, starting at 0, and running for each count in `counts`.
+        /// </summary>
+        /// <remarks>
+        /// This is useful for things like dealing with `Max(predicate)`,
+        /// allowing multiple predicate values for the same source count to be tested.
+        /// The number of variations is equal to the longest modifier enumeration (all others will cycle).
+        /// </remarks>
+        /// <param name="counts">The sizes of ranges to return.</param>
+        /// <param name="modifiers">A set of modifiers to return as additional parameters.</param>
+        /// <returns>Entries for test data.
+        /// The first element is the Labeled{ParallelQuery{int}} range,
+        /// the second element is the count, and one additional element for each modifier.</returns>
         public static IEnumerable<object[]> Ranges<T>(IEnumerable<int> counts, params Func<int, IEnumerable<T>>[] modifiers)
         {
             if (modifiers == null || !modifiers.Any())
@@ -119,11 +196,11 @@ namespace Test
             yield return Labeled.Label("Enumerable.Range", Enumerable.Range(start, count).AsParallel());
             int[] rangeArray = GetRangeArray(start, count);
             yield return Labeled.Label("Array", rangeArray.AsParallel());
+            IList<int> rangeList = rangeArray.ToList();
+            yield return Labeled.Label("List", rangeList.AsParallel());
             if (count < AdditionalTypeLimit + 1)
             {
                 yield return Labeled.Label("Partitioner", Partitioner.Create(rangeArray).AsParallel());
-                IList<int> rangeList = rangeArray.ToList();
-                yield return Labeled.Label("List", rangeList.AsParallel());
                 yield return Labeled.Label("ReadOnlyCollection", new ReadOnlyCollection<int>(rangeList).AsParallel());
             }
         }
