be8c79c9ded1e8f7a0f6b435300c9da61053391e
Jon Hanna <jon@hackcraft.net>
2015-07-02 18:12:39 +0100

    Tests for single.

---
+++ src/System.Linq/tests/SingleTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq.Tests.Helpers;
+using Xunit;
+
+namespace System.Linq.Tests
+{
+    public class SingleTests
+    {
+        [Fact]
+        public void FailOnEmpty()
+        {
+            Assert.Throws<InvalidOperationException>(() => Enumerable.Empty<int>().Single());
+        }
+
+        [Fact]
+        public void DefaultOnEmpty()
+        {
+            Assert.Equal(0, Enumerable.Empty<int>().SingleOrDefault());
+        }
+
+        // Warning: At time of writing these two tests, they will incorrectly run for about five millennia.
+        // They'll then probably throw OverflowException rather than the correct InvalidOperationException,
+        // but I haven't had time to confirm.
+
+        [Fact]
+        public void FailOnManyMatches()
+        {
+            Assert.Throws<InvalidOperationException>(() => Enumerable.Range(0, int.MaxValue).Single(i => i > 0));
+        }
+
+        [Fact]
+        public void DefaultFailOnManyMatches()
+        {
+            Assert.Throws<InvalidOperationException>(() => Enumerable.Range(0, int.MaxValue).SingleOrDefault(i => i > 0));
+        }
+
+        [Fact]
+        public void FindSingle()
+        {
+            Assert.Equal(42, Enumerable.Repeat(42, 1).Single());
+        }
+
+        [Theory]
+        [InlineData(1, 100)]
+        [InlineData(42, 100)]
+        public void FindSingleMatch(int target, int range)
+        {
+            Assert.Equal(target, Enumerable.Range(0, range).Single(i => i == target));
+        }
+    }
+}
