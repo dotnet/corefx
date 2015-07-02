acf5808fe00583acc0f7fcc36b9adbb26619f91d
Jon Hanna <jon@hackcraft.net>
2015-07-03 00:44:41 +0100

    Skip tests.

---
+++ src/System.Linq/tests/SkipTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace System.Linq.Tests
+{
+    public class SkipTests
+    {
+        [Fact]
+        public void SkipSome()
+        {
+            Assert.Equal(Enumerable.Range(10, 10), Enumerable.Range(0, 20).Skip(10));
+        }
+
+        [Fact]
+        public void SkipExcessive()
+        {
+            Assert.Equal(Enumerable.Empty<int>(), Enumerable.Range(0, 20).Skip(42));
+        }
+
+        [Fact]
+        public void SkipAllExactly()
+        {
+            Assert.False(Enumerable.Range(0, 20).Skip(20).Any());
+        }
+
+        [Fact]
+        public void SkipThrowsOnNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => ((IEnumerable<DateTime>)null).Skip(3));
+        }
+
+        [Fact]
+        public void SkipNegative()
+        {
+            Assert.Equal(Enumerable.Range(0, 20), Enumerable.Range(0, 20).Skip(-42));
+        }
+    }
+}
