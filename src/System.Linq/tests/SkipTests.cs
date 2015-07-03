cf29a9ebacaf8714df3a8556065587f261111f12
Jon Hanna <jon@hackcraft.net>
2015-07-03 01:58:22 +0100

    SkipWhile tests.

--- src/System.Linq/tests/SkipTests.cs
+++ src/System.Linq/tests/SkipTests.cs
@@ -34,5 +34,35 @@ namespace System.Linq.Tests
         {
             Assert.Equal(Enumerable.Range(0, 20), Enumerable.Range(0, 20).Skip(-42));
         }
+
+        [Fact]
+        public void SkipWhileAllTrue()
+        {
+            Assert.Equal(Enumerable.Empty<int>(), Enumerable.Range(0, 20).SkipWhile(i => i < 40));
+            Assert.Equal(Enumerable.Empty<int>(), Enumerable.Range(0, 20).SkipWhile((i, idx) => i == idx));
+        }
+
+        [Fact]
+        public void SkipWhileAllFalse()
+        {
+            Assert.Equal(Enumerable.Range(0, 20), Enumerable.Range(0, 20).SkipWhile(i => i != 0));
+            Assert.Equal(Enumerable.Range(0, 20), Enumerable.Range(0, 20).SkipWhile((i, idx) => i != idx));
+        }
+
+        [Fact]
+        public void SkipWhileThrowsOnNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => ((IEnumerable<int>)null).SkipWhile(i => i < 40));
+            Assert.Throws<ArgumentNullException>(() => ((IEnumerable<int>)null).SkipWhile((i, idx) => i == idx));
+            Assert.Throws<ArgumentNullException>(() => Enumerable.Range(0, 20).SkipWhile((Func<int, int, bool>)null));
+            Assert.Throws<ArgumentNullException>(() => Enumerable.Range(0, 20).SkipWhile((Func<int, bool>)null));
+        }
+
+        [Fact]
+        public void SkipWhileHalf()
+        {
+            Assert.Equal(Enumerable.Range(10, 10), Enumerable.Range(0, 20).SkipWhile(i => i < 10));
+            Assert.Equal(Enumerable.Range(10, 10), Enumerable.Range(0, 20).SkipWhile((i, idx) => idx < 10));
+        }
     }
 }
