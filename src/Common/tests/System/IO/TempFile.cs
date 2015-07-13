6f1a32cc4d969df6fa461e4507447f3ecb30f4fb
stephentoub <stoub@microsoft.com>
2015-07-13 12:06:42 -0400

    Add Common test helper classes for file cleanup
    Later one we can consolidate to these with the ones used in other test projects (and have additional test projects use these to avoid leaving straggling test files behind).

---
+++ src/Common/tests/System/IO/TempFile.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace System.IO
+{
+    /// <summary>
+    /// Represents a temporary file.  Creating an instance creates a file at the specified path,
+    /// and disposing the instance deletes the file.
+    /// </summary>
+    public sealed class TempFile : IDisposable
+    {
+        /// <summary>Gets the created file's path.</summary>
+        public string Path { get; private set; }
+
+        public TempFile(string path, long length = 0)
+        {
+            Path = path;
+            using (FileStream fs = File.Create(path))
+            {
+                if (length > 0)
+                {
+                    // Fill with zeros up to the desired length.
+                    fs.Write(new byte[length], 0, (int)length);
+                }
+            }
+        }
+
+        ~TempFile() { DeleteFile(); }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            DeleteFile();
+        }
+
+        private void DeleteFile()
+        {
+            File.Delete(Path);
+        }
+    }
+}
