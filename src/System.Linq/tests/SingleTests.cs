f8faadf56ed646d5997a44efaf12bbc8aad78a4a
Jon Hanna <jon@hackcraft.net>
2015-07-14 00:43:09 +0100

    More tests for enumerable.
    Tests originally commited as part of pull-request #2237
    moved out into separate pull-request.
    
    Consistently favour type names over c# names.
    
    Single element sequences on Min and Max
    
    (What other edge cases should we hit?)
    
    MinValue and MaxValue for element types.
    
    Min & Max with selector
    
    Remove tests on short-circuit/not short-circuit for now.
    
    More skip tests.

---
+++ src/System.Linq/tests/SingleTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq.Tests.Helpers;
+using Xunit;
+
+namespace System.Linq.Tests
+{
+    public class SingleTests
+    {
+        [Fact]
+        public void FailOnEmpty()
+        {
+            Assert.Throws<InvalidOperationException>(() => Enumerable.Empty<int>().Single());
+        }
+
+        [Fact]
+        public void DefaultOnEmpty()
+        {
+            Assert.Equal(0, Enumerable.Empty<int>().SingleOrDefault());
+        }
+
+        [Fact]
+        public void FindSingle()
+        {
+            Assert.Equal(42, Enumerable.Repeat(42, 1).Single());
+        }
+
+        [Theory]
+        [InlineData(1, 100)]
+        [InlineData(42, 100)]
+        public void FindSingleMatch(int target, int range)
+        {
+            Assert.Equal(target, Enumerable.Range(0, range).Single(i => i == target));
+        }
+    }
+}
