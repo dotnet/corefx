ce0506baea623efdb74707aee0e4aa169d6f468c
Jeremy Barton <jbarton@microsoft.com>
2015-07-24 08:13:47 -0700

    Repartiton crypto contracts, and refresh the API set.
    Some of the contracts were moved in their entirety:
    
    System.Security.Cryptography.Encryption            => System.Security.Cryptography.Primitives
    System.Security.Cryptography.Hashing               => System.Security.Cryptography.Primitives
    
    System.Security.Cryptography.RandomNumberGenerator => System.Security.Cryptography.Algorithms
    System.Security.Cryptography.Encryption.Aes        => System.Security.Cryptography.Algorithms
    System.Security.Cryptography.Hashing.Algorithms    => System.Security.Cryptography.Algorithms
    System.Security.Cryptography.DeriveBytes           => System.Security.Cryptography.Algorithms
    
    The only partitioned library was System.Security.Cryptography.RSA:
      The RSA base class and supporting types                            => System.Security.Cryptography.Algorithms
      The RSACryptoServiceProvider class and additional supporting types => System.Security.Cryptography.Csp
      The internal RSAOpenSsl type was made public                       => System.Security.Cryptography.OpenSsl
      RSACng was copied from desktop                                     => System.Security.Cryptography.Cng
    
    This change also pulls in the RSA contract refresh from 4.6.
    It does not yet add RSA.Create() to do the right reflection-forward lookups (the change was already big enough)
    It also has the sad side effect of turning off the RSA tests for Linux, but fixing that problem is high on my list after getting the packages published. (I wanted to keep as many files as possible intact so they show as moves both in TFS and on github... rename+edit in TFS will likely not show up as cleanly in git).
    Git builds will continue to have test coverage, but with the old contracts disabled ToF tests will shut down.  ToF tests will be re-enabled in a followup change.
    
    [tfs-changeset: 1505642]

---
+++ src/System.Security.Cryptography.Algorithms/src/Internal/Cryptography/Helpers.cs
@@ -0,0 +1,70 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace Internal.Cryptography
+{
+    internal static class Helpers
+    {
+        public static byte[] CloneByteArray(this byte[] src)
+        {
+            return (byte[])(src.Clone());
+        }
+
+        public static bool UsesIv(this CipherMode cipherMode)
+        {
+            return cipherMode != CipherMode.ECB;
+        }
+
+        public static bool IsLegalSize(this int size, KeySizes[] legalSizes)
+        {
+            for (int i = 0; i < legalSizes.Length; i++)
+            {
+                // If a cipher has only one valid key size, MinSize == MaxSize and SkipSize will be 0
+                if (legalSizes[i].SkipSize == 0)
+                {
+                    if (legalSizes[i].MinSize == size)
+                        return true;
+                }
+                else
+                {
+                    for (int j = legalSizes[i].MinSize; j <= legalSizes[i].MaxSize; j += legalSizes[i].SkipSize)
+                    {
+                        if (j == size)
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
+        
+        public static byte[] GenerateRandom(int count)
+        {
+            byte[] buffer = new byte[count];
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(buffer);
+            }
+            return buffer;
+        }
+
+        // encodes the integer i into a 4-byte array, in big endian.
+        public static void WriteInt(uint i, byte[] arr, int offset)
+        {
+            unchecked
+            {
+                Debug.Assert(arr != null);
+                Debug.Assert(arr.Length >= offset + sizeof(uint));
+
+                arr[offset] = (byte)(i >> 24);
+                arr[offset + 1] = (byte)(i >> 16);
+                arr[offset + 2] = (byte)(i >> 8);
+                arr[offset + 3] = (byte)i;
+            }
+        }
+    }
+}
+
