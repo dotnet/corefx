87fe10e4415728089834f46aed891b514bf83788
bartonjs <jbarton@microsoft.com>
2015-07-27 11:26:15 -0700

    Unify crypto tests on Common\tests\Streams\PositionValueStream.
    Moved the namespace to Test.IO.Streams, so that it bespeaks of the ability to be shared.

--- src/Common/tests/Cryptography/AlgorithmImplementations/RSA/SignVerify.cs
+++ src/Common/tests/Cryptography/AlgorithmImplementations/RSA/SignVerify.cs
@@ -4,6 +4,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Test.IO.Streams;
 using Xunit;
 
 namespace System.Security.Cryptography.Rsa.Tests
--- src/System.Security.Cryptography.Primitives/tests/SimpleHashAlgorithmTest.cs
+++ src/System.Security.Cryptography.Primitives/tests/SimpleHashAlgorithmTest.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.IO;
+using Test.IO.Streams;
 using Xunit;
 
 namespace System.Security.Cryptography.Hashing.Tests
