75f839d3c4d542a8b21d34acd0956d6366b0e1b3
Pat Gavlin <pagavlin@microsoft.com>
2015-07-30 12:40:52 -0700

    Implement System.Net.Primitives for *nix.
    Platform abstractions have been added for IP address parsing and host information.
    The closest APIs that most POSIX systems have to the NTDLL IP address parsing APIs
    are getaddrinfo/getnameinfo. In the case of IPv6 in particular, inet_ntop/inet_pton
    are not sufficient, as these APIs do not support scope IDs.

--- src/Common/src/Interop/Unix/libc/Interop.getdomainname.cs
+++ src/Common/src/Interop/Unix/libc/Interop.getdomainname.cs
@@ -11,24 +11,24 @@ internal static partial class Interop
     internal static partial class libc
     {
         [DllImport(Libraries.Libc, SetLastError = true)]
-        private static extern unsafe int gethostname(byte* name, int len);
+        private static extern unsafe int getdomainname(byte* name, int len);
 
-        internal static unsafe string gethostname()
+        internal static unsafe string getdomainname()
         {
-            const int HOST_NAME_MAX = 255; // man gethostname
+            const int HOST_NAME_MAX = 255; // man getdomainname
             const int ArrLength = HOST_NAME_MAX + 1;
 
             byte* name = stackalloc byte[ArrLength];
-            int err = gethostname(name, ArrLength);
+            int err = getdomainname(name, ArrLength);
             if (err != 0)
             {
                 // This should never happen.  According to the man page,
-                // the only possible errno for gethostname is ENAMETOOLONG,
+                // the only possible errno for getdomainname is ENAMETOOLONG,
                 // which should only happen if the buffer we supply isn't big
                 // enough, and we're using a buffer size that the man page
                 // says is the max for POSIX (and larger than the max for Linux).
-                Debug.Fail("gethostname failed");
-                throw new InvalidOperationException(string.Format("gethostname returned {0}", err));
+                Debug.Fail("getdomainname failed");
+                throw new InvalidOperationException(string.Format("getdomainname returned {0}", err));
             }
 
             // Marshal.PtrToStringAnsi uses UTF8 on Unix.
