f65ebf6e311fdf5f68418e9f9764907532b2e2d2
Nick Guerrera <nicholg@microsoft.com>
2015-07-31 22:31:44 -0700

    Add errno shim

--- src/Native/configure.cmake
+++ src/Native/configure.cmake
@@ -1,5 +1,6 @@
 include(CheckFunctionExists)
 include(CheckStructHasMember)
+include(CheckCXXSourceCompiles)
 
 check_function_exists(
     stat64
@@ -10,6 +11,13 @@ check_struct_has_member(
     st_birthtime
     "sys/types.h;sys/stat.h"
     HAVE_STAT_BIRTHTIME)
+
+check_cxx_source_compiles(
+    "
+    #include <string.h>
+    int main() { char* c = strerror_r(0, 0, 0); }
+    "
+    HAVE_GNU_STRERROR_R)
 
 configure_file(
     ${CMAKE_CURRENT_SOURCE_DIR}/config.h.in
