26857972862e5dfa8901abe955ae6f6edab3624c
Atsushi Kanamori <atsushik@microsoft.com>
2015-08-05 06:31:08 -0700

    Implement System.Security.Cryptography.CngKey

---
+++ src/System.Security.Cryptography.Cng/tests/PropertyTests.cs
@@ -0,0 +1,110 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Test.Cryptography;
+using Xunit;
+
+namespace System.Security.Cryptography.Cng.Tests
+{
+    public static class PropertyTests
+    {
+        [Fact]
+        public static void GetProperty_NoSuchProperty()
+        {
+            using (CngKey key = CngKey.Import(TestData.Key_ECDiffieHellmanP256, CngKeyBlobFormat.GenericPublicBlob))
+            {
+                Assert.Throws<CryptographicException>(() => key.GetProperty("DOES NOT EXIST", CngPropertyOptions.CustomProperty));
+            }
+        }
+
+        [Fact]
+        public static void SetPropertyZeroLengthCornerCase()
+        {
+            using (CngKey key = CngKey.Import(TestData.Key_ECDiffieHellmanP256, CngKeyBlobFormat.GenericPublicBlob))
+            {
+                const string propertyName = "CustomZeroLengthProperty";
+                CngProperty p = new CngProperty(propertyName, new byte[0], CngPropertyOptions.CustomProperty);
+                key.SetProperty(p);
+
+                CngProperty p2 = key.GetProperty(propertyName, CngPropertyOptions.CustomProperty);
+                Assert.Equal(propertyName, p2.Name);
+                Assert.Equal(CngPropertyOptions.CustomProperty, p2.Options);
+
+                // This one is odd. CNG keys can have properties with zero length but CngKey.GetProperty() transforms this into null.
+                Assert.Equal(null, p2.GetValue());
+            }
+        }
+
+        [Fact]
+        public static void SetPropertyNullCornerCase()
+        {
+            using (CngKey key = CngKey.Import(TestData.Key_ECDiffieHellmanP256, CngKeyBlobFormat.GenericPublicBlob))
+            {
+                const string propertyName = "CustomNullProperty";
+                CngProperty p = new CngProperty(propertyName, null, CngPropertyOptions.CustomProperty);
+                Assert.Throws<CryptographicException>(() => key.SetProperty(p));
+            }
+        }
+
+        [Fact]
+        public static void HasProperty()
+        {
+            using (CngKey key = CngKey.Import(TestData.Key_ECDiffieHellmanP256, CngKeyBlobFormat.GenericPublicBlob))
+            {
+                const string propertyName = "CustomProperty";
+                bool hasProperty;
+
+                hasProperty = key.HasProperty(propertyName, CngPropertyOptions.CustomProperty);
+                Assert.False(hasProperty);
+
+                key.SetProperty(new CngProperty(propertyName, new byte[0], CngPropertyOptions.CustomProperty));
+                hasProperty = key.HasProperty(propertyName, CngPropertyOptions.CustomProperty);
+                Assert.True(hasProperty);
+            }
+        }
+
+        [Fact]
+        public static void GetAndSetProperties()
+        {
+            using (CngKey key = CngKey.Import(TestData.Key_ECDiffieHellmanP256, CngKeyBlobFormat.GenericPublicBlob))
+            {
+                string propertyName = "Are you there";
+                bool hasProperty = key.HasProperty(propertyName, CngPropertyOptions.CustomProperty);
+                Assert.False(hasProperty);
+
+                byte[] propertyValue = { 1, 2, 3 };
+                CngProperty property = new CngProperty(propertyName, propertyValue, CngPropertyOptions.CustomProperty);
+                key.SetProperty(property);
+
+                byte[] actualValue = key.GetProperty(propertyName, CngPropertyOptions.CustomProperty).GetValue();
+                Assert.Equal<byte>(propertyValue, actualValue);
+            }
+        }
+
+        [Fact]
+        public static void OverwriteProperties()
+        {
+            using (CngKey key = CngKey.Import(TestData.Key_ECDiffieHellmanP256, CngKeyBlobFormat.GenericPublicBlob))
+            {
+                string propertyName = "Are you there";
+                bool hasProperty = key.HasProperty(propertyName, CngPropertyOptions.CustomProperty);
+                Assert.False(hasProperty);
+
+                // Set it once.
+                byte[] propertyValue = { 1, 2, 3 };
+                CngProperty property = new CngProperty(propertyName, propertyValue, CngPropertyOptions.CustomProperty);
+                key.SetProperty(property);
+
+                // Set it again.
+                propertyValue = new byte[] { 5, 6, 7 };
+                property = new CngProperty(propertyName, propertyValue, CngPropertyOptions.CustomProperty);
+                key.SetProperty(property);
+
+                CngProperty retrievedProperty = key.GetProperty(propertyName, CngPropertyOptions.CustomProperty);
+                Assert.Equal(propertyName, retrievedProperty.Name);
+                Assert.Equal<byte>(propertyValue, retrievedProperty.GetValue());
+                Assert.Equal(CngPropertyOptions.CustomProperty, retrievedProperty.Options);
+            }
+        }
+    }
+}
