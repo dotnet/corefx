dotnet/corefx

commit b00fed79842d13d9931cf963749aa00006fdacc0

Eliminate extra IFileSystemObject allocation on Win32 and Unix
FileSystemInfo/FileInfo/DirectoryInfo all carry a IFileSystemObject
which is used internally for the actual implementation. While this
enables FileSystemInfo/FileInfo/DirectoryInfo to be used with WinRT,
it comes with the cost of an extra unnecessary allocation for plain
Win32 and Unix. This change eliminates the allocation on Win32 and
Unix. After this change, the extra object allocation only happens
when the WinRT APIs need to be used.

src/System.IO.FileSystem/src/System/IO/MultiplexingWin32WinRTFileSystem.cs
@@ -12,6 +12,13 @@ namespace System.IO
         private readonly FileSystem _win32FileSystem = new Win32FileSystem();
         private readonly FileSystem _winRTFileSystem = new WinRTFileSystem();
 
+        internal static IFileSystemObject GetFileSystemObject(FileSystemInfo caller, string fullPath)
+        {
+            return ShouldUseWinRT(sourceFullPath, isCreate: false) ?
+                (IFileSystemObject)new WinRTFileSystemObject(fullPath, asDirectory: caller is DirectoryInfo) :
+                (IFileSystemObject)caller;
+        }
+
         public override int MaxPath { get { return Interop.mincore.MAX_PATH; } }
         public override int MaxDirectoryPath { get { return Interop.mincore.MAX_DIRECTORY_PATH; } }
 
@@ -141,7 +148,7 @@ namespace System.IO
             return (ShouldUseWinRT(sourceFullPath, isCreate: false) || ShouldUseWinRT(destFullPath, isCreate: true)) ? _winRTFileSystem : _win32FileSystem;
         }
 
-        private bool ShouldUseWinRT(string fullPath, bool isCreate)
+        private static bool ShouldUseWinRT(string fullPath, bool isCreate)
         {
             // The purpose of this method is to determine if we can access a path
             // via Win32 or if we need to fallback to WinRT.

src/Common/src/Interop/Windows/mincore/Interop.GetFileAttributesEx.cs
@@ -30,7 +30,7 @@ internal partial class Interop
             internal uint fileSizeHigh;
             internal uint fileSizeLow;
 
-            internal void PopulateFrom(WIN32_FIND_DATA findData)
+            internal void PopulateFrom(ref WIN32_FIND_DATA findData)
             {
                 // Copy the information to data
                 fileAttributes = (int)findData.dwFileAttributes;

src/System.IO.FileSystem/src/System.IO.FileSystem.csproj
@@ -57,6 +57,8 @@
   <ItemGroup Condition="'$(TargetsWindows)' == 'true'">
     <Compile Include="Microsoft\Win32\SafeHandles\SafeFileHandle.Windows.cs" />
     <Compile Include="Microsoft\Win32\SafeHandles\SafeFindHandle.Windows.cs" />
+    <Compile Include="System\IO\DirectoryInfo.Windows.cs" />
+    <Compile Include="System\IO\FileInfo.Windows.cs" />
     <Compile Include="System\IO\FileStream.Win32.cs" />
     <Compile Include="System\IO\FileSystemInfo.Windows.cs" />
     <Compile Include="System\IO\PathHelpers.Windows.cs" />
@@ -191,6 +193,7 @@
       <Link>Common\Interop\Windows\Interop.CopyFile.cs</Link>
     </Compile>
     <Compile Include="System\IO\FileSystem.Current.Win32.cs" />
+    <Compile Include="System\IO\FileSystemInfo.Win32.cs" />
     <Compile Include="$(CommonPath)\Interop\Windows\mincore\Interop.CreateFile.cs">
       <Link>Common\Interop\Windows\Interop.CreateFile.cs</Link>
     </Compile>
@@ -219,6 +222,7 @@
       <Link>Common\Interop\Windows\WinRT\Interop.SetErrorMode.cs</Link>
     </Compile>
     <Compile Include="System\IO\FileSystem.Current.MuxWin32WinRT.cs" />
+    <Compile Include="System\IO\FileSystemInfo.WinRT.cs" />
     <Compile Include="System\IO\MultiplexingWin32WinRTFileSystem.cs" />
     <Compile Include="System\IO\WinRTIOExtensions.cs" />
     <Compile Include="System\IO\WinRTFileStream.cs" />

src/System.IO.FileSystem/src/System/IO/DirectoryInfo.Windows.cs
@@ -1,0 +1,19 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics;
+using System.Security;
+
+namespace System.IO
+{
+    partial class DirectoryInfo
+    {
+        [SecurityCritical]
+        internal DirectoryInfo(string fullPath, ref Interop.mincore.WIN32_FIND_DATA findData)
+            : this(fullPath, findData.cFileName)
+        {
+            Debug.Assert(string.Equals(findData.cFileName, Path.GetFileName(fullPath), StringComparison.Ordinal));
+            Init(ref findData);
+        }
+    }
+}

src/System.IO.FileSystem/src/System/IO/DirectoryInfo.cs
@@ -8,7 +8,7 @@ using System.Security;
 
 namespace System.IO
 {
-    public sealed class DirectoryInfo : FileSystemInfo
+    public sealed partial class DirectoryInfo : FileSystemInfo
     {
         [System.Security.SecuritySafeCritical]
         public DirectoryInfo(String path)
@@ -23,12 +23,12 @@ namespace System.IO
         }
 
         [System.Security.SecuritySafeCritical]
-        internal DirectoryInfo(String fullPath, IFileSystemObject fileSystemObject) : base(fileSystemObject)
+        internal DirectoryInfo(String fullPath, String originalPath)
         {
-            Debug.Assert(PathInternal.GetRootLength(fullPath) > 0, "fullPath must be fully qualified!");
-            
+            Debug.Assert(Path.IsPathRooted(fullPath), "fullPath must be fully qualified!");
+
             // Fast path when we know a DirectoryInfo exists.
-            OriginalPath = Path.GetFileName(fullPath);
+            OriginalPath = originalPath ?? Path.GetFileName(fullPath);
             FullPath = fullPath;
             DisplayPath = GetDisplayName(OriginalPath, FullPath);
         }

src/System.IO.FileSystem/src/System/IO/FileInfo.Windows.cs
@@ -1,0 +1,19 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics;
+using System.Security;
+
+namespace System.IO
+{
+    partial class FileInfo
+    {
+        [SecurityCritical]
+        internal FileInfo(string fullPath, ref Interop.mincore.WIN32_FIND_DATA findData)
+            : this(fullPath, findData.cFileName)
+        {
+            Debug.Assert(string.Equals(findData.cFileName, Path.GetFileName(fullPath), StringComparison.Ordinal));
+            Init(ref findData);
+        }
+    }
+}

src/System.IO.FileSystem/src/System/IO/FileInfo.cs
@@ -13,7 +13,7 @@ namespace System.IO
 {
     // Class for creating FileStream objects, and some basic file management
     // routines such as Delete, etc.
-    public sealed class FileInfo : FileSystemInfo
+    public sealed partial class FileInfo : FileSystemInfo
     {
         private String _name;
 
@@ -48,10 +48,10 @@ namespace System.IO
         }
 
         [System.Security.SecuritySafeCritical]
-        internal FileInfo(String fullPath, IFileSystemObject fileSystemObject) : base(fileSystemObject)
+        internal FileInfo(String fullPath, String originalPath)
         {
             Debug.Assert(Path.IsPathRooted(fullPath), "fullPath must be fully qualified!");
-            _name = Path.GetFileName(fullPath);
+            _name = originalPath ?? Path.GetFileName(fullPath);
             OriginalPath = _name;
             FullPath = fullPath;
             DisplayPath = _name;

commit 0d0c501379b6091b4613cac512a1c22dd9db16d6

Fix regression caused by https://github.com/dotnet/corefx/commit/b00fed79842d13d9931cf963749aa00006fdacc0
[tfs-changeset: 1514359]

src/System.IO.FileSystem/src/System/IO/MultiplexingWin32WinRTFileSystem.cs
@@ -14,8 +14,8 @@ namespace System.IO
 
         internal static IFileSystemObject GetFileSystemObject(FileSystemInfo caller, string fullPath)
         {
-            return ShouldUseWinRT(sourceFullPath, isCreate: false) ?
-                (IFileSystemObject)new WinRTFileSystemObject(fullPath, asDirectory: caller is DirectoryInfo) :
+            return ShouldUseWinRT(fullPath, isCreate: false) ?
+                (IFileSystemObject)new WinRTFileSystem.WinRTFileSystemObject(fullPath, asDirectory: caller is DirectoryInfo) :
                 (IFileSystemObject)caller;
         }
 