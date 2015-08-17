ed6c51db567a4f4094931f16a0a6794f942377d5
stephentoub <stoub@microsoft.com>
2015-08-17 15:16:21 -0400

    Address PR feedback
    - Added PersistedFiles.GetTempFeatureDirectory and used it from MemoryMappedFiles (can't be used well from Pipes due to assembly layering)
    - Added directory existence check in Pipes before creating the directory structure
    - Recreate the pipes path when needed if it's deleted during the execution of the app (maps is already doing this)

--- src/System.IO.Pipes/src/System/IO/Pipes/PipeStream.Unix.cs
+++ src/System.IO.Pipes/src/System/IO/Pipes/PipeStream.Unix.cs
@@ -37,7 +37,7 @@ namespace System.IO.Pipes
             }
 
             // Return the pipe path
-            return Path.Combine(GetPipeDirectoryPath(), pipeName);
+            return Path.Combine(EnsurePipeDirectoryPath(), pipeName);
         }
 
         /// <summary>Throws an exception if the supplied handle does not represent a valid pipe.</summary>
@@ -228,31 +228,57 @@ namespace System.IO.Pipes
 
         private static string s_pipeDirectoryPath;
 
-        private static string GetPipeDirectoryPath()
+        private static string EnsurePipeDirectoryPath()
         {
-            string path = s_pipeDirectoryPath;
-            if (path == null)
+            const string PipesFeatureName = "pipes";
+
+            // Ideally this would simply use PersistedFiles.GetTempFeatureDirectory(PipesFeatureName) and then
+            // Directory.CreateDirectory to ensure it exists.  But this assembly doesn't reference System.IO.FileSystem.
+            // As such, we'd be calling GetTempFeatureDirectory, only to then need to parse it in order
+            // to create each of the individual directories as part of the path.  We instead access the named portions 
+            // of the path directly and do the building of the path and directory structure manually.
+
+            // First ensure we know what the full path should be, e.g. /tmp/.dotnet/corefx/pipes/
+            string fullPath = s_pipeDirectoryPath;
+            string tempPath = null;
+            if (fullPath == null)
             {
-                // Create the pipes temporary directory, e.g. "/tmp/.dotnet/corefx/pipes".
-                // We don't have access to Directory.CreateDirectory (which handles creating
-                // all levels of the directory full path specified), so we create each nested
-                // level individually (though we assume that the temp path already exists).
-
-                path = Path.GetTempPath();
-
-                path = Path.Combine(path, PersistedFiles.TopLevelHiddenDirectory);
-                CreateDirectory(path);
-
-                path = Path.Combine(path, PersistedFiles.SecondLevelDirectory);
-                CreateDirectory(path);
-
-                const string PipesFeatureName = "pipes";
-                path = Path.Combine(path, PipesFeatureName);
-                CreateDirectory(path);
-
-                s_pipeDirectoryPath = path;
+                tempPath = Path.GetTempPath();
+                fullPath = Path.Combine(tempPath, PersistedFiles.TopLevelHiddenDirectory, PersistedFiles.SecondLevelDirectory, PipesFeatureName);
+                s_pipeDirectoryPath = fullPath;
             }
-            return path;
+
+            // Then create the directory if it doesn't already exist.  If we get any error back from stat,
+            // just proceed to build up the directory, failing in the CreateDirectory calls if there's some
+            // problem.  Similarly, it's possible stat succeeds but the path is a file rather than directory; we'll
+            // call that success for now and let this fail later when the code tries to create a file in this "directory"
+            // (we don't want to overwrite/delete whatever that unknown file may be, and this is similar to other cases
+            // we can't control where the file system is manipulated concurrently with and separately from this code).
+            Interop.Sys.FileStatus ignored;
+            bool pathExists = Interop.Sys.Stat(fullPath, out ignored) == 0;
+            if (!pathExists)
+            {
+                // We need to build up the directory manually.  Ensure we have the temp directory in which
+                // we'll create the structure, e.g. /tmp/
+                if (tempPath == null)
+                {
+                    tempPath = Path.GetTempPath();
+                }
+                Debug.Assert(Interop.Sys.Stat(tempPath, out ignored) == 0, "Path.GetTempPath() directory could not be accessed");
+
+                // Create /tmp/.dotnet/ if it doesn't exist.
+                string partialPath = Path.Combine(tempPath, PersistedFiles.TopLevelHiddenDirectory);
+                CreateDirectory(partialPath);
+
+                // Create /tmp/.dotnet/corefx/ if it doesn't exist
+                partialPath = Path.Combine(partialPath, PersistedFiles.SecondLevelDirectory);
+                CreateDirectory(partialPath);
+
+                // Create /tmp/.dotnet/corefx/pipes/ if it doesn't exist
+                CreateDirectory(fullPath);
+            }
+
+            return fullPath;
         }
 
         private static void CreateDirectory(string directoryPath)
