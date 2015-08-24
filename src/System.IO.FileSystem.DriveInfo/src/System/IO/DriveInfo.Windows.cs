6a0beb6b176ad555abe6ba7d9992245400d8f442
Ian Hays <ianha@microsoft.com>
2015-08-24 16:31:16 -0700

    Add coverage to DriveInfo tests
    FileSystem.DriveInfo.dll coverage raised to at or near testable maximum. Added several scenarios that weren't present before and also split up Volume Label setting/getting to several tests so that errors can be better diagnosed.

--- src/System.IO.FileSystem.DriveInfo/src/System/IO/DriveInfo.Windows.cs
+++ src/System.IO.FileSystem.DriveInfo/src/System/IO/DriveInfo.Windows.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Diagnostics;
 using System.Diagnostics.Contracts;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -12,8 +13,7 @@ namespace System.IO
     {
         private static string NormalizeDriveName(string driveName)
         {
-            if (driveName == null)
-                throw new ArgumentNullException("driveName");
+            Debug.Assert(driveName != null);
             Contract.EndContractBlock();
 
             string name;
