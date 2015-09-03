4e0542c5554a1072495f6785738428c4a1ef7ed7
Alexander Köplinger <alex.koeplinger@outlook.com>
2015-09-04 01:05:49 +0200

    System.IO.Compression: use consistent namespace name for tests
    As per https://github.com/dotnet/corefx/issues/2898 the convention is $(Namespace of Type under Test).Tests
    Some tests were in .Test, this change renames them to .Tests

--- src/System.IO.Compression.ZipFile/tests/ZipFileTests.cs
+++ src/System.IO.Compression.ZipFile/tests/ZipFileTests.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace System.IO.Compression.Test
+namespace System.IO.Compression.Tests
 {
     public partial class ZipTest : IDisposable
     {
