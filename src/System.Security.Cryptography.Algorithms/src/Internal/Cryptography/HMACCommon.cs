59ce351e554fc4e0ceca4ff5b29f84db695b4cb8
Atsushi Kanamori <atsushik@microsoft.com>
2015-09-09 09:53:31 -0700

    Fix Issue #1774
    HMAC::get_Key returns the original key passed in, not the
    actual key used (which will differ if the original key
    exceeded a certain length. See Rfc2104 section 2).

--- src/System.Security.Cryptography.Algorithms/src/Internal/Cryptography/HMACCommon.cs
+++ src/System.Security.Cryptography.Algorithms/src/Internal/Cryptography/HMACCommon.cs
@@ -19,9 +19,10 @@ namespace Internal.Cryptography
     //
     internal sealed class HMACCommon
     {
-        public HMACCommon(String hashAlgorithmId, byte[] key)
+        public HMACCommon(String hashAlgorithmId, byte[] key, int BlockSize)
         {
             _hashAlgorithmId = hashAlgorithmId;
+            _blockSize = BlockSize;
             ChangeKey(key);
         }
 
@@ -35,12 +36,27 @@ namespace Internal.Cryptography
 
         public void ChangeKey(byte[] key)
         {
+            if (key.Length > _blockSize)
+            {
+                // Perform RFC 2104, section 2 key adjustment.
+                if (_lazyHashProvider == null)
+                    _lazyHashProvider = HashProviderDispenser.CreateHashProvider(_hashAlgorithmId);
+                _lazyHashProvider.AppendHashData(key, 0, key.Length);
+                key = _lazyHashProvider.FinalizeHashAndReset();
+            }
+
             HashProvider oldHashProvider = _hMacProvider;
             _hMacProvider = null;
             if (oldHashProvider != null)
                 oldHashProvider.Dispose(true);
             _hMacProvider = HashProviderDispenser.CreateMacProvider(_hashAlgorithmId, key);
+
+            ActualKey = key;
         }
+
+        // The actual key used for hashing. This will not be the same as the original key passed to ChangeKey() if the original key exceeded the
+        // hash algorithm's block size. (See RFC 2104, section 2)
+        public byte[] ActualKey { get; private set; }
 
         // Adds new data to be hashed. This can be called repeatedly in order to hash data from incontiguous sources.
         public void AppendHashData(byte[] data, int offset, int count)
@@ -66,5 +82,8 @@ namespace Internal.Cryptography
 
         private readonly String _hashAlgorithmId;
         private HashProvider _hMacProvider;
+        private volatile HashProvider _lazyHashProvider;
+
+        private readonly int _blockSize;
     }
 }
--- src/System.Security.Cryptography.Algorithms/src/System/Security/Cryptography/HMACSHA1.cs
+++ src/System.Security.Cryptography.Algorithms/src/System/Security/Cryptography/HMACSHA1.cs
@@ -17,15 +17,15 @@ namespace System.Security.Cryptography
     public class HMACSHA1 : HMAC
     {
         public HMACSHA1()
-            : this(Helpers.GenerateRandom(64))
+            : this(Helpers.GenerateRandom(BlockSize))
         {
         }
 
         public HMACSHA1(byte[] key)
         {
             this.HashName = HashAlgorithmNames.SHA1;
-            _hMacCommon = new HMACCommon(HashAlgorithmNames.SHA1, key);
-            base.Key = key;
+            _hMacCommon = new HMACCommon(HashAlgorithmNames.SHA1, key, BlockSize);
+            base.Key = _hMacCommon.ActualKey; 
         }
 
         public override int HashSize
@@ -44,8 +44,8 @@ namespace System.Security.Cryptography
             }
             set
             {
-                base.Key = value;
                 _hMacCommon.ChangeKey(value);
+                base.Key = _hMacCommon.ActualKey;
             }
         }
 
@@ -79,5 +79,6 @@ namespace System.Security.Cryptography
         }
 
         private HMACCommon _hMacCommon;
+        private const int BlockSize = 64;
     }
 }
