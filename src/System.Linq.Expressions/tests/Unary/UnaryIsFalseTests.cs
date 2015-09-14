e1a09e470e2ba8c56773db241bc4f135f8257e94
Bart De Smet <bartde@microsoft.com>
2015-09-14 14:38:56 -0700

    Adding tests for UnaryPlus, IsTrue, IsFalse.

---
+++ src/System.Linq.Expressions/tests/Unary/UnaryIsFalseTests.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Xunit;
+
+namespace Tests.ExpressionCompiler.Unary
+{
+    public static class UnaryIsFalseTests
+    {
+        #region Test methods
+
+        [Fact] //[WorkItem(3196, "https://github.com/dotnet/corefx/issues/3196")]
+        public static void CheckUnaryIsFalseBoolTest()
+        {
+            bool[] values = new bool[] { false, true };
+            for (int i = 0; i < values.Length; i++)
+            {
+                VerifyIsFalseBool(values[i]);
+            }
+        }
+
+        #endregion
+
+        #region Test verifiers
+
+        private static void VerifyIsFalseBool(bool value)
+        {
+            Expression<Func<bool>> e =
+                Expression.Lambda<Func<bool>>(
+                    Expression.IsFalse(Expression.Constant(value, typeof(bool))),
+                    Enumerable.Empty<ParameterExpression>());
+            Func<bool> f = e.Compile();
+            Assert.Equal((bool)(value == false), f());
+        }
+
+
+        #endregion
+    }
+}
