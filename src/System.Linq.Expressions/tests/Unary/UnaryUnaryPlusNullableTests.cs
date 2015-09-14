b5d749c8bb4c57acfc0b45e380b471ae5548a43e
Bart De Smet <bartde@microsoft.com>
2015-09-14 14:38:56 -0700

    Adding tests for UnaryPlus, IsTrue, IsFalse.

---
+++ src/System.Linq.Expressions/tests/Unary/UnaryUnaryPlusNullableTests.cs
@@ -0,0 +1,203 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Xunit;
+
+namespace Tests.ExpressionCompiler.Unary
+{
+    public static class UnaryPlusNullableTests
+    {
+        #region Test methods
+
+        [Fact] //[WorkItem(3196, "https://github.com/dotnet/corefx/issues/3196")]
+        public static void CheckUnaryArithmeticUnaryPlusNullableShortTest()
+        {
+            short?[] values = new short?[] { null, 0, 1, -1, short.MinValue, short.MaxValue };
+            for (int i = 0; i < values.Length; i++)
+            {
+                VerifyArithmeticUnaryPlusNullableShort(values[i]);
+            }
+        }
+
+        [Fact] //[WorkItem(3196, "https://github.com/dotnet/corefx/issues/3196")]
+        public static void CheckUnaryArithmeticUnaryPlusNullableUShortTest()
+        {
+            ushort?[] values = new ushort?[] { null, 0, 1, ushort.MaxValue };
+            for (int i = 0; i < values.Length; i++)
+            {
+                VerifyArithmeticUnaryPlusNullableUShort(values[i]);
+            }
+        }
+
+        [Fact] //[WorkItem(3196, "https://github.com/dotnet/corefx/issues/3196")]
+        public static void CheckUnaryArithmeticUnaryPlusNullableIntTest()
+        {
+            int?[] values = new int?[] { null, 0, 1, -1, int.MinValue, int.MaxValue };
+            for (int i = 0; i < values.Length; i++)
+            {
+                VerifyArithmeticUnaryPlusNullableInt(values[i]);
+            }
+        }
+
+        [Fact] //[WorkItem(3196, "https://github.com/dotnet/corefx/issues/3196")]
+        public static void CheckUnaryArithmeticUnaryPlusNullableUIntTest()
+        {
+            uint?[] values = new uint?[] { null, 0, 1, uint.MaxValue };
+            for (int i = 0; i < values.Length; i++)
+            {
+                VerifyArithmeticUnaryPlusNullableUInt(values[i]);
+            }
+        }
+
+        [Fact] //[WorkItem(3196, "https://github.com/dotnet/corefx/issues/3196")]
+        public static void CheckUnaryArithmeticUnaryPlusNullableLongTest()
+        {
+            long?[] values = new long?[] { null, 0, 1, -1, long.MinValue, long.MaxValue };
+            for (int i = 0; i < values.Length; i++)
+            {
+                VerifyArithmeticUnaryPlusNullableLong(values[i]);
+            }
+        }
+
+        [Fact] //[WorkItem(3196, "https://github.com/dotnet/corefx/issues/3196")]
+        public static void CheckUnaryArithmeticUnaryPlusNullableULongTest()
+        {
+            ulong?[] values = new ulong?[] { null, 0, 1, ulong.MaxValue };
+            for (int i = 0; i < values.Length; i++)
+            {
+                VerifyArithmeticUnaryPlusNullableULong(values[i]);
+            }
+        }
+
+        [Fact] //[WorkItem(3196, "https://github.com/dotnet/corefx/issues/3196")]
+        public static void CheckUnaryArithmeticUnaryPlusNullableFloatTest()
+        {
+            float?[] values = new float?[] { null, 0, 1, -1, float.MinValue, float.MaxValue, float.Epsilon, float.NegativeInfinity, float.PositiveInfinity, float.NaN };
+            for (int i = 0; i < values.Length; i++)
+            {
+                VerifyArithmeticUnaryPlusNullableFloat(values[i]);
+            }
+        }
+
+        [Fact] //[WorkItem(3196, "https://github.com/dotnet/corefx/issues/3196")]
+        public static void CheckUnaryArithmeticUnaryPlusNullableDoubleTest()
+        {
+            double?[] values = new double?[] { null, 0, 1, -1, double.MinValue, double.MaxValue, double.Epsilon, double.NegativeInfinity, double.PositiveInfinity, double.NaN };
+            for (int i = 0; i < values.Length; i++)
+            {
+                VerifyArithmeticUnaryPlusNullableDouble(values[i]);
+            }
+        }
+
+        [Fact] //[WorkItem(3196, "https://github.com/dotnet/corefx/issues/3196")]
+        public static void CheckUnaryArithmeticUnaryPlusNullableDecimalTest()
+        {
+            decimal?[] values = new decimal?[] { null, decimal.Zero, decimal.One, decimal.MinusOne, decimal.MinValue, decimal.MaxValue };
+            for (int i = 0; i < values.Length; i++)
+            {
+                VerifyArithmeticUnaryPlusNullableDecimal(values[i]);
+            }
+        }
+
+        #endregion
+
+        #region Test verifiers
+
+        private static void VerifyArithmeticUnaryPlusNullableShort(short? value)
+        {
+            Expression<Func<short?>> e =
+                Expression.Lambda<Func<short?>>(
+                    Expression.UnaryPlus(Expression.Constant(value, typeof(short?))),
+                    Enumerable.Empty<ParameterExpression>());
+            Func<short?> f = e.Compile();
+            Assert.Equal((short?)(+value), f());
+        }
+
+        private static void VerifyArithmeticUnaryPlusNullableUShort(ushort? value)
+        {
+            Expression<Func<ushort?>> e =
+                Expression.Lambda<Func<ushort?>>(
+                    Expression.UnaryPlus(Expression.Constant(value, typeof(ushort?))),
+                    Enumerable.Empty<ParameterExpression>());
+            Func<ushort?> f = e.Compile();
+            Assert.Equal((ushort?)(+value), f());
+        }
+
+        private static void VerifyArithmeticUnaryPlusNullableInt(int? value)
+        {
+            Expression<Func<int?>> e =
+                Expression.Lambda<Func<int?>>(
+                    Expression.UnaryPlus(Expression.Constant(value, typeof(int?))),
+                    Enumerable.Empty<ParameterExpression>());
+            Func<int?> f = e.Compile();
+            Assert.Equal((int?)(+value), f());
+        }
+
+        private static void VerifyArithmeticUnaryPlusNullableUInt(uint? value)
+        {
+            Expression<Func<uint?>> e =
+                Expression.Lambda<Func<uint?>>(
+                    Expression.UnaryPlus(Expression.Constant(value, typeof(uint?))),
+                    Enumerable.Empty<ParameterExpression>());
+            Func<uint?> f = e.Compile();
+            Assert.Equal((uint?)(+value), f());
+        }
+
+        private static void VerifyArithmeticUnaryPlusNullableLong(long? value)
+        {
+            Expression<Func<long?>> e =
+                Expression.Lambda<Func<long?>>(
+                    Expression.UnaryPlus(Expression.Constant(value, typeof(long?))),
+                    Enumerable.Empty<ParameterExpression>());
+            Func<long?> f = e.Compile();
+            Assert.Equal((long?)(+value), f());
+        }
+
+        private static void VerifyArithmeticUnaryPlusNullableULong(ulong? value)
+        {
+            Expression<Func<ulong?>> e =
+                Expression.Lambda<Func<ulong?>>(
+                    Expression.UnaryPlus(Expression.Constant(value, typeof(ulong?))),
+                    Enumerable.Empty<ParameterExpression>());
+            Func<ulong?> f = e.Compile();
+            Assert.Equal((ulong?)(+value), f());
+        }
+
+        private static void VerifyArithmeticUnaryPlusNullableFloat(float? value)
+        {
+            Expression<Func<float?>> e =
+                Expression.Lambda<Func<float?>>(
+                    Expression.UnaryPlus(Expression.Constant(value, typeof(float?))),
+                    Enumerable.Empty<ParameterExpression>());
+            Func<float?> f = e.Compile();
+            Assert.Equal((float?)(+value), f());
+        }
+
+        private static void VerifyArithmeticUnaryPlusNullableDouble(double? value)
+        {
+            Expression<Func<double?>> e =
+                Expression.Lambda<Func<double?>>(
+                    Expression.UnaryPlus(Expression.Constant(value, typeof(double?))),
+                    Enumerable.Empty<ParameterExpression>());
+            Func<double?> f = e.Compile();
+            Assert.Equal((double?)(+value), f());
+        }
+
+        private static void VerifyArithmeticUnaryPlusNullableDecimal(decimal? value)
+        {
+            Expression<Func<decimal?>> e =
+                Expression.Lambda<Func<decimal?>>(
+                    Expression.UnaryPlus(Expression.Constant(value, typeof(decimal?))),
+                    Enumerable.Empty<ParameterExpression>());
+            Func<decimal?> f = e.Compile();
+            Assert.Equal((decimal?)(+value), f());
+        }
+
+
+        #endregion
+    }
+}
