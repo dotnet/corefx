dd4c56e1e2b3016f67f05969903c23ea1e36a042
Bart De Smet <bartde@microsoft.com>
2015-09-14 19:25:24 -0700

    Improving tests for unary operators.

---
+++ src/System.Linq.Expressions/tests/Unary/UnaryArithmeticNegateNullableOneOffTests.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace Tests.ExpressionCompiler.Unary
+{
+    public static unsafe class UnaryArithmeticNegateNullableOneOffTests
+    {
+        [Fact] //[WorkItem(3197, "https://github.com/dotnet/corefx/issues/3197")]
+        public static void UnaryArithmeticNegateNullableStackBalance()
+        {
+            var e = Expression.Lambda<Func<decimal?>>(
+                Expression.Negate(
+                    Expression.Negate(
+                        Expression.Constant(1.0m, typeof(decimal?))
+                    )
+                )
+            );
+
+            var f = e.Compile();
+
+            Assert.True(f() == 1.0m);
+        }
+    }
+}
