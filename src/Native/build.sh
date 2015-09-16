425d47f8494b2c2a4414efd799e65fb6b58cc9f7
Jan Henke <Jan.Henke@taujhe.de>
2015-09-16 17:44:41 +0200

    Drop src/Native/build.sh completely.
    PR #3175 merged the separate build.sh files for managed and native
    builds into one file on the top level of the repository.
    src/Native/build.sh was left until the CI is updated to use the new
    location. After the CI is now updated, drop the old file completely.

--- src/Native/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-__ScriptDir="$( dirname "${BASH_SOURCE[0]}" )"
-__ScriptDir="$( cd $__ScriptDir && pwd )"
-__ProjectRoot="$( cd $__ScriptDir && cd ../.. && pwd )"
-
-"$__ProjectRoot/build.sh" native "$@"
