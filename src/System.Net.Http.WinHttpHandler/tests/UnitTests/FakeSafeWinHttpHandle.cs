b30b39707bfc9359c46e4749552f879ae37fcf13
David Shulman <david.shulman@microsoft.com>
2015-09-19 08:16:11 -0700

    Disable SafeHandle leak checking in WinHttpHandler Unit Tests
    Addresses issue #3312.

--- src/System.Net.Http.WinHttpHandler/tests/UnitTests/FakeSafeWinHttpHandle.cs
+++ src/System.Net.Http.WinHttpHandler/tests/UnitTests/FakeSafeWinHttpHandle.cs
@@ -2,7 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using System.Net.Http;
+using System.Diagnostics;
 using System.Threading;
 
 namespace System.Net.Http.WinHttpHandlerUnitTests
@@ -10,13 +10,17 @@ namespace System.Net.Http.WinHttpHandlerUnitTests
     internal class FakeSafeWinHttpHandle : Interop.WinHttp.SafeWinHttpHandle
     {
         private static int s_HandlesOpen = 0;
-        
+
         public FakeSafeWinHttpHandle(bool markAsValid)
         {
             if (markAsValid)
             {
                 SetHandle(Marshal.AllocHGlobal(1));
                 Interlocked.Increment(ref s_HandlesOpen);
+                Debug.WriteLine(
+                    "FakeSafeWinHttpHandle.cctor, handle=#{0}, s_HandlesOpen={1}",
+                    handle.GetHashCode(),
+                    s_HandlesOpen);
             }
             else
             {
@@ -32,19 +36,13 @@ namespace System.Net.Http.WinHttpHandlerUnitTests
             }
         }
 
-        public static void ForceGarbageCollection()
-        {
-            // Make several passes through the FReachable list.
-            for (int i = 0; i < 10; i++)
-            {
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
-            }
-        }
-        
         protected override bool ReleaseHandle()
         {
             Interlocked.Decrement(ref s_HandlesOpen);
+            Debug.WriteLine(
+                "FakeSafeWinHttpHandle.ReleaseHandle, handle=#{0}, s_HandlesOpen={1}",
+                handle.GetHashCode(),
+                s_HandlesOpen);
             
             return base.ReleaseHandle();
         }
