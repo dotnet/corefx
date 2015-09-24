deebfed3373c537882f38ad5c80b38ead02619c9
bartonjs <jbarton@microsoft.com>
2015-09-24 10:32:21 -0700

    EVP_PKEY_get1_EC_KEY was added in a different file in dev-sslstream and master; unifying on the one whose file was named more accurately.
    (cherry picked from commit 7941baa3248a9160f4f8f9b319433bd908a9fe01)

--- src/Common/src/Interop/Unix/libcrypto/Interop.EvpPkey.EcKey.cs
+++ src/Common/src/Interop/Unix/libcrypto/Interop.EvpPkey.EcKey.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 
 using Microsoft.Win32.SafeHandles;
 
@@ -10,6 +11,10 @@ internal static partial class Interop
     internal static partial class libcrypto
     {
         [DllImport(Libraries.LibCrypto)]
-        internal static extern SafeEcKeyHandle EVP_PKEY_get1_EC_KEY(SafeEvpPkeyHandle pkey);
+        internal static extern SafeEcKeyHandle EVP_PKEY_get1_EC_KEY(SafeEvpPKeyHandle pkey);
+
+        [DllImport(Libraries.LibCrypto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool EVP_PKEY_set1_EC_KEY(SafeEvpPKeyHandle pkey, SafeEcKeyHandle rsa);
     }
 }
