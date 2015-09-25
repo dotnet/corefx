5b5717c2eb8fc88ed5bd59babe39890c581ad622
Stephen A. Imhoff <clockwork-muse@outlook.com>
2015-09-25 21:29:55 +0900

    Update test namespace in System.IO.FileSystem
    Update test namespace where applicable.
    Cleanup `using`s in updated files.

--- src/System.IO.FileSystem/tests/FileStream/WriteAsync.cs
+++ src/System.IO.FileSystem/tests/FileStream/WriteAsync.cs
@@ -1,16 +1,13 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace System.IO.FileSystem.Tests
+namespace System.IO.Tests
 {
     public class FileStream_WriteAsync : FileSystemTest
     {
--- src/System.IO.FileSystem/tests/FileStream/ctor_str_fm_fa_fs.write.cs
+++ src/System.IO.FileSystem/tests/FileStream/ctor_str_fm_fa_fs.write.cs
@@ -5,7 +5,7 @@ using System;
 using System.IO;
 using Xunit;
 
-namespace System.IO.FileSystem.Tests
+namespace System.IO.Tests
 {
     public partial class FileStream_ctor_str_fm_fa_fs
     {
