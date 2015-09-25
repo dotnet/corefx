5d98db946cd949471b8a6d6c3163181f5036a757
Stephen A. Imhoff <clockwork-muse@outlook.com>
2015-09-25 16:22:54 +0900

    System.Collections.Immutable

--- src/System.Collections.Immutable/tests/ImmutableDictionaryTest.cs
+++ src/System.Collections.Immutable/tests/ImmutableDictionaryTest.cs
@@ -6,7 +6,7 @@ using System.Diagnostics;
 using System.Linq;
 using Xunit;
 
-namespace System.Collections.Immutable.Test
+namespace System.Collections.Immutable.Tests
 {
     public class ImmutableDictionaryTest : ImmutableDictionaryTestBase
     {
