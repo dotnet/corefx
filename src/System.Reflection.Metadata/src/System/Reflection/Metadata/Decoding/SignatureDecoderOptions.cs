a6916a3aa5969ada46e7c590549fe40b7d3663f8
Nick Guerrera <nicholg@microsoft.com>
2015-09-29 17:07:26 -0700

    Convert SignatureDecoder and CustomAttributeDecoder to be instances
    This facilitates keeping state in the type provider while still not
    having  to repeat TType over and over again.

---
+++ src/System.Reflection.Metadata/src/System/Reflection/Metadata/Decoding/SignatureDecoderOptions.cs
@@ -0,0 +1,19 @@
+﻿namespace System.Reflection.Metadata.Decoding
+{
+    [Flags]
+    public enum SignatureDecoderOptions
+    {
+        /// <summary>
+        /// Disable all options (default when no options are passed).
+        /// </summary>
+        None = 0x0,
+
+        /// <summary>
+        /// Causes the decoder to pass non-null for <see cref="Nullable{Bool}"/> isValueType arguments.
+        /// </summary>
+        /// <remarks>
+        /// There is additional overhead for this case when dealing with .winmd files to handle projection.
+        /// </remarks>
+        DifferentiateValueTypes = 0x1
+    }
+}
