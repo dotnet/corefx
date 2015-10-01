f346e17ff4e9602d5b56ba6bf65ded651a02cb2c
Stephen A. Imhoff <clockwork-muse@outlook.com>
2015-10-01 16:23:13 +0900

    Update test namespace in System.Xml.XmlDocument per #2898 .  - Change some names to avoid conflicts.  - Clean up `using`s.

--- src/System.Xml.XmlDocument/tests/XmlNodeTests/NextSiblingTests.cs
+++ src/System.Xml.XmlDocument/tests/XmlNodeTests/NextSiblingTests.cs
@@ -1,10 +1,9 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Xml;
 using Xunit;
 
-namespace XmlDocumentTests.XmlNodeTests
+namespace System.Xml.Tests
 {
     public static class NextSiblingTests
     {
