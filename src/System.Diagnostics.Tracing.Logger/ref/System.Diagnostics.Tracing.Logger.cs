d9cb7eef4d131b4c1d1894683eb24d84b73463f7
Rahul Kumar <rahku@microsoft.com>
2015-10-06 13:20:22 -0700

    Initial checkin of System.Diagnostics.Tracing.Logger

---
+++ src/System.Diagnostics.Tracing.Logger/ref/System.Diagnostics.Tracing.Logger.cs
@@ -0,0 +1,5 @@
+namespace System.Diagnostics.Tracing {
+  public partial class Logger {
+  }
+}
+
---
+++ src/System.Diagnostics.Tracing.Logger/src/System/Diagnostics/Tracing/Logger.cs
@@ -0,0 +1,163 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+
+namespace System.Diagnostics.Tracing
+{
+    /// <summary>
+    /// 
+    /// 
+    /// Logger uses TelemetryListener in its implementation, which means that you can listen to the data presented here using TelemetryListener.AllListeners
+    /// </summary>
+    public class Logger : DiagnosticListener
+    {
+        /// <summary>
+        /// Creates a new logger with the given name.   A Logger is a TelemetrySource which understands LogLevel (verbosity levels)
+        /// </summary>
+        /// <param name="name"></param>
+        public Logger(string name) : base(name)
+        {
+            _level = Off;     // Turn the verbosity to 'off' initially as there are no subscribers
+        }
+
+        /// <summary>
+        /// Returns true if 'level' is less verbose (more severe) than the current level for the Logger.  
+        /// </summary>
+        public bool IsEnabled(LogLevel level)
+        {
+            return (level <= _level);
+        }
+
+        /// <summary>
+        /// Because calling this method typically requires a anonymous type to be generated for the 'arguments'
+        /// value if this logging happens in a 'hot' code path you should have a 'IsEnabled' check before calling
+        /// it to avoid having to make the object just to throw it away if logging is off.   
+        /// </summary>
+        public void Log(LogLevel level, string logItemName, object arguments = null)
+        {
+            if (IsEnabled(level))
+            {
+                 Write(logItemName, new LoggerArguments { Level = level, LoggerName = Name, Arguments = arguments });
+            }
+        }
+
+        /// <summary>
+        /// If the logger is enabled for 'level' then geneate a 'activityName'.Start message with the given argument.
+        /// It returns an IDisposable that when Disposed() will send a 'activityName'.Stop message with the SAME arguments 
+        /// object.  Thus the arguments object itself can be used as an ID that links the two together.
+        /// </summary>
+        public IDisposable ActivityStart(LogLevel level, string activityName, object arguments)
+        {
+            var activity = new LoggerActivity(this, level, activityName, arguments);
+            if (IsEnabled(level))
+                Log(level, activityName + ".Start", arguments);
+            else
+                activity.Disposed = true;
+            return activity;
+        }
+
+        /// <summary>
+        /// Subscribe to the Logger, sending events to 'observer' for any logging that is enabled for verbosity 'level'.  
+        /// 
+        /// Note that in multi-subscriber cases you MAY get messages that have a more verbose level than you asked for.
+        /// You can filter them out yourself.   If there is only one subscriber (or all subscribers subscribe at the 
+        /// same verbosity.  It works fine.   
+        /// </summary>
+        public virtual IDisposable Subscribe(IObserver<KeyValuePair<string, object>> observer, LogLevel level)
+        {
+            // We turn up the volume to the maximum of any subscriber.
+            if (_level < level || _level == Off)
+                _level = level;
+            return base.Subscribe(observer);
+        }
+
+        #region private
+        const LogLevel Off = (LogLevel)(-1);
+
+        private sealed class LoggerActivity : IDisposable
+        {
+            public LoggerActivity(Logger logger, LogLevel level, string activityName, object arguments)
+            {
+                _logger = logger;
+                _level = level;
+                _activityName = activityName;
+                _arguments = arguments;
+            }
+            public bool Disposed;
+
+            public void Dispose()
+            {
+                if (!Disposed)
+                {
+                    Disposed = true;
+                    _logger.Log(_level, _activityName + ".Stop", _arguments);
+                }
+            }
+            Logger _logger;
+            LogLevel _level;
+            string _activityName;
+            object _arguments;
+        }
+
+        private LogLevel _level;
+        #endregion 
+    }
+
+    /// <summary>
+    /// LoggerArguments is the payload that you get back from the LogStream.   The string in the KeyValue pair
+    /// is the name of the logging message, and the object is a LoggerArguments.  
+    /// </summary>
+    public class LoggerArguments
+    {
+        /// <summary>
+        /// This is the verbosity level that was passed to the 'Log' function 
+        /// </summary>
+        public LogLevel Level;
+        /// <summary>
+        /// This is the name of the logger that logged the event 
+        /// </summary>
+        public string LoggerName;
+        /// <summary>
+        /// This is the 'arguments parameter passed to the 'Log' method.  
+        /// </summary>
+        public object Arguments;
+        // TODO should we add a IsActivity? (They can get that from the suffix).  
+    }
+
+    /// <summary>
+    /// This is identical to System.Diagnostics.Tracing.EventLevel.  It is only here to avoid taking a dependency on that type. 
+    /// They should be kept in sync. 
+    /// </summary>
+    public enum LogLevel
+    {
+        /// <summary>
+        /// Log always. This level is not supported for subscribers. 
+        /// </summary>
+        LogAlways = 0,
+        /// <summary>
+        /// Only critical errors
+        /// </summary>
+        Critical,
+        /// <summary>
+        /// All errors, including previous levels
+        /// </summary>
+        Error,
+        /// <summary>
+        /// All warnings, including previous levels
+        /// </summary>
+        Warning,
+        /// <summary>
+        /// All informational events, including previous levels
+        /// </summary>
+        Informational,
+        /// <summary>
+        /// All events, including previous levels 
+        /// </summary>
+        Verbose  // = 5
+    }
+}
