e2feb56641d417a7d75d4ef39662ceae6b446c84
Rahul Kumar <rahku@microsoft.com>
2015-10-09 18:17:53 -0700

    Make interface public & add comments

--- src/System.Diagnostics.Tracing.Logger/src/System/Diagnostics/Tracing/ILogger.cs
+++ src/System.Diagnostics.Tracing.Logger/src/System/Diagnostics/Tracing/ILogger.cs
@@ -2,11 +2,41 @@
 
 namespace System.Diagnostics.Tracing
 {
-    interface ILogger
+    /// <summary>
+    /// A generic interface for logging.
+    /// </summary>
+    public interface ILogger
     {
+        /// <summary>
+        /// Checks if the given LogLevel is enabled.
+        /// </summary>
+        /// <param name="level"></param>
+        /// <returns></returns>
         bool IsEnabled(LogLevel level);
+
+        /// <summary>
+        /// Log a single item
+        /// </summary>
+        /// <param name="logItemName"></param>
+        /// <param name="level"></param>
+        /// <param name="arguments"></param>
         void Log(string logItemName, LogLevel level, object arguments = null);
+
+        /// <summary>
+        /// Start of new Activity
+        /// </summary>
+        /// <param name="activityName"></param>
+        /// <param name="level"></param>
+        /// <param name="arguments"></param>
+        /// <returns></returns>
         IDisposable ActivityStart(string activityName, LogLevel level = LogLevel.Critical, object arguments = null);
+
+        /// <summary>
+        /// Add an observer
+        /// </summary>
+        /// <param name="observer"></param>
+        /// <param name="level"></param>
+        /// <returns></returns>
         IDisposable Subscribe(IObserver<KeyValuePair<string, object>> observer, LogLevel level);
     }
 }
