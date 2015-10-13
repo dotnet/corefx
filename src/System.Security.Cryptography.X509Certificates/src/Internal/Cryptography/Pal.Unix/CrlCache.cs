ead1920f6313e7b1713ceaf2669eb2dab8c0017b
bartonjs <jbarton@microsoft.com>
2015-10-12 21:27:34 -0700

    Add DerSequenceReader tests in S.S.C.Encoding
    One day a successor to DerSequenceReader might be public,
    and it would live in the Encoding library, so add the tests there.
    
    The X509 library already has some indirect tests of the functionality, but this sets the stage for a place to add more tests.

--- src/System.Security.Cryptography.X509Certificates/src/Internal/Cryptography/Pal.Unix/CrlCache.cs
+++ src/System.Security.Cryptography.X509Certificates/src/Internal/Cryptography/Pal.Unix/CrlCache.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.Win32.SafeHandles;
 
