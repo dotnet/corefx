f2bd42d3e74cc3d7dae63150a80ef181f9ac053c
Stephen A. Imhoff <clockwork-muse@outlook.com>
2015-10-14 10:16:41 +0900

    Cleanup TaskStatusTests
    - Rename file to match class name
    
    Correct namespace
    
    Make test class static
    
    Extract test parameters to inline data
    
    Extract Promise tests.
    
    Extract cancel-scheduled test
    
    Extract Cancel Created Task test
    
    Remove created-only task test (covered by cancel-created)
    
    Add wrapped exception helper
    
    Extract simple Failed.
    
    Add maximum wait time safety.
    
    Extract Running/RunToCompletion
    
    Extract cancel running task test.
    
    Streamline test setup (remaining tests at this point all create child tasks).
    
    unstep some indentation, eliminate unnecessary variables.
    
    Extract basic completion tests.
    
    Extract child-only-cancellation tests.
    
    Extract failed-parent-only tests.
    
    Extract failed child task tests.
    
    Extract task-canceled-child-? tests, remove framework.

---
+++ src/System.Threading.Tasks/tests/Helpers/Functions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Xunit;
+
+namespace System.Threading.Tasks.Tests
+{
+    internal static class Functions
+    {
+        public static void AssertThrowsWrapped<T>(Action query)
+        {
+            AggregateException ae = Assert.Throws<AggregateException>(query);
+            Assert.All(ae.InnerExceptions, e => Assert.IsType<T>(e));
+        }
+    }
+}
