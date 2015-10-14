8668749265b82b37c75309d5c9462935c428e1b1
Andrew Arnott <andarno@microsoft.com>
2015-10-14 12:02:05 -0700

    Remove reference to setter on API docs that don't fit
    This was a copy/paste bug.

--- src/System.Collections.Immutable/src/System/Collections/Immutable/ImmutableList`1.cs
+++ src/System.Collections.Immutable/src/System/Collections/Immutable/ImmutableList`1.cs
@@ -1146,7 +1146,7 @@ namespace System.Collections.Immutable
         /// </summary>
         /// <param name="index">The index.</param>
         /// <param name="item">The item.</param>
-        /// <exception cref="NotSupportedException">Always thrown from the setter.</exception>
+        /// <exception cref="NotSupportedException">Always thrown.</exception>
         void IList<T>.Insert(int index, T item)
         {
             throw new NotSupportedException();
@@ -1156,7 +1156,7 @@ namespace System.Collections.Immutable
         /// Removes the value at the specified index.
         /// </summary>
         /// <param name="index">The index.</param>
-        /// <exception cref="NotSupportedException">Always thrown from the setter.</exception>
+        /// <exception cref="NotSupportedException">Always thrown.</exception>
         void IList<T>.RemoveAt(int index)
         {
             throw new NotSupportedException();
