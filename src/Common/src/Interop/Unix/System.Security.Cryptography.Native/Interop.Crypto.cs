690dc858675b726bbfd281b6dedd4a26ead975f0
Eric Erhardt <eric.erhardt@microsoft.com>
2015-10-14 16:27:44 -0500

    Adding shims for all Interop.Pkcs7 methods.
    I also moved the existing GetPkcs7Certificates method from Interop.Crypto to Interop.Pkcs7, so System.Net.Security didn't need to include all the Pkcs7 files, which it doesn't use.

--- src/Common/src/Interop/Unix/System.Security.Cryptography.Native/Interop.Crypto.cs
+++ src/Common/src/Interop/Unix/System.Security.Cryptography.Native/Interop.Crypto.cs
@@ -107,9 +107,6 @@ internal static partial class Interop
         private static extern IntPtr GetX509RootStorePath_private();
 
         [DllImport(Libraries.CryptoNative)]
-        private static extern int GetPkcs7Certificates(SafePkcs7Handle p7, out SafeSharedX509StackHandle certs);
-
-        [DllImport(Libraries.CryptoNative)]
         private static extern int SetX509ChainVerifyTime(
             SafeX509StoreCtxHandle ctx,
             int year,
@@ -169,27 +166,6 @@ internal static partial class Interop
             {
                 throw Interop.Crypto.CreateOpenSslCryptographicException();
             }
-        }
-
-        internal static SafeSharedX509StackHandle GetPkcs7Certificates(SafePkcs7Handle p7)
-        {
-            if (p7 == null || p7.IsInvalid)
-            {
-                return SafeSharedX509StackHandle.InvalidHandle;
-            }
-
-            SafeSharedX509StackHandle certs;
-            int result = GetPkcs7Certificates(p7, out certs);
-
-            if (result != 1)
-            {
-                throw Interop.Crypto.CreateOpenSslCryptographicException();
-            }
-
-            // Track the parent relationship for the interior pointer so lifetime is well-managed.
-            certs.SetParent(p7);
-
-            return certs;
         }
 
         private static byte[] GetDynamicBuffer<THandle>(NegativeSizeReadMethod<THandle> method, THandle handle)
