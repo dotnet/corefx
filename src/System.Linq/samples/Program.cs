8df76f75ef8d5397da44114b52e9c108247e54b3
Phillip Carter <phillip.carter@outlook.com>
2015-10-21 15:18:01 -0700

    Add sample project and doc folder to System.Linq
    This commit models what I'd like to see corefx adopt for reference
    documentation and sample code.
    
    This is the "core" of the overall samples plan for .NET where sample
    code is a part of the build system (somehow), ensuring correctness for
    the future and having a more central place to update samples if they
    need to be updated.

---
+++ src/System.Linq/samples/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace System.Linq.Samples
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            AggregateSamples.Overload1();
+            AggregateSamples.Overload2();
+            AggregateSamples.Overload3();
+        }
+    }
+}
