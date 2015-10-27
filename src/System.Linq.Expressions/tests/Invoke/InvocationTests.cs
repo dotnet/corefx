c8e84d0bf68d194c2fdc1d6584d859210164b74c
Jon Hanna <jon@hackcraft.net>
2015-10-27 01:44:53 +0000

    Interpreted expressions shouldn't write back to delegate instances of calls.
    Fixes #4150
    
    Interpreted expressions executing a `‎Call()` write back to the instance of the
    method after the call, so that mutable structs are mutated correctly. This is
    harmless in most cases to which it doesn't apply, but can cause bugs when the
    instance is mutated from the outside over the course of the call, including
    when delegates are `Invoke()`d as that becomes a `Call()` with the delegate
    as its instance.

--- src/System.Linq.Expressions/tests/Invoke/InvocationTests.cs
+++ src/System.Linq.Expressions/tests/Invoke/InvocationTests.cs
@@ -63,5 +63,41 @@ namespace System.Linq.Expressions.Tests
                 Assert.Equal(1, DoItA(this));
             }
         }
+
+        private class FuncHolder
+        {
+            public Func<int> Function;
+
+            public FuncHolder()
+            {
+                Function = () =>
+                {
+                    Function = () => 1;
+                    return 0;
+                };
+            }
+        }
+
+        [Fact]
+        public static void InvocationDoesNotChangeFunctionInvokedCompiled()
+        {
+            FuncHolder holder = new FuncHolder();
+            var fld = Expression.Field(Expression.Constant(holder), "Function");
+            var inv = Expression.Invoke(fld);
+            Func<int> act = (Func<int>)Expression.Lambda(inv).Compile(false);
+            act();
+            Assert.Equal(1, holder.Function());
+        }
+
+        [Fact]
+        public static void InvocationDoesNotChangeFunctionInvokedInterpreted()
+        {
+            FuncHolder holder = new FuncHolder();
+            var fld = Expression.Field(Expression.Constant(holder), "Function");
+            var inv = Expression.Invoke(fld);
+            Func<int> act = (Func<int>)Expression.Lambda(inv).Compile(true);
+            act();
+            Assert.Equal(1, holder.Function());
+        }
     }
 }
