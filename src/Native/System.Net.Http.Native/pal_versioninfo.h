18d5afe0c3b910e90a8d53807d1e29d104db06ba
Eric Erhardt <eric.erhardt@microsoft.com>
2015-10-27 18:38:50 -0500

    Add shim for libcurl version info.
    Adding a native shim for curl_version_info and removing the managed constants that are no longer needed.

---
+++ src/Native/System.Net.Http.Native/pal_versioninfo.h
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#pragma once
+
+#include "pal_types.h"
+
+/*
+Gets the curl version information.
+
+Returns 1 upon success, otherwise 0.
+*/
+extern "C" int32_t GetCurlVersionInfo(int32_t* age, int32_t* supportsSsl, int32_t* supportsAutoDecompression);
