beff9f605579a25d608f6e22fc79c9c3f552ba0e
Jon Hanna <jon@hackcraft.net>
2015-10-27 03:40:06 +0000

    Interpreted expressions shouldn't write back to delegate instances of calls.
    Fixes #4150
    
    Interpreted expressions executing a `‎Call()` write back to the instance of the
    method after the call, so that mutable structs are mutated correctly. This is
    harmless in most cases to which it doesn't apply, but can cause bugs when the
    instance is mutated from the outside over the course of the call, including
    when delegates are `Invoke()`d as that becomes a `Call()` with the delegate
    as its instance.

--- src/System.Linq.Expressions/tests/Invoke/InvocationTests.cs
+++ src/System.Linq.Expressions/tests/Invoke/InvocationTests.cs
@@ -54,7 +54,6 @@ namespace Tests
         }
 
         [Fact]
-        [ActiveIssue(4150)]
         public static void InvocationDoesNotChangeFunctionInvokedInterpreted()
         {
             FuncHolder holder = new FuncHolder();
