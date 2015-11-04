d22ce1196b1d30c9913302a812f489b8953aec38
bartonjs <jbarton@microsoft.com>
2015-11-03 18:39:53 -0800

    Add support for exporting as a PKCS#7 on Unix
    This change also finishes off the exceptions for unsupported formats in Unix.  SerializedStore and SerializedCert are PlatformNotSupportedException, anything else is the same "unknown type" CryptographicException from Windows.

--- src/Native/System.Security.Cryptography.Native/pal_pkcs7.cpp
+++ src/Native/System.Security.Cryptography.Native/pal_pkcs7.cpp
@@ -25,6 +25,25 @@ extern "C" PKCS7* D2IPkcs7Bio(BIO* bp)
     return d2i_PKCS7_bio(bp, nullptr);
 }
 
+extern "C" PKCS7* Pkcs7CreateSigned()
+{
+    PKCS7* pkcs7 = PKCS7_new();
+
+    if (pkcs7 == nullptr)
+    {
+        return nullptr;
+    }
+
+    if (!PKCS7_set_type(pkcs7, NID_pkcs7_signed) ||
+        !PKCS7_content_new(pkcs7, NID_pkcs7_data))
+    {
+        PKCS7_free(pkcs7);
+        return nullptr;
+    }
+
+    return pkcs7;
+}
+
 extern "C" void Pkcs7Destroy(PKCS7* p7)
 {
     if (p7 != nullptr)
@@ -51,4 +70,24 @@ extern "C" int32_t GetPkcs7Certificates(PKCS7* p7, X509Stack** certs)
     }
 
     return 0;
+}
+
+extern "C" int32_t Pkcs7AddCertificate(PKCS7* p7, X509* x509)
+{
+    if (p7 == nullptr || x509 == nullptr)
+    {
+        return 0;
+    }
+
+    return PKCS7_add_certificate(p7, x509);
+}
+
+extern "C" int32_t GetPkcs7DerSize(PKCS7* p7)
+{
+    return i2d_PKCS7(p7, nullptr);
+}
+
+extern "C" int32_t EncodePkcs7(PKCS7* p7, uint8_t* buf)
+{
+    return i2d_PKCS7(p7, &buf);
 }
