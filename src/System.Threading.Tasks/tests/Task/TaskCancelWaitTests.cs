e97a2283711bfd4d5f7c4515b2b13dba39e76f24
Stephen A. Imhoff <clockwork-muse@outlook.com>
2015-11-04 14:36:25 +0900

    Correct namespace, explicitly specify namespace for old helper types.

--- src/System.Threading.Tasks/tests/Task/TaskCancelWaitTests.cs
+++ src/System.Threading.Tasks/tests/Task/TaskCancelWaitTests.cs
@@ -4,15 +4,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
-using System;
 
-namespace System.Threading.Tasks.Tests.CancelWait
+using CW = System.Threading.Tasks.Tests.CancelWait;
+
+namespace System.Threading.Tasks.Tests
 {
     public static class TaskCancelWaitTests
     {
         #region Test Methods
 
-        private static TaskInfo Nest(TaskInfo root, params Func<TaskInfo, TaskInfo>[] children)
+        private static CW.TaskInfo Nest(CW.TaskInfo root, params Func<CW.TaskInfo, CW.TaskInfo>[] children)
         {
             root.AddChildren(children.Select(f => f(root)).ToArray());
             return root;
@@ -20,65 +21,65 @@ namespace System.Threading.Tasks.Tests.CancelWait
 
         public static IEnumerable<object[]> Task_Data()
         {
-            foreach (WorkloadType load in Enum.GetValues(typeof(WorkloadType)))
+            foreach (CW.WorkloadType load in Enum.GetValues(typeof(CW.WorkloadType)))
             {
                 foreach (TaskCreationOptions option in new[] { TaskCreationOptions.None, TaskCreationOptions.LongRunning })
                 {
                     // 14, 5, 19, 16
-                    yield return new object[] { new TaskInfo(null, "node", load, option) };
+                    yield return new object[] { new CW.TaskInfo(null, "node", load, option) };
 
-                    foreach (WorkloadType work in Enum.GetValues(typeof(WorkloadType)))
+                    foreach (CW.WorkloadType work in Enum.GetValues(typeof(CW.WorkloadType)))
                     {
                         // 22, 4, 1, 9, 23
                         yield return new object[] {
-                            Nest(new TaskInfo(null, "node", load, option),
-                                node => new TaskInfo(node, "node_1", work, option))
+                            Nest(new CW.TaskInfo(null, "node", load, option),
+                                node => new CW.TaskInfo(node, "node_1", work, option))
                             };
                         yield return new object[] {
-                            Nest(new TaskInfo(null, "node", load, option),
-                                node => new TaskInfo(node, "node_1", work, option, true))
+                            Nest(new CW.TaskInfo(null, "node", load, option),
+                                node => new CW.TaskInfo(node, "node_1", work, option, true))
                             };
                         yield return new object[] {
-                            Nest(new TaskInfo(null, "node", load, option, true, false),
-                                node => new TaskInfo(node, "node_1", work, option))
+                            Nest(new CW.TaskInfo(null, "node", load, option, true, false),
+                                node => new CW.TaskInfo(node, "node_1", work, option))
                             };
                         yield return new object[] {
-                            Nest(new TaskInfo(null, "node", load, option),
-                                node => new TaskInfo(node, "node_1", work, option | TaskCreationOptions.AttachedToParent))
+                            Nest(new CW.TaskInfo(null, "node", load, option),
+                                node => new CW.TaskInfo(node, "node_1", work, option | TaskCreationOptions.AttachedToParent))
                             };
                         yield return new object[] {
-                            Nest(new TaskInfo(null, "node", load, option),
-                                node => new TaskInfo(node, "node_1", work, option| TaskCreationOptions.AttachedToParent, true))
+                            Nest(new CW.TaskInfo(null, "node", load, option),
+                                node => new CW.TaskInfo(node, "node_1", work, option| TaskCreationOptions.AttachedToParent, true))
                             };
                     }
 
                     // 18, 21, 8, 13, 2, 24, 12, 10
                     yield return new object[] {
-                        Nest(new TaskInfo(null, "node", load, option),
-                            node => new TaskInfo(node, "node_1", WorkloadType.Medium, TaskCreationOptions.LongRunning | TaskCreationOptions.AttachedToParent),
-                            node => new TaskInfo(node, "node_2", WorkloadType.Light, TaskCreationOptions.AttachedToParent, true),
-                            node => new TaskInfo(node, "node_3", WorkloadType.VeryHeavy, TaskCreationOptions.AttachedToParent, true),
-                            node => new TaskInfo(node, "node_4", WorkloadType.VeryHeavy, TaskCreationOptions.LongRunning, true),
-                            node => new TaskInfo(node, "node_5", WorkloadType.VeryLight, TaskCreationOptions.LongRunning | TaskCreationOptions.AttachedToParent),
-                            node => new TaskInfo(node, "node_6", WorkloadType.VeryLight, TaskCreationOptions.LongRunning, true),
-                            node => new TaskInfo(node, "node_7", WorkloadType.Medium, TaskCreationOptions.AttachedToParent, true))
+                        Nest(new CW.TaskInfo(null, "node", load, option),
+                            node => new CW.TaskInfo(node, "node_1", CW.WorkloadType.Medium, TaskCreationOptions.LongRunning | TaskCreationOptions.AttachedToParent),
+                            node => new CW.TaskInfo(node, "node_2", CW.WorkloadType.Light, TaskCreationOptions.AttachedToParent, true),
+                            node => new CW.TaskInfo(node, "node_3", CW.WorkloadType.VeryHeavy, TaskCreationOptions.AttachedToParent, true),
+                            node => new CW.TaskInfo(node, "node_4", CW.WorkloadType.VeryHeavy, TaskCreationOptions.LongRunning, true),
+                            node => new CW.TaskInfo(node, "node_5", CW.WorkloadType.VeryLight, TaskCreationOptions.LongRunning | TaskCreationOptions.AttachedToParent),
+                            node => new CW.TaskInfo(node, "node_6", CW.WorkloadType.VeryLight, TaskCreationOptions.LongRunning, true),
+                            node => new CW.TaskInfo(node, "node_7", CW.WorkloadType.Medium, TaskCreationOptions.AttachedToParent, true))
                     };
 
                     // 20, 6, 15, 3, 17, 11, 25, 7
                     yield return new object[] {
-                        Nest(new TaskInfo(null, "node", load, option),
-                            node => Nest(new TaskInfo(node, "node_1", WorkloadType.VeryLight, TaskCreationOptions.None),
-                                node_1 => new TaskInfo(node_1, "node_1_1", WorkloadType.Heavy, TaskCreationOptions.AttachedToParent, true),
-                                node_1 => new TaskInfo(node_1, "node_1_2", WorkloadType.VeryHeavy, TaskCreationOptions.LongRunning, true),
-                                node_1 => new TaskInfo(node_1, "node_1_3", WorkloadType.Medium, TaskCreationOptions.LongRunning | TaskCreationOptions.AttachedToParent)),
-                            node => Nest(new TaskInfo(node, "node_2", WorkloadType.Heavy, TaskCreationOptions.LongRunning),
-                                node_2 => new TaskInfo(node_2, "node_2_1", WorkloadType.VeryHeavy, TaskCreationOptions.LongRunning),
-                                node_2 => new TaskInfo(node_2, "node_2_2", WorkloadType.Heavy, TaskCreationOptions.None),
-                                node_2 => new TaskInfo(node_2, "node_2_3", WorkloadType.Heavy, TaskCreationOptions.AttachedToParent)),
-                            node => Nest(new TaskInfo(node, "node_3", WorkloadType.Heavy, TaskCreationOptions.AttachedToParent),
-                                node_3 => new TaskInfo(node_3, "node_3_1", WorkloadType.Heavy, TaskCreationOptions.None, true),
-                                node_3 => new TaskInfo(node_3, "node_3_2", WorkloadType.Heavy, TaskCreationOptions.AttachedToParent)),
-                            node => new TaskInfo(node, "node_4", WorkloadType.Medium, TaskCreationOptions.AttachedToParent, true))
+                        Nest(new CW.TaskInfo(null, "node", load, option),
+                            node => Nest(new CW.TaskInfo(node, "node_1", CW.WorkloadType.VeryLight, TaskCreationOptions.None),
+                                node_1 => new CW.TaskInfo(node_1, "node_1_1", CW.WorkloadType.Heavy, TaskCreationOptions.AttachedToParent, true),
+                                node_1 => new CW.TaskInfo(node_1, "node_1_2", CW.WorkloadType.VeryHeavy, TaskCreationOptions.LongRunning, true),
+                                node_1 => new CW.TaskInfo(node_1, "node_1_3", CW.WorkloadType.Medium, TaskCreationOptions.LongRunning | TaskCreationOptions.AttachedToParent)),
+                            node => Nest(new CW.TaskInfo(node, "node_2", CW.WorkloadType.Heavy, TaskCreationOptions.LongRunning),
+                                node_2 => new CW.TaskInfo(node_2, "node_2_1", CW.WorkloadType.VeryHeavy, TaskCreationOptions.LongRunning),
+                                node_2 => new CW.TaskInfo(node_2, "node_2_2", CW.WorkloadType.Heavy, TaskCreationOptions.None),
+                                node_2 => new CW.TaskInfo(node_2, "node_2_3", CW.WorkloadType.Heavy, TaskCreationOptions.AttachedToParent)),
+                            node => Nest(new CW.TaskInfo(node, "node_3", CW.WorkloadType.Heavy, TaskCreationOptions.AttachedToParent),
+                                node_3 => new CW.TaskInfo(node_3, "node_3_1", CW.WorkloadType.Heavy, TaskCreationOptions.None, true),
+                                node_3 => new CW.TaskInfo(node_3, "node_3_2", CW.WorkloadType.Heavy, TaskCreationOptions.AttachedToParent)),
+                            node => new CW.TaskInfo(node, "node_4", CW.WorkloadType.Medium, TaskCreationOptions.AttachedToParent, true))
                     };
                 }
             }
@@ -122,28 +123,28 @@ namespace System.Threading.Tasks.Tests.CancelWait
 
         [Theory]
         [MemberData("Task_Data")]
-        public static void Task_Cancel_Test(TaskInfo node)
+        public static void Task_Cancel_Test(CW.TaskInfo node)
         {
-            TestParameters parameters = new TestParameters(node, API.Cancel, WaitBy.None, -1);
+            CW.TestParameters parameters = new CW.TestParameters(node, CW.API.Cancel, CW.WaitBy.None, -1);
 
-            TaskCancelWaitTest test = new TaskCancelWaitTest(parameters);
+            CW.TaskCancelWaitTest test = new CW.TaskCancelWaitTest(parameters);
             test.RealRun();
         }
 
         [Theory]
         [MemberData("Task_Wait_Data")]
-        public static void Task_Wait_Millisecond_Test(TaskInfo node, int duration)
+        public static void Task_Wait_Millisecond_Test(CW.TaskInfo node, int duration)
         {
-            TestParameters parameters = new TestParameters(node, API.Wait, WaitBy.Millisecond, duration);
+            CW.TestParameters parameters = new CW.TestParameters(node, CW.API.Wait, CW.WaitBy.Millisecond, duration);
 
-            TaskCancelWaitTest test = new TaskCancelWaitTest(parameters);
+            CW.TaskCancelWaitTest test = new CW.TaskCancelWaitTest(parameters);
             test.RealRun();
         }
 
         [Theory]
         [OuterLoop]
         [MemberData("Task_Wait_Data_Longrunning")]
-        public static void Task_Wait_Millisecond_Test_Longrunning(TaskInfo node, int duration)
+        public static void Task_Wait_Millisecond_Test_Longrunning(CW.TaskInfo node, int duration)
         {
             Task_Wait_Millisecond_Test(node, duration);
         }
@@ -151,28 +152,28 @@ namespace System.Threading.Tasks.Tests.CancelWait
         [Theory]
         [OuterLoop]
         [MemberData("Task_Data")]
-        public static void Task_Wait_Test(TaskInfo node)
+        public static void Task_Wait_Test(CW.TaskInfo node)
         {
-            TestParameters parameters = new TestParameters(node, API.Wait, WaitBy.None, -1);
+            CW.TestParameters parameters = new CW.TestParameters(node, CW.API.Wait, CW.WaitBy.None, -1);
 
-            TaskCancelWaitTest test = new TaskCancelWaitTest(parameters);
+            CW.TaskCancelWaitTest test = new CW.TaskCancelWaitTest(parameters);
             test.RealRun();
         }
 
         [Theory]
         [MemberData("Task_Wait_Data")]
-        public static void Task_Wait_TimeSpan_Test(TaskInfo node, int duration)
+        public static void Task_Wait_TimeSpan_Test(CW.TaskInfo node, int duration)
         {
-            TestParameters parameters = new TestParameters(node, API.Wait, WaitBy.TimeSpan, duration);
+            CW.TestParameters parameters = new CW.TestParameters(node, CW.API.Wait, CW.WaitBy.TimeSpan, duration);
 
-            TaskCancelWaitTest test = new TaskCancelWaitTest(parameters);
+            CW.TaskCancelWaitTest test = new CW.TaskCancelWaitTest(parameters);
             test.RealRun();
         }
 
         [Theory]
         [OuterLoop]
         [MemberData("Task_Wait_Data_Longrunning")]
-        public static void Task_Wait_TimeSpan_Test_Longrunning(TaskInfo node, int duration)
+        public static void Task_Wait_TimeSpan_Test_Longrunning(CW.TaskInfo node, int duration)
         {
             Task_Wait_TimeSpan_Test(node, duration);
         }
