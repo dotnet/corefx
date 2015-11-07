3e9f21b5f32e88925d97b928b9456f4cf8f5a1f5
stephentoub <stoub@microsoft.com>
2015-11-07 10:39:52 -0800

    Enable building all of corefx with an "unknown" Unix
    After all of the shim work that's been done, the vast majority of corefx assemblies now are either platform-agnostic or have a Windows build and a Unix build, without further specialization based on OS in managed code.  We do still have a few such assemblies, though (and this is unlikely to change any time soon):
    - System.Diagnostics.Process
    - System.IO.FileSystem.Watcher
    - System.Net.NetworkInformation
    - System.Runtime.InteropServices.RuntimeInformation
    
    As a result, attempting to do a full build for Unix targets other than Linux or OS X currently results in compilation failures. This commit adds a new TargetsUnknownUnix property to the build as well as stub implementations that throw PlatformNotSupportedException for all of the missing functionality that causes such compilation errors.
    
    With this change, doing ```build.cmd /p:SkipTests=true /p:OSGroup=FreeBSD" now completes successfully, as does ```build.cmd /p:SkipTests=true /p:OSGroup=Stephenix```.

---
+++ src/System.IO.FileSystem.Watcher/src/System/IO/FileSystemWatcher.UnknownUnix.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace System.IO
+{
+    public partial class FileSystemWatcher
+    {
+        /// <summary>Called when FileSystemWatcher is finalized.</summary>
+        private void FinalizeDispose()
+        {
+        }
+
+        private void StartRaisingEvents()
+        {
+            throw new PlatformNotSupportedException();
+        }
+
+        private void StopRaisingEvents()
+        {
+            throw new PlatformNotSupportedException();
+        }
+    }
+}
