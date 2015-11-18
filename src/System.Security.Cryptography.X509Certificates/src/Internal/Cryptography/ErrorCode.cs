180fdb242eb78011bb3dac8bd4a0879c79ba7cb1
bartonjs <jbarton@microsoft.com>
2015-11-17 18:12:32 -0800

    Add contract support for new X509ChainStatusFlags values.
    These values were added in .NET Framework 4.6.1, and independently to the CoreFX implementation (but not contract).  The Windows error codes also weren't aligned, so a generic message was presented instead of the one which matches.

--- src/System.Security.Cryptography.X509Certificates/src/Internal/Cryptography/ErrorCode.cs
+++ src/System.Security.Cryptography.X509Certificates/src/Internal/Cryptography/ErrorCode.cs
@@ -17,12 +17,14 @@ namespace Internal.Cryptography
     internal static class ErrorCode
     {
         public const int CERT_E_CHAINING = unchecked((int)0x800B010A);
+        public const int CERT_E_CRITICAL = unchecked((int)0x800B0105);
         public const int CERT_E_EXPIRED = unchecked((int)0x800B0101);
         public const int CERT_E_INVALID_NAME = unchecked((int)0x800B0114);
         public const int CERT_E_INVALID_POLICY = unchecked((int)0x800B0113);
         public const int CERT_E_UNTRUSTEDROOT = unchecked((int)0x800B0109);
         public const int CERT_E_VALIDITYPERIODNESTING = unchecked((int)0x800B0102);
         public const int CERT_E_WRONG_USAGE = unchecked((int)0x800B0110);
+        public const int CERTSRV_E_WEAK_SIGNATURE_OR_KEY = unchecked((int)0x80094016);
         public const int CRYPT_E_NO_REVOCATION_CHECK = unchecked((int)0x80092012);
         public const int CRYPT_E_NOT_FOUND = unchecked((int)0x80092004);
         public const int CRYPT_E_REVOCATION_OFFLINE = unchecked((int)0x80092013);
@@ -34,5 +36,6 @@ namespace Internal.Cryptography
         public const int NTE_BAD_PUBLIC_KEY = unchecked((int)0x80090015);
         public const int TRUST_E_BASIC_CONSTRAINTS = unchecked((int)0x80096019);
         public const int TRUST_E_CERT_SIGNATURE = unchecked((int)0x80096004);
+        public const int TRUST_E_EXPLICIT_DISTRUST = unchecked((int)0x800B0111);
     }
 }
