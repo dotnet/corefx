2f63d91d96dd4ac4fdbac3d47494f3a60ea4c115
Matt Mitchell <mmitche@microsoft.com>
2015-11-19 15:53:50 -0800

    Add outerloop testing to corefx netci

--- netci.groovy
+++ netci.groovy
@@ -58,3 +58,41 @@ def project = 'dotnet/corefx'
         Utilities.addGithubPushTrigger(newJob)
     }
 }
+
+// **************************
+// Define outerloop windows testing.  Run locally on each machine.
+// **************************
+
+def osShortName = ['Windows 10': 'win10', 'Windows 7' : 'win7', 'Windows_NT' : 'windows_nt']
+[true, false].each { isPR ->
+    ['Windows 10', 'Windows 7', 'Windows_NT'].each { os ->
+        ['Debug', 'Release'].each { configuration ->
+
+            def newJobName = "outerloop_${osShortName[os]}_${configuration.toLowerCase()}"
+
+            def newJob = job(Utilities.getFullJobName(project, newJobName, isPR)) {
+                steps {
+                    batchFile("call \"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\Tools\\VsDevCmd.bat\" && Build.cmd /p:Configuration=${configuration} /p:WithCategories="InnerLoop;OuterLoop" /p:TestWithLocalLibraries=true")
+                }
+            }
+
+            // Set the affinity.  OS name matches the machine affinity.
+            Utilities.setMachineAffinity(newJob, os)
+            // Set up standard options.
+            Utilities.standardJobSetup(newJob, project, isPR)
+            // Add the unit test results
+            Utilities.addXUnitDotNETResults(newJob, 'bin/tests/**/testResults.xml')
+
+            // Set up appropriate triggers.  PR on demand, otherwise nightly
+            if (isPR) {
+                // Set PR trigger.
+                // TODO: More elaborate regex trigger?
+                Utilities.addGithubPRTrigger(newJob, "OuterLoop ${os} ${configuration}", "(?i).*test\\W+outerloop.*")
+            }
+            else {
+                // Set a periodic trigger
+                Utilities.addPeriodicTrigger(newJob, '@daily')
+            }
+        }
+    }
+}
