4b789b544cd66315a12ee81a214c5299f295c54c
Pat Gavlin <pgavlin@gmail.com>
2015-11-30 16:22:16 -0800

    Minor changes to the Poll() shim.
    - Minor cosmetic changes to the Poll() datatypes
    - Change the Poll() shim to return an Error instead of an errno.
      The number of triggered file descriptors is now an output
      parameter.
    - Fix the implementation of Poll() to handle > 2 poll events.
    
    This change is preparing for a future change that will move
    Socket.{Select,Poll} to use `poll` instead of `select`.

--- src/Common/src/Interop/Unix/System.Native/Interop.Poll.cs
+++ src/Common/src/Interop/Unix/System.Native/Interop.Poll.cs
@@ -8,54 +8,54 @@ internal static partial class Interop
 {
     internal static partial class Sys
     {
-        internal enum PollFlags : short
+        internal enum PollEvents : short
         {
-            POLLIN   = 0x0001,  /* any readable data available */
-            POLLOUT  = 0x0004,  /* data can be written without blocked */
-            POLLERR  = 0x0008,  /* an error occurred */
-            POLLHUP  = 0x0010,  /* the file descriptor hung up */
-            POLLNVAL = 0x0020,  /* the requested events were invalid */
+            POLLNONE = 0x0000,  // No events occurred.
+            POLLIN   = 0x0001,  // any readable data available
+            POLLOUT  = 0x0004,  // data can be written without blocked
+            POLLERR  = 0x0008,  // an error occurred
+            POLLHUP  = 0x0010,  // the file descriptor hung up
+            POLLNVAL = 0x0020,  // the requested events were invalid
         }
 
-        internal struct PollFD
+        internal struct PollEvent
         {
-            internal int        FD;     // The file descriptor to poll
-            internal PollFlags  Events;    // The events to poll for
-            internal PollFlags  REvents;     // The events that occurred which triggered the poll
+            internal int FileDescriptor;         // The file descriptor to poll
+            internal PollEvents Events;          // The events to poll for
+            internal PollEvents TriggeredEvents; // The events that occurred which triggered the poll
         }
 
         /// <summary>
         /// Polls a set of file descriptors for signals and returns what signals have been set
         /// </summary>
-        /// <param name="pollData">A pointer to pollfd structs to look for</param>
-        /// <param name="numberOfPollFds">The number of entries in pollData</param>
+        /// <param name="pollEvents">A list of PollEvent entries</param>
+        /// <param name="numberOfPollFds">The number of entries in pollEvents</param>
         /// <param name="timeout">The amount of time to wait; -1 for infinite, 0 for immediate return, and a positive number is the number of milliseconds</param>
-        /// <returns>
-        /// Returns a positive number (which is the number of structures with nonzero revent files), 0 for a timeout or no 
-        /// descriptors were ready, or -1 on error.
-        /// </returns>
-        [DllImport(Libraries.SystemNative, SetLastError = true)]
-        internal static unsafe extern int Poll(PollFD* pollData, uint numberOfPollFds, int timeout);
+        /// <param name="triggered">The number of events triggered (i.e. the number of entries in pollEvents with a non-zero TriggeredEvents). May be zero in the event of a timeout.</param>
+        /// <returns>An error or Error.SUCCESS.</returns>
+        [DllImport(Libraries.SystemNative)]
+        internal static unsafe extern Error Poll(PollEvent* pollEvents, uint eventCount, int timeout, uint* triggered);
 
         /// <summary>
         /// Polls a File Descriptor for the passed in flags.
         /// </summary>
         /// <param name="fd">The descriptor to poll</param>
-        /// <param name="flags">The flags to poll for</param>
+        /// <param name="events">The events to poll for</param>
         /// <param name="timeout">The amount of time to wait; -1 for infinite, 0 for immediate return, and a positive number is the number of milliseconds</param>
-        /// <param name="resultFlags">The flags that were returned by the poll call</param>
-        /// <returns>
-        /// Returns a positive number (which is the number of structures with nonzero revent files), 0 for a timeout or no 
-        /// descriptors were ready, or -1 on error.
-        /// </returns>
-        internal static unsafe int Poll(int fd, PollFlags flags, int timeout, out PollFlags resultFlags)
+        /// <param name="triggered">The events that were returned by the poll call. May be PollEvents.POLLNONE in the case of a timeout.</param>
+        /// <returns>An error or Error.SUCCESS.</returns>
+        internal static unsafe Error Poll(int fd, PollEvents events, int timeout, out PollEvents triggered)
         {
-            PollFD pfd = default(PollFD);
-            pfd.FD = fd;
-            pfd.Events = flags;
-            int result = Poll(&pfd, 1, timeout);
-            resultFlags = pfd.REvents;
-            return result;
+            var pollEvent = new PollEvent
+            {
+                FileDescriptor = fd,
+                Events = events,
+            };
+
+            uint unused;
+            Error err = Poll(&pollEvent, 1, timeout, &unused);
+            triggered = pollEvent.TriggeredEvents;
+            return err;
         }
     }
 }
