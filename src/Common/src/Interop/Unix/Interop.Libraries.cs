af23e104e4912df980804b9e91667cc456951579
stephentoub <stoub@microsoft.com>
2015-12-02 16:33:55 -0500

    Remove System.Threading.Semaphore implementation from corefx
    It's moving into mscorlib. This removes the last of the corefx dependencies on the libcoreclr PAL.  Also means that System.Threading.dll no longer needs separate Windows and Unix builds.

--- src/Common/src/Interop/Unix/Interop.Libraries.cs
+++ src/Common/src/Interop/Unix/Interop.Libraries.cs
@@ -5,8 +5,6 @@ internal static partial class Interop
 {
     private static partial class Libraries
     {
-        internal const string LibCoreClr = "libcoreclr";       // CoreCLR runtime
-
         // Shims
         internal const string SystemNative = "System.Native";
         internal const string HttpNative = "System.Net.Http.Native";
