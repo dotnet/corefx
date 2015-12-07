93f2685bde7995454d9df76614f6e68214a6fd51
stephentoub <stoub@microsoft.com>
2015-12-07 16:50:08 -0500

    Ensure all CheckIo calls are wrapped in whiles

--- src/System.Runtime.Extensions/src/System/IO/Path.Unix.cs
+++ src/System.Runtime.Extensions/src/System/IO/Path.Unix.cs
@@ -189,7 +189,7 @@ namespace System.IO
 
             // Create, open, and close the temp file.
             IntPtr fd;
-            Interop.CheckIo(fd = Interop.Sys.MksTemps(name, SuffixByteLength));
+            while (Interop.CheckIo(fd = Interop.Sys.MksTemps(name, SuffixByteLength))) ;
             Interop.Sys.Close(fd); // ignore any errors from close; nothing to do if cleanup isn't possible
 
             // 'name' is now the name of the file
