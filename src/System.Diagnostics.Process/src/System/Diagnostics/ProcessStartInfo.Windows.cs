973203e4b751331238516ba5ba9baacbe0fb0ddd
stephentoub <stoub@microsoft.com>
2015-12-07 12:51:28 -0500

    Add ProcessStartInfo.UseShellExecute support on Unix
    Implemented via the existing fork/execve mechanism, but using /bin/sh as the target filename.

--- src/System.Diagnostics.Process/src/System/Diagnostics/ProcessStartInfo.Windows.cs
+++ src/System.Diagnostics.Process/src/System/Diagnostics/ProcessStartInfo.Windows.cs
@@ -37,5 +37,19 @@ namespace System.Diagnostics
             get { return _loadUserProfile; }
             set { _loadUserProfile = value; }
         }
+
+        // CoreCLR can't correctly support UseShellExecute=true for the following reasons
+        // 1. ShellExecuteEx is not supported on onecore.
+        // 2. ShellExecuteEx needs to run as STA but managed code runs as MTA by default and Thread.SetApartmentState() is not supported on all platforms.
+        //
+        // Irrespective of the limited functionality of the property we still expose it in the contract as it can be implemented
+        // on other platforms.  Further, the default value of UseShellExecute is true on desktop and scenarios like redirection mandates 
+        // the value to be false. So in order to provide maximum code portability we expose UseShellExecute in the contract 
+        // and throw PlatformNotSupportedException in portable library in case it is set to true.
+        public bool UseShellExecute
+        {
+            get { return false; }
+            set { if (value == true) throw new PlatformNotSupportedException(SR.UseShellExecute); }
+        }
     }
 }
