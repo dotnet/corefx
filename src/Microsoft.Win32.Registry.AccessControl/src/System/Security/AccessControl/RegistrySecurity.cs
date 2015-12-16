9e45f4652aea4928dc369c6be5cce22b6b39092e
Eric Mellino <erme@microsoft.com>
2015-12-15 16:43:24 -0800

    Respond to PR feedback

--- src/Microsoft.Win32.Registry.AccessControl/src/System/Security/AccessControl/RegistrySecurity.cs
+++ src/Microsoft.Win32.Registry.AccessControl/src/System/Security/AccessControl/RegistrySecurity.cs
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Security.Principal;
 using Microsoft.Win32.SafeHandles;
-using System.Runtime.InteropServices;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
 
 namespace System.Security.AccessControl
 {
