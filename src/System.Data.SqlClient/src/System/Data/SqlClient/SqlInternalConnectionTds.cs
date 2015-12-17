1ed61d8060d6ef447b792f1dccf80a739f2ee6fb
Saurabh Singh <saurabh500@gmail.com>
2015-12-16 19:42:40 -0800

    Addressing PR comments

--- src/System.Data.SqlClient/src/System/Data/SqlClient/SqlInternalConnectionTds.cs
+++ src/System.Data.SqlClient/src/System/Data/SqlClient/SqlInternalConnectionTds.cs
@@ -109,7 +109,44 @@ namespace System.Data.SqlClient
         internal SessionData _currentSessionData; // internal for use from TdsParser only, otehr should use CurrentSessionData property that will fix database and language
         private SessionData _recoverySessionData;
 
-        private static HashSet<int> transientErrors = new HashSet<int>();
+        // The erros in the transient error set are contained in
+        // https://azure.microsoft.com/en-us/documentation/articles/sql-database-develop-error-messages/#transient-faults-connection-loss-and-other-temporary-errors
+        private static readonly HashSet<int> s_transientErrors = new HashSet<int>
+        {
+            // SQL Error Code: 4060
+            // Cannot open database "%.*ls" requested by the login. The login failed.
+            4060,
+
+            // SQL Error Code: 10928
+            // Resource ID: %d. The %s limit for the database is %d and has been reached.
+            10928,
+
+            // SQL Error Code: 10929
+            // Resource ID: %d. The %s minimum guarantee is %d, maximum limit is %d and the current usage for the database is %d. 
+            // However, the server is currently too busy to support requests greater than %d for this database.
+            10929,
+
+            // SQL Error Code: 40197
+            // You will receive this error, when the service is down due to software or hardware upgrades, hardware failures, 
+            // or any other failover problems. The error code (%d) embedded within the message of error 40197 provides 
+            // additional information about the kind of failure or failover that occurred. Some examples of the error codes are 
+            // embedded within the message of error 40197 are 40020, 40143, 40166, and 40540.
+            40197,
+
+            40020,
+            40143,
+            40166,
+
+            // The service has encountered an error processing your request. Please try again.
+            40540,
+
+            // The service is currently busy. Retry the request after 10 seconds. Incident ID: %ls. Code: %d.
+            40501,
+
+            // Database '%.*ls' on server '%.*ls' is not currently available. Please retry the connection later. 
+            // If the problem persists, contact customer support, and provide them the session tracing ID of '%.*ls'.
+            40613
+        };
 
         internal SessionData CurrentSessionData
         {
@@ -261,11 +298,7 @@ namespace System.Data.SqlClient
         private RoutingInfo _routingInfo = null;
         private Guid _originalClientConnectionId = Guid.Empty;
         private string _routingDestination = null;
-
-        static SqlInternalConnectionTds()
-        {
-            populateTransientErrors();
-        }
+        
 
         // although the new password is generally not used it must be passed to the c'tor
         // the new Login7 packet will always write out the new password (or a length of zero and no bytes if not present)
@@ -355,39 +388,7 @@ namespace System.Data.SqlClient
             }
         }
 
-        // The erros in the transient error set are contained in
-        // https://azure.microsoft.com/en-us/documentation/articles/sql-database-develop-error-messages/#transient-faults-connection-loss-and-other-temporary-errors
-        private static void populateTransientErrors()
-        {
-            // SQL Error Code: 4060
-            // Cannot open database "%.*ls" requested by the login. The login failed.
-            transientErrors.Add(4060);
-            // SQL Error Code: 10928
-            // Resource ID: %d. The %s limit for the database is %d and has been reached.
-            transientErrors.Add(10928);
-            // SQL Error Code: 10929
-            // Resource ID: %d. The %s minimum guarantee is %d, maximum limit is %d and the current usage for the database is %d. 
-            // However, the server is currently too busy to support requests greater than %d for this database.
-            transientErrors.Add(10929);
-            // SQL Error Code: 40197
-            // You will receive this error, when the service is down due to software or hardware upgrades, hardware failures, 
-            // or any other failover problems. The error code (%d) embedded within the message of error 40197 provides 
-            // additional information about the kind of failure or failover that occurred. Some examples of the error codes are 
-            // embedded within the message of error 40197 are 40020, 40143, 40166, and 40540.
-            transientErrors.Add(40197);
-            transientErrors.Add(40020);
-            transientErrors.Add(40143);
-            transientErrors.Add(40166);
-            // The service has encountered an error processing your request. Please try again.
-            transientErrors.Add(40540);
-            // The service is currently busy. Retry the request after 10 seconds. Incident ID: %ls. Code: %d.
-            transientErrors.Add(40501);
-            // Database '%.*ls' on server '%.*ls' is not currently available. Please retry the connection later. 
-            // If the problem persists, contact customer support, and provide them the session tracing ID of '%.*ls'.
-            transientErrors.Add(40613);
-        }
-
-
+        
         // Returns true if the Sql error is a transient.
         private bool IsTransientError(SqlException exc)
         {
@@ -397,7 +398,7 @@ namespace System.Data.SqlClient
             }
             foreach (SqlError error in exc.Errors)
             {
-                if (transientErrors.Contains(error.Number))
+                if (s_transientErrors.Contains(error.Number))
                 {
                     return true;
                 }
