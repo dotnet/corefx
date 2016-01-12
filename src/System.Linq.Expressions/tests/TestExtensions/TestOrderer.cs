9f844838c1be02e5d4dba00c87fa8bc5f3d88b28
Jon Hanna <jon@hackcraft.net>
2016-01-12 23:53:27 +0000

    Tests for Expression class itself, rather than its derived types and its factory methods the produce them.
    Focuses on its methods of correctly dealing with errors in custom
    expressions, as those are not covered by any of the other tests.
    
    Tests the validation methods used by subclasses via uses in those
    subclasses.

---
+++ src/System.Linq.Expressions/tests/TestExtensions/TestOrderer.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace System.Linq.Expressions.Tests
+{
+    /// <summary>Forces tests to be carried out according to the order of their <see cref="TestOrderAttribute.Order"/>, with
+    /// those tests with no attribute happening in the same batch as those with an Order of zero.</summary>
+    internal class TestOrderer : ITestCaseOrderer
+    {
+        IEnumerable<TTestCase> ITestCaseOrderer.OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases)
+        {
+            Dictionary<int, List<TTestCase>> queue = new Dictionary<int, List<TTestCase>>();
+            foreach (TTestCase testCase in testCases)
+            {
+                var orderAttribute = testCase.TestMethod.Method.GetCustomAttributes(typeof(TestOrderAttribute)).FirstOrDefault();
+                int order;
+                if (orderAttribute == null || (order = orderAttribute.GetConstructorArguments().Cast<int>().First()) == 0)
+                {
+                    yield return testCase;
+                }
+                else
+                {
+                    List<TTestCase> batch;
+                    if (!queue.TryGetValue(order, out batch))
+                        queue.Add(order, batch = new List<TTestCase>());
+                    batch.Add(testCase);
+                }
+            }
+            foreach (var order in queue.Keys.OrderBy(i => i))
+                foreach (var testCase in queue[order])
+                    yield return testCase;
+        }
+    }
+}
