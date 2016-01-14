3d92738669b44bd8be981c3fc700efbbc0cd4f1c
Lakshmi Priya Sekar <lasekar@microsoft.com>
2016-01-14 12:22:57 -0800

    Add Environment.MachineName to .NET Core.

---
+++ src/Common/src/Interop/Unix/System.Native/Interop.GetNodeName.cs
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+internal static partial class Interop
+{
+    internal static partial class Sys
+    {
+        [DllImport(Libraries.SystemNative, EntryPoint = "SystemNative_GetNodeName", SetLastError = true)]
+        private static extern unsafe int GetNodeName(char* name, out int len);
+
+        internal static unsafe string GetNodeName()
+        {
+            // max value of _UTSNAME_LENGTH on known Unix platforms is 1024.
+            const int _UTSNAME_LENGTH = 1024;
+            int len = _UTSNAME_LENGTH;
+            char* name = stackalloc char[_UTSNAME_LENGTH];
+            int err = GetNodeName(name, out len);
+            if (err != 0)
+            {
+                // max domain name can be 255 chars. 
+                Debug.Fail("getnodename failed");
+                throw new InvalidOperationException(string.Format("getnodename returned {0}", err));
+            }
+
+            // Marshal.PtrToStringAnsi uses UTF8 on Unix.
+            return Marshal.PtrToStringAnsi((IntPtr)name);
+        }
+    }
+}
