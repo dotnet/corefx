0e5b75a051d53936bc49e8b762efc11b052466a8
Khoa Dang <Khoa.Dang@microsoft.com>
2016-01-15 14:19:02 -0800

    Fix typo introduced during merge

--- src/System.Runtime.Serialization.Xml/tests/DataContractSerializer.cs
+++ src/System.Runtime.Serialization.Xml/tests/DataContractSerializer.cs
@@ -1964,8 +1964,10 @@ public static partial class DataContractSerializerTests
         Assert.Throws<InvalidDataContractException>(() => {
             var obj = new TypeWithEnumerableInterfaceGetOnlyCollection(new List<string>() { "item1", "item2", "item3" });
             SerializeAndDeserialize(obj, @"<TypeWithEnumerableInterfaceGetOnlyCollection xmlns=""http://schemas.datacontract.org/2004/07/"" xmlns:i=""http://www.w3.org/2001/XMLSchema-instance""><Items xmlns:a=""http://schemas.microsoft.com/2003/10/Serialization/Arrays""><a:string>item1</a:string><a:string>item2</a:string><a:string>item3</a:string></Items></TypeWithEnumerableInterfaceGetOnlyCollection>");
+        });
     }
 
+    [Fact]
     public static void DCS_RecursiveCollection()
     {
         Assert.Throws<InvalidDataContractException>(() =>
