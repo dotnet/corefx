5fa94653304b7ad9cbd7703219d86a9a3b31f153
Koundinya Veluri <kouvel@microsoft.com>
2016-01-25 13:31:05 -0800

    Expose AssemblyLoadContext.Unloading event publically
    This change is dependent on https://github.com/dotnet/coreclr/pull/2867, which actually implements the Unloading event.
    
    API review: https://github.com/dotnet/corefx/issues/5205

--- src/System.Runtime.Loader/ref/System.Runtime.Loader.cs
+++ src/System.Runtime.Loader/ref/System.Runtime.Loader.cs
@@ -33,5 +33,6 @@ namespace System.Runtime.Loader
         public void SetProfileOptimizationRoot(string directoryPath) { }
         public void StartProfileOptimization(string profile) { }
         public event Func<AssemblyLoadContext, System.Reflection.AssemblyName, System.Reflection.Assembly> Resolving;
+        public event Action<AssemblyLoadContext> Unloading;
     }
 }
