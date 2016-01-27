cb294cecc5cba8c58b7e597675501fa70fae276f
Jon Hanna <jon@hackcraft.net>
2016-01-27 11:07:26 +0000

    Remove personal copyright from License Headers
    Change all license headers © Jon Hanna to .NET Foundation and Contributors.
    
    Change all license headers © .NET Foundation and contributors. (lower case) to
    .NET Foundation and Contributors to match form in project license file.

--- src/System.Linq.Expressions/tests/Goto/Continue.cs
+++ src/System.Linq.Expressions/tests/Goto/Continue.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Jon Hanna. All rights reserved.
+﻿// Copyright (c) .NET Foundation and Contributors. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
