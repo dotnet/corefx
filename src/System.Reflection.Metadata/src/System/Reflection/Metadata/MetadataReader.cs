f2124b43f8f77c5fcc2621f03c48d2ff25ed41d6
Tomas Matousek <tomas.matousek@microsoft.com>
2016-01-26 18:51:00 -0800

    Disallow nil MethodDef token as an entry-point in Portable PDB

--- src/System.Reflection.Metadata/src/System/Reflection/Metadata/MetadataReader.cs
+++ src/System.Reflection.Metadata/src/System/Reflection/Metadata/MetadataReader.cs
@@ -486,7 +486,7 @@ namespace System.Reflection.Metadata
             }
         }
 
-        private int[] ReadMetadataTableRowCounts(ref BlobReader memReader, ulong presentTableMask)
+        private static int[] ReadMetadataTableRowCounts(ref BlobReader memReader, ulong presentTableMask)
         {
             ulong currentTableBit = 1;
 
@@ -515,7 +515,8 @@ namespace System.Reflection.Metadata
             return rowCounts;
         }
 
-        private void ReadStandalonePortablePdbStream(MemoryBlock block, out DebugMetadataHeader debugMetadataHeader, out int[] externalTableRowCounts)
+        // internal for testing
+        internal static void ReadStandalonePortablePdbStream(MemoryBlock block, out DebugMetadataHeader debugMetadataHeader, out int[] externalTableRowCounts)
         {
             var reader = new BlobReader(block);
 
@@ -529,7 +530,8 @@ namespace System.Reflection.Metadata
             // The return type of the entry point method shall be void, int32, or unsigned int32. 
             // The entry point method cannot be defined in a generic class.
             uint entryPointToken = reader.ReadUInt32();
-            if (entryPointToken != 0 && (entryPointToken & TokenTypeIds.TypeMask) != TokenTypeIds.MethodDef)
+            int entryPointRowId = (int)(entryPointToken & TokenTypeIds.RIDMask);
+            if (entryPointToken != 0 && ((entryPointToken & TokenTypeIds.TypeMask) != TokenTypeIds.MethodDef || entryPointRowId == 0))
             {
                 throw new BadImageFormatException(string.Format(SR.InvalidEntryPointToken, entryPointToken));
             }
@@ -548,7 +550,7 @@ namespace System.Reflection.Metadata
 
             debugMetadataHeader = new DebugMetadataHeader(
                 ImmutableByteArrayInterop.DangerousCreateFromUnderlyingArray(ref pdbId),
-                MethodDefinitionHandle.FromRowId((int)(entryPointToken & TokenTypeIds.RIDMask)));
+                MethodDefinitionHandle.FromRowId(entryPointRowId));
         }
 
         private const int SmallIndexSize = 2;
