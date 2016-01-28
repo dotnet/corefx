41b29e83e25ee88f8bb4397fc1505ae78c714d2c
Ian Hays <ianha@microsoft.com>
2016-01-28 14:27:06 -0800

    Add tests for HashSet

---
+++ src/System.Collections/tests/Generic/HashSet/HashSet.Generic.Tests.AsNonGenericIEnumerable.cs
@@ -0,0 +1,53 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace System.Collections.Tests
+{
+    public class HashSet_IEnumerable_NonGeneric_Tests : IEnumerable_NonGeneric_Tests
+    {
+        protected override IEnumerable NonGenericIEnumerableFactory(int count)
+        {
+            var set = new HashSet<string>();
+            int seed = 12354;
+            while (set.Count < count)
+                set.Add(TFactory(set, seed++));
+            return set;
+        }
+
+        protected override int WaysToModify { get { return 1; } }
+        protected override bool Enumerator_Current_UndefinedOperation_Throws { get { return true; } }
+
+        protected override void ModifyEnumerable(IEnumerable enumerable, int enumerationCode)
+        {
+            HashSet<string> casted = ((HashSet<string>)enumerable);
+            switch (enumerationCode)
+            {
+                case 0: // Clear
+                    if (casted.Count > 0)
+                        casted.Clear();
+                    else
+                        casted.Add(TFactory(casted, 4531));
+                    break;
+            }
+        }
+
+        protected string TFactory(HashSet<string> set, int seed)
+        {
+            int stringLength = seed % 10 + 5;
+            Random rand = new Random(seed);
+            byte[] bytes = new byte[stringLength];
+            rand.NextBytes(bytes);
+            string ret = Convert.ToBase64String(bytes);
+            while (set.Contains(ret))
+            {
+                rand.NextBytes(bytes);
+                ret = Convert.ToBase64String(bytes);
+            }
+            return ret;
+        }
+
+    }
+}
---
+++ src/System.Collections/tests/Generic/HashSet/HashSet.Generic.Tests.cs
@@ -0,0 +1,284 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace System.Collections.Tests
+{
+    /// <summary>
+    /// Contains tests that ensure the correctness of the HashSet class.
+    /// </summary>
+    public abstract class HashSet_Generic_Tests<T> : ISet_Generic_Tests<T>
+    {
+        #region ISet<T> Helper Methods
+
+        protected override bool ResetImplemented
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        protected override ISet<T> GenericISetFactory()
+        {
+            return new HashSet<T>();
+        }
+
+        #endregion
+
+        #region Constructors
+
+        private static IEnumerable<int> NonSquares(int limit)
+        {
+            for (int i = 0; i != limit; ++i)
+            {
+                int root = (int)Math.Sqrt(i);
+                if (i != root * root)
+                    yield return i;
+            }
+        }
+
+        [Fact]
+        public void HashSet_Generic_Constructor()
+        {
+            HashSet<T> set = new HashSet<T>();
+            Assert.Empty(set);
+        }
+
+        [Fact]
+        public void HashSet_Generic_Constructor_IEqualityComparer()
+        {
+            IEqualityComparer<T> comparer = GetIEqualityComparer();
+            HashSet<T> set = new HashSet<T>(comparer);
+            if (comparer == null)
+                Assert.Equal(EqualityComparer<T>.Default, set.Comparer);
+            else
+                Assert.Equal(comparer, set.Comparer);
+        }
+
+        [Fact]
+        public void HashSet_Generic_Constructor_NullIEqualityComparer()
+        {
+            IEqualityComparer<T> comparer = null;
+            HashSet<T> set = new HashSet<T>(comparer);
+            if (comparer == null)
+                Assert.Equal(EqualityComparer<T>.Default, set.Comparer);
+            else
+                Assert.Equal(comparer, set.Comparer);
+        }
+
+        [Theory]
+        [MemberData("EnumerableTestData")]
+        public void HashSet_Generic_Constructor_IEnumerable(EnumerableType enumerableType, int setLength, int enumerableLength, int numberOfMatchingElements, int numberOfDuplicateElements)
+        {
+            IEnumerable<T> enumerable = CreateEnumerable(enumerableType, null, enumerableLength, 0, numberOfDuplicateElements);
+            HashSet<T> set = new HashSet<T>(enumerable);
+            Assert.True(set.SetEquals(enumerable));
+        }
+
+        [Theory]
+        [MemberData("ValidCollectionSizes")]
+        public void HashSet_Generic_Constructor_IEnumerable_WithManyDuplicates(int count)
+        {
+            IEnumerable<T> items = CreateEnumerable(EnumerableType.List, null, count, 0, 0);
+            HashSet<T> hashSetFromDuplicates = new HashSet<T>(Enumerable.Range(0, 40).SelectMany(i => items).ToArray());
+            HashSet<T> hashSetFromNoDuplicates = new HashSet<T>(items);
+            Assert.True(hashSetFromNoDuplicates.SetEquals(hashSetFromDuplicates));
+        }
+
+        [Theory]
+        [MemberData("ValidCollectionSizes")]
+        public void HashSet_Generic_Constructor_HashSet_SparselyFilled(int count)
+        {
+            HashSet<T> source = (HashSet <T>)CreateEnumerable(EnumerableType.HashSet, null, count, 0, 0);
+            List<T> sourceElements = source.ToList();
+            foreach (int i in NonSquares(count))
+                source.Remove(sourceElements[i]);// Unevenly spaced survivors increases chance of catching any spacing-related bugs.
+
+
+            HashSet<T> set = new HashSet<T>(source, GetIEqualityComparer());
+            Assert.True(set.SetEquals(source));
+        }
+
+        [Fact]
+        public void HashSet_Generic_Constructor_IEnumerable_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new HashSet<T>((IEnumerable<T>)null));
+            Assert.Throws<ArgumentNullException>(() => new HashSet<T>((IEnumerable<T>)null, EqualityComparer<T>.Default));
+        }
+
+        [Theory]
+        [MemberData("EnumerableTestData")]
+        public void HashSet_Generic_Constructor_IEnumerable_IEqualityComparer(EnumerableType enumerableType, int setLength, int enumerableLength, int numberOfMatchingElements, int numberOfDuplicateElements)
+        {
+            IEnumerable<T> enumerable = CreateEnumerable(enumerableType, null, enumerableLength, 0, 0);
+            HashSet<T> set = new HashSet<T>(enumerable, GetIEqualityComparer());
+            Assert.True(set.SetEquals(enumerable));
+        }
+
+        #endregion
+
+        #region RemoveWhere
+
+        [Theory]
+        [MemberData("ValidCollectionSizes")]
+        public void HashSet_Generic_RemoveWhere_AllElements(int setLength)
+        {
+            HashSet<T> set = (HashSet<T>)GenericISetFactory(setLength);
+            int removedCount = set.RemoveWhere((value) => { return true; });
+            Assert.Equal(setLength, removedCount);
+        }
+
+        [Theory]
+        [MemberData("ValidCollectionSizes")]
+        public void HashSet_Generic_RemoveWhere_NoElements(int setLength)
+        {
+            HashSet<T> set = (HashSet<T>)GenericISetFactory(setLength);
+            int removedCount = set.RemoveWhere((value) => { return false; });
+            Assert.Equal(0, removedCount);
+            Assert.Equal(setLength, set.Count);
+        }
+
+        [Theory]
+        [MemberData("ValidCollectionSizes")]
+        public void HashSet_Generic_RemoveWhere_NewObject(int setLength) // Regression Dev10_624201
+        {
+            object[] array = new object[2];
+            object obj = new object();
+            HashSet<object> set = new HashSet<object>();
+
+            set.Add(obj);
+            set.Remove(obj);
+            foreach (object o in set) { }
+            set.CopyTo(array, 0, 2);
+            set.RemoveWhere((element) => { return false; });
+        }
+
+        [Theory]
+        [MemberData("ValidCollectionSizes")]
+        public void HashSet_Generic_RemoveWhere_NullMatchPredicate(int setLength)
+        {
+            HashSet<T> set = (HashSet<T>)GenericISetFactory(setLength);
+            Assert.Throws<ArgumentNullException>(() => set.RemoveWhere(null));
+        }
+
+        #endregion
+
+        #region TrimExcess
+
+        [Theory]
+        [MemberData("ValidCollectionSizes")]
+        public void HashSet_Generic_TrimExcess_OnValidSetThatHasntBeenRemovedFrom(int setLength)
+        {
+            HashSet<T> set = (HashSet<T>)GenericISetFactory(setLength);
+            set.TrimExcess();
+        }
+
+        [Theory]
+        [MemberData("ValidCollectionSizes")]
+        public void HashSet_Generic_TrimExcess_Repeatedly(int setLength)
+        {
+            HashSet<T> set = (HashSet<T>)GenericISetFactory(setLength);
+            List<T> expected = set.ToList();
+            set.TrimExcess();
+            set.TrimExcess();
+            set.TrimExcess();
+            Assert.True(set.SetEquals(expected));
+        }
+
+        [Theory]
+        [MemberData("ValidCollectionSizes")]
+        public void HashSet_Generic_TrimExcess_AfterRemovingOneElement(int setLength)
+        {
+            if (setLength > 0)
+            {
+                HashSet<T> set = (HashSet<T>)GenericISetFactory(setLength);
+                List<T> expected = set.ToList();
+                T elementToRemove = set.ElementAt(0);
+
+                set.TrimExcess();
+                Assert.True(set.Remove(elementToRemove));
+                expected.Remove(elementToRemove);
+                set.TrimExcess();
+
+                Assert.True(set.SetEquals(expected));
+            }
+        }
+
+        [Theory]
+        [MemberData("ValidCollectionSizes")]
+        public void HashSet_Generic_TrimExcess_AfterClearingAndAddingSomeElementsBack(int setLength)
+        {
+            if (setLength > 0)
+            {
+                HashSet<T> set = (HashSet<T>)GenericISetFactory(setLength);
+                set.TrimExcess();
+                set.Clear();
+                set.TrimExcess();
+                Assert.Equal(0, set.Count);
+
+                AddToCollection(set, setLength / 10);
+                set.TrimExcess();
+                Assert.Equal(setLength / 10, set.Count);
+            }
+        }
+
+        [Theory]
+        [MemberData("ValidCollectionSizes")]
+        public void HashSet_Generic_TrimExcess_AfterClearingAndAddingAllElementsBack(int setLength)
+        {
+            if (setLength > 0)
+            {
+                HashSet<T> set = (HashSet<T>)GenericISetFactory(setLength);
+                set.TrimExcess();
+                set.Clear();
+                set.TrimExcess();
+                Assert.Equal(0, set.Count);
+
+                AddToCollection(set, setLength);
+                set.TrimExcess();
+                Assert.Equal(setLength, set.Count);
+            }
+        }
+
+        #endregion
+
+        #region CopyTo
+
+        [Theory]
+        [MemberData("ValidCollectionSizes")]
+        public void HashSet_Generic_CopyTo_NegativeCount_ThrowsArgumentOutOfRangeException(int count)
+        {
+            HashSet<T> set = (HashSet<T>)GenericISetFactory(count);
+            T[] arr = new T[count];
+            Assert.Throws<ArgumentOutOfRangeException>(() => set.CopyTo(arr, 0, -1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => set.CopyTo(arr, 0, int.MinValue));
+        }
+
+        [Theory]
+        [MemberData("ValidCollectionSizes")]
+        public void HashSet_Generic_CopyTo_NoIndexDefaultsToZero(int count)
+        {
+            HashSet<T> set = (HashSet<T>)GenericISetFactory(count);
+            T[] arr1 = new T[count];
+            T[] arr2 = new T[count];
+            set.CopyTo(arr1);
+            set.CopyTo(arr2, 0);
+            Assert.True(arr1.SequenceEqual(arr2));
+        }
+
+        #endregion
+
+        [Fact]
+        public void CanBeCastedToISet()
+        {
+            HashSet<T> set = new HashSet<T>();
+            ISet<T> iset = (set as ISet<T>);
+            Assert.NotNull(iset);
+        }
+    }
+}
