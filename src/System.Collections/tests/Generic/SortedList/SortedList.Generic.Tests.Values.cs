1143c4e470d456c8e8f89ce441e49c69b88451ac
Ian Hays <ianha@microsoft.com>
2016-01-28 14:27:54 -0800

    Add tests for SortedList

---
+++ src/System.Collections/tests/Generic/SortedList/SortedList.Generic.Tests.Values.cs
@@ -0,0 +1,95 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using Xunit;
+
+namespace System.Collections.Tests
+{
+    public class SortedList_Generic_Tests_Values : IList_Generic_Tests<string>
+    {
+        protected override bool DefaultValueAllowed { get { return true; } }
+        protected override bool DuplicateValuesAllowed { get { return true; } }
+        protected override bool IsReadOnly { get { return true; } }
+        protected override int WaysToModify { get { return 0; } }
+
+        protected override IList<string> GenericIListFactory()
+        {
+            return new SortedList<string, string>().Values;
+        }
+
+        protected override IList<string> GenericIListFactory(int count)
+        {
+            SortedList<string, string> list = new SortedList<string, string>();
+            int seed = 13453;
+            for (int i = 0; i < count; i++)
+                list.Add(TFactory(seed++), TFactory(seed++));
+            return list.Values;
+        }
+
+        protected override string TFactory(int seed)
+        {
+            int stringLength = seed % 10 + 5;
+            Random rand = new Random(seed);
+            byte[] bytes = new byte[stringLength];
+            rand.NextBytes(bytes);
+            return Convert.ToBase64String(bytes);
+        }
+
+        [Theory]
+        [MemberData("ValidCollectionSizes")]
+        public override void Enumerator_MoveNext_AfterDisposal(int count)
+        {
+            // Disposal of the enumerator is treated the same as a Reset call
+            IEnumerator<string> enumerator = GenericIEnumerableFactory(count).GetEnumerator();
+            for (int i = 0; i < count; i++)
+                enumerator.MoveNext();
+            enumerator.Dispose();
+            if (count > 0)
+                Assert.True(enumerator.MoveNext());
+        }
+    }
+
+    public class SortedList_Generic_Tests_Values_AsICollection : ICollection_NonGeneric_Tests
+    {
+        protected override bool NullAllowed { get { return true; } }
+        protected override bool DuplicateValuesAllowed { get { return true; } }
+        protected override bool IsReadOnly { get { return true; } }
+        protected override bool Enumerator_Current_UndefinedOperation_Throws { get { return true; } }
+
+        protected override ICollection NonGenericICollectionFactory()
+        {
+            return (ICollection)(new SortedList<string, string>().Values);
+        }
+
+        protected override ICollection NonGenericICollectionFactory(int count)
+        {
+            SortedList<string, string> list = new SortedList<string, string>();
+            int seed = 13453;
+            for (int i = 0; i < count; i++)
+                list.Add(TFactory(seed++), TFactory(seed++));
+            return (ICollection)(list.Values);
+        }
+
+        private string TFactory(int seed)
+        {
+            int stringLength = seed % 10 + 5;
+            Random rand = new Random(seed);
+            byte[] bytes = new byte[stringLength];
+            rand.NextBytes(bytes);
+            return Convert.ToBase64String(bytes);
+        }
+
+        protected override void AddToCollection(ICollection collection, int numberOfItemsToAdd)
+        {
+            Debug.Assert(false);
+        }
+
+        protected override void ModifyEnumerable(IEnumerable enumerable, int modificationCode)
+        {
+            Debug.Assert(false);
+        }
+    }
+}
---
+++ src/System.Collections/tests/Generic/SortedList/SortedList.Generic.cs
@@ -0,0 +1,80 @@
+﻿// Copyright(c) Microsoft.All rights reserved.
+// Licensed under the MIT license.See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Xunit;
+
+namespace System.Collections.Tests
+{
+    public class SortedList_Generic_Tests_string_string : SortedList_Generic_Tests<string, string>
+    {
+        protected override KeyValuePair<string, string> TFactory(int seed)
+        {
+            return new KeyValuePair<string, string>(TKeyFactory(seed), TKeyFactory(seed + 500));
+        }
+
+        protected override string TKeyFactory(int seed)
+        {
+            int stringLength = seed % 10 + 5;
+            Random rand = new Random(seed);
+            byte[] bytes1 = new byte[stringLength];
+            rand.NextBytes(bytes1);
+            return Convert.ToBase64String(bytes1);
+        }
+
+        protected override string TValueFactory(int seed)
+        {
+            return TKeyFactory(seed);
+        }
+    }
+
+    public class SortedList_Generic_Tests_int_int : SortedList_Generic_Tests<int, int>
+    {
+        protected override bool DefaultValueAllowed { get { return true; } }
+        protected override KeyValuePair<int, int> TFactory(int seed)
+        {
+            Random rand = new Random(seed);
+            return new KeyValuePair<int, int>(rand.Next(), rand.Next());
+        }
+
+        protected override int TKeyFactory(int seed)
+        {
+            Random rand = new Random(seed);
+            return rand.Next();
+        }
+
+        protected override int TValueFactory(int seed)
+        {
+            return TKeyFactory(seed);
+        }
+    }
+
+    public class SortedList_Generic_Tests_EquatableBackwardsOrder_int : SortedList_Generic_Tests<EquatableBackwardsOrder, int>
+    {
+        protected override KeyValuePair<EquatableBackwardsOrder, int> TFactory(int seed)
+        {
+            Random rand = new Random(seed);
+            return new KeyValuePair<EquatableBackwardsOrder, int>(new EquatableBackwardsOrder(rand.Next()), rand.Next());
+        }
+
+        protected override EquatableBackwardsOrder TKeyFactory(int seed)
+        {
+            Random rand = new Random(seed);
+            return new EquatableBackwardsOrder(rand.Next());
+        }
+
+        protected override int TValueFactory(int seed)
+        {
+            Random rand = new Random(seed);
+            return rand.Next();
+        }
+
+        protected override IDictionary<EquatableBackwardsOrder, int> GenericIDictionaryFactory()
+        {
+            return new SortedList<EquatableBackwardsOrder, int>();
+        }
+    }
+}
