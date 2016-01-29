c7f630678f752e96a95f8fc53c208306981eb7ea
stephentoub <stoub@microsoft.com>
2016-01-28 18:27:29 -0800

    Reenable cert validation test on Linux

--- src/System.Net.Security/tests/FunctionalTests/CertificateValidationRemoteServer.cs
+++ src/System.Net.Security/tests/FunctionalTests/CertificateValidationRemoteServer.cs
@@ -13,7 +13,7 @@ namespace System.Net.Security.Tests
 {
     public class CertificateValidationRemoteServer
     {
-        [ActiveIssue(5555, PlatformID.AnyUnix)]
+        [ActiveIssue(5555, PlatformID.OSX)]
         [Fact]
         public async Task CertificateValidationRemoteServer_EndToEnd_Ok()
         {
