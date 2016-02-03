b90b33d2efa47f885b3466a17b24982b4bf07f24
Jon Hanna <jon@hackcraft.net>
2016-02-03 13:21:33 +0000

    Add finalizers to ‎DbDataReader and DbTransaction.
    Also suppress them in derived classes that don't need them.
    
    Fixes #5865

--- src/System.Data.Common/src/System/Data/Common/DbTransaction.cs
+++ src/System.Data.Common/src/System/Data/Common/DbTransaction.cs
@@ -18,6 +18,11 @@ namespace System.Data.Common
         {
         }
 
+        ~DbTransaction()
+        {
+            Dispose(disposing: false);
+        }
+
         public DbConnection Connection
         {
             get
---
+++ src/System.Data.Common/tests/DbTransactionTests.cs
@@ -0,0 +1,63 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace System.Data.Common.Tests
+{
+    class DbTransactionTests
+    {
+        private static volatile bool _wasFinalized;
+
+        private class FinalizingTransaction : DbTransaction
+        {
+            public static void CreateAndRelease()
+            {
+                new FinalizingTransaction();
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                if (!disposing)
+                    _wasFinalized = true;
+                base.Dispose(disposing);
+            }
+
+            public override IsolationLevel IsolationLevel
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            protected override DbConnection DbConnection
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public override void Commit()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void Rollback()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [Fact]
+        public void CanBeFinalized()
+        {
+            FinalizingTransaction.CreateAndRelease();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            Assert.True(_wasFinalized);
+        }
+    }
+}
