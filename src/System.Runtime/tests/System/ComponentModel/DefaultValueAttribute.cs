089bdfc4f020dfbc240db9bdeb14f37ad1b06f5d
Hugh Bellamy <hughbellars@gmail.com>
2016-02-08 23:08:34 +0000

    Cleanup collections and component model tests

--- src/System.Runtime/tests/System/ComponentModel/DefaultValueAttribute.cs
+++ src/System.Runtime/tests/System/ComponentModel/DefaultValueAttribute.cs
@@ -9,36 +9,87 @@ namespace System.ComponentModel.Tests
     public static class DefaultValueAttributeTests
     {
         [Fact]
-        public static void TestCtor()
+        public static void TestCtor_Bool()
         {
             Assert.Equal(true, new DefaultValueAttribute(true).Value);
             Assert.Equal(false, new DefaultValueAttribute(false).Value);
+        }
+
+        [Fact]
+        public static void TestCtor_Byte()
+        {
             Assert.Equal((byte)1, new DefaultValueAttribute((byte)1).Value);
+        }
+
+        [Fact]
+        public static void TestCtor_Char()
+        {
             Assert.Equal('c', new DefaultValueAttribute('c').Value);
+        }
+
+        [Fact]
+        public static void TestCtor_Double()
+        {
             Assert.Equal(3.14, new DefaultValueAttribute(3.14).Value);
+        }
+
+        [Fact]
+        public static void TestCtor_Float()
+        {
             Assert.Equal(3.14f, new DefaultValueAttribute(3.14f).Value);
+        }
+
+        [Fact]
+        public static void TestCtor_Int()
+        {
             Assert.Equal(42, new DefaultValueAttribute(42).Value);
+        }
+
+        [Fact]
+        public static void TestCtor_Long()
+        {
             Assert.Equal(42L, new DefaultValueAttribute(42L).Value);
+        }
+
+        [Fact]
+        public static void TestCtor_Object()
+        {
             Assert.Equal("test", new DefaultValueAttribute((object)"test").Value);
+        }
+
+        [Fact]
+        public static void TestCtor_Short()
+        {
             Assert.Equal((short)42, new DefaultValueAttribute((short)42).Value);
+        }
+
+        [Fact]
+        public static void TestCtor_String()
+        {
             Assert.Equal("test", new DefaultValueAttribute("test").Value);
+        }
+
+        [Fact]
+        public static void TestCtor_Type_String()
+        {
             Assert.Equal(DayOfWeek.Monday, new DefaultValueAttribute(typeof(DayOfWeek), "Monday").Value);
             Assert.Equal(TimeSpan.FromHours(1), new DefaultValueAttribute(typeof(TimeSpan), "1:00:00").Value);
+
             Assert.Equal(42, new DefaultValueAttribute(typeof(int), "42").Value);
-            Assert.Equal(null, new DefaultValueAttribute(typeof(int), "caughtException").Value);
+            Assert.Null(new DefaultValueAttribute(typeof(int), "caughtException").Value);
         }
 
         [Fact]
         public static void TestEqual()
         {
-            DefaultValueAttribute attr;
-
-            attr = new DefaultValueAttribute(42);
+            var attr = new DefaultValueAttribute(42);
             Assert.Equal(attr, attr);
             Assert.True(attr.Equals(attr));
             Assert.Equal(attr.GetHashCode(), attr.GetHashCode());
+
             Assert.Equal(attr, new DefaultValueAttribute(42));
             Assert.Equal(attr.GetHashCode(), new DefaultValueAttribute(42).GetHashCode());
+
             Assert.NotEqual(new DefaultValueAttribute(43), attr);
             Assert.NotEqual(new DefaultValueAttribute(null), attr);
             Assert.False(attr.Equals(null));
