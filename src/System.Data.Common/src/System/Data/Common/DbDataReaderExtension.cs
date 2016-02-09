75f2d8813bda09cf3c3873dde291bbc20e06aab0
Saurabh Singh <saurabh500@gmail.com>
2016-02-08 21:08:01 -0800

    Adding the GetSchemaTable() replacement APIs as proposed in https://github.com/dotnet/corefx/issues/5915

---
+++ src/System.Data.Common/src/System/Data/Common/DbDataReaderExtension.cs
@@ -0,0 +1,18 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Data.Common
+{
+    public static class DbDataReaderExtension
+    {
+        public static System.Collections.ObjectModel.ReadOnlyCollection<DbColumn> GetColumnSchema(this DbDataReader reader)
+        {
+            if (reader is IDbColumnSchemaGenerator)
+            {
+                return ((IDbColumnSchemaGenerator)reader).GetColumnSchema();
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
