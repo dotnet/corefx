3e17bf8d67b6606a332d3a05a0faaaa832b20556
SunnyWar <itsallthesametome@hotmail.com>
2016-02-18 06:16:52 -0800

    #2727 Removed unnecessary IsPrime function after expanding table.

--- src/System.Collections/src/System/Collections/Generic/HashSet.cs
+++ src/System.Collections/src/System/Collections/Generic/HashSet.cs
@@ -1013,8 +1013,6 @@ namespace System.Collections.Generic
         /// </summary>
         private void SetCapacity(int newSize, bool forceNewHashCodes)
         {
-            Debug.Assert(HashHelpers.IsPrime(newSize), "New size is not prime!");
-
             Debug.Assert(_buckets != null, "SetCapacity called on a set with no elements");
 
             Slot[] newSlots = new Slot[newSize];
