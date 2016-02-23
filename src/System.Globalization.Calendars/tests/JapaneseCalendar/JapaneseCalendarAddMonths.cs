f731c8348243cb7d30394597affeb1f203813f0b
Hugh Bellamy <hughbellars@gmail.com>
2016-02-23 18:27:27 +0000

    Cleanup JapeneseCalendar tests
    Pretty small cleanup stuff here, but still worth doing.
    
    Contributes to #2336

--- src/System.Globalization.Calendars/tests/JapaneseCalendar/JapaneseCalendarAddMonths.cs
+++ src/System.Globalization.Calendars/tests/JapaneseCalendar/JapaneseCalendarAddMonths.cs
@@ -2,70 +2,41 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Globalization;
+using System.Collections.Generic;
 using Xunit;
 
 namespace System.Globalization.CalendarsTests
 {
-    // AddMonths(System.DateTime,System.Int32)
     public class JapaneseCalendarAddMonths
     {
-        #region Positive Test Cases
-        // PosTest1: Call AddMonths to add valid value
-        [Fact]
-        public void PosTest1()
+        public static IEnumerable<object[]> AddMonths_TestData()
         {
-            System.Globalization.Calendar calendar = new JapaneseCalendar();
+            yield return new object[] { new DateTime(2006, 11, 28), 1, new DateTime(2006, 12, 28) };
+            yield return new object[] { new DateTime(2006, 11, 28), -1, new DateTime(2006, 10, 28) };
+            yield return new object[] { new DateTime(2006, 11, 28), 0, new DateTime(2006, 11, 28) };
+            yield return new object[] { new DateTime(2006, 11, 28), 1000, new DateTime(2090, 3, 28) };
 
-            VerificationHelper(calendar, new DateTime(2006, 11, 28), 1, new DateTime(2006, 12, 28));
-            VerificationHelper(calendar, new DateTime(2006, 11, 28), -1, new DateTime(2006, 10, 28));
-            VerificationHelper(calendar, new DateTime(2006, 11, 28), 0, new DateTime(2006, 11, 28));
-            VerificationHelper(calendar, new DateTime(2006, 11, 28), 1000, new DateTime(2090, 3, 28));
+            yield return new object[] { new DateTime(2006, 12, 1), 1, new DateTime(2007, 1, 1) };
+            yield return new object[] { new DateTime(2007, 1, 1), -1, new DateTime(2006, 12, 1) };
 
-            VerificationHelper(calendar, new DateTime(2006, 12, 1), 1, new DateTime(2007, 1, 1));
-            VerificationHelper(calendar, new DateTime(2007, 1, 1), -1, new DateTime(2006, 12, 1));
+            // Boundary values
+            yield return new object[] { new DateTime(1868, 9, 8), 1, new DateTime(1868, 10, 8) };
+            yield return new object[] { new DateTime(1868, 10, 8), -1, new DateTime(1868, 9, 8) };
+            yield return new object[] { new DateTime(9999, 11, 30), 1, new DateTime(9999, 12, 30) };
+            yield return new object[] { new DateTime(9999, 12, 30), -1, new DateTime(9999, 11, 30) };
+            yield return new object[] { DateTime.MaxValue, 0, DateTime.MaxValue };
+            yield return new object[] { new DateTime(1868, 9, 8), 0, new DateTime(1868, 9, 8) };
+
+            // Day is not in the month
+            yield return new object[] { new DateTime(2006, 10, 31), 1, new DateTime(2006, 11, 30) };
+            yield return new object[] { new DateTime(2006, 12, 31), -1, new DateTime(2006, 11, 30) };
         }
 
-        // PosTest2: Call AddMonths to add boundary value
-        [Fact]
-        public void PosTest2()
+        [Theory]
+        [MemberData(nameof(AddMonths_TestData))]
+        public void AddMonths(DateTime time, int months, DateTime expected)
         {
-            System.Globalization.Calendar calendar = new JapaneseCalendar();
-
-            VerificationHelper(calendar, new DateTime(1868, 9, 8), 1, new DateTime(1868, 10, 8));
-            VerificationHelper(calendar, new DateTime(1868, 10, 8), -1, new DateTime(1868, 9, 8));
-            VerificationHelper(calendar, new DateTime(9999, 11, 30), 1, new DateTime(9999, 12, 30));
-            VerificationHelper(calendar, new DateTime(9999, 12, 30), -1, new DateTime(9999, 11, 30));
-            VerificationHelper(calendar, DateTime.MaxValue, 0, DateTime.MaxValue);
-            VerificationHelper(calendar, new DateTime(1868, 9, 8), 0, new DateTime(1868, 9, 8));
+            Assert.Equal(expected, new JapaneseCalendar().AddMonths(time, months));
         }
-
-        // PosTest3: Call AddMonths to add month when the day is not in the month
-        [Fact]
-        public void PosTest3()
-        {
-            System.Globalization.Calendar calendar = new JapaneseCalendar();
-
-            VerificationHelper(calendar, new DateTime(2006, 10, 31), 1, new DateTime(2006, 11, 30));
-            VerificationHelper(calendar, new DateTime(2006, 12, 31), -1, new DateTime(2006, 11, 30));
-        }
-        #endregion
-
-        #region Private Methods
-        private void VerificationHelper(Calendar calendar, DateTime time, int months, DateTime expected)
-        {
-            DateTime actual = calendar.AddMonths(time, months);
-            Assert.Equal(expected, actual);
-        }
-
-        private void VerificationHelper<T>(Calendar calendar, DateTime time, int months) where T : Exception
-        {
-            Assert.Throws<T>(() =>
-            {
-                DateTime actual = calendar.AddMonths(time, months);
-            });
-        }
-        #endregion
     }
 }
--- src/System.Globalization.Calendars/tests/JapaneseCalendar/JapaneseCalendarEras.cs
+++ src/System.Globalization.Calendars/tests/JapaneseCalendar/JapaneseCalendarEras.cs
@@ -2,27 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Globalization;
 using Xunit;
 
 namespace System.Globalization.CalendarsTests
 {
     public class JapaneseCalendarEras
     {
-        #region Public Fields
-        public static readonly int[] c_EXPECTED_ERAS = new int[] { 4, 3, 2, 1 };
-        #endregion
-
-        #region Positive Test Cases
         [Fact]
-        public void PosTest1()
+        public void Eras()
         {
-            int[] actual = new JapaneseCalendar().Eras;
-            Assert.NotNull(actual);
-            Assert.Equal(c_EXPECTED_ERAS.Length, actual.Length);
-            Assert.Equal(c_EXPECTED_ERAS, actual);
+            Assert.Equal(new int[] { 4, 3, 2, 1 }, new JapaneseCalendar().Eras);
         }
-        #endregion
     }
 }
