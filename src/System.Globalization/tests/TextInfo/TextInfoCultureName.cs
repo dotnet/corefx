30093f3bb497a9dbddcc842c69d71c7ba9a4584a
Hugh Bellamy <hughbellars@gmail.com>
2016-02-29 00:04:17 +0000

    Respond to PR feedback

--- src/System.Globalization/tests/TextInfo/TextInfoCultureName.cs
+++ src/System.Globalization/tests/TextInfo/TextInfoCultureName.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
 using Xunit;
 
 namespace System.Globalization.Tests
@@ -9,15 +10,21 @@ namespace System.Globalization.Tests
     public class TextInfoCultureName
     {
         [Theory]
-        [InlineData("")]
-        [InlineData("en-US")]
-        [InlineData("fr-FR")]
-        [InlineData("EN-us")]
-        [InlineData("FR-fr")]
-        public void CultureName(string name)
+        public static IEnumerable<object[]> CultureName_TestData()
         {
-            CultureInfo culture = new CultureInfo(name);
-            Assert.Equal(culture.Name, culture.TextInfo.CultureName);
+            yield return new object[] { CultureInfo.InvariantCulture.TextInfo, "" };
+            yield return new object[] { new CultureInfo("").TextInfo, "" };
+            yield return new object[] { new CultureInfo("en-US").TextInfo, "en-US" };
+            yield return new object[] { new CultureInfo("fr-FR").TextInfo, "fr-FR" };
+            yield return new object[] { new CultureInfo("EN-us").TextInfo, "en-US" };
+            yield return new object[] { new CultureInfo("FR-fr").TextInfo, "fr-FR" };
+        }
+
+        [Theory]
+        [MemberData(nameof(CultureName_TestData))]
+        public void CultureName(TextInfo textInfo, string expected)
+        {
+            Assert.Equal(expected, textInfo.CultureName);
         }
     }
 }
