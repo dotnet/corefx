f297eece5e0f2b09612ae743c9c18b9adbb27c23
Cory Rivera <corivera@microsoft.com>
2016-03-08 17:23:11 -0800

    Update SqlClient RunTests script with EnableManualTests flag

--- src/System.Data.SqlClient/tests/ManualTests/RunTests.cmd
+++ src/System.Data.SqlClient/tests/ManualTests/RunTests.cmd
@@ -3,8 +3,8 @@ IF "%1"=="/?" (
 ECHO Usage: "%0" [TestClass]
 ) ELSE IF "%1"=="" (
 echo Running all tests.
-msbuild /t:BuildAndTest
+msbuild /t:BuildAndTest /p:EnableManualTests=true
 ) ELSE (
 echo Running SqlClient test suite "%1".
-msbuild /t:BuildAndTest "/p:XunitOptions=-class System.Data.SqlClient.ManualTesting.Tests.%1"
+msbuild /t:BuildAndTest /p:EnableManualTests=true "/p:XunitOptions=-class System.Data.SqlClient.ManualTesting.Tests.%1"
 )
