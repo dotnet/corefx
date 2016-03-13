b0c61aebb93c5557ff87306c42f2cc73f4c2fd3a
James Ko <jamesqko@gmail.com>
2016-03-13 18:44:29 -0400

    Add tests for MetadataBuilder and MetadataSerializer

---
+++ src/System.Reflection.Metadata/tests/Metadata/Ecma335/MetadataSerializerTests.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace System.Reflection.Metadata.Ecma335.Tests
+{
+    public class MetadataSerializerTests
+    {
+        [Fact]
+        public void TypeSystemSerializer_EmptyRowCounts()
+        {
+            var serializer = CreateTSSerializer();
+            var externalRowCounts = serializer.MetadataSizes.ExternalRowCounts;
+            Assert.Equal(MetadataTokens.TableCount, externalRowCounts.Length);
+            Assert.DoesNotContain(externalRowCounts, count => count != 0); // array should be zeroed out
+        }
+
+        private TypeSystemMetadataSerializer CreateTSSerializer() =>
+            new TypeSystemMetadataSerializer(new MetadataBuilder(), "PDB v1.0", false);
+    }
+}
