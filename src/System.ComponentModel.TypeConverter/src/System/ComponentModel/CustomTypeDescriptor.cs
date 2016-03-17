270c03f374c6235011bb7d93de1c5c1ea655ea05
Charles Lowell <chlowe@microsoft.com>
2016-03-17 11:08:40 -0700

    Add public types used by TypeDescriptor
    CustomTypeDescriptor, ITypeDescriptorFilterService, RefreshEventArgs, RefreshEventHandler, TypeDescriptionProvider

--- src/System.ComponentModel.TypeConverter/src/System/ComponentModel/CustomTypeDescriptor.cs
+++ src/System.ComponentModel.TypeConverter/src/System/ComponentModel/CustomTypeDescriptor.cs
@@ -2,14 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Security.Permissions;
-
 namespace System.ComponentModel
 {
     /// <devdoc>
     /// </devdoc>
-    [HostProtection(SharedState = true)]
     public abstract class CustomTypeDescriptor : ICustomTypeDescriptor
     {
         private ICustomTypeDescriptor _parent;
