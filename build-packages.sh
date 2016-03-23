c8510f51d93fe441462eda29490493125be4510e
maririos <mariari@microsoft.com>
2016-03-23 13:57:06 -0700

    Build-packages.sh

---
+++ build-packages.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+usage()
+{
+    echo "Builds the NuGet packages from the binaries that were built in the Build product binaries step."
+    echo "No option parameters."
+    exit 1
+}
+
+working_tree_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+build_packages_log=$working_tree_root/build-packages.log
+arguments="$@"
+
+echo -e "Running build-packages.sh $arguments" > $build_packages_log
+
+# Parse arguments
+if [ $arguments == "-h" ] || [ $arguments == "--help" ]; then
+    usage
+fi
+
+echo -e "\n$working_tree_root/Tools/corerun $working_tree_root/Tools/MSBuild.exe $working_tree_root/src/packages.builds $arguments /nologo /v:minimal /flp:v=detailed;Append;LogFile=$build_packages_log" >> $build_packages_log
+$working_tree_root/Tools/corerun $working_tree_root/Tools/MSBuild.exe $working_tree_root/src/packages.builds $arguments /nologo /v:minimal "/flp:v=detailed;Append;LogFile=$build_packages_log"
+
+
+if [ $? -ne 0 ]; then
+    echo -e "\nAn error occurred. Aborting build-packages.sh ." >> $build_packages_log
+    echo "ERROR: An error occurred while building packages, see $build_packages_log for more details."
+    exit 1
+fi
+
+echo "Done building packages."
+echo -e "\nDone building packages." >> $build_packages_log
+exit 0
