371d98cc3644d859f027c69a74d49bc9737292e9
bartonjs <jbarton@microsoft.com>
2016-03-28 08:24:34 -0700

    Revert "Baseline Win7 CNG symmetric encryption tests"
    This reverts commit 0b3a591fb104009ea2e7a896263bf79245439e44.

--- src/System.Security.Cryptography.Cng/tests/AesProvider.cs
+++ src/System.Security.Cryptography.Cng/tests/AesProvider.cs
@@ -2,24 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Runtime.InteropServices;
-
 namespace System.Security.Cryptography.Encryption.Aes.Tests
 {
     using Aes = System.Security.Cryptography.Aes;
 
     public class AesProvider : IAesProvider
     {
-        // Issue 7201: Windows 7 (Microsoft Windows 6.1) KSP does not support
-        // AES, so temporarily recycle the BCrypt.dll-based implementation
-        // from the Algorithms library, pending a change to make ephemeral
-        // keys always use BCrypt.
-        private static readonly Func<Aes> s_creator =
-            RuntimeInformation.OSDescription.Contains("Windows 6.1") ? Aes.Create : (Func<Aes>)(() => new AesCng());
-
         public Aes Create()
         {
-            return s_creator();
+            return new AesCng();
         }
     }
 
--- src/System.Security.Cryptography.Cng/tests/TripleDESCngProvider.cs
+++ src/System.Security.Cryptography.Cng/tests/TripleDESCngProvider.cs
@@ -2,22 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Runtime.InteropServices;
-
 namespace System.Security.Cryptography.Encryption.TripleDes.Tests
 {
     public class TripleDESCngProvider : ITripleDESProvider
     {
-        // Issue 7201: Windows 7 (Microsoft Windows 6.1) KSP does not support
-        // 3DES, so temporarily recycle the BCrypt.dll-based implementation
-        // from the Algorithms library, pending a change to make ephemeral
-        // keys always use BCrypt.
-        private static readonly Func<TripleDES> s_creator =
-            RuntimeInformation.OSDescription.Contains("Windows 6.1") ? TripleDES.Create : (Func<TripleDES>)(() => new TripleDESCng());
-
         public TripleDES Create()
         {
-            return s_creator();
+            return new TripleDESCng();
         }
     }
 
