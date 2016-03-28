933a0cd2faaa26b479b75ae5e54a5b57ed9c15f6
Eric Mellino <erme@microsoft.com>
2016-03-28 13:20:08 -0700

    Revert PR 7188.
    The newest version of MSBuild has fixed the embedded resource culture
    issue that was preventing us from using periods in embedded resource
    names. This reverts the changes to the System.Reflection.Metadata tests
    which removed the periods from some embedded resource file names in the
    test assembly.

--- src/System.Reflection.Metadata/tests/Resources/TestResources.cs
+++ src/System.Reflection.Metadata/tests/Resources/TestResources.cs
@@ -6,14 +6,11 @@ using System.IO;
 
 namespace System.Reflection.Metadata.Tests
 {
-    // NOTE: We cannot currently use periods in embedded resource file names due to an issue with x-plat MSBuild.
-    // See https://github.com/Microsoft/msbuild/issues/541 for more information.
-
     internal static class Interop
     {
         public static readonly byte[] IndexerWithByRefParam = ResourceHelper.GetResource("Interop.IndexerWithByRefParam.dll");
-        public static readonly byte[] Interop_Mock01 = ResourceHelper.GetResource("Interop.InteropMock01.dll");
-        public static readonly byte[] Interop_Mock01_Impl = ResourceHelper.GetResource("Interop.InteropMock01Impl.dll");
+        public static readonly byte[] Interop_Mock01 = ResourceHelper.GetResource("Interop.Interop.Mock01.dll");
+        public static readonly byte[] Interop_Mock01_Impl = ResourceHelper.GetResource("Interop.Interop.Mock01.Impl.dll");
     }
 
     internal static class Misc
