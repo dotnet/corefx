5a163059ade525224cf61924c10a1d7dca8bfe21
James Ko <jamesqko@gmail.com>
2016-03-29 19:06:04 -0400

    Avoid string.Split allocations in FrameworkName

--- src/System.Runtime.Extensions/tests/System/Runtime/Versioning/FrameworkName.cs
+++ src/System.Runtime.Extensions/tests/System/Runtime/Versioning/FrameworkName.cs
@@ -12,8 +12,8 @@ namespace System.Runtime.Versioning.Tests
         private const string TestProfile = "TestProfile";
 
         private static readonly Version s_testVersion = new Version(1, 2, 3, 4);
-        private static readonly string s_testNameString = string.Format("{0},Version=v{1},Profile={2}", TestIdentifier, s_testVersion, TestProfile);
-        private static readonly string s_testNameNoProfileString = string.Format("{0},Version=v{1}", TestIdentifier, s_testVersion);
+        private static readonly string s_testNameString = $"{TestIdentifier},Version=v{s_testVersion},Profile={TestProfile}";
+        private static readonly string s_testNameNoProfileString = $"{TestIdentifier},Version=v{s_testVersion}";
         private static readonly FrameworkName s_testName = new FrameworkName(s_testNameString);
         private static readonly FrameworkName s_testNameNoProfile = new FrameworkName(s_testNameNoProfileString);
 
@@ -50,6 +50,9 @@ namespace System.Runtime.Versioning.Tests
             Assert.Throws<ArgumentException>(() => new FrameworkName("A,Version=1.z.0.0,Profile=C"));
             Assert.Throws<ArgumentException>(() => new FrameworkName("A,Something=1.z.0.0,Profile=C"));
             Assert.Throws<ArgumentException>(() => new FrameworkName("A,Profile=C"));
+            Assert.Throws<ArgumentException>(() => new FrameworkName("A,======"));
+            Assert.Throws<ArgumentException>(() => new FrameworkName("A,    =B="));
+            Assert.Throws<ArgumentException>(() => new FrameworkName("A,1  =2=3"));
         }
 
         [Fact]
