37ea15044131f34d77327e4f4d780315ac216a2d
Stephen A. Imhoff <clockwork-muse@outlook.com>
2016-04-01 11:34:26 +0900

    Add default unordered source.

--- src/System.Linq.Parallel/tests/Helpers/UnorderedSources.cs
+++ src/System.Linq.Parallel/tests/Helpers/UnorderedSources.cs
@@ -10,6 +10,26 @@ namespace System.Linq.Parallel.Tests
 {
     public static class UnorderedSources
     {
+        /// <summary>
+        /// Returns a default ParallelQuery source.
+        /// </summary>
+        /// For most instances when dealing with unordered input, the individual source does not matter.
+        ///
+        /// Instead, that is reserved for ordered, where partitioning and dealing with indices has important
+        /// secondary effects.  The goal of unordered input, then, is mostly to make sure the query works.
+        /// <param name="count">The count of elements.</param>
+        /// <returns>A ParallelQuery with elements running from 0 to count - 1</returns>
+        public static ParallelQuery<int> Default(int count)
+        {
+            // Of the underlying types used elsewhere, some have "problems",
+            // in the sense that they may be too-easily indexible.
+            // For instance, Array and List are both trivially range partitionable and indexible.
+            // A parallelized Enumerable.Range is being used (not easily partitionable or indexible),
+            // but at the moment ParallelEnumerable.Range is being used for speed and ease of use.
+            // ParallelEnumerable.Range is not trivially indexible, but is easily range partitioned.
+            return ParallelEnumerable.Range(0, count);
+        }
+
         public const int AdditionalTypeLimit = 1024;
 
         // Get a set of ranges, of each count in `counts`.
