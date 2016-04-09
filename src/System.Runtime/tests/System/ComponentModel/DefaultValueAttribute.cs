854031c19683e76d23a75812517c1f8926342018
Hugh Bellamy <hughbellars@gmail.com>
2016-04-09 21:21:34 +0100

    Cleanup misc runtime tests
    * Cleanup misc runtime test
    
    Contributes to #6286
    Cherry picked from #5490

--- src/System.Runtime/tests/System/ComponentModel/DefaultValueAttribute.cs
+++ src/System.Runtime/tests/System/ComponentModel/DefaultValueAttribute.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
 using Xunit;
 
 namespace System.ComponentModel.Tests
@@ -13,39 +14,49 @@ namespace System.ComponentModel.Tests
         {
             Assert.Equal(true, new DefaultValueAttribute(true).Value);
             Assert.Equal(false, new DefaultValueAttribute(false).Value);
-            Assert.Equal((byte)1, new DefaultValueAttribute((byte)1).Value);
-            Assert.Equal('c', new DefaultValueAttribute('c').Value);
+
             Assert.Equal(3.14, new DefaultValueAttribute(3.14).Value);
             Assert.Equal(3.14f, new DefaultValueAttribute(3.14f).Value);
+
+            Assert.Equal((byte)1, new DefaultValueAttribute((byte)1).Value);
             Assert.Equal(42, new DefaultValueAttribute(42).Value);
             Assert.Equal(42L, new DefaultValueAttribute(42L).Value);
-            Assert.Equal("test", new DefaultValueAttribute((object)"test").Value);
             Assert.Equal((short)42, new DefaultValueAttribute((short)42).Value);
+
+            Assert.Equal('c', new DefaultValueAttribute('c').Value);
             Assert.Equal("test", new DefaultValueAttribute("test").Value);
+
+            Assert.Equal("test", new DefaultValueAttribute((object)"test").Value);
+
             Assert.Equal(DayOfWeek.Monday, new DefaultValueAttribute(typeof(DayOfWeek), "Monday").Value);
             Assert.Equal(TimeSpan.FromHours(1), new DefaultValueAttribute(typeof(TimeSpan), "1:00:00").Value);
+
             Assert.Equal(42, new DefaultValueAttribute(typeof(int), "42").Value);
-            Assert.Equal(null, new DefaultValueAttribute(typeof(int), "caughtException").Value);
+            Assert.Null(new DefaultValueAttribute(typeof(int), "caughtException").Value);
         }
 
-        [Fact]
-        public static void TestEqual()
+        public static IEnumerable<object[]> Equals_TestData()
         {
-            DefaultValueAttribute attr;
+            var attr = new DefaultValueAttribute(42);
+            yield return new object[] { attr, attr, true };
+            yield return new object[] { attr, new DefaultValueAttribute(42), true };
+            yield return new object[] { attr, new DefaultValueAttribute(43), false };
+            yield return new object[] { attr, new DefaultValueAttribute(null), false };
+            yield return new object[] { attr, null, false };
+            yield return new object[] { new DefaultValueAttribute(null), new DefaultValueAttribute(null), true };
+        }
 
-            attr = new DefaultValueAttribute(42);
-            Assert.Equal(attr, attr);
-            Assert.True(attr.Equals(attr));
-            Assert.Equal(attr.GetHashCode(), attr.GetHashCode());
-            Assert.Equal(attr, new DefaultValueAttribute(42));
-            Assert.Equal(attr.GetHashCode(), new DefaultValueAttribute(42).GetHashCode());
-            Assert.NotEqual(new DefaultValueAttribute(43), attr);
-            Assert.NotEqual(new DefaultValueAttribute(null), attr);
-            Assert.False(attr.Equals(null));
+        [Theory]
+        [MemberData(nameof(Equals_TestData))]
+        public static void TestEquals(DefaultValueAttribute attr1, object obj, bool expected)
+        {
+            Assert.Equal(expected, attr1.Equals(obj));
 
-            attr = new DefaultValueAttribute(null);
-            Assert.Equal(new DefaultValueAttribute(null), attr);
-            Assert.Equal(attr.GetHashCode(), new DefaultValueAttribute(null).GetHashCode());
+            DefaultValueAttribute attr2 = obj as DefaultValueAttribute;
+            if (attr2 != null)
+            {
+                Assert.Equal(expected, attr1.GetHashCode() == attr2.GetHashCode());
+            }
         }
     }
 }
--- src/System.Runtime/tests/System/Type.cs
+++ src/System.Runtime/tests/System/Type.cs
@@ -4,7 +4,6 @@
 
 using System;
 using System.Collections.Generic;
-
 using Xunit;
 
 internal class Outside
