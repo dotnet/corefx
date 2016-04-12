c44a6f314fe155895d283f8ce3730db7661aa6e3
Hugh Bellamy <hughbellars@gmail.com>
2016-04-12 13:18:51 +0100

    Add parameter names to System.Collection.Immutable tests

--- src/System.Collections.Immutable/tests/ImmutableArrayBuilderTest.cs
+++ src/System.Collections.Immutable/tests/ImmutableArrayBuilderTest.cs
@@ -22,7 +22,7 @@ namespace System.Collections.Immutable.Tests
         [Fact]
         public void CreateBuilderInvalidCapacity()
         {
-            Assert.Throws<ArgumentOutOfRangeException>(() => ImmutableArray.CreateBuilder<int>(-1));
+            Assert.Throws<ArgumentOutOfRangeException>("capacity", () => ImmutableArray.CreateBuilder<int>(-1));
         }
 
         [Fact]
@@ -131,20 +131,20 @@ namespace System.Collections.Immutable.Tests
             builder1.AddRange(array);
             Assert.Equal(new[] { 1, 2, 3 }, builder1);
 
-            Assert.Throws<ArgumentNullException>(() => builder1.AddRange((int[])null));
-            Assert.Throws<ArgumentNullException>(() => builder1.AddRange(null, 42));
-            Assert.Throws<ArgumentOutOfRangeException>(() => builder1.AddRange(new int[0], -1));
-            Assert.Throws<ArgumentOutOfRangeException>(() => builder1.AddRange(new int[0], 42));
+            Assert.Throws<ArgumentNullException>("items", () => builder1.AddRange((int[])null));
+            Assert.Throws<ArgumentNullException>("items", () => builder1.AddRange(null, 42));
+            Assert.Throws<ArgumentOutOfRangeException>("length", () => builder1.AddRange(new int[0], -1));
+            Assert.Throws<ArgumentOutOfRangeException>("length", () => builder1.AddRange(new int[0], 42));
 
-            Assert.Throws<ArgumentNullException>(() => builder1.AddRange((ImmutableArray<int>.Builder)null));
-            Assert.Throws<ArgumentNullException>(() => builder1.AddRange((IEnumerable<int>)null));
+            Assert.Throws<ArgumentNullException>("items", () => builder1.AddRange((ImmutableArray<int>.Builder)null));
+            Assert.Throws<ArgumentNullException>("items", () => builder1.AddRange((IEnumerable<int>)null));
 
             Assert.Throws<NullReferenceException>(() => builder1.AddRange(default(ImmutableArray<int>)));
             builder1.AddRange(default(ImmutableArray<int>), 42);
 
             var builder2 = new ImmutableArray<object>.Builder();
             builder2.AddRange(default(ImmutableArray<string>));
-            Assert.Throws<ArgumentNullException>(() => builder2.AddRange((ImmutableArray<string>.Builder)null));
+            Assert.Throws<ArgumentNullException>("items", () => builder2.AddRange((ImmutableArray<string>.Builder)null));
         }
 
         [Fact]
@@ -214,8 +214,8 @@ namespace System.Collections.Immutable.Tests
             builder.Insert(1, 4);
             builder.Insert(4, 5);
             Assert.Equal(new[] { 1, 4, 2, 3, 5 }, builder);
-            Assert.Throws<ArgumentOutOfRangeException>(() => builder.Insert(-1, 0));
-            Assert.Throws<ArgumentOutOfRangeException>(() => builder.Insert(builder.Count + 1, 0));
+            Assert.Throws<ArgumentOutOfRangeException>("index", () => builder.Insert(-1, 0));
+            Assert.Throws<ArgumentOutOfRangeException>("index", () => builder.Insert(builder.Count + 1, 0));
         }
 
         [Fact]
@@ -240,8 +240,8 @@ namespace System.Collections.Immutable.Tests
             var builder = new ImmutableArray<int>.Builder();
             builder.AddRange(1, 2, 3, 4);
             builder.RemoveAt(0);
-            Assert.Throws<ArgumentOutOfRangeException>(() => builder.RemoveAt(-1));
-            Assert.Throws<ArgumentOutOfRangeException>(() => builder.RemoveAt(3));
+            Assert.Throws<ArgumentOutOfRangeException>("index", () => builder.RemoveAt(-1));
+            Assert.Throws<ArgumentOutOfRangeException>("index", () => builder.RemoveAt(3));
             Assert.Equal(new[] { 2, 3, 4 }, builder);
             builder.RemoveAt(1);
             Assert.Equal(new[] { 2, 4 }, builder);
@@ -297,7 +297,7 @@ namespace System.Collections.Immutable.Tests
         [Fact]
         public void Sort_NullComparison_Throws()
         {
-            Assert.Throws<ArgumentNullException>(() => ImmutableArray.CreateBuilder<int>().Sort((Comparison<int>)null));
+            Assert.Throws<ArgumentNullException>("comparison", () => ImmutableArray.CreateBuilder<int>().Sort((Comparison<int>)null));
         }
 
         [Fact]
@@ -340,9 +340,9 @@ namespace System.Collections.Immutable.Tests
         {
             var builder = new ImmutableArray<int>.Builder();
             builder.AddRange(2, 4, 1, 3);
-            Assert.Throws<ArgumentOutOfRangeException>(() => builder.Sort(-1, 2, Comparer<int>.Default));
-            Assert.Throws<ArgumentOutOfRangeException>(() => builder.Sort(1, 4, Comparer<int>.Default));
-            Assert.Throws<ArgumentOutOfRangeException>(() => builder.Sort(0, -1, Comparer<int>.Default));
+            Assert.Throws<ArgumentOutOfRangeException>("index", () => builder.Sort(-1, 2, Comparer<int>.Default));
+            Assert.Throws<ArgumentOutOfRangeException>("count", () => builder.Sort(1, 4, Comparer<int>.Default));
+            Assert.Throws<ArgumentOutOfRangeException>("count", () => builder.Sort(0, -1, Comparer<int>.Default));
 
             builder.Sort(builder.Count, 0, Comparer<int>.Default);
             Assert.Equal(new int[] { 2, 4, 1, 3 }, builder);
@@ -451,9 +451,9 @@ namespace System.Collections.Immutable.Tests
             builder.CopyTo(target, 1);
             Assert.Equal(new[] { 0, 1, 2, 3 }, target);
 
-            Assert.Throws<ArgumentNullException>(() => builder.CopyTo(null, 0));
-            Assert.Throws<ArgumentOutOfRangeException>(() => builder.CopyTo(target, -1));
-            Assert.Throws<ArgumentOutOfRangeException>(() => builder.CopyTo(target, 2));
+            Assert.Throws<ArgumentNullException>("array", () => builder.CopyTo(null, 0));
+            Assert.Throws<ArgumentOutOfRangeException>("index", () => builder.CopyTo(target, -1));
+            Assert.Throws<ArgumentOutOfRangeException>("index", () => builder.CopyTo(target, 2));
         }
 
         [Fact]
@@ -554,7 +554,7 @@ namespace System.Collections.Immutable.Tests
             builder.Add(13);
             Assert.Equal(4, builder.Capacity);
             Assert.Equal(2, builder.Count);
-            Assert.Throws(typeof(InvalidOperationException), () => builder.MoveToImmutable());
+            Assert.Throws<InvalidOperationException>(() => builder.MoveToImmutable());
         }
 
         [Fact]
@@ -590,7 +590,7 @@ namespace System.Collections.Immutable.Tests
             builder[0] = "a";
             builder[1] = "b";
             builder.Clear();
-            Assert.Throws(typeof(InvalidOperationException), () => builder.MoveToImmutable());
+            Assert.Throws<InvalidOperationException>(() => builder.MoveToImmutable());
         }
 
         [Fact]
--- src/System.Collections.Immutable/tests/RequiresTests.cs
+++ src/System.Collections.Immutable/tests/RequiresTests.cs
@@ -13,15 +13,15 @@ namespace System.Collections.Immutable.Tests
         {
             Requires.Argument(true);
             Requires.Argument(true, "parameterName", "message");
-            Assert.Throws<ArgumentException>(() => Requires.Argument(false));
-            Assert.Throws<ArgumentException>(() => Requires.Argument(false, "parameterName", "message"));
+            Assert.Throws<ArgumentException>(null, () => Requires.Argument(false));
+            Assert.Throws<ArgumentException>("parameterName", () => Requires.Argument(false, "parameterName", "message"));
         }
 
         [Fact]
         public void FailRange()
         {
-            Assert.Throws<ArgumentOutOfRangeException>(() => Requires.FailRange("parameterName"));
-            Assert.Throws<ArgumentOutOfRangeException>(() => Requires.FailRange("parameterName", "message"));
+            Assert.Throws<ArgumentOutOfRangeException>("parameterName", () => Requires.FailRange("parameterName"));
+            Assert.Throws<ArgumentOutOfRangeException>("parameterName", () => Requires.FailRange("parameterName", "message"));
         }
 
         [Fact]
@@ -29,15 +29,15 @@ namespace System.Collections.Immutable.Tests
         {
             Requires.Range(true, "parameterName");
             Requires.Range(true, "parameterName", "message");
-            Assert.Throws<ArgumentOutOfRangeException>(() => Requires.Range(false, "parameterName"));
-            Assert.Throws<ArgumentOutOfRangeException>(() => Requires.Range(false, "parameterName", "message"));
+            Assert.Throws<ArgumentOutOfRangeException>("parameterName", () => Requires.Range(false, "parameterName"));
+            Assert.Throws<ArgumentOutOfRangeException>("parameterName", () => Requires.Range(false, "parameterName", "message"));
         }
 
         [Fact]
         public void NotNull()
         {
             Requires.NotNull(new object(), "parameterName");
-            Assert.Throws<ArgumentNullException>(() => Requires.NotNull((object)null, "parameterName"));
+            Assert.Throws<ArgumentNullException>("parameterName", () => Requires.NotNull((object)null, "parameterName"));
         }
 
         [Fact]
@@ -45,7 +45,7 @@ namespace System.Collections.Immutable.Tests
         {
             Requires.NotNullAllowStructs(0, "parameterName");
             Requires.NotNullAllowStructs(new object(), "parameterName");
-            Assert.Throws<ArgumentNullException>(() => Requires.NotNullAllowStructs((object)null, "parameterName"));
+            Assert.Throws<ArgumentNullException>("parameterName", () => Requires.NotNullAllowStructs((object)null, "parameterName"));
         }
     }
 }
