1603216245e9cad8c04356f173e0d4b7df8fc598
Lakshmi Priya Sekar <lasekar@microsoft.com>
2016-04-26 14:49:47 -0700

    Add ubuntu.16.04-x64 pkg support.

--- build.sh
+++ build.sh
@@ -215,13 +215,18 @@ case $OSName in
 
     Linux)
         __HostOS=Linux
+        __OSVersion=$(lsb_release -rs)
         source /etc/os-release
         if [ "$ID" == "centos" ]; then
             __TestNugetRuntimeId=centos.7-x64
         elif [ "$ID" == "rhel" ]; then
             __TestNugetRuntimeId=rhel.7-x64
         elif [ "$ID" == "ubuntu" ]; then
-            __TestNugetRuntimeId=ubuntu.14.04-x64
+            if [ "$__OSVersion" == "16.04" ]; then
+                __TestNugetRuntimeId=ubuntu.16.04-x64
+            else
+                __TestNugetRuntimeId=ubuntu.14.04-x64
+            fi
         elif [ "$ID" == "debian" ]; then
             __TestNugetRuntimeId=debian.8-x64
         else
