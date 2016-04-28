9212edcead1e4e25f82bd844b27d8112d8e0dd08
Mariana Rios Flores <mariari@microsoft.com>
2016-04-28 14:30:06 -0700

    Update BuildTools version and minor fix in publish-packages.sh

--- publish-packages.sh
+++ publish-packages.sh
@@ -30,10 +30,10 @@ $working_tree_root/Tools/corerun $working_tree_root/Tools/MSBuild.exe $working_t
 
 if [ $? -ne 0 ]; then
     echo -e "\nAn error occurred. Aborting publish-packages.sh ." >> $publish_packages_log
-    echo "ERROR: An error occurred while building packages, see $publish_packages_log for more details."
+    echo "ERROR: An error occurred while publishing packages, see $publish_packages_log for more details."
     exit 1
 fi
 
-echo "Done building packages."
-echo -e "\nDone building packages." >> $publish_packages_log
+echo "Done publishing packages."
+echo -e "\nDone publishing packages." >> $publish_packages_log
 exit 0
