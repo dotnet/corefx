2a859b98182019e00ebb83fcaffb981a60210951
Tomas Matousek <tomas.matousek@microsoft.com>
2016-04-28 13:32:56 -0700

    Split MethodBodyEncoder.cs to mutliple files

--- src/System.Reflection.Metadata/src/System/Reflection/Metadata/Ecma335/Blobs/MethodBodyEncoder.cs
+++ src/System.Reflection.Metadata/src/System/Reflection/Metadata/Ecma335/Blobs/MethodBodyEncoder.cs
@@ -7,59 +7,7 @@ using System.Diagnostics;
 
 namespace System.Reflection.Metadata.Ecma335.Blobs
 {
-    [Flags]
-    public enum MethodBodyAttributes
-    {
-        None = 0,
-        InitLocals = 1,
-        LargeExceptionRegions = 2,
-    }
-
-    public struct MethodBodiesEncoder
-    {
-        public BlobBuilder Builder { get; }
-
-        public MethodBodiesEncoder(BlobBuilder builder = null)
-        {
-            if (builder == null)
-            {
-                builder = new BlobBuilder();
-            }
-
-            // Fat methods are 4-byte aligned. We calculate the alignment relative to the start of the ILStream.
-            //
-            // See ECMA-335 paragraph 25.4.5, Method data section:
-            // "At the next 4-byte boundary following the method body can be extra method data sections."
-            if ((builder.Count % 4) != 0)
-            {
-                // TODO: error message
-                throw new ArgumentException("Builder has to be aligned to 4 byte boundary", nameof(builder));
-            }
-
-            Builder = builder;
-        }
-
-        public MethodBodyEncoder AddMethodBody(
-            int maxStack = 8,
-            int exceptionRegionCount = 0,
-            StandaloneSignatureHandle localVariablesSignature = default(StandaloneSignatureHandle),
-            MethodBodyAttributes attributes = MethodBodyAttributes.InitLocals)
-        {
-            if (unchecked((ushort)maxStack) > ushort.MaxValue)
-            {
-                throw new ArgumentOutOfRangeException(nameof(maxStack));
-            }
-
-            if (exceptionRegionCount < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(exceptionRegionCount));
-            }
-
-            return new MethodBodyEncoder(Builder, (ushort)maxStack, exceptionRegionCount, localVariablesSignature, attributes);
-        }
-    }
-
-public struct MethodBodyEncoder
+    public struct MethodBodyEncoder
     {
         public BlobBuilder Builder { get; }
 
