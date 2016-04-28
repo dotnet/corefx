4e0ae0a2660cc1bca5b861681ae6e9f2efe2cb0f
Justin Van Patten <jvp@justinvp.com>
2016-04-28 07:21:24 -0700

    System.Net.Requests: Remove dependency on System.Diagnostics.Contracts
    It's only used in one place and we're not maintaining code contracts
    in .NET Core.

--- src/System.Net.Requests/src/System/Net/WebResponse.cs
+++ src/System.Net.Requests/src/System/Net/WebResponse.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Diagnostics.Contracts;
 using System.IO;
 
 namespace System.Net
@@ -83,7 +82,6 @@ namespace System.Net
             // read-only
             get
             {
-                Contract.Ensures(Contract.Result<WebHeaderCollection>() != null);
                 throw NotImplemented.ByDesignWithMessage(SR.net_PropertyNotImplementedException);
             }
         }
