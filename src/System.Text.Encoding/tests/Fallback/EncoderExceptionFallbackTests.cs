29e32d8b563e7d04bafb30a9e3e76d4c40bb3689
Hugh Bellamy <hughbellars@gmail.com>
2016-05-02 22:57:11 +0100

    Add some more tests for Encoding and Encoder/Decoder Fallbacks
    - Encoding (ctor, GetEncoding)
    - DecoderExceptionFallback
    - EncoderExceptionFallback
    - DecoderReplacementFallback
    - EncoderReplacementFallback

---
+++ src/System.Text.Encoding/tests/Fallback/EncoderExceptionFallbackTests.cs
@@ -0,0 +1,60 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using Xunit;
+
+namespace System.Text.Tests
+{
+    public class EncoderExceptionFallbackTests
+    {
+        [Fact]
+        public void Ctor_Empty()
+        {
+            EncoderExceptionFallback fallback = new EncoderExceptionFallback();
+            Assert.Equal(0, fallback.MaxCharCount);
+            Assert.Equal(654, fallback.GetHashCode());
+        }
+
+        public static IEnumerable<object[]> Equals_TestData()
+        {
+            yield return new object[] { new EncoderExceptionFallback(), new EncoderExceptionFallback(), true };
+            yield return new object[] { new EncoderExceptionFallback(), new object(), false };
+            yield return new object[] { new EncoderExceptionFallback(), null, false };
+        }
+
+        [Theory]
+        [MemberData(nameof(Equals_TestData))]
+        public void Equals(EncoderExceptionFallback fallback, object value, bool expected)
+        {
+            Assert.Equal(expected, fallback.Equals(value));
+        }
+
+        [Fact]
+        public void CreateFallbackBuffer()
+        {
+            EncoderFallbackBuffer buffer = new EncoderExceptionFallback().CreateFallbackBuffer();
+
+            Assert.Equal((char)0, buffer.GetNextChar());
+            Assert.False(buffer.MovePrevious());
+            Assert.Equal(0, buffer.Remaining);
+
+            EncoderFallbackException ex = Assert.Throws<EncoderFallbackException>(() => buffer.Fallback('a', 0));
+            Assert.Equal('a', ex.CharUnknown);
+
+            ex = Assert.Throws<EncoderFallbackException>(() => buffer.Fallback('\uD800', '\uDC00', 0));
+            Assert.Equal('\uD800', ex.CharUnknownHigh);
+            Assert.Equal('\uDC00', ex.CharUnknownLow);
+        }
+
+        [Fact]
+        public void CreateFallbackBuffer_Fallback_InvalidSurrogateChars_ThrowsArgumentOutOfRangeException()
+        {
+            EncoderFallbackBuffer buffer = new EncoderExceptionFallback().CreateFallbackBuffer();
+
+            Assert.Throws<ArgumentOutOfRangeException>("charUnknownHigh", () => buffer.Fallback('a', '\uDC00', 0));
+            Assert.Throws<ArgumentOutOfRangeException>("CharUnknownLow", () => buffer.Fallback('\uD800', 'a', 0));
+        }
+    }
+}
---
+++ src/System.Text.Encoding/tests/Fallback/EncoderReplacementFallbackTests.cs
@@ -0,0 +1,115 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using Xunit;
+
+namespace System.Text.Tests
+{
+    public class EncoderReplacementFallbackTests
+    {
+        [Fact]
+        public void Ctor_Empty()
+        {
+            EncoderReplacementFallback exception = new EncoderReplacementFallback();
+            Assert.Equal(1, exception.MaxCharCount);
+            Assert.Equal("?", exception.DefaultString);
+            Assert.Equal("?".GetHashCode(), exception.GetHashCode());
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("a")]
+        [InlineData("abc")]
+        [InlineData("\uD800\uDC00")]
+        public void Ctor_String(string replacement)
+        {
+            EncoderReplacementFallback fallback = new EncoderReplacementFallback(replacement);
+            Assert.Equal(replacement.Length, fallback.MaxCharCount);
+            Assert.Equal(replacement, fallback.DefaultString);
+            Assert.Equal(replacement.GetHashCode(), fallback.GetHashCode());
+        }
+
+        [Fact]
+        public void Ctor_Invalid()
+        {
+            Assert.Throws<ArgumentNullException>("replacement", () => new EncoderReplacementFallback(null));
+
+            // Invalid surrogate pair
+            Assert.Throws<ArgumentException>(() => new EncoderReplacementFallback("\uD800"));
+            Assert.Throws<ArgumentException>(() => new EncoderReplacementFallback("\uD800a"));
+            Assert.Throws<ArgumentException>(() => new EncoderReplacementFallback("\uDC00"));
+            Assert.Throws<ArgumentException>(() => new EncoderReplacementFallback("a\uDC00"));
+            Assert.Throws<ArgumentException>(() => new EncoderReplacementFallback("\uDC00\uDC00"));
+            Assert.Throws<ArgumentException>(() => new EncoderReplacementFallback("\uD800\uD800"));
+        }
+
+        public static IEnumerable<object[]> Equals_TestData()
+        {
+            yield return new object[] { new EncoderReplacementFallback(), new EncoderReplacementFallback(), true };
+            yield return new object[] { new EncoderReplacementFallback(), new EncoderReplacementFallback("?"), true };
+
+            yield return new object[] { new EncoderReplacementFallback("abc"), new EncoderReplacementFallback("abc"), true };
+            yield return new object[] { new EncoderReplacementFallback("abc"), new EncoderReplacementFallback("def"), false };
+
+            yield return new object[] { new EncoderReplacementFallback(), new object(), false };
+            yield return new object[] { new EncoderReplacementFallback(), null, false };
+        }
+
+        [Theory]
+        [MemberData(nameof(Equals_TestData))]
+        public void Equals(EncoderReplacementFallback fallback, object value, bool expected)
+        {
+            Assert.Equal(expected, fallback.Equals(value));
+        }
+
+        [Fact]
+        public void CreateFallbackBuffer()
+        {
+            EncoderFallbackBuffer buffer = new EncoderReplacementFallback().CreateFallbackBuffer();
+            Assert.Equal((char)0, buffer.GetNextChar());
+            Assert.False(buffer.MovePrevious());
+            Assert.Equal(0, buffer.Remaining);
+        }
+
+        [Theory]
+        [InlineData("", 'a', false)]
+        [InlineData("?", 'a', true)]
+        public void CreateFallbackBuffer_Fallback_Char(string replacement, char charUnknown, bool expected)
+        {
+            EncoderFallbackBuffer buffer = new EncoderReplacementFallback(replacement).CreateFallbackBuffer();
+            Assert.Equal(expected, buffer.Fallback(charUnknown, 0));
+        }
+
+        [Theory]
+        [InlineData("?")]
+        [InlineData("\uD800\uDC00")]
+        public void CreateFallbackBuffer_MultipleFallback_ThrowsArgumentException(string replacement)
+        {
+            EncoderFallbackBuffer buffer = new EncoderReplacementFallback(replacement).CreateFallbackBuffer();
+            buffer.Fallback('a', 0);
+
+            Assert.Throws<ArgumentException>("chars", () => buffer.Fallback('a', 0));
+            Assert.Throws<ArgumentException>("chars", () => buffer.Fallback('\uD800', '\uDC00', 0));
+        }
+
+        [Theory]
+        [InlineData("", false)]
+        [InlineData("a", true)]
+        public void CreateFallbackBuffer_Fallback_Char_Char(string replacement, bool expected)
+        {
+            EncoderFallbackBuffer buffer = new EncoderReplacementFallback(replacement).CreateFallbackBuffer();
+            Assert.Equal(expected, buffer.Fallback('\uD800', '\uDC00', 0));
+        }
+
+        [Fact]
+        public void CreateFallbackBuffer_Fallback_InvalidSurrogateChars_ThrowsArgumentOutOfRangeException()
+        {
+            EncoderFallbackBuffer buffer = new EncoderReplacementFallback().CreateFallbackBuffer();
+
+            Assert.Throws<ArgumentOutOfRangeException>("charUnknownHigh", () => buffer.Fallback('a', '\uDC00', 0));
+            Assert.Throws<ArgumentOutOfRangeException>("CharUnknownLow", () => buffer.Fallback('\uD800', 'a', 0));
+        }
+    }
+}
