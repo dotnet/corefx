d44b53067db6584e07c4e5dade8dad7388111ee4
Cory Rivera <corivera@microsoft.com>
2016-05-05 14:08:48 -0700

    Fix a bug in SqlDataReader.InvokeRetryable where task exceptions aren't being propagated.

--- src/System.Data.SqlClient/src/System/Data/SqlClient/SqlDataReader.cs
+++ src/System.Data.SqlClient/src/System/Data/SqlClient/SqlDataReader.cs
@@ -4391,9 +4391,7 @@ namespace System.Data.SqlClient
 
                 if (task.IsCompleted)
                 {
-                    // If we've completed sync, then don't bother handling the TaskCompletionSource - we'll just return the completed task
                     CompleteRetryable(task, source, objectToDispose);
-                    return task;
                 }
                 else
                 {
