9d534bab27002acab27b87e2f82bfbe2596c6850
Kamil Rytarowski <n54@gmx.com>
2016-05-07 13:58:34 +0200

    Fix build on NetBSD with LLVM-3.9
    /tmp/pkgsrc-tmp/wip/corefx-git/work/corefx/src/Native/System.Native/pal_console.cpp:466:58:
    error: possible misuse of comma operator here [-Werror,-Wcomma]
        newAction.sa_sigaction = &TransferSignalToHandlerLoop,
                                                             ^

--- src/Native/System.Native/pal_console.cpp
+++ src/Native/System.Native/pal_console.cpp
@@ -463,7 +463,7 @@ static bool InitializeSignalHandling()
     int rv;
 
     // Hook up signal handlers for use with ctrl-C / ctrl-Break handling
-    newAction.sa_sigaction = &TransferSignalToHandlerLoop,
+    newAction.sa_sigaction = &TransferSignalToHandlerLoop;
     rv = sigaction(SIGINT, &newAction, &g_origSigIntHandler);
     assert(rv == 0);
     rv = sigaction(SIGQUIT, &newAction, &g_origSigQuitHandler);
