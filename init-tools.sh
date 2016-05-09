3fece38aada3c099586cdf990c909d497bf6e987
Jose Perez Rodriguez <joperezr@microsoft.com>
2016-05-09 10:06:06 -0700

    Catching errors if they are thrown in the inner init-tools

--- init-tools.sh
+++ init-tools.sh
@@ -82,6 +82,10 @@ if [ ! -e $__PROJECT_JSON_FILE ]; then
     echo "Initializing BuildTools..."
     echo "Running: $__BUILD_TOOLS_PATH/init-tools.sh $__scriptpath $__DOTNET_CMD $__TOOLRUNTIME_DIR" >> $__init_tools_log
     $__BUILD_TOOLS_PATH/init-tools.sh $__scriptpath $__DOTNET_CMD $__TOOLRUNTIME_DIR >> $__init_tools_log
+    if [ "$?" != "0" ]; then
+        echo "ERROR: An error occured when trying to initialize the tools. Please check '$__init_tools_log' for more details."
+        exit 1
+    fi
     echo "Done initializing tools."
 else
     echo "Tools are already initialized"
