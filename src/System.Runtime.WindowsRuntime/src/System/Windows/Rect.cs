b82b1734a0f9cfc2123149174592ef19535964a8
Hugh Bellamy <hughbellars@gmail.com>
2016-05-15 22:21:14 +0100

    Fix incorrect ordering of parameter names and messages in ArgumentOutOfRangeException (#8549)
    * Fix incorrect ordering of parameter names and messages in ArgumentOutOfRangeException
    
    Correct ctor is `paramName, message`, not `message, paramName`

--- src/System.Runtime.WindowsRuntime/src/System/Windows/Rect.cs
+++ src/System.Runtime.WindowsRuntime/src/System/Windows/Rect.cs
@@ -51,9 +51,9 @@ namespace Windows.Foundation
                     double height)
         {
             if (width < 0)
-                throw new ArgumentOutOfRangeException(SR.ArgumentOutOfRange_NeedNonNegNum, nameof(width));
+                throw new ArgumentOutOfRangeException(nameof(width), SR.ArgumentOutOfRange_NeedNonNegNum);
             if (height < 0)
-                throw new ArgumentOutOfRangeException(SR.ArgumentOutOfRange_NeedNonNegNum, nameof(height));
+                throw new ArgumentOutOfRangeException(nameof(height), SR.ArgumentOutOfRange_NeedNonNegNum);
 
             _x = (float)x;
             _y = (float)y;
@@ -120,7 +120,7 @@ namespace Windows.Foundation
             set
             {
                 if (value < 0)
-                    throw new ArgumentOutOfRangeException(SR.ArgumentOutOfRange_NeedNonNegNum, nameof(Width));
+                    throw new ArgumentOutOfRangeException(nameof(Width), SR.ArgumentOutOfRange_NeedNonNegNum);
 
                 _width = (float)value;
             }
@@ -132,7 +132,7 @@ namespace Windows.Foundation
             set
             {
                 if (value < 0)
-                    throw new ArgumentOutOfRangeException(SR.ArgumentOutOfRange_NeedNonNegNum, nameof(Height));
+                    throw new ArgumentOutOfRangeException(nameof(Height), SR.ArgumentOutOfRange_NeedNonNegNum);
 
                 _height = (float)value;
             }
