8202cd499b0e78aca09147facc748627ed7e5613
Stephen Toub <stoub@microsoft.com>
2016-05-19 12:16:24 -0400

    Fix build break in System.Linq.Expressions

--- src/System.Linq.Expressions/src/System/Linq/Expressions/ConstantExpression.cs
+++ src/System.Linq.Expressions/src/System/Linq/Expressions/ConstantExpression.cs
@@ -110,15 +110,15 @@ namespace System.Linq.Expressions
         public static ConstantExpression Constant(object value, Type type)
         {
             ContractUtils.RequiresNotNull(type, nameof(type));
-            TypeUtils.ValidateType(type);
+            TypeUtils.ValidateType(type, nameof(type));
             if (type.IsByRef)
             {
-                throw Error.TypeMustNotBeByRef();
+                throw Error.TypeMustNotBeByRef(nameof(type));
             }
 
             if (type.IsPointer)
             {
-                throw Error.TypeMustNotBePointer();
+                throw Error.TypeMustNotBePointer(nameof(type));
             }
 
             if (value == null)
