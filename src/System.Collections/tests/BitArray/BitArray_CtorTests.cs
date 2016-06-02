eeecbe4f92c16a86affa82a14e8b18c8ab2dd21f
Stephen A. Imhoff <clockwork-muse@outlook.com>
2016-06-02 14:14:43 +0900

    Add missing data cases, make some cases explicit.

--- src/System.Collections/tests/BitArray/BitArray_CtorTests.cs
+++ src/System.Collections/tests/BitArray/BitArray_CtorTests.cs
@@ -27,12 +27,17 @@ namespace System.Collections.Tests
             ICollection collection = bitArray;
             Assert.Equal(length, collection.Count);
             Assert.False(collection.IsSynchronized);
-
-            Ctor_BitArray(bitArray);
-            Ctor_Int_Bool(length, false);
-            Ctor_Int_Bool(length, true);
         }
 
+        [Theory]
+        [InlineData(0, true)]
+        [InlineData(0, false)]
+        [InlineData(40, true)]
+        [InlineData(40, false)]
+        [InlineData(200, true)]
+        [InlineData(200, false)]
+        [InlineData(65551, true)]
+        [InlineData(65551, false)]
         public static void Ctor_Int_Bool(int length, bool defaultValue)
         {
             BitArray bitArray = new BitArray(length, defaultValue);
@@ -45,8 +50,6 @@ namespace System.Collections.Tests
             ICollection collection = bitArray;
             Assert.Equal(length, collection.Count);
             Assert.False(collection.IsSynchronized);
-
-            Ctor_BitArray(bitArray);
         }
 
         [Fact]
@@ -56,9 +59,22 @@ namespace System.Collections.Tests
             Assert.Throws<ArgumentOutOfRangeException>("length", () => new BitArray(-1, false));
         }
 
+        public static IEnumerable<object[]> Ctor_BoolArray_TestData()
+        {
+            yield return new object[] { new bool[0] };
+            yield return new object[] { new bool[] { false } };
+            yield return new object[] { new bool[] { true } };
+            yield return new object[] { new bool[] { false, false, true, false, false, false, true, false, false, true } };
+            yield return new object[] { Enumerable.Repeat(true, 32).ToArray() };
+            yield return new object[] { Enumerable.Repeat(false, 32).ToArray() };
+            yield return new object[] { Enumerable.Range(0, 32).Select(x => x % 2 == 0).ToArray() };
+            yield return new object[] { Enumerable.Repeat(true, 64).ToArray() };
+            yield return new object[] { Enumerable.Repeat(false, 64).ToArray() };
+            yield return new object[] { Enumerable.Range(0, 64).Select(x => x % 2 == 0).ToArray() };
+        }
+
         [Theory]
-        [InlineData(new bool[0])]
-        [InlineData(new bool[] { false, false, true, false, false, false, true, false, false, true })]
+        [MemberData(nameof(Ctor_BoolArray_TestData))]
         public static void Ctor_BoolArray(bool[] values)
         {
             BitArray bitArray = new BitArray(values);
@@ -71,11 +87,30 @@ namespace System.Collections.Tests
             ICollection collection = bitArray;
             Assert.Equal(values.Length, collection.Count);
             Assert.False(collection.IsSynchronized);
-
-            Ctor_BitArray(bitArray);
         }
 
-        public static void Ctor_BitArray(BitArray bits)
+        public static IEnumerable<object[]> Ctor_BitArray_TestData()
+        {
+            foreach (int size in new[] { 0, 1, 8, 32, 64 })
+            {
+                yield return new object[] { "length", new BitArray(size) };
+                yield return new object[] { "length|default(true)", new BitArray(size, true) };
+                yield return new object[] { "length|default(false)", new BitArray(size, false) };
+                yield return new object[] { "bool[](all)", new BitArray(Enumerable.Repeat(true, size).ToArray()) };
+                yield return new object[] { "bool[](none)", new BitArray(Enumerable.Repeat(false, size).ToArray()) };
+                yield return new object[] { "bool[](alternating)", new BitArray(Enumerable.Range(0, size).Select(x => x % 2 == 0).ToArray()) };
+                yield return new object[] { "byte[](all)", new BitArray(Enumerable.Repeat((byte)255, size / 8).ToArray()) };
+                yield return new object[] { "byte[](none)", new BitArray(Enumerable.Repeat((byte)0, size / 8).ToArray()) };
+                yield return new object[] { "byte[](alternating)", new BitArray(Enumerable.Repeat((byte)0xaa, size / 8).ToArray()) };
+                yield return new object[] { "int[](all)", new BitArray(Enumerable.Repeat(unchecked((int)0xffffffff), size / 32).ToArray()) };
+                yield return new object[] { "int[](none)", new BitArray(Enumerable.Repeat(0, size / 32).ToArray()) };
+                yield return new object[] { "int[](alternating)", new BitArray(Enumerable.Repeat(unchecked((int)0xaaaaaaaa), size / 32).ToArray()) };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(Ctor_BitArray_TestData))]
+        public static void Ctor_BitArray(string label, BitArray bits)
         {
             BitArray bitArray = new BitArray(bits);
             Assert.Equal(bits.Length, bitArray.Length);
@@ -91,6 +126,7 @@ namespace System.Collections.Tests
 
         public static IEnumerable<object[]> Ctor_IntArray_TestData()
         {
+            yield return new object[] { new int[0], new bool[0] };
             yield return new object[] { Enumerable.Repeat(unchecked((int)0xffffffff), 10).ToArray(), Enumerable.Repeat(true, 320).ToArray() };
             yield return new object[] { Enumerable.Repeat(0, 10).ToArray(), Enumerable.Repeat(false, 320).ToArray() };
             yield return new object[] { Enumerable.Repeat(unchecked((int)0xaaaaaaaa), 10).ToArray(), Enumerable.Range(0, 320).Select(i => i % 2 == 1).ToArray() };
@@ -117,7 +153,7 @@ namespace System.Collections.Tests
         {
             Assert.Throws<ArgumentNullException>("bits", () => new BitArray((BitArray)null));
         }
-        
+
         [Fact]
         public static void Ctor_BoolArray_Null_ThrowsArgumentNullException()
         {
@@ -132,8 +168,15 @@ namespace System.Collections.Tests
 
         public static IEnumerable<object[]> Ctor_ByteArray_TestData()
         {
+            yield return new object[] { new byte[0], new bool[0] };
             yield return new object[] { new byte[] { 255, 255 }, Enumerable.Repeat(true, 16).ToArray() };
             yield return new object[] { new byte[] { 0, 0 }, Enumerable.Repeat(false, 16).ToArray() };
+            yield return new object[] { new byte[] { 255, 255, 255, 255 }, Enumerable.Repeat(true, 32).ToArray() };
+            yield return new object[] { new byte[] { 0, 0, 0, 0 }, Enumerable.Repeat(false, 32).ToArray() };
+            yield return new object[] { Enumerable.Repeat((byte)0xaa, 4).ToArray(), Enumerable.Range(0, 32).Select(i => i % 2 == 1).ToArray() };
+            yield return new object[] { new byte[] { 255, 255, 255, 255, 255, 255, 255, 255 }, Enumerable.Repeat(true, 64).ToArray() };
+            yield return new object[] { new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 }, Enumerable.Repeat(false, 64).ToArray() };
+            yield return new object[] { Enumerable.Repeat((byte)0xaa, 8).ToArray(), Enumerable.Range(0, 64).Select(i => i % 2 == 1).ToArray() };
         }
 
         [Theory]
