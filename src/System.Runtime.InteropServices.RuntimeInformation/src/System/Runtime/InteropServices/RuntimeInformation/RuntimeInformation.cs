2465a77fb43b5380607f22f62671ddd3aa7dbbdb
Eric St. John <ericstj@microsoft.com>
2016-06-08 15:39:22 -0700

    Add win8 and wpa81 support to RuntimeInformation
    Currently restoring NETStandard.Library package on win8, win81, and
    wpa81 will fail even though these platforms support NETStandard.
    
    The reason for the failure is that S.R.Interop.RuntimeInformation didn't
    include support for these platforms.
    
    I have added two new configurations for this library that return the
    right information on these platforms and pass the WACK.

--- src/System.Runtime.InteropServices.RuntimeInformation/src/System/Runtime/InteropServices/RuntimeInformation/RuntimeInformation.cs
+++ src/System.Runtime.InteropServices.RuntimeInformation/src/System/Runtime/InteropServices/RuntimeInformation/RuntimeInformation.cs
@@ -10,9 +10,9 @@ namespace System.Runtime.InteropServices
     {
 #if netcore50aot
         private const string FrameworkName = ".NET Native";
-#elif net45
+#elif net45 || win8
         private const string FrameworkName = ".NET Framework";
-#else
+#else // netcore50 || wpa81 || other
         private const string FrameworkName = ".NET Core";
 #endif
 
