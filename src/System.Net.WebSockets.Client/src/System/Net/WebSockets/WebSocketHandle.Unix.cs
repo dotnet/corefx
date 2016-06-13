7a2acc72a6b4ede8a7fa82e83b2a58e00e7e2b82
Stephen Toub <stoub@microsoft.com>
2016-06-13 14:43:41 -0400

    Use custom network stream in managed WebSockets on Unix
    We only need to support a single read and a single write at a time, which means we can create two SocketAsyncEventArgs instances and repeatedly reuse them for all read and write operations on the stream.  This avoids a significant amount of the overhead involved in reading/writing with the network.

--- src/System.Net.WebSockets.Client/src/System/Net/WebSockets/WebSocketHandle.Unix.cs
+++ src/System.Net.WebSockets.Client/src/System/Net/WebSockets/WebSocketHandle.Unix.cs
@@ -243,7 +243,7 @@ namespace System.Net.WebSockets
                 {
                     // Connect over TCP to the remote server
                     await _client.ConnectAsync(uri.Host, uri.Port).ConfigureAwait(false);
-                    _stream = _client.GetStream();
+                    _stream = new AsyncEventArgsNetworkStream(_client.Client);
 
                     // Upgrade to SSL if needed
                     if (uri.Scheme == UriScheme.Wss)
@@ -1358,7 +1358,123 @@ namespace System.Net.WebSockets
                 public bool Fin;
                 public long PayloadLength;
             }
+
+            /// <summary>
+            /// A custom network stream that stores and reuses a single SocketAsyncEventArgs instance
+            /// for reads and a single SocketAsyncEventArgs instance for writes.  This limits it to
+            /// supporting a single read and a single write at a time, but with much less per-operation
+            /// overhead than with System.Net.Sockets.NetworkStream.
+            /// </summary>
+            private sealed class AsyncEventArgsNetworkStream : NetworkStream
+            {
+                private readonly Socket _socket;
+                private readonly SocketAsyncEventArgs _readArgs;
+                private readonly SocketAsyncEventArgs _writeArgs;
+
+                private AsyncTaskMethodBuilder<int> _readTcs;
+                private AsyncTaskMethodBuilder _writeTcs;
+                private bool _disposed;
+
+                public AsyncEventArgsNetworkStream(Socket socket) : base(socket)
+                {
+                    _socket = socket;
+
+                    _readArgs = new SocketAsyncEventArgs();
+                    _readArgs.Completed += ReadCompleted;
+
+                    _writeArgs = new SocketAsyncEventArgs();
+                    _writeArgs.Completed += WriteCompleted;
+                }
+
+                protected override void Dispose(bool disposing)
+                {
+                    base.Dispose(disposing);
+
+                    if (disposing && !_disposed)
+                    {
+                        _disposed = true;
+                        try
+                        {
+                            _readArgs.Dispose();
+                            _writeArgs.Dispose();
+                        }
+                        catch (ObjectDisposedException) { }
+                    }
+                }
+
+                public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        return Task.FromCanceled<int>(cancellationToken);
+                    }
+
+                    _readTcs = new AsyncTaskMethodBuilder<int>();
+                    Task<int> t = _readTcs.Task;
+
+                    _readArgs.SetBuffer(buffer, offset, count);
+                    if (!_socket.ReceiveAsync(_readArgs))
+                    {
+                        ReadCompleted(null, _readArgs);
+                    }
+
+                    return t;
+                }
+
+                private void ReadCompleted(object sender, SocketAsyncEventArgs e)
+                {
+                    if (e.SocketError == SocketError.Success)
+                    {
+                        _readTcs.SetResult(e.BytesTransferred);
+                    }
+                    else
+                    {
+                        _readTcs.SetException(_disposed ? (Exception)
+                            new ObjectDisposedException(GetType().Name) :
+                            new IOException(SR.net_WebSockets_Generic, new SocketException((int)e.SocketError)));
+                    }
+                }
+
+                public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        return Task.FromCanceled(cancellationToken);
+                    }
+
+                    _writeTcs = new AsyncTaskMethodBuilder();
+                    Task t = _writeTcs.Task;
+
+                    _writeArgs.SetBuffer(buffer, offset, count);
+                    if (!_socket.SendAsync(_writeArgs))
+                    {
+                        // TODO: #4900 This path should be hit very frequently (sends should very frequently simply
+                        // write into the kernel's send buffer), but it's practically never getting hit due to the current
+                        // System.Net.Sockets.dll implementation that always completing asynchronously on success :(
+                        // If that doesn't get fixed, we should try to come up with some alternative here.  This is
+                        // an important path, in part as it means the caller will complete awaits synchronously rather
+                        // than spending the costs associated with yielding in each async method up the call chain.
+                        // (This applies to ReadAsync as well, but typically to a much less extent.)
+                        WriteCompleted(null, _writeArgs);
+                    }
+
+                    return t;
+                }
+
+                private void WriteCompleted(object sender, SocketAsyncEventArgs e)
+                {
+                    if (e.SocketError == SocketError.Success)
+                    {
+                        _writeTcs.SetResult();
+                    }
+                    else
+                    {
+                        _writeTcs.SetException(_disposed ? (Exception)
+                            new ObjectDisposedException(GetType().Name) :
+                            new IOException(SR.net_WebSockets_Generic, new SocketException((int)e.SocketError)));
+                    }
+                }
+            }
         }
     }
 }
-
