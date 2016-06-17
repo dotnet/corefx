6fa7bd8a76a226995dab2c936ec004c5e3f8e483
Hugh Bellamy <hughbellars@gmail.com>
2016-06-17 21:57:49 +0100

    Fix git rebase

--- src/System.Reflection.Emit/tests/Utilities.cs
+++ src/System.Reflection.Emit/tests/Utilities.cs
@@ -4,6 +4,33 @@
 
 using System.Reflection.Emit;
 
+namespace System.Reflection.Emit.Tests
+{
+    public static class Helpers
+    {
+        public static AssemblyBuilder DynamicAssembly(string name = "TestAssembly")
+        {
+            AssemblyName assemblyName = new AssemblyName(name);
+            return AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+        }
+
+        public static ModuleBuilder DynamicModule(string assemblyName = "TestAssembly", string moduleName = "TestModule")
+        {
+            return DynamicAssembly(assemblyName).DefineDynamicModule(moduleName);
+        }
+
+        public static TypeBuilder DynamicType(TypeAttributes attributes, string assemblyName = "TestAssembly", string moduleName = "TestModule", string typeName = "TestType")
+        {
+            return DynamicModule(assemblyName, moduleName).DefineType(typeName, attributes);
+        }
+
+        public static EnumBuilder DynamicEnum(TypeAttributes visibility, Type underlyingType, string enumName = "TestEnum", string assemblyName = "TestAssembly", string moduleName = "TestModule")
+        {
+            return DynamicModule(assemblyName, moduleName).DefineEnum(enumName, visibility, underlyingType);
+        }
+    }
+}
+
 namespace TestLibrary
 {
     public static class Utilities
