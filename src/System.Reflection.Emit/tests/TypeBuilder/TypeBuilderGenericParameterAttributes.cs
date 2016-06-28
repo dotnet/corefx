1a5b7a580baf7775e940103baa460092abcff23f
Hugh Bellamy <hughbellars@gmail.com>
2016-06-28 17:21:27 +0100

    Cleanup TypeBuilder tests

--- src/System.Reflection.Emit/tests/TypeBuilder/TypeBuilderGenericParameterAttributes.cs
+++ src/System.Reflection.Emit/tests/TypeBuilder/TypeBuilderGenericParameterAttributes.cs
@@ -2,36 +2,19 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Threading;
 using Xunit;
 
 namespace System.Reflection.Emit.Tests
 {
     public class TypeBuilderGenericParameterAttributes
     {
-        public const string ModuleName = "ModuleName";
-        public const string TypeName = "TypeName";
-        public const string Method = "Method";
-
-        private TypeBuilder GetTypeBuilder(string typename)
-        {
-            AssemblyName assemblyname = new AssemblyName("assemblyname");
-            AssemblyBuilder assemblybuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyname, AssemblyBuilderAccess.Run);
-            ModuleBuilder modulebuilder = TestLibrary.Utilities.GetModuleBuilder(assemblybuilder, ModuleName);
-            return modulebuilder.DefineType(typename);
-        }
-
         [Fact]
-        public void TestGenericParameterAttributes()
+        public void GenericParameterAttributes_ReturnsNone()
         {
-            TypeBuilder typebuilder = GetTypeBuilder(TypeName);
-            string[] argu = { "argu0" };
-            GenericTypeParameterBuilder[] gtpb = typebuilder.DefineGenericParameters(argu);
-            gtpb[0].SetGenericParameterAttributes(GenericParameterAttributes.SpecialConstraintMask);
-            GenericParameterAttributes gpa = gtpb[0].DeclaringType.GetTypeInfo().GenericParameterAttributes;
+            TypeBuilder type = Helpers.DynamicType(TypeAttributes.NotPublic);
+            GenericTypeParameterBuilder[] genericParameters = type.DefineGenericParameters("T");
+            genericParameters[0].SetGenericParameterAttributes(GenericParameterAttributes.SpecialConstraintMask);
+            GenericParameterAttributes gpa = genericParameters[0].DeclaringType.GetTypeInfo().GenericParameterAttributes;
             Assert.Equal(GenericParameterAttributes.None, gpa);
         }
     }
