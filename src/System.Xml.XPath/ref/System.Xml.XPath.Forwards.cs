1527154272f67404c768856f3205d11d18885cb8
Krzysztof Wicher <kwicher@microsoft.com>
2016-07-05 09:58:02 -0700

    add system.xml.xpath facade

---
+++ src/System.Xml.XPath/ref/System.Xml.XPath.Forwards.cs
@@ -0,0 +1,16 @@
+using System.Runtime.CompilerServices;
+
+[assembly:TypeForwardedTo(typeof(System.Xml.XmlNodeOrder))]
+[assembly:TypeForwardedTo(typeof(System.Xml.XPath.IXPathNavigable))]
+[assembly:TypeForwardedTo(typeof(System.Xml.XPath.XPathDocument))]
+[assembly:TypeForwardedTo(typeof(System.Xml.XPath.XPathException))]
+[assembly:TypeForwardedTo(typeof(System.Xml.XPath.XmlSortOrder))]
+[assembly:TypeForwardedTo(typeof(System.Xml.XPath.XmlCaseOrder))]
+[assembly:TypeForwardedTo(typeof(System.Xml.XPath.XmlDataType))]
+[assembly:TypeForwardedTo(typeof(System.Xml.XPath.XPathResultType))]
+[assembly:TypeForwardedTo(typeof(System.Xml.XPath.XPathExpression))]
+[assembly:TypeForwardedTo(typeof(System.Xml.XPath.XPathItem))]
+[assembly:TypeForwardedTo(typeof(System.Xml.XPath.XPathNamespaceScope))]
+[assembly:TypeForwardedTo(typeof(System.Xml.XPath.XPathNavigator))]
+[assembly:TypeForwardedTo(typeof(System.Xml.XPath.XPathNodeIterator))]
+[assembly:TypeForwardedTo(typeof(System.Xml.XPath.XPathNodeType))]
