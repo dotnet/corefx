bbcfa61e44fb055477d99cdf1c6cbb4ababe178f
sepidehMS <sekho@microsoft.com>
2016-07-19 11:44:36 -0700

    Add XsltCompiler tests

---
+++ src/System.Xml/tests/Xslt/TestFiles/TestData/xsltc/baseline/bft15b.xsl
@@ -0,0 +1,11 @@
+<xsl:stylesheet version= '1.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform' >  
+
+    <xsl:include href="bft15a.xsl"/>
+
+    <xsl:template match="/">  
+        <xsl:for-each select="//foo">
+        <xsl:value-of select="."/>
+        </xsl:for-each>
+    </xsl:template>  
+ 
+</xsl:stylesheet>  
---
+++ src/System.Xml/tests/Xslt/XsltCompiler/Program.cs
@@ -0,0 +1,26 @@
+﻿using ModuleCore;
+using System;
+using System.IO;
+
+namespace XsltCompilerTests
+{
+    internal class Program
+    {
+        public static int Main()
+        {
+            try
+            {
+                // Verify xsltc.exe is available
+                XmlCoreTest.Common.XsltVerificationLibrary.SearchPath("xsltc.exe");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Could not find 'xsltc.exe'.  Make sure that the .NET SDK is installed");
+                return 99;
+            }
+
+            string args = @"ExecutionMode:File Host:None SDKInstalled:NotInstalled";
+            return 100 + TestRunner.Execute(args);
+        }
+    }
+}
