2e7872ff8964b8cbe6b1c1ccc15b32cf8450277f
Stephen Toub <stoub@microsoft.com>
2016-07-20 22:15:14 -0700

    Disable two failing ValueTuple tests

--- src/System.ValueTuple/tests/ValueTuple/UnitTests.cs
+++ src/System.ValueTuple/tests/ValueTuple/UnitTests.cs
@@ -324,6 +324,7 @@ public class ValueTupleTests
         ValueTupleDriverA.TestToString("(" + ((short)10000) + ", " + ((int)1000000) + ", " + ((long)10000000) + ", 2008?7?2?, 0, " + ((Single)0.0001) + ", " + ((Double)0.0000001) + ", " + now + ", (False, System.Object), " + TimeSpan.Zero + ")");
     }
 
+    [ActiveIssue(10207, Xunit.PlatformID.AnyUnix)]
     [Fact]
     public static void TestEquals_GetHashCode()
     {
@@ -415,6 +416,7 @@ public class ValueTupleTests
         ValueTupleDriverA.TestEquals_GetHashCode(ValueTupleDriverC, false, false);
     }
 
+    [ActiveIssue(10207, Xunit.PlatformID.AnyUnix)]
     [Fact]
     public static void TestCompareTo()
     {
