2b37489770c18168823dabb1a5a51600152f2e34
Jose Perez Rodriguez <joperezr@microsoft.com>
2016-07-26 16:34:41 -0700

    Uncommenting APM methods in System.IO.Compression now that Stream has been updated

--- src/System.IO.Compression/src/System/IO/Compression/DeflateStream.cs
+++ src/System.IO.Compression/src/System/IO/Compression/DeflateStream.cs
@@ -346,12 +346,11 @@ namespace System.IO.Compression
             throw new InvalidOperationException(SR.CannotWriteToDeflateStream);
         }
 
-        // Uncomment when Stream Begin/End Read is available
-        //public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback asyncCallback, object asyncState) =>
-        //    TaskToApm.Begin(ReadAsync(buffer, offset, count, CancellationToken.None), asyncCallback, asyncState);
+        public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback asyncCallback, object asyncState) =>
+            TaskToApm.Begin(ReadAsync(buffer, offset, count, CancellationToken.None), asyncCallback, asyncState);
 
-        //public override int EndRead(IAsyncResult asyncResult) =>
-        //    TaskToApm.End<int>(asyncResult);
+        public override int EndRead(IAsyncResult asyncResult) =>
+            TaskToApm.End<int>(asyncResult);
 
         public override Task<int> ReadAsync(Byte[] array, int offset, int count, CancellationToken cancellationToken)
         {
@@ -595,12 +594,11 @@ namespace System.IO.Compression
             }
         }
 
-        // Uncomment when Stream Begin/End Write is available
-        //public override IAsyncResult BeginWrite(byte[] array, int offset, int count, AsyncCallback asyncCallback, object asyncState) =>
-        //    TaskToApm.Begin(WriteAsync(array, offset, count, CancellationToken.None), asyncCallback, asyncState);
+        public override IAsyncResult BeginWrite(byte[] array, int offset, int count, AsyncCallback asyncCallback, object asyncState) =>
+            TaskToApm.Begin(WriteAsync(array, offset, count, CancellationToken.None), asyncCallback, asyncState);
 
-        //public override void EndWrite(IAsyncResult asyncResult) =>
-        //    TaskToApm.End(asyncResult);
+        public override void EndWrite(IAsyncResult asyncResult) =>
+            TaskToApm.End(asyncResult);
 
         public override Task WriteAsync(Byte[] array, int offset, int count, CancellationToken cancellationToken)
         {
