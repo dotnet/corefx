3fb5f81b2d45d7e9a5a2d995067c3c96261d174c
Mariana Rios Flores <mariari@microsoft.com>
2016-07-29 09:33:42 -0700

    Stop passing Platform=arm-soft for build-managed

--- scripts/arm32_ci_script.sh
+++ scripts/arm32_ci_script.sh
@@ -147,7 +147,7 @@ function cross_build_corefx {
 
     #Cross building for emulator rootfs
     ROOTFS_DIR="$__ARMRootfsMountPath" CPLUS_INCLUDE_PATH=$LINUX_ARM_INCPATH CXXFLAGS=$LINUX_ARM_CXXFLAGS ./build-native.sh -buildArch=$__buildArch -$__buildConfig -- cross $__verboseFlag
-    ROOTFS_DIR="$__ARMRootfsMountPath" CPLUS_INCLUDE_PATH=$LINUX_ARM_INCPATH CXXFLAGS=$LINUX_ARM_CXXFLAGS ./build-managed.sh -buildArch=$__buildArch -$__buildConfig -skipTests
+    ROOTFS_DIR="$__ARMRootfsMountPath" CPLUS_INCLUDE_PATH=$LINUX_ARM_INCPATH CXXFLAGS=$LINUX_ARM_CXXFLAGS ./build-managed.sh -$__buildConfig -skipTests
 
     #Reset the code to the upstream version
     (set +x; echo 'Rewinding HEAD to master code')
