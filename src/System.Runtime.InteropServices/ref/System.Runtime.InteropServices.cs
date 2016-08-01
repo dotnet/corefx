7a22b935161f8b0e08c1c4088e44e44178b26042
Tijoy Tom Kalathiparambil <tijoytk@microsoft.com>
2016-08-01 14:47:16 -0700

    Renaming ZeroFreeMemoryUTF8 to ZeroFreeCoTaskMemUTF8

--- src/System.Runtime.InteropServices/ref/System.Runtime.InteropServices.cs
+++ src/System.Runtime.InteropServices/ref/System.Runtime.InteropServices.cs
@@ -659,7 +659,7 @@ namespace System.Runtime.InteropServices
         [System.Security.SecurityCriticalAttribute]
         public static void ZeroFreeGlobalAllocUnicode(System.IntPtr s) { }
         [System.Security.SecurityCriticalAttribute]
-        public static void ZeroFreeMemoryUTF8(System.IntPtr s) { }
+        public static void ZeroFreeCoTaskMemUTF8(System.IntPtr s) { }
     }
     [System.AttributeUsageAttribute((System.AttributeTargets)(10496), Inherited = false)]
     public sealed partial class MarshalAsAttribute : System.Attribute
