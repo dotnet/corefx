0f285f009f4be35a2875bdd535032b14304469b4
Sedar Gokbulut <sedarg@microsoft.com>
2016-08-04 14:07:25 -0700

    uap10.1: moving some APIs from system.io and system.reflection.primitives to corelib

---
+++ src/System.Reflection.Primitives/src/System/Reflection/TypeForwarders.uap101aot.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+[assembly: global::System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reflection.CallingConventions))]
+[assembly: global::System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reflection.EventAttributes))]
+[assembly: global::System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reflection.FieldAttributes))]
+[assembly: global::System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reflection.GenericParameterAttributes))]
+[assembly: global::System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reflection.MethodAttributes))]
+[assembly: global::System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reflection.MethodImplAttributes))]
+[assembly: global::System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reflection.ParameterAttributes))]
+[assembly: global::System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reflection.PropertyAttributes))]
+[assembly: global::System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Reflection.TypeAttributes))]
+
