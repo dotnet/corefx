b63c1ead447bb180bad3cd01bd2d618313af6522
R. Fontenot <nfontenot27@gmail.com>
2016-08-16 10:57:30 -0500

    Spell-check. Punctuation-check.

--- src/Native/Unix/System.Native/pal_io.cpp
+++ src/Native/Unix/System.Native/pal_io.cpp
@@ -371,7 +371,7 @@ extern "C" int32_t SystemNative_ReadDirR(DIR* dir, void* buffer, int32_t bufferS
 //    size of the dirent struct.
 // 2) The managed code creates a byte[] buffer of the size of the native dirent
 //    and passes a pointer to this buffer to this function.
-// 3) This function gets a pointer to the possibly staticly allocated directory entry.
+// 3) This function gets a pointer to the possibly statically allocated directory entry.
 // 4) Then, byte[] entry is copied into the byte[] buffer for bufferSize bytes.
 //    This makes the 1st strcpy.
 // 5) The ConvertDirent function will set a pointer to the start of the inode name
@@ -393,7 +393,7 @@ extern "C" int32_t SystemNative_ReadDirR(DIR* dir, void* buffer, int32_t bufferS
     }
 
     errno = 0;
-    // returns a pointer to memory that may be staticly allocated by glibc.
+    // Returns a pointer to memory that may be statically allocated by glibc.
     // Data returned by readdir may be overwritten by other readdir calls 
     // on the same directory stream.
     dirent* entry = readdir(dir);
@@ -401,7 +401,7 @@ extern "C" int32_t SystemNative_ReadDirR(DIR* dir, void* buffer, int32_t bufferS
     // positive error number returned -> failure
     if (errno != 0)
     {
-        assert(errno == EBADF); // Invalid directory stream discriptor dir.
+        assert(errno == EBADF); // Invalid directory stream descriptor dir.
         *outputEntry = {}; // managed out param must be initialized
         return errno;
     }
