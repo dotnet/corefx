bbd2b810cd4a9c880a2726763d1e0bc6115f7417
Hugh Bellamy <hughbellars@gmail.com>
2016-08-17 13:07:22 +0100

    Cleanup BinaryExpression comparison tests

--- src/System.Linq.Expressions/tests/BinaryOperators/Comparison/CompareTests.cs
+++ src/System.Linq.Expressions/tests/BinaryOperators/Comparison/CompareTests.cs
@@ -7,13 +7,12 @@ using Xunit;
 
 namespace System.Linq.Expressions.Tests
 {
-    public static class BinaryEqualTests
+    public static class BinaryGreaterThanOrEqualTests
     {
         public static IEnumerable<object[]> TestData()
         {
             foreach (bool useInterpreter in new bool[] { true, false })
             {
-                yield return new object[] { new bool[] { true, false }, useInterpreter };
                 yield return new object[] { new byte[] { 0, 1, byte.MaxValue }, useInterpreter };
                 yield return new object[] { new char[] { '\0', '\b', 'A', '\uffff' }, useInterpreter };
                 yield return new object[] { new decimal[] { decimal.Zero, decimal.One, decimal.MinusOne, decimal.MinValue, decimal.MaxValue }, useInterpreter };
@@ -26,10 +25,7 @@ namespace System.Linq.Expressions.Tests
                 yield return new object[] { new uint[] { 0, 1, uint.MaxValue }, useInterpreter };
                 yield return new object[] { new ulong[] { 0, 1, ulong.MaxValue }, useInterpreter };
                 yield return new object[] { new ushort[] { 0, 1, ushort.MaxValue }, useInterpreter };
-                yield return new object[] { new TestClass[] { new TestClass(), new TestClass() }, useInterpreter };
-                yield return new object[] { new TestEnum[] { new TestEnum(), new TestEnum() }, useInterpreter };
 
-                yield return new object[] { new bool?[] { null, true, false }, useInterpreter };
                 yield return new object[] { new byte?[] { null, 0, 1, byte.MaxValue }, useInterpreter };
                 yield return new object[] { new char?[] { null, '\0', '\b', 'A', '\uffff' }, useInterpreter };
                 yield return new object[] { new decimal?[] { null, decimal.Zero, decimal.One, decimal.MinusOne, decimal.MinValue, decimal.MaxValue }, useInterpreter };
@@ -47,7 +43,7 @@ namespace System.Linq.Expressions.Tests
 
         [Theory]
         [MemberData(nameof(TestData))]
-        public static void Equal(Array array, bool useInterpreter)
+        public static void GreaterThanOrEqual(Array array, bool useInterpreter)
         {
             Type type = array.GetType().GetElementType();
             for (int i = 0; i < array.Length; i++)
@@ -56,15 +52,49 @@ namespace System.Linq.Expressions.Tests
                 {
                     object a = array.GetValue(i);
                     object b = array.GetValue(j);
-                    BinaryExpression equal = Expression.Equal(Expression.Constant(a, type), Expression.Constant(b, type));
-                    GeneralBinaryTests.CompileBinaryExpression(equal, useInterpreter, GeneralBinaryTests.CustomEquals(a, b));
+                    BinaryExpression equal = Expression.GreaterThanOrEqual(Expression.Constant(a, type), Expression.Constant(b, type));
+                    GeneralBinaryTests.CompileBinaryExpression(equal, useInterpreter, GeneralBinaryTests.CustomGreaterThanOrEqual(a, b));
+                }
+            }
+        }
+        
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public static void GreaterThan(Array array, bool useInterpreter)
+        {
+            Type type = array.GetType().GetElementType();
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    object a = array.GetValue(i);
+                    object b = array.GetValue(j);
+                    BinaryExpression equal = Expression.GreaterThan(Expression.Constant(a, type), Expression.Constant(b, type));
+                    GeneralBinaryTests.CompileBinaryExpression(equal, useInterpreter, GeneralBinaryTests.CustomGreaterThan(a, b));
+                }
+            }
+        }
+        
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public static void LessThanOrEqual(Array array, bool useInterpreter)
+        {
+            Type type = array.GetType().GetElementType();
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    object a = array.GetValue(i);
+                    object b = array.GetValue(j);
+                    BinaryExpression equal = Expression.LessThanOrEqual(Expression.Constant(a, type), Expression.Constant(b, type));
+                    GeneralBinaryTests.CompileBinaryExpression(equal, useInterpreter, GeneralBinaryTests.CustomLessThanOrEqual(a, b));
                 }
             }
         }
 
         [Theory]
         [MemberData(nameof(TestData))]
-        public static void NotEqual(Array array, bool useInterpreter)
+        public static void LessThan(Array array, bool useInterpreter)
         {
             Type type = array.GetType().GetElementType();
             for (int i = 0; i < array.Length; i++)
@@ -73,55 +103,43 @@ namespace System.Linq.Expressions.Tests
                 {
                     object a = array.GetValue(i);
                     object b = array.GetValue(j);
-                    BinaryExpression equal = Expression.NotEqual(Expression.Constant(a, type), Expression.Constant(b, type));
-                    GeneralBinaryTests.CompileBinaryExpression(equal, useInterpreter, !GeneralBinaryTests.CustomEquals(a, b));
-                }
-            }
-        }
-
-        [Theory, ClassData(typeof(CompilationTypes))]
-        public static void Equal_Constant_DefaultString(bool useInterpreter)
-        {
-            var array = new Expression[] { Expression.Constant("bar", typeof(string)), Expression.Constant(null, typeof(string)), Expression.Default(typeof(string)) };
-            var isNull = new bool[] { false, true, true };
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = 0; j < array.Length; j++)
-                {
-                    BinaryExpression equal = Expression.Equal(array[i], array[j]);
-                    GeneralBinaryTests.CompileBinaryExpression(equal, useInterpreter, isNull[i] == isNull[j]);
-                }
-            }
-        }
-
-        [Theory, ClassData(typeof(CompilationTypes))]
-        public static void Equal_Constant_DefaultNullable(bool useInterpreter)
-        {
-            var array = new Expression[] { Expression.Constant(42, typeof(int?)), Expression.Constant(null, typeof(int?)), Expression.Default(typeof(int?)) };
-            var isNull = new bool[] { false, true, true };
-            for (int i = 0; i < array.Length; i++)
-            {
-                for (int j = 0; j < array.Length; j++)
-                {
-                    BinaryExpression equal = Expression.Equal(array[i], array[j]);
-                    GeneralBinaryTests.CompileBinaryExpression(equal, useInterpreter, isNull[i] == isNull[j]);
+                    BinaryExpression equal = Expression.LessThan(Expression.Constant(a, type), Expression.Constant(b, type));
+                    GeneralBinaryTests.CompileBinaryExpression(equal, useInterpreter, GeneralBinaryTests.CustomLessThan(a, b));
                 }
             }
         }
 
         [Fact]
-        public static void Equal_CannotReduce()
+        public static void GreaterThanOrEqual_CannotReduce()
         {
-            Expression exp = Expression.Equal(Expression.Constant(0), Expression.Constant(0));
+            Expression exp = Expression.GreaterThanOrEqual(Expression.Constant(0), Expression.Constant(0));
             Assert.False(exp.CanReduce);
             Assert.Same(exp, exp.Reduce());
             Assert.Throws<ArgumentException>(null, () => exp.ReduceAndCheck());
         }
 
         [Fact]
-        public static void NotEqual_CannotReduce()
+        public static void GreaterThan_CannotReduce()
         {
-            Expression exp = Expression.NotEqual(Expression.Constant(0), Expression.Constant(0));
+            Expression exp = Expression.GreaterThan(Expression.Constant(0), Expression.Constant(0));
+            Assert.False(exp.CanReduce);
+            Assert.Same(exp, exp.Reduce());
+            Assert.Throws<ArgumentException>(null, () => exp.ReduceAndCheck());
+        }
+        
+        [Fact]
+        public static void LessThanOrEqual_CannotReduce()
+        {
+            Expression exp = Expression.LessThanOrEqual(Expression.Constant(0), Expression.Constant(0));
+            Assert.False(exp.CanReduce);
+            Assert.Same(exp, exp.Reduce());
+            Assert.Throws<ArgumentException>(null, () => exp.ReduceAndCheck());
+        }
+
+        [Fact]
+        public static void LessThan_CannotReduce()
+        {
+            Expression exp = Expression.LessThan(Expression.Constant(0), Expression.Constant(0));
             Assert.False(exp.CanReduce);
             Assert.Same(exp, exp.Reduce());
             Assert.Throws<ArgumentException>(null, () => exp.ReduceAndCheck());
@@ -130,15 +148,19 @@ namespace System.Linq.Expressions.Tests
         [Fact]
         public static void ThrowsOnLeftNull()
         {
-            Assert.Throws<ArgumentNullException>("left", () => Expression.Equal(null, Expression.Constant("")));
-            Assert.Throws<ArgumentNullException>("left", () => Expression.NotEqual(null, Expression.Constant("")));
+            Assert.Throws<ArgumentNullException>("left", () => Expression.GreaterThanOrEqual(null, Expression.Constant(0)));
+            Assert.Throws<ArgumentNullException>("left", () => Expression.GreaterThan(null, Expression.Constant(0)));
+            Assert.Throws<ArgumentNullException>("left", () => Expression.LessThanOrEqual(null, Expression.Constant(0)));
+            Assert.Throws<ArgumentNullException>("left", () => Expression.LessThanOrEqual(null, Expression.Constant(0)));
         }
 
         [Fact]
         public static void ThrowsOnRightNull()
         {
-            Assert.Throws<ArgumentNullException>("right", () => Expression.Equal(Expression.Constant(""), null));
-            Assert.Throws<ArgumentNullException>("right", () => Expression.NotEqual(Expression.Constant(""), null));
+            Assert.Throws<ArgumentNullException>("right", () => Expression.GreaterThanOrEqual(Expression.Constant(0), null));
+            Assert.Throws<ArgumentNullException>("right", () => Expression.GreaterThan(Expression.Constant(0), null));
+            Assert.Throws<ArgumentNullException>("right", () => Expression.LessThanOrEqual(Expression.Constant(0), null));
+            Assert.Throws<ArgumentNullException>("right", () => Expression.LessThan(Expression.Constant(0), null));
         }
 
         private static class Unreadable<T>
@@ -153,59 +175,48 @@ namespace System.Linq.Expressions.Tests
         public static void ThrowsOnLeftUnreadable()
         {
             Expression value = Expression.Property(null, typeof(Unreadable<int>), "WriteOnly");
-            Assert.Throws<ArgumentException>("left", () => Expression.Equal(value, Expression.Constant(1)));
-            Assert.Throws<ArgumentException>("left", () => Expression.NotEqual(value, Expression.Constant(1)));
+            Assert.Throws<ArgumentException>("left", () => Expression.GreaterThanOrEqual(value, Expression.Constant(1)));
+            Assert.Throws<ArgumentException>("left", () => Expression.GreaterThan(value, Expression.Constant(1)));
+            Assert.Throws<ArgumentException>("left", () => Expression.LessThanOrEqual(value, Expression.Constant(1)));
+            Assert.Throws<ArgumentException>("left", () => Expression.LessThan(value, Expression.Constant(1)));
         }
 
         [Fact]
         public static void ThrowsOnRightUnreadable()
         {
             Expression value = Expression.Property(null, typeof(Unreadable<int>), "WriteOnly");
-            Assert.Throws<ArgumentException>("right", () => Expression.Equal(Expression.Constant(1), value));
-            Assert.Throws<ArgumentException>("right", () => Expression.NotEqual(Expression.Constant(1), value));
-        }
-
-        [Theory, ClassData(typeof(CompilationTypes))]
-        public static void Update_ReferenceEquals(bool useInterpreter)
-        {
-            TestClass testClass1 = new TestClass();
-            TestClass testClass2 = new TestClass();
-            BinaryExpression equal = Expression.Equal(Expression.Constant(testClass1), Expression.Constant(testClass2));
-            BinaryExpression newEqual = equal.Update(Expression.Constant(testClass1), equal.Conversion, Expression.Constant(testClass1));
-
-            // Original BinaryExpression should be unchanged
-            GeneralBinaryTests.CompileBinaryExpression(equal, useInterpreter, false);
-            GeneralBinaryTests.CompileBinaryExpression(newEqual, useInterpreter, true);
-        }
-
-        [Theory, ClassData(typeof(CompilationTypes))]
-        public static void Update_ReferenceNotEquals(bool useInterpreter)
-        {
-            TestClass testClass1 = new TestClass();
-            TestClass testClass2 = new TestClass();
-            BinaryExpression equal = Expression.NotEqual(Expression.Constant(testClass1), Expression.Constant(testClass2));
-            BinaryExpression newEqual = equal.Update(Expression.Constant(testClass1), equal.Conversion, Expression.Constant(testClass1));
-
-            // Original BinaryExpression should be unchanged
-            GeneralBinaryTests.CompileBinaryExpression(equal, useInterpreter, true);
-            GeneralBinaryTests.CompileBinaryExpression(newEqual, useInterpreter, false);
+            Assert.Throws<ArgumentException>("right", () => Expression.GreaterThanOrEqual(Expression.Constant(1), value));
+            Assert.Throws<ArgumentException>("right", () => Expression.GreaterThan(Expression.Constant(1), value));
+            Assert.Throws<ArgumentException>("right", () => Expression.LessThanOrEqual(Expression.Constant(1), value));
+            Assert.Throws<ArgumentException>("right", () => Expression.LessThan(Expression.Constant(1), value));
         }
 
         [Fact]
-        public static void Equal_ToString()
+        public static void GreaterThanOrEqual_ToString()
         {
-            var e = Expression.Equal(Expression.Parameter(typeof(int), "a"), Expression.Parameter(typeof(int), "b"));
-            Assert.Equal("(a == b)", e.ToString());
+            var e = Expression.GreaterThanOrEqual(Expression.Parameter(typeof(int), "a"), Expression.Parameter(typeof(int), "b"));
+            Assert.Equal("(a >= b)", e.ToString());
         }
 
         [Fact]
-        public static void NotEqual_ToString()
+        public static void GreaterThan_ToString()
         {
-            var e = Expression.NotEqual(Expression.Parameter(typeof(int), "a"), Expression.Parameter(typeof(int), "b"));
-            Assert.Equal("(a != b)", e.ToString());
+            var e = Expression.GreaterThan(Expression.Parameter(typeof(int), "a"), Expression.Parameter(typeof(int), "b"));
+            Assert.Equal("(a > b)", e.ToString());
         }
 
-        public class TestClass { }
-        public enum TestEnum { }
+        [Fact]
+        public static void LessThanOrEqual_ToString()
+        {
+            var e = Expression.LessThanOrEqual(Expression.Parameter(typeof(int), "a"), Expression.Parameter(typeof(int), "b"));
+            Assert.Equal("(a <= b)", e.ToString());
+        }
+
+        [Fact]
+        public static void LessThan_ToString()
+        {
+            var e = Expression.LessThan(Expression.Parameter(typeof(int), "a"), Expression.Parameter(typeof(int), "b"));
+            Assert.Equal("(a < b)", e.ToString());
+        }
     }
 }
