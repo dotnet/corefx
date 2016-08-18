846b8f87590fad3346d99ba808ed72c4fc93ebe8
Denis Istomin <istomin.den@gmail.com>
2016-08-18 08:51:12 -0700

    Add license header and delegate invocation syntax

--- src/System.Linq.Expressions/tests/IndexExpression/IndexExpressionHelpers.cs
+++ src/System.Linq.Expressions/tests/IndexExpression/IndexExpressionHelpers.cs
@@ -1,24 +1,28 @@
-﻿using Xunit;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
-namespace System.Linq.Expressions.Tests.IndexExpression
+using Xunit;
+
+namespace System.Linq.Expressions.Tests
 {
     public static class IndexExpressionHelpers
     {
-        internal static void AssertEqual(Expressions.IndexExpression expected, Expressions.IndexExpression actual)
+        internal static void AssertEqual(IndexExpression expected, IndexExpression actual)
         {
             Assert.Equal(expected.Object, actual.Object);
             Assert.Equal(expected.Indexer, actual.Indexer);
             Assert.Equal(expected.Arguments, actual.Arguments);
         }
 
-        internal static void AssertInvokeCorrect<T>(T expected, Expressions.IndexExpression expression)
+        internal static void AssertInvokeCorrect<T>(T expected, IndexExpression expression)
         {
             var lambda = Expression.Lambda<Func<T>>(expression);
-
+            
             // Compile and evaluate with interpretation flag and without
             // in case there are bugs in the compiler/interpreter. 
-            Assert.Equal(expected, lambda.Compile(false).Invoke());
-            Assert.Equal(expected, lambda.Compile(true).Invoke());
+            Assert.Equal(expected, lambda.Compile(false)());
+            Assert.Equal(expected, lambda.Compile(true)());
         }
     }
 }
--- src/System.Linq.Expressions/tests/IndexExpression/SampleClassWithProperties.cs
+++ src/System.Linq.Expressions/tests/IndexExpression/SampleClassWithProperties.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
 using System.Reflection;
 
-namespace System.Linq.Expressions.Tests.IndexExpression
+namespace System.Linq.Expressions.Tests
 {
     internal class SampleClassWithProperties
     {
@@ -11,7 +15,7 @@ namespace System.Linq.Expressions.Tests.IndexExpression
         internal MemberExpression DefaultPropertyExpression => Expression.Property(Expression.Constant(this),
             typeof(SampleClassWithProperties).GetProperty(nameof(DefaultProperty)));
 
-        internal Expressions.IndexExpression DefaultIndexExpression => Expression.MakeIndex(
+        internal IndexExpression DefaultIndexExpression => Expression.MakeIndex(
             DefaultPropertyExpression,
             DefaultIndexer,
             DefaultArguments);
