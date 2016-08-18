5939f1f347a298ea6c1240abf8eddfaa3a58e124
James Ko <jamesqko@gmail.com>
2016-08-18 15:23:23 -0400

    Update the issue number to a one that's not stale

--- src/System.Runtime/tests/System/StringTests.cs
+++ src/System.Runtime/tests/System/StringTests.cs
@@ -572,7 +572,7 @@ namespace System.Tests
                 {
                     // Use Compare(string, int, string, int, int) or Compare(string, int, string, int, int, false)
                     Assert.Equal(expected, Math.Sign(string.Compare(strA, indexA, strB, indexB, length)));
-                    // Uncomment when this is exposed in .NET Core (dotnet/corefx#10963)
+                    // Uncomment when this is exposed in .NET Core (dotnet/corefx#10066)
                     // Assert.Equal(expected, Math.Sign(string.Compare(strA, indexA, strB, indexB, length, ignoreCase: false)));
                 }
             }
@@ -584,7 +584,7 @@ namespace System.Tests
                 if (!skipNonComparisonOverloads)
                 {
                     // Use Compare(string, int, string, int, int, true)
-                    // Uncomment when this is exposed in .NET Core (dotnet/corefx#10963)
+                    // Uncomment when this is exposed in .NET Core (dotnet/corefx#10066)
                     // Assert.Equal(expected, Math.Sign(string.Compare(strA, indexA, strB, indexB, length, ignoreCase: true)));
                 }
             }
