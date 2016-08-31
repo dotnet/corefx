4263c1378c5d5bbff98681361eb0c728abfa610e
sepidehMS <sekho@microsoft.com>
2016-08-31 12:53:54 -0700

    Address feedback

--- src/System.Runtime/src/System/Reflection/AssemblyNameProxy.cs
+++ src/System.Runtime/src/System/Reflection/AssemblyNameProxy.cs
@@ -16,9 +16,14 @@
 namespace System.Reflection {
     using System;
     using System.Runtime.Versioning;
+    using System.Runtime.Loader;
 
     [System.Runtime.InteropServices.ComVisible(true)]
     public class AssemblyNameProxy : MarshalByRefObject
     {
+        public AssemblyName GetAssemblyName(String assemblyFile)
+        {
+            return AssemblyLoadContext.GetAssemblyName(assemblyFile);
+        }
     }
 }
