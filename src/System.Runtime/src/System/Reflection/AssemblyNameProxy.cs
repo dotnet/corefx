2c9fa581b80b5088eebe83f6284ce18b579e9ae5
sepidehMS <sekho@microsoft.com>
2016-08-31 19:53:46 -0700

    Address feedback: Clean up

--- src/System.Runtime/src/System/Reflection/AssemblyNameProxy.cs
+++ src/System.Runtime/src/System/Reflection/AssemblyNameProxy.cs
@@ -2,17 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-/*============================================================
-**
-**
-**
-**
-**
-**
-** Purpose: Remotable version the AssemblyName
-**
-**
-===========================================================*/
 namespace System.Reflection {
     using System;
     using System.Runtime.Versioning;
