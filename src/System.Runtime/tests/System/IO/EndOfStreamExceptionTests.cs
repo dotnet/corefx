687771d7bcadc91195374d496313f4c9685ebbd4
danmosemsft <danmose@microsoft.com>
2016-09-06 07:43:26 -0700

    Separate EndOfStreamException

--- src/System.Runtime/tests/System/IO/EndOfStreamExceptionTests.cs
+++ src/System.Runtime/tests/System/IO/EndOfStreamExceptionTests.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -8,38 +8,37 @@ using Xunit;
 
 namespace System.Tests
 {
-    public static class NullReferenceExceptionTests
+    public static class EndOfStreamExceptionTests
     {
         private const string exceptionMessage = "Created an exception";
         private const string innerExceptionMessage = "Created an InnerException";
         private const uint COR_E_ENDOFSTREAM = 0x80070026;
-        private const uint E_POINTER = 0x80004003;
 
         [Fact]
-        public static void NullReferenceException_Ctor_Empty()
+        public static void EndOfStreamException_Ctor_Empty()
         {
-            NullReferenceException i = new NullReferenceException();
-            Assert.Equal(E_POINTER, (uint)i.HResult);
+            EndOfStreamException i = new EndOfStreamException();
+            Assert.Equal(COR_E_ENDOFSTREAM, (uint)i.HResult);
         }
 
         [Fact]
-        public static void NullReferenceException_Ctor_String()
+        public static void EndOfStreamException_Ctor_String()
         {
-            NullReferenceException i = new NullReferenceException(exceptionMessage);
+            EndOfStreamException i = new EndOfStreamException(exceptionMessage);
             Assert.Equal(exceptionMessage, i.Message);
-            Assert.Equal(E_POINTER, (uint)i.HResult);
+            Assert.Equal(COR_E_ENDOFSTREAM, (uint)i.HResult);
         }
 
         [Fact]
-        public static void NullReferenceException_Ctor_String_Exception()
+        public static void EndOfStreamException_Ctor_String_Exception()
         {
             Exception ex = new Exception(innerExceptionMessage);
-            NullReferenceException i = new NullReferenceException(exceptionMessage, ex);
+            EndOfStreamException i = new EndOfStreamException(exceptionMessage, ex);
 
             Assert.Equal(exceptionMessage, i.Message);
             Assert.Equal(i.InnerException.Message, innerExceptionMessage);
             Assert.Equal(i.InnerException.HResult, ex.HResult);
-            Assert.Equal(E_POINTER, (uint)i.HResult);
+            Assert.Equal(COR_E_ENDOFSTREAM, (uint)i.HResult);
         }
     }
 }
