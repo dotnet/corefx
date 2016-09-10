cffe9e6ef80dbd6cce7f55e8db990718c4c55bf4
James Ko <jamesqko@gmail.com>
2016-09-09 22:43:48 -0400

    Add comments to HashHelpers.Combine

--- src/Common/src/System/Numerics/Hashing/HashHelpers.cs
+++ src/Common/src/System/Numerics/Hashing/HashHelpers.cs
@@ -11,6 +11,10 @@ namespace System.Numerics.Hashing
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Combine(int h1, int h2)
         {
+            // This is the same algorithm that is used by Boost for combining hashes.
+            // You can find an explanation of what the magic number is here:
+            // http://stackoverflow.com/a/4948967/4077294
+
             uint mask = (uint)h2 + 0x9e3779b9 + ((uint)h1 << 6) + ((uint)h1 >> 2);
             h1 ^= (int)mask;
             return h1;
