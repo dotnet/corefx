0539e539387623a7a43d8275f3925f787ad8eac8
Eric Eilebrecht <ericeil@microsoft.com>
2016-09-22 09:19:02 -0700

    Add System.Net.Dns APIs for netstandard 2.0 (#11950)

--- src/System.Net.NameResolution/ref/System.Net.NameResolution.cs
+++ src/System.Net.NameResolution/ref/System.Net.NameResolution.cs
@@ -10,10 +10,34 @@ namespace System.Net
 {
     public static partial class Dns
     {
+        public static IAsyncResult BeginGetHostAddresses(string hostNameOrAddress, AsyncCallback requestCallback, object state) { return default(IAsyncResult); }
+        [Obsolete("BeginGetHostByName is obsoleted for this type, please use BeginGetHostEntry instead. http://go.microsoft.com/fwlink/?linkid=14202")]
+        public static IAsyncResult BeginGetHostByName(string hostName, AsyncCallback requestCallback, object stateObject) { return default(IAsyncResult); }
+        public static IAsyncResult BeginGetHostEntry(string hostNameOrAddress, AsyncCallback requestCallback, object stateObject) { return default(IAsyncResult); }
+        public static IAsyncResult BeginGetHostEntry(IPAddress address, AsyncCallback requestCallback, object stateObject) { return default(IAsyncResult); }
+        [Obsolete("BeginResolve is obsoleted for this type, please use BeginGetHostEntry instead. http://go.microsoft.com/fwlink/?linkid=14202")]
+        public static IAsyncResult BeginResolve(string hostName, AsyncCallback requestCallback, object stateObject) { return default(IAsyncResult); }
+        public static IPAddress[] EndGetHostAddresses(IAsyncResult asyncResult) { return default(IPAddress[]); }
+        [Obsolete("EndGetHostByName is obsoleted for this type, please use EndGetHostEntry instead. http://go.microsoft.com/fwlink/?linkid=14202")]
+        public static IPHostEntry EndGetHostByName(IAsyncResult asyncResult) { return default(IPHostEntry); }
+        public static IPHostEntry EndGetHostEntry(IAsyncResult asyncResult) { return default(IPHostEntry); }
+        [Obsolete("EndResolve is obsoleted for this type, please use EndGetHostEntry instead. http://go.microsoft.com/fwlink/?linkid=14202")]
+        public static IPHostEntry EndResolve(IAsyncResult asyncResult) { return default(IPHostEntry); }
+        public static IPAddress[] GetHostAddresses(string hostNameOrAddress) { return default(IPAddress[]); }
+        [Obsolete("GetHostByAddress is obsoleted for this type, please use GetHostEntry instead. http://go.microsoft.com/fwlink/?linkid=14202")]
+        public static IPHostEntry GetHostByAddress(IPAddress address) { return default(IPHostEntry); }
+        [Obsolete("GetHostByAddress is obsoleted for this type, please use GetHostEntry instead. http://go.microsoft.com/fwlink/?linkid=14202")]
+        public static IPHostEntry GetHostByAddress(string address) { return default(IPHostEntry); }
+        [Obsolete("GetHostByName is obsoleted for this type, please use GetHostEntry instead. http://go.microsoft.com/fwlink/?linkid=14202")]
+        public static IPHostEntry GetHostByName(string hostName) { return default(IPHostEntry); }
         public static System.Threading.Tasks.Task<System.Net.IPAddress[]> GetHostAddressesAsync(string hostNameOrAddress) { return default(System.Threading.Tasks.Task<System.Net.IPAddress[]>); }
+        public static IPHostEntry GetHostEntry(string hostNameOrAddress) { return default(IPHostEntry); }
+        public static IPHostEntry GetHostEntry(IPAddress address) { return default(IPHostEntry); }
         public static System.Threading.Tasks.Task<System.Net.IPHostEntry> GetHostEntryAsync(System.Net.IPAddress address) { return default(System.Threading.Tasks.Task<System.Net.IPHostEntry>); }
         public static System.Threading.Tasks.Task<System.Net.IPHostEntry> GetHostEntryAsync(string hostNameOrAddress) { return default(System.Threading.Tasks.Task<System.Net.IPHostEntry>); }
         public static string GetHostName() { return default(string); }
+        [Obsolete("Resolve is obsoleted for this type, please use GetHostEntry instead. http://go.microsoft.com/fwlink/?linkid=14202")]
+        public static IPHostEntry Resolve(string hostName) { return default(IPHostEntry); }
     }
     public partial class IPHostEntry
     {
