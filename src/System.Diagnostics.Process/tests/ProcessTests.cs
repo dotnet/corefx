7b7ef8f7f73ae688c823625856a733ee9462af76
Mathieu Binette <mbinette91@gmail.com>
2016-09-23 00:25:29 -0700

    [Fixed test] Wait for process to exit before asserting HasExited
    Kill() is async, so we need to wait for the process to actually exit
    before asserting HasExited. Might as well merge the new test with the
    existing one which already does WaitForExit. Kept separate asserts to
    make it easier to investigate test failures. I couldn't repro locally,
    but this should work (thank you CI). If it doesn't, I'll work harder on
    a repro.
    
    Validated that the test fails without the Process.HasExited change, and
    succeeds with the change (ran -x500).
    
    [Please squash with initial commit]

--- src/System.Diagnostics.Process/tests/ProcessTests.cs
+++ src/System.Diagnostics.Process/tests/ProcessTests.cs
@@ -104,7 +104,7 @@ namespace System.Diagnostics.Tests
         [InlineData(true)]
         [InlineData(false)]
         [InlineData(null)]
-        public void TestEnableRaiseEventsWaitForExit(bool? enable)
+        public void TestEnableRaiseEvents(bool? enable)
         {
             bool exitedInvoked = false;
 
@@ -130,25 +130,6 @@ namespace System.Diagnostics.Tests
             {
                 Assert.False(exitedInvoked);
             }
-        }
-
-        [Theory]
-        [InlineData(true)]
-        [InlineData(false)]
-        [InlineData(null)]
-        public void TestEnableRaiseEventsHasExited(bool? enable)
-        {
-            bool exitedInvoked = false;
-
-            Process p = CreateProcess();
-            if (enable.HasValue)
-            {
-                p.EnableRaisingEvents = enable.Value;
-            }
-            p.Exited += delegate { exitedInvoked = true; };
-
-            p.Start();
-            p.Kill();
 
             // HasExited can raise Exited events too
             Assert.True(p.HasExited);
