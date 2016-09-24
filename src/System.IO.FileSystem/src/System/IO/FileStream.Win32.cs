5376c25bea9523265696ed5806f3dc8b51bbbf25
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2016-09-23 22:12:18 -0700

    Add negative tests for Begin/End
    Adds negative tests for Begin/End, removes unnecessary block for `\\.\`
    paths.
    
    (The block for `\\.\` paths fulfills a need that predates our supported
    Windows OSes. You cannot inadvertently mess up "raw" drives. In addition
    we currently allow `\\?\` which is another form of device path access.)

--- src/System.IO.FileSystem/src/System/IO/FileStream.Win32.cs
+++ src/System.IO.FileSystem/src/System/IO/FileStream.Win32.cs
@@ -11,19 +11,12 @@ namespace System.IO
     {
         public FileStream(Microsoft.Win32.SafeHandles.SafeFileHandle handle, FileAccess access, int bufferSize)
         {
-            this._innerStream = new Win32FileStream(handle, access, bufferSize, this);
+            _innerStream = new Win32FileStream(handle, access, bufferSize, this);
         }
 
         public FileStream(Microsoft.Win32.SafeHandles.SafeFileHandle handle, FileAccess access, int bufferSize, bool isAsync)
         {
-            this._innerStream = new Win32FileStream(handle, access, bufferSize, isAsync, this);
-        }
-
-        static partial void ValidatePath(string fullPath, string paramName)
-        {
-            // Prevent access to your disk drives as raw block devices.
-            if (fullPath.StartsWith("\\\\.\\", StringComparison.Ordinal))
-                throw new ArgumentException(SR.Arg_DevicesNotSupported, paramName);
+            _innerStream = new Win32FileStream(handle, access, bufferSize, isAsync, this);
         }
     }
 }
