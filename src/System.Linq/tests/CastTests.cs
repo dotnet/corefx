67fa9dfbefc8cb145b0ad68d3de92e8e85a52776
James Ko <jamesqko@gmail.com>
2016-09-24 22:50:50 -0400

    Add extensive test coverage for the changes

--- src/System.Linq/tests/CastTests.cs
+++ src/System.Linq/tests/CastTests.cs
@@ -3,13 +3,28 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Xunit;
 
 namespace System.Linq.Tests
 {
     public class CastTests : EnumerableTests
     {
+        private static List<Func<IEnumerable<T>, IEnumerable<T>>> IdentityTransforms<T>()
+        {
+            return new List<Func<IEnumerable<T>, IEnumerable<T>>>
+            {
+                e => e,
+                e => e.ToArray(), // well-known ICollection
+                e => e.ToList(), // well-known ICollection
+                e => new Queue<T>(e), // not-well-known ICollection
+                e => e.Select(i => i), // IPartition
+                e => !e.Any() ? e : e.Skip(1).Prepend(e.First()) // IIListProvider, non-IPartition
+            };
+        }
+
         [Fact]
         public void CastIntToLongThrows()
         {
@@ -216,6 +231,164 @@ namespace System.Linq.Tests
             // Don't insist on this behaviour, but check it's correct if it happens
             var en = iterator as IEnumerator<string>;
             Assert.False(en != null && en.MoveNext());
+        }
+
+        [Fact]
+        public void CastDisposeWorksForDisposableAndNonDisposableNonGenericIEnumerators()
+        {
+            int state1 = 0;
+            var disposable = new DisposableDelegateBasedNonGenericEnumerator
+            {
+                MoveNextWorker = () => false, // Enumerator represents a collection w/ 0 elements.
+                DisposeWorker = () => state1++
+            };
+
+            var nonDisposable = new DelegateBasedNonGenericEnumerator();
+
+            var enumerable = new DelegateBasedNonGenericEnumerable { GetEnumeratorWorker = () => disposable };
+
+            using (var e = enumerable.Cast<object>().GetEnumerator())
+            {
+                Assert.Equal(0, state1);
+
+                // Once we reach the end of the sequence, we should call Dispose if
+                // the non-generic IEnumerator implements IDisposable.
+                e.MoveNext();
+                Assert.Equal(1, state1);
+
+                // We shouldn't call it twice.
+                e.MoveNext();
+                Assert.Equal(1, state1);
+            }
+
+            enumerable.GetEnumeratorWorker = () => nonDisposable;
+
+            using (var e = enumerable.Cast<object>().GetEnumerator())
+            {
+                // If the IEnumerator does not implement IDisposable, things should still work correctly.
+                e.MoveNext();
+                e.MoveNext();
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidCastOptimizationWithAtLeastOneNonStringData))]
+        public void CountIsNotInvalidlyOptimized(IEnumerable source)
+        {
+            // If not all of the items in a collection are of type T, then
+            // optimizing collection.Cast<T>().Count() would be invalid.
+
+            Assert.Throws<InvalidCastException>(() => source.Cast<string>().Count());
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidCastOptimizationWithAtLeastOneNonStringData))]
+        public void SkipIsNotInvalidlyOptimized(IEnumerable source)
+        {
+            // Can't skip over non-Ts.
+
+            object[] sourceArray = source.Cast<object>().ToArray();
+            int lastNonStringIndex = Array.FindLastIndex(sourceArray, obj => !(obj is string));
+
+            Assert.Throws<InvalidCastException>(() =>
+            {
+                foreach (string item in source.Cast<string>().Skip(lastNonStringIndex + 1)) { }
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidCastOptimizationWithAtLeastOneNonStringData))]
+        public void ElementAtIsNotInvalidlyOptimized(IEnumerable source)
+        {
+            object[] sourceArray = source.Cast<object>().ToArray();
+            int lastNonStringIndex = Array.FindLastIndex(sourceArray, obj => !(obj is string));
+            
+            if (lastNonStringIndex == source.Cast<object>().Count() - 1)
+            {
+                // Add a string at the end.
+                source = source.Cast<object>().Append(string.Empty);
+            }
+
+            Assert.Throws<InvalidCastException>(() => source.Cast<string>().ElementAt(lastNonStringIndex + 1));
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidCastOptimizationWithAtLeastOneNonStringData))]
+        public void LastIsNotInvalidlyOptimized(IEnumerable source)
+        {
+            if (source.Cast<object>().Last() is string)
+            {
+                Assert.Throws<InvalidCastException>(() => source.Cast<string>().Last());
+            }
+            else
+            {
+                source = source.Cast<object>().Prepend(3).Append(string.Empty);
+
+                Assert.Throws<InvalidCastException>(() => source.Cast<string>().Last());
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidCastOptimizationWithAtLeastOneNonStringData))]
+        public void InvalidCastToArrayToList(IEnumerable source)
+        {
+            Assert.Throws<InvalidCastException>(() => source.Cast<string>().ToArray());
+            Assert.Throws<InvalidCastException>(() => source.Cast<string>().ToList());
+        }
+
+        public static IEnumerable<object[]> InvalidCastOptimizationWithAtLeastOneNonStringData()
+        {
+            var sources = new List<IEnumerable<object>>
+            {
+                new object[] { 3, "foo", new object() },
+                Enumerable.Repeat<object>("foo", 37).Append(3),
+                Enumerable.Range(1, 3).Cast<object>(),
+                Enumerable.Repeat(new object(), 1010),
+                Enumerable.Repeat<object>(1, 1).Append("foo").Append("foo"),
+                new object[] { 3, null, null, "foo" },
+                new object[] { null, 3 },
+                new object[] { null, 3, null, 3 }.Reverse()
+            };
+
+            foreach (var equivalentSource in IdentityTransforms<object>().SelectMany(t => sources.Select(s => t(s))))
+            {
+                yield return new object[] { equivalentSource };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(UntypedCollectionWithAllStringsData))]
+        public void CountToArrayAndToListShouldWorkAsExpected(IEnumerable source)
+        {
+            Assert.Equal(NaiveCast<string>(source).Count(), source.Cast<string>().Count());
+            Assert.Equal(NaiveCast<string>(source), source.Cast<string>().ToArray());
+            Assert.Equal(NaiveCast<string>(source), source.Cast<string>().ToList());
+        }
+
+        public static IEnumerable<object[]> UntypedCollectionWithAllStringsData()
+        {
+            var sources = new List<IEnumerable<object>>
+            {
+                Enumerable.Repeat<object>("foo", 37),
+                Enumerable.Repeat<object>(null, 123),
+                new object[] { null, "strstr", "bar" },
+                Array.Empty<object>()
+            };
+
+            foreach (var equivalentSource in IdentityTransforms<object>().SelectMany(t => sources.Select(s => t(s))))
+            {
+                yield return new object[] { equivalentSource };
+            }
+        }
+
+        private static IEnumerable<T> NaiveCast<T>(IEnumerable source)
+        {
+            Debug.Assert(source != null);
+
+            foreach (object obj in source)
+            {
+                yield return (T)obj;
+            }
         }
     }
 }
