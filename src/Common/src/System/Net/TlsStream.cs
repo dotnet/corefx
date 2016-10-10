13b779a7a0b455fd5ba3414a0008e28ad7c66d11
Lakshmi Priya Sekar <lasekar@microsoft.com>
2016-10-10 13:41:10 -0700

    Move TlsStream.cs to common location.

--- src/Common/src/System/Net/TlsStream.cs
+++ src/Common/src/System/Net/TlsStream.cs
@@ -8,7 +8,7 @@ using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
-namespace System.Net.Mail
+namespace System.Net
 {
     internal class TlsStream : NetworkStream
     {
