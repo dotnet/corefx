5cbda7bd8978bf6b93e6488e301c0ea8e8fb103f
Stephen A. Imhoff <clockwork-muse@outlook.com>
2016-10-19 13:06:34 -0700

    Use only NullReferenceException test.

--- src/System.Globalization.Extensions/tests/Normalization/StringNormalizationTests.cs
+++ src/System.Globalization.Extensions/tests/Normalization/StringNormalizationTests.cs
@@ -24,8 +24,7 @@ namespace System.Globalization.Tests
         }
 
         [Fact]
-        [PlatformSpecific(~TestPlatforms.Windows)]
-        public void IsNormalized_Invalid_NonWindows()
+        public void IsNormalized_Invalid()
         {
             Assert.Throws<ArgumentException>(() => "\uFB01".IsNormalized((NormalizationForm)10));
 
@@ -33,40 +32,7 @@ namespace System.Globalization.Tests
             Assert.Throws<ArgumentException>("strInput", () => "\uD800\uD800".IsNormalized()); // Invalid surrogate pair
 
             Assert.Throws<ArgumentNullException>("strInput", () => StringNormalizationExtensions.IsNormalized(null));
-
-            Assert.Throws<ArgumentNullException>("strInput", () => ((string)null).IsNormalized());
-        }
-
-        [Fact]
-        [PlatformSpecific(TestPlatforms.Windows)]
-        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework | TargetFrameworkMonikers.Netcoreapp)]
-        public void IsNormalized_Invalid_NetCore()
-        {
-            Assert.Throws<ArgumentException>(() => "\uFB01".IsNormalized((NormalizationForm)10));
-
-            Assert.Throws<ArgumentException>("strInput", () => "\uFFFE".IsNormalized()); // Invalid codepoint
-            Assert.Throws<ArgumentException>("strInput", () => "\uD800\uD800".IsNormalized()); // Invalid surrogate pair
-
-            Assert.Throws<ArgumentNullException>("strInput", () => StringNormalizationExtensions.IsNormalized(null));
-
-            Assert.Throws<ArgumentNullException>("strInput", () => ((string)null).IsNormalized());
-        }
-
-        [Fact]
-        [PlatformSpecific(TestPlatforms.Windows)]
-        [SkipOnTargetFramework(~(TargetFrameworkMonikers.NetFramework | TargetFrameworkMonikers.Netcoreapp))]
-        public void IsNormalized_Invalid_Desktop()
-        {
-            Assert.Throws<ArgumentException>(() => "\uFB01".IsNormalized((NormalizationForm)10));
-
-            Assert.Throws<ArgumentException>("strInput", () => "\uFFFE".IsNormalized()); // Invalid codepoint
-            Assert.Throws<ArgumentException>("strInput", () => "\uD800\uD800".IsNormalized()); // Invalid surrogate pair
-
-            Assert.Throws<ArgumentNullException>("strInput", () => StringNormalizationExtensions.IsNormalized(null));
-
-            // On desktop IsNormalized is not extension method, trying to do ((string)null).IsNormalized()
-            // will get NullReferenceException, in .Net Core we use extension method which will throw
-            // ArgumentNullException
+            // Ensure that extension method not used by current framework.
             Assert.Throws<NullReferenceException>(() => ((string)null).IsNormalized());
         }
 
@@ -92,8 +58,7 @@ namespace System.Globalization.Tests
         }
 
         [Fact]
-        [PlatformSpecific(~TestPlatforms.Windows)]
-        public void Normalize_Invalid_NonWindows()
+        public void Normalize_Invalid()
         {
             //Assert.True(false, "Framework not skipped");
             Assert.Throws<ArgumentException>(() => "\uFB01".Normalize((NormalizationForm)7));
@@ -102,40 +67,7 @@ namespace System.Globalization.Tests
             Assert.Throws<ArgumentException>("strInput", () => "\uD800\uD800".Normalize()); // Invalid surrogate pair
 
             Assert.Throws<ArgumentNullException>("strInput", () => StringNormalizationExtensions.Normalize(null));
-
-            Assert.Throws<ArgumentNullException>("strInput", () => ((string)null).Normalize());
-        }
-
-        [Fact]
-        [PlatformSpecific(TestPlatforms.Windows)]
-        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework | TargetFrameworkMonikers.Netcoreapp)]
-        public void Normalize_Invalid_NetCore()
-        {
-            Assert.Throws<ArgumentException>(() => "\uFB01".Normalize((NormalizationForm)7));
-
-            Assert.Throws<ArgumentException>("strInput", () => "\uFFFE".Normalize()); // Invalid codepoint
-            Assert.Throws<ArgumentException>("strInput", () => "\uD800\uD800".Normalize()); // Invalid surrogate pair
-
-            Assert.Throws<ArgumentNullException>("strInput", () => StringNormalizationExtensions.Normalize(null));
-
-            Assert.Throws<ArgumentNullException>("strInput", () => ((string)null).Normalize());
-        }
-
-        [Fact]
-        [PlatformSpecific(TestPlatforms.Windows)]
-        [SkipOnTargetFramework(~(TargetFrameworkMonikers.NetFramework | TargetFrameworkMonikers.Netcoreapp))]
-        public void Normalize_Invalid_Desktop()
-        {
-            Assert.Throws<ArgumentException>(() => "\uFB01".Normalize((NormalizationForm)7));
-
-            Assert.Throws<ArgumentException>("strInput", () => "\uFFFE".Normalize()); // Invalid codepoint
-            Assert.Throws<ArgumentException>("strInput", () => "\uD800\uD800".Normalize()); // Invalid surrogate pair
-
-            Assert.Throws<ArgumentNullException>("strInput", () => StringNormalizationExtensions.Normalize(null));
-
-            // On desktop Normalize is not extension method, trying to do ((string)null).Normalize()
-            // will get NullReferenceException, in .Net Core we use extension method which will throw
-            // ArgumentNullException
+            // Ensure that extension method not used by current framework.
             Assert.Throws<NullReferenceException>(() => ((string)null).Normalize());
         }
     }
