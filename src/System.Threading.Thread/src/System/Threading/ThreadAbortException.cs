268d20106b95075c716ba8951a2bcd34a1110242
Stephen Toub <stoub@microsoft.com>
2016-10-18 21:48:38 -0400

    Add System.Threading.Thread serialization

--- src/System.Threading.Thread/src/System/Threading/ThreadAbortException.cs
+++ src/System.Threading.Thread/src/System/Threading/ThreadAbortException.cs
@@ -2,12 +2,19 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Runtime.Serialization;
+
 namespace System.Threading 
 {
     // Thread.Abort() is not supported in .NET core, so this is currently just a stub to make the type available at compile time
+    [Serializable]
     public sealed class ThreadAbortException : SystemException 
     {
         private ThreadAbortException()
+        {
+        }
+
+        internal ThreadAbortException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
 
