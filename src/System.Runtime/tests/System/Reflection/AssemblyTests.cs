11ad5c7adebee674221dcacd0aeecb72ea23ac89
Rahul Kumar <rahku@microsoft.com>
2016-10-25 14:58:08 -0700

    Re-enable assembly.load tests

--- src/System.Runtime/tests/System/Reflection/AssemblyTests.cs
+++ src/System.Runtime/tests/System/Reflection/AssemblyTests.cs
@@ -133,7 +133,6 @@ namespace System.Reflection.Tests
             Assert.Throws<ArgumentException>(() => Assembly.Load(emptyCName));
         }
 
-        [ActiveIssue(12818)]
         [Fact]
         public static void AssemblyLoadFromBytes()
         {
@@ -145,7 +144,6 @@ namespace System.Reflection.Tests
             Assert.Equal(assembly.FullName, loadedAssembly.FullName);
         }
 
-        [ActiveIssue(12818)]
         [Fact]
         public static void AssemblyLoadFromBytesNeg()
         {
