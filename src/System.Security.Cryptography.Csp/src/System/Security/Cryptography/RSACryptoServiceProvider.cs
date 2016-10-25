fa04ddbaa3a6e8d899c6d77c6646a16986e2657c
Alex Perovich <alperovi@microsoft.com>
2016-10-25 16:15:11 -0700

    Add SuppressMessage attributes for valid uses of SHA-1 and MD5 (#13014)

--- src/System.Security.Cryptography.Csp/src/System/Security/Cryptography/RSACryptoServiceProvider.cs
+++ src/System.Security.Cryptography.Csp/src/System/Security/Cryptography/RSACryptoServiceProvider.cs
@@ -564,6 +564,8 @@ namespace System.Security.Cryptography
             }
         }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA5351", Justification = "MD5 is used when the user asks for it.")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA5350", Justification = "SHA1 is used when the user asks for it.")]
         private static HashAlgorithm GetHashAlgorithm(HashAlgorithmName hashAlgorithm)
         {
             switch (hashAlgorithm.Name)
