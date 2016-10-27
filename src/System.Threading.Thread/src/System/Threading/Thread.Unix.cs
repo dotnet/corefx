a196c91b0a246ad689175656f644edb1224f1f54
Koundinya Veluri <kouvel@users.noreply.github.com>
2016-10-26 21:43:16 -0700

    Expose some Thread members (#12827)
    * Expose some Thread members
    
    - Implemented Thread type in CoreFX, so with this, Thread will no longer be type-forwarded to the runtime. It instead forwards selectively to the runtime through a RuntimeThread instance for functionality is currently strongly tied with the runtime. Some of this functionality may further be moved to CoreFX later.
    - S.Threading.Thread compiles against S.Private.CoreLib of the runtime for access to RuntimeThread, and due to type conflicts between CoreLib and CoreFX assemblies, uses project references to the CoreFX implementation projects and aliases where necessary to reference the intended public type
    
    Related to #11632, #11638

---
+++ src/System.Threading.Thread/src/System/Threading/Thread.Unix.cs
@@ -0,0 +1,26 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.ConstrainedExecution;
+
+namespace System.Threading
+{
+    public sealed partial class Thread : CriticalFinalizerObject
+    {
+        public ApartmentState GetApartmentState()
+        {
+            return ApartmentState.Unknown;
+        }
+
+        private static Exception GetApartmentStateChangeFailedException()
+        {
+            return new PlatformNotSupportedException();
+        }
+
+        private bool TrySetApartmentStateUnchecked(ApartmentState state)
+        {
+            return state == GetApartmentState();
+        }
+    }
+}
