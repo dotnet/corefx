5cfbd066343338c696b4a5345c7bd188c87b0096
Koundinya Veluri <kouvel@users.noreply.github.com>
2016-10-26 21:43:16 -0700

    Expose some Thread members (#12827)
    * Expose some Thread members
    
    - Implemented Thread type in CoreFX, so with this, Thread will no longer be type-forwarded to the runtime. It instead forwards selectively to the runtime through a RuntimeThread instance for functionality is currently strongly tied with the runtime. Some of this functionality may further be moved to CoreFX later.
    - S.Threading.Thread compiles against S.Private.CoreLib of the runtime for access to RuntimeThread, and due to type conflicts between CoreLib and CoreFX assemblies, uses project references to the CoreFX implementation projects and aliases where necessary to reference the intended public type
    
    Related to #11632, #11638

--- src/System.Threading.Thread/tests/ThreadExceptionEventArgsTests.cs
+++ src/System.Threading.Thread/tests/ThreadExceptionEventArgsTests.cs
@@ -2,20 +2,21 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Threading;
 using Xunit;
 
-public static class ThreadExceptionEventArgsTests
+namespace System.Threading.Threads.Tests
 {
-    [Fact]
-    public static void ConstructorTest()
+    public static class ThreadExceptionEventArgsTests
     {
-        var e = new ThreadExceptionEventArgs(null);
-        Assert.Null(e.Exception);
+        [Fact]
+        public static void ConstructorTest()
+        {
+            var e = new ThreadExceptionEventArgs(null);
+            Assert.Null(e.Exception);
 
-        var ex = new Exception();
-        e = new ThreadExceptionEventArgs(ex);
-        Assert.Equal(ex, e.Exception);
+            var ex = new Exception();
+            e = new ThreadExceptionEventArgs(ex);
+            Assert.Same(ex, e.Exception);
+        }
     }
 }
