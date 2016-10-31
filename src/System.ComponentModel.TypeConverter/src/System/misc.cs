e5e604646fe15c8d029a4fa05036c8595f53623c
Santiago Fernandez Madero <safern@microsoft.com>
2016-10-31 12:00:04 -0700

    Nit fixes

--- src/System.ComponentModel.TypeConverter/src/System/misc.cs
+++ src/System.ComponentModel.TypeConverter/src/System/misc.cs
@@ -96,27 +96,32 @@ namespace System
     }
 }
 
-namespace System.ComponentModel{
-
+namespace System.ComponentModel
+{
     [HostProtection(SharedState = true)]
     internal static class CompModSwitches
     {
-        
         private static volatile BooleanSwitch commonDesignerServices;
         private static volatile TraceSwitch eventLog;
                 
-        public static BooleanSwitch CommonDesignerServices {
-            get {
-                if (commonDesignerServices == null) {
+        public static BooleanSwitch CommonDesignerServices 
+        {
+            get 
+            {
+                if (commonDesignerServices == null) 
+                {
                     commonDesignerServices = new BooleanSwitch("CommonDesignerServices", "Assert if any common designer service is not found.");
                 }
                 return commonDesignerServices;
             }
         }   
         
-        public static TraceSwitch EventLog {
-            get {
-                if (eventLog == null) {
+        public static TraceSwitch EventLog 
+        {
+            get 
+            {
+                if (eventLog == null) 
+                {
                     eventLog = new TraceSwitch("EventLog", "Enable tracing for the EventLog component.");
                 }
                 return eventLog;
