7ca4d240bf10f2d25c68c4d956148cc59e4d8529
Santiago Fernandez Madero <safern@microsoft.com>
2016-11-01 16:39:50 -0700

    Netstandard 1.7 EnvironmentVariables data roundtrip with Environment test

--- src/System.Diagnostics.Process/tests/ProcessStartInfoTests.netstandard1.7.cs
+++ src/System.Diagnostics.Process/tests/ProcessStartInfoTests.netstandard1.7.cs
@@ -20,159 +20,63 @@ namespace System.Diagnostics.Tests
     {
         [PlatformSpecific(TestPlatforms.Windows)]
         [Fact]
-        public void TestEnvironmentVariablesPropertyWindows()
+        public void TestEnvironmentVariables_Environment_DataRoundTrips()
         {
-            Assert.NotEqual(0, new Process().StartInfo.EnvironmentVariables.Count);
-
             ProcessStartInfo psi = new ProcessStartInfo();
 
             // Creating a detached ProcessStartInfo will pre-populate the environment
             // with current environmental variables.
+            psi.Environment.Clear();
 
-            StringDictionary environmentVariables = psi.EnvironmentVariables;
+            psi.EnvironmentVariables.Add("NewKey", "NewValue");
+            psi.Environment.Add("NewKey2", "NewValue2");
+            Assert.Equal(psi.Environment["NewKey"], psi.EnvironmentVariables["NewKey"]);
+            Assert.Equal(psi.Environment["NewKey2"], psi.EnvironmentVariables["NewKey2"]);
 
-            Assert.NotEqual(environmentVariables.Count, 0);
+            Assert.Equal(2, psi.EnvironmentVariables.Count);
+            Assert.Equal(psi.Environment.Count, psi.EnvironmentVariables.Count);
 
-            int CountItems = environmentVariables.Count;
+            Assert.Throws<ArgumentException>(() => { psi.EnvironmentVariables.Add("NewKey2", "NewValue2"); });
+            psi.EnvironmentVariables.Add("NewKey3", "NewValue3");
 
-            environmentVariables.Add("NewKey", "NewValue");
-            environmentVariables.Add("NewKey2", "NewValue2");
+            Assert.Throws<ArgumentException>(() => { psi.Environment.Add("NewKey3", "NewValue3"); });
 
-            Assert.Equal(CountItems + 2, environmentVariables.Count);
-            environmentVariables.Remove("NewKey");
-            Assert.Equal(CountItems + 1, environmentVariables.Count);
+            psi.EnvironmentVariables.Clear();
+            Assert.Equal(0, psi.Environment.Count);
 
-            //Exception not thrown with invalid key
-            Assert.Throws<ArgumentException>(() => { environmentVariables.Add("NewKey2", "NewValue2"); });
+            psi.EnvironmentVariables.Add("NewKey", "NewValue");
+            psi.EnvironmentVariables.Add("NewKey2", "NewValue2");
 
-            //Clear
-            environmentVariables.Clear();
-            Assert.Equal(0, environmentVariables.Count);
-
-            //ContainsKey 
-            environmentVariables.Add("NewKey", "NewValue");
-            environmentVariables.Add("NewKey2", "NewValue2");
-            Assert.True(environmentVariables.ContainsKey("NewKey"));
-            Assert.True(environmentVariables.ContainsKey("NewKey2"));
-            Assert.False(environmentVariables.ContainsKey("NewKey99"));
-
-            //Contains Value
-            Assert.True(environmentVariables.ContainsValue("NewValue"));
-            Assert.True(environmentVariables.ContainsValue("NewValue2"));
-
-            //Iterating
-            string result = null;
-            int index = 0;
-            foreach (string e1 in environmentVariables.Values)
+            string environmentResultKey = "";
+            string environmentResultValue = "";
+            foreach(var entry in psi.Environment)
             {
-                index++;
-                result += e1;
-            }
-            Assert.Equal(2, index);
-            Assert.Equal("NewValueNewValue2", result);
-
-            result = null;
-            index = 0;
-            foreach (string e1 in environmentVariables.Keys)
-            {
-                index++;
-                result += e1;
-            }
-            Assert.Equal("NewKeyNewKey2", result);
-            Assert.Equal(2, index);
-
-            result = null;
-            index = 0;
-            foreach (KeyValuePair<string, string> e1 in environmentVariables)
-            {
-                index++;
-                result += e1.Key;
-            }
-            Assert.Equal("NewKeyNewKey2", result);
-            Assert.Equal(2, index);
-
-            //Exception not thrown with invalid key
-            Assert.Throws<ArgumentNullException>(() => environmentVariables.ContainsKey(null));
-
-            environmentVariables.Add("NewKey98","NewValue98");
-
-            //Indexed
-            string newIndexItem = environmentVariables["NewKey98"];
-            Assert.Equal("NewValue98", newIndexItem);
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                Assert.True(environmentVariables.ContainsKey("NeWkEy"));
-                Assert.Equal("NewValue", environmentVariables["NeWKeY"]);
-                Assert.Throws<ArgumentException>(() => environmentVariables.Add("newkey", "newvalue100"));
+                environmentResultKey += entry.Key;
+                environmentResultValue += entry.Value;
             }
 
-            Assert.False(environmentVariables.ContainsKey("NewKey99"));
+            Assert.Equal("NewKeyNewKey2", environmentResultKey);
+            Assert.Equal("NewValueNewValue2", environmentResultValue);
 
-            //Key not found
-            Assert.Throws<KeyNotFoundException>(() =>
+            string envVarResultKey = "";
+            string envVarResultValue = "";
+            foreach(KeyValuePair<string, string> entry in psi.EnvironmentVariables)
             {
-                string stringout = environmentVariables["NewKey99"];
-            });            
+                envVarResultKey += entry.Key;
+                envVarResultValue += entry.Value;
+            }
 
-            //Exception not thrown with invalid key
-            Assert.Throws<ArgumentNullException>(() =>
-            {
-                string stringout = environmentVariables[null];
-            });
+            Assert.Equal(environmentResultKey, envVarResultKey);
+            Assert.Equal(environmentResultValue, envVarResultValue);
 
-            //Exception not thrown with invalid key
-            Assert.Throws<ArgumentNullException>(() => environmentVariables.Add(null, "NewValue2"));
-
-            //Invalid Key to add
-            Assert.Throws<ArgumentException>(() => environmentVariables.Add("NewKey2", "NewValue2"));
-
-            //Remove Item
-            environmentVariables.Remove("NewKey98");
-
-            //Exception not thrown with null key
-            Assert.Throws<ArgumentNullException>(() => { environmentVariables.Remove(null); });
-
-            //"Exception not thrown with null key"
-            Assert.Throws<KeyNotFoundException>(() => environmentVariables["1bB"]);
-
-            Assert.True(environmentVariables.ContainsKey("NewKey2"));
-            Assert.True(environmentVariables.ContainsValue("NewValue2"));
-
-            Assert.False(environmentVariables.ContainsValue("newvalue2"));
-
-            //Use KeyValuePair Enumerator
-            var x = environmentVariables.GetEnumerator() as IEnumerator<KeyValuePair<string, string>>;
-            x.MoveNext();
-            var y1 = x.Current;
-            Assert.Equal("NewKey NewValue", y1.Key + " " + y1.Value);
-            x.MoveNext();
-            y1 = x.Current;
-            Assert.Equal("NewKey2 NewValue2", y1.Key + " " + y1.Value);
-
-            environmentVariables.Add("NewKey3", "NewValue3");
-            //CopyTo
-            KeyValuePair<string, string>[] kvpa = new KeyValuePair<string, string>[10];
-            environmentVariables.CopyTo(kvpa, 0);
-            Assert.Equal("NewKey", kvpa[0].Key);
+            psi.EnvironmentVariables.Add("NewKey3", "NewValue3");
+            KeyValuePair<string, string>[] kvpa = new KeyValuePair<string, string>[5];
+            psi.Environment.CopyTo(kvpa, 0);
             Assert.Equal("NewKey3", kvpa[2].Key);
             Assert.Equal("NewValue3", kvpa[2].Value);
 
-            string[] kvp = new string[10];
-            Assert.Throws<ArgumentException>(() => { environmentVariables.CopyTo(kvp, 6); });
-            environmentVariables.CopyTo(kvpa, 6);
-            Assert.Equal("NewKey", kvpa[6].Key);
-            Assert.Equal("NewValue", kvpa[6].Value);
-
-            Assert.Throws<ArgumentOutOfRangeException>(() => { environmentVariables.CopyTo(kvpa, -1); });
-
-            Assert.Throws<ArgumentException>(() => { environmentVariables.CopyTo(kvpa, 9); });
-
-            Assert.Throws<ArgumentNullException>(() =>
-            {
-                KeyValuePair<string, string>[] kvpanull = null;
-                environmentVariables.CopyTo(kvpanull, 0);
-            });
+            psi.EnvironmentVariables.Remove("NewKey3");
+            Assert.False(psi.Environment.Contains(new KeyValuePair<string,string>("NewKey3", "NewValue3")));            
         }
 
         [PlatformSpecific(TestPlatforms.Windows)]
