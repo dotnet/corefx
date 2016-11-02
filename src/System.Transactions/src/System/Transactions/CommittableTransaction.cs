5169c2f5b18c149825cd1198cf86715bc1dbc7f3
Jim Carley <jcarley@microsoft.com>
2016-11-02 15:05:11 -0700

    Suppressing FxCop warning CA229 about lack of serialization constructors. (#13276)
    When we support distributed transactions, transaction serialization will actually happen through something like an abstract DistributedTransaction base class.

--- src/System.Transactions/src/System/Transactions/CommittableTransaction.cs
+++ src/System.Transactions/src/System/Transactions/CommittableTransaction.cs
@@ -7,6 +7,7 @@ using System.Transactions.Diagnostics;
 
 namespace System.Transactions
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2229", Justification="Serialization not yet supported and will be done using DistributedTransaction")]
     [Serializable]
     public sealed class CommittableTransaction : Transaction, IAsyncResult
     {
