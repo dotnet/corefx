a5b4c6e172df14c27722608937b0de8c4c98dcab
Eric Eilebrecht <ericeil@microsoft.com>
2016-11-03 10:26:01 -0700

    Rename contract: System.Net.Http.Listener->System.Net.HttpListener

--- src/System.Net.HttpListener/src/System/Net/NetEventSource.HttpListener.cs
+++ src/System.Net.HttpListener/src/System/Net/NetEventSource.HttpListener.cs
@@ -7,7 +7,7 @@ using System.Diagnostics.Tracing;
 namespace System.Net
 {
     //TODO: If localization resources are not found, logging does not work. Issue #5126.
-    [EventSource(Name = "Microsoft-System-Net-HttpListener", LocalizationResources = "FxResources.System.Net.Http.Listener.SR")]
+    [EventSource(Name = "Microsoft-System-Net-HttpListener", LocalizationResources = "FxResources.System.Net.HttpListener.SR")]
     internal sealed partial class NetEventSource
     {
     }
