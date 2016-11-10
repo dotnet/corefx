354cc7002514683d14ec36077fc0626072d47e32
Jeremy Barton <jbarton@microsoft.com>
2016-11-10 12:55:43 -0800

    Rename OpenSslAsymmetricAlgorithmCore to AsymmetricAlgorithmHelpers
    Since the class is helpful to both OpenSsl and Apple SecurityTransforms

--- src/Common/src/System/Security/Cryptography/RSAOpenSsl.cs
+++ src/Common/src/System/Security/Cryptography/RSAOpenSsl.cs
@@ -352,12 +352,12 @@ namespace System.Security.Cryptography
 
         protected override byte[] HashData(byte[] data, int offset, int count, HashAlgorithmName hashAlgorithm)
         {
-            return OpenSslAsymmetricAlgorithmCore.HashData(data, offset, count, hashAlgorithm);
+            return AsymmetricAlgorithmHelpers.HashData(data, offset, count, hashAlgorithm);
         }
 
         protected override byte[] HashData(Stream data, HashAlgorithmName hashAlgorithm)
         {
-            return OpenSslAsymmetricAlgorithmCore.HashData(data, hashAlgorithm);
+            return AsymmetricAlgorithmHelpers.HashData(data, hashAlgorithm);
         }
 
         public override byte[] SignHash(byte[] hash, HashAlgorithmName hashAlgorithm, RSASignaturePadding padding)
