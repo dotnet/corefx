fa1315729ebfa4a28c6931e43be131bce81d4d33
Ian Hays <ianha@microsoft.com>
2016-11-11 11:13:19 -0800

    Expose missing NamedPipeClientStream methods
    Exposes the protected override of CheckPipePropertyOperations as well as the HandleInheritability constructor. Adds a basic argument test for the latter.

---
+++ src/System.IO.Pipes/tests/NamedPipeTests/NamedPipeTest.netstandard17.cs
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Security.Principal;
+using Xunit;
+
+namespace System.IO.Pipes.Tests
+{
+    public class NamedPipeTest_netstandard17 : NamedPipeTestBase
+    {
+        [Fact]
+        public void NamedPipeClientStream_InvalidHandleInerhitability()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>("inheritability", () => new NamedPipeClientStream("a", "b", PipeDirection.Out, 0, TokenImpersonationLevel.Delegation, HandleInheritability.None - 1));
+            Assert.Throws<ArgumentOutOfRangeException>("inheritability", () => new NamedPipeClientStream("a", "b", PipeDirection.Out, 0, TokenImpersonationLevel.Delegation, HandleInheritability.Inheritable + 1));
+        }
+    }
+}
