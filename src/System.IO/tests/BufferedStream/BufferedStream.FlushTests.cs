89c82798429f288e17cb32c8f020d7936c1a7c26
James Ko <jamesqko@gmail.com>
2016-11-12 21:47:01 -0500

    Add more tests to cover the new changes.

--- src/System.IO/tests/BufferedStream/BufferedStream.FlushTests.cs
+++ src/System.IO/tests/BufferedStream/BufferedStream.FlushTests.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace System.IO.Tests
@@ -10,18 +11,19 @@ namespace System.IO.Tests
     public class BufferedStreamFlushTests
     {
         [Fact]
-        public async void ShouldNotFlushUnderlyingStreamIfReadOnly()
+        public async Task ShouldNotFlushUnderlyingStreamIfReadOnly()
         {
             var underlying = new DelegateStream(
                 // These properties are necessary for the codepath in question to be hit.
                 canReadFunc: () => true,
                 canWriteFunc: () => false,
                 canSeekFunc: () => true,
-                readFunc: (buffer, offset, count) => 123,
-                writeFunc: (buffer, offset, count) =>
+                readFunc: (_, __, ___) => 123,
+                writeFunc: (_, __, ___) =>
                 {
                     throw new NotSupportedException();
-                }
+                },
+                seekFunc: (_, __) => 123L
             );
             // These properties are necessary for the codepath in question to be hit.
             Debug.Assert(underlying.CanRead && !underlying.CanWrite && underlying.CanSeek);
@@ -36,6 +38,41 @@ namespace System.IO.Tests
 
             await buffered.FlushAsync();
             Assert.Equal(0, wrapper.TimesCalled(nameof(wrapper.FlushAsync)));
+        }
+
+        [Theory]
+        [InlineData(true, true)]
+        [InlineData(true, false)]
+        [InlineData(false, true)]
+        [InlineData(false, false)]
+        public async Task ShouldAlwaysFlushUnderlyingStreamIfWritable(bool underlyingCanRead, bool underlyingCanSeek)
+        {
+            var underlying = new DelegateStream(
+                canReadFunc: () => underlyingCanRead,
+                canWriteFunc: () => true,
+                canSeekFunc: () => underlyingCanSeek,
+                readFunc: (_, __, ___) => 123,
+                writeFunc: (_, __, ___) => { },
+                seekFunc: (_, __) => 123L
+            );
+
+            var wrapper = new CallTrackingStream(underlying);
+
+            var buffered = new BufferedStream(wrapper);
+            
+            buffered.Flush();
+            Assert.Equal(1, wrapper.TimesCalled(nameof(wrapper.Flush)));
+
+            await buffered.FlushAsync();
+            Assert.Equal(1, wrapper.TimesCalled(nameof(wrapper.FlushAsync)));
+
+            buffered.WriteByte(0);
+            
+            buffered.Flush();
+            Assert.Equal(2, wrapper.TimesCalled(nameof(wrapper.Flush)));
+
+            await buffered.FlushAsync();
+            Assert.Equal(2, wrapper.TimesCalled(nameof(wrapper.FlushAsync)));
         }
     }
 }
