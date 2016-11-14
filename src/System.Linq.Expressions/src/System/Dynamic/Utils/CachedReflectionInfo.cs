acb69131c342cd61bed8f1106d9a7d79c0d0c1d7
Bart De Smet <bartde@microsoft.com>
2016-11-13 16:09:01 -0800

    Refactoring cached reflection info.

---
+++ src/System.Linq.Expressions/src/System/Dynamic/Utils/CachedReflectionInfo.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace System.Linq.Expressions
+{
+    internal static partial class CachedReflectionInfo
+    {
+        private static MethodInfo s_String_Format_String_ObjectArray;
+        public  static MethodInfo   String_Format_String_ObjectArray =>
+                                  s_String_Format_String_ObjectArray ??
+                                 (s_String_Format_String_ObjectArray = typeof(string).GetMethod("Format", new Type[] { typeof(string), typeof(object[]) }));
+
+        private static ConstructorInfo s_InvalidCastException_Ctor_String;
+        public  static ConstructorInfo   InvalidCastException_Ctor_String =>
+                                       s_InvalidCastException_Ctor_String ??
+                                      (s_InvalidCastException_Ctor_String = typeof(InvalidCastException).GetConstructor(new Type[] { typeof(string) }));
+
+        private static MethodInfo s_CallSiteOps_SetNotMatched;
+        public  static MethodInfo   CallSiteOps_SetNotMatched =>
+                                  s_CallSiteOps_SetNotMatched ??
+                                 (s_CallSiteOps_SetNotMatched = typeof(CallSiteOps).GetMethod(nameof(CallSiteOps.SetNotMatched)));
+    }
+}
--- src/System.Linq.Expressions/src/System/Linq/Expressions/Common/CachedReflectionInfo.cs
+++ src/System.Linq.Expressions/src/System/Linq/Expressions/Common/CachedReflectionInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.CompilerServices;
 
 namespace System.Linq.Expressions
 {
-    internal static class CachedReflectionInfo
+    internal static partial class CachedReflectionInfo
     {
         private static Type[] s_ArrayOfType_Bool;
         public  static Type[]   ArrayOfType_Bool =>
@@ -154,20 +154,5 @@ namespace System.Linq.Expressions
         public  static MethodInfo   Math_Pow_Double_Double =>
                                   s_Math_Pow_Double_Double ??
                                  (s_Math_Pow_Double_Double = typeof(Math).GetMethod(nameof(Math.Pow), new[] { typeof(double), typeof(double) }));
-
-        private static MethodInfo s_String_Format_String_ObjectArray;
-        public  static MethodInfo   String_Format_String_ObjectArray =>
-                                  s_String_Format_String_ObjectArray ??
-                                 (s_String_Format_String_ObjectArray = typeof(string).GetMethod("Format", new Type[] { typeof(string), typeof(object[]) }));
-
-        private static ConstructorInfo s_InvalidCastException_Ctor_String;
-        public  static ConstructorInfo   InvalidCastException_Ctor_String =>
-                                       s_InvalidCastException_Ctor_String ??
-                                      (s_InvalidCastException_Ctor_String = typeof(InvalidCastException).GetConstructor(new Type[] { typeof(string) }));
-
-        private static MethodInfo s_CallSiteOps_SetNotMatched;
-        public  static MethodInfo   CallSiteOps_SetNotMatched =>
-                                  s_CallSiteOps_SetNotMatched ??
-                                 (s_CallSiteOps_SetNotMatched = typeof(CallSiteOps).GetMethod(nameof(CallSiteOps.SetNotMatched)));
     }
 }
