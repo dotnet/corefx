3334c945084f6ad328b9ff91987138e20c8aa42b
Atsushi Kanamori <atsushik@microsoft.com>
2016-11-14 09:16:23 -0800

    Follow Ben's suggested ThrowHelper pattern.

--- src/System.Memory/src/System/ThrowHelper.cs
+++ src/System.Memory/src/System/ThrowHelper.cs
@@ -6,43 +6,37 @@ using System.Runtime.CompilerServices;
 
 namespace System
 {
+    //
+    // This pattern of inlinable "void Throw" routines that stack on top of NoInlining factory methods is a compromise between older JITs and newer JITs.
+    // The package this file appears in is targeted specifically at older JITs. Older JITs cannot inline methods that have conditional throws in them,
+    // thus we use these helpers to hide the throws. Newer JIT's will inline the Throw routines and will benefit from the code size reductions of
+    // splitting the creation details into their own non-inlinable factory methods.
+    //
     internal static class ThrowHelper
     {
+        internal static void ThrowArgumentNullException(ExceptionArgument argument) { throw CreateArgumentNullException(argument); }
         [MethodImpl(MethodImplOptions.NoInlining)]
-        internal static Exception CreateThrowArgumentNullException(ExceptionArgument argument)
-        {
-            return new ArgumentNullException(argument.ToString());
-        }
+        private static Exception CreateArgumentNullException(ExceptionArgument argument) { return new ArgumentNullException(argument.ToString()); }
 
+        internal static void ThrowArrayTypeMismatchException_ArrayTypeMustBeExactMatch(Type type) { throw CreateArrayTypeMismatchException_ArrayTypeMustBeExactMatch(type); }
         [MethodImpl(MethodImplOptions.NoInlining)]
-        internal static Exception CreateThrowArrayTypeMismatchException_ArrayTypeMustBeExactMatch(Type type)
-        {
-            return new ArrayTypeMismatchException(SR.Format(SR.ArrayTypeMustBeExactMatch, type));
-        }
+        private static Exception CreateArrayTypeMismatchException_ArrayTypeMustBeExactMatch(Type type) { return new ArrayTypeMismatchException(SR.Format(SR.ArrayTypeMustBeExactMatch, type)); }
 
+        internal static void ThrowArgumentException_InvalidTypeWithPointersNotSupported(Type type) { throw CreateArgumentException_InvalidTypeWithPointersNotSupported(type); }
         [MethodImpl(MethodImplOptions.NoInlining)]
-        internal static Exception CreateThrowArgumentException_InvalidTypeWithPointersNotSupported(Type type)
-        {
-            return new ArgumentException(SR.Format(SR.Argument_InvalidTypeWithPointersNotSupported, type));
-        }
+        private static Exception CreateArgumentException_InvalidTypeWithPointersNotSupported(Type type) { return new ArgumentException(SR.Format(SR.Argument_InvalidTypeWithPointersNotSupported, type)); }
 
+        internal static void ThrowArgumentException_DestinationTooShort() { throw CreateArgumentException_DestinationTooShort(); }
         [MethodImpl(MethodImplOptions.NoInlining)]
-        internal static Exception CreateThrowArgumentException_DestinationTooShort()
-        {
-            return new ArgumentException(SR.Argument_DestinationTooShort);
-        }
+        private static Exception CreateArgumentException_DestinationTooShort() { return new ArgumentException(SR.Argument_DestinationTooShort); }
 
+        internal static void ThrowIndexOutOfRangeException() { throw CreateIndexOutOfRangeException(); }
         [MethodImpl(MethodImplOptions.NoInlining)]
-        internal static Exception CreateThrowIndexOutOfRangeException()
-        {
-            return new IndexOutOfRangeException();
-        }
+        private static Exception CreateIndexOutOfRangeException() { return new IndexOutOfRangeException(); }
 
+        internal static void ThrowArgumentOutOfRangeException(ExceptionArgument argument) { throw CreateArgumentOutOfRangeException(argument); }
         [MethodImpl(MethodImplOptions.NoInlining)]
-        internal static Exception CreateThrowArgumentOutOfRangeException(ExceptionArgument argument)
-        {
-            return new ArgumentOutOfRangeException(argument.ToString());
-        }
+        private static Exception CreateArgumentOutOfRangeException(ExceptionArgument argument) { return new ArgumentOutOfRangeException(argument.ToString()); }
     }
 
     internal enum ExceptionArgument
