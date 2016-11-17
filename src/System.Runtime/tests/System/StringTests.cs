d1dda5c08edb42150abf4af19b602b46808a3d22
Alexander Radchenko <radchenkosasha@gmail.com>
2016-11-17 10:26:40 +0700

    Join(string separator, params object[] values) is fixed
    Join issue for first null in object array is fixed. So calling string.Join(",", null, 1, 2, 3) now return ",1,2,3", but not empty string as before. See https://github.com/dotnet/coreclr/pull/8114

--- src/System.Runtime/tests/System/StringTests.cs
+++ src/System.Runtime/tests/System/StringTests.cs
@@ -1618,8 +1618,8 @@ namespace System.Tests
             yield return new object[] { null, new object[] { "Foo", "Bar", "Baz" }, "FooBarBaz" };
             yield return new object[] { "$$", new object[] { "Foo", null, "Baz" }, "Foo$$$$Baz" };
 
-            // Join does nothing if array[0] is null
-            yield return new object[] { "$$", new object[] { null, "Bar", "Baz" }, "" };
+            // Join is fixed if array[0] is null
+            yield return new object[] { "$$", new object[] { null, "Bar", "Baz" }, "$$Bar$$Baz" };
 
             // Join should ignore objects that have a null ToString() value
             yield return new object[] { "|", new object[] { new ObjectWithNullToString(), "Foo", new ObjectWithNullToString(), "Bar", new ObjectWithNullToString() }, "|Foo||Bar|" };
@@ -1630,10 +1630,9 @@ namespace System.Tests
         public static void Join_ObjectArray(string separator, object[] values, string expected)
         {
             Assert.Equal(expected, string.Join(separator, values));
-            if (!(values.Length > 0 && values[0] == null))
-            {
+            // Join is  fixed if array[0] is null
+            //if (!(values.Length > 0 && values[0] == null))
                 Assert.Equal(expected, string.Join(separator, (IEnumerable<object>)values));
-            }
         }
 
         [Fact]
