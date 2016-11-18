86400ba6d0188f4a7f1f22a3b8a230afafa377a2
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2016-11-17 17:10:15 -0800

    Add IdnMapping to System.Runtime contract (#13779)
    This change also contains some cleanups after have exposed IdnMapping from corelib

--- src/System.Runtime/ref/System.Runtime.cs
+++ src/System.Runtime/ref/System.Runtime.cs
@@ -4336,6 +4336,20 @@ namespace System.Globalization
         public static System.Globalization.CultureInfo ReadOnly(System.Globalization.CultureInfo ci) { throw null; }
         public override string ToString() { throw null; }
     }
+    public sealed partial class IdnMapping
+    {
+        public IdnMapping() { }
+        public bool AllowUnassigned { get { throw null; } set { } }
+        public bool UseStd3AsciiRules { get { throw null; } set { } }
+        public override bool Equals(object obj) { throw null; }
+        public string GetAscii(string unicode) { throw null; }
+        public string GetAscii(string unicode, int index) { throw null; }
+        public string GetAscii(string unicode, int index, int count) { throw null; }
+        public override int GetHashCode() { throw null; }
+        public string GetUnicode(string ascii) { throw null; }
+        public string GetUnicode(string ascii, int index) { throw null; }
+        public string GetUnicode(string ascii, int index, int count) { throw null; }
+    }    
     public partial class CultureNotFoundException : System.ArgumentException, System.Runtime.Serialization.ISerializable
     {
         public CultureNotFoundException() { }
