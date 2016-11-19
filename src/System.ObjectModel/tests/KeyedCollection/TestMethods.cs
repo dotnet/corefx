736106ad9c1bdc3dd776ce00ae318fa4068afbaf
Viacheslav Ivanov <viacheslav.ivanov@gmail.com>
2016-11-19 22:08:53 +0300

    Add tests for KeyedCollection::TryGetValue method
    Related to #4690 Add bool TryGetValue(TKey, out TItem) method to a
    KeyedCollection<,> class

--- src/System.ObjectModel/tests/KeyedCollection/TestMethods.cs
+++ src/System.ObjectModel/tests/KeyedCollection/TestMethods.cs
@@ -1667,6 +1667,57 @@ namespace System.Collections.ObjectModel.Tests
 
         [Theory]
         [MemberData(nameof(ContainsKeyData))]
+        public void TryGetValue(
+            int collectionSize,
+            Named<KeyedCollectionGetKeyedValue<TKey, TValue>>
+                generateKeyedItem)
+        {
+            TKey[] keys;
+            IKeyedItem<TKey, TValue>[] items;
+            IKeyedItem<TKey, TValue>[] itemsWithKeys;
+            var collection =
+                new TestKeyedCollectionOfIKeyedItem<TKey, TValue>();
+            collection.AddItems(
+                generateKeyedItem.Value.Bind(
+                    GenerateValue,
+                    GetKeyForItem),
+                ki => ki.Key,
+                collectionSize,
+                out keys,
+                out items,
+                out itemsWithKeys);
+            IKeyedItem<TKey, TValue> itemNotIn =
+                generateKeyedItem.Value(GenerateValue, GetKeyForItem);
+
+            TKey keyNotIn = itemNotIn.Key;
+            if (keyNotIn == null)
+            {
+                IKeyedItem<TKey, TValue> item;
+                Assert.Throws<ArgumentNullException>(
+                    () => collection.TryGetValue(keyNotIn, out item));
+            }
+            else
+            {
+                IKeyedItem<TKey, TValue> item;
+                Assert.False(collection.TryGetValue(keyNotIn, out item));
+            }
+            foreach (TKey k in keys)
+            {
+                IKeyedItem<TKey, TValue> item;
+                TKey key = k;
+                if (key == null)
+                {
+                    Assert.Throws<ArgumentNullException>(
+                        () => collection.TryGetValue(key, out item));
+                    continue;
+                }
+                Assert.True(collection.TryGetValue(key, out item));
+                Assert.Equal(item.Key, key);
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(ContainsKeyData))]
         public void ContainsKey(
             int collectionSize,
             Named<KeyedCollectionGetKeyedValue<TKey, TValue>>
