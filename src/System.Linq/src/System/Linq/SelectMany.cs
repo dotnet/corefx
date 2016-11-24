4a189f12b21ac67d77214b011774e1873d352490
James Ko <jamesqko@gmail.com>
2016-11-23 22:15:51 -0500

    Optimize common overload of SelectMany.

--- src/System.Linq/src/System/Linq/SelectMany.cs
+++ src/System.Linq/src/System/Linq/SelectMany.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace System.Linq
 {
@@ -20,10 +21,10 @@ namespace System.Linq
                 throw Error.ArgumentNull(nameof(selector));
             }
 
-            return SelectManyIterator(source, selector);
+            return SelectManyIterator1(source, selector);
         }
 
-        private static IEnumerable<TResult> SelectManyIterator<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
+        private static IEnumerable<TResult> SelectManyIterator1<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
         {
             foreach (TSource element in source)
             {
@@ -46,10 +47,10 @@ namespace System.Linq
                 throw Error.ArgumentNull(nameof(selector));
             }
 
-            return SelectManyIterator(source, selector);
+            return SelectManyIterator2(source, selector);
         }
 
-        private static IEnumerable<TResult> SelectManyIterator<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, int, IEnumerable<TResult>> selector)
+        private static IEnumerable<TResult> SelectManyIterator2<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, int, IEnumerable<TResult>> selector)
         {
             int index = -1;
             foreach (TSource element in source)
@@ -83,10 +84,10 @@ namespace System.Linq
                 throw Error.ArgumentNull(nameof(resultSelector));
             }
 
-            return SelectManyIterator(source, collectionSelector, resultSelector);
+            return SelectManyIterator3(source, collectionSelector, resultSelector);
         }
 
-        private static IEnumerable<TResult> SelectManyIterator<TSource, TCollection, TResult>(IEnumerable<TSource> source, Func<TSource, int, IEnumerable<TCollection>> collectionSelector, Func<TSource, TCollection, TResult> resultSelector)
+        private static IEnumerable<TResult> SelectManyIterator3<TSource, TCollection, TResult>(IEnumerable<TSource> source, Func<TSource, int, IEnumerable<TCollection>> collectionSelector, Func<TSource, TCollection, TResult> resultSelector)
         {
             int index = -1;
             foreach (TSource element in source)
@@ -120,10 +121,10 @@ namespace System.Linq
                 throw Error.ArgumentNull(nameof(resultSelector));
             }
 
-            return SelectManyIterator(source, collectionSelector, resultSelector);
+            return SelectManyIterator4(source, collectionSelector, resultSelector);
         }
 
-        private static IEnumerable<TResult> SelectManyIterator<TSource, TCollection, TResult>(IEnumerable<TSource> source, Func<TSource, IEnumerable<TCollection>> collectionSelector, Func<TSource, TCollection, TResult> resultSelector)
+        private static IEnumerable<TResult> SelectManyIterator4<TSource, TCollection, TResult>(IEnumerable<TSource> source, Func<TSource, IEnumerable<TCollection>> collectionSelector, Func<TSource, TCollection, TResult> resultSelector)
         {
             foreach (TSource element in source)
             {
@@ -131,6 +132,122 @@ namespace System.Linq
                 {
                     yield return resultSelector(element, subElement);
                 }
+            }
+        }
+
+        private sealed class SelectManyIterator<TSource, TResult> : Iterator<TResult>, IIListProvider<TResult>
+        {
+            private readonly IEnumerable<TSource> _source;
+            private readonly Func<TSource, IEnumerable<TResult>> _selector;
+            private IEnumerator<TSource> _sourceEnumerator;
+            private IEnumerator<TResult> _subEnumerator;
+
+            internal SelectManyIterator(IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
+            {
+                Debug.Assert(source != null);
+                Debug.Assert(selector != null);
+
+                _source = source;
+                _selector = selector;
+            }
+
+            public override Iterator<TResult> Clone()
+            {
+                return new SelectManyIterator<TSource, TResult>(_source, _selector);
+            }
+
+            public override void Dispose()
+            {
+                if (_sourceEnumerator != null)
+                {
+                    _sourceEnumerator.Dispose();
+                    _sourceEnumerator = null;
+                }
+
+                base.Dispose();
+            }
+
+            public int GetCount(bool onlyIfCheap)
+            {
+                if (onlyIfCheap)
+                {
+                    return -1;
+                }
+
+                int count = 0;
+
+                foreach (TSource element in _source)
+                {
+                    checked
+                    {
+                        count += _selector(element).Count();
+                    }
+                }
+
+                return count;
+            }
+
+            public override bool MoveNext()
+            {
+                switch (_state)
+                {
+                    case 1:
+                        // Retrieve the source enumerator.
+                        _sourceEnumerator = _source.GetEnumerator();
+                        _state = 2;
+                        goto case 2;
+                    case 2:
+                        // Take the next element from the source enumerator.
+                        if (!_sourceEnumerator.MoveNext())
+                        {
+                            break;
+                        }
+
+                        TSource element = _sourceEnumerator.Current;
+
+                        // Project it into a sub-collection and get its enumerator.
+                        _subEnumerator = _selector(element).GetEnumerator();
+                        _state = 3;
+                        goto case 3;
+                    case 3:
+                        // Take the next element from the sub-collection and yield.
+                        if (!_subEnumerator.MoveNext())
+                        {
+                            _subEnumerator.Dispose();
+                            _state = 2;
+                            goto case 2;
+                        }
+
+                        _current = _subEnumerator.Current;
+                        return true;
+                }
+
+                Dispose();
+                return false;
+            }
+
+            public TResult[] ToArray()
+            {
+                var builder = new LargeArrayBuilder<TResult>(initialize: true);
+
+                foreach (TSource element in _source)
+                {
+                    builder.AddRange(_selector(element));
+                }
+
+                return builder.ToArray();
+            }
+
+            public List<TResult> ToList()
+            {
+                var list = new List<TResult>();
+
+                foreach (TSource element in _source)
+                {
+                    list.AddRange(_selector(element));
+                }
+
+                return list;
             }
         }
     }
