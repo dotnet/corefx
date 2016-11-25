7237a1e706ab854f3a0349d5a461696cd578ac91
Bart De Smet <bartde@microsoft.com>
2016-11-25 15:54:07 -0800

    Removing some unused usings.

--- src/System.Linq.Expressions/tests/TestExtensions/InlinePerCompilationTypeAttribute.cs
+++ src/System.Linq.Expressions/tests/TestExtensions/InlinePerCompilationTypeAttribute.cs
@@ -2,10 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
-using Xunit;
 using Xunit.Sdk;
 
 namespace System.Linq.Expressions.Tests
