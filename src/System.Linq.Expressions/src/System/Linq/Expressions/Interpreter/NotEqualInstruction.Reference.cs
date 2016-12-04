3c087a9573f8e3cbe684b28c6ec208e46a9932d7
Bart De Smet <bartde@microsoft.com>
2016-12-04 13:36:34 -0800

    Generating equality instructions.

---
+++ src/System.Linq.Expressions/src/System/Linq/Expressions/Interpreter/NotEqualInstruction.Reference.cs
@@ -0,0 +1,18 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Linq.Expressions.Interpreter
+{
+    internal abstract partial class NotEqualInstruction : Instruction
+    {
+        private sealed class NotEqualReference : NotEqualInstruction
+        {
+            public override int Run(InterpretedFrame frame)
+            {
+                frame.Push(frame.Pop() != frame.Pop());
+                return +1;
+            }
+        }
+    }
+}
