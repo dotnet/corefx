8435039645e8853fbe2563c62b4b9ac4e68fbec8
Alex Perovich <alperovi@microsoft.com>
2016-12-06 16:55:35 -0800

    Enable CA2002:DoNotLockOnObjectsWithWeakIdentity
    This change enables CA2002 and adds baseline files for current failures.

---
+++ src/System.Diagnostics.TextWriterTraceListener/src/FxCopBaseline.cs
@@ -0,0 +1,3 @@
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Microsoft.Reliability", "CA2002:DoNotLockOnObjectsWithWeakIdentity", Scope = "member", Target = "System.Diagnostics.DelimitedListTraceListener.#set_Delimiter(System.String)")]
---
+++ src/System.Private.Uri/src/FxCopBaseline.cs
@@ -0,0 +1,3 @@
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Microsoft.Reliability", "CA2002:DoNotLockOnObjectsWithWeakIdentity", Scope = "member", Target = "System.Uri.#CreateUriInfo(System.Uri+Flags)")]
