ba0f67301f434da0b8ec970a59a186a19d86c262
ntr <ntr@ihfood.dk>
2016-12-10 04:31:30 +0100

    S.M.Unsafe: Add more tests for Clear/Fill for different types.

--- src/System.Memory/tests/Span/Clear.cs
+++ src/System.Memory/tests/Span/Clear.cs
@@ -9,7 +9,7 @@ namespace System.SpanTests
     public static partial class SpanTests
     {
         [Fact]
-        public static void Clear()
+        public static void ClearValueTypeWithoutReferences()
         {
             int[] actual = { 1, 2, 3 };
             int[] expected = { 0, 0, 0 };
@@ -17,6 +17,34 @@ namespace System.SpanTests
             Span<int> span = new Span<int>(actual);
             span.Clear();
             Assert.Equal<int>(expected, actual);
+        }
+
+        [Fact]
+        public static void ClearReferenceType()
+        {
+            string[] actual = { "a", "b", "c" };
+            string[] expected = { null, null, null };
+
+            var span = new Span<string>(actual);
+            span.Clear();
+            Assert.Equal<string>(expected, actual);
+        }
+
+        [Fact]
+        public static void ClearValueTypeWithReferences()
+        {
+            TestValueTypeWithReference[] actual = {
+                new TestValueTypeWithReference() { I = 1, S = "a" },
+                new TestValueTypeWithReference() { I = 2, S = "b" },
+                new TestValueTypeWithReference() { I = 3, S = "c" } };
+            TestValueTypeWithReference[] expected = {
+                default(TestValueTypeWithReference),
+                default(TestValueTypeWithReference),
+                default(TestValueTypeWithReference) };
+
+            var span = new Span<TestValueTypeWithReference>(actual);
+            span.Clear();
+            Assert.Equal<TestValueTypeWithReference>(expected, actual);
         }
     }
 }
--- src/System.Memory/tests/Span/Fill.cs
+++ src/System.Memory/tests/Span/Fill.cs
@@ -9,14 +9,42 @@ namespace System.SpanTests
     public static partial class SpanTests
     {
         [Fact]
-        public static void Fill()
+        public static void FillValueTypeWithoutReferences()
         {
             int[] actual = { 1, 2, 3 };
             int[] expected = { 5, 5, 5 };
 
-            Span<int> span = new Span<int>(actual);
+            var span = new Span<int>(actual);
             span.Fill(5);
             Assert.Equal<int>(expected, actual);
+        }
+
+        [Fact]
+        public static void FillReferenceType()
+        {
+            string[] actual = { "a", "b", "c" };
+            string[] expected = { "d", "d", "d" };
+
+            var span = new Span<string>(actual);
+            span.Fill("d");
+            Assert.Equal<string>(expected, actual);
+        }
+
+        [Fact]
+        public static void FillValueTypeWithReferences()
+        {
+            TestValueTypeWithReference[] actual = {
+                new TestValueTypeWithReference() { I = 1, S = "a" },
+                new TestValueTypeWithReference() { I = 2, S = "b" },
+                new TestValueTypeWithReference() { I = 3, S = "c" } };
+            TestValueTypeWithReference[] expected = {
+                new TestValueTypeWithReference() { I = 5, S = "d" },
+                new TestValueTypeWithReference() { I = 5, S = "d" },
+                new TestValueTypeWithReference() { I = 5, S = "d" } };
+
+            var span = new Span<TestValueTypeWithReference>(actual);
+            span.Fill(new TestValueTypeWithReference() { I = 5, S = "d" });
+            Assert.Equal<TestValueTypeWithReference>(expected, actual);
         }
     }
 }
