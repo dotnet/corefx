4e6b38c195291b8fb55c1bb491762c822c905822
Alexander Radchenko <radchenkosasha@gmail.com>
2016-12-20 13:16:23 +0700

    @justinvp Code review

--- src/System.Runtime.Extensions/tests/System/StringComparer.netcoreapp1.1.cs
+++ src/System.Runtime.Extensions/tests/System/StringComparer.netcoreapp1.1.cs
@@ -11,18 +11,15 @@ namespace System.Tests
 {
     public static partial class MiscStringComparerTests
     {
-        public static object[][] FromComparison_TestData()
-        {
-            return new object[][] {
-            //                  StringComparison                StringComparer
-                new object[] { StringComparison.CurrentCulture, StringComparer.CurrentCulture },
-                new object[] { StringComparison.CurrentCultureIgnoreCase, StringComparer.CurrentCultureIgnoreCase },
-                new object[] { StringComparison.InvariantCulture, StringComparer.InvariantCulture },
-                new object[] { StringComparison.InvariantCultureIgnoreCase, StringComparer.InvariantCultureIgnoreCase },
-                new object[] { StringComparison.Ordinal, StringComparer.Ordinal },
-                new object[] { StringComparison.OrdinalIgnoreCase, StringComparer.OrdinalIgnoreCase },
-            };
-        }
+        public static object[][] FromComparison_TestData = new object[][] {
+            //              StringComparison                StringComparer
+            new object[] { StringComparison.CurrentCulture, StringComparer.CurrentCulture },
+            new object[] { StringComparison.CurrentCultureIgnoreCase, StringComparer.CurrentCultureIgnoreCase },
+            new object[] { StringComparison.InvariantCulture, StringComparer.InvariantCulture },
+            new object[] { StringComparison.InvariantCultureIgnoreCase, StringComparer.InvariantCultureIgnoreCase },
+            new object[] { StringComparison.Ordinal, StringComparer.Ordinal },
+            new object[] { StringComparison.OrdinalIgnoreCase, StringComparer.OrdinalIgnoreCase },
+        };
 
         [Theory]
         [MemberData(nameof(FromComparison_TestData))]
