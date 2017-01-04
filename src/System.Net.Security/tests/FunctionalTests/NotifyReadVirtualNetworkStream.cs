c8f66779e3ce3fa632a16d02bee286fa081214d3
Cesar Blum Silveira <cesarbs@gmail.com>
2017-01-03 17:04:52 -0800

    Fix race condition in SslStream_StreamToStream_WriteAsync_ReadAsync_Pending_Success (#14830)
    * Fix race condition in SslStream_StreamToStream_WriteAsync_ReadAsync_Pending_Success.
    
    * Address PR feedback.

---
+++ src/System.Net.Security/tests/FunctionalTests/NotifyReadVirtualNetworkStream.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Net.Test.Common;
+
+namespace System.Net.Security.Tests
+{
+    internal class NotifyReadVirtualNetworkStream : VirtualNetworkStream
+    {
+        public delegate void ReadEventHandler(byte[] buffer, int offset, int count);
+        public event ReadEventHandler OnRead;
+
+        public NotifyReadVirtualNetworkStream(VirtualNetwork network, bool isServer)
+            : base(network, isServer)
+        {
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            OnRead?.Invoke(buffer, offset, count);
+            return base.Read(buffer, offset, count);
+        }
+    }
+}
