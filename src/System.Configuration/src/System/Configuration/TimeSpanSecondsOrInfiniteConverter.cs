f3dc9715de48d4df5c8abe5e16109ac1b1c1ce60
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2017-01-06 13:21:08 -0800

    Port System.Configuration types from System.dll (#14922)

--- src/System.Configuration/src/System/Configuration/TimeSpanSecondsOrInfiniteConverter.cs
+++ src/System.Configuration/src/System/Configuration/TimeSpanSecondsOrInfiniteConverter.cs
@@ -4,7 +4,6 @@
 
 using System.Globalization;
 using System.ComponentModel;
-using System.Diagnostics;
 
 namespace System.Configuration
 {
@@ -19,8 +18,6 @@ namespace System.Configuration
 
         public override object ConvertFrom(ITypeDescriptorContext ctx, CultureInfo ci, object data)
         {
-            Debug.Assert(data is string, "data is string");
-
             return (string)data == "Infinite" ? TimeSpan.MaxValue : base.ConvertFrom(ctx, ci, data);
         }
     }
