a855345ce9eab6c8f4636388bde6c6e7de1dead2
Marek Safar <marek.safar@gmail.com>
2017-01-06 14:58:23 +0100

    [System.Xml.Private] Extracts XmlSchemaConstants
    - Moved to new file
    - Removed unsed constant
    - Moved to assembly namespace

--- src/System.Private.Xml/src/System/Xml/Schema/XmlSchema.cs
+++ src/System.Private.Xml/src/System/Xml/Schema/XmlSchema.cs
@@ -2,16 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace System.ServiceModel.Dispatcher
-{
-    using XmlSchema = XmlSchemaConstants;
-    internal static class XmlSchemaConstants
-    {
-        public const string Namespace = "http://www.w3.org/2001/XMLSchema";
-        public const string InstanceNamespace = "http://www.w3.org/2001/XMLSchema-instance";
-    }
-}
-
 namespace System.Xml.Schema
 {
 #if SILVERLIGHT
