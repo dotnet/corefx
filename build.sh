609eedb2925a2401d8f80b637e12b1490ab973ef
Jose Perez Rodriguez <joperezr@microsoft.com>
2017-01-06 15:00:34 -0800

    Build working on non-Windows after merging master

--- build.sh
+++ build.sh
@@ -23,57 +23,10 @@ fi
 
 __scriptpath=$(cd "$(dirname "$0")"; pwd -P)
 
-__ServerGC=0
-__IsCentos=0
-__IsOSX=0
-
-while :; do
-    if [ $# -le 0 ]; then
-        break
-    fi
-    
-    if [[ $1 == *"centos.7"* ]] ; then
-       __IsCentos=1
-    fi
-    
-    if [[ $1 == *"osx.10"* ]] ; then
-       __IsOSX=1
-    fi
-
-    lowerI="$(echo $1 | awk '{print tolower($0)}')"
-    case $lowerI in
-        useservergc)
-            __ServerGC=1
-            ;;
-        *)
-    __UnprocessedBuildArgs="$__UnprocessedBuildArgs $1"
-    esac
-
-    shift
-done
-
-export CORECLR_SERVER_GC="$__ServerGC"
-
-"$__scriptpath/build-native.sh" $__UnprocessedBuildArgs
+"$__scriptpath/build-native.sh" $*
 if [ $? -ne 0 ];then
    exit 1
 fi
 
-if [ $__IsCentos -eq 1 ] ; then
-   "$__scriptpath/build-packages.sh" -DisableManagedPackage -FilterToOSGroup=rhel.7 $__UnprocessedBuildArgs
-elif [ $__IsOSX -eq 1 ] ; then
-   "$__scriptpath/build-packages.sh" -DisableManagedPackage -FilterToOSGroup=osx.10 $__UnprocessedBuildArgs
-else
-   "$__scriptpath/build-packages.sh" -DisableManagedPackage $__UnprocessedBuildArgs
-fi
-if [ $? -ne 0 ];then
-   exit 1
-fi
-
-"$__scriptpath/build-managed.sh" $__UnprocessedBuildArgs
-if [ $? -ne 0 ];then
-   exit 1
-fi
-
-"$__scriptpath/build-tests.sh" $__UnprocessedBuildArgs
+"$__scriptpath/build-managed.sh" $*
 exit $?
