9a3e44c2e031e5ad7bdb463a4191196241269e6c
Jay Simmons <boatboy22@hotmail.com>
2017-01-18 06:41:12 -0800

    Initial set of changes to get System.DirectoryServices.Protocols and System.DirectoryServices.AccountManagement building.

--- src/System.DirectoryServices.AccountManagement/src/System/DirectoryServices/AccountManagement/Context.cs
+++ src/System.DirectoryServices.AccountManagement/src/System/DirectoryServices/AccountManagement/Context.cs
@@ -102,8 +102,9 @@ namespace System.DirectoryServices.AccountManagement
 
             try
             {
-                if (Thread.CurrentThread.GetApartmentState() == ApartmentState.Unknown)
-                    Thread.CurrentThread.SetApartmentState(ApartmentState.MTA);
+                // jsimmons - I presumeno COM apartments in .NET Core 
+                //if (Thread.CurrentThread.GetApartmentState() == ApartmentState.Unknown)
+                //    Thread.CurrentThread.SetApartmentState(ApartmentState.MTA);
                 // We need the credentials to be in the form <machine>\\<user>
                 // if they just passed user then append the machine name here.
                 if (null != userName)
