049295ae2f07363e76ad5eb43de2ee265cf90f89
michael <the.dwyer@gmail.com>
2017-01-21 15:30:00 -0800

    Improve code coverage for System.IO.FileSystem.AccessControl

--- src/System.IO.FileSystem.AccessControl/tests/FileSystemAclExtensionsTests.cs
+++ src/System.IO.FileSystem.AccessControl/tests/FileSystemAclExtensionsTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Security.AccessControl;
 using Xunit;
 
 namespace System.IO
@@ -9,9 +10,51 @@ namespace System.IO
     public class FileSystemAclExtensionsTests
     {
         [Fact]
-        public void GetAccessControl_InvalidArguments()
+        public void GetAccessControl_DirectoryInfo_InvalidArguments()
         {
             Assert.Throws<NullReferenceException>(() => FileSystemAclExtensions.GetAccessControl((DirectoryInfo)null));
+        }
+
+        [Fact]
+        public void GetAccessControl_DirectoryInfo_AccessControlSections_InvalidArguments()
+        {
+            Assert.Throws<NullReferenceException>(() => FileSystemAclExtensions.GetAccessControl((DirectoryInfo)null, new AccessControlSections()));
+        }
+
+        [Fact]
+        public void GetAccessControl_FileInfo_InvalidArguments()
+        {
+            Assert.Throws<NullReferenceException>(() => FileSystemAclExtensions.GetAccessControl((FileInfo)null));
+        }
+
+        [Fact]
+        public void GetAccessControl_FileInfo_AccessControlSections_InvalidArguments()
+        {
+            Assert.Throws<NullReferenceException>(() => FileSystemAclExtensions.GetAccessControl((FileInfo)null, new AccessControlSections()));
+        }
+
+        [Fact]
+        public void GetAccessControl_Filestream_InvalidArguments()
+        {
+            Assert.Throws<NullReferenceException>(() => FileSystemAclExtensions.GetAccessControl((FileStream)null));
+        }
+
+        [Fact]
+        public void SetAccessControl_DirectoryInfo_DirectorySecurity_InvalidArguments()
+        {
+            Assert.Throws<ArgumentNullException>(() => FileSystemAclExtensions.SetAccessControl((DirectoryInfo)null, (DirectorySecurity)null));
+        }
+
+        [Fact]
+        public void SetAccessControl_FileInfo_FileSecurity_InvalidArguments()
+        {
+            Assert.Throws<ArgumentNullException>(() => FileSystemAclExtensions.SetAccessControl((FileInfo)null, (FileSecurity)null));
+        }
+
+        [Fact]
+        public void SetAccessControl_FileStream_FileSecurity_InvalidArguments()
+        {
+            Assert.Throws<NullReferenceException>(() => FileSystemAclExtensions.SetAccessControl((FileStream)null, (FileSecurity)null));
         }
     }
 }
