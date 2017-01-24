47a3d9f0a6a48770ec9567d11458df25c52e674f
ntr <ntr@ihfood.dk>
2017-01-24 20:18:30 +0100

    Handle OutOfMemoryException in longer-than-uint-MaxValue Clear tests by catching the exception and skipping.

--- src/System.Memory/tests/Span/Clear.cs
+++ src/System.Memory/tests/Span/Clear.cs
@@ -198,31 +198,39 @@ namespace System.SpanTests
         {
             if (sizeof(IntPtr) == sizeof(long))
             {
-                // The maximum index in any single dimension is 2,147,483,591 (0x7FFFFFC7) 
-                // for byte arrays and arrays of single-byte structures, 
-                // and 2,146,435,071 (0X7FEFFFFF) for other types.
-                const int maxArraySizeForLargerThanByteTypes = 0X7FEFFFFF;
-                var a = new int[maxArraySizeForLargerThanByteTypes];
-
-                int initial = 5;
-                for (int i = 0; i < a.Length; i++)
+                try
                 {
-                    a[i] = initial;
-                }
+                    // The maximum index in any single dimension is 2,147,483,591 (0x7FFFFFC7) 
+                    // for byte arrays and arrays of single-byte structures, 
+                    // and 2,146,435,071 (0X7FEFFFFF) for other types.
+                    const int maxArraySizeForLargerThanByteTypes = 0X7FEFFFFF;
+                    var a = new int[maxArraySizeForLargerThanByteTypes];
 
-                var span = new Span<int>(a);
-
-                // Act
-                span.Clear();
-
-                // Assert using custom code for perf and to avoid allocating extra memory
-                for (int i = 0; i < a.Length; i++)
-                {
-                    var actual = a[i];
-                    if (actual != 0)
+                    int initial = 5;
+                    for (int i = 0; i < a.Length; i++)
                     {
-                        Assert.Equal(0, actual);
+                        a[i] = initial;
                     }
+
+                    var span = new Span<int>(a);
+
+                    // Act
+                    span.Clear();
+
+                    // Assert using custom code for perf and to avoid allocating extra memory
+                    for (int i = 0; i < a.Length; i++)
+                    {
+                        var actual = a[i];
+                        if (actual != 0)
+                        {
+                            Assert.Equal(0, actual);
+                        }
+                    }
+                }
+                // Skipping test if Out-of-Memory, since this test can only be run, if there is enough memory
+                catch (OutOfMemoryException)
+                {
+                    Console.WriteLine($"Span.Clear test {nameof(ClearLongerThanUintMaxValueBytes)} skipped due to {nameof(OutOfMemoryException)}.");
                 }
             }
         }
@@ -233,37 +241,45 @@ namespace System.SpanTests
         {
             if (sizeof(IntPtr) == sizeof(long))
             {
-                // Arrange
-                IntPtr bytes = (IntPtr)(((long)int.MaxValue) * sizeof(int));
-                int length = (int)(((long)bytes) / sizeof(int));
-
-                var ptr = (int*)Runtime.InteropServices.Marshal.AllocHGlobal(bytes);
                 try
                 {
-                    int initial = 5;
-                    for (int i = 0; i < length; i++)
+                    // Arrange
+                    IntPtr bytes = (IntPtr)(((long)int.MaxValue) * sizeof(int));
+                    int length = (int)(((long)bytes) / sizeof(int));
+
+                    var ptr = (int*)Runtime.InteropServices.Marshal.AllocHGlobal(bytes);
+                    try
                     {
-                        *(ptr + i) = initial;
-                    }
-
-                    var span = new Span<int>(ptr, length);
-
-                    // Act
-                    span.Clear();
-
-                    // Assert using custom code for perf and to avoid allocating extra memory
-                    for (int i = 0; i < length; i++)
-                    {
-                        var actual = *(ptr + i);
-                        if (actual != 0)
+                        int initial = 5;
+                        for (int i = 0; i < length; i++)
                         {
-                            Assert.Equal(0, actual);
+                            *(ptr + i) = initial;
+                        }
+
+                        var span = new Span<int>(ptr, length);
+
+                        // Act
+                        span.Clear();
+
+                        // Assert using custom code for perf and to avoid allocating extra memory
+                        for (int i = 0; i < length; i++)
+                        {
+                            var actual = *(ptr + i);
+                            if (actual != 0)
+                            {
+                                Assert.Equal(0, actual);
+                            }
                         }
                     }
+                    finally
+                    {
+                        Runtime.InteropServices.Marshal.FreeHGlobal(new IntPtr(ptr));
+                    }
                 }
-                finally
+                // Skipping test if Out-of-Memory, since this test can only be run, if there is enough memory
+                catch (OutOfMemoryException)
                 {
-                    Runtime.InteropServices.Marshal.FreeHGlobal(new IntPtr(ptr));
+                    Console.WriteLine($"Span.Clear test {nameof(ClearNativeLongerThanUintMaxValueBytes)} skipped due to {nameof(OutOfMemoryException)}.");
                 }
             }
         }
