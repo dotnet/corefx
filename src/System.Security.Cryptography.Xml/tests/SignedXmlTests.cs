24864b3d25e665b6305f116890328527db07f1e1
Adam Friedman <tintoy@tintoy.io>
2017-01-27 08:49:59 +1100

    Add first test for SignedXml to prove that we can :-)

---
+++ src/System.Security.Cryptography.Xml/tests/SignedXmlTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace System.Security.Cryptography.Xml.Tests
+{
+    public class SignedXmlTests
+    {
+        [Fact]
+        public void Constructor_Document_Null()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new SignedXml(document: null)
+            );
+        }
+    }
+}
