7911498d70a7a07f0e5ebe95dedf7c03bb53648a
Anthony Langsworth <anthony.langsworth@gmail.com>
2017-01-28 13:23:22 +1100

    First test

--- src/System.Security.Cryptography.Xml/tests/SignedXmlTests.cs
+++ src/System.Security.Cryptography.Xml/tests/SignedXmlTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System.Xml;
+using Xunit;
 
 namespace System.Security.Cryptography.Xml.Tests
 {
@@ -8,7 +9,15 @@ namespace System.Security.Cryptography.Xml.Tests
         public void Constructor_Document_Null()
         {
             Assert.Throws<ArgumentNullException>(
-                () => new SignedXml(document: null)
+                () => new SignedXml((XmlDocument) null)
+            );
+        }
+
+        [Fact]
+        public void Constructor_XmlElement_Null()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () => new SignedXml((XmlElement) null)
             );
         }
     }
