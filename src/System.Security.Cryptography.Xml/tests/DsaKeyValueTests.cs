6ba58f0e50ad95da09b6f27de38b64630677151a
Anthony Langsworth <anthony.langsworth@gmail.com>
2017-01-29 13:39:22 +1100

    Add DsaKeyValueTest skeleton and initial test plan

---
+++ src/System.Security.Cryptography.Xml/tests/DsaKeyValueTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace System.Security.Cryptography.Xml.Tests
+{
+    public class DsaKeyValueTests
+    {
+        // TODO: Expand
+
+        [Fact]
+        public void Constructor_Emtpy()
+        {
+            DSAKeyValue dsaKeyValue = new DSAKeyValue();
+        }
+    }
+}
+    
