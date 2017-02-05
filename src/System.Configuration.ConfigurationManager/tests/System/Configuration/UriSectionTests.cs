2179e1f9388f713cdec1a1476c7db015e540b16e
Maltsev Pavel <fire.alkazar@gmail.com>
2017-02-06 02:47:53 +0300

    Add tests for System.Configuration.UriSection

---
+++ src/System.Configuration.ConfigurationManager/tests/System/Configuration/UriSectionTests.cs
@@ -0,0 +1,53 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Configuration;
+using Xunit;
+
+namespace System.ConfigurationTests
+{
+    public class UriSectionTests
+    {
+        public static string UriSectionConfiguration =
+@"<?xml version='1.0' encoding='utf-8' ?>
+<configuration>
+    <configSections>
+        <section name='uri' type='System.Configuration.UriSection, System' />
+    </configSections>
+    <uri>
+        <idn enabled='All' />
+        <iriParsing enabled='true' />
+        <schemeSettings>
+            <add name='ftp' genericUriParserOptions='DontCompressPath' />
+        </schemeSettings>
+    </uri>
+</configuration>";
+
+        [Fact]
+        public void UriSectionIdnIriParsing()
+        {
+            using (var temp = new TempConfig(UriSectionConfiguration))
+            {
+                var config = ConfigurationManager.OpenExeConfiguration(temp.ExePath);
+                UriSection uriSection = (UriSection)config.GetSection("uri");
+                Assert.Equal(UriIdnScope.All, uriSection.Idn.Enabled);
+                Assert.Equal(true, uriSection.IriParsing.Enabled);
+            }
+        }
+
+        [Fact]
+        public void UriSectionSchemeSettings()
+        {
+            using (var temp = new TempConfig(UriSectionConfiguration))
+            {
+                var config = ConfigurationManager.OpenExeConfiguration(temp.ExePath);
+                UriSection uriSection = (UriSection)config.GetSection("uri");
+                Assert.Equal(1, uriSection.SchemeSettings.Count);
+                SchemeSettingElement schemeSettingElement = uriSection.SchemeSettings[0];
+                Assert.Equal("ftp", schemeSettingElement.Name);
+                Assert.Equal(GenericUriParserOptions.DontCompressPath, schemeSettingElement.GenericUriParserOptions);
+            }
+        }
+    }
+}
