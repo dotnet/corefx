0dfd68864ddbf535e238a6ade3aa5667808afe70
Anthony Langsworth <anthony.langsworth@gmail.com>
2017-02-05 17:44:29 +1100

    Remove middle line from license header as per instructions

--- src/System.Security.Cryptography.Xml/tests/KeyInfoNodeTest.cs
+++ src/System.Security.Cryptography.Xml/tests/KeyInfoNodeTest.cs
@@ -7,7 +7,6 @@
 // (C) 2002, 2003 Motus Technologies Inc. (http://www.motus.com)
 //
 // Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
 using System.Xml;
--- src/System.Security.Cryptography.Xml/tests/KeyInfoRetrievalMethodTest.cs
+++ src/System.Security.Cryptography.Xml/tests/KeyInfoRetrievalMethodTest.cs
@@ -7,6 +7,8 @@
 // (C) 2002, 2003 Motus Technologies Inc. (http://www.motus.com)
 // Copyright (C) 2005 Novell, Inc (http://www.novell.com)
 //
+// Licensed to the .NET Foundation under one or more agreements.
+// See the LICENSE file in the project root for more information.
 
 using System.Xml;
 using Xunit;
