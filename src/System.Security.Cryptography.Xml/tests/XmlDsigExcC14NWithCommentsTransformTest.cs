b1be7213ce547838f89614e5d6683471fc1b4ec6
Anthony Langsworth <anthony.langsworth@gmail.com>
2017-02-05 21:38:49 +1100

    Convert XmlDsigExcC14NWithCommentsTransformTest

--- src/System.Security.Cryptography.Xml/tests/XmlDsigExcC14NWithCommentsTransformTest.cs
+++ src/System.Security.Cryptography.Xml/tests/XmlDsigExcC14NWithCommentsTransformTest.cs
@@ -13,124 +13,125 @@
 // (C) 2004 Novell (http://www.novell.com)
 // (C) 2008 Gert Driesen
 //
-
+// Licensed to the .NET Foundation under one or more agreements.
+// See the LICENSE file in the project root for more information.
 
 using System.IO;
 using System.Xml;
+using Xunit;
 
-namespace System.Security.Cryptography.Xml.Tests {
-	public class UnprotectedXmlDsigExcC14NWithCommentsTransform : XmlDsigExcC14NWithCommentsTransform {
-		public UnprotectedXmlDsigExcC14NWithCommentsTransform ()
-		{
-		}
+namespace System.Security.Cryptography.Xml.Tests
+{
+    public class UnprotectedXmlDsigExcC14NWithCommentsTransform : XmlDsigExcC14NWithCommentsTransform
+    {
+        public UnprotectedXmlDsigExcC14NWithCommentsTransform()
+        {
+        }
 
-		public UnprotectedXmlDsigExcC14NWithCommentsTransform (string inclusiveNamespacesPrefixList)
-			: base (inclusiveNamespacesPrefixList)
-		{
-		}
+        public UnprotectedXmlDsigExcC14NWithCommentsTransform(string inclusiveNamespacesPrefixList)
+            : base(inclusiveNamespacesPrefixList)
+        {
+        }
 
-		public XmlNodeList UnprotectedGetInnerXml ()
-		{
-			return base.GetInnerXml ();
-		}
-	}
+        public XmlNodeList UnprotectedGetInnerXml()
+        {
+            return base.GetInnerXml();
+        }
+    }
 
-	[TestFixture]
-	public class XmlDsigExcC14NWithCommentsTransformTest {
-		private UnprotectedXmlDsigExcC14NWithCommentsTransform transform;
+    public class XmlDsigExcC14NWithCommentsTransformTest
+    {
+        private UnprotectedXmlDsigExcC14NWithCommentsTransform transform;
 
-		[SetUp]
-		public void SetUp ()
-		{
-			transform = new UnprotectedXmlDsigExcC14NWithCommentsTransform ();
-		}
+        public XmlDsigExcC14NWithCommentsTransformTest()
+        {
+            transform = new UnprotectedXmlDsigExcC14NWithCommentsTransform();
+        }
 
-		[Test] // ctor ()
-		public void Constructor1 ()
-		{
-			CheckProperties (transform);
-			Assert.IsNull (transform.InclusiveNamespacesPrefixList);
-		}
+        [Fact] // ctor ()
+        public void Constructor1()
+        {
+            CheckProperties(transform);
+            Assert.Null(transform.InclusiveNamespacesPrefixList);
+        }
 
-		[Test] // ctor (Boolean)
-		public void Constructor2 ()
-		{
-			transform = new UnprotectedXmlDsigExcC14NWithCommentsTransform (null);
-			CheckProperties (transform);
-			Assert.IsNull (transform.InclusiveNamespacesPrefixList);
+        [Fact] // ctor (Boolean)
+        public void Constructor2()
+        {
+            transform = new UnprotectedXmlDsigExcC14NWithCommentsTransform(null);
+            CheckProperties(transform);
+            Assert.Null(transform.InclusiveNamespacesPrefixList);
 
-			transform = new UnprotectedXmlDsigExcC14NWithCommentsTransform (string.Empty);
-			CheckProperties (transform);
-			Assert.AreEqual (string.Empty, transform.InclusiveNamespacesPrefixList);
+            transform = new UnprotectedXmlDsigExcC14NWithCommentsTransform(string.Empty);
+            CheckProperties(transform);
+            Assert.Equal(string.Empty, transform.InclusiveNamespacesPrefixList);
 
-			transform = new UnprotectedXmlDsigExcC14NWithCommentsTransform ("#default xsd");
-			CheckProperties (transform);
-			Assert.AreEqual ("#default xsd", transform.InclusiveNamespacesPrefixList);
-		}
+            transform = new UnprotectedXmlDsigExcC14NWithCommentsTransform("#default xsd");
+            CheckProperties(transform);
+            Assert.Equal("#default xsd", transform.InclusiveNamespacesPrefixList);
+        }
 
-		void CheckProperties (XmlDsigExcC14NWithCommentsTransform transform)
-		{
-			Assert.AreEqual ("http://www.w3.org/2001/10/xml-exc-c14n#WithComments",
-				transform.Algorithm, "Algorithm");
+        void CheckProperties(XmlDsigExcC14NWithCommentsTransform transform)
+        {
+            Assert.Equal("http://www.w3.org/2001/10/xml-exc-c14n#WithComments", transform.Algorithm);
 
-			Type[] input = transform.InputTypes;
-			Assert.AreEqual (3, input.Length, "Input #");
-			// check presence of every supported input types
-			bool istream = false;
-			bool ixmldoc = false;
-			bool ixmlnl = false;
-			foreach (Type t in input) {
-				if (t == typeof (Stream))
-					istream = true;
-				if (t == typeof (XmlDocument))
-					ixmldoc = true;
-				if (t == typeof (XmlNodeList))
-					ixmlnl = true;
-			}
-			Assert.IsTrue (istream, "Input Stream");
-			Assert.IsTrue (ixmldoc, "Input XmlDocument");
-			Assert.IsTrue (ixmlnl, "Input XmlNodeList");
+            Type[] input = transform.InputTypes;
+            Assert.Equal(3, input.Length);
+            // check presence of every supported input types
+            bool istream = false;
+            bool ixmldoc = false;
+            bool ixmlnl = false;
+            foreach (Type t in input)
+            {
+                if (t == typeof(Stream))
+                    istream = true;
+                if (t == typeof(XmlDocument))
+                    ixmldoc = true;
+                if (t == typeof(XmlNodeList))
+                    ixmlnl = true;
+            }
+            Assert.True(istream, "Input Stream");
+            Assert.True(ixmldoc, "Input XmlDocument");
+            Assert.True(ixmlnl, "Input XmlNodeList");
 
-			Type[] output = transform.OutputTypes;
-			Assert.AreEqual (1, output.Length, "Output #");
-			Assert.AreEqual (typeof (Stream), output [0], "Output Type");
-		}
+            Type[] output = transform.OutputTypes;
+            Assert.Equal(1, output.Length);
+            Assert.Equal(typeof(Stream), output[0]);
+        }
 
-		[Test]
-		public void InputTypes ()
-		{
-			Type [] input = transform.InputTypes;
-			input [0] = null;
-			input [1] = null;
-			input [2] = null;
-			// property does not return a clone
-			foreach (Type t in transform.InputTypes)
-				Assert.IsNull (t);
+        [Fact]
+        public void InputTypes()
+        {
+            Type[] input = transform.InputTypes;
+            input[0] = null;
+            input[1] = null;
+            input[2] = null;
+            // property does not return a clone
+            Assert.All(transform.InputTypes, Assert.NotNull);
 
-			// it's not a static array
-			transform = new UnprotectedXmlDsigExcC14NWithCommentsTransform ();
-			foreach (Type t in transform.InputTypes)
-				Assert.IsNotNull (t);
-		}
+            // it's not a static array
+            transform = new UnprotectedXmlDsigExcC14NWithCommentsTransform();
+            Assert.All(transform.InputTypes, Assert.NotNull);
+        }
 
-		[Test]
-		public void GetInnerXml ()
-		{
-			XmlNodeList xnl = transform.UnprotectedGetInnerXml ();
-			Assert.IsNull (xnl, "Default InnerXml");
-		}
+        [Fact]
+        public void GetInnerXml()
+        {
+            XmlNodeList xnl = transform.UnprotectedGetInnerXml();
+            Assert.Null(xnl);
+        }
 
-		[Test]
-		public void OutputTypes ()
-		{
-			// property does not return a clone
-			transform.OutputTypes [0] = null;
-			Assert.IsNull (transform.OutputTypes [0]);
+        [Fact]
+        public void OutputTypes()
+        {
+            // property does not return a clone
+            transform.OutputTypes[0] = null;
+            Assert.Null(transform.OutputTypes[0]);
 
-			// it's not a static array
-			transform = new UnprotectedXmlDsigExcC14NWithCommentsTransform ();
-			Assert.IsNotNull (transform.OutputTypes [0]);
-		}
-	}
+            // it's not a static array
+            transform = new UnprotectedXmlDsigExcC14NWithCommentsTransform();
+            Assert.NotNull(transform.OutputTypes[0]);
+        }
+    }
 }
 
