c60f89643b97484fbf4adb02de31ee88e1baee62
darxis <nowikowski22@hotmail.com>
2017-02-06 20:17:13 +0100

    Make Microsoft.Win32.Registry.AccessControl assembly a partial facade unconditionally. Removed type forwards

--- src/Microsoft.Win32.Registry.AccessControl/src/Microsoft.Win32.Registry.AccessControl.TypeForwards.cs
+++
@@ -1,7 +0,0 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Security.AccessControl.RegistryAccessRule))]
-[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Security.AccessControl.RegistryAuditRule))]
-[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Security.AccessControl.RegistrySecurity))]
