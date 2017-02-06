fe756fa5d4809eff8f8abd7b256bc064b9318ced
darxis <nowikowski22@hotmail.com>
2017-02-06 19:28:26 +0100

    Removed duplicate RegistryAclExtensions

--- src/Microsoft.Win32.Registry.AccessControl/src/Microsoft/Win32/RegistryAclExtensions.cs
+++ src/Microsoft.Win32.Registry.AccessControl/src/Microsoft/Win32/RegistryAclExtensions.cs
@@ -2,26 +2,23 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Security;
 using System.Security.AccessControl;
 
 namespace Microsoft.Win32
 {
     public static class RegistryAclExtensions
     {
-        public static RegistrySecurity GetAccessControl(this RegistryKey key)
+        public static RegistrySecurity GetAccessControl(RegistryKey key)
         {
             return key.GetAccessControl();
         }
 
-        [SecuritySafeCritical]
-        public static RegistrySecurity GetAccessControl(this RegistryKey key, AccessControlSections includeSections)
+        public static RegistrySecurity GetAccessControl(RegistryKey key, AccessControlSections includeSections)
         {
             return key.GetAccessControl(includeSections);
         }
 
-        [SecuritySafeCritical]
-        public static void SetAccessControl(this RegistryKey key, RegistrySecurity registrySecurity)
+        public static void SetAccessControl(RegistryKey key, RegistrySecurity registrySecurity)
         {
             key.SetAccessControl(registrySecurity);
         }
