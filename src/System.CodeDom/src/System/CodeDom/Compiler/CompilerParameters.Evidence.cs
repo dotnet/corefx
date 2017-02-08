dotnet/corefx

commit a929f58f5d98f6800383f97804eb30ef62343eab

Remove System.Security.Permissions dependency from System.CodeDom

src/System.CodeDom/src/System/CodeDom/Compiler/CompilerParameters.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Specialized;
-using System.Security.Policy;
 
 namespace System.CodeDom.Compiler
 {
@@ -16,8 +15,6 @@ namespace System.CodeDom.Compiler
 
         [NonSerialized]
         private TempFileCollection _tempFiles;
-        [NonSerialized]
-        private Evidence _evidence;
 
         public CompilerParameters() : this(null, null)
         {
@@ -90,21 +87,5 @@ namespace System.CodeDom.Compiler
         public StringCollection LinkedResources => _linkedResources;
 
         public IntPtr UserToken { get; set; }
-
-        [Obsolete("CAS policy is obsolete and will be removed in a future release of the .NET Framework."
-                + " Please see http://go2.microsoft.com/fwlink/?LinkId=131738 for more information.")]
-        public Evidence Evidence
-        {
-            get
-            {
-                Evidence e = null;
-                if (_evidence != null)
-                {
-                    e = _evidence.Clone();
-                }
-                return e;
-            }
-            set { _evidence = value?.Clone(); }
-        }
     }
 }

src/System.CodeDom/src/System/CodeDom/Compiler/CompilerResults.cs
@@ -4,7 +4,6 @@
 
 using System.Collections.Specialized;
 using System.Reflection;
-using System.Security.Policy;
 
 namespace System.CodeDom.Compiler
 {
@@ -14,7 +13,6 @@ namespace System.CodeDom.Compiler
         private readonly CompilerErrorCollection _errors = new CompilerErrorCollection();
         private readonly StringCollection _output = new StringCollection();
         private Assembly _compiledAssembly;
-        private Evidence _evidence;
 
         public CompilerResults(TempFileCollection tempFiles)
         {
@@ -22,21 +20,6 @@ namespace System.CodeDom.Compiler
         }
 
         public TempFileCollection TempFiles { get; set; }
-
-        [Obsolete("CAS policy is obsolete and will be removed in a future release of the .NET Framework. Please see http://go2.microsoft.com/fwlink/?LinkId=131738 for more information.")]
-        public Evidence Evidence
-        {
-            get
-            {
-                Evidence e = null;
-                if (_evidence != null)
-                {
-                    e = _evidence.Clone();
-                }
-                return e;
-            }
-            set { _evidence = value?.Clone(); }
-        }
 
         public Assembly CompiledAssembly
         {

src/System.CodeDom/ref/System.CodeDom.cs
@@ -1105,8 +1105,6 @@ namespace System.CodeDom.Compiler
         public string CompilerOptions { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
         public string CoreAssemblyFileName { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
         public System.Collections.Specialized.StringCollection EmbeddedResources { get { throw null; } }
-        [System.ObsoleteAttribute("CAS policy is obsolete and will be removed in a future release of the .NET Framework. Please see http://go2.microsoft.com/fwlink/?LinkId=131738 for more information.")]
-        public System.Security.Policy.Evidence Evidence { get { throw null; } set { } }
         public bool GenerateExecutable { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
         public bool GenerateInMemory { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
         public bool IncludeDebugInformation { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
@@ -1125,8 +1123,6 @@ namespace System.CodeDom.Compiler
         public CompilerResults(System.CodeDom.Compiler.TempFileCollection tempFiles) { }
         public System.Reflection.Assembly CompiledAssembly { get { throw null; } set { } }
         public System.CodeDom.Compiler.CompilerErrorCollection Errors { get { throw null; } }
-        [System.ObsoleteAttribute("CAS policy is obsolete and will be removed in a future release of the .NET Framework. Please see http://go2.microsoft.com/fwlink/?LinkId=131738 for more information.")]
-        public System.Security.Policy.Evidence Evidence { get { throw null; } set { } }
         public int NativeCompilerReturnValue { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }
         public System.Collections.Specialized.StringCollection Output { get { throw null; } }
         public string PathToAssembly { [System.Runtime.CompilerServices.CompilerGeneratedAttribute]get { throw null; } [System.Runtime.CompilerServices.CompilerGeneratedAttribute]set { } }

commit cb27a4edb3b4ad041ed8a58c72288798d095f0e7

Adds back CodeDom Evidence as additional implementation.
This fixes Mono breakage caused by https://github.com/dotnet/corefx/commit/a929f58f5d98f6800383f97804eb30ef62343eab

src/System.CodeDom/src/System/CodeDom/Compiler/CompilerParameters.Evidence.cs
@@ -1,0 +1,22 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Security.Policy;
+
+namespace System.CodeDom.Compiler
+{
+    public partial class CompilerParameters
+    {
+        [NonSerialized]
+        private Evidence _evidence;
+
+        [Obsolete("CAS policy is obsolete and will be removed in a future release of the .NET Framework."
+                + " Please see http://go2.microsoft.com/fwlink/?LinkId=131738 for more information.")]
+        public Evidence Evidence
+        {
+            get { return _evidence?.Clone(); }
+            set { _evidence = value?.Clone(); }
+         }
+    }
+}

src/System.CodeDom/src/System/CodeDom/Compiler/CompilerParameters.cs
@@ -7,7 +7,7 @@ using System.Collections.Specialized;
 namespace System.CodeDom.Compiler
 {
     [Serializable]
-    public class CompilerParameters
+    public partial class CompilerParameters
     {
         private readonly StringCollection _assemblyNames = new StringCollection();
         private readonly StringCollection _embeddedResources = new StringCollection();

src/System.CodeDom/src/System/CodeDom/Compiler/CompilerResults.Evidence.cs
@@ -1,0 +1,20 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Security.Policy;
+
+namespace System.CodeDom.Compiler
+{
+    public partial class CompilerResults
+    {
+    	private Evidence _evidence;
+
+        [Obsolete("CAS policy is obsolete and will be removed in a future release of the .NET Framework. Please see http://go2.microsoft.com/fwlink/?LinkId=131738 for more information.")]
+        public Evidence Evidence
+        {
+            get { return _evidence?.Clone(); }
+            set { _evidence = value?.Clone(); }
+        }
+    }
+}

src/System.CodeDom/src/System/CodeDom/Compiler/CompilerResults.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 namespace System.CodeDom.Compiler
 {
     [Serializable]
-    public class CompilerResults
+    public partial class CompilerResults
     {
         private readonly CompilerErrorCollection _errors = new CompilerErrorCollection();
         private readonly StringCollection _output = new StringCollection();