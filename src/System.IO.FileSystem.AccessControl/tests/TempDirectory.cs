8f22b78c7098dcf976c1684336fb4405178fbc5c
michael <the.dwyer@gmail.com>
2017-02-12 17:30:33 -0800

    use temp directory and file

---
+++ src/System.IO.FileSystem.AccessControl/tests/TempDirectory.cs
@@ -0,0 +1,40 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace System.IO
+{
+    public class TempDirectory : IDisposable
+    {
+        private bool disposed;
+        private SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+        private string directory;
+
+        public TempDirectory(string directory)
+        {
+            this.directory = directory;
+            Directory.CreateDirectory(directory);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                handle.Dispose();
+                Directory.Delete(directory);
+            }
+
+            disposed = true;
+        }
+    }
+}
---
+++ src/System.IO.FileSystem.AccessControl/tests/TempFile.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
+
+namespace System.IO
+{
+    public class TempFile : IDisposable
+    {
+        private bool disposed;
+        private SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+        private FileStream fileStream;
+        private string file;
+
+        public TempFile(string file)
+        {
+            this.file = file;
+            fileStream = File.Create(file);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                handle.Dispose();
+
+                fileStream.Close();
+                fileStream.Dispose();
+                File.Delete(file);
+            }
+
+            disposed = true;
+        }
+    }
+}
