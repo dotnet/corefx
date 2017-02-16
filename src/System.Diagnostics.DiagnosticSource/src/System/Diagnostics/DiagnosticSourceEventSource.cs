8d774a8244e6d52c5a9b8ec1ddc29e7c7f584061
Gaurav Khanna <gkhanna@microsoft.com>
2017-02-16 13:51:44 -0800

    Revert "Update AspNetCoreHosting keyword"

--- src/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/DiagnosticSourceEventSource.cs
+++ src/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/DiagnosticSourceEventSource.cs
@@ -164,9 +164,7 @@ namespace System.Diagnostics
                 "httpContext.Request.Path;" +
                 "httpContext.Request.QueryString" +
             "\n" +
-            "Microsoft.AspNetCore/Microsoft.AspNetCore.Hosting.EndRequest@Activity1Stop:-" +
-                "httpContext.TraceIdentifier;" +
-                "httpContext.Response.StatusCode";
+            "Microsoft.AspNetCore/Microsoft.AspNetCore.Hosting.EndRequest@Activity1Stop:-";
 
         // Setting EntityFrameworkCoreCommands is like having this in the FilterAndPayloadSpecs string
         // It turns on basic SQL commands.
