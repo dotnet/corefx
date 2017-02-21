7accf4add072d26cd9993f704e54f698659769d2
Radka <radkavectra@gmail.com>
2017-02-21 06:19:20 +0100

    Add F25 & F26 into #16237 "Disable Crypto tests for Fedora\Ubuntu" (#16303)

--- src/Common/tests/System/PlatformDetection.cs
+++ src/Common/tests/System/PlatformDetection.cs
@@ -69,6 +69,8 @@ namespace System
         public static bool IsUbuntu1610 { get; } = IsDistroAndVersion("ubuntu", "16.10");
         public static bool IsFedora23 { get; } = IsDistroAndVersion("fedora", "23");
         public static bool IsFedora24 { get; } = IsDistroAndVersion("fedora", "24");
+        public static bool IsFedora25 { get; } = IsDistroAndVersion("fedora", "25");
+        public static bool IsFedora26 { get; } = IsDistroAndVersion("fedora", "26");
         public static bool IsCentos7 { get; } = IsDistroAndVersion("centos", "7");
 
         /// <summary>
