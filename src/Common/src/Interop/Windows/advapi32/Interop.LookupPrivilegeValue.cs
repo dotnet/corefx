a5a49ce83dc68bd5e5d071161f090c5656c15ca4
Jose Perez Rodriguez <joperezr@microsoft.com>
2017-02-23 16:52:41 -0800

    Setting up ILCInputFolder that will be passed in to the ilc.exe that gets restored in order to run tests on uapaot (#16394)
    * Setting up ILCInputFolder that will be passed in to the ilc.exe that gets
    restored in order to run tests on uapaot
    
    * PR Feedback
    
    * Using netcore50 TFM and switching to use stable package version

--- src/Common/src/Interop/Windows/advapi32/Interop.LookupPrivilegeValue.cs
+++ src/Common/src/Interop/Windows/advapi32/Interop.LookupPrivilegeValue.cs
@@ -8,7 +8,7 @@ internal partial class Interop
 {
     internal partial class Advapi32
     {
-        [DllImport(Libraries.Advapi32, CharSet = CharSet.Unicode, SetLastError = true, BestFitMapping = false)]
+        [DllImport(Libraries.Advapi32, CharSet = CharSet.Unicode, SetLastError = true, BestFitMapping = false, EntryPoint = "LookupPrivilegeValueW")]
         internal static extern bool LookupPrivilegeValue([MarshalAs(UnmanagedType.LPTStr)] string lpSystemName, [MarshalAs(UnmanagedType.LPTStr)] string lpName, out LUID lpLuid);
 
         internal const string SeDebugPrivilege = "SeDebugPrivilege";
