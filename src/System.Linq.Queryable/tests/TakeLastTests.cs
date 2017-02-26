c4be00fccbf0e6055e7e871a72c120b59de9e105
Jon Hanna <jon@hackcraft.net>
2017-02-26 18:24:44 +0000

    Tests confirming Queryable.SkipLast & TakeLast throw on null source.

--- src/System.Linq.Queryable/tests/TakeLastTests.cs
+++ src/System.Linq.Queryable/tests/TakeLastTests.cs
@@ -18,5 +18,11 @@ namespace System.Linq.Tests
 
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void TakeLastThrowsOnNull()
+        {
+            Assert.Throws<ArgumentNullException>("source", () => ((IQueryable<DateTime>)null).TakeLast(3));
+        }
     }
 }
