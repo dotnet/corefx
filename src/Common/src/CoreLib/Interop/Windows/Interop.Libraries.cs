ffd5c35262c96fb75e7da4b5035634f5d2ac632a
Alex Perovich <alperovi@microsoft.com>
2017-02-27 20:54:54 -0600

    Move Used interop sources into shared folder (dotnet/coreclr#9840)
    Signed-off-by: dotnet-bot-corefx-mirror <dotnet-bot@microsoft.com>

---
+++ src/Common/src/CoreLib/Interop/Windows/Interop.Libraries.cs
@@ -0,0 +1,15 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+internal static partial class Interop
+{
+    internal static class Libraries
+    {
+        internal const string BCrypt = "BCrypt.dll";
+        internal const string Crypt32 = "crypt32.dll";
+        internal const string Kernel32 = "kernel32.dll";
+        internal const string NtDll = "ntdll.dll";
+        internal const string OleAut32 = "oleaut32.dll";
+    }
+}
