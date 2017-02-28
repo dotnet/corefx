3046bde1191792603d1bd0d6e742f31895d1841d
Peter Marcu <Peter.Marcu@Microsoft.com>
2017-02-27 19:45:00 -0800

    Removing unneccessary comment (#16517)

--- src/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/Semantics/Conversion.cs
+++ src/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/Semantics/Conversion.cs
@@ -1568,7 +1568,6 @@ namespace Microsoft.CSharp.RuntimeBinder.Semantics
                     if (!srcIntegral)
                     {
                         valueInt = (long)(ulong)valueFlt;
-                        // code below stolen from jit...
                         const double two63 = 2147483648.0 * 4294967296.0;
                         if (valueFlt < two63)
                         {
