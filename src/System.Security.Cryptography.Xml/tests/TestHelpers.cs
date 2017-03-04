710775a27afa8617604999b500015e130a219fd6
Anthony Langsworth <anthony.langsworth@gmail.com>
2017-03-04 23:23:29 +1100

    Remove TestHelpers.Create*

--- src/System.Security.Cryptography.Xml/tests/TestHelpers.cs
+++ src/System.Security.Cryptography.Xml/tests/TestHelpers.cs
@@ -10,59 +10,8 @@ using System.Xml.Resolvers;
 
 namespace System.Security.Cryptography.Xml.Tests
 {
-    public class TempFile : IDisposable
-    {
-        public TempFile(string fileName)
-        {
-            Path = fileName;
-        }
-
-        public string Path {  get; }
-
-        public void Dispose()
-        {
-    
-        }
-    }
-
     internal static class TestHelpers
     {
-        public static TempFile CreateTestDtdFile(string testName)
-        {
-            if (testName == null)
-                throw new ArgumentNullException(nameof(testName));
-
-            var file = new TempFile(
-                Path.Combine(Directory.GetCurrentDirectory(), testName + ".dtd")
-            );
-
-            File.WriteAllText(file.Path, "<!-- presence, not content, required -->");
-
-            return file;
-        }
-
-        public static TempFile CreateTestTextFile(string testName, string content)
-        {
-            if (testName == null)
-                throw new ArgumentNullException(nameof(testName));
-
-            if (content == null)
-                throw new ArgumentNullException(nameof(content));
-
-            var file = new TempFile(
-                Path.Combine(Directory.GetCurrentDirectory(), testName + ".txt")
-            );
-
-            File.WriteAllText(file.Path, content);
-
-            return file;
-        }
-
-        public static string EscapePath(string path)
-        {
-            return path.Replace("-", "&#2D;");
-        }
-
         /// <summary>
         /// Convert a <see cref="Stream"/> to a <see cref="string"/> using the given <see cref="Encoding"/>.
         /// </summary>
