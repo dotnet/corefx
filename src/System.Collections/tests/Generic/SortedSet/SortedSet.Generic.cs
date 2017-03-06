cbafaf8d58c4d3244f20a83aab58cf871ed6fbd3
Matthew Parkinson <parky@outlook.com>
2017-03-06 17:52:57 +0000

    Fix behaviour of TreeSubSet Min and Max
    Provide correct override in TreeSubSet for Min and Max, they now take
    account of the range of the subset.
    
    As the root is guaranteed to be in the range, then the code does not
    need to check something is in the range, if _root is not null.

--- src/System.Collections/tests/Generic/SortedSet/SortedSet.Generic.cs
+++ src/System.Collections/tests/Generic/SortedSet/SortedSet.Generic.cs
@@ -29,7 +29,6 @@ namespace System.Collections.Tests
 
         protected override bool DefaultValueAllowed => true;
 
-        [ActiveIssue(16760)]
         [Fact]
         public void SortedSet_Generic_GetViewBetween_MinMax()
         {
