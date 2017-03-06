38e60f7d5b1bfa68b6dba3481fed63fad29ce315
WinCPP <mandards@gmail.com>
2017-03-07 02:52:03 +0530

    Issue #3862 Use lazy initializers in place of double check locks

--- src/Common/src/System/Net/NetworkInformation/HostInformationPal.Windows.cs
+++ src/Common/src/System/Net/NetworkInformation/HostInformationPal.Windows.cs
@@ -68,17 +68,7 @@ namespace System.Net.NetworkInformation
 
         private static void EnsureFixedInfo()
         {
-            if (!Volatile.Read(ref s_fixedInfoInitialized))
-            {
-                lock (s_syncObject)
-                {
-                    if (!s_fixedInfoInitialized)
-                    {
-                        s_fixedInfo = GetFixedInfo();
-                        Volatile.Write(ref s_fixedInfoInitialized, true);
-                    }
-                }
-            }
+            LazyInitializer.EnsureInitialized(ref s_fixedInfo, ref s_fixedInfoInitialized, ref s_syncObject, () => GetFixedInfo());
         }
     }
 }
