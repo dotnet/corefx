a591907b1149601ef9daa2e850d81666e3a273f9
safern <safern@SAFERNMAIN>
2017-03-07 11:01:21 -0800

    Address PR Feedback

--- src/Common/tests/System/Collections/ISet.Generic.Tests.cs
+++ src/Common/tests/System/Collections/ISet.Generic.Tests.cs
@@ -419,7 +419,7 @@ namespace System.Collections.Tests
 
         [Theory]
         [MemberData(nameof(ValidCollectionSizes))]
-        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework)] //Throws InvalidOperationException, issue: dotnet/corefx #16796
+        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework, "dotnet/corefx #16796")] //Throws InvalidOperationException
         public void ISet_Generic_IntersectWith_Itself(int setLength)
         {
             ISet<T> set = GenericISetFactory(setLength);
