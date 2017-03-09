aa4c03b0800fcc90d204ef61b0e779388eff8e95
Anton Lapounov <antonl@microsoft.com>
2017-03-08 17:08:35 -0800

    Add missing #using directive

--- src/System.Threading/src/System/Threading/ReaderWriterLockSlim.cs
+++ src/System.Threading/src/System/Threading/ReaderWriterLockSlim.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Internal.Runtime.Augments;
 using System.Diagnostics; // for TraceInformation
 using System.Threading;
 using System.Runtime.CompilerServices;
