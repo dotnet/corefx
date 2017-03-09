0a3b466b78ce57ce66b71cc03ddcdf2aae45e1f2
Kishchenko <Kishchenko>
2017-03-09 14:45:40 +0300

    Fixed typo in PropertyTabAttribute.cs

--- src/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/Semantics/MemberLookup.cs
+++ src/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/Semantics/MemberLookup.cs
@@ -104,9 +104,9 @@ namespace Microsoft.CSharp.RuntimeBinder.Semantics
             _csym++;
 
             // If it is first, record it.
-            if (_swtFirst != null)
+            if (_swtFirst == null)
             {
-                _swtFirst.Set(sym, type);
+                _swtFirst.Set(sym, type); // NullReferenceException possible!
                 Debug.Assert(_csym == 1);
                 Debug.Assert(_prgtype[0] == type);
                 _fMulti = sym.IsMethodSymbol() || sym.IsPropertySymbol() && sym.AsPropertySymbol().isIndexer();
