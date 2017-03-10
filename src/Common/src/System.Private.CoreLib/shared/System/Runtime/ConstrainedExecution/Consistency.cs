1bccc513558fd05c071808c8163e2d2a5ceee659
Atsushi Kanamori <AtsushiKan@users.noreply.github.com>
2017-03-09 19:20:12 -0800

    Move more types into the shared partition. (dotnet/coreclr#10068)
    * Split/Move files verbatim into shared partition.
    
    * Style cleanup.
    
    - Delete unused usings
    
    - Use autoprops when possible
    
    - Lambda syntax for TypeDelegator
      (yes, it's a lot of code review noise
      but if there was ever a canonical use-case
      for that syntax, this is it.)
    
    Signed-off-by: dotnet-bot-corefx-mirror <dotnet-bot@microsoft.com>

---
+++ src/Common/src/System.Private.CoreLib/shared/System/Runtime/ConstrainedExecution/Consistency.cs
@@ -0,0 +1,15 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Runtime.ConstrainedExecution
+{
+    [Serializable]
+    public enum Consistency : int
+    {
+        MayCorruptProcess = 0,
+        MayCorruptAppDomain = 1,
+        MayCorruptInstance = 2,
+        WillNotCorruptState = 3,
+    }
+}
