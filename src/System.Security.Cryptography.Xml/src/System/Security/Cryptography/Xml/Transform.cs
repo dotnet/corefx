bc607fc80558841183c131fe5ac567b3499eb3b7
Anthony Langsworth <anthony.langsworth@gmail.com>
2017-03-13 21:50:55 +1100

    Add and use internal Context property

--- src/System.Security.Cryptography.Xml/src/System/Security/Cryptography/Xml/Transform.cs
+++ src/System.Security.Cryptography.Xml/src/System/Security/Cryptography/Xml/Transform.cs
@@ -174,7 +174,7 @@ namespace System.Security.Cryptography.Xml
                 if (signedXml == null)
                     return null;
 
-                return signedXml._context;
+                return signedXml.Context;
             }
             set
             {
@@ -204,8 +204,8 @@ namespace System.Security.Cryptography.Xml
                 CanonicalXmlNodeList namespaces = null;
                 if (reference != null)
                     namespaces = reference._namespaces;
-                else if (signedXml?._context != null)
-                    namespaces = Utils.GetPropagatedAttributes(signedXml._context);
+                else if (signedXml?.Context != null)
+                    namespaces = Utils.GetPropagatedAttributes(signedXml.Context);
 
                 // if no namespaces have been propagated, return an empty hashtable.
                 if (namespaces == null)
