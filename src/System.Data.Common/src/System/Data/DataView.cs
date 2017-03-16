4502d53a4d97bf72e0f63f747d30f5f561bf39ae
Rion Williams <rionmonster@gmail.com>
2017-03-16 15:17:11 -0500

    Reverted `TryAdd()` To Previous Approach
    Reverted to earlier approach to avoid unnessary allocation of  a new
    row.

--- src/System.Data.Common/src/System/Data/DataView.cs
+++ src/System.Data.Common/src/System/Data/DataView.cs
@@ -1330,9 +1330,9 @@ namespace System.Data
                         _addNewRow = null;
                         _addNewMoved = new ListChangedEventArgs(ListChangedType.ItemMoved, index, Count - 1);
                     }
-                    else if (_rowViewCache.TryAdd(row, buffer ?? new DataRowView(this, row)))
+                    else if (!_rowViewCache.ContainsKey(row))
                     {
-                        // Row was added
+                        _rowViewCache.Add(row, buffer ?? new DataRowView(this, row));
                     }
                     else
                     {
