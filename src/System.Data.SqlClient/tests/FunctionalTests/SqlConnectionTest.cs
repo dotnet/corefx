ec45b08d3d6a8d9fc773dba3efcae63d29e5da84
Saurabh Singh <saurabh500@gmail.com>
2017-03-17 17:58:59 -0700

    Adding Integrated Auth Test with Test Server

--- src/System.Data.SqlClient/tests/FunctionalTests/SqlConnectionTest.cs
+++ src/System.Data.SqlClient/tests/FunctionalTests/SqlConnectionTest.cs
@@ -30,6 +30,29 @@ namespace System.Data.SqlClient.Tests
             Assert.Null(e);
         }
 
+
+        [Fact]
+        [PlatformSpecific(TestPlatforms.Windows)]  // Integ auth on Test server is supported on Windows right now
+        public void IntegratedAuthConnectionTest()
+        {
+            Exception e = null;
+
+            using (TestTdsServer server = TestTdsServer.StartTestServer())
+            {
+                try
+                {
+                    SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(server.ConnectionString);
+                    builder.IntegratedSecurity = true;
+                    SqlConnection connection = new SqlConnection(builder.ConnectionString);
+                    connection.Open();
+                }
+                catch (Exception ce)
+                {
+                    e = ce;
+                }
+            }
+            Assert.Null(e);
+        }
     }
 
 }
