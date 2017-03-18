9cd7bdea403e78853b700ded590ba5c906dd84a7
Hugh Bellamy <hughbellars@gmail.com>
2017-03-18 11:39:30 +0700

    Expose and test new string.Join overloads

--- src/System.Runtime/tests/System/StringTests.cs
+++ src/System.Runtime/tests/System/StringTests.cs
@@ -1607,19 +1607,31 @@ namespace System.Tests
         }
 
         [Fact]
-        public static void Join_StringArray_Invalid()
+        public static void Join_String_NullValues_ThrowsArgumentNullException()
         {
-            // Values is null
             Assert.Throws<ArgumentNullException>("value", () => string.Join("$$", null));
             Assert.Throws<ArgumentNullException>("value", () => string.Join("$$", null, 0, 0));
             Assert.Throws<ArgumentNullException>("values", () => string.Join("|", (IEnumerable<string>)null));
             Assert.Throws<ArgumentNullException>("values", () => string.Join<string>("|", (IEnumerable<string>)null)); // Generic overload
+        }
 
-            Assert.Throws<ArgumentOutOfRangeException>("startIndex", () => string.Join("$$", new string[] { "Foo" }, -1, 0)); // Start index < 0
-            Assert.Throws<ArgumentOutOfRangeException>("count", () => string.Join("$$", new string[] { "Foo" }, 0, -1)); // Count < 0
+        [Fact]
+        public static void Join_String_NegativeStartIndex_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>("startIndex", () => string.Join("$$", new string[] { "Foo" }, -1, 0));
+        }
 
-            // Start index > separators.Length
+        [Fact]
+        public static void Join_String_NegativeCount_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>("count", () => string.Join("$$", new string[] { "Foo" }, 0, -1));
+        }
+
+        [Fact]
+        public static void Join_String_InvalidStartIndexOut_ThrowsArgumentOutOfRangeException()
+        {
             Assert.Throws<ArgumentOutOfRangeException>("startIndex", () => string.Join("$$", new string[] { "Foo" }, 2, 1));
+            Assert.Throws<ArgumentOutOfRangeException>("startIndex", () => string.Join("$$", new string[] { "Foo" }, 1, 2));
             Assert.Throws<ArgumentOutOfRangeException>("startIndex", () => string.Join("$$", new string[] { "Foo" }, 0, 2));
         }
 
