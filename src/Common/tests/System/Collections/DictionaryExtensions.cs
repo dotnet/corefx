51a57d24b09e44be350c7e84102340fa94245672
Rion Williams <rionmonster@gmail.com>
2017-03-20 14:02:28 -0500

    Extended TryAdd() Extension Method
    Updated the existing `TryAdd()` extension method to target any Dictionary
    related interfaces (as opposed to Dictionary implementations)

--- src/Common/tests/System/Collections/DictionaryExtensions.cs
+++ src/Common/tests/System/Collections/DictionaryExtensions.cs
@@ -6,7 +6,7 @@ namespace System.Collections.Generic
 {
     internal static class DictionaryExtensions
     {
-        public static bool TryAdd<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue value)
+        public static bool TryAdd<TKey, TValue>(this IDictionary<TKey, TValue> dict, TKey key, TValue value)
         {
             if (!dict.ContainsKey(key))
             {
