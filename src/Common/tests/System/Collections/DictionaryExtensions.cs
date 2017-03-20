7a2736527d343be38f083eb4e19871d125a1d090
Rion Williams <rionmonster@gmail.com>
2017-03-20 11:48:14 -0500

    Updated Namespace for Dictionary Extension Methods
    Changed from `System.Collection.Tests` to `System.Collections.Generic`
    as this wasn't actually a test but rather an extension method.

--- src/Common/tests/System/Collections/DictionaryExtensions.cs
+++ src/Common/tests/System/Collections/DictionaryExtensions.cs
@@ -2,9 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Collections.Generic;
-
-namespace System.Collections.Tests
+namespace System.Collections.Generic
 {
     internal static class DictionaryExtensions
     {
