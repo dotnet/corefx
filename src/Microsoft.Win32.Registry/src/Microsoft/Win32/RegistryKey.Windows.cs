fb714a9db4f7271f2e03f23db0c4749bb352146c
Wes Haggard <Wes.Haggard@microsoft.com>
2017-03-20 14:51:04 -0700

    Remove System.Security.Permissions dependency from Registry
    We don't support CAS so we are removing the Permissions check
    that we do in Registry as it isn't necessary.

--- src/Microsoft.Win32.Registry/src/Microsoft/Win32/RegistryKey.Windows.cs
+++ src/Microsoft.Win32.Registry/src/Microsoft/Win32/RegistryKey.Windows.cs
@@ -33,7 +33,7 @@ using System.Security.AccessControl;
 /*
   Note on ACL support:
   The key thing to note about ACL's is you set them on a kernel object like a
-  registry key, then the ACL only gets checked when you construct handles to 
+  registry key, then the ACL only gets checked when you construct handles to
   them.  So if you set an ACL to deny read access to yourself, you'll still be
   able to read with that handle, but not with new handles.
 
@@ -45,8 +45,8 @@ using System.Security.AccessControl;
   may not be able to read or write to a registry key.  It's very strange.  But
   the real test of these handles is attempting to read or set a value in an
   affected registry key.
-  
-  For reference, at least two registry keys must be set to particular values 
+
+  For reference, at least two registry keys must be set to particular values
   for this behavior:
   HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Terminal Server\RegistryExtensionFlags, the least significant bit must be 1.
   HKLM\SYSTEM\CurrentControlSet\Control\TerminalServer\TSAppCompat must be 1
@@ -67,14 +67,14 @@ namespace Microsoft.Win32
             // System keys should never be closed.  However, we want to call RegCloseKey
             // on HKEY_PERFORMANCE_DATA when called from PerformanceCounter.CloseSharedResources
             // (i.e. when disposing is true) so that we release the PERFLIB cache and cause it
-            // to be refreshed (by re-reading the registry) when accessed subsequently. 
-            // This is the only way we can see the just installed perf counter.  
+            // to be refreshed (by re-reading the registry) when accessed subsequently.
+            // This is the only way we can see the just installed perf counter.
             // NOTE: since HKEY_PERFORMANCE_DATA is process wide, there is inherent race in closing
             // the key asynchronously. While Vista is smart enough to rebuild the PERFLIB resources
-            // in this situation the down level OSes are not. We have a small window of race between  
-            // the dispose below and usage elsewhere (other threads). This is By Design. 
-            // This is less of an issue when OS > NT5 (i.e Vista & higher), we can close the perfkey  
-            // (to release & refresh PERFLIB resources) and the OS will rebuild PERFLIB as necessary. 
+            // in this situation the down level OSes are not. We have a small window of race between
+            // the dispose below and usage elsewhere (other threads). This is By Design.
+            // This is less of an issue when OS > NT5 (i.e Vista & higher), we can close the perfkey
+            // (to release & refresh PERFLIB resources) and the OS will rebuild PERFLIB as necessary.
             Interop.Advapi32.RegCloseKey(HKEY_PERFORMANCE_DATA);
         }
 
@@ -106,7 +106,6 @@ namespace Microsoft.Win32
             if (ret == 0 && !result.IsInvalid)
             {
                 RegistryKey key = new RegistryKey(result, (permissionCheck != RegistryKeyPermissionCheck.ReadSubTree), false, _remoteKey, false, _regView);
-                CheckPermission(RegistryInternalCheck.CheckSubTreePermission, subkey, false, permissionCheck);
                 key._checkMode = permissionCheck;
 
                 if (subkey.Length == 0)
@@ -162,7 +161,7 @@ namespace Microsoft.Win32
             int errorCode = Interop.Advapi32.RegDeleteValue(_hkey, name);
 
             //
-            // From windows 2003 server, if the name is too long we will get error code ERROR_FILENAME_EXCED_RANGE  
+            // From windows 2003 server, if the name is too long we will get error code ERROR_FILENAME_EXCED_RANGE
             // This still means the name doesn't exist. We need to be consistent with previous OS.
             //
             if (errorCode == Interop.Errors.ERROR_FILE_NOT_FOUND ||
@@ -532,8 +531,8 @@ namespace Microsoft.Win32
                 else
                 {
                     // For stuff like ERROR_FILE_NOT_FOUND, we want to return null (data).
-                    // Some OS's returned ERROR_MORE_DATA even in success cases, so we 
-                    // want to continue on through the function. 
+                    // Some OS's returned ERROR_MORE_DATA even in success cases, so we
+                    // want to continue on through the function.
                     if (ret != Interop.Errors.ERROR_MORE_DATA)
                     {
                         return data;
@@ -613,7 +612,7 @@ namespace Microsoft.Win32
                         }
                         else
                         {
-                            // in the very unlikely case the data is missing null termination, 
+                            // in the very unlikely case the data is missing null termination,
                             // pass in the whole char[] to prevent truncating a character
                             data = new string(blob);
                         }
@@ -644,7 +643,7 @@ namespace Microsoft.Win32
                         }
                         else
                         {
-                            // in the very unlikely case the data is missing null termination, 
+                            // in the very unlikely case the data is missing null termination,
                             // pass in the whole char[] to prevent truncating a character
                             data = new string(blob);
                         }
@@ -703,8 +702,8 @@ namespace Microsoft.Win32
                                 }
                                 else
                                 {
-                                    // we found an empty string.  But if we're at the end of the data, 
-                                    // it's just the extra null terminator. 
+                                    // we found an empty string.  But if we're at the end of the data,
+                                    // it's just the extra null terminator.
                                     if (nextNull != len - 1)
                                     {
                                         toAdd = string.Empty;
@@ -777,14 +776,14 @@ namespace Microsoft.Win32
 
                     case RegistryValueKind.MultiString:
                         {
-                            // Other thread might modify the input array after we calculate the buffer length.                            
+                            // Other thread might modify the input array after we calculate the buffer length.
                             // Make a copy of the input array to be safe.
                             string[] dataStrings = (string[])(((string[])value).Clone());
 
                             // First determine the size of the array
                             //
                             // Format is null terminator between strings and final null terminator at the end.
-                            //    e.g. str1\0str2\0str3\0\0 
+                            //    e.g. str1\0str2\0str3\0\0
                             //
                             int sizeInChars = 1; // no matter what, we have the final null terminator.
                             for (int i = 0; i < dataStrings.Length; i++)
@@ -903,10 +902,10 @@ namespace Microsoft.Win32
                     // SafeRegHandle and only throw the IOException.  This is to workaround reentrancy issues
                     // in PerformanceCounter.NextValue() where the API could throw {NullReference, ObjectDisposed, ArgumentNull}Exception
                     // on reentrant calls because of this error code path in RegistryKey
-                    // 
+                    //
                     // Normally we'd make our caller synchronize access to a shared RegistryKey instead of doing something like this,
-                    // however we shipped PerformanceCounter.NextValue() un-synchronized in v2.0RTM and customers have taken a dependency on 
-                    // this behavior (being able to simultaneously query multiple remote-machine counters on multiple threads, instead of 
+                    // however we shipped PerformanceCounter.NextValue() un-synchronized in v2.0RTM and customers have taken a dependency on
+                    // this behavior (being able to simultaneously query multiple remote-machine counters on multiple threads, instead of
                     // having serialized access).
                     if (!IsPerfDataKey())
                     {
