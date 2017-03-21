8e9663e97e08f5cdd6ed2ad0672535e9dbb82365
Eric St. John <ericstj@microsoft.com>
2017-03-21 14:32:25 -0700

    Add netstandard build of Vectors

---
+++ src/System.Numerics.Vectors/ref/System.Numerics.Vectors.Forwards.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+// ------------------------------------------------------------------------------
+// Changes to this file must follow the http://aka.ms/api-review process.
+// ------------------------------------------------------------------------------
+
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Numerics.Matrix3x2))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Numerics.Matrix4x4))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Numerics.Plane))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Numerics.Quaternion))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Numerics.Vector))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Numerics.Vector<>))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Numerics.Vector2))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Numerics.Vector3))]
+[assembly: System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Numerics.Vector4))]
