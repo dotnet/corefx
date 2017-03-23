95e70496b4eff4518fa5d19cb40de02d48ce9140
safern <safern@SAFERNMAIN>
2017-03-22 19:37:01 -0700

    Fix netcoreapp tests

--- src/System.Runtime.Serialization.Xml/tests/DataContractSerializer.netcoreapp.cs
+++ src/System.Runtime.Serialization.Xml/tests/DataContractSerializer.netcoreapp.cs
@@ -1,3 +1,11 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using SerializationTypes;
+using System.IO;
+using System.Runtime.Serialization;
+using Xunit;
 
 public static partial class DataContractSerializerTests
 {
