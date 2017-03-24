b4eeffa109fa05bd7b6c24bb030915901f9a14d4
Eric Mellino <erme@microsoft.com>
2017-03-24 15:29:04 -0700

    Disable assembly-level parallelization of the System.Runtime.Numerics tests.
    See issue 17499.

---
+++ src/System.Runtime.Numerics/tests/XunitAssemblyTestCollection.cs
@@ -0,0 +1,6 @@
+﻿using Xunit;
+
+// The MyBigIntImp class contains public static shared state. This means it is not safe to run tests
+// which modify that state in parallel. In order to run these tests in parallel, we need to fix that.
+// See issue: https://github.com/dotnet/corefx/issues/17499
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly)]
