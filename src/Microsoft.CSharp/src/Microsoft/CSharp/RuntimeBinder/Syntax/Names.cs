a14d8789ce0efdd6fc2bdcfac671ddef1bd051a8
Jon Hanna <jon@hackcraft.net>
2017-03-24 20:16:13 +0000

    Autoproperty in Names

--- src/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/Syntax/Names.cs
+++ src/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/Syntax/Names.cs
@@ -6,21 +6,13 @@ namespace Microsoft.CSharp.RuntimeBinder.Syntax
 {
     internal class Name
     {
-        private readonly string _text;
-
         public Name(string text)
         {
-            _text = text;
+            Text = text;
         }
 
-        public string Text
-        {
-            get { return _text; }
-        }
+        public string Text { get; }
 
-        public override string ToString()
-        {
-            return _text;
-        }
+        public override string ToString() => Text;
     }
 }
