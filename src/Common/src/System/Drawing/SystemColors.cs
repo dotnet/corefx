efa529fe49c99326df8cb1338ce9a5fb5e5bc4b0
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-03-30 13:21:31 +0200

    Use Color.FromKnownColor

--- src/Common/src/System/Drawing/SystemColors.cs
+++ src/Common/src/System/Drawing/SystemColors.cs
@@ -6,70 +6,70 @@ namespace System.Drawing
 {
     internal static class SystemColors
     {
-        public static Color ActiveBorder => new Color(KnownColor.ActiveBorder);
+        public static Color ActiveBorder => Color.FromKnownColor(KnownColor.ActiveBorder);
 
-        public static Color ActiveCaption => new Color(KnownColor.ActiveCaption);
+        public static Color ActiveCaption => Color.FromKnownColor(KnownColor.ActiveCaption);
 
-        public static Color ActiveCaptionText => new Color(KnownColor.ActiveCaptionText);
+        public static Color ActiveCaptionText => Color.FromKnownColor(KnownColor.ActiveCaptionText);
 
-        public static Color AppWorkspace => new Color(KnownColor.AppWorkspace);
+        public static Color AppWorkspace => Color.FromKnownColor(KnownColor.AppWorkspace);
 
-        public static Color ButtonFace => new Color(KnownColor.ButtonFace);
+        public static Color ButtonFace => Color.FromKnownColor(KnownColor.ButtonFace);
 
-        public static Color ButtonHighlight => new Color(KnownColor.ButtonHighlight);
+        public static Color ButtonHighlight => Color.FromKnownColor(KnownColor.ButtonHighlight);
 
-        public static Color ButtonShadow => new Color(KnownColor.ButtonShadow);
+        public static Color ButtonShadow => Color.FromKnownColor(KnownColor.ButtonShadow);
 
-        public static Color Control => new Color(KnownColor.Control);
+        public static Color Control => Color.FromKnownColor(KnownColor.Control);
 
-        public static Color ControlDark => new Color(KnownColor.ControlDark);
+        public static Color ControlDark => Color.FromKnownColor(KnownColor.ControlDark);
 
-        public static Color ControlDarkDark => new Color(KnownColor.ControlDarkDark);
+        public static Color ControlDarkDark => Color.FromKnownColor(KnownColor.ControlDarkDark);
 
-        public static Color ControlLight => new Color(KnownColor.ControlLight);
+        public static Color ControlLight => Color.FromKnownColor(KnownColor.ControlLight);
 
-        public static Color ControlLightLight => new Color(KnownColor.ControlLightLight);
+        public static Color ControlLightLight => Color.FromKnownColor(KnownColor.ControlLightLight);
 
-        public static Color ControlText => new Color(KnownColor.ControlText);
+        public static Color ControlText => Color.FromKnownColor(KnownColor.ControlText);
 
-        public static Color Desktop => new Color(KnownColor.Desktop);
+        public static Color Desktop => Color.FromKnownColor(KnownColor.Desktop);
 
-        public static Color GradientActiveCaption => new Color(KnownColor.GradientActiveCaption);
+        public static Color GradientActiveCaption => Color.FromKnownColor(KnownColor.GradientActiveCaption);
 
-        public static Color GradientInactiveCaption => new Color(KnownColor.GradientInactiveCaption);
+        public static Color GradientInactiveCaption => Color.FromKnownColor(KnownColor.GradientInactiveCaption);
 
-        public static Color GrayText => new Color(KnownColor.GrayText);
+        public static Color GrayText => Color.FromKnownColor(KnownColor.GrayText);
 
-        public static Color Highlight => new Color(KnownColor.Highlight);
+        public static Color Highlight => Color.FromKnownColor(KnownColor.Highlight);
 
-        public static Color HighlightText => new Color(KnownColor.HighlightText);
+        public static Color HighlightText => Color.FromKnownColor(KnownColor.HighlightText);
 
-        public static Color HotTrack => new Color(KnownColor.HotTrack);
+        public static Color HotTrack => Color.FromKnownColor(KnownColor.HotTrack);
 
-        public static Color InactiveBorder => new Color(KnownColor.InactiveBorder);
+        public static Color InactiveBorder => Color.FromKnownColor(KnownColor.InactiveBorder);
 
-        public static Color InactiveCaption => new Color(KnownColor.InactiveCaption);
+        public static Color InactiveCaption => Color.FromKnownColor(KnownColor.InactiveCaption);
 
-        public static Color InactiveCaptionText => new Color(KnownColor.InactiveCaptionText);
+        public static Color InactiveCaptionText => Color.FromKnownColor(KnownColor.InactiveCaptionText);
 
-        public static Color Info => new Color(KnownColor.Info);
+        public static Color Info => Color.FromKnownColor(KnownColor.Info);
 
-        public static Color InfoText => new Color(KnownColor.InfoText);
+        public static Color InfoText => Color.FromKnownColor(KnownColor.InfoText);
 
-        public static Color Menu => new Color(KnownColor.Menu);
+        public static Color Menu => Color.FromKnownColor(KnownColor.Menu);
 
-        public static Color MenuBar => new Color(KnownColor.MenuBar);
+        public static Color MenuBar => Color.FromKnownColor(KnownColor.MenuBar);
 
-        public static Color MenuHighlight => new Color(KnownColor.MenuHighlight);
+        public static Color MenuHighlight => Color.FromKnownColor(KnownColor.MenuHighlight);
 
-        public static Color MenuText => new Color(KnownColor.MenuText);
+        public static Color MenuText => Color.FromKnownColor(KnownColor.MenuText);
 
-        public static Color ScrollBar => new Color(KnownColor.ScrollBar);
+        public static Color ScrollBar => Color.FromKnownColor(KnownColor.ScrollBar);
 
-        public static Color Window => new Color(KnownColor.Window);
+        public static Color Window => Color.FromKnownColor(KnownColor.Window);
 
-        public static Color WindowFrame => new Color(KnownColor.WindowFrame);
+        public static Color WindowFrame => Color.FromKnownColor(KnownColor.WindowFrame);
 
-        public static Color WindowText => new Color(KnownColor.WindowText);
+        public static Color WindowText => Color.FromKnownColor(KnownColor.WindowText);
     }
 }
