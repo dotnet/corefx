71c931a8240358f288ace037dd09ad1747129711
Atsushi Kanamori <AtsushiKan@users.noreply.github.com>
2017-03-31 11:41:39 -0700

    NS2.0 Inject more missing types into CoreRT by sharing them. (#10613)
    * NS2.0 Inject more missing types into CoreRT by sharing them.
    
    - ArgIterator was cleaned up for sharing but actually
      sharing it today is too messy with TypedReference
      being in the wrong namespace on half of CoreRT.
    
      Some preparatory fixes discovered during the
      attempt will be going in on CoreRT...
    
    * Leave RuntimeArgumentHandle and ArgIterator unshared.
    
    Signed-off-by: dotnet-bot-corefx-mirror <dotnet-bot@microsoft.com>

---
+++ src/Common/src/System.Private.CoreLib/shared/System/TypeUnloadedException.cs
@@ -0,0 +1,39 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.Serialization;
+
+namespace System
+{
+    [Serializable]
+    public class TypeUnloadedException : SystemException
+    {
+        public TypeUnloadedException()
+            : base(SR.Arg_TypeUnloadedException)
+        {
+            SetErrorCode(__HResults.COR_E_TYPEUNLOADED);
+        }
+
+        public TypeUnloadedException(string message)
+            : base(message)
+        {
+            SetErrorCode(__HResults.COR_E_TYPEUNLOADED);
+        }
+
+        public TypeUnloadedException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+            SetErrorCode(__HResults.COR_E_TYPEUNLOADED);
+        }
+
+        //
+        // This constructor is required for serialization;
+        //
+        protected TypeUnloadedException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
+
