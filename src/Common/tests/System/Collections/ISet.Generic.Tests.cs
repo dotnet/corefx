403e2ec85b6c5a3a836ea415b2ebd76cc6892f1d
danmosemsft <danmose@microsoft.com>
2017-04-01 16:53:33 -0700

    NETFX failure by design

--- src/Common/tests/System/Collections/ISet.Generic.Tests.cs
+++ src/Common/tests/System/Collections/ISet.Generic.Tests.cs
@@ -419,7 +419,7 @@ namespace System.Collections.Tests
 
         [Theory]
         [MemberData(nameof(ValidCollectionSizes))]
-        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework, "dotnet/corefx #16796")] //Throws InvalidOperationException
+        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework, "Full framework throws InvalidOperationException")]
         public void ISet_Generic_IntersectWith_Itself(int setLength)
         {
             ISet<T> set = GenericISetFactory(setLength);
