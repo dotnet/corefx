7207bfc3342f62c721986ef8023cf25d89bb555d
Atsushi Kanamori <atsushik@microsoft.com>
2017-04-04 15:25:26 -0700

    NS2.0 - Add SHA256/384/512CryptoServiceProvider
    Recent NetStandard 2.0 stragglers...

--- src/System.Security.Cryptography.Csp/src/System/Security/Cryptography/SHA384CryptoServiceProvider.cs
+++ src/System.Security.Cryptography.Csp/src/System/Security/Cryptography/SHA384CryptoServiceProvider.cs
@@ -10,14 +10,14 @@ namespace System.Security.Cryptography
     // If you change this file, make the corresponding changes to all of the SHA*CryptoServiceProvider.cs files.
     //
     [EditorBrowsable(EditorBrowsableState.Never)]
-    public sealed class SHA1CryptoServiceProvider : SHA1
+    public sealed class SHA384CryptoServiceProvider : SHA384
     {
-        private const int HashSizeBits = 160;
+        private const int HashSizeBits = 384;
         private readonly IncrementalHash _incrementalHash;
 
-        public SHA1CryptoServiceProvider()
+        public SHA384CryptoServiceProvider()
         {
-            _incrementalHash = IncrementalHash.CreateHash(HashAlgorithmName.SHA1);
+            _incrementalHash = IncrementalHash.CreateHash(HashAlgorithmName.SHA384);
             HashSizeValue = HashSizeBits;
         }
 
