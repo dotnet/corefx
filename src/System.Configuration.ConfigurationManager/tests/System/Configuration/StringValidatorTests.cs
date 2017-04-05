2c2222613d3999476a9bd4862fc44ced29efca95
dhoehna <dhoehna@yahoo.com>
2017-04-05 13:29:20 -0700

    Adding tests

---
+++ src/System.Configuration.ConfigurationManager/tests/System/Configuration/StringValidatorTests.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Configuration;
+using Xunit;
+
+namespace System.ConfigurationTests
+{
+    public class StringValidatorTests
+    {
+        [Fact]
+        public void Constructor_MinLength()
+        {
+            //We don't expect this to fail
+            StringValidator validator = new StringValidator(5);
+        }
+
+        [Fact]
+        public void Constructor_MinValueAndMaxValue()
+        {
+            StringValidator validator = new StringValidator(5, 10);
+        }
+
+        [Fact]
+        public void Constructor_MinValueMaxValueAndInvalidChars()
+        {
+            StringValidator validator = new StringValidator(5, 10, "abcde");
+        }
+    }
+}
