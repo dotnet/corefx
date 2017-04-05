ce11eb43170125c54e8c368429d741e3d6ae6c22
Stanislav Ushakov <sushakov@dataart.com>
2017-04-05 18:07:23 +0300

    Added tests for XmlLicenseTransform (coverage 93.9%)
    Added tests and test data for XmlLicenseTransform.
    Overall code coverage for System.Security.Cryptography.Xml is 70.4%
    
    Part of #16781

--- src/System.Security.Cryptography.Xml/tests/XmlLicenseTransformTest.cs
+++ src/System.Security.Cryptography.Xml/tests/XmlLicenseTransformTest.cs
@@ -15,6 +15,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // See the LICENSE file in the project root for more information.
 
+using System.IO;
 using System.Xml;
 using Xunit;
 
@@ -25,6 +26,21 @@ namespace System.Security.Cryptography.Xml.Tests
         public XmlNodeList UnprotectedGetInnerXml()
         {
             return base.GetInnerXml();
+        }
+    }
+
+    public class DummyDecryptor : IRelDecryptor
+    {
+        public string ContentToReturn { get; set; }
+
+        public Stream Decrypt(EncryptionMethod encryptionMethod, KeyInfo keyInfo, Stream toDecrypt)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(ContentToReturn);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
         }
     }
 
@@ -82,6 +98,75 @@ namespace System.Security.Cryptography.Xml.Tests
             // it's not a static array
             transform = new UnprotectedXmlLicenseTransform();
             Assert.NotNull(transform.OutputTypes[0]);
+        }
+
+        [Fact]
+        public void Context_Null()
+        {
+            XmlDocument doc = GetDocumentFromResource("System.Security.Cryptography.Xml.Tests.XmlLicenseSample.xml");
+
+            Assert.Throws<CryptographicException>(() => transform.LoadInput(doc));
+        }
+
+        [Fact]
+        public void NoLicenseXml()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml("<root />");
+            transform.Context = doc.DocumentElement;
+
+            Assert.Throws<CryptographicException>(() => transform.LoadInput(doc));
+        }
+
+        [Fact]
+        public void Decryptor_Null()
+        {
+            XmlDocument doc = GetDocumentFromResource("System.Security.Cryptography.Xml.Tests.XmlLicenseSample.xml");
+
+            XmlNamespaceManager namespaceManager = new XmlNamespaceManager(doc.NameTable);
+            namespaceManager.AddNamespace("r", "urn:mpeg:mpeg21:2003:01-REL-R-NS");
+            transform.Context = doc.DocumentElement.SelectSingleNode("//r:issuer[1]", namespaceManager) as XmlElement;
+
+            Assert.Throws<CryptographicException>(() => transform.LoadInput(doc));
+        }
+
+        [Fact]
+        public void ValidLicense()
+        {
+            XmlDocument doc = GetDocumentFromResource("System.Security.Cryptography.Xml.Tests.XmlLicenseSample.xml");
+
+            XmlNamespaceManager namespaceManager = new XmlNamespaceManager(doc.NameTable);
+            namespaceManager.AddNamespace("r", "urn:mpeg:mpeg21:2003:01-REL-R-NS");
+            transform.Context = doc.DocumentElement.SelectSingleNode("//r:issuer[1]", namespaceManager) as XmlElement;
+            DummyDecryptor decryptor = new DummyDecryptor { ContentToReturn = "Encrypted Content" };
+            transform.Decryptor = decryptor;
+            transform.LoadInput(doc);
+            XmlDocument output = transform.GetOutput(typeof(XmlDocument)) as XmlDocument;
+
+            string decodedXml = @"<r:license xmlns:r=""urn:mpeg:mpeg21:2003:01-REL-R-NS"" licenseId=""{00000000-0000-0000-0000-123456789012}"">";
+            decodedXml += "<r:title>Test License</r:title><r:grant>Encrypted Content</r:grant>";
+            decodedXml += "<r:issuer><r:details><r:timeOfIssue>2017-01-71T00:00:00Z</r:timeOfIssue></r:details></r:issuer></r:license>";
+            Assert.NotNull(output);
+            Assert.Equal(decodedXml, output.OuterXml);
+        }
+
+        [Fact]
+        public void GetOutput_InvalidType()
+        {
+            Assert.Throws<ArgumentException>(() => transform.GetOutput(typeof(string)));
+        }
+
+        private XmlDocument GetDocumentFromResource(string resourceName)
+        {
+            XmlDocument doc = new XmlDocument();
+            using (Stream stream = TestHelpers.LoadResourceStream(resourceName))
+            using (StreamReader streamReader = new StreamReader(stream))
+            {
+                string originalXml = streamReader.ReadToEnd();
+                doc.LoadXml(originalXml);
+            }
+
+            return doc;
         }
     }
 }
