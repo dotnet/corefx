43aa376ed9192b87eb5ebb66bc6a625790325e48
nbarbettini <nathanaelb@gmail.com>
2017-04-06 06:57:26 -0700

    Add TryAdd and Remove to CollectionExtensions

--- src/System.Collections/src/System/Collections/Generic/CollectionExtensions.cs
+++ src/System.Collections/src/System/Collections/Generic/CollectionExtensions.cs
@@ -37,5 +37,43 @@ namespace System.Collections.Generic
             TValue value;            
             return dictionary.TryGetValue(key, out value) ? value : defaultValue;
         }
+
+        public static bool TryAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            if (dictionary == null)
+            {
+                throw new ArgumentNullException(nameof(dictionary));
+            }
+
+            try
+            {
+                dictionary.Add(key, value);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static bool Remove<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, out TValue value)
+        {
+            if (dictionary == null)
+            {
+                throw new ArgumentNullException(nameof(dictionary));
+            }
+
+            try
+            {
+                value = dictionary[key];
+                dictionary.Remove(key);
+                return true;
+            }
+            catch (Exception)
+            {
+                value = default(TValue);
+                return false;
+            }
+        }
     }
 }
