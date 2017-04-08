5de4f3a503e010cb263586525623f1d2afd3c6e8
Santiago Fernandez <safern@microsoft.com>
2017-04-07 19:52:37 -0700

    nit fix

--- src/System.Private.Xml/tests/XmlSerializer/XmlSerializerTests.netcoreapp.cs
+++ src/System.Private.Xml/tests/XmlSerializer/XmlSerializerTests.netcoreapp.cs
@@ -12,7 +12,7 @@ public static partial class XmlSerializerTests
 
     static XmlSerializerTests()
     {
-        var method = typeof(XmlSerializer).GetMethod(SerializationModeSetterName, BindingFlags.NonPublic | BindingFlags.Static);
+        MethodInfo method = typeof(XmlSerializer).GetMethod(SerializationModeSetterName, BindingFlags.NonPublic | BindingFlags.Static);
         Assert.True(method != null, $"No method named {SerializationModeSetterName}");
         method.Invoke(null, new object[] { 1 });
     }
