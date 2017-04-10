75d5bffe6f8b3c80d8d7b33781107d117d4faf4c
Omar Tawfik <OmarTawfik@users.noreply.github.com>
2017-04-10 12:52:22 -0700

    Add empty System.Runtime.CompilerServices.RuntimeFeature.IsSupported() API (#10779)
    Signed-off-by: dotnet-bot-corefx-mirror <dotnet-bot@microsoft.com>

---
+++ src/Common/src/CoreLib/System/Runtime/CompilerServices/RuntimeFeature.cs
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Runtime.CompilerServices
+{
+    public static class RuntimeFeature
+    {
+        /// <summary>
+        /// Checks whether a certain feature is supported by the Runtime.
+        /// </summary>
+        public static bool IsSupported(string feature)
+        {
+            // No features are supported for now.
+            // These features should be added as public static readonly string fields in the same class.
+            return false;
+        }
+    }
+}
