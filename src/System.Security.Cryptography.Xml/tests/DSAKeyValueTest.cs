eef416788a90a94680396d3ffe8a491e952e3617
Krzysztof Wicher <kwicher@microsoft.com>
2017-04-10 14:19:14 -0700

    fix KeyInfoTest.ImportKeyNode

--- src/System.Security.Cryptography.Xml/tests/DSAKeyValueTest.cs
+++ src/System.Security.Cryptography.Xml/tests/DSAKeyValueTest.cs
@@ -123,5 +123,42 @@ namespace System.Security.Cryptography.Xml.Tests
             DSAKeyValue dsa1 = new DSAKeyValue();
             Assert.Throws<ArgumentNullException>(() => dsa1.LoadXml(null));
         }
+
+        [Fact]
+        public void ImportDSAKeyValue()
+        {
+            string p = "nHoAAyz1ZrA/4ajJnY09oVYvr/juj6cJHhVKnrIpIlWRh2EHIFsDiHR7hFvTvvTwmPiNRKBqO7Q1LST5qabEkvbvhk11d5wwItTWmLqO969NkUzA2cTqebEdshDhnl8YN7DrkCdrYu0PE0ZL/ho5yp6aRYME/Zoq2qDet+He3v9ZkjlJLg5mLPK6Q6tmLeSNTob/6fABv1RCREsyvYegWp4TxLvrgb/fd6Wtt7GC4fwWQlBeRmBIYFklNcE2g6iYMQOkgM7D/a0YrhUI3Vy3vfFNR/kKF+OBhB82V+QPhqPbdkXA+StpwOYj48mHHyr3ZvmlQdHJSkEY/p5JFbG8vw==";
+            string q = "ni1fE84dSrJerbAmEAYI/0w+q97KSFmqrzUFyHk2V48=";
+            string g = "m46FXiDGgIUpO7ehefrbdGqVn1+VOEc6Cyxi5xKXIDTYtLST6wy+FPybWOuYv1Sya8AeEOmz6HAht/k3GQZwVkt1ZSZCJZw8/p/zwyHTST+NdnCHXGDGvFh/wnEa0UsmTBmqBsxPiCOahWXsQ4VvTI5exRsu+/qwc/Nqbw1K8eq6pzn67ZA2/RyLepLPZGBo9vkdPmYtFKRj3PmC7/mjVWfA3tyPa0coFIx9XtjPiVtnmG9lJ9dVhGIY2cO7j8iDdBLdx6enMu67sDyHlPBDpLYSRx2Qqkcy1IdlAyxF6fOnxsDBC/N7LXNHea52XQn1TO2CvbIG7KXI8SxLr+CVbA==";
+            string y = "Yt23Dtp7UT1h/HsE6qlLBX+PIwdK0N0p0wFdWmIkw91hD41Y7OKC5xXs5jkHqSfQtiGYsUaS4aYoxQiuE9wnjiPUEii5Zau7s3jVkeZAvmRDQkjvl6SPb7kNIcw2PYdL9wSsL1euaTCWeH6m7fhO0tBJN0INYFXZ3ca4bWBYwNgs2UgCooaoO2df6eMf/1rFSr5gDZLqOtGgXJTOvb1MO/R8pTmaNmbkpGp4X5ApbExzqyb9v4dPNna3qgPBJn0vwTw0RUyLPlFOjaMHBeoRuC77e2TNqVMXGxRRnlB25XJsCsFY/+bePsPQZXGIFB1gsMzDtB/M6WKcPuAsevZDrw==";
+            string seed = "3Y9u5gc5dVKPhwiUzwmMV09ojummGzJ87A0llG9ey60=";
+            string pgenCounter = "AWs=";
+            string dsaKeyXml = $@"<KeyInfo xmlns=""http://www.w3.org/2000/09/xmldsig#""><KeyValue><DSAKeyValue><P>{p}</P><Q>{q}</Q><G>{g}</G><Y>{y}</Y><Seed>{seed}</Seed><PgenCounter>{pgenCounter}</PgenCounter></DSAKeyValue></KeyValue></KeyInfo>";
+
+            var expected = new Dictionary<string, string>()
+            {
+                { "P", p },
+                { "Q", q },
+                { "G", g },
+                { "Y", y },
+                { "Seed", seed },
+                { "PgenCounter", pgenCounter }
+            };
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(dsaKeyXml);
+
+            KeyInfo info = new KeyInfo();
+            info.LoadXml(doc.DocumentElement);
+
+            XmlElement el = info.GetXml();
+
+            foreach (var kv in expected)
+            {
+                XmlNode node = el.SelectSingleNode($"//*[local-name()='DSAKeyValue']/*[local-name()='{kv.Key}']");
+                Assert.NotNull(node);
+                Assert.Equal(kv.Value, node.InnerText);
+            }
+        }
     }
 }
