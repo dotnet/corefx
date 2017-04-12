b81eab27039ad59ad28f5d48804fc99eee9f27dd
Jan Kotas <jkotas@microsoft.com>
2017-04-11 17:44:40 -0700

    Move files to shared CoreLib partition (dotnet/coreclr#10885)
    Signed-off-by: dotnet-bot-corefx-mirror <dotnet-bot@microsoft.com>

---
+++ src/Common/src/System.Private.CoreLib/shared/System/Threading/ThreadStartException.cs
@@ -0,0 +1,29 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.Serialization;
+
+namespace System.Threading
+{
+    [Serializable]
+    public sealed class ThreadStartException : SystemException
+    {
+        internal ThreadStartException()
+            : base(SR.Arg_ThreadStartException)
+        {
+            HResult = __HResults.COR_E_THREADSTART;
+        }
+
+        internal ThreadStartException(Exception reason)
+            : base(SR.Arg_ThreadStartException, reason)
+        {
+            HResult = __HResults.COR_E_THREADSTART;
+        }
+
+        internal ThreadStartException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
