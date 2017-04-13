5a82f0f450d91cbb640f178c73e7827cd7e8fa6c
Viktor Hofer <viktor.hofer@microsoft.com>
2017-04-13 17:44:39 +0200

    Unseal crypto types and members

--- src/System.Security.Cryptography.Algorithms/src/System/Security/Cryptography/SHA1Managed.cs
+++ src/System.Security.Cryptography.Algorithms/src/System/Security/Cryptography/SHA1Managed.cs
@@ -9,7 +9,7 @@ namespace System.Security.Cryptography
 {
     [EditorBrowsable(EditorBrowsableState.Never)]
     // SHA1Managed has a copy of the same implementation as SHA1
-    public sealed class SHA1Managed : SHA1
+    public class SHA1Managed : SHA1
     {
         private readonly HashProvider _hashProvider;
 
@@ -19,17 +19,17 @@ namespace System.Security.Cryptography
             HashSizeValue = _hashProvider.HashSizeInBytes * 8;
         }
 
-        protected sealed override void HashCore(byte[] array, int ibStart, int cbSize)
+        protected override void HashCore(byte[] array, int ibStart, int cbSize)
         {
             _hashProvider.AppendHashData(array, ibStart, cbSize);
         }
 
-        protected sealed override byte[] HashFinal()
+        protected override byte[] HashFinal()
         {
             return _hashProvider.FinalizeHashAndReset();
         }
 
-        public sealed override void Initialize()
+        public override void Initialize()
         {
             // Nothing to do here. We expect HashAlgorithm to invoke HashFinal() and Initialize() as a pair. This reflects the 
             // reality that our native crypto providers (e.g. CNG) expose hash finalization and object reinitialization as an atomic operation.
