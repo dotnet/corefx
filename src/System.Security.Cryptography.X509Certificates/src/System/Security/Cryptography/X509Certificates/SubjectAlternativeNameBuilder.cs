5fe5f9aae7b2987adc7082f90712b265bee5eefc
Jeremy Barton <jbarton@microsoft.com>
2017-04-18 14:06:42 -0700

    Enable the creation of certificates and cert requests
    The new CertificateRequest class allows a caller to specify a subject name,
    public key, and extensions for an extension request into a PKCS#10 blob to send
    to a certificate authority.
    
    The new CertificateRequest class allows a caller to set all of the fields in an
    X.509 v3 certificate, with the exception of the v2 IssuerUniqueId and SubjectUniqueId
    fields, as they are considered deprecated.  Accelerators exist for self-signing,
    as well as chain-signing given an existing X509Certificate2 instance with a
    private key already associated.
    
    This change also adds
    CopyWithPrivateKey(this X509Certificate2 cert, {RSA|DSA|ECDSA} key)
    to enable scenarios where a public cert and private key are held independently
    but a unification is desired.  This makes UNIX-style .pem/.key loading easier,
    but still requires users understand how to load the .key file.

---
+++ src/System.Security.Cryptography.X509Certificates/src/System/Security/Cryptography/X509Certificates/SubjectAlternativeNameBuilder.cs
@@ -0,0 +1,65 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using System.Net;
+using Internal.Cryptography;
+
+namespace System.Security.Cryptography.X509Certificates
+{
+    public sealed class SubjectAlternativeNameBuilder
+    {
+        // Because GeneralNames is a SEQUENCE, just make a rolling list, it doesn't need to be re-sorted.
+        private readonly List<byte[][]> _encodedTlvs = new List<byte[][]>();
+        private readonly GeneralNameEncoder _generalNameEncoder = new GeneralNameEncoder();
+
+        public void AddEmailAddress(string emailAddress)
+        {
+            if (string.IsNullOrEmpty(emailAddress))
+                throw new ArgumentOutOfRangeException(nameof(emailAddress), SR.Arg_EmptyOrNullString);
+
+            _encodedTlvs.Add(_generalNameEncoder.EncodeEmailAddress(emailAddress));
+        }
+
+        public void AddDnsName(string dnsName)
+        {
+            if (string.IsNullOrEmpty(dnsName))
+                throw new ArgumentOutOfRangeException(nameof(dnsName), SR.Arg_EmptyOrNullString);
+
+            _encodedTlvs.Add(_generalNameEncoder.EncodeDnsName(dnsName));
+        }
+
+        public void AddUri(Uri uri)
+        {
+            if (uri == null)
+                throw new ArgumentNullException(nameof(uri));
+
+            _encodedTlvs.Add(_generalNameEncoder.EncodeUri(uri));
+        }
+
+        public void AddIpAddress(IPAddress ipAddress)
+        {
+            if (ipAddress == null)
+                throw new ArgumentNullException(nameof(ipAddress));
+
+            _encodedTlvs.Add(_generalNameEncoder.EncodeIpAddress(ipAddress));
+        }
+
+        public void AddUserPrincipalName(string upn)
+        {
+            if (string.IsNullOrEmpty(upn))
+                throw new ArgumentOutOfRangeException(nameof(upn), SR.Arg_EmptyOrNullString);
+
+            _encodedTlvs.Add(_generalNameEncoder.EncodeUserPrincipalName(upn));
+        }
+
+        public X509Extension Build(bool critical=false)
+        {
+            return new X509Extension(
+                Oids.SubjectAltName,
+                DerEncoder.ConstructSequence(_encodedTlvs),
+                critical);
+        }
+    }
+}
