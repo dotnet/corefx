2ce3b635163b0e183b403b3f77b16e63c4e9a4a5
Jon Hanna <jon@hackcraft.net>
2017-04-19 13:47:29 +0100

    Prohibit Expression.Bind with members of open generic types.
    Closes #8081

--- src/System.Linq.Expressions/src/System/Linq/Expressions/MemberAssignment.cs
+++ src/System.Linq.Expressions/src/System/Linq/Expressions/MemberAssignment.cs
@@ -81,28 +81,31 @@ namespace System.Linq.Expressions
 
         private static void ValidateSettableFieldOrPropertyMember(MemberInfo member, out Type memberType)
         {
-            FieldInfo fi = member as FieldInfo;
-            if (fi == null)
+            Type decType = member.DeclaringType;
+            if (decType == null)
             {
-                PropertyInfo pi = member as PropertyInfo;
-                if (pi == null)
-                {
-                    throw Error.ArgumentMustBeFieldInfoOrPropertyInfo(nameof(member));
-                }
-                if (!pi.CanWrite)
-                {
-                    throw Error.PropertyDoesNotHaveSetter(pi, nameof(member));
-                }
-                memberType = pi.PropertyType;
+                throw Error.NotAMemberOfAnyType(member, nameof(member));
             }
-            else
-            {
-                if (fi.DeclaringType == null)
-                {
-                    throw Error.NotAMemberOfAnyType(fi, nameof(member));
-                }
 
-                memberType = fi.FieldType;
+            // Null paramName as there are two paths here with different parameter names at the API
+            TypeUtils.ValidateType(decType, null);
+            switch (member)
+            {
+                case PropertyInfo pi:
+                    if (!pi.CanWrite)
+                    {
+                        throw Error.PropertyDoesNotHaveSetter(pi, nameof(member));
+                    }
+
+                    memberType = pi.PropertyType;
+                    break;
+
+                case FieldInfo fi:
+                    memberType = fi.FieldType;
+                    break;
+
+                default:
+                    throw Error.ArgumentMustBeFieldInfoOrPropertyInfo(nameof(member));
             }
         }
     }
