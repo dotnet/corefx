f59cfc4a19d8b2ee3d5024e16ed3989d19abe8ca
dhoehna <dhoehna@yahoo.com>
2017-04-21 14:55:43 -0700

    Removing inline data, replaced with member data.

--- src/System.Configuration.ConfigurationManager/tests/System/Configuration/StringValidatorTests.cs
+++ src/System.Configuration.ConfigurationManager/tests/System/Configuration/StringValidatorTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
 using System.Configuration;
 using Xunit;
 
@@ -76,20 +77,7 @@ namespace System.ConfigurationTests
         }
 
         [Theory]
-        [InlineData("Hello-")]
-        [InlineData("Hello_")]
-        [InlineData("-Hello")]
-        [InlineData("_Hello")]
-        [InlineData("He-llo")]
-        [InlineData("He_llo")]
-        [InlineData("H-e-l-l-o")]
-        [InlineData("H_e_l_l_o_")]
-        [InlineData("H_e-l_l-o_")]
-        [InlineData("He__llo")]
-        [InlineData("He--;o")]
-        [InlineData("H_-el- -o")]
-        [InlineData("_")]
-        [InlineData("-")]
+        [MemberData(nameof(GetInvalidStrings))]
         public void Validate_UsinginvalidCharacters(string stringToValidate)
         {
             StringValidator validator = new StringValidator(1, 20, "_-");
@@ -101,6 +89,25 @@ namespace System.ConfigurationTests
         {
             StringValidator validator = new StringValidator(5);
             validator.Validate("Hello");
+        }
+
+
+        public static IEnumerable<object[]> GetInvalidStrings()
+        {
+            yield return new object[] { "Hello-" };
+            yield return new object[] { "Hello_" };
+            yield return new object[] { "-Hello" };
+            yield return new object[] { "_Hello" };
+            yield return new object[] { "He-llo" };
+            yield return new object[] { "He_llo" };
+            yield return new object[] { "H-e-l-l-o" };
+            yield return new object[] { "H_e_l_l_o_" };
+            yield return new object[] { "H_e-l_l-o_" };
+            yield return new object[] { "He__llo" };
+            yield return new object[] { "He--llo" };
+            yield return new object[] { "H_-el-l-o" };
+            yield return new object[] { "_" };
+            yield return new object[] { "-" };
         }
     }
 }
