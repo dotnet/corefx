37d8aab5eca39b44ef05d19d4df25040b0e8ca1a
ektrah <klaus.hartke@gmail.com>
2017-04-22 00:59:47 +0200

    this

--- src/System.Memory/src/System/SpanExtensions.cs
+++ src/System.Memory/src/System/SpanExtensions.cs
@@ -299,7 +299,7 @@ namespace System
         /// <summary>
         /// Determines whether two sequences overlap.
         /// </summary>
-        public static unsafe bool Overlaps<T>(Span<T> first, ReadOnlySpan<T> second)
+        public static unsafe bool Overlaps<T>(this Span<T> first, ReadOnlySpan<T> second)
         {
             ref T firstRef = ref first.DangerousGetPinnableReference();
             ref T secondRef = ref second.DangerousGetPinnableReference();
@@ -317,7 +317,7 @@ namespace System
         /// <summary>
         /// Determines whether two sequences overlap.
         /// </summary>
-        public static unsafe bool Overlaps<T>(ReadOnlySpan<T> first, ReadOnlySpan<T> second)
+        public static unsafe bool Overlaps<T>(this ReadOnlySpan<T> first, ReadOnlySpan<T> second)
         {
             ref T firstRef = ref first.DangerousGetPinnableReference();
             ref T secondRef = ref second.DangerousGetPinnableReference();
