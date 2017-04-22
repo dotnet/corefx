10727586ba16f18062a92c4ee38fd8787f66e92c
Tomas Matousek <tomat@microsoft.com>
2017-04-22 11:55:57 -0700

    Harden finalizers

---
+++ src/System.Reflection.Metadata/src/System/Reflection/Internal/Utilities/CriticalDisposableObject.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.ConstrainedExecution;
+
+namespace System.Reflection.Internal
+{
+    internal abstract class CriticalDisposableObject : CriticalFinalizerObject, IDisposable
+    {
+        protected abstract void Dispose(bool disposing);
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~CriticalDisposableObject()
+        {
+            Dispose(false);
+        }
+    }
+}
