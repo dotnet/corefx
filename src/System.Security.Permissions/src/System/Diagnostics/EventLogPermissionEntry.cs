f42c3898ca17c2f9a979a70563115c7141c92e34
Jay Guadagnoli <jguady@gmail.com>
2017-04-22 02:02:10 -0600

    License for S.Diag files

--- src/System.Security.Permissions/src/System/Diagnostics/EventLogPermissionEntry.cs
+++ src/System.Security.Permissions/src/System/Diagnostics/EventLogPermissionEntry.cs
@@ -1,4 +1,7 @@
-﻿namespace System.Diagnostics
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+namespace System.Diagnostics
 {
     [Serializable]
     public class EventLogPermissionEntry
