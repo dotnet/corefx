e7e3c581870787d87393e674db22589ad56e944c
David Shulman <david.shulman@microsoft.com>
2017-04-24 21:00:18 -0700

    Root cause PostScenarioTests (Http) test failures on NETFX
    Fixed a test bug in PostAuthHelper which caused the wrong entity-body
    semantics to get used during the test. This actually exposed a netfx
    bug dealing with ambiguous semantics (Content-Length vs. chunked).
    
    After fixing this test bug, it then exposed a product bug where we
    leak an InvalidOperationException that isn't wrapped properly by an
    HttpRequestException. The scenario that exposed this related to
    POST'ing content that is non-rewindable and having to resubmit the
    POST due to authentication challenges. This test was passing on
    .NET Core because the test was written using the wrong assumptions.
    It failed on .NET Framework which exposed the problem.
    
    The InvalidOperationException comes from when a StreamContent's
    SerializeToStreamAsync method is called and the content needs to be rewound.
    If the content can't be rewound, then the exception is thrown with the
    error message "The stream was already consumed. It cannot be read again."
    
    Updated the WinHttpHandler and CurlHandler to wrap the InvalidOperationException
    in an HttpRequestException. Updated the test accordingly.
    
    Fixes #17621.

--- src/System.Net.Http.WinHttpHandler/src/System/Net/Http/WinHttpHandler.cs
+++ src/System.Net.Http.WinHttpHandler/src/System/Net/Http/WinHttpHandler.cs
@@ -1249,7 +1249,7 @@ namespace System.Net.Http
                 // If the exception was due to the cancellation token being canceled, throw cancellation exception.
                 state.Tcs.TrySetCanceled(state.CancellationToken);
             }
-            else if (ex is WinHttpException || ex is IOException)
+            else if (ex is WinHttpException || ex is IOException || ex is InvalidOperationException)
             {
                 // Wrap expected exceptions as HttpRequestExceptions since this is considered an error during
                 // execution. All other exception types, including ArgumentExceptions and ProtocolViolationExceptions
