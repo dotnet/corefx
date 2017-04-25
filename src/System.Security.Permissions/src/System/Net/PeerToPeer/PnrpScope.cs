bd7569b3d09ba0934551f9f025f41a10812bf28b
Jay Guadagnoli <jguady@gmail.com>
2017-04-25 02:35:57 -0600

    Added OleDbPermissionAttribute

--- src/System.Security.Permissions/src/System/Net/PeerToPeer/PnrpScope.cs
+++ src/System.Security.Permissions/src/System/Net/PeerToPeer/PnrpScope.cs
@@ -1,10 +1,13 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-public enum PnrpScope
+namespace System.Net.PeerToPeer
 {
-    All = 0,
-    Global = 1,
-    LinkLocal = 3,
-    SiteLocal = 2,
+    public enum PnrpScope
+    {
+        All = 0,
+        Global = 1,
+        LinkLocal = 3,
+        SiteLocal = 2,
+    }
 }
