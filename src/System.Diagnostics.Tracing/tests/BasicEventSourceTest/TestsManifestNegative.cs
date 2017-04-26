d281f66a286e4d30882d5dc3a3902dce5a7b029f
Santiago Fernandez Madero <safern@microsoft.com>
2017-04-26 12:24:53 -0400

    Fixed test under System.Diagnostics.Tracing (#18965)
    * Fix test to get resource string from environment on desktop

--- src/System.Diagnostics.Tracing/tests/BasicEventSourceTest/TestsManifestNegative.cs
+++ src/System.Diagnostics.Tracing/tests/BasicEventSourceTest/TestsManifestNegative.cs
@@ -31,9 +31,16 @@ namespace BasicEventSourceTests
         private static string GetResourceStringFromReflection(string key)
         {
             BindingFlags flags = BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
-            Type sr = typeof(EventSource).Assembly.GetType("System.SR", throwOnError: true, ignoreCase: false);
-            PropertyInfo resourceProp = sr.GetProperty(key, flags);
-            return (string)resourceProp.GetValue(null);
+            if (!PlatformDetection.IsFullFramework)
+            {
+              Type sr = typeof(EventSource).Assembly.GetType("System.SR", throwOnError: true, ignoreCase: false);
+              PropertyInfo resourceProp = sr.GetProperty(key, flags);
+              return (string)resourceProp.GetValue(null);
+            }
+
+            Type[] paramsType = new Type[] { typeof(string) };
+            MethodInfo getResourceString = typeof(Environment).GetMethod("GetResourceString", flags, null, paramsType, null);
+            return (string)getResourceString.Invoke(null, new object[] { key });
         }
         #endregion
 
