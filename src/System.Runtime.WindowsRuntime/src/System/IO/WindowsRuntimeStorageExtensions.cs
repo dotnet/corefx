b854ee99116e5e91d88683346bdf3ca5da7e8c84
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2017-04-26 19:38:23 -0700

    Add WinRT to SafeFileHandle converters
    - Adds methods to convert IStorageFolder/IStorageFile to SafeFileHandle
    - Adds test project and tests to S.R.WindowsRuntime

--- src/System.Runtime.WindowsRuntime/src/System/IO/WindowsRuntimeStorageExtensions.cs
+++ src/System.Runtime.WindowsRuntime/src/System/IO/WindowsRuntimeStorageExtensions.cs
@@ -4,6 +4,7 @@
 
 using System.Diagnostics;
 using System.Threading.Tasks;
+using Microsoft.Win32.SafeHandles;
 using Windows.Storage;
 using Windows.Storage.FileProperties;
 using Windows.Storage.Streams;
@@ -51,7 +52,6 @@ namespace System.IO
 
             return OpenStreamForWriteAsyncCore(windowsRuntimeFile, 0);
         }
-
 
         private static async Task<Stream> OpenStreamForWriteAsyncCore(this IStorageFile windowsRuntimeFile, long offset)
         {
@@ -173,6 +173,146 @@ namespace System.IO
                 WinRtIOHelper.NativeExceptionToIOExceptionInfo(ex).Throw();
                 return null;
             }
+        }
+
+        [CLSCompliant(false)]
+        public static SafeFileHandle CreateSafeFileHandle(
+            this IStorageFile windowsRuntimeFile,
+            FileAccess access = FileAccess.ReadWrite,
+            FileShare share = FileShare.Read,
+            FileOptions options = FileOptions.None)
+        {
+            if (windowsRuntimeFile == null)
+                throw new ArgumentNullException(nameof(windowsRuntimeFile));
+
+            HANDLE_ACCESS_OPTIONS accessOptions = FileAccessToHandleAccessOptions(access);
+            HANDLE_SHARING_OPTIONS sharingOptions = FileShareToHandleSharingOptions(share);
+            HANDLE_OPTIONS handleOptions = FileOptionsToHandleOptions(options);
+
+            IStorageItemHandleAccess handleAccess = ((object)windowsRuntimeFile) as IStorageItemHandleAccess;
+
+            if (handleAccess == null)
+                return null;
+
+            SafeFileHandle handle;
+
+            int result = handleAccess.Create(
+                accessOptions,
+                sharingOptions,
+                handleOptions,
+                IntPtr.Zero,
+                out handle);
+
+            if (result != 0)
+                throw Win32Marshal.GetExceptionForWin32Error(Win32Marshal.TryMakeWin32ErrorCodeFromHR(result), windowsRuntimeFile.Name);
+
+            return handle;
+        }
+
+        [CLSCompliant(false)]
+        public static SafeFileHandle CreateSafeFileHandle(
+            this IStorageFolder rootDirectory,
+            string relativePath,
+            FileMode mode)
+        {
+            return rootDirectory.CreateSafeFileHandle(relativePath, mode, (mode == FileMode.Append ? FileAccess.Write : FileAccess.ReadWrite));
+        }
+
+        [CLSCompliant(false)]
+        public static SafeFileHandle CreateSafeFileHandle(
+            this IStorageFolder rootDirectory,
+            string relativePath,
+            FileMode mode,
+            FileAccess access,
+            FileShare share = FileShare.Read,
+            FileOptions options = FileOptions.None)
+        {
+            if (rootDirectory == null)
+                throw new ArgumentNullException(nameof(rootDirectory));
+            if (relativePath == null)
+                throw new ArgumentNullException(nameof(relativePath));
+
+            HANDLE_CREATION_OPTIONS creationOptions = FileModeToCreationOptions(mode);
+            HANDLE_ACCESS_OPTIONS accessOptions = FileAccessToHandleAccessOptions(access);
+            HANDLE_SHARING_OPTIONS sharingOptions = FileShareToHandleSharingOptions(share);
+            HANDLE_OPTIONS handleOptions = FileOptionsToHandleOptions(options);
+
+            IStorageFolderHandleAccess handleAccess = ((object)rootDirectory) as IStorageFolderHandleAccess;
+
+            if (handleAccess == null)
+                return null;
+
+            SafeFileHandle handle;
+
+            int result = handleAccess.Create(
+                relativePath,
+                creationOptions,
+                accessOptions,
+                sharingOptions,
+                handleOptions,
+                IntPtr.Zero,
+                out handle);
+
+            if (result != 0)
+                throw Win32Marshal.GetExceptionForWin32Error(Win32Marshal.TryMakeWin32ErrorCodeFromHR(result), relativePath);
+
+            return handle;
+        }
+
+        private static HANDLE_ACCESS_OPTIONS FileAccessToHandleAccessOptions(FileAccess access)
+        {
+            switch (access)
+            {
+                case FileAccess.ReadWrite:
+                    return HANDLE_ACCESS_OPTIONS.HAO_READ | HANDLE_ACCESS_OPTIONS.HAO_WRITE;
+                case FileAccess.Read:
+                    return HANDLE_ACCESS_OPTIONS.HAO_READ;
+                case FileAccess.Write:
+                    return HANDLE_ACCESS_OPTIONS.HAO_WRITE;
+            }
+
+            throw new ArgumentOutOfRangeException(nameof(access));
+        }
+
+        private static HANDLE_SHARING_OPTIONS FileShareToHandleSharingOptions(FileShare share)
+        {
+            if ((share & FileShare.Inheritable) != 0)
+                throw new NotSupportedException(SR.NotSupported_Inheritable);
+            if (share < FileShare.None || share > (FileShare.ReadWrite | FileShare.Delete))
+                throw new ArgumentOutOfRangeException(nameof(share));
+
+            HANDLE_SHARING_OPTIONS sharingOptions = HANDLE_SHARING_OPTIONS.HSO_SHARE_NONE;
+            if ((share & FileShare.Read) != 0)
+                sharingOptions |= HANDLE_SHARING_OPTIONS.HSO_SHARE_READ;
+            if ((share & FileShare.Write) != 0)
+                sharingOptions |= HANDLE_SHARING_OPTIONS.HSO_SHARE_WRITE;
+            if ((share & FileShare.Delete) != 0)
+                sharingOptions |= HANDLE_SHARING_OPTIONS.HSO_SHARE_DELETE;
+
+            return sharingOptions;
+        }
+
+        private static HANDLE_OPTIONS FileOptionsToHandleOptions(FileOptions options)
+        {
+            if ((options & FileOptions.Encrypted) != 0)
+                throw new NotSupportedException(SR.NotSupported_Encrypted);
+            if (options != FileOptions.None && (options &
+                ~(FileOptions.WriteThrough | FileOptions.Asynchronous | FileOptions.RandomAccess | FileOptions.DeleteOnClose |
+                  FileOptions.SequentialScan | (FileOptions)0x20000000 /* NoBuffering */)) != 0)
+                throw new ArgumentOutOfRangeException(nameof(options));
+
+            return (HANDLE_OPTIONS)options;
+        }
+
+        private static HANDLE_CREATION_OPTIONS FileModeToCreationOptions(FileMode mode)
+        {
+            if (mode < FileMode.CreateNew || mode > FileMode.Append)
+                throw new ArgumentOutOfRangeException(nameof(mode));
+
+            if (mode == FileMode.Append)
+                return HANDLE_CREATION_OPTIONS.HCO_CREATE_ALWAYS;
+
+            return (HANDLE_CREATION_OPTIONS)mode;
         }
     }
 }
