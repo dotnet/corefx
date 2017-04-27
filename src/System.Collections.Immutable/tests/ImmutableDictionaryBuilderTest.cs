60af73c41d3bae052fc083151a3c98cb9f2411c5
Atsushi Kanamori <AtsushiKan@users.noreply.github.com>
2017-04-27 03:47:32 -0700

    Fix AoT failures in System.Collections.Immutable tests (#19014)
    Most of it DebuggerAttribute tests.
    
    One helper was using internal Reflection to its dirty
    work - substituted another way to do its dirty work.

--- src/System.Collections.Immutable/tests/ImmutableDictionaryBuilderTest.cs
+++ src/System.Collections.Immutable/tests/ImmutableDictionaryBuilderTest.cs
@@ -247,6 +247,7 @@ namespace System.Collections.Immutable.Tests
         }
 
         [Fact]
+        [SkipOnTargetFramework(TargetFrameworkMonikers.UapAot, "Cannot do DebuggerAttribute testing on UapAot: requires internal Reflection on framework types.")]
         public void DebuggerAttributesValid()
         {
             DebuggerAttributes.ValidateDebuggerDisplayReferences(ImmutableDictionary.CreateBuilder<string, int>());
