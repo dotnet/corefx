a73c62ffbe0698bed3871d098ed11607e250d54b
Vance Morrison <vancemorrison@comcast.net>
2017-04-27 12:42:27 -0700

    Added Readonly qualifer

--- src/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/Activity.cs
+++ src/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/Activity.cs
@@ -451,7 +451,7 @@ namespace System.Diagnostics
         private int _currentChildId;  // A unique number for all children of this activity.  
 
         // Used to generate an ID it represents the machine and process we are in.  
-        private static string s_uniqSuffix = "-" + GetRandomNumber().ToString("x") + ".";
+        private static readonly string s_uniqSuffix = "-" + GetRandomNumber().ToString("x") + ".";
 
         //A unique number inside the appdomain, randomized between appdomains. 
         //Int gives enough randomization and keeps hex-encoded s_currentRootId 8 chars long for most applications
