6597a6357cb81c0453683d2560ae739fa18cbe21
stone-li <v-stoli@microsoft.com>
2017-04-27 22:35:44 +0100

    Try to fix the issue in fedora (#19030)
    * Try to fix the issue in fedora
    
    * update
    
    We can't find the time zone setting which set "-0753" in the failed log.

--- src/System.Runtime.Serialization.Json/tests/DataContractJsonSerializer.cs
+++ src/System.Runtime.Serialization.Json/tests/DataContractJsonSerializer.cs
@@ -2505,7 +2505,7 @@ public static partial class DataContractJsonSerializerTests
             KnownTypes = new List<Type>()
         };
         var value = new DateTime(2010, 12, 1);
-        var offsetMinutes = (int)TimeZoneInfo.Local.GetUtcOffset(new DateTime()).TotalMinutes;
+        var offsetMinutes = (int)TimeZoneInfo.Local.GetUtcOffset(value).TotalMinutes;
         var timeZoneString = string.Format("{0:+;-}{1}", offsetMinutes, new TimeSpan(0, offsetMinutes, 0).ToString(@"hhmm"));
         var baseline = $"\"\\/Date({1291161600000 - offsetMinutes * 60 * 1000}{timeZoneString})\\/\"";
         var actual = SerializeAndDeserialize(value, baseline, dcjsSettings);
