cc549c4cc9d0ad37e73d59b6112fae3da89ee10e
James Lanng <james@jltc.co.uk>
2017-05-04 18:24:13 +0100

    TypeDelegator should behave as Type

--- src/System.Runtime/tests/System/Reflection/TypeDelegatorTests.netcoreapp.cs
+++ src/System.Runtime/tests/System/Reflection/TypeDelegatorTests.netcoreapp.cs
@@ -1,40 +1,113 @@
 using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace System.Reflection.Tests
 {
     public class TypeDelegatorNetcoreTests
     {
-        private static IEnumerable<object[]> SZArrayOrNotTypes()
+        private static readonly IEnumerable<Type> NonArrayBaseTypes = new Type[]
+         {
+            typeof(int),
+            typeof(void),
+            typeof(int*),
+            typeof(Outside),
+            typeof(Outside<>),
+            typeof(Outside<>).GetTypeInfo().GenericTypeParameters[0],
+            Type.GetTypeFromCLSID(default(Guid)),
+            new object().GetType().GetType()
+         };
+
+        [Fact]
+        public void IsSZArray_FalseForNonArrayTypes()
         {
-            yield return new object[] { typeof(int[]), true };
-            yield return new object[] { typeof(string[]), true };
-            yield return new object[] { typeof(void), false };
-            yield return new object[] { typeof(int), false };
-            yield return new object[] { typeof(int[]).MakeByRefType(), false };
-            yield return new object[] { typeof(int[,]), false };
-            yield return new object[] { typeof(TypeInfoTests), false };
-            yield return new object[] { Array.CreateInstance(typeof(int), new[] { 2 }, new[] { -1 }).GetType(), false };
-            yield return new object[] { Array.CreateInstance(typeof(int), new[] { 2 }, new[] { 1 }).GetType(), false };
-            yield return new object[] { Array.CreateInstance(typeof(int), new[] { 2 }, new[] { 0 }).GetType(), true };
-            yield return new object[] { typeof(int[][]), true };
-            yield return new object[] { Type.GetType("System.Int32[]"), true };
-            yield return new object[] { Type.GetType("System.Int32[*]"), false };
-            yield return new object[] { Type.GetType("System.Int32"), false };
-            yield return new object[] { typeof(int).MakeArrayType(), true };
-            yield return new object[] { typeof(int).MakeArrayType(1), false };
-            yield return new object[] { typeof(int).MakeArrayType().MakeArrayType(), true };
-            yield return new object[] { typeof(int).MakeArrayType(2), false };
-            yield return new object[] { typeof(Outside<int>.Inside<string>), false };
-            yield return new object[] { typeof(Outside<int>.Inside<string>[]), true };
-            yield return new object[] { typeof(Outside<int>.Inside<string>[,]), false };
-            yield return new object[] { Array.CreateInstance(typeof(Outside<int>.Inside<string>), new[] { 2 }, new[] { -1 }).GetType(), false };
+            foreach (Type type in NonArrayBaseTypes)
+            {
+                Assert.False(new TypeDelegator(type).IsSZArray);
+            }
         }
 
-        [Theory, MemberData(nameof(SZArrayOrNotTypes))]
-        public void IsSZArray(Type type, bool expected)
+        [Fact]
+        public void IsSZArray_TrueForSZArrayTypes()
         {
-            Assert.Equal(expected, new TypeDelegator(type).IsSZArray);
+            foreach (Type type in NonArrayBaseTypes.Select(nonArrayBaseType => nonArrayBaseType.MakeArrayType()))
+            {
+                Assert.True(new TypeDelegator(type).IsSZArray);
+            }
+        }
+
+        [Fact]
+        public void IsSZArray_FalseForVariableBoundArrayTypes()
+        {
+            foreach (Type type in NonArrayBaseTypes.Select(nonArrayBaseType => nonArrayBaseType.MakeArrayType(1)))
+            {
+                Assert.False(new TypeDelegator(type).IsSZArray);
+            }
+        }
+
+        [Fact]
+        public void IsSZArray_FalseForNonArrayByRefType()
+        {
+            Assert.False(new TypeDelegator(typeof(int).MakeByRefType()).IsSZArray);
+        }
+
+        [Fact]
+        public void IsSZArray_FalseForByRefSZArrayType()
+        {
+            Assert.False(new TypeDelegator(typeof(int[]).MakeByRefType()).IsSZArray);
+        }
+
+
+        [Fact]
+        public void IsSZArray_FalseForByRefVariableArrayType()
+        {
+            Assert.False(new TypeDelegator(typeof(int[,]).MakeByRefType()).IsSZArray);
+        }
+
+        [Fact]
+        public void IsVariableBoundArray_FalseForNonArrayTypes()
+        {
+            foreach (Type type in NonArrayBaseTypes)
+            {
+                Assert.False(new TypeDelegator(type).IsVariableBoundArray);
+            }
+        }
+
+        [Fact]
+        public void IsVariableBoundArray_FalseForSZArrayTypes()
+        {
+            foreach (Type type in NonArrayBaseTypes.Select(nonArrayBaseType => nonArrayBaseType.MakeArrayType()))
+            {
+                Assert.False(new TypeDelegator(type).IsVariableBoundArray);
+            }
+        }
+
+        [Fact]
+        public void IsVariableBoundArray_TrueForVariableBoundArrayTypes()
+        {
+            foreach (Type type in NonArrayBaseTypes.Select(nonArrayBaseType => nonArrayBaseType.MakeArrayType(1)))
+            {
+                Assert.True(new TypeDelegator(type).IsVariableBoundArray);
+            }
+        }
+
+        [Fact]
+        public void IsVariableBoundArray_FalseForNonArrayByRefType()
+        {
+            Assert.False(new TypeDelegator(typeof(int).MakeByRefType()).IsVariableBoundArray);
+        }
+
+        [Fact]
+        public void IsVariableBoundArray_FalseForByRefSZArrayType()
+        {
+            Assert.False(new TypeDelegator(typeof(int[]).MakeByRefType()).IsVariableBoundArray);
+        }
+
+
+        [Fact]
+        public void IsVariableBoundArray_FalseForByRefVariableArrayType()
+        {
+            Assert.False(new TypeDelegator(typeof(int[,]).MakeByRefType()).IsVariableBoundArray);
         }
     }
 }
