cbddb98edd6756d13baa0b4df81322512aaa868a
Peter Toonen <peter@toonen.io>
2017-05-05 17:27:43 +0200

    Source init-tools rather than execute (#19363)
    * Source init-tools rather than execute in order to prevent a new process from spawning.
    
    * Added comment to prevent change from being undone at a later point in time.

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 __scriptpath=$(cd "$(dirname "$0")"; pwd -P)
-$__scriptpath/init-tools.sh
-if [ $? -ne 0 ]; then
-    exit 1
-fi
+
+# Source the init-tools.sh script rather than execute in order to preserve ulimit values in child-processes. https://github.com/dotnet/corefx/issues/19152
+. $__scriptpath/init-tools.sh
 
 __toolRuntime=$__scriptpath/Tools
 __dotnet=$__toolRuntime/dotnetcli/dotnet
