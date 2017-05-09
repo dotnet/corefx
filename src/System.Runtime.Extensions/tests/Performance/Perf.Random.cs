bfaf925a5532717f2358ea8416c9f7c5840bd6c4
Steve Franceschelli <stephenmichaelf@gmail.com>
2017-05-09 10:59:55 -0400

    Add perf tests for Random.cs.

--- src/System.Runtime.Extensions/tests/Performance/Perf.Random.cs
+++ src/System.Runtime.Extensions/tests/Performance/Perf.Random.cs
@@ -49,5 +49,34 @@ namespace System.Tests
                         rand.Next(100, 10000); rand.Next(100, 10000); rand.Next(100, 10000);
                     }
         }
+
+        [Benchmark]
+        public void NextBytes()
+        {
+            Random rand = new Random(123456);
+            byte[] b1 = new byte[1000];
+            foreach (var iteration in Benchmark.Iterations)
+                using (iteration.StartMeasurement())
+                    for (int i = 0; i < 10000; i++)
+                    {
+                        rand.NextBytes(b1); rand.NextBytes(b1); rand.NextBytes(b1);
+                        rand.NextBytes(b1); rand.NextBytes(b1); rand.NextBytes(b1);
+                        rand.NextBytes(b1); rand.NextBytes(b1); rand.NextBytes(b1);
+                    }
+        }
+
+        [Benchmark]
+        public void NextDouble()
+        {
+            Random rand = new Random(123456);
+            foreach (var iteration in Benchmark.Iterations)
+                using (iteration.StartMeasurement())
+                    for (int i = 0; i < 40000; i++)
+                    {
+                        rand.NextDouble(); rand.NextDouble(); rand.NextDouble();
+                        rand.NextDouble(); rand.NextDouble(); rand.NextDouble();
+                        rand.NextDouble(); rand.NextDouble(); rand.NextDouble();
+                    }
+        }
     }
 }
