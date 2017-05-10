c0bedde49ce77ae281f32feecf684005924abcf0
Steve Franceschelli <stephenmichaelf@gmail.com>
2017-05-10 18:08:46 -0400

    Add performance tests for Environment.cs.

--- src/System.Runtime.Extensions/tests/Performance/Perf.Environment.cs
+++ src/System.Runtime.Extensions/tests/Performance/Perf.Environment.cs
@@ -4,6 +4,7 @@
 
 using Microsoft.Xunit.Performance;
 using System.Runtime.InteropServices;
+using Xunit;
 
 namespace System.Tests
 {
@@ -64,6 +65,106 @@ namespace System.Tests
                 // clear the variable that we set
                 Environment.SetEnvironmentVariable(env, null);
             }
+        }
+
+        [Benchmark]
+        public void GetEnvironmentVariables()
+        {
+            PerfUtils utils = new PerfUtils();
+            string env = utils.CreateString(15);
+            try
+            {
+                // setup the environment variable so we can read it
+                Environment.SetEnvironmentVariable(env, "value");
+
+                // read the valid environment variable for the test
+                foreach (var iteration in Benchmark.Iterations)
+                    using (iteration.StartMeasurement())
+                        for (int i = 0; i < 40000; i++)
+                        {
+                            Environment.GetEnvironmentVariables(); Environment.GetEnvironmentVariables(); Environment.GetEnvironmentVariables();
+                            Environment.GetEnvironmentVariables(); Environment.GetEnvironmentVariables(); Environment.GetEnvironmentVariables();
+                            Environment.GetEnvironmentVariables(); Environment.GetEnvironmentVariables(); Environment.GetEnvironmentVariables();
+                        }
+            }
+            finally
+            {
+                // clear the variable that we set
+                Environment.SetEnvironmentVariable(env, null);
+            }
+        }
+
+        [Benchmark]
+        [PlatformSpecific(TestPlatforms.OSX)]
+        [InlineData(Environment.SpecialFolder.System, Environment.SpecialFolderOption.None)]
+        public void GetFolderPath_OSX(Environment.SpecialFolder folder, Environment.SpecialFolderOption option)
+        {
+            foreach (var iteration in Benchmark.Iterations)
+                using (iteration.StartMeasurement())
+                    for (int i = 0; i < 40000; i++)
+                    {
+                        Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option);
+                        Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option);
+                        Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option);
+                    }
+        }
+
+        [Benchmark]
+        [PlatformSpecific(TestPlatforms.AnyUnix)]
+        [InlineData(Environment.SpecialFolder.LocalApplicationData, Environment.SpecialFolderOption.DoNotVerify)]
+        public void GetFolderPath_Unix(Environment.SpecialFolder folder, Environment.SpecialFolderOption option)
+        {
+            foreach (var iteration in Benchmark.Iterations)
+                using (iteration.StartMeasurement())
+                    for (int i = 0; i < 40000; i++)
+                    {
+                        Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option);
+                        Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option);
+                        Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option);
+                    }
+        }
+
+        [Benchmark]
+        [PlatformSpecific(TestPlatforms.Windows)]
+        [InlineData(Environment.SpecialFolder.Windows, Environment.SpecialFolderOption.None)]
+        public void GetFolderPath_Windows(Environment.SpecialFolder folder, Environment.SpecialFolderOption option)
+        {
+            foreach (var iteration in Benchmark.Iterations)
+                using (iteration.StartMeasurement())
+                    for (int i = 0; i < 40000; i++)
+                    {
+                        Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option);
+                        Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option);
+                        Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option); Environment.GetFolderPath(folder, option);
+                    }
+        }
+
+        [Benchmark]
+        [PlatformSpecific(TestPlatforms.AnyUnix)]
+        public void GetLogicalDrives_Unix()
+        {
+            foreach (var iteration in Benchmark.Iterations)
+                using (iteration.StartMeasurement())
+                    for (int i = 0; i < 40000; i++)
+                    {
+                        Environment.GetLogicalDrives(); Environment.GetLogicalDrives(); Environment.GetLogicalDrives();
+                        Environment.GetLogicalDrives(); Environment.GetLogicalDrives(); Environment.GetLogicalDrives();
+                        Environment.GetLogicalDrives(); Environment.GetLogicalDrives(); Environment.GetLogicalDrives();
+                    }
+        }
+
+        [Benchmark]
+        [PlatformSpecific(TestPlatforms.Windows)]
+        public void GetLogicalDrives_Windows()
+        {
+            foreach (var iteration in Benchmark.Iterations)
+                using (iteration.StartMeasurement())
+                    for (int i = 0; i < 40000; i++)
+                    {
+                        Environment.GetLogicalDrives(); Environment.GetLogicalDrives(); Environment.GetLogicalDrives();
+                        Environment.GetLogicalDrives(); Environment.GetLogicalDrives(); Environment.GetLogicalDrives();
+                        Environment.GetLogicalDrives(); Environment.GetLogicalDrives(); Environment.GetLogicalDrives();
+                    }
         }
     }
 }
