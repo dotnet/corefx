b1b41638523be5ec9ff1ab64fdb0fe1694ac981c
Matt Mitchell <mmitche@microsoft.com>
2017-05-11 15:17:17 -0700

    Move pipelines to sub directory

--- buildpipeline/pipelinejobs.groovy
+++ buildpipeline/pipelinejobs.groovy
@@ -12,7 +12,7 @@ def branch = GithubBranchName
 // Define innerloop testing.  These jobs run on every merge and a subset of them run on every PR, the ones
 // that don't run per PR can be requested via a magic phrase.
 // **************************
-def linuxPipeline = Pipeline.createPipelineForGithub(this, project, branch, 'portable-linux.groovy')
+def linuxPipeline = Pipeline.createPipelineForGithub(this, project, branch, 'buildpipeline/portable-linux.groovy')
 
 ['netcoreapp'].each { targetGroup ->
 	['Debug', 'Release'].each { configurationGroup ->
@@ -24,7 +24,7 @@ def linuxPipeline = Pipeline.createPipelineForGithub(this, project, branch, 'por
 }
 
 // Create a pipeline for portable windows
-def windowsPipeline = Pipeline.createPipelineForGithub(this, project, branch, 'portable-windows.groovy')
+def windowsPipeline = Pipeline.createPipelineForGithub(this, project, branch, 'buildpipeline/portable-windows.groovy')
 ['netcoreapp'].each { targetGroup ->
 	['Debug', 'Release'].each { configurationGroup ->
 		['Windows x64'].each { osName ->
