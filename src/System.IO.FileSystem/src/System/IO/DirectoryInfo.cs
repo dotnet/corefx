4ba52a0b6b3ad295310408f379bcb5c8ab968f9a
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2017-05-12 11:16:34 -0700

    Fix check for Unix
    Unix impl doesn't tolerate a trailing separator. Public api
    explicitly checks for this.

--- src/System.IO.FileSystem/src/System/IO/DirectoryInfo.cs
+++ src/System.IO.FileSystem/src/System/IO/DirectoryInfo.cs
@@ -415,7 +415,7 @@ namespace System.IO
 
             // Windows will throw if the source file/directory doesn't exist, we preemptively check
             // to make sure our cross platform behavior matches NetFX behavior.
-            if (!Exists && !FileSystem.Current.FileExists(fullSourcePath))
+            if (!Exists && !FileSystem.Current.FileExists(FullPath))
                 throw new DirectoryNotFoundException(SR.Format(SR.IO_PathNotFound_Path, FullPath));
 
             if (FileSystem.Current.DirectoryExists(fullDestDirName))
