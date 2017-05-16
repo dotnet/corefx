977d3ebdee6962948e414c01c571138527eb41b8
Huangli Wu <huanwu@microsoft.com>
2017-05-16 10:28:51 -0700

    Fix several code generator issues. (#19737)
    * Fix Sgen issues.
    
    * Updates based on the CR feedback.
    
    * Update the location of else if condition when type is TimeSpan
    
    * Update the namespace.

--- src/System.Private.Xml/src/System/Xml/Serialization/XmlAttributeAttribute.cs
+++ src/System.Private.Xml/src/System/Xml/Serialization/XmlAttributeAttribute.cs
@@ -6,11 +6,7 @@ using System;
 using System.Xml.Schema;
 
 
-#if XMLSERIALIZERGENERATOR
-namespace Microsoft.XmlSerializer.Generator
-#else
 namespace System.Xml.Serialization
-#endif
 {
     /// <devdoc>
     ///    <para>[To be supplied.]</para>
