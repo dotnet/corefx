4be4b45708d687021456731e6cb65639dc629ab3
Stephen Toub <stoub@microsoft.com>
2017-05-17 17:01:03 -0400

    Special-case DangerousAcceptAnyServerCertificateValidator on Unix
    In situations where setting ServerCertificateValidationCallback would result in a PlatformNotSupportedException, if it was set to DangerousAcceptAnyServerCertificateValidator instead simply disable CURLOPT_SSL_VERIFYPEER and CURLOPT_SSL_VERIFYHOST.  This enables a common case of `delegate { return true; }` to work on libcurls with non-SSL backends, such as the default on macOS.

--- src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.AcceptAllCerts.cs
+++ src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.AcceptAllCerts.cs
@@ -11,6 +11,8 @@ using Xunit;
 
 namespace System.Net.Http.Functional.Tests
 {
+    using Configuration = System.Net.Test.Common.Configuration;
+
     public class HttpClientHandler_DangerousAcceptAllCertificatesValidator_Test
     {
         [Fact]
@@ -19,6 +21,51 @@ namespace System.Net.Http.Functional.Tests
             Assert.NotNull(HttpClientHandler.DangerousAcceptAnyServerCertificateValidator);
             Assert.Same(HttpClientHandler.DangerousAcceptAnyServerCertificateValidator, HttpClientHandler.DangerousAcceptAnyServerCertificateValidator);
             Assert.True(HttpClientHandler.DangerousAcceptAnyServerCertificateValidator(null, null, null, SslPolicyErrors.None));
+        }
+
+        [Theory]
+        [InlineData(SslProtocols.Tls, false)] // try various protocols to ensure we correctly set versions even when accepting all certs
+        [InlineData(SslProtocols.Tls, true)]
+        [InlineData(SslProtocols.Tls12 | SslProtocols.Tls11 | SslProtocols.Tls, false)]
+        [InlineData(SslProtocols.Tls12 | SslProtocols.Tls11 | SslProtocols.Tls, true)]
+        [InlineData(SslProtocols.None, false)]
+        [InlineData(SslProtocols.None, true)]
+        public async Task SetDelegate_ConnectionSucceeds(SslProtocols acceptedProtocol, bool requestOnlyThisProtocol)
+        {
+            using (var handler = new HttpClientHandler() { ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator })
+            using (var client = new HttpClient(handler))
+            {
+                if (requestOnlyThisProtocol)
+                {
+                    handler.SslProtocols = acceptedProtocol;
+                }
+
+                var options = new LoopbackServer.Options { UseSsl = true, SslProtocols = acceptedProtocol };
+                await LoopbackServer.CreateServerAsync(async (server, url) =>
+                {
+                    await TestHelper.WhenAllCompletedOrAnyFailed(
+                        LoopbackServer.ReadRequestAndSendResponseAsync(server, options: options),
+                        client.GetAsync(url));
+                }, options);
+            }
+        }
+
+        public static readonly object[][] InvalidCertificateServers =
+        {
+            new object[] { Configuration.Http.ExpiredCertRemoteServer },
+            new object[] { Configuration.Http.SelfSignedCertRemoteServer },
+            new object[] { Configuration.Http.WrongHostNameCertRemoteServer },
+        };
+
+        [OuterLoop] // TODO: Issue #11345
+        [Theory]
+        [MemberData(nameof(InvalidCertificateServers))]
+        public async Task InvalidCertificateServers_CertificateValidationDisabled_Succeeds(string url)
+        {
+            using (var client = new HttpClient(new HttpClientHandler() { ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator }))
+            {
+                (await client.GetAsync(url)).Dispose();
+            }
         }
     }
 }
