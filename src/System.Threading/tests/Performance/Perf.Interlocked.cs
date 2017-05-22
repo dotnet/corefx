12071697bf85babe306c10c0e17717320450717c
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-22 22:40:06 +0100

    Replace Benchmark.Iterate with raw loops to remove overhead on test results.

--- src/System.Threading/tests/Performance/Perf.Interlocked.cs
+++ src/System.Threading/tests/Performance/Perf.Interlocked.cs
@@ -12,60 +12,102 @@ namespace System.Threading.Tests
         public static void Increment_int()
         {
             int location = 0;
-            Benchmark.Iterate(() =>
+
+            foreach (var iteration in Benchmark.Iterations)
             {
-                Interlocked.Increment(ref location);
-            });
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        Interlocked.Increment(ref location);
+                    }
+                }
+            }
         }
 
         [Benchmark(InnerIterationCount = 1000)]
         public static void Decrement_int()
         {
             int location = 0;
-            Benchmark.Iterate(() =>
+
+            foreach (var iteration in Benchmark.Iterations)
             {
-                Interlocked.Decrement(ref location);
-            });
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        Interlocked.Decrement(ref location);
+                    }
+                }
+            }
         }
 
         [Benchmark(InnerIterationCount = 1000)]
         public void Increment_long()
         {
             long location = 0;
-            Benchmark.Iterate(() =>
+
+            foreach (var iteration in Benchmark.Iterations)
             {
-                Interlocked.Increment(ref location);
-            });
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        Interlocked.Increment(ref location);
+                    }
+                }
+            }
         }
 
         [Benchmark(InnerIterationCount = 1000)]
         public void Decrement_long()
         {
             long location = 0;
-            Benchmark.Iterate(() =>
+
+            foreach (var iteration in Benchmark.Iterations)
             {
-                Interlocked.Decrement(ref location);
-            });
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        Interlocked.Decrement(ref location);
+                    }
+                }
+            }
         }
 
         [Benchmark(InnerIterationCount = 1000)]
         public void Add_int()
         {
             int location = 0;
-            Benchmark.Iterate(() =>
+
+            foreach (var iteration in Benchmark.Iterations)
             {
-                Interlocked.Add(ref location, 2);
-            });
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        Interlocked.Add(ref location, 2);
+                    }
+                }
+            }
         }
 
         [Benchmark(InnerIterationCount = 1000)]
         public void Add_long()
         {
             long location = 0;
-            Benchmark.Iterate(() =>
+
+            foreach (var iteration in Benchmark.Iterations)
             {
-                Interlocked.Add(ref location, 2);
-            });
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        Interlocked.Add(ref location, 2);
+                    }
+                }
+            }
         }
 
         [Benchmark(InnerIterationCount = 1000)]
@@ -74,10 +116,16 @@ namespace System.Threading.Tests
             int location = 0;
             int newValue = 1;
 
-            Benchmark.Iterate(() =>
+            foreach (var iteration in Benchmark.Iterations)
             {
-                Interlocked.Exchange(ref location, newValue);
-            });
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        Interlocked.Exchange(ref location, newValue);
+                    }
+                }
+            }
         }
 
         [Benchmark(InnerIterationCount = 1000)]
@@ -86,10 +134,16 @@ namespace System.Threading.Tests
             long location = 0;
             long newValue = 1;
 
-            Benchmark.Iterate(() =>
+            foreach (var iteration in Benchmark.Iterations)
             {
-                Interlocked.Exchange(ref location, newValue);
-            });
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        Interlocked.Exchange(ref location, newValue);
+                    }
+                }
+            }
         }
 
         [Benchmark(InnerIterationCount = 500)]
@@ -99,10 +153,16 @@ namespace System.Threading.Tests
             int newValue = 1;
             int comparand = 0;
 
-            Benchmark.Iterate(() =>
+            foreach (var iteration in Benchmark.Iterations)
             {
-                Interlocked.CompareExchange(ref location, newValue, comparand);
-            });
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        Interlocked.CompareExchange(ref location, newValue, comparand);
+                    }
+                }
+            }
         }
 
         [Benchmark(InnerIterationCount = 500)]
@@ -112,10 +172,16 @@ namespace System.Threading.Tests
             long newValue = 1;
             long comparand = 0;
 
-            Benchmark.Iterate(() =>
+            foreach (var iteration in Benchmark.Iterations)
             {
-                Interlocked.CompareExchange(ref location, newValue, comparand);
-            });
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < Benchmark.InnerIterationCount; i++)
+                    {
+                        Interlocked.CompareExchange(ref location, newValue, comparand);
+                    }
+                }
+            }
         }
     }
 }
