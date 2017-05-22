dd547693a7bb8652250e190db2fb3f36ada3fc05
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-22 20:24:20 +0100

    Refactor performance tests for System.Threading.Volatile to improve accuracy and isolation.

--- src/System.Threading/tests/Performance/Perf.Volalite.cs
+++ src/System.Threading/tests/Performance/Perf.Volalite.cs
@@ -8,32 +8,17 @@ namespace System.Threading.Tests
 {
     public class Perf_Volatile
     {
-        [Benchmark]
-        public void Read_double()
-        {
-            double location = 0;
-            foreach (var iteration in Benchmark.Iterations)
-            {
-                using (iteration.StartMeasurement())
-                {
-                    Volatile.Read(ref location);
-                }
-            }
-        }
-
-        [Benchmark]
-        public void Write_double()
+        [Benchmark(InnerIterationCount = 500)]
+        public void ReadWrite_double()
         {
             double location = 0;
             double newValue = 1;
 
-            foreach (var iteration in Benchmark.Iterations)
+            Benchmark.Iterate(() =>
             {
-                using (iteration.StartMeasurement())
-                {
-                    Volatile.Write(ref location, newValue);
-                }
-            }
+                Volatile.Read(ref location);
+                Volatile.Write(ref location, newValue);
+            });
         }
     }
 }
