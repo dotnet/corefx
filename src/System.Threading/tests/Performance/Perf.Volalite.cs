9fd823d170d6203841136a0bfcc4d74c5f0e8470
Paulo Gomes <paulo.gomes.uk@gmail.com>
2017-05-22 06:53:01 +0100

    Implement Performance tests for System.Threading.Volatile.

---
+++ src/System.Threading/tests/Performance/Perf.Volalite.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Xunit.Performance;
+
+namespace System.Threading.Tests
+{
+    public class Perf_Volatile
+    {
+        [Benchmark]
+        public void Read_double()
+        {
+            double location = 0;
+            foreach (var iteration in Benchmark.Iterations)
+            {
+                using (iteration.StartMeasurement())
+                {
+                    Volatile.Read(ref location);
+                }
+            }
+        }
+
+        [Benchmark]
+        public void Write_double()
+        {
+            double location = 0;
+            double newValue = 1;
+
+            foreach (var iteration in Benchmark.Iterations)
+            {
+                using (iteration.StartMeasurement())
+                {
+                    Volatile.Write(ref location, newValue);
+                }
+            }
+        }
+    }
+}
