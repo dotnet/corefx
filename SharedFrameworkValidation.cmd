42a00bb3eb1d310c649e635f28684c17461562d3
Jose Perez Rodriguez <joperezr@microsoft.com>
2017-05-25 05:52:48 +0000

    Download Shared Framework Zips and adding their ApiCompat runs

--- SharedFrameworkValidation.cmd
+++ SharedFrameworkValidation.cmd
@@ -3,7 +3,10 @@ setlocal
 
 call %~dp0sync.cmd
 if NOT [%ERRORLEVEL%]==[0] exit /b 1
-call %~dp0build-managed.cmd -Project:%~dp0src\SharedFrameworkValidation\SharedFrameworkValidation.proj -- /t:ReBuild
+call %~dp0build-managed.cmd -Project:%~dp0src\SharedFrameworkValidation\SharedFrameworkValidation.proj -- /t:CreateScriptToDownloadSharedFrameworkZip
+if NOT [%ERRORLEVEL%]==[0] exit /b 1
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "%~dp0bin\DownloadSharedFramework.ps1"
+call %~dp0build-managed.cmd -Project:%~dp0src\SharedFrameworkValidation\SharedFrameworkValidation.proj
 if NOT [%ERRORLEVEL%]==[0] exit /b 1
 call %~dp0bin\CloneAndRunTests.cmd
 exit /b %ERRORLEVEL%
