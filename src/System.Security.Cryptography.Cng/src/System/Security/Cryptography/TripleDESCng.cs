231741917c62ad322ca03ea898cc399bb899baca
Steve Harter <sharter@microsoft.com>
2017-05-25 12:35:27 -0500

    Support Two-Key Triple DES (#20275)

--- src/System.Security.Cryptography.Cng/src/System/Security/Cryptography/TripleDESCng.cs
+++ src/System.Security.Cryptography.Cng/src/System/Security/Cryptography/TripleDESCng.cs
@@ -94,15 +94,6 @@ namespace System.Security.Cryptography
             _core.GenerateIV();
         }
 
-        public override KeySizes[] LegalKeySizes
-        {
-            get
-            {
-                // CNG does not support 128-bit keys.
-                return new KeySizes[] { new KeySizes(minSize: 3 * 64, maxSize: 3 * 64, skipSize: 0) };
-            }
-        }
-
         protected override void Dispose(bool disposing)
         {
             base.Dispose(disposing);
@@ -124,6 +115,21 @@ namespace System.Security.Cryptography
         string ICngSymmetricAlgorithm.GetNCryptAlgorithmIdentifier()
         {
             return Interop.NCrypt.NCRYPT_3DES_ALGORITHM;
+        }
+
+        byte[] ICngSymmetricAlgorithm.PreprocessKey(byte[] key)
+        {
+            if (key.Length == 16)
+            {
+                // Cng does not support Two-Key Triple DES, so manually support it here for consistency with System.Security.Cryptography.Algorithms.
+                // Two-Key Triple DES contains two 8-byte keys {K1}{K2} with {K1} appended to make {K1}{K2}{K1}.
+                byte[] newkey = new byte[24];
+                Array.Copy(key, 0, newkey, 0, 16);
+                Array.Copy(key, 0, newkey, 16, 8);
+                return newkey;
+            }
+
+            return key;
         }
 
         private CngSymmetricAlgorithmCore _core;
