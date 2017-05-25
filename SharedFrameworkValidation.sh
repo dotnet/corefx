d18812c186a52ffab968945809c3f7951f10e585
Jose Perez Rodriguez <joperezr@microsoft.com>
2017-05-25 17:17:04 -0500

    Download Shared Framework Zips and adding their ApiCompat runs (#20301)
    * Download Shared Framework Zips and adding their ApiCompat runs
    
    * Add architecture as a parameter to windows scripts so we can test all rids

--- SharedFrameworkValidation.sh
+++ SharedFrameworkValidation.sh
@@ -6,7 +6,14 @@ if [ "$?" != "0" ]; then
     echo "ERROR: An error occured when trying to initialize the tools. Please check '$__scriptpath/init-tools.log' for more details."
     exit 1
 fi
-$__scriptpath/build-managed.sh -Project:$__scriptpath/src/SharedFrameworkValidation/SharedFrameworkValidation.proj -- /t:ReBuild
+$__scriptpath/build-managed.sh -Project:$__scriptpath/src/SharedFrameworkValidation/SharedFrameworkValidation.proj  -- /t:CreateScriptToDownloadSharedFrameworkZip
+if [ "$?" != "0" ]; then
+    echo "ERROR: An error occured when trying to write the script to download the shared framework. Please check '$__scriptpath/msbuild.log' for more details."
+    exit 1
+fi
+chmod a+x $__scriptpath/bin/DownloadSharedFramework.sh
+$__scriptpath/bin/DownloadSharedFramework.sh
+$__scriptpath/build-managed.sh -Project:$__scriptpath/src/SharedFrameworkValidation/SharedFrameworkValidation.proj
 if [ "$?" != "0" ]; then
     echo "ERROR: An error occured when trying to restore the shared framework. Please check '$__scriptpath/msbuild.log' for more details."
     exit 1
