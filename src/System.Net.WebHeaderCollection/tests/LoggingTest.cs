b32f140fd3e2887521306b515c7102b76088c358
David Shulman <david.shulman@microsoft.com>
2017-05-30 15:34:34 -0700

    Disable some WebHeaderCollection tests on ILC test runs
    Disable some WebHeaderCollection tests on 'uap-aot' (ILC) test runs.
    
    Contributes to #20465 and #20470

--- src/System.Net.WebHeaderCollection/tests/LoggingTest.cs
+++ src/System.Net.WebHeaderCollection/tests/LoggingTest.cs
@@ -9,6 +9,7 @@ namespace System.Net.Tests
 {
     public class WebHeaderCollectionLoggingTest
     {
+        [ActiveIssue(20470, TargetFrameworkMonikers.UapAot)]
         [Fact]
         [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework, "NetEventSource is only part of .NET Core")]
         public void EventSource_ExistsWithCorrectId()
