3f2c7335c40d3fca147c6f7a98590bd50d0a544a
Gene Lee <gelee@microsoft.com>
2017-05-31 17:36:04 -0700

    PR comment fix

--- src/System.Data.SqlClient/tests/ManualTests/SQL/SqlSchemaInfoTest/SqlSchemaInfoTest.cs
+++ src/System.Data.SqlClient/tests/ManualTests/SQL/SqlSchemaInfoTest/SqlSchemaInfoTest.cs
@@ -3,46 +3,46 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Data.Common;
+using Xunit;
 
 namespace System.Data.SqlClient.ManualTesting.Tests
 {
     public static class SqlSchemaInfoTest
     {
         [CheckConnStrSetupFact]
-        public static void TestSqlSchemaInfo()
+        public static void TestGetSchema()
         {
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(DataTestUtility.TcpConnStr);
-
             using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
             {
                 connection.Open();
 
-                TestGetSchema(connection);
-                TestCommandBuilder(connection);
-                TestInitialCatalogStandardValues(connection);
+                DataTable dataTable = connection.GetSchema("DATABASES");
+
+                if (dataTable.Rows.Count > 0)
+                {
+                    DataTable metaDataCollections = connection.GetSchema(DbMetaDataCollectionNames.MetaDataCollections);
+                    Assert.True(metaDataCollections != null && metaDataCollections.Rows.Count > 0);
+                    
+                    DataTable metaDataSourceInfo = connection.GetSchema(DbMetaDataCollectionNames.DataSourceInformation);
+                    Assert.True(metaDataSourceInfo != null && metaDataSourceInfo.Rows.Count > 0);
+
+                    DataTable metaDataTypes = connection.GetSchema(DbMetaDataCollectionNames.DataTypes);
+                    Assert.True(metaDataTypes != null && metaDataTypes.Rows.Count > 0);
+                }
 
                 connection.Close();
             }
         }
 
-        public static void TestGetSchema(SqlConnection connection)
-        {
-            DataTable dataTable = connection.GetSchema("DATABASES");
-
-            if (dataTable.Rows.Count > 0)
-            {
-                DataTable metaDataCollections = connection.GetSchema(DbMetaDataCollectionNames.MetaDataCollections);
-                DataTable metaDataSourceInfo = connection.GetSchema(DbMetaDataCollectionNames.DataSourceInformation);
-                DataTable metaDataTypes = connection.GetSchema(DbMetaDataCollectionNames.DataTypes);
-            }
-        }
-
-        public static void TestCommandBuilder(SqlConnection connection)
+        [CheckConnStrSetupFact]
+        public static void TestCommandBuilder()
         {
             // CommandBuilder is not supported yet in .NET Core.
         }
 
-        public static void TestInitialCatalogStandardValues(SqlConnection connection)
+        [CheckConnStrSetupFact]
+        public static void TestInitialCatalogStandardValues()
         {
             // PropertyDescriptor is not supported yet in .NET Core.
         }
