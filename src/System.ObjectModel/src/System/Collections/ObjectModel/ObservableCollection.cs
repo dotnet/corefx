2044368bd87af1da34cb7ea7e21b79e8ff7f5595
Justin Van Patten <jvp@justinvp.com>
2017-06-02 13:57:30 -0700

    Add TypeForwardedFrom

--- src/System.ObjectModel/src/System/Collections/ObjectModel/ObservableCollection.cs
+++ src/System.ObjectModel/src/System/Collections/ObjectModel/ObservableCollection.cs
@@ -18,6 +18,7 @@ namespace System.Collections.ObjectModel
     [Serializable]
     [DebuggerTypeProxy(typeof(CollectionDebugView<>))]
     [DebuggerDisplay("Count = {Count}")]
+    [System.Runtime.CompilerServices.TypeForwardedFrom("WindowsBase, Version=3.0.0.0, Culture=Neutral, PublicKeyToken=31bf3856ad364e35")]
     public class ObservableCollection<T> : Collection<T>, INotifyCollectionChanged, INotifyPropertyChanged
     {
         //------------------------------------------------------
@@ -375,6 +376,7 @@ namespace System.Collections.ObjectModel
 
         // this class helps prevent reentrant calls
         [Serializable]
+        [System.Runtime.CompilerServices.TypeForwardedFrom("WindowsBase, Version=3.0.0.0, Culture=Neutral, PublicKeyToken=31bf3856ad364e35")]
         private sealed class SimpleMonitor : IDisposable
         {
             internal int _busyCount; // Only used during (de)serialization to maintain compatibility with desktop.
