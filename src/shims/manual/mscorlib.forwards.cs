de66beae187d95147dd1f0b072d2b1957a4706ff
Viktor Hofer <viktor.hofer@microsoft.com>
2017-06-05 20:02:08 +0200

    Removed not needed typeforward and added description

--- src/shims/manual/mscorlib.forwards.cs
+++ src/shims/manual/mscorlib.forwards.cs
@@ -2,6 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+// Add any internal types that we need to forward from mscorlib.
+
+// These types are required for Desktop to Core serialization as they are not covered by GenAPI because they are marked as internal.
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.CultureAwareComparer))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.OrdinalComparer))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.OrdinalIgnoreCaseComparer))]
@@ -21,4 +24,3 @@
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Collections.Generic.SByteEnumEqualityComparer`1))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Collections.Generic.ShortEnumEqualityComparer`1))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Collections.Generic.LongEnumEqualityComparer`1))]
-[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.__RestrictedErrorObject))]
