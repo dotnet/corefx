84d4fc257fc812c02154bcd6564fc0afd360805d
Will Dean <will.dean@indcomp.co.uk>
2017-06-05 20:04:11 +0100

    Adjust CI-suppression rules for difficult serial tests

--- src/System.IO.Ports/tests/Support/TCSupport.cs
+++ src/System.IO.Ports/tests/Support/TCSupport.cs
@@ -100,14 +100,6 @@ namespace Legacy.Support
                 portName2 = openablePortNames.FirstOrDefault(name => name != portName1);
             }
 
-            // See Github issue #15961 and #16033 - hardware tests are currently insufficiently stable on master CI
-            if (loopbackPortName == null && !nullModemPresent)
-            {
-                // We don't have any supporting hardware - disable all the tests which would use just an open port
-                PrintInfo("No support hardware - not using serial ports");
-                portName1 = portName2 = null;
-            }
-
             PrintInfo("First available port name  : " + portName1);
             PrintInfo("Second available port name : " + portName2);
             PrintInfo("Loopback port name         : " + loopbackPortName);
