6d18358116165d125e69227525e968a981d967a5
Wes Haggard <Wes.Haggard@microsoft.com>
2017-06-05 08:59:49 -0700

    Manually build mscorlib shim to allow for internal type forwards

---
+++ src/shims/manual/mscorlib.forwards.cs
@@ -0,0 +1,2 @@
+//Add any internal types that we need to forward once we get internal access to System.Private.CoreLib
+//[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.CultureAwareComparer))]
