eaf88ce63f5c2d88126d320ebe86a7e4c19818dc
Tijoy Tom Kalathiparambil <tijoytk@microsoft.com>
2017-06-06 12:16:07 -0700

    Fixing System.Diagnostics.Tracing tests on uap-aot All tests expect 4 are fixed , the four fails because of reflectionblock.These need to be looked at by tracing folks.

--- src/System.Diagnostics.Tracing/tests/BasicEventSourceTest/TestsEventSourceLifetime.cs
+++ src/System.Diagnostics.Tracing/tests/BasicEventSourceTest/TestsEventSourceLifetime.cs
@@ -21,6 +21,7 @@ namespace BasicEventSourceTests
         /// </summary>
         [Fact]
         [PlatformSpecific(TestPlatforms.Windows)] // non-Windows EventSources don't have lifetime
+        [SkipOnTargetFramework(TargetFrameworkMonikers.UapAot, "ExerciseEventSource use private member reflection")]
         public void Test_EventSource_Lifetime()
         {
             TestUtilities.CheckNoEventSourcesRunning("Start");
