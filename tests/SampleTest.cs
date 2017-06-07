e48777c5e64b749cb85cd23270f58ec29ac6a41c
Matt Connew <mconnew@microsoft.com>
2017-06-07 09:51:48 -0700

    Microsoft.ServiceModel.Syndication skeleton project

---
+++ tests/SampleTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Microsoft.ServiceModel.Syndication.Tests
+{
+    public static class SampleTest
+    {
+        [Fact]
+        public static void PassingTest()
+        {
+        }
+
+        [Fact]
+        public static void FailingTest()
+        {
+            Assert.True(false, "This test is expected to fail");
+        }
+
+    }
+}
