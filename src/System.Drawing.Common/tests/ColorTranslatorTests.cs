26ab902cfa2171485f4f73c34531d9396a0c7575
Eric Mellino <erme@microsoft.com>
2017-06-13 11:56:06 -0700

    Disable some ColorTranslator tests.

--- src/System.Drawing.Common/tests/ColorTranslatorTests.cs
+++ src/System.Drawing.Common/tests/ColorTranslatorTests.cs
@@ -76,6 +76,7 @@ namespace System.Drawing.Tests
             }
         }
 
+        [ActiveIssue(20998)]
         [Theory]
         [MemberData(nameof(FromOle_TestData))]
         public void FromOle_Color_ReturnsExpected(int oleColor, Color color)
@@ -103,6 +104,7 @@ namespace System.Drawing.Tests
             yield return new object[] { SystemColors.ButtonShadow, unchecked((int)0x80000010) };
         }
 
+        [ActiveIssue(20998)]
         [Theory]
         [MemberData(nameof(ToOle_TestData))]
         public void ToOle_Color_ReturnsExpected(Color color, int oleColor)
@@ -184,6 +186,7 @@ namespace System.Drawing.Tests
             yield return new object[] { "threedlightshadow", SystemColors.ControlLightLight };
         }
 
+        [ActiveIssue(20998)]
         [Theory]
         [MemberData(nameof(FromHtml_TestData))]
         public void FromHtml_String_ReturnsExpected(string htmlColor, Color expected)
@@ -238,6 +241,7 @@ namespace System.Drawing.Tests
             yield return new object[] { SystemColors.ButtonShadow, "" };
         }
 
+        [ActiveIssue(20998)]
         [Theory]
         [MemberData(nameof(ToHtml_TestData))]
         public void ToHtml_Color_ReturnsExpected(Color color, string expected)
