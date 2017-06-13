dcc67c2f2de2960757e3d488aeb4580a2c759393
Hugh Bellamy <hughbellars@gmail.com>
2017-06-13 11:25:07 +0700

    Cleanup some code

--- src/System.Composition.Runtime/src/System/Composition/ExportOfT.cs
+++ src/System.Composition.Runtime/src/System/Composition/ExportOfT.cs
@@ -11,7 +11,6 @@ namespace System.Composition
     /// <typeparam name="T"></typeparam>
     public sealed class Export<T> : IDisposable
     {
-        private readonly T _value;
         private readonly Action _disposeAction;
 
         /// <summary>
@@ -21,31 +20,19 @@ namespace System.Composition
         /// <param name="disposeAction">An action that releases resources associated with the export.</param>
         public Export(T value, Action disposeAction)
         {
-            _value = value;
+            Value = value;
             _disposeAction = disposeAction;
         }
 
         /// <summary>
         /// The exported value.
         /// </summary>
-        public T Value
-        {
-            get
-            {
-                return _value;
-            }
-        }
+        public T Value { get; }
 
         /// <summary>
         /// Release the parts associated with the exported value.
         /// </summary>
-        public void Dispose()
-        {
-            if (_disposeAction != null)
-            {
-                _disposeAction.Invoke();
-            }
-        }
+        public void Dispose() => _disposeAction?.Invoke();
     }
 }
 
