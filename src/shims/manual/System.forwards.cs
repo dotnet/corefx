18921a9ce4d53d107495cba20b7849ed6db19579
Morgan Brown <morganb@microsoft.com>
2017-06-16 18:54:23 -0700

    Fixes the ifdefs for the mscorlib and System facades to build for aot as well. Also includes a minor rd.xml tweak for BinaryFormatter. Fixes hundreds of BinaryFormatter tests. Progress on #19119

--- src/shims/manual/System.forwards.cs
+++ src/shims/manual/System.forwards.cs
@@ -5,8 +5,6 @@
 // Add any internal types that we need to forward from System. 
 
 // These types are required for Desktop <--> Core serialization as they are not covered by GenAPI because they are not exposed.
-#if netcoreapp
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Collections.Generic.TreeSet<>))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Net.CookieVariant))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Net.PathList))]
-#endif
