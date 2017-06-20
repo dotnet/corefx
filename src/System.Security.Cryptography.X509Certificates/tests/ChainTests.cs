7c3b4bba799ac3ff50764095a09f8c2619377b0f
Jeremy Barton <jbarton@microsoft.com>
2017-06-20 09:34:37 -0700

    Make VerifyWithRevocation skip if the Online chain can't build
    When the system doesn't have a current CRL cached for the entirety of the chain,
    and the CRL Distribution Point is offline (or there's a local network issue) then
    the test fails. This is tolerable for a manual run, but not for the automated runs.
    
    So unless the X.509 "RunManualTests" configuration is set, treat a failure of the
    online chain to be a skip.  Ideally we'd throw a SkipTestException, or something
    of that ilk, but xunit doesn't support it.

--- src/System.Security.Cryptography.X509Certificates/tests/ChainTests.cs
+++ src/System.Security.Cryptography.X509Certificates/tests/ChainTests.cs
@@ -571,7 +571,14 @@ namespace System.Security.Cryptography.X509Certificates.Tests
                     }
                 }
 
-                Assert.True(valid, $"Online Chain Built Validly within {RetryLimit} tries");
+                if (TestEnvironmentConfiguration.RunManualTests)
+                {
+                    Assert.True(valid, $"Online Chain Built Validly within {RetryLimit} tries");
+                }
+                else if (!valid)
+                {
+                    Console.WriteLine($"SKIP [{nameof(VerifyWithRevocation)}]: Chain failed to build within {RetryLimit} tries.");
+                }
 
                 // Since the network was enabled, we should get the whole chain.
                 Assert.Equal(3, onlineChain.ChainElements.Count);
