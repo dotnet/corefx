9866df97086f9e12de1edb1a200ad5ad71dd5aff
Hugh Bellamy <hughbellars@gmail.com>
2017-06-21 21:36:09 +0700

    Address PR feedback and add SystemIcons tests

--- src/System.Drawing.Common/src/System/Drawing/SystemIcons.cs
+++ src/System.Drawing.Common/src/System/Drawing/SystemIcons.cs
@@ -19,23 +19,23 @@ namespace System.Drawing
         private static Icon s_winlogo = null;
         private static Icon s_shield = null;
 
-        public static Icon Application => GetIcon(s_application, SafeNativeMethods.IDI_APPLICATION);
+        public static Icon Application => GetIcon(ref s_application, SafeNativeMethods.IDI_APPLICATION);
 
-        public static Icon Asterisk => GetIcon(s_asterisk, SafeNativeMethods.IDI_ASTERISK);
+        public static Icon Asterisk => GetIcon(ref s_asterisk, SafeNativeMethods.IDI_ASTERISK);
 
-        public static Icon Error => GetIcon(s_error, SafeNativeMethods.IDI_ERROR);
+        public static Icon Error => GetIcon(ref s_error, SafeNativeMethods.IDI_ERROR);
 
-        public static Icon Exclamation => GetIcon(s_exclamation, SafeNativeMethods.IDI_EXCLAMATION);
+        public static Icon Exclamation => GetIcon(ref s_exclamation, SafeNativeMethods.IDI_EXCLAMATION);
 
-        public static Icon Hand => GetIcon(s_hand, SafeNativeMethods.IDI_HAND);
+        public static Icon Hand => GetIcon(ref s_hand, SafeNativeMethods.IDI_HAND);
 
-        public static Icon Information => GetIcon(s_information, SafeNativeMethods.IDI_INFORMATION);
+        public static Icon Information => GetIcon(ref s_information, SafeNativeMethods.IDI_INFORMATION);
 
-        public static Icon Question => GetIcon(s_question, SafeNativeMethods.IDI_QUESTION);
+        public static Icon Question => GetIcon(ref s_question, SafeNativeMethods.IDI_QUESTION);
 
-        public static Icon Warning => GetIcon(s_warning, SafeNativeMethods.IDI_WARNING);
+        public static Icon Warning => GetIcon(ref s_warning, SafeNativeMethods.IDI_WARNING);
 
-        public static Icon WinLogo => GetIcon(s_winlogo, SafeNativeMethods.IDI_WINLOGO);
+        public static Icon WinLogo => GetIcon(ref s_winlogo, SafeNativeMethods.IDI_WINLOGO);
 
         public static Icon Shield
         {
@@ -73,14 +73,9 @@ namespace System.Drawing
             }
         }
 
-        private static Icon GetIcon(Icon icon, int iconId)
+        private static Icon GetIcon(ref Icon icon, int iconId)
         {
-            if (icon == null)
-            {
-                icon = new Icon(SafeNativeMethods.LoadIcon(NativeMethods.NullHandleRef, SafeNativeMethods.IDI_WINLOGO));
-            }
-
-            return icon;
+            return icon ?? (icon = new Icon(SafeNativeMethods.LoadIcon(NativeMethods.NullHandleRef, iconId)));
         }
     }
 }
---
+++ src/System.Drawing.Common/tests/SystemIconsTests.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using Xunit;
+
+namespace System.Drawing.Tests
+{
+    public class SystemIconsTests
+    {
+        public static IEnumerable<object[]> SystemIcons_TestData()
+        {
+            yield return Icon(() => SystemIcons.Application);
+            yield return Icon(() => SystemIcons.Asterisk);
+            yield return Icon(() => SystemIcons.Error);
+            yield return Icon(() => SystemIcons.Exclamation);
+            yield return Icon(() => SystemIcons.Hand);
+            yield return Icon(() => SystemIcons.Information);
+            yield return Icon(() => SystemIcons.Question);
+            //[ActiveIssue(21310)]
+            //yield return Icon(() => SystemIcons.Shield);
+            yield return Icon(() => SystemIcons.Warning);
+            yield return Icon(() => SystemIcons.WinLogo);
+        }
+
+        public static object[] Icon(Func<Icon> iconThunk) => new object[] { iconThunk };
+
+        [ConditionalTheory(nameof(PlatformDetection) + "." + nameof(PlatformDetection.IsNotWindowsNanoServer))]
+        [MemberData(nameof(SystemIcons_TestData))]
+        public void SystemIcons_Get_ReturnsExpected(Func<Icon> iconThunk)
+        {
+            Icon icon = iconThunk();
+            Assert.Same(icon, iconThunk());
+        }
+    }
+}
