6e470d7cce6b50698c029583c81ccc73f915d3ea
Viktor Hofer <viktor.hofer@microsoft.com>
2017-06-22 12:16:58 +0200

    Skipping for netfx as this only tests core impl

--- src/System.ObjectModel/tests/ObservableCollection/ObservableCollection_Serialization.cs
+++ src/System.ObjectModel/tests/ObservableCollection/ObservableCollection_Serialization.cs
@@ -1,3 +1,4 @@
+
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
@@ -29,6 +30,7 @@ namespace System.Collections.ObjectModel.Tests
         }
 
         [Fact]
+        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework)]
         public void OnDeserialized_MonitorNotInitialized_ExpectSuccess()
         {
             var observableCollection = new ObservableCollection<int>();
