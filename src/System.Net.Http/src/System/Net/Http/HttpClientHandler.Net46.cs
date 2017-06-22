eac755df47f70e5ef3853c0b71ed4ab12455bb96
David Shulman <david.shulman@microsoft.com>
2017-06-22 09:50:42 -0700

    Fix DefaultProxyCredentials edge case on NETFX
    After my PR #21325, I discovered an edge case where we need to check for
    a non-null Proxy property in the underlying WebRequest.
    
    On .NET Framework, System.Net.Http is built on WebRequest
    (HttpWebRequest) and is subject to side-affects from it. The static
    property WebRequest.DefaultWebProxy affects the initial value of
    WebRequest.Proxy. So, it's possible that a developer may have set
    WebRequest.DefaultWebProxy to null prior to using System.Net.Http in the
    app.
    
    The net affect of this is that that won't be any proxy used as the
    system default proxy and thus the
    HttpClientHandler.DefaultProxyCredentials is ignored.
    
    On .NET Core, there is no relationship between System.Net.Http and
    System.Net.WebRequest.  So, in all cases on .NET Core, where a developer
    has specified HttpClientHander.UseProxy=true and HttpClientHandler.Proxy
    = null (default values), the handler will always try to use the system
    default proxy (if configured).

--- src/System.Net.Http/src/System/Net/Http/HttpClientHandler.Net46.cs
+++ src/System.Net.Http/src/System/Net/Http/HttpClientHandler.Net46.cs
@@ -526,12 +526,13 @@ namespace System.Net.Http
                 webRequest.ServerCertificateValidationCallback = ServerCertificateValidationCallback;
             }
 
-            if (_defaultProxyCredentials != null && _useProxy && _proxy == null)
+            if (_defaultProxyCredentials != null && _useProxy && _proxy == null && webRequest.Proxy != null)
             {
-                // The HttpClientHandler has specified to use a proxy but has not
-                // set an explicit IWebProxy. That means to use the system default
-                // proxy setting object which is the default value of webRequest.Proxy.
-                Debug.Assert(webRequest.Proxy != null);
+                // The HttpClientHandler has specified to use a proxy but has not set an explicit IWebProxy.
+                // That means to use the default proxy on the underlying webrequest object. The initial value
+                // of the webrequest.Proxy when first created comes from the static WebRequest.DefaultWebProxy.
+                // In the default case, this value is non-null. But can be set later to null. That is why the
+                // 'if' check above validates for a non-null webRequest.Proxy.
                 webRequest.Proxy.Credentials = _defaultProxyCredentials;
             }
 
