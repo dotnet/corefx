c79de0509785d6d126806c39bd67769efdd280a3
Hugh Bellamy <hughbellars@gmail.com>
2017-06-25 04:09:16 +0700

    Fix broken string resources in System.Directory.AccountManagement (#21497)
    * Cleanup existing tests
    
    * Fix broken string resources in System.DirectoryServices.AccountManagement

--- src/System.DirectoryServices.AccountManagement/tests/ComputerPrincipalTest.cs
+++ src/System.DirectoryServices.AccountManagement/tests/ComputerPrincipalTest.cs
@@ -2,58 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.DirectoryServices.AccountManagement;
 using Xunit;
 
-namespace AccountManagementUnitTests
+namespace System.DirectoryServices.AccountManagement.Tests
 {
-    /// <summary>
-    ///This is a test class for ComputerPrincipalTest and is intended
-    ///to contain all ComputerPrincipalTest Unit Tests
-    ///</summary>
     public class ComputerPrincipalTest : PrincipalTest
     {
-        #region Additional test attributes
-        // 
-        //You can use the following additional attributes as you write your tests:
-        //
-        //Use ClassInitialize to run code before running the first test in the class
-        //[ClassInitialize()]
-        //public static void MyClassInitialize(TestContext testContext)
-        //{
-        //}
-        //
-        //Use ClassCleanup to run code after all tests in a class have run
-        //[ClassCleanup()]
-        //public static void MyClassCleanup()
-        //{
-        //}
-        //
-        //Use TestInitialize to run code before running each test
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{
-        //}
-        //
-        //Use TestCleanup to run code after each test has run
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{
-        //}
-        //
-        #endregion
-
-        /// <summary>
-        ///A test for ComputerPrincipal Constructor
-        ///</summary>
         [Fact]
         public void ComputerPrincipalConstructorTest()
         {
-            ComputerPrincipal computer = new ComputerPrincipal(domainContext);
+            ComputerPrincipal computer = new ComputerPrincipal(DomainContext);
             computer.Dispose();
-            // jsimmons - I currently do not see a straightforward 
-            //   XUnit replacement for Assert.Inconclusive.
-            //Assert.Inconclusive("TODO: Implement code to verify target");
         }
 
         internal override Principal CreatePrincipal(PrincipalContext context, string name)
@@ -65,13 +24,12 @@ namespace AccountManagementUnitTests
 
         internal override Principal CreateExtendedPrincipal(PrincipalContext context, string name)
         {
-            //Assert.Inconclusive("TODO: Implement code to verify target");
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         internal override Principal FindExtendedPrincipal(PrincipalContext context, string name)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 }
