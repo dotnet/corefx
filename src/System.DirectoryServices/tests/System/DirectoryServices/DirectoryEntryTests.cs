edfcaf67d454186f56f067c63a5df3c0e922da13
Santiago Fernandez <safern@microsoft.com>
2017-06-26 05:46:26 -0700

    PR Feedback

--- src/System.DirectoryServices/tests/System/DirectoryServices/DirectoryEntryTests.cs
+++ src/System.DirectoryServices/tests/System/DirectoryServices/DirectoryEntryTests.cs
@@ -220,7 +220,7 @@ namespace System.DirectoryServices.Tests
         }
 
         [Fact]
-        [SkipOnTargetFramework(TargetFrameworkMonikers.Uap, "PNE in UAP")]
+        [SkipOnTargetFramework(TargetFrameworkMonikers.Uap, "PNSE in UAP")]
         public void ObjectSecurity_Set_GetReturnsExpected()
         {
             var security = new ActiveDirectorySecurity();
