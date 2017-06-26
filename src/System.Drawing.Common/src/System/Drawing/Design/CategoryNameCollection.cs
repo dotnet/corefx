68bbc343fd7839db0f0e2763e06323fdc720cd95
Hugh Bellamy <hughbellars@gmail.com>
2017-06-26 09:36:27 +0700

    Add CategoryNameCollection tests

--- src/System.Drawing.Common/src/System/Drawing/Design/CategoryNameCollection.cs
+++ src/System.Drawing.Common/src/System/Drawing/Design/CategoryNameCollection.cs
@@ -3,68 +3,23 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections;
+using System.Security.Permissions;
 
 namespace System.Drawing.Design
 {
-    /// <summary>
-    /// A collection that stores <see cref='string'/> objects.
-    /// </summary>
-    [System.Security.Permissions.PermissionSetAttribute(System.Security.Permissions.SecurityAction.LinkDemand, Name = "FullTrust")]
+    [PermissionSet(SecurityAction.LinkDemand, Name = "FullTrust")]
     public sealed class CategoryNameCollection : ReadOnlyCollectionBase
     {
-        /// <summary>
-        /// Initializes a new instance of <see cref='CategoryNameCollection'/> based on another
-        /// <see cref='CategoryNameCollection'/>.
-        /// </summary>
-        public CategoryNameCollection(CategoryNameCollection value)
-        {
-            InnerList.AddRange(value);
-        }
+        public CategoryNameCollection(CategoryNameCollection value) => InnerList.AddRange(value);
 
-        /// <summary>
-        /// Initializes a new instance of <see cref='CategoryNameCollection'/> containing any array of
-        /// <see cref='string'/> objects.
-        /// </summary>
-        public CategoryNameCollection(String[] value)
-        {
-            InnerList.AddRange(value);
-        }
+        public CategoryNameCollection(string[] value) => InnerList.AddRange(value);
 
-        /// <summary>
-        /// Represents the entry at the specified index of the <see cref='string'/>.
-        /// </summary>
-        public string this[int index]
-        {
-            get
-            {
-                return ((string)(InnerList[index]));
-            }
-        }
+        public string this[int index] => ((string)(InnerList[index]));
 
-        /// <summary>
-        /// Gets a value indicating whether the  <see cref='CategoryNameCollection'/> contains the specified
-        /// <see cref='string'/>.
-        /// </summary>
-        public bool Contains(string value)
-        {
-            return InnerList.Contains(value);
-        }
+        public bool Contains(string value) => InnerList.Contains(value);
 
-        /// <summary>
-        /// Copies the <see cref='CategoryNameCollection'/> values to a one-dimensional <see cref='Array'/> instance
-        /// at the specified index.
-        /// </summary>
-        public void CopyTo(String[] array, int index)
-        {
-            InnerList.CopyTo(array, index);
-        }
+        public void CopyTo(string[] array, int index) => InnerList.CopyTo(array, index);
 
-        /// <summary>
-        /// Returns the index of a <see cref='string'/> in  the <see cref='CategoryNameCollection'/> .
-        /// </summary>
-        public int IndexOf(string value)
-        {
-            return InnerList.IndexOf(value);
-        }
+        public int IndexOf(string value) => InnerList.IndexOf(value);
     }
 }
