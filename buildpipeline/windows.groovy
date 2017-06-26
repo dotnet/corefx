5010d08419bcdf4132de6218bf1157b3826b006d
Jose Perez Rodriguez <joperezr@microsoft.com>
2017-06-26 10:40:59 -0700

    Enabling CI for UWP F5 runs

--- buildpipeline/windows.groovy
+++ buildpipeline/windows.groovy
@@ -8,7 +8,7 @@
 // TestOuter - If true, runs outerloop, if false runs just innerloop
 
 def submittedHelixJson = null
-def submitToHelix = (params.TGroup == 'netcoreapp')
+def submitToHelix = (params.TGroup == 'netcoreapp' || params.TGroup == 'uap')
 
 simpleNode('Windows_NT','latest') {
     stage ('Checkout source') {
@@ -61,7 +61,7 @@ simpleNode('Windows_NT','latest') {
             if (submitToHelix) {
                 archiveTests = 'true'
             }
-            if (submitToHelix || params.TGroup == 'uap' || params.TGroup == 'uapaot') {
+            if (submitToHelix || params.TGroup == 'uapaot') {
                 additionalArgs += ' -SkipTests'
             }
             bat ".\\build-tests.cmd ${framework} -buildArch=${params.AGroup} -${params.CGroup}${additionalArgs} -- /p:RuntimeOS=win10 /p:ArchiveTests=${archiveTests}"
@@ -80,15 +80,21 @@ simpleNode('Windows_NT','latest') {
                 def helixCreator = getUser()
 
                 // Target queues
-                def targetHelixQueues = ['Windows.10.Amd64.Open',
+                def targetHelixQueues = []
+                if (params.TGroup == 'netcoreapp')
+                {
+                    targetHelixQueues = ['Windows.10.Amd64.Open',
                                          'Windows.7.Amd64.Open',
                                          'Windows.81.Amd64.Open']
+                } else if (params.TGroup == 'uap') {
+                    targetHelixQueues = ['Windows.10.Amd64.ClientRS2.Open']
+                }
 
                 if (params.AGroup == 'x64') {
                     targetHelixQueues += ['Windows.10.Nano.Amd64.Open']
                 }
 
-                bat "\"%VS140COMNTOOLS%\\VsDevCmd.bat\" && msbuild src\\upload-tests.proj /p:ArchGroup=${params.AGroup} /p:ConfigurationGroup=${params.CGroup} /p:TestProduct=corefx /p:TimeoutInSeconds=1200 /p:TargetOS=Windows_NT /p:HelixJobType=test/functional/cli/ /p:HelixSource=${helixSource} /p:BuildMoniker=${helixBuild} /p:HelixCreator=${helixCreator} /p:CloudDropAccountName=dotnetbuilddrops /p:CloudResultsAccountName=dotnetjobresults /p:CloudDropAccessToken=%CloudDropAccessToken% /p:CloudResultsAccessToken=%OutputCloudResultsAccessToken% /p:HelixApiEndpoint=https://helix.dot.net/api/2017-04-14/jobs /p:TargetQueues=\"${targetHelixQueues.join(',')}\" /p:HelixLogFolder= /p:HelixLogFolder=${WORKSPACE}\\${logFolder}\\ /p:HelixCorrelationInfoFileName=SubmittedHelixRuns.txt"
+                bat "\"%VS140COMNTOOLS%\\VsDevCmd.bat\" && msbuild src\\upload-tests.proj /p:TargetGroup=${params.TGroup} /p:ArchGroup=${params.AGroup} /p:ConfigurationGroup=${params.CGroup} /p:TestProduct=corefx /p:TimeoutInSeconds=1200 /p:TargetOS=Windows_NT /p:HelixJobType=test/functional/cli/ /p:HelixSource=${helixSource} /p:BuildMoniker=${helixBuild} /p:HelixCreator=${helixCreator} /p:CloudDropAccountName=dotnetbuilddrops /p:CloudResultsAccountName=dotnetjobresults /p:CloudDropAccessToken=%CloudDropAccessToken% /p:CloudResultsAccessToken=%OutputCloudResultsAccessToken% /p:HelixApiEndpoint=https://helix.dot.net/api/2017-04-14/jobs /p:TargetQueues=\"${targetHelixQueues.join(',')}\" /p:HelixLogFolder= /p:HelixLogFolder=${WORKSPACE}\\${logFolder}\\ /p:HelixCorrelationInfoFileName=SubmittedHelixRuns.txt"
 
                 submittedHelixJson = readJSON file: "${logFolder}\\SubmittedHelixRuns.txt"
             }
@@ -100,10 +106,10 @@ if (submitToHelix) {
     stage ('Execute Tests') {
         def contextBase
         if (params.TestOuter) {
-            contextBase = "Win tests w/outer - ${params.AGroup} ${params.CGroup}"
+            contextBase = "Win tests w/outer - ${params.TGroup} ${params.AGroup} ${params.CGroup}"
         }
         else {
-            contextBase = "Win tests - ${params.AGroup} ${params.CGroup}"
+            contextBase = "Win tests - ${params.TGroup} ${params.AGroup} ${params.CGroup}"
         }
         waitForHelixRuns(submittedHelixJson, contextBase)
     }
