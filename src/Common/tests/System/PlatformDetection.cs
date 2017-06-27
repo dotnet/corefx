e30877f64a06aebc78b1ec3282d5ece7e9402ff4
Jose Perez Rodriguez <joperezr@microsoft.com>
2017-06-27 14:31:30 -0700

    Fix the three System.Data.SqlClient tests that were failing on arm due to sni.dll not being available on arm

--- src/Common/tests/System/PlatformDetection.cs
+++ src/Common/tests/System/PlatformDetection.cs
@@ -36,6 +36,9 @@ namespace System
             GetWindowsVersion() == 10 && GetWindowsMinorVersion() == 0 && GetWindowsBuildNumber() >= 14393;
         public static bool IsWindows10Version1703OrGreater => IsWindows &&
             GetWindowsVersion() == 10 && GetWindowsMinorVersion() == 0 && GetWindowsBuildNumber() >= 15063;
+        public static bool IsArmProcess => RuntimeInformation.ProcessArchitecture == Architecture.Arm;
+        public static bool IsNotArmProcess => !IsArmProcess;
+        public static bool IsNotArmProcessAndIsNotWindowsNanoServer => IsNotArmProcess && IsNotWindowsNanoServer;
 
         // Windows OneCoreUAP SKU doesn't have httpapi.dll
         public static bool IsNotOneCoreUAP => (!IsWindows || 
--- src/System.Data.SqlClient/tests/FunctionalTests/SqlConnectionTest.cs
+++ src/System.Data.SqlClient/tests/FunctionalTests/SqlConnectionTest.cs
@@ -26,7 +26,7 @@ namespace System.Data.SqlClient.Tests
 
 
         [PlatformSpecific(TestPlatforms.Windows)]  // Integ auth on Test server is supported on Windows right now
-        [ConditionalFact(nameof(PlatformDetection) + "." + nameof(PlatformDetection.IsNotWindowsNanoServer))] // https://github.com/dotnet/corefx/issues/19218
+        [ConditionalFact(nameof(PlatformDetection) + "." + nameof(PlatformDetection.IsNotArmProcessAndIsNotWindowsNanoServer))] // https://github.com/dotnet/corefx/issues/19218 And https://github.com/dotnet/corefx/issues/21598
         [ActiveIssue("https://github.com/dotnet/corefx/issues/20718", TargetFrameworkMonikers.UapAot)]
         public void IntegratedAuthConnectionTest()
         {
