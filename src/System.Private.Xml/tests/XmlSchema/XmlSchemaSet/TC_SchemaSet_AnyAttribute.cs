3b9589487c33b7fc34075018b0ec2cde7beea8d4
programmer <localbusinesspl@gmail.com>
2017-07-07 00:08:47 +0200

    Add TC_SchemaSet_AnyAttribute class and initializers (#16649)

---
+++ src/System.Private.Xml/tests/XmlSchema/XmlSchemaSet/TC_SchemaSet_AnyAttribute.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+using System.IO;
+using System.Xml.Schema;
+
+namespace System.Xml.Tests
+{
+    public class TC_SchemaSet_AnyAttribute : TC_SchemaSetBase
+    {
+        private ITestOutputHelper _output;
+
+        public TC_SchemaSet_AnyAttribute(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        public bool bWarningCallback;
+
+        public bool bErrorCallback;
+        public int errorCount;
+        public int warningCount;
+        public bool WarningInnerExceptionSet = false;
+        public bool ErrorInnerExceptionSet = false;
+
+        public void Initialize()
+        {
+            bWarningCallback = bErrorCallback = false;
+            errorCount = warningCount = 0;
+            WarningInnerExceptionSet = ErrorInnerExceptionSet = false;
+        }
+
+        //hook up validaton callback
+        public void ValidationCallback(object sender, ValidationEventArgs args)
+        {
+            if (args.Severity == XmlSeverityType.Warning)
+            {
+                _output.WriteLine("WARNING: ");
+                bWarningCallback = true;
+                warningCount++;
+                WarningInnerExceptionSet = (args.Exception.InnerException != null);
+                _output.WriteLine("\nInnerExceptionSet : " + WarningInnerExceptionSet + "\n");
+            }
+            else if (args.Severity == XmlSeverityType.Error)
+            {
+                _output.WriteLine("ERROR: ");
+                bErrorCallback = true;
+                errorCount++;
+                ErrorInnerExceptionSet = (args.Exception.InnerException != null);
+                _output.WriteLine("\nInnerExceptionSet : " + ErrorInnerExceptionSet + "\n");
+            }
+
+            _output.WriteLine(args.Message); // Print the error to the screen.
+        }
+    }
+}
