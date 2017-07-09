0b3134f7b9a2b2be73dcbfa0deca7a8da24d6621
Stephen Toub <stoub@microsoft.com>
2017-07-09 00:50:15 -0400

    Disable GetAsync_UnsupportedSSLVersion_Throws test on Linux

--- src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.SslProtocols.cs
+++ src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.SslProtocols.cs
@@ -154,11 +154,10 @@ namespace System.Net.Http.Functional.Tests
         [MemberData(nameof(NotSupportedSSLVersionServers))]
         public async Task GetAsync_UnsupportedSSLVersion_Throws(string name, string url)
         {
-            if (ManagedHandlerTestHelpers.IsEnabled &&
-                PlatformDetection.IsWindows &&
-                !PlatformDetection.IsWindows10Version1607OrGreater)
+            if (ManagedHandlerTestHelpers.IsEnabled && !PlatformDetection.IsWindows10Version1607OrGreater)
             {
-                // https://github.com/dotnet/corefx/issues/21925#issuecomment-313408314
+                // On Windows, https://github.com/dotnet/corefx/issues/21925#issuecomment-313408314
+                // On Linux, an older version of OpenSSL may permit negotiating SSLv3.
                 return;
             }
 
