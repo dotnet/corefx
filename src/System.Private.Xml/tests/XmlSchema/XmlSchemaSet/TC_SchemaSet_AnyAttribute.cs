d5d1166819ff353dcc29a4c4349bf348563d85d1
programmer <localbusinesspl@gmail.com>
2017-07-09 21:37:33 +0200

    Add XmlSchemaAnyAttribute test cases(#16649)

--- src/System.Private.Xml/tests/XmlSchema/XmlSchemaSet/TC_SchemaSet_AnyAttribute.cs
+++ src/System.Private.Xml/tests/XmlSchema/XmlSchemaSet/TC_SchemaSet_AnyAttribute.cs
@@ -52,9 +52,155 @@ namespace System.Xml.Tests
             _output.WriteLine(args.Message); // Print the error to the screen.
         }
 
-        [Theory]
-        public void v1()
+        public XmlSchema GetUnionSchema(string ns1, string ns2, string attrNs)
         {
+            var xsd = @"<xs:schema xmlns:xs='http://www.w3.org/2001/XMLSchema' targetNamespace='" + attrNs + @"' xmlns='" + attrNs + @"'>
+                        <xs:attribute name='baseattribute'>
+                          <xs:simpleType >
+                          <xs:restriction base = 'xs:int' >
+                          <xs:maxInclusive value = '1000' />
+                          </xs:restriction >
+                        </xs:simpleType >
+                        </xs:attribute >
+                        <xs:complexType name = 't'>
+                            <xs:sequence >
+                            <xs:element name = 'e1' type = 'xs:string' />
+                            </xs:sequence >
+                            <xs:anyAttribute namespace='" + ns2 + @"'/>
+                        </xs:complexType>
+                        <xs:complexType name = 't1'>
+                          <xs:complexContent>
+                           <xs:extension base='t'>
+                            <xs:sequence >
+                             <xs:element name = 'e2' type = 'xs:string' />
+                            </xs:sequence>
+                            <xs:anyAttribute namespace='" + ns1 + @"'/>
+                           </xs:extension>
+                         </xs:complexContent>
+                        </xs:complexType>
+                        <xs:complexType name = 't2'>
+                           <xs:complexContent>
+                            <xs:restriction base='t1'>
+                            <xs:sequence >
+                            <xs:element name = 'e1' type = 'xs:string' fixed='name'/>
+                            <xs:element name = 'e2' type = 'xs:string' fixed='name'/>
+                            </xs:sequence >
+                            <xs:attribute ref='baseattribute'/>
+                            </xs:restriction>
+                            </xs:complexContent>
+                        </xs:complexType>
+                        </xs:schema>";
+
+            XmlSchema schema = XmlSchema.Read(new StringReader(xsd), null);
+            return schema;
+        }
+
+        public XmlSchema GetIntersectionSchema(string ns1, string ns2, string attrNs)
+        {
+            var xsd = @"<xs:schema xmlns:xs='http://www.w3.org/2001/XMLSchema' targetNamespace='" + attrNs + @"' xmlns='" + attrNs + @"'>
+                        <xs:attribute name='baseattribute'>
+                          <xs:simpleType >
+                          <xs:restriction base = 'xs:int' >
+                          <xs:maxInclusive value = '1000' />
+                          </xs:restriction >
+                        </xs:simpleType >
+                        </xs:attribute >
+                        <xs:attributeGroup name = 'attr-gr' >
+                            <xs:attribute name = 'a1' type = 'xs:int' />
+                            <xs:anyAttribute namespace='" + ns2 + @"'/>
+                        </xs:attributeGroup >
+                        <xs:complexType name = 't'>
+                            <xs:sequence >
+                            <xs:element name = 'e1' type = 'xs:string' />
+                            </xs:sequence >
+                            <xs:attributeGroup ref='attr-gr' />
+                            <xs:anyAttribute namespace='" + ns1 + @"'/>
+                        </xs:complexType>
+                        <xs:complexType name = 't1'>
+                           <xs:complexContent>
+                            <xs:restriction base='t'>
+                            <xs:sequence >
+                            <xs:element name = 'e1' type = 'xs:string' fixed='name'/>
+                            </xs:sequence >
+                            <xs:attribute ref='baseattribute'/>
+                            </xs:restriction>
+                            </xs:complexContent>
+                        </xs:complexType>
+                        </xs:schema>";
+
+            XmlSchema schema = XmlSchema.Read(new StringReader(xsd), null);
+            return schema;
+        }
+
+        //Intersection namespaces
+        [Theory]
+        //[Variation(Desc = "complextype Any ns - ##any, attrgroup Any ns2, allow ns2 attribute")]
+        [InlineData("##any", "ns2", "ns2", 0)]
+        //[Variation(Desc = "complextype Any ns - ##any, attrgroup Any ns2, not allow ns1 attribute")]
+        [InlineData("##any", "ns2", "ns1", 1)]
+        //[Variation(Desc = "complextype Any ns - ns2, attrgroup Any ##any, allow ns2 attribute")]
+        [InlineData("ns2", "##any", "ns2", 0)]
+        //[Variation(Desc = "complextype Any ns - ns2, attrgroup Any ##any, not allow ns1 attribute")]
+        [InlineData("ns2", "##any", "ns1", 1)]
+        //[Variation(Desc = "complextype Any ns - ns1 ns2, attrgroup Any ##other, not allow ns1 attribute")]
+        [InlineData("ns1 ns2", "##other", "ns1", 1)]
+        //[Variation(Desc = "complextype Any ns - ns1 ns2, attrgroup Any ##other, not allow ns2 attribute")]
+        [InlineData("ns1 ns2", "##other", "ns2", 1)]
+        //[Variation(Desc = "complextype Any ns - ns1 ns2, attrgroup Any #other, not allow ns3 attribute")]
+        [InlineData("ns1 ns2", "##other", "ns3", 1)]
+        //[Variation(Desc = "complextype Any ns - ##other, attrgroup Any ns1 ns2, not allow ns1 attribute")]
+        [InlineData("##other", "ns1 ns2", "ns1", 1)]
+        //[Variation(Desc = "complextype Any ns - ##other, attrgroup Any ns1 ns2, not allow ns2 attribute")]
+        [InlineData("##other", "ns1 ns2", "ns2", 1)]
+        //[Variation(Desc = "complextype Any ns - ##other, attrgroup Any ns1 ns2, not allow ns3 attribute")]
+        [InlineData("##other", "ns1 ns2", "ns3", 1)]
+        //[Variation(Desc = "complextype Any ns - ns1 ns3, attrgroup Any ns1 ns2, not allow ns3 attribute")]
+        [InlineData("ns1 ns3", "ns1 ns2", "ns3", 1)]
+        //[Variation(Desc = "complextype Any ns - ns1 ns3, attrgroup Any ns1 ns2, not allow ns2 attribute")]
+        [InlineData("ns1 ns3", "ns1 ns2", "ns2", 1)]
+        //[Variation(Desc = "complextype Any ns - ns1 ns3, attrgroup Any ns1 ns2, allow ns1 attribute")]
+        [InlineData("ns1 ns3", "ns1 ns2", "ns1", 0)]
+        //[Variation(Desc = "complextype Any ns - ##other, attrgroup Any ##other, not allow ns1 attribute")]
+        [InlineData("##other", "##other", "ns1", 1)]
+        public void v1(string ns1, string ns2, string attrNs, int expectedError)
+        {
+            Initialize();
+            XmlSchemaSet xss = new XmlSchemaSet();
+            xss.XmlResolver = new XmlUrlResolver();
+            xss.ValidationEventHandler += new ValidationEventHandler(ValidationCallback);
+            xss.Add(GetIntersectionSchema(ns1, ns2, attrNs));
+            xss.Compile();
+
+            Assert.Equal(errorCount, expectedError);
+        }
+
+        [Theory]
+        //[Variation(Desc = "basetype Any ns - ##any, derivedType Any ns - ns1, allow ns2 attribute")]
+        [InlineData("##any", "ns1", "ns2",0)]
+        //[Variation(Desc = "basetype Any ns - ns1, derivedType Any ns - ##any, allow ns2 attribute")]
+        [InlineData("ns1", "##any", "ns2", 0)]
+        //[Variation(Desc = "basetype Any ns - ns1 ns2, derivedType Any ns - ns2 ns3 , allow ns3 attribute")]
+        [InlineData("ns1 ns2", "ns2 ns3", "ns3", 0)]
+        //[Variation(Desc = "basetype Any ns - ##other, derivedType Any ns - ##other , not allow current ns")]
+        [InlineData("##other", "##other", "ns1", 1)]
+        //[Variation(Desc = "basetype Any ns - ns1 ns2, derivedType Any ns - ##other , allow ns1")]
+        [InlineData("ns1 ns2", "##other", "ns1", 0)]
+        //[Variation(Desc = "basetype Any ns - ns1 ns2, derivedType Any ns - ##other , allow ns2")]
+        [InlineData("ns1 ns2", "##other", "ns2", 0)]
+        //[Variation(Desc = "basetype Any ns - ##other, derivedType Any ns - ns1 ns2 , allow ns2")]
+        [InlineData("##other", "ns1 ns2", "ns1", 0)]
+        //[Variation(Desc = "basetype Any ns - ##other, derivedType Any ns - ns1 ns2 , allow ns2")]
+        [InlineData("##other", "ns1 ns2", "ns2", 0)]
+        public void v2(string ns1, string ns2, string attrNs, int expectedError)
+        {
+            Initialize();
+            XmlSchemaSet xss = new XmlSchemaSet();
+            xss.XmlResolver = new XmlUrlResolver();
+            xss.ValidationEventHandler += new ValidationEventHandler(ValidationCallback);
+            xss.Add(GetUnionSchema(ns1, ns2, attrNs));
+            xss.Compile();
+
+            Assert.Equal(errorCount, expectedError);
         }
     }
 }
