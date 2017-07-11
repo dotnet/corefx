a2098090565f6992b542d420bcb796f8a87447d0
Jon Hanna <jon@hackcraft.net>
2017-07-11 19:09:07 +0100

    Add test for attempt dynamic invocation of conditional method.

---
+++ src/Microsoft.CSharp/tests/BindingErrors.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#define TEST_DEFINITION
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.CSharp.RuntimeBinder.Tests
+{
+    public class BindingErrors
+    {
+
+
+        private class TypeWithConditional
+        {
+            [Conditional("TEST_DEFINITION")]
+            public virtual void DoNothing()
+            {
+            }
+        }
+
+        private class DerivedTypeWithConditional : TypeWithConditional
+        {
+            public override void DoNothing()
+            {
+            }
+        }
+
+        [Fact]
+        public void CannotBindToConditional()
+        {
+            var obj = new TypeWithConditional();
+            obj.DoNothing();
+            dynamic d = obj;
+            Assert.Throws<RuntimeBinderException>(() => d.DoNothing());
+        }
+
+        [Fact]
+        public void CannotBindToOverriddenConditional()
+        {
+            var obj = new DerivedTypeWithConditional();
+            obj.DoNothing();
+            dynamic d = obj;
+            Assert.Throws<RuntimeBinderException>(() => d.DoNothing());
+        }
+    }
+}
