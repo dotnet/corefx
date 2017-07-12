e524b91fb647c4d5f2f8fe0fd22ec686c5d0e0ad
Jeremy Barton <jbarton@microsoft.com>
2017-07-12 09:20:00 -0700

    Disable tests on High Sierra which trigger corrupt chains
    Whatever bad loop is currently present on High Sierra with building self-signed
    chains with revocation enabled seems to be inside a critical section, so any other
    tests running concurrently have a chance of hitting the "took too long" internal
    timeout and reporting an invalid chain, too.
    
    Avoiding calling Verify on a self-signed cert in TestVerify and avoiding
    TestResetMethod (whose first chain has revocation enabled) results in all tests
    passing with parallelism enabled across ~60 sequential runs on High Sierra Beta2.

--- src/System.Security.Cryptography.X509Certificates/tests/ChainTests.cs
+++ src/System.Security.Cryptography.X509Certificates/tests/ChainTests.cs
@@ -14,6 +14,7 @@ namespace System.Security.Cryptography.X509Certificates.Tests
     public static class ChainTests
     {
         internal static bool CanModifyStores { get; } = TestEnvironmentConfiguration.CanModifyStores;
+        internal static bool CanBuildSelfSignedChainReliably { get; } = !PlatformDetection.IsMacOsHighSierra;
 
         private static bool TrustsMicrosoftDotComRoot
         {
@@ -164,7 +165,7 @@ namespace System.Security.Cryptography.X509Certificates.Tests
             Assert.Equal(IntPtr.Zero, chain.ChainContext);
         }
 
-        [Fact]
+        [ConditionalFact(nameof(CanBuildSelfSignedChainReliably))]
         public static void TestResetMethod()
         {
             using (var sampleCert = new X509Certificate2(TestData.DssCer))
