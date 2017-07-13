e740973fed660da4426218bf2ec4be35a60ad095
David Shulman <david.shulman@microsoft.com>
2017-07-13 13:21:41 -0700

    Reconcile behavior of MaxRequestContentBufferSize across .NET Core (#22201)
    * Reconcile behavior of MaxRequestContentBufferSize across .NET Core
    
    The HttpClientHandler property, MaxRequestContentBufferSize, has been
    deprecated. This PR reconciles the behavior of the getter and setter of
    the property.
    
    Similar to other decisions in HttpClient PRs with these kinds of
    behavior differences, we are not going to throw PNSE. We will document
    the differences in the AppCompat docs.
    
    Fixes #7879
    
    * Address PR feedback

--- src/System.Net.Http/src/System/Net/Http/HttpClientHandler.Core.cs
+++ src/System.Net.Http/src/System/Net/Http/HttpClientHandler.Core.cs
@@ -2,25 +2,65 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Globalization;
+
 namespace System.Net.Http
 {
     public partial class HttpClientHandler : HttpMessageHandler
     {
         // This partial implementation contains members common to Windows and Unix running on .NET Core.
 
+        private volatile bool _operationStarted = false;
+        private volatile bool _disposed = false;
+
         public long MaxRequestContentBufferSize
         {
-            // This property has been deprecated. In the .NET Desktop it was only used when the handler needed to 
+            // This property is not supported. In the .NET Framework it was only used when the handler needed to 
             // automatically buffer the request content. That only happened if neither 'Content-Length' nor 
             // 'Transfer-Encoding: chunked' request headers were specified. So, the handler thus needed to buffer
             // in the request content to determine its length and then would choose 'Content-Length' semantics when
-            // POST'ing. In CoreCLR and .NETNative, the handler will resolve the ambiguity by always choosing
+            // POST'ing. In .NET Core and UAP platforms, the handler will resolve the ambiguity by always choosing
             // 'Transfer-Encoding: chunked'. The handler will never automatically buffer in the request content.
-            get { return 0; }
+            get
+            {
+                return 0; // Returning zero is appropriate since in .NET Framework it means no limit.
+            }
 
-            // TODO (#7879): Add message/link to exception explaining the deprecation. 
-            // Update corresponding exception in HttpClientHandler.Unix.cs if/when this is updated.
-            set { throw new PlatformNotSupportedException(); }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+
+                if (value > HttpContent.MaxBufferSize)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value,
+                        string.Format(CultureInfo.InvariantCulture, SR.net_http_content_buffersize_limit,
+                        HttpContent.MaxBufferSize));
+                }                
+
+                CheckDisposedOrStarted();
+
+                // No-op on property setter.
+            }
+        }
+
+        private void CheckDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(GetType().ToString());
+            }
+        }
+
+        private void CheckDisposedOrStarted()
+        {
+            CheckDisposed();
+            if (_operationStarted)
+            {
+                throw new InvalidOperationException(SR.net_http_operation_started);
+            }
         }
     }
 }
