3c359c5cb7a9305aef74e3ac34b5a306a2f7b7fd
Jan Kotas <jkotas@microsoft.com>
2017-07-16 04:28:15 +0200

    Use POSIX "command" instead of non-standard "which" (#22289)

--- init-tools.sh
+++ init-tools.sh
@@ -19,7 +19,7 @@ if [ -z "$__DOTNET_PKG" ]; then
     if [ "$(uname -m | grep "i[3456]86")" = "i686" ]; then
         echo "Warning: build not supported on 32 bit Unix"
     fi
-OSName=$(uname -s)
+    OSName=$(uname -s)
     case $OSName in
         Darwin)
             OS=OSX
@@ -73,11 +73,10 @@ if [ ! -e $__INIT_TOOLS_DONE_MARKER ]; then
             __DOTNET_LOCATION="https://dotnetcli.azureedge.net/dotnet/Sdk/${__DOTNET_TOOLS_VERSION}/${__DOTNET_PKG}.${__DOTNET_TOOLS_VERSION}.tar.gz"
             # curl has HTTPS CA trust-issues less often than wget, so lets try that first.
             echo "Installing '${__DOTNET_LOCATION}' to '$__DOTNET_PATH/dotnet.tar'" >> $__init_tools_log
-            which curl > /dev/null 2> /dev/null
-            if [ $? -ne 0 ]; then
-                wget -q -O $__DOTNET_PATH/dotnet.tar ${__DOTNET_LOCATION}
-            else
+            if command -v curl > /dev/null; then
                 curl --retry 10 -sSL --create-dirs -o $__DOTNET_PATH/dotnet.tar ${__DOTNET_LOCATION}
+            else
+                wget -q -O $__DOTNET_PATH/dotnet.tar ${__DOTNET_LOCATION}
             fi
             cd $__DOTNET_PATH
             tar -xf $__DOTNET_PATH/dotnet.tar
@@ -113,7 +112,7 @@ if [ ! -e $__INIT_TOOLS_DONE_MARKER ]; then
         chmod +x $__BUILD_TOOLS_PATH/init-tools.sh
         $__BUILD_TOOLS_PATH/init-tools.sh $__scriptpath $__DOTNET_CMD $__TOOLRUNTIME_DIR >> $__init_tools_log
         if [ "$?" != "0" ]; then
-            echo "ERROR: An error occured when trying to initialize the tools." 1>&2
+            echo "ERROR: An error occurred when trying to initialize the tools." 1>&2
             display_error_message
             exit 1
         fi
