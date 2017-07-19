7842f11941c3440e1fab790c97c6241ed29c3a8c
Karthik Rajasekaran <karajas@microsoft.com>
2017-07-19 15:39:25 -0700

    Port of https://github.com/dotnet/corefx/pull/21894 to release/1.1.0

--- init-tools.sh
+++ init-tools.sh
@@ -85,6 +85,13 @@ OSName=$(uname -s)
             ;;
   esac
 fi
+
+display_error_message()
+{
+    echo "Please check the detailed log that follows." 1>&2
+    cat "$__init_tools_log" 1>&2
+}
+
 if [ ! -e $__INIT_TOOLS_DONE_MARKER ]; then
     if [ -e $__TOOLRUNTIME_DIR ]; then rm -rf -- $__TOOLRUNTIME_DIR; fi
     echo "Running: $__scriptpath/init-tools.sh" > $__init_tools_log
@@ -113,14 +120,18 @@ if [ ! -e $__INIT_TOOLS_DONE_MARKER ]; then
         echo "Restoring BuildTools version $__BUILD_TOOLS_PACKAGE_VERSION..."
         echo "Running: $__DOTNET_CMD restore \"$__PROJECT_JSON_FILE\" --no-cache --packages $__PACKAGES_DIR --source $__BUILDTOOLS_SOURCE" >> $__init_tools_log
         $__DOTNET_CMD restore "$__PROJECT_JSON_FILE" --no-cache --packages $__PACKAGES_DIR --source $__BUILDTOOLS_SOURCE >> $__init_tools_log
-        if [ ! -e "$__BUILD_TOOLS_PATH/init-tools.sh" ]; then echo "ERROR: Could not restore build tools correctly. See '$__init_tools_log' for more details."1>&2; fi
+        if [ ! -e "$__BUILD_TOOLS_PATH/init-tools.sh" ]; then
+            echo "ERROR: Could not restore build tools correctly." 1>&2
+            display_error_message
+        fi
     fi
 
     echo "Initializing BuildTools..."
     echo "Running: $__BUILD_TOOLS_PATH/init-tools.sh $__scriptpath $__DOTNET_CMD $__TOOLRUNTIME_DIR" >> $__init_tools_log
     $__BUILD_TOOLS_PATH/init-tools.sh $__scriptpath $__DOTNET_CMD $__TOOLRUNTIME_DIR >> $__init_tools_log
     if [ "$?" != "0" ]; then
-        echo "ERROR: An error occured when trying to initialize the tools. Please check '$__init_tools_log' for more details."1>&2
+        echo "ERROR: Could not restore build tools correctly." 1>&2
+        display_error_message
         exit 1
     fi
     touch $__INIT_TOOLS_DONE_MARKER
