682617c63467c021292616c0024b56a079765880
Stephen Toub <stoub@microsoft.com>
2017-07-24 14:59:50 -0400

    Expose {ReadOnly}Span from System.Runtime

--- src/System.Runtime/ref/System.Runtime.cs
+++ src/System.Runtime/ref/System.Runtime.cs
@@ -1813,6 +1813,38 @@ namespace System
         public RankException(string message, System.Exception innerException) { }
         protected RankException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public partial struct ReadOnlySpan<T>
+    {
+        public static ReadOnlySpan<T> Empty { get { throw null; } }
+        public ReadOnlySpan(T[] array) { throw null; }
+        public ReadOnlySpan(T[] array, int start) { throw null; }
+        public ReadOnlySpan(T[] array, int start, int length) { throw null; }
+        [CLSCompliant(false)]
+        public unsafe ReadOnlySpan(void* pointer, int length) { throw null; }
+        public bool IsEmpty { get { throw null; } }
+        public T this[int index] { get { throw null; } }
+        public int Length { get { throw null; } }
+        public void CopyTo(Span<T> destination) { }
+        public static ReadOnlySpan<T> DangerousCreate(object obj, ref T objectData, int length) { throw null; }
+        public ref T DangerousGetPinnableReference() { throw null; }
+#pragma warning disable 0809
+        [System.ObsoleteAttribute("Equals() on ReadOnlySpan will always throw an exception. Use == instead.")]
+        [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+        public override bool Equals(object obj) { throw null; }
+        [System.ObsoleteAttribute("GetHashCode() on ReadOnlySpan will always throw an exception.")]
+        [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+        public override int GetHashCode() { throw null; }
+#pragma warning restore 0809
+        public static bool operator ==(ReadOnlySpan<T> left, ReadOnlySpan<T> right) { throw null; }
+        public static implicit operator ReadOnlySpan<T>(T[] array) { throw null; }
+        public static implicit operator ReadOnlySpan<T>(ArraySegment<T> arraySegment) { throw null; }
+        public static bool operator !=(ReadOnlySpan<T> left, ReadOnlySpan<T> right) { throw null; }
+        public ReadOnlySpan<T> Slice(int start) { throw null; }
+        public ReadOnlySpan<T> Slice(int start, int length) { throw null; }
+        public T[] ToArray() { throw null; }
+        public bool TryCopyTo(Span<T> destination) { throw null; }
+    }
     public partial class ResolveEventArgs : System.EventArgs
     {
         public ResolveEventArgs(string name) { }
@@ -1956,6 +1988,41 @@ namespace System
         public string ToString(string format, System.IFormatProvider provider) { throw null; }
         public static bool TryParse(string s, System.Globalization.NumberStyles style, System.IFormatProvider provider, out float result) { throw null; }
         public static bool TryParse(string s, out float result) { throw null; }
+    }
+    [System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
+    public partial struct Span<T>
+    {
+        public static Span<T> Empty { get { throw null; } }
+        public Span(T[] array) { throw null; }
+        public Span(T[] array, int start) { throw null; }
+        public Span(T[] array, int start, int length) { throw null; }
+        [CLSCompliant(false)]
+        public unsafe Span(void* pointer, int length) { throw null; }
+        public bool IsEmpty { get { throw null; } }
+        public ref T this[int index] { get { throw null; } }
+        public int Length { get { throw null; } }
+        public void Clear() { }
+        public void Fill(T value) { }
+        public void CopyTo(Span<T> destination) { }
+        public static Span<T> DangerousCreate(object obj, ref T objectData, int length) { throw null; }
+        public ref T DangerousGetPinnableReference() { throw null; }
+#pragma warning disable 0809
+        [System.ObsoleteAttribute("Equals() on Span will always throw an exception. Use == instead.")]
+        [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+        public override bool Equals(object obj) { throw null; }
+        [System.ObsoleteAttribute("GetHashCode() on Span will always throw an exception.")]
+        [System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+        public override int GetHashCode() { throw null; }
+#pragma warning restore 0809
+        public static bool operator ==(Span<T> left, Span<T> right) { throw null; }
+        public static implicit operator Span<T>(T[] array) { throw null; }
+        public static implicit operator Span<T>(ArraySegment<T> arraySegment) { throw null; }
+        public static implicit operator ReadOnlySpan<T>(Span<T> span) { throw null; }
+        public static bool operator !=(Span<T> left, Span<T> right) { throw null; }
+        public Span<T> Slice(int start) { throw null; }
+        public Span<T> Slice(int start, int length) { throw null; }
+        public T[] ToArray() { throw null; }
+        public bool TryCopyTo(Span<T> destination) { throw null; }
     }
     [System.AttributeUsageAttribute((System.AttributeTargets)(64))]
     public sealed partial class STAThreadAttribute : System.Attribute
