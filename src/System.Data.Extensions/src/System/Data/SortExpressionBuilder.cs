de5caf6b912c5db43a0e5e451e9f9c2933f42a38
t-nichik <t-nichik@microsoft.com>
2017-07-26 13:47:59 -0700

    Clean up missed files

--- src/System.Data.Extensions/src/System/Data/SortExpressionBuilder.cs
+++ src/System.Data.Extensions/src/System/Data/SortExpressionBuilder.cs
@@ -1,10 +1,6 @@
-﻿//------------------------------------------------------------------------------
-// <copyright file="EnumerableDataTable.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
-// <owner current="true" primary="true">Microsoft</owner>
-// <owner current="true" primary="false">Microsoft</owner>
-//------------------------------------------------------------------------------
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
 using System.Diagnostics;
