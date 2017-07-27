24ad046152e3829d31d0e75843fae35355b39fe5
t-nichik <t-nichik@microsoft.com>
2017-07-26 17:22:08 -0700

    Reduce references in /src and /ref .csproj files, remove unused strings, change to netstandard

--- src/System.Data.Extensions/ref/System.Data.Extensions.cs
+++ src/System.Data.Extensions/ref/System.Data.Extensions.cs
@@ -1,97 +1,88 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-
-using System.Collections;
-using System.Collections.Generic;
+// ------------------------------------------------------------------------------
+// Changes to this file must follow the http://aka.ms/api-review process.
+// ------------------------------------------------------------------------------
 
 namespace System.Data
 {
-    [Serializable]
-    public abstract class TypedTableBase<T> : DataTable, IEnumerable<T> where T : DataRow
+    public static partial class DataRowComparer
     {
-        public IEnumerator<T> GetEnumerator() { throw null; }
-        IEnumerator IEnumerable.GetEnumerator() { throw null; }
-        public EnumerableRowCollection<TResult> Cast<TResult>() { throw null; }
+        public static System.Data.DataRowComparer<System.Data.DataRow> Default { get { throw null; } }
     }
-
-    public static class TypedTableBaseExtensions
+    public sealed partial class DataRowComparer<TRow> : System.Collections.Generic.IEqualityComparer<TRow> where TRow : System.Data.DataRow
     {
-        public static EnumerableRowCollection<TRow> Where<TRow>(this TypedTableBase<TRow> source, Func<TRow, bool> predicate) where TRow : DataRow { throw null; }
-        public static OrderedEnumerableRowCollection<TRow> OrderBy<TRow, TKey>(this TypedTableBase<TRow> source, Func<TRow, TKey> keySelector) where TRow : DataRow { throw null; }
-        public static OrderedEnumerableRowCollection<TRow> OrderBy<TRow, TKey>(this TypedTableBase<TRow> source, Func<TRow, TKey> keySelector, IComparer<TKey> comparer) where TRow : DataRow { throw null; }
-        public static OrderedEnumerableRowCollection<TRow> OrderByDescending<TRow, TKey>(this TypedTableBase<TRow> source, Func<TRow, TKey> keySelector) where TRow : DataRow { throw null; }
-        public static OrderedEnumerableRowCollection<TRow> OrderByDescending<TRow, TKey>(this TypedTableBase<TRow> source, Func<TRow, TKey> keySelector, IComparer<TKey> comparer) where TRow : DataRow { throw null; }
-        public static EnumerableRowCollection<S> Select<TRow, S>(this TypedTableBase<TRow> source, Func<TRow, S> selector) where TRow : DataRow { throw null; }
-        public static EnumerableRowCollection<TRow> AsEnumerable<TRow>(this TypedTableBase<TRow> source) where TRow : DataRow { throw null; }
-        public static TRow ElementAtOrDefault<TRow>(this TypedTableBase<TRow> source, int index) where TRow : DataRow { throw null; }
-    }
-
-    public sealed class OrderedEnumerableRowCollection<TRow> : EnumerableRowCollection<TRow>
-    {
-        internal OrderedEnumerableRowCollection() { }
-    }
-
-    public abstract class EnumerableRowCollection : IEnumerable
-    {
-        internal EnumerableRowCollection() { }
-        IEnumerator IEnumerable.GetEnumerator() { throw null; }
-    }
-
-    public class EnumerableRowCollection<TRow> : EnumerableRowCollection, IEnumerable<TRow>
-    {
-        internal EnumerableRowCollection() { }
-        IEnumerator IEnumerable.GetEnumerator() { throw null; }
-        public IEnumerator<TRow> GetEnumerator() { throw null; }
-    }
-
-    public static class EnumerableRowCollectionExtensions
-    {
-        public static EnumerableRowCollection<TRow> Where<TRow>(this EnumerableRowCollection<TRow> source, Func<TRow, bool> predicate) { throw null; }
-        public static OrderedEnumerableRowCollection<TRow> OrderBy<TRow, TKey>(this EnumerableRowCollection<TRow> source, Func<TRow, TKey> keySelector) { throw null; }
-        public static OrderedEnumerableRowCollection<TRow> OrderBy<TRow, TKey>(this EnumerableRowCollection<TRow> source, Func<TRow, TKey> keySelector, IComparer<TKey> comparer) { throw null; }
-        public static OrderedEnumerableRowCollection<TRow> OrderByDescending<TRow, TKey>(this EnumerableRowCollection<TRow> source, Func<TRow, TKey> keySelector) { throw null; }
-        public static OrderedEnumerableRowCollection<TRow> OrderByDescending<TRow, TKey>(this EnumerableRowCollection<TRow> source, Func<TRow, TKey> keySelector, IComparer<TKey> comparer) { throw null; }
-        public static OrderedEnumerableRowCollection<TRow> ThenBy<TRow, TKey>(this OrderedEnumerableRowCollection<TRow> source, Func<TRow, TKey> keySelector) { throw null; }
-        public static OrderedEnumerableRowCollection<TRow> ThenBy<TRow, TKey>(this OrderedEnumerableRowCollection<TRow> source, Func<TRow, TKey> keySelector, IComparer<TKey> comparer) { throw null; }
-        public static OrderedEnumerableRowCollection<TRow> ThenByDescending<TRow, TKey>(this OrderedEnumerableRowCollection<TRow> source, Func<TRow, TKey> keySelector) { throw null; }
-        public static OrderedEnumerableRowCollection<TRow> ThenByDescending<TRow, TKey>(this OrderedEnumerableRowCollection<TRow> source, Func<TRow, TKey> keySelector, IComparer<TKey> comparer) { throw null; }
-        public static EnumerableRowCollection<S> Select<TRow, S>(this EnumerableRowCollection<TRow> source, Func<TRow, S> selector) { throw null; }
-        public static EnumerableRowCollection<TResult> Cast<TResult>(this EnumerableRowCollection source) { throw null; }
-    }
-
-    public static class DataTableExtensions
-    {
-        public static EnumerableRowCollection<DataRow> AsEnumerable(this DataTable source) { throw null; }
-        public static DataTable CopyToDataTable<T>(this IEnumerable<T> source) where T : DataRow { throw null; }
-        public static void CopyToDataTable<T>(this IEnumerable<T> source, DataTable table, LoadOption options) where T : DataRow { throw null; }
-        public static void CopyToDataTable<T>(this IEnumerable<T> source, DataTable table, LoadOption options, FillErrorEventHandler errorHandler) where T : DataRow { throw null; }
-    }
-
-    public static class DataRowExtensions
-    {
-        public static T Field<T>(this DataRow row, string columnName) { throw null; }
-        public static T Field<T>(this DataRow row, DataColumn column) { throw null; }
-        public static T Field<T>(this DataRow row, int columnIndex) { throw null; }
-        public static T Field<T>(this DataRow row, int columnIndex, DataRowVersion version) { throw null; }
-        public static T Field<T>(this DataRow row, string columnName, DataRowVersion version) { throw null; }
-        public static T Field<T>(this DataRow row, DataColumn column, DataRowVersion version) { throw null; }
-        public static void SetField<T>(this DataRow row, int columnIndex, T value) { throw null; }
-        public static void SetField<T>(this DataRow row, string columnName, T value) { throw null; }
-        public static void SetField<T>(this DataRow row, DataColumn column, T value) { throw null; }
-    }
-
-
-    public sealed class DataRowComparer<TRow> : IEqualityComparer<TRow> where TRow : DataRow
-    {
-        private DataRowComparer() { }
-        public static DataRowComparer<TRow> Default { get { throw null; } }
+        internal DataRowComparer() { }
+        public static System.Data.DataRowComparer<TRow> Default { get { throw null; } }
         public bool Equals(TRow leftRow, TRow rightRow) { throw null; }
         public int GetHashCode(TRow row) { throw null; }
     }
-
-    public static class DataRowComparer
+    public static partial class DataRowExtensions
     {
-        public static DataRowComparer<DataRow> Default { get { throw null; } }
+        public static T Field<T>(this System.Data.DataRow row, System.Data.DataColumn column) { throw null; }
+        public static T Field<T>(this System.Data.DataRow row, System.Data.DataColumn column, System.Data.DataRowVersion version) { throw null; }
+        public static T Field<T>(this System.Data.DataRow row, int columnIndex) { throw null; }
+        public static T Field<T>(this System.Data.DataRow row, int columnIndex, System.Data.DataRowVersion version) { throw null; }
+        public static T Field<T>(this System.Data.DataRow row, string columnName) { throw null; }
+        public static T Field<T>(this System.Data.DataRow row, string columnName, System.Data.DataRowVersion version) { throw null; }
+        public static void SetField<T>(this System.Data.DataRow row, System.Data.DataColumn column, T value) { }
+        public static void SetField<T>(this System.Data.DataRow row, int columnIndex, T value) { }
+        public static void SetField<T>(this System.Data.DataRow row, string columnName, T value) { }
+    }
+    public static partial class DataTableExtensions
+    {
+        public static System.Data.EnumerableRowCollection<System.Data.DataRow> AsEnumerable(this System.Data.DataTable source) { throw null; }
+        public static System.Data.DataTable CopyToDataTable<T>(this System.Collections.Generic.IEnumerable<T> source) where T : System.Data.DataRow { throw null; }
+        public static void CopyToDataTable<T>(this System.Collections.Generic.IEnumerable<T> source, System.Data.DataTable table, System.Data.LoadOption options) where T : System.Data.DataRow { }
+        public static void CopyToDataTable<T>(this System.Collections.Generic.IEnumerable<T> source, System.Data.DataTable table, System.Data.LoadOption options, System.Data.FillErrorEventHandler errorHandler) where T : System.Data.DataRow { }
+    }
+    public abstract partial class EnumerableRowCollection : System.Collections.IEnumerable
+    {
+        internal EnumerableRowCollection() { }
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() { throw null; }
+    }
+    public static partial class EnumerableRowCollectionExtensions
+    {
+        public static System.Data.EnumerableRowCollection<TResult> Cast<TResult>(this System.Data.EnumerableRowCollection source) { throw null; }
+        public static System.Data.OrderedEnumerableRowCollection<TRow> OrderByDescending<TRow, TKey>(this System.Data.EnumerableRowCollection<TRow> source, System.Func<TRow, TKey> keySelector) { throw null; }
+        public static System.Data.OrderedEnumerableRowCollection<TRow> OrderByDescending<TRow, TKey>(this System.Data.EnumerableRowCollection<TRow> source, System.Func<TRow, TKey> keySelector, System.Collections.Generic.IComparer<TKey> comparer) { throw null; }
+        public static System.Data.OrderedEnumerableRowCollection<TRow> OrderBy<TRow, TKey>(this System.Data.EnumerableRowCollection<TRow> source, System.Func<TRow, TKey> keySelector) { throw null; }
+        public static System.Data.OrderedEnumerableRowCollection<TRow> OrderBy<TRow, TKey>(this System.Data.EnumerableRowCollection<TRow> source, System.Func<TRow, TKey> keySelector, System.Collections.Generic.IComparer<TKey> comparer) { throw null; }
+        public static System.Data.EnumerableRowCollection<S> Select<TRow, S>(this System.Data.EnumerableRowCollection<TRow> source, System.Func<TRow, S> selector) { throw null; }
+        public static System.Data.OrderedEnumerableRowCollection<TRow> ThenByDescending<TRow, TKey>(this System.Data.OrderedEnumerableRowCollection<TRow> source, System.Func<TRow, TKey> keySelector) { throw null; }
+        public static System.Data.OrderedEnumerableRowCollection<TRow> ThenByDescending<TRow, TKey>(this System.Data.OrderedEnumerableRowCollection<TRow> source, System.Func<TRow, TKey> keySelector, System.Collections.Generic.IComparer<TKey> comparer) { throw null; }
+        public static System.Data.OrderedEnumerableRowCollection<TRow> ThenBy<TRow, TKey>(this System.Data.OrderedEnumerableRowCollection<TRow> source, System.Func<TRow, TKey> keySelector) { throw null; }
+        public static System.Data.OrderedEnumerableRowCollection<TRow> ThenBy<TRow, TKey>(this System.Data.OrderedEnumerableRowCollection<TRow> source, System.Func<TRow, TKey> keySelector, System.Collections.Generic.IComparer<TKey> comparer) { throw null; }
+        public static System.Data.EnumerableRowCollection<TRow> Where<TRow>(this System.Data.EnumerableRowCollection<TRow> source, System.Func<TRow, bool> predicate) { throw null; }
+    }
+    public partial class EnumerableRowCollection<TRow> : System.Data.EnumerableRowCollection, System.Collections.Generic.IEnumerable<TRow>, System.Collections.IEnumerable
+    {
+        internal EnumerableRowCollection() { }
+        public System.Collections.Generic.IEnumerator<TRow> GetEnumerator() { throw null; }
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() { throw null; }
+    }
+    public sealed partial class OrderedEnumerableRowCollection<TRow> : System.Data.EnumerableRowCollection<TRow>
+    {
+        internal OrderedEnumerableRowCollection() { }
+    }
+    public static partial class TypedTableBaseExtensions
+    {
+        public static System.Data.EnumerableRowCollection<TRow> AsEnumerable<TRow>(this System.Data.TypedTableBase<TRow> source) where TRow : System.Data.DataRow { throw null; }
+        public static TRow ElementAtOrDefault<TRow>(this System.Data.TypedTableBase<TRow> source, int index) where TRow : System.Data.DataRow { throw null; }
+        public static System.Data.OrderedEnumerableRowCollection<TRow> OrderByDescending<TRow, TKey>(this System.Data.TypedTableBase<TRow> source, System.Func<TRow, TKey> keySelector) where TRow : System.Data.DataRow { throw null; }
+        public static System.Data.OrderedEnumerableRowCollection<TRow> OrderByDescending<TRow, TKey>(this System.Data.TypedTableBase<TRow> source, System.Func<TRow, TKey> keySelector, System.Collections.Generic.IComparer<TKey> comparer) where TRow : System.Data.DataRow { throw null; }
+        public static System.Data.OrderedEnumerableRowCollection<TRow> OrderBy<TRow, TKey>(this System.Data.TypedTableBase<TRow> source, System.Func<TRow, TKey> keySelector) where TRow : System.Data.DataRow { throw null; }
+        public static System.Data.OrderedEnumerableRowCollection<TRow> OrderBy<TRow, TKey>(this System.Data.TypedTableBase<TRow> source, System.Func<TRow, TKey> keySelector, System.Collections.Generic.IComparer<TKey> comparer) where TRow : System.Data.DataRow { throw null; }
+        public static System.Data.EnumerableRowCollection<S> Select<TRow, S>(this System.Data.TypedTableBase<TRow> source, System.Func<TRow, S> selector) where TRow : System.Data.DataRow { throw null; }
+        public static System.Data.EnumerableRowCollection<TRow> Where<TRow>(this System.Data.TypedTableBase<TRow> source, System.Func<TRow, bool> predicate) where TRow : System.Data.DataRow { throw null; }
+    }
+    public abstract partial class TypedTableBase<T> : System.Data.DataTable, System.Collections.Generic.IEnumerable<T>, System.Collections.IEnumerable where T : System.Data.DataRow
+    {
+        protected TypedTableBase() { }
+        protected TypedTableBase(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) { }
+        public System.Data.EnumerableRowCollection<TResult> Cast<TResult>() { throw null; }
+        public System.Collections.Generic.IEnumerator<T> GetEnumerator() { throw null; }
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() { throw null; }
     }
 }
