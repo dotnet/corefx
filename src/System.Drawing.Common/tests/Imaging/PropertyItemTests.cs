7b4f19adb476ea84e3b95981ea7beb5cc5fe1e59
KostaVlev <kostadinvlev@gmail.com>
2017-08-01 18:13:04 +0300

    Adding [ActiveIssue(20884, TestPlatforms.AnyUnix)]

--- src/System.Drawing.Common/tests/Imaging/PropertyItemTests.cs
+++ src/System.Drawing.Common/tests/Imaging/PropertyItemTests.cs
@@ -39,6 +39,7 @@ namespace System.Drawing.Imaging.Tests
             yield return new object[] { 0, 0, 0, new byte[0] };
         }
 
+        [ActiveIssue(20884, TestPlatforms.AnyUnix)]
         [ConditionalTheory(Helpers.GdiplusIsAvailable)]
         [MemberData(nameof(Properties_TestData))]
         public void Properties_SetValues_ReturnsExpected(int id, int len, short type, byte[] value)
