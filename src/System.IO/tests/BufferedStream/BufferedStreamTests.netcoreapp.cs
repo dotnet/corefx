3890541a510a42c171d947cdeebeb350387b52cc
Stephen Toub <stoub@microsoft.com>
2017-08-03 23:12:49 -0400

    Add Read/Write(Span) overrides to BufferedStream (#22948)
    Copy existing Read/Write(array) implementations and just tweak them to work for span.  I purposefully kept the same logic as was there.  I also didn't just make the array implementations work in terms of the span ones so that the array ones still result in calls to the underlying stream's array-based methods.

--- src/System.IO/tests/BufferedStream/BufferedStreamTests.netcoreapp.cs
+++ src/System.IO/tests/BufferedStream/BufferedStreamTests.netcoreapp.cs
@@ -23,5 +23,31 @@ namespace System.IO.Tests
             var bufferedStream = new BufferedStream(new MemoryStream(), 1234);
             Assert.Equal(1234, bufferedStream.BufferSize);
         }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(1, 2)]
+        [InlineData(1024, 4096)]
+        [InlineData(4096, 4097)]
+        [InlineData(4096, 1)]
+        [InlineData(2047, 4096)]
+        public void ReadSpan_WriteSpan_AllDataCopied(int spanSize, int bufferSize)
+        {
+            byte[] data = new byte[80000];
+            new Random(42).NextBytes(data);
+
+            var result = new MemoryStream();
+            using (var output = new BufferedStream(result, bufferSize))
+            using (var input = new BufferedStream(new MemoryStream(data), bufferSize))
+            {
+                Span<byte> span = new byte[spanSize];
+                int bytesRead;
+                while ((bytesRead = input.Read(span)) != 0)
+                {
+                    output.Write(span.Slice(0, bytesRead));
+                }
+            }
+            Assert.Equal(data, result.ToArray());
+        }
     }
 }
