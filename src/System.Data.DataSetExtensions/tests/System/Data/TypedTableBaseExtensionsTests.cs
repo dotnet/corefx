66b1de006c819ac65d360c387d82ca1991c93ff4
t-nichik <t-nichik@microsoft.com>
2017-08-07 16:41:53 -0700

    Add more tests, add XML to correct location
    XML will automatically compile when moved to right location that is in
    the project, not on local desktop.
    
    Add tests for TypedTableBaseExtensions and
    EnumerableRowCollectionExtensions

--- src/System.Data.DataSetExtensions/tests/System/Data/TypedTableBaseExtensionsTests.cs
+++ src/System.Data.DataSetExtensions/tests/System/Data/TypedTableBaseExtensionsTests.cs
@@ -1,3 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using Xunit;
 
 namespace System.Data.Tests
@@ -34,6 +38,48 @@ namespace System.Data.Tests
         public void Where_NullSource_ThrowsArgumentNullException()
         {
             AssertExtensions.Throws<ArgumentNullException>("source", () => TypedTableBaseExtensions.Where<DataRow>(null, row => true));
+        }
+
+        public class TestTypedTable<T> : TypedTableBase<T> where T : DataRow
+        {
+            public TestTypedTable() : base() { }
+        }
+
+        [Fact]
+        public void ElementAtOrDefault_ValidIndex()
+        {
+            TypedTableBase<DataRow> table = new TestTypedTable<DataRow>();
+            table.Columns.Add();
+            DataRow zero = table.Rows.Add(0);
+
+            Assert.Same(zero, table.ElementAtOrDefault(0));
+        }
+
+        [Fact]
+        public void ElementAtOrDefault_InvalidIndex()
+        {
+            TypedTableBase<DataRow> table = new TestTypedTable<DataRow>();
+            table.Columns.Add();
+            DataRow zero = table.Rows.Add(0);
+
+            Assert.Same(default(DataRow), table.ElementAtOrDefault(1));
+        } 
+
+        [Fact]
+        public void Select_ToListOfInts()
+        {
+            TypedTableBase<DataRow> table = new TestTypedTable<DataRow>();
+            table.Columns.Add();
+            table.Rows.Add(10);
+            table.Rows.Add(5);
+
+            var chosen = table.Select(row => int.Parse((string)row.ItemArray[0]));
+            int total = 0;
+            foreach (int num in chosen)
+            {
+                total += num;
+            }
+            Assert.Equal(15, total);
         }
     }
 }
