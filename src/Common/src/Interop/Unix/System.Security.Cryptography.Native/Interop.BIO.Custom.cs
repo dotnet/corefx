3ec53c69cf37855956f21a7a3a567a874dd1ee8d
Drawaes <seawardtim@gmail.com>
2017-08-08 02:55:31 +0100

    React to shim changes in the managed layer

--- src/Common/src/Interop/Unix/System.Security.Cryptography.Native/Interop.BIO.Custom.cs
+++ src/Common/src/Interop/Unix/System.Security.Cryptography.Native/Interop.BIO.Custom.cs
@@ -7,15 +7,11 @@ internal static partial class Interop
 {
     internal static class CustomBio
     {
-        private static IntPtr s_customBioMethodStructure;
-        private static CreateDelegate s_createDelegate;
-        private static DestroyDelegate s_destroyDelegate;
-        private unsafe static ControlDelegate s_controlDelegate;
         private unsafe static ReadDelegate s_readDelegate;
         private unsafe static WriteDelegate s_writeDelegate;
 
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private unsafe delegate int CreateDelegate(bio_st* bio);
+        [DllImport(Libraries.CryptoNative, EntryPoint = "CryptoNative_InitCustomBioMethod")]
+        private static extern void InitCustomBioMethod(WriteDelegate bwrite, ReadDelegate bread);
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         private unsafe delegate int ReadDelegate(IntPtr bio, void* buf, int size);
@@ -23,40 +19,14 @@ internal static partial class Interop
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         private unsafe delegate int WriteDelegate(IntPtr bio, void* buf, int num);
 
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private unsafe delegate long ControlDelegate(IntPtr bio, Crypto.BIO_CTRL cmd, long num, void* ptr);
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate int DestroyDelegate(IntPtr bio);
-
-        static CustomBio()
-        {
-            CustomBio.Initialize();
-        }
+        static CustomBio() => Initialize();
 
         internal unsafe static void Initialize()
         {
-            s_createDelegate = Create;
-            s_controlDelegate = Control;
-            s_destroyDelegate = Destroy;
             s_writeDelegate = Write;
             s_readDelegate = Read;
 
-            IntPtr name = Marshal.StringToHGlobalAnsi("Managed Bio");
-            var bioStruct = new bio_method_st()
-            {
-                create = s_createDelegate,
-                name = name,
-                type = BIO_TYPE.BIO_TYPE_SOURCE_SINK,
-                destroy = s_destroyDelegate,
-                ctrl = s_controlDelegate,
-                bread = s_readDelegate,
-                bwrite = s_writeDelegate,
-            };
-
-            IntPtr memory = Marshal.AllocHGlobal(Marshal.SizeOf<bio_method_st>());
-            Marshal.StructureToPtr(bioStruct, memory, true);
-            s_customBioMethodStructure = memory;
+            InitCustomBioMethod(s_writeDelegate, s_readDelegate);
         }
 
         internal static GCHandle BioGetGCHandle(IntPtr bio)
@@ -76,31 +46,7 @@ internal static partial class Interop
             Crypto.BioSetAppData(bio, ptr);
         }
 
-        internal static SafeBioHandle CreateCustomBio()
-        {
-            SafeBioHandle bio = Crypto.CreateCustomBio(s_customBioMethodStructure);
-            return bio;
-        }
-
-        private unsafe static int Create(bio_st* bio)
-        {
-            bio[0].init = 1;
-            return 1;
-        }
-
-        private static int Destroy(IntPtr bio) => 1;
-
-        private static unsafe long Control(IntPtr bio, Crypto.BIO_CTRL cmd, long param, void* ptr)
-        {
-            switch (cmd)
-            {
-                case Crypto.BIO_CTRL.BIO_CTRL_FLUSH:
-                case Crypto.BIO_CTRL.BIO_CTRL_POP:
-                case Crypto.BIO_CTRL.BIO_CTRL_PUSH:
-                    return 1;
-            }
-            return 0;
-        }
+        internal static SafeBioHandle CreateCustomBio() => Crypto.CreateCustomBio();
 
         private static unsafe int Write(IntPtr bio, void* input, int size)
         {
@@ -123,41 +69,5 @@ internal static partial class Interop
             }
             return -1;
         }
-
-        [Flags]
-        private enum BIO_TYPE
-        {
-            BIO_TYPE_SOURCE_SINK = 0x0400,
-        }
-                
-        [StructLayout(LayoutKind.Sequential)]
-        private unsafe struct bio_st
-        {
-            public void* method;
-            public void* callback;
-            public void* cb_arg;
-            public int init;
-            public int shutdown;
-            public int flags;
-            public int retry_reason;
-            public int num;
-            public void* ptr;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private unsafe struct bio_method_st
-        {
-            public BIO_TYPE type;
-            public IntPtr name;
-            public WriteDelegate bwrite; // int (* bwrite) (BIO*, const char*, int);
-            public ReadDelegate bread; // int (* bread) (BIO*, char*, int);
-            public IntPtr bputs; // int (* bputs) (BIO*, const char*);
-            public IntPtr bgets; // int (* bgets) (BIO*, char*, int);
-            public ControlDelegate ctrl; // long (* ctrl) (BIO*, int, long, void*);
-            public CreateDelegate create; // int (* create) (BIO*);
-            public DestroyDelegate destroy; // int (* destroy) (BIO*);
-            public IntPtr callback_ctrl; // long (* callback_ctrl) (BIO*, int, bio_info_cb*);
-        }
-
     }
 }
--- src/Common/src/Interop/Unix/System.Security.Cryptography.Native/Interop.BIO.cs
+++ src/Common/src/Interop/Unix/System.Security.Cryptography.Native/Interop.BIO.cs
@@ -37,7 +37,7 @@ internal static partial class Interop
         internal static extern int BioCtrlPending(SafeBioHandle bio);
 
         [DllImport(Libraries.CryptoNative, EntryPoint = "CryptoNative_CreateCustomBio")]
-        internal static extern SafeBioHandle CreateCustomBio(IntPtr bioMethodStruct);
+        internal static extern SafeBioHandle CreateCustomBio();
 
         [DllImport(Libraries.CryptoNative, EntryPoint = "CryptoNative_BioSetAppData")]
         internal static extern void BioSetAppData(SafeBioHandle bio, IntPtr data);
@@ -50,22 +50,5 @@ internal static partial class Interop
 
         [DllImport(Libraries.CryptoNative, EntryPoint = "CryptoNative_BioSetShoudRetryReadFlag")]
         internal static extern void BioSetShoudRetryReadFlag(SafeBioHandle bio);
-        
-        internal enum BIO_CTRL
-        {
-            BIO_CTRL_RESET = 1,/* opt - rewind/zero etc */
-            BIO_CTRL_EOF = 2,/* opt - are we at the eof */
-            BIO_CTRL_INFO = 3,/* opt - extra tit-bits */
-            BIO_CTRL_SET = 4,/* man - set the 'IO' type */
-            BIO_CTRL_GET = 5,/* man - get the 'IO' type */
-            BIO_CTRL_PUSH = 6,/* opt - internal, used to signify change */
-            BIO_CTRL_POP = 7,/* opt - internal, used to signify change */
-            BIO_CTRL_GET_CLOSE = 8,/* man - set the 'close' on free */
-            BIO_CTRL_SET_CLOSE = 9,/* man - set the 'close' on free */
-            BIO_CTRL_PENDING = 10,/* opt - is their more data buffered */
-            BIO_CTRL_FLUSH = 11,/* opt - 'flush' buffered output */
-            BIO_CTRL_DUP = 12,/* man - extra stuff for 'duped' BIO */
-            BIO_CTRL_WPENDING = 13,/* opt - number of bytes still to write */
-        }
     }
 }
