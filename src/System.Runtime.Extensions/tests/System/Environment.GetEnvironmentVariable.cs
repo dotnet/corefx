81f162c5fd13aa058ef8328c2462b4f2d0ed7e5e
Jiayi Li <v-lijiay@microsoft.com>
2017-08-09 09:41:02 +0800

    Update Environment.GetEnvironmentVariable.cs
    Change to ActiveIssue.

--- src/System.Runtime.Extensions/tests/System/Environment.GetEnvironmentVariable.cs
+++ src/System.Runtime.Extensions/tests/System/Environment.GetEnvironmentVariable.cs
@@ -170,7 +170,7 @@ namespace System.Tests
         [Theory]
         [InlineData(null)]
         [MemberData(nameof(EnvironmentTests.EnvironmentVariableTargets), MemberType = typeof(EnvironmentTests))]
-        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework, "https://github.com/dotnet/corefx/issues/23003")]
+        [ActiveIssue("https://github.com/dotnet/corefx/issues/23003", TargetFrameworkMonikers.NetFramework)]
         public void GetEnumerator_LinqOverDictionaryEntries_Success(EnvironmentVariableTarget? target)
         {
             IDictionary envVars = target != null ?
