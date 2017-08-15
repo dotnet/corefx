d773c96d39af052658e740c4258c848b00d8d7d4
Paulo Janotti <pauloja@microsoft.com>
2017-08-15 11:18:00 -0700

    Add support for serial port to UWP
    Change in corefx: UWP support for serial port (#22887)
    
    * [WIP] add serial port to UWP
    
    update build and test tools
    
    * OpenCommPort signature fixes
    
    * [WIP] review feedback
    
    * [WIP] add missing files
    
    * fix packaging issue
    
    * [WIP] replaced ThreadPool.BindHandle
    
    Replaced calls to ThreadPool.BindHandle w/
    ThreadPoolBoundHandle.BindHandle
    
    This is still working in progress due to issues in tests preventing full
    code coverage (same issues happening without the current changes).
    
    minor self-review
    
    * CR feedback
    
    * [WIP] System.IO.Ports NullModem running all tests
    
    * Finish UWP work to enable serial ports
    
    * Revert "[WIP] System.IO.Ports NullModem running all tests"
    
    This reverts commit a97edbaba463feb1871100cdcafc5f9d5f79663c.
    
    * add comment for conditional fact
    
    * changes for AbortOnError

---
+++ src/System.IO.Ports/src/System/IO/Ports/SerialStream.Win32.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.Win32.SafeHandles;
+
+namespace System.IO.Ports
+{
+    internal sealed partial class SerialStream
+    {
+        public SafeFileHandle OpenPort(uint portNumber)
+        {
+            return Interop.Kernel32.CreateFileDefaultSecurity(
+                @"\\?\COM" + portNumber,
+                Interop.Kernel32.GenericOperations.GENERIC_READ | Interop.Kernel32.GenericOperations.GENERIC_WRITE,
+                0,              // comm devices must be opened w/exclusive-access
+                FileMode.Open,  // comm devices must use OPEN_EXISTING
+                NativeMethods.FILE_FLAG_OVERLAPPED);
+        }
+    }
+}
