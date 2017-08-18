7e5663cc33c614bafd0b8578bc47efef9ed9c1db
Huangli Wu <huanwu@microsoft.com>
2017-08-18 10:32:37 -0700

    Add brackets.

--- src/Microsoft.XmlSerializer.Generator/src/Microsoft/XmlSerializer/Generator/XmlSerializationWriter.cs
+++ src/Microsoft.XmlSerializer.Generator/src/Microsoft/XmlSerializer/Generator/XmlSerializationWriter.cs
@@ -2044,6 +2044,7 @@ namespace Microsoft.XmlSerializer.Generator
                 else if (type == typeof(Int32))
                     Writer.Write(((Int32)value).ToString(null, NumberFormatInfo.InvariantInfo));
                 else if (type == typeof(Double))
+                {
                     if (double.IsNaN((Double)value))
                     {
                         Writer.Write("double.NaN");
@@ -2052,6 +2053,7 @@ namespace Microsoft.XmlSerializer.Generator
                     {
                         Writer.Write(((Double)value).ToString("R", NumberFormatInfo.InvariantInfo));
                     }
+                }
                 else if (type == typeof(Boolean))
                     Writer.Write((bool)value ? "true" : "false");
                 else if ((type == typeof(Int16)) || (type == typeof(Int64)) || (type == typeof(UInt16)) || (type == typeof(UInt32)) || (type == typeof(UInt64)) || (type == typeof(Byte)) || (type == typeof(SByte)))
@@ -2064,6 +2066,7 @@ namespace Microsoft.XmlSerializer.Generator
                     Writer.Write(")");
                 }
                 else if (type == typeof(Single))
+                {
                     if (Single.IsNaN((Single)value))
                     {
                         Writer.Write("System.Single.NaN");
@@ -2073,6 +2076,7 @@ namespace Microsoft.XmlSerializer.Generator
                         Writer.Write(((Single)value).ToString("R", NumberFormatInfo.InvariantInfo));
                         Writer.Write("f");
                     }
+                }
                 else if (type == typeof(Decimal))
                 {
                     Writer.Write(((Decimal)value).ToString(null, NumberFormatInfo.InvariantInfo));
