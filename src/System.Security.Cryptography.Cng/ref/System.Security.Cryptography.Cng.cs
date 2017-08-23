d6c3a902240c659e3e97267a10e476be3f17b174
Eric St. John <ericstj@microsoft.com>
2017-08-23 14:51:05 -0700

    Make netfx mean net471
    This maps netfx to net471 and removes the netfx support package and corresponding
    configurations from all projects.
    
    In the cases of packages that still support netfx I only kept a live configuration in the package
    if that package had type-defs.  Otherwise I just harvested the past assets.

--- src/System.Security.Cryptography.Cng/ref/System.Security.Cryptography.Cng.cs
+++ src/System.Security.Cryptography.Cng/ref/System.Security.Cryptography.Cng.cs
@@ -278,25 +278,19 @@ namespace System.Security.Cryptography
         public ECDsaCng() { }
         public ECDsaCng(int keySize) { }
         public ECDsaCng(System.Security.Cryptography.CngKey key) { }
-#if FEATURE_ECPARAMETERS // types missing from netfx and net462 targeting pack
         public ECDsaCng(System.Security.Cryptography.ECCurve curve) { }
-#endif
         public CngAlgorithm HashAlgorithm { get { throw null; } set { } }
         public System.Security.Cryptography.CngKey Key { get { throw null; } }
         public override int KeySize { get { throw null; } set { } }
         public override System.Security.Cryptography.KeySizes[] LegalKeySizes { get { throw null; } }
         protected override void Dispose(bool disposing) { }
-#if FEATURE_ECPARAMETERS // types missing from netfx and net462 targeting pack
         public override System.Security.Cryptography.ECParameters ExportExplicitParameters(bool includePrivateParameters) { throw null; }
         public override System.Security.Cryptography.ECParameters ExportParameters(bool includePrivateParameters) { throw null; }
         public override void GenerateKey(System.Security.Cryptography.ECCurve curve) { }
-#endif
         public void FromXmlString(string xml, ECKeyXmlFormat format) { throw null; }
         protected override byte[] HashData(byte[] data, int offset, int count, System.Security.Cryptography.HashAlgorithmName hashAlgorithm) { throw null; }
         protected override byte[] HashData(System.IO.Stream data, System.Security.Cryptography.HashAlgorithmName hashAlgorithm) { throw null; }
-#if FEATURE_ECPARAMETERS // types missing from netfx and net462 targeting pack
         public override void ImportParameters(System.Security.Cryptography.ECParameters parameters) { }
-#endif
         public byte[] SignData(byte[] data) { throw null; }
         public byte[] SignData(byte[] data, int offset, int count) { throw null; }
         public byte[] SignData(System.IO.Stream data) { throw null; }
