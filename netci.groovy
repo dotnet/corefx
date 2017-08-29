c7ec0ce58957bc4d5c33d3086992da96ea6019cd
Matt Mitchell <mmitche@microsoft.com>
2017-08-29 14:43:34 -0700

    Eliminate OpenSuSE 13.2 (EOL) (#23624)

--- netci.groovy
+++ netci.groovy
@@ -22,7 +22,6 @@ def osGroupMap = ['Windows 7':'Windows_NT',
                   'Fedora24':'Linux',
                   'OSX10.12':'OSX',
                   'CentOS7.1': 'Linux',
-                  'OpenSUSE13.2': 'Linux',
                   'OpenSUSE42.1': 'Linux',
                   'RHEL7.2': 'Linux',
                   'PortableLinux': 'Linux']
@@ -36,7 +35,6 @@ def osShortName = ['Windows 7' : 'win7',
                    'Fedora24' : 'fedora24',
                    'OSX10.12' : 'osx',
                    'CentOS7.1' : 'centos7.1',
-                   'OpenSUSE13.2' : 'opensuse13.2',
                    'OpenSUSE42.1' : 'opensuse42.1',
                    'RHEL7.2' : 'rhel7.2',
                    'PortableLinux' : 'portablelinux']
@@ -44,10 +42,10 @@ def osShortName = ['Windows 7' : 'win7',
 def buildArchConfiguration = ['Debug': 'x86',
                               'Release': 'x64']
 
-def targetGroupOsMapOuterloop = ['netcoreapp': ['Windows 7', 'Windows_NT', 'Ubuntu14.04', 'Ubuntu16.04', 'Ubuntu16.10', 'CentOS7.1', 'OpenSUSE13.2', 'OpenSUSE42.1',
+def targetGroupOsMapOuterloop = ['netcoreapp': ['Windows 7', 'Windows_NT', 'Ubuntu14.04', 'Ubuntu16.04', 'Ubuntu16.10', 'CentOS7.1', 'OpenSUSE42.1',
                                         'RHEL7.2', 'Fedora24', 'Debian8.4', 'OSX10.12', 'PortableLinux']]
 
-def targetGroupOsMapInnerloop = ['netcoreapp': ['Windows_NT', 'Ubuntu14.04', 'Ubuntu16.04', 'Ubuntu16.10', 'CentOS7.1', 'OpenSUSE13.2', 'OpenSUSE42.1',
+def targetGroupOsMapInnerloop = ['netcoreapp': ['Windows_NT', 'Ubuntu14.04', 'Ubuntu16.04', 'Ubuntu16.10', 'CentOS7.1', 'OpenSUSE42.1',
                                         'RHEL7.2', 'Fedora24', 'Debian8.4', 'OSX10.12', 'PortableLinux']]
 
 // **************************
