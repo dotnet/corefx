6893fb2760a83d46c3319bd9e703fa48be54c113
Matt Mitchell <mmitche@microsoft.com>
2017-08-31 09:18:34 -0700

    [DO NOT MERGE] Remove retry to attempt to track down cloning hangs

--- buildpipeline/windows.groovy
+++ buildpipeline/windows.groovy
@@ -12,9 +12,7 @@ def submitToHelix = (params.TGroup == 'netcoreapp' || params.TGroup == 'uap')
 
 simpleNode('Windows_NT','latest') {
     stage ('Checkout source') {
-        retry (10) {
-            checkout scm
-        }
+        checkout scm
     }
 
     def logFolder = getLogFolder()
