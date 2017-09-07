54e89a60294963bab46656819c337596860a565a
Krzysztof Wicher <kwicher@microsoft.com>
2017-09-07 10:03:29 -0700

    add copyright header, simplify method syntax

--- src/Common/tests/System/Xml/ModuleCore/XunitRunner.cs
+++ src/Common/tests/System/Xml/ModuleCore/XunitRunner.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -65,10 +69,7 @@ namespace OLEDB.Test.ModuleCore
             return GenerateTestCases(moduleGenerator);
         }
 
-        public virtual bool SupportsDiscoveryEnumeration(IAttributeInfo dataAttribute, IMethodInfo testMethod)
-        {
-            return true;
-        }
+        public virtual bool SupportsDiscoveryEnumeration(IAttributeInfo dataAttribute, IMethodInfo testMethod) => true;
     }
 
     [DataDiscoverer("OLEDB.Test.ModuleCore.XmlInlineDataDiscoverer", "ModuleCore")]
