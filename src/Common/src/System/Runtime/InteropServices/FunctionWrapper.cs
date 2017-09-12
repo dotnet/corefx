a6dbbdb7663c132823191fe6aa118f5134235f73
Eric Mellino <erme@microsoft.com>
2017-09-11 17:37:20 -0700

    Better emulate PInvoke exceptions in FunctionWrapper.cs
    * Constructing a FunctionWrapper should no longer ever throw an exception.
    * Like PInvokes, calling the wrapped delegate may now throw a DllNotFoundException
      in addition to an EntryPointNotFoundException. These are now catchable at usage-time,
      as expected.

--- src/Common/src/System/Runtime/InteropServices/FunctionWrapper.cs
+++ src/Common/src/System/Runtime/InteropServices/FunctionWrapper.cs
@@ -2,44 +2,73 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics;
+
 namespace System.Runtime.InteropServices
 {
     /// <summary>
-    /// Utility class which lazily loads function pointers and throws an EntryPointNotFoundException at
-    /// usage-time if the function pointer can not be loaded.
+    /// Utility class which lazily loads function pointers and throws a DllNotFoundException or
+    /// an EntryPointNotFoundException at usage-time if the function pointer can not be loaded.
     /// </summary>
     /// <typeparam name="T">The type of the function to wrap.</typeparam>
     internal class FunctionWrapper<T> where T : class
     {
-        private Lazy<T> _lazyDelegate;
+        private Lazy<FunctionLoadResult<T>> _lazyDelegate;
 
-        public FunctionWrapper(Lazy<T> lazyDelegate)
+        public FunctionWrapper(Lazy<FunctionLoadResult<T>> lazyDelegate)
         {
             _lazyDelegate = lazyDelegate;
         }
 
-        public T Delegate => _lazyDelegate.Value ?? throw new EntryPointNotFoundException();
+        public T Delegate
+        {
+            get
+            {
+                FunctionLoadResult<T> loadResult = _lazyDelegate.Value;
+                switch (loadResult.ResultKind)
+                {
+                    case FunctionLoadResultKind.Success:
+                        return loadResult.Delegate;
+                    case FunctionLoadResultKind.LibraryNotFound:
+                        throw new DllNotFoundException();
+                    case FunctionLoadResultKind.FunctionNotFound:
+                        throw new EntryPointNotFoundException();
+                    default:
+                        Debug.Fail("Illegal FunctionLoadResultKind: " + loadResult.ResultKind);
+                        return null;
+                }
+            }
+        }
+    }
+
+    public enum FunctionLoadResultKind { Success, LibraryNotFound, FunctionNotFound }
+
+    public struct FunctionLoadResult<T>
+    {
+        public FunctionLoadResultKind ResultKind { get; }
+        public T Delegate { get; }
+        public FunctionLoadResult(FunctionLoadResultKind kind, T del) { ResultKind = kind; Delegate = del; }
     }
 
     internal static partial class FunctionWrapper
     {
         public static FunctionWrapper<T> Load<T>(IntPtr nativeLibraryHandle, string name) where T : class
         {
-            if (nativeLibraryHandle == IntPtr.Zero)
+            Lazy<FunctionLoadResult<T>> lazyDelegate = new Lazy<FunctionLoadResult<T>>(() =>
             {
-                throw new DllNotFoundException();
-            }
+                if (nativeLibraryHandle == IntPtr.Zero)
+                {
+                    return new FunctionLoadResult<T>(FunctionLoadResultKind.LibraryNotFound, null);
+                }
 
-            Lazy<T> lazyDelegate = new Lazy<T>(() =>
-            {
                 IntPtr funcPtr = LoadFunctionPointer(nativeLibraryHandle, name);
                 if (funcPtr == IntPtr.Zero)
                 {
-                    return null;
+                    return new FunctionLoadResult<T>(FunctionLoadResultKind.FunctionNotFound, null);
                 }
                 else
                 {
-                    return Marshal.GetDelegateForFunctionPointer<T>(funcPtr);
+                    return new FunctionLoadResult<T>(FunctionLoadResultKind.Success, Marshal.GetDelegateForFunctionPointer<T>(funcPtr));
                 }
             });
 
