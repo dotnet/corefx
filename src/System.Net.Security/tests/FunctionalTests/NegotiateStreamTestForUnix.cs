1c1a452eb3031ae5a5db9196e8a1794f290ec2ec
Dan Moseley <danmose@microsoft.com>
2017-09-16 14:53:38 -0700

    oversight

--- src/System.Net.Security/tests/FunctionalTests/NegotiateStreamTestForUnix.cs
+++ src/System.Net.Security/tests/FunctionalTests/NegotiateStreamTestForUnix.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Test.Common;
+using System.Runtime.InteropServices;
 using System.Security.Authentication;
 using System.Security.Principal;
 using System.Text;
