878731debbf32bb4565cf7c4f42ecd63ddad9862
ahsonkhan <ahson_ahmedk@yahoo.com>
2017-09-22 20:27:51 -0700

    Removing unnecessary using directives.

--- src/System.Memory/src/System/MemoryDebugView.cs
+++ src/System.Memory/src/System/MemoryDebugView.cs
@@ -3,8 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Diagnostics;
-using System.Reflection;
-using System.Runtime.CompilerServices;
 
 namespace System
 {
