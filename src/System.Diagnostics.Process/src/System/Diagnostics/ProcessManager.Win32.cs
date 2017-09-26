1d1d90b840b39390fd08fc08aef3913286e460ab
dnickless <daniel.hegener@gmx.net>
2017-09-26 17:23:36 -0400

    Revert "Attempt to make the UAP build happy again"
    This reverts commit 9ca6a02f2d2886ed9acc7e9643307e0ba0ca94b8.

--- src/System.Diagnostics.Process/src/System/Diagnostics/ProcessManager.Win32.cs
+++ src/System.Diagnostics.Process/src/System/Diagnostics/ProcessManager.Win32.cs
@@ -253,7 +253,7 @@ namespace System.Diagnostics
         // Cache a single buffer for use in GetProcessInfos().
         private static long[] CachedBuffer;
 
-        public static ProcessInfo[] GetProcessInfos(int processId = AllProcessIds)
+        internal static ProcessInfo[] GetProcessInfos(int processId = AllProcessIds)
         {
             int requiredSize = 0;
             int status;
