d5c8751a02314b4efcb7e4215f40ba38cae22398
Michelle McDaniel <adiaaida@gmail.com>
2017-10-03 10:39:08 -0700

    Add tests, update ref, update guid

---
+++ src/System.Diagnostics.PerformanceCounter/tests/PerformanceCounterTests.cs
@@ -0,0 +1,54 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections;
+using System.Collections.Specialized;
+using Xunit;
+
+namespace System.Diagnostics.Tests
+{
+    public static class PerformanceCounterTests
+    {
+        [Fact]
+        public static void SetupCategory()
+        {
+            if ( !PerformanceCounterCategory.Exists("AverageCounter64SampleCategory") ) 
+            {
+
+                CounterCreationDataCollection counterDataCollection = new CounterCreationDataCollection();
+
+                // Add the counter.
+                CounterCreationData averageCount64 = new CounterCreationData();
+                averageCount64.CounterType = PerformanceCounterType.AverageCount64;
+                averageCount64.CounterName = "AverageCounter64Sample";
+                counterDataCollection.Add(averageCount64);
+
+                // Add the base counter.
+                CounterCreationData averageCount64Base = new CounterCreationData();
+                averageCount64Base.CounterType = PerformanceCounterType.AverageBase;
+                averageCount64Base.CounterName = "AverageCounter64SampleBase";
+                counterDataCollection.Add(averageCount64Base);
+
+                // Create the category.
+                PerformanceCounterCategory.Create("AverageCounter64SampleCategory",
+                    "Demonstrates usage of the AverageCounter64 performance counter type.",
+                    PerformanceCounterCategoryType.SingleInstance, counterDataCollection);
+            }
+
+            Assert.True(PerformanceCounterCategory.Exists("AverageCounter64SampleCategory"));
+        }
+
+        [Fact]
+        public static void CreateCounters()
+        {
+            PerformanceCounter averageCounter64Sample = new PerformanceCounter("AverageCounter64SampleCategory", 
+                "AverageCounter64Sample", false);
+
+            averageCounter64Sample.RawValue = 0;
+
+            Assert.Equal(0, averageCounter64Sample.RawValue);
+        }
+    }
+}
