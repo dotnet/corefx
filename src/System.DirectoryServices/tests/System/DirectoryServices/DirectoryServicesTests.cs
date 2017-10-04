453ac241dcb8df878cf8e9968cf4ec42a83ac496
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2017-10-04 14:15:08 -0700

    More DirectoryServices tests (#24388)
    * More DirectoryServices tests
    
    * Minor fix

--- src/System.DirectoryServices/tests/System/DirectoryServices/DirectoryServicesTests.cs
+++ src/System.DirectoryServices/tests/System/DirectoryServices/DirectoryServicesTests.cs
@@ -16,10 +16,7 @@ namespace System.DirectoryServices.Tests
         [ConditionalFact(nameof(IsLdapConfigurationExist))]
         public void TestOU() // adding and removing organization unit
         {
-            using (DirectoryEntry de = new DirectoryEntry(LdapConfiguration.Configuration.LdapPath,
-                                                            LdapConfiguration.Configuration.UserName,
-                                                            LdapConfiguration.Configuration.Password,
-                                                            LdapConfiguration.Configuration.AuthenticationTypes))
+            using (DirectoryEntry de = CreateRootEntry())
             {
                 string ouName = "NetCoreDevs";
                 
@@ -42,10 +39,7 @@ namespace System.DirectoryServices.Tests
         [ConditionalFact(nameof(IsLdapConfigurationExist))]
         public void TestOrganizationalRole() // adding and removing users to/from the ou
         {
-            using (DirectoryEntry de = new DirectoryEntry(LdapConfiguration.Configuration.LdapPath,
-                                                            LdapConfiguration.Configuration.UserName,
-                                                            LdapConfiguration.Configuration.Password,
-                                                            LdapConfiguration.Configuration.AuthenticationTypes))
+            using (DirectoryEntry de = CreateRootEntry())
             {
                 DeleteOU(de, "CoreFxRootOU");
 
@@ -56,8 +50,8 @@ namespace System.DirectoryServices.Tests
                         DirectoryEntry child1OU = CreateOU(rootOU, "CoreFxChild1OU", "CoreFx Child OU 1");
                         DirectoryEntry child2OU = CreateOU(rootOU, "CoreFxChild2OU", "CoreFx Child OU 2");
 
-                        CreateOrganizationalRole(child1OU, "user.ou1.1", "User 1 is in CoreFx ou 1", "1 111 111 11111");
-                        CreateOrganizationalRole(child1OU, "user.ou1.2", "User 2 is in CoreFx ou 1", "1 222 222 22222");
+                        CreateOrganizationalRole(child1OU, "user.ou1.1", "User 1 is in CoreFx ou 1", "1 111 111 1111");
+                        CreateOrganizationalRole(child1OU, "user.ou1.2", "User 2 is in CoreFx ou 1", "1 222 222 2222");
 
                         CreateOrganizationalRole(child2OU, "user.ou2.1", "User 1 is in CoreFx ou 2", "1 333 333 3333");
                         CreateOrganizationalRole(child2OU, "user.ou2.2", "User 2 is in CoreFx ou 2", "1 333 333 3333");
@@ -77,6 +71,234 @@ namespace System.DirectoryServices.Tests
                         // rootOU.DeleteTree(); doesn't work as getting "A protocol error occurred. (Exception from HRESULT: 0x80072021)"
                         DeleteOU(de, "CoreFxRootOU");
                     }
+                }
+            }
+        }
+
+        [ConditionalFact(nameof(IsLdapConfigurationExist))]
+        public void TestPropertyCaching() 
+        {
+            using (DirectoryEntry de = CreateRootEntry())
+            {
+                DeleteOU(de, "CachingOU");
+
+                using (DirectoryEntry rootOU = CreateOU(de, "CachingOU", "Caching OU"))
+                {
+                    try
+                    {
+                        using (DirectoryEntry userEntry = CreateOrganizationalRole(rootOU, "caching.user.1", "User 1 in caching OU", "1 111 111 1111"))
+                        {
+                            Assert.True(userEntry.UsePropertyCache);
+                            
+                            SearchOrganizationalRole(rootOU, "caching.user.1");
+                            string originalPhone = (string) userEntry.Properties["telephoneNumber"].Value;
+                            string newPhone = "1 222 222 2222";
+                            userEntry.Properties["telephoneNumber"].Value = newPhone;
+
+                            using (DirectoryEntry sameUserEntry = GetOrganizationalRole(rootOU, "caching.user.1"))
+                            {
+                                Assert.Equal(originalPhone, (string) sameUserEntry.Properties["telephoneNumber"].Value);
+                            }
+
+			                userEntry.CommitChanges();
+
+                            using (DirectoryEntry sameUserEntry = GetOrganizationalRole(rootOU, "caching.user.1"))
+                            {
+                                Assert.Equal(newPhone, (string) sameUserEntry.Properties["telephoneNumber"].Value);
+                            }
+
+			                userEntry.UsePropertyCache = false;
+                            Assert.False(userEntry.UsePropertyCache);
+
+                            userEntry.Properties["telephoneNumber"].Value = originalPhone;
+                            using (DirectoryEntry sameUserEntry = GetOrganizationalRole(rootOU, "caching.user.1"))
+                            {
+                                Assert.Equal(originalPhone, (string) sameUserEntry.Properties["telephoneNumber"].Value);
+                            }
+                        }
+                    }
+                    finally
+                    {
+                        DeleteOU(de, "CachingOU");
+                    }
+                }
+            }
+        }
+
+        [ConditionalFact(nameof(IsLdapConfigurationExist))]
+        public void TestMoveTo() 
+        {
+            using (DirectoryEntry de = CreateRootEntry())
+            {
+                DeleteOU(de, "MoveingOU");
+                
+                try
+                {
+                    using (DirectoryEntry rootOU = CreateOU(de, "MoveingOU", "Moving Root OU"))
+                    {
+                        using (DirectoryEntry fromOU = CreateOU(rootOU, "MoveFromOU", "Moving From OU"))
+                        using (DirectoryEntry toOU   = CreateOU(rootOU, "MoveToOU", "Moving To OU"))
+                        {
+                            DirectoryEntry user = CreateOrganizationalRole(fromOU, "user.move.1", "User to move across OU's", "1 111 111 1111");
+                            SearchOrganizationalRole(fromOU, "user.move.1");
+
+                            user.MoveTo(toOU);
+                            user.CommitChanges();
+
+                            SearchOrganizationalRole(toOU, "user.move.1");
+                            Assert.Throws<DirectoryServicesCOMException>(() => SearchOrganizationalRole(fromOU, "user.move.1"));
+
+                            user.MoveTo(fromOU, "cn=user.moved.1");
+                            user.CommitChanges();
+
+                            SearchOrganizationalRole(fromOU, "user.moved.1");
+
+                            Assert.Throws<DirectoryServicesCOMException>(() => SearchOrganizationalRole(fromOU, "user.move.1"));
+                            Assert.Throws<DirectoryServicesCOMException>(() => SearchOrganizationalRole(toOU, "user.move.1"));
+                            Assert.Throws<DirectoryServicesCOMException>(() => user.MoveTo(toOU, "user.move.2"));
+                        }
+                    }
+                }
+                finally
+                {
+                    DeleteOU(de, "MoveingOU");
+                }
+            }
+        }
+
+        [ConditionalFact(nameof(IsLdapConfigurationExist))]
+        public void TestCopyTo()
+        {
+            using (DirectoryEntry de = CreateRootEntry())
+            {
+                DeleteOU(de, "CopyingOU");
+                
+                try
+                {
+                    using (DirectoryEntry rootOU = CreateOU(de, "CopyingOU", "Copying Root OU"))
+                    {
+                        using (DirectoryEntry fromOU = CreateOU(rootOU, "CopyFromOU", "Copying From OU"))
+                        using (DirectoryEntry toOU   = CreateOU(rootOU, "CopyToOU", "Copying To OU"))
+                        {
+                            DirectoryEntry user = CreateOrganizationalRole(fromOU, "user.move.1", "User to copy across OU's", "1 111 111 1111");
+                            SearchOrganizationalRole(fromOU, "user.move.1");
+
+                            // The Lightweight Directory Access Protocol (LDAP) provider does not currently support the CopyTo(DirectoryEntry) method.
+                            Assert.Throws<NotImplementedException>(() => user.CopyTo(toOU));
+                            Assert.Throws<NotImplementedException>(() => user.CopyTo(toOU, "user.move.2"));
+                        }
+                    }
+                }
+                finally
+                {
+                    DeleteOU(de, "CopyingOU");
+                }
+            }
+        }
+
+        [ConditionalFact(nameof(IsLdapConfigurationExist))]
+        public void TestRename() 
+        {
+            using (DirectoryEntry de = CreateRootEntry())
+            {
+                DeleteOU(de, "RenamingOU");
+                
+                try
+                {
+                    using (DirectoryEntry rootOU = CreateOU(de, "RenamingOU", "Renaming Root OU"))
+                    {
+                        DirectoryEntry user = CreateOrganizationalRole(rootOU, "user.before.rename", "User to rename", "1 111 111 1111");
+                        SearchOrganizationalRole(rootOU, "user.before.rename");
+
+
+                        user.Rename("cn=user.after.rename");
+                        user.CommitChanges();
+
+                        Assert.Throws<DirectoryServicesCOMException>(() => SearchOrganizationalRole(rootOU, "user.before.rename"));
+
+                        SearchOrganizationalRole(rootOU, "user.after.rename");
+
+                        Assert.Throws<DirectoryServicesCOMException>(() => user.Rename("user.after.after.rename"));
+                    }
+                }
+                finally
+                {
+                    DeleteOU(de, "RenamingOU");
+                }
+            }
+        }
+
+        [ConditionalFact(nameof(IsLdapConfigurationExist))]
+        public void TestParent()
+        {
+            using (DirectoryEntry de = CreateRootEntry())
+            {
+                DeleteOU(de, "GrandParent");
+                
+                try
+                {
+                    using (DirectoryEntry grandOU = CreateOU(de, "GrandParent", "Grand Parent OU"))
+                    using (DirectoryEntry parentOU = CreateOU(grandOU, "Parent", "Parent OU"))
+                    using (DirectoryEntry childOU = CreateOU(parentOU, "Child", "Child OU"))
+                    {
+                        SearchOUByName(de, "GrandParent");
+                        SearchOUByName(grandOU, "Parent");
+                        SearchOUByName(parentOU, "Child");
+
+                        Assert.Equal(de.Name, grandOU.Parent.Name);
+                        Assert.Equal(grandOU.Name, parentOU.Parent.Name);
+                        Assert.Equal(parentOU.Name, childOU.Parent.Name);
+                    }
+                }
+                finally
+                {
+                    DeleteOU(de, "GrandParent");
+                }
+            }
+        }
+        
+        [ConditionalFact(nameof(IsLdapConfigurationExist))]
+        public void TestDeleteTree()
+        {
+            using (DirectoryEntry de = CreateRootEntry())
+            {
+                DeleteOU(de, "RootToDelete");
+                
+                try
+                {
+                    using (DirectoryEntry rootOU = CreateOU(de, "RootToDelete", "Root OU"))
+                    using (DirectoryEntry childOU = CreateOU(rootOU, "Child1", "Root Child 1 OU"))
+                    using (DirectoryEntry anotherChildOU = CreateOU(rootOU, "Child2", "Root Child 2 OU"))
+                    using (DirectoryEntry grandChildOU = CreateOU(childOU, "GrandChild", "Grand Child OU"))
+                    using (DirectoryEntry user1 = CreateOrganizationalRole(grandChildOU, "user.grandChild.1", "Grand Child User", "1 111 111 1111"))
+                    using (DirectoryEntry user2 = CreateOrganizationalRole(grandChildOU, "user.grandChild.2", "Grand Child User", "1 222 222 2222"))
+                    {
+                        SearchOUByName(de, "RootToDelete");
+                        SearchOUByName(rootOU, "Child1");
+                        SearchOUByName(rootOU, "Child2");
+                        SearchOUByName(childOU, "GrandChild");
+                        SearchOrganizationalRole(grandChildOU, "user.grandChild.1");
+
+                        if (LdapConfiguration.Configuration.IsActiveDirectoryServer)
+                        {
+                            rootOU.DeleteTree();
+
+                            Assert.Throws<DirectoryServicesCOMException>(() => SearchOUByName(de, "RootToDelete"));
+                            Assert.Throws<DirectoryServicesCOMException>(() => SearchOUByName(rootOU, "Child1"));
+                            Assert.Throws<DirectoryServicesCOMException>(() => SearchOUByName(rootOU, "Child2"));
+                            Assert.Throws<DirectoryServicesCOMException>(() => SearchOUByName(childOU, "GrandChild"));
+                            Assert.Throws<DirectoryServicesCOMException>(() => SearchOrganizationalRole(grandChildOU, "user.grandChild.1"));
+                        }
+                        else
+                        {
+                            // on non Active Directory Servers, DeleteTree() throws DirectoryServicesCOMException : A protocol error occurred. (Exception from HRESULT: 0x80072021)
+                            Assert.Throws<DirectoryServicesCOMException>(() => rootOU.DeleteTree());
+                        }
+                    }
+                }
+                finally
+                {
+                    DeleteOU(de, "RootToDelete");
                 }
             }
         }
@@ -137,6 +359,10 @@ namespace System.DirectoryServices.Tests
                 ds.Filter = $"(&(objectClass=organizationalUnit)(ou={ouName}))";
                 ds.PropertiesToLoad.Add("ou");
                 SearchResult sr = ds.FindOne();
+                if (sr == null)
+                {
+                    throw new DirectoryServicesCOMException($"Couldn't find {ouName} in the entry {de.Name}");
+                }
                 Assert.Equal(ouName, sr.Properties["ou"][0]);
             }
         }
@@ -145,11 +371,38 @@ namespace System.DirectoryServices.Tests
         {
             using (DirectorySearcher ds = new DirectorySearcher(de))
             {
+                ds.ClientTimeout = new TimeSpan(0, 2, 0);
                 ds.Filter = $"(&(objectClass=organizationalRole)(cn={cnName}))";
                 ds.PropertiesToLoad.Add("cn");
                 SearchResult sr = ds.FindOne();
+
+                if (sr == null)
+                {
+                    throw new DirectoryServicesCOMException($"Couldn't find {cnName} in the entry {de.Name}");
+                }
+
                 Assert.Equal(cnName, sr.Properties["cn"][0]);
             }
+        }
+
+        private DirectoryEntry GetOrganizationalRole(DirectoryEntry de, string cnName)
+        {
+            using (DirectorySearcher ds = new DirectorySearcher(de))
+            {
+                ds.ClientTimeout = new TimeSpan(0, 2, 0);
+                ds.Filter = $"(&(objectClass=organizationalRole)(cn={cnName}))";
+                ds.PropertiesToLoad.Add("cn");
+                SearchResult sr = ds.FindOne();
+                return sr.GetDirectoryEntry();
+            }
+        }
+
+        private DirectoryEntry CreateRootEntry()
+        {
+            return new DirectoryEntry(LdapConfiguration.Configuration.LdapPath,
+                                      LdapConfiguration.Configuration.UserName,
+                                      LdapConfiguration.Configuration.Password,
+                                      LdapConfiguration.Configuration.AuthenticationTypes);
         }
     }
 }
