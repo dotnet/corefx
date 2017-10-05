2c1c5b441d65bd100e80be85103ed218f9419aff
Anirudh Agnihotry <anagniho@microsoft.com>
2017-10-05 11:12:36 -0700

    Minor project revert changes

--- src/Common/src/Interop/Windows/kernel32/Interop.LoadLibraryEx.cs
+++ src/Common/src/Interop/Windows/kernel32/Interop.LoadLibraryEx.cs
@@ -4,13 +4,14 @@
 
 using System;
 using System.Runtime.InteropServices;
+
 using Microsoft.Win32.SafeHandles;
 
 internal partial class Interop
 {
     internal partial class Kernel32
     {
-    	public const int LOAD_LIBRARY_AS_DATAFILE = 0x00000002;
+        public const int LOAD_LIBRARY_AS_DATAFILE = 0x00000002;
 
         [DllImport(Libraries.Kernel32, ExactSpelling = true, CharSet = CharSet.Unicode, SetLastError = true)]
         public static extern SafeLibraryHandle LoadLibraryExW([In] string lpwLibFileName, [In] IntPtr hFile, [In] uint dwFlags);
