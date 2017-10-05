ca16a7304172810e1a01c2365ef1fb3b70af3df3
Anirudh Agnihotry <anagniho@microsoft.com>
2017-10-05 10:58:40 -0700

    Removed changes from other files except .sln files

--- src/Common/src/Interop/Windows/kernel32/Interop.FormatMessage.cs
+++ src/Common/src/Interop/Windows/kernel32/Interop.FormatMessage.cs
@@ -3,8 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Runtime.InteropServices;
 using System.Text;
+using System.Runtime.InteropServices;
 
 internal partial class Interop
 {
