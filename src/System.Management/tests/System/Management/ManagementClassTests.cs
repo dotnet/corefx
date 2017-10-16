63815fca956418186e1bea132e659345f78986eb
Paulo Janotti <pauloja@microsoft.com>
2017-10-16 16:37:06 -0700

    Missing files from previous commit

---
+++ src/System.Management/tests/System/Management/ManagementClassTests.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.CodeDom;
+using Xunit;
+
+namespace System.Management.Tests
+{
+    public class ManagementClassTests
+    {
+        [Fact]
+        public void CodeTypeDeclaration_For_Win32_LogicalDisk()
+        {
+            var managementClass = new ManagementClass(null, "Win32_LogicalDisk", null);
+            CodeTypeDeclaration classDom = managementClass.GetStronglyTypedClassCode(false, false);
+            Assert.Equal("LogicalDisk", classDom.Name);
+        }
+    }
+}
