0b2dec177bebc4733f99e265d791792273779ca0
Anirudh Agnihotry <anagniho@microsoft.com>
2017-10-24 13:16:48 -0700

    review completed

--- src/System.Diagnostics.EventLog/tests/Helpers.cs
+++ src/System.Diagnostics.EventLog/tests/Helpers.cs
@@ -7,19 +7,6 @@ namespace System.Diagnostics.Tests
     internal class Helpers
     {
         public static bool IsElevatedAndSupportsEventLogs { get => AdminHelpers.IsProcessElevated() && SupportsEventLogs; }
-        public static bool SupportsEventLogs
-        {
-            get
-            {
-                if (PlatformDetection.IsWindows7)
-                {
-                    return false;
-                }
-                else
-                {
-                    return PlatformDetection.IsNotWindowsNanoServer;
-                }
-            }
-        }
+        public static bool SupportsEventLogs { get => PlatformDetection.IsNotWindowsNanoServer && !PlatformDetection.IsWindows7; }
     }
 }
