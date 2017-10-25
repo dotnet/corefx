e956413bea8fefc37c696e2b998ad5d14b0d683b
Ahson Khan <ahkha@microsoft.com>
2017-10-25 09:44:00 -0700

    Marking Memory struct as readonly, fixing corefx#23809 (#14684)
    Signed-off-by: dotnet-bot-corefx-mirror <dotnet-bot@microsoft.com>

--- src/Common/src/System.Private.CoreLib/shared/System/ReadOnlyMemory.cs
+++ src/Common/src/System.Private.CoreLib/shared/System/ReadOnlyMemory.cs
@@ -14,7 +14,7 @@ namespace System
 {
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     [DebuggerTypeProxy(typeof(MemoryDebugView<>))]
-    public struct ReadOnlyMemory<T>
+    public readonly struct ReadOnlyMemory<T>
     {
         // NOTE: With the current implementation, Memory<T> and ReadOnlyMemory<T> must have the same layout,
         // as code uses Unsafe.As to cast between them.
