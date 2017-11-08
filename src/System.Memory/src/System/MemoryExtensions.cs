ef35f9fabae679df940a4c0ec4e3dc373b319162
ektrah <klaus.hartke@gmail.com>
2017-11-08 15:43:01 +0100

    Add implementation notes

--- src/System.Memory/src/System/MemoryExtensions.cs
+++ src/System.Memory/src/System/MemoryExtensions.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -275,6 +275,134 @@ namespace System
             new ReadOnlySpan<T>(array).CopyTo(destination);
         }
 
+        //
+        //  Overlaps
+        //  ========
+        //
+        //  The following methods can be used to determine if two sequences
+        //  overlap in memory.
+        //
+        //  Two sequences overlap if they have positions in common and neither
+        //  is empty. Empty sequences do not overlap with any other sequence.
+        //
+        //  If two sequences overlap, the element offset is the number of
+        //  elements by which the second sequence is offset from the first
+        //  sequence (i.e., second minus first). The number is rounded away from
+        //  zero if it is not a whole number, which can happen when casting a
+        //  sequence of a smaller type to a sequence of a larger type. If the
+        //  sequences do not overlap, the offset is meaningless and arbitrarily
+        //  set to zero.
+        //
+        //  Implementation
+        //  --------------
+        //
+        //  Implementing this correctly is quite tricky due of two problems:
+        //
+        //  * If the sequences refer to two different objects on the managed
+        //    heap, the garbage collector can move them freely around or change
+        //    their relative order in memory.
+        //
+        //  * The distance between two sequences can be greater than
+        //    int.MaxValue (on a 32-bit system) or long.MaxValue (on a 64-bit
+        //    system).
+        //
+        //  (For simplicity, the following text assumes a 32-bit system, but
+        //  everything also applies to a 64-bit system if every 32 is replaced a
+        //  64.)
+        //
+        //  The first problem is solved by calculating the distance with exactly
+        //  one atomic operation. If the garbage collector happens to move the
+        //  sequences afterwards and the sequences overlapped before, they will
+        //  still overlap after the move and their distance hasn't changed. If
+        //  the sequences did not overlap, the distance can change but the
+        //  sequences still won't overlap.
+        //
+        //  The second problem is solved by making all addresses relative to the
+        //  start of the first sequence and performing all operations in
+        //  unsigned integer arithmetic modulo 2³².
+        //
+        //  Example
+        //  -------
+        //
+        //  Let's say there are two sequences, x and y. Let
+        //
+        //      ref T xRef    = x.DangerousGetPinnableReference()
+        //      uint  xLength = x.Length * Unsafe.SizeOf<T>()
+        //      ref T yRef    = y.DangerousGetPinnableReference()
+        //      uint  yLength = y.Length * Unsafe.SizeOf<T>()
+        //
+        //  Visually, the two sequences are located somewhere in the 32-bit
+        //  address space as follows:
+        //
+        //      [----------------------------------------------)                            normal address space
+        //      0                                             2³²
+        //                            [------------------)                                  first sequence
+        //                            xRef            xRef + xLength
+        //              [--------------------------)     .                                  second sequence
+        //              yRef          .         yRef + yLength
+        //              :             .            .     .
+        //              :             .            .     .
+        //                            .            .     .
+        //                            .            .     .
+        //                            .            .     .
+        //                            [----------------------------------------------)      relative address space
+        //                            0            .     .                          2³²
+        //                            [------------------)             :                    first sequence
+        //                            x1           .     x2            :
+        //                            -------------)                   [-------------       second sequence
+        //                                         y2                  y1
+        //
+        //  The idea is to make all addresses relative to xRef: Let x1 be the
+        //  start address of x in this relative address space, x2 the end
+        //  address of x, y1 the start address of y, and y2 the end address of
+        //  y:
+        //
+        //      nuint x1 = 0
+        //      nuint x2 = xLength
+        //      nuint y1 = (nuint)Unsafe.ByteOffset(xRef, yRef)
+        //      nuint y2 = y1 + yLength
+        //  
+        //  xRef relative to xRef is 0.
+        //  
+        //  x2 is simply x1 + xLength. This cannot overflow.
+        //  
+        //  yRef relative to xRef is (yRef - xRef). If (yRef - xRef) is
+        //  negative, casting it to an unsigned 32-bit integer turns it into
+        //  (yRef - xRef + 2³²). So, in the example above, y1 moves to the right
+        //  of x2.
+        //  
+        //  y2 is simply y1 + yLength. Note that this can overflow, as in the
+        //  example above, which must be avoided.
+        //
+        //  The two sequences do *not* overlap if y is entirely in the space
+        //  right of x in the relative address space. (It can't be left of it!)
+        //
+        //          (y1 >= x2) && (y2 <= 2³²)
+        //
+        //  Inversely, they do overlap if
+        //
+        //          (y1 < x2) || (y2 > 2³²)
+        //
+        //  After substituting x2 and y2 with their respective definition:
+        //
+        //      ==  (y1 < xLength) || (y1 + yLength > 2³²)
+        //
+        //  Since yLength can't be greater than the size of the address space,
+        //  the overflow can be avoided as follows:
+        //
+        //      ==  (y1 < xLength) || (y1 > 2³² - yLength)
+        //
+        //  However, 2³² cannot be stored in an unsigned 32-bit integer, so one
+        //  more change is needed to keep doing everything with unsigned 32-bit
+        //  integers:
+        //
+        //      ==  (y1 < xLength) || (y1 > -yLength)
+        //  
+        //  Due to modulo arithmetic, this gives exactly same result *except* if
+        //  yLength is zero, since 2³² - 0 is 0 and not 2³². So the case
+        //  y.IsEmpty must be handled separately first.
+        //  
+
         /// <summary>
         /// Copies the contents of the array into the memory. If the source
         /// and destinations overlap, this method behaves as if the original values are in
@@ -313,10 +441,6 @@ namespace System
         /// <summary>
         /// Determines whether two sequences overlap in memory.
         /// </summary>
-        /// <remarks>
-        /// Two sequences overlap if they have positions in common and neither is empty.
-        /// Empty sequences do not overlap with any other sequence.
-        /// </remarks>
         public static bool Overlaps<T>(this ReadOnlySpan<T> first, ReadOnlySpan<T> second)
         {
             if (first.IsEmpty || second.IsEmpty)
@@ -343,16 +467,6 @@ namespace System
         /// <summary>
         /// Determines whether two sequences overlap in memory and outputs the element offset.
         /// </summary>
-        /// <remarks>
-        /// Two sequences overlap if they have positions in common and neither is empty.
-        /// Empty sequences do not overlap with any other sequence. If the sequences
-        /// overlap, `elementOffset` is the number of elements by which the second sequence
-        /// is offset from the first sequence (i.e., second minus first). The number is
-        /// rounded away from zero if it is not a whole number, which can happen when
-        /// casting a sequence of a smaller type to a sequence of a larger type. If the
-        /// sequences do not overlap, `elementOffset` is meaningless and arbitrarily set to
-        /// zero.
-        /// </remarks>
         public static bool Overlaps<T>(this ReadOnlySpan<T> first, ReadOnlySpan<T> second, out int elementOffset)
         {
             if (first.IsEmpty || second.IsEmpty)
