06778ff99c1ffc7ba5cc4390d70ad7d58bc5fece
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-11-09 09:11:25 +0100

    PR feedback

--- src/Native/Unix/System.Native/pal_dl.h
+++ src/Native/Unix/System.Native/pal_dl.h
@@ -10,6 +10,17 @@ enum
     PAL_RTLD_LAZY = 1,
     PAL_RTLD_NOW = 2
 };
+
+/**
+ * Loads a dynamic library file. Implemented as shim to dlopen(3).
+ *
+ * An opaque handle for the dynamic library, or null on failure.
+ */
 extern "C" void* SystemNative_DlOpen(const char *file, int mode);
 
+/**
+ * Gets, for a given dynamic library, the address of a symbol in that library. Implemented as shim to dlopen(3).
+ *
+ * The address of the symbol, or null on failure.
+ */
 extern "C" void* SystemNative_DlSym(void *handle, const char *name);
