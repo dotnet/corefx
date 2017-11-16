519c1c9002d3aeb83a5d956f2e46e4c50c632ab7
Victor Nate Graf <vigraf@microsoft.com>
2017-11-15 16:23:16 -0800

    Change label to use new performance machine pool

--- perf.groovy
+++ perf.groovy
@@ -58,7 +58,7 @@ def osShortName = ['Windows 10': 'win10',
                     label('windows_clr_perf')
                 }
                 else {
-                    label('linux_clr_perf')
+                    label('ubuntu_1604_clr_perf')
                 }
 
                 wrappers {
