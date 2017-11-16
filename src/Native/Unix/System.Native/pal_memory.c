6a9f579209491ae9041f208bfe34b89085c0279e
Ludovic Henry <luhenry@microsoft.com>
2017-11-16 11:45:26 -0500

    Start switching System.Native from C++ to C (#25032)
    * Move System.Native/pal_maphardwaretype.cpp to C
    
    * Switch C compiler from C11 to C99
    
    * Move System.Native/pal_errno.cpp to C
    
    * Move System.Native/pal_tcpstate.cpp to C
    
    * Move System.Native/pal_networkstatistics.cpp to C
    
    * Move System.Native/pal_memory.cpp to C
    
    * Move System.Native/pal_io.cpp to C
    
    * Move System.Native/pal_networking.cpp to C
    
    * Define c_static_assert for compiler that don't support static_assert

--- src/Native/Unix/System.Native/pal_memory.c
+++ src/Native/Unix/System.Native/pal_memory.c
@@ -5,7 +5,7 @@
 #include "pal_memory.h"
 #include <string.h>
 
-extern "C" void* SystemNative_MemSet(void *s, int c, uintptr_t n)
+void* SystemNative_MemSet(void *s, int c, uintptr_t n)
 {
-    return memset(s, c, static_cast<size_t>(n));
+    return memset(s, c, (size_t)n);
 }
