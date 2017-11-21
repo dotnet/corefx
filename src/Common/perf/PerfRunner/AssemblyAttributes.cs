04dbfef8503d81e8d7acad537f342dc6ffa8381b
Jon Hanna <jon@hackcraft.net>
2017-11-21 18:59:26 +0000

    Remove ExcludeFromCodeCoverageAssemblyAttribute
    Since ExcludeFromCodeCoverageAttribute can now be applied to
    assemblies, it no longer serves any purpose.

--- src/Common/perf/PerfRunner/AssemblyAttributes.cs
+++ src/Common/perf/PerfRunner/AssemblyAttributes.cs
@@ -2,4 +2,4 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-[assembly: System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverageAssembly]
+[assembly: System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
