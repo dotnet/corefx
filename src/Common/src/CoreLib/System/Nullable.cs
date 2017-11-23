5d5592d1a7d2ca21f4f2dc26574cc9df53346daf
Stephen Toub <stoub@microsoft.com>
2017-11-23 11:05:59 -0500

    Revert making Nullable<T> readonly (dotnet/coreclr#15198)
    * Revert making Nullable<T> readonly
    
    * Address PR feedback
    
    Signed-off-by: dotnet-bot-corefx-mirror <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/Nullable.cs
+++ src/Common/src/CoreLib/System/Nullable.cs
@@ -14,10 +14,10 @@ namespace System
     [Serializable]
     [NonVersionable] // This only applies to field layout
     [System.Runtime.CompilerServices.TypeForwardedFrom("mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
-    public readonly struct Nullable<T> where T : struct
+    public struct Nullable<T> where T : struct
     {
         private readonly bool hasValue; // Do not rename (binary serialization)
-        internal readonly T value; // Do not rename (binary serialization)
+        internal T value; // Do not rename (binary serialization) or make readonly (can be mutated in ToString, etc.)
 
         [NonVersionable]
         public Nullable(T value)
