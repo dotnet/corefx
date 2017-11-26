554e56451bd1b06a3c9ef59ed4e5a3170bad346b
Jon Hanna <jon@hackcraft.net>
2017-11-26 15:58:09 +0000

    Remove dead path from SetParameterAttributes
    Looks for default decimal value, but we've already looked for that via
    DecimalConstantAttribute.

--- src/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/SymbolTable.cs
+++ src/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/SymbolTable.cs
@@ -1705,6 +1705,7 @@ namespace Microsoft.CSharp.RuntimeBinder
                     {
                         object defValue = parameter.DefaultValue;
 #endif
+                        Debug.Assert(Type.GetTypeCode(defValue.GetType()) != TypeCode.Decimal); // Handled above
                         switch (Type.GetTypeCode(defValue.GetType()))
                         {
 
@@ -1736,11 +1737,6 @@ namespace Microsoft.CSharp.RuntimeBinder
                             case TypeCode.Double:
                                 cv = ConstVal.Get((double)defValue);
                                 cvType = _semanticChecker.SymbolLoader.GetPredefindType(PredefinedType.PT_DOUBLE);
-                                break;
-
-                            case TypeCode.Decimal:
-                                cv = ConstVal.Get((decimal)defValue);
-                                cvType = _semanticChecker.SymbolLoader.GetPredefindType(PredefinedType.PT_DECIMAL);
                                 break;
 
                             case TypeCode.Char:
