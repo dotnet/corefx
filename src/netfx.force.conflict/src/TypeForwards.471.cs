fed6daaa5127e55f1ae90ecc14b0ff5cb98a75b6
Jose Perez Rodriguez <joperezr@microsoft.com>
2017-12-07 15:46:59 -0800

    Adding netfx.force.conflicts.dll to 471 Support package

---
+++ src/netfx.force.conflict/src/TypeForwards.471.cs
@@ -0,0 +1,14 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.CompilerServices;
+
+// System.Diagnostics.Tracing
+[assembly: TypeForwardedTo(typeof(System.Diagnostics.Tracing.EventSource))]
+// System.IO.Compression
+[assembly: TypeForwardedTo(typeof(System.IO.Compression.GZipStream))]
+//  System.Net.Http
+[assembly: TypeForwardedTo(typeof(System.Net.Http.HttpClient))]
+//  System.Runtime.Serialization.Primitives
+[assembly: TypeForwardedTo(typeof(System.Runtime.Serialization.DataContractAttribute))]
