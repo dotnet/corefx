3d0ac2076408086b1906296cdb7696056cb638ca
ntr <ntr@ihfood.dk>
2017-12-07 16:12:10 +0100

    Fix string test, add double tests.

--- src/System.Memory/tests/ReadOnlySpan/BinarySearch.cs
+++ src/System.Memory/tests/ReadOnlySpan/BinarySearch.cs
@@ -10,30 +10,53 @@ namespace System.SpanTests
     public static partial class ReadOnlySpanTests
     {
         [Theory]
-        [InlineData(new uint[] { 1u, 2u, 3u, 4u }, 0u, -1)]
-        [InlineData(new uint[] { 1u, 2u, 3u, 4u }, 1u, 0)]
-        [InlineData(new uint[] { 1u, 2u, 3u, 4u }, 2u, 1)]
-        [InlineData(new uint[] { 1u, 2u, 3u, 4u }, 3u, 2)]
-        [InlineData(new uint[] { 1u, 2u, 3u, 4u }, 4u, 3)]
-        [InlineData(new uint[] { 1u, 2u, 3u, 4u }, 5u, -5)]
+        [InlineData(new uint[] { 1u, 2u, 4u, 5u }, 0u, -1)]
+        [InlineData(new uint[] { 1u, 2u, 4u, 5u }, 1u, 0)]
+        [InlineData(new uint[] { 1u, 2u, 4u, 5u }, 2u, 1)]
+        [InlineData(new uint[] { 1u, 2u, 4u, 5u }, 3u, -3)]
+        [InlineData(new uint[] { 1u, 2u, 4u, 5u }, 4u, 2)]
+        [InlineData(new uint[] { 1u, 2u, 4u, 5u }, 5u, 3)]
+        [InlineData(new uint[] { 1u, 2u, 4u, 5u }, 6u, -5)]
         public static void BinarySearch_UInt(uint[] a, uint value, int expectedIndex)
         {
-            ReadOnlySpan<uint> span = new ReadOnlySpan<uint>(a);
+            var span = new ReadOnlySpan<uint>(a);
 
             var index = span.BinarySearch(value);
 
             Assert.Equal(expectedIndex, index);
         }
 
-        [Fact]
-        public static void BinarySearch_String_B()
+        [Theory]
+        [InlineData(new double[] { 1, 2, 4, 5 }, 0, -1)]
+        [InlineData(new double[] { 1, 2, 4, 5 }, 1, 0)]
+        [InlineData(new double[] { 1, 2, 4, 5 }, 2, 1)]
+        [InlineData(new double[] { 1, 2, 4, 5 }, 3, -3)]
+        [InlineData(new double[] { 1, 2, 4, 5 }, 4, 2)]
+        [InlineData(new double[] { 1, 2, 4, 5 }, 5, 3)]
+        [InlineData(new double[] { 1, 2, 4, 5 }, 6, -5)]
+        public static void BinarySearch_Double(double[] a, double value, int expectedIndex)
         {
-            string[] a = { "a", "b", "c", "d" };
-            ReadOnlySpan<string> span = new ReadOnlySpan<string>(a);
+            var span = new ReadOnlySpan<double>(a);
 
-            var index = span.BinarySearch("b");
+            var index = span.BinarySearch(value);
 
-            Assert.Equal(1, index);
+            Assert.Equal(expectedIndex, index);
+        }
+
+        [Theory]
+        [InlineData(new string[] { "b", "c", "d", "e" }, "a", -1)]
+        [InlineData(new string[] { "b", "c", "d", "e" }, "b", 0)]
+        [InlineData(new string[] { "b", "c", "d", "e" }, "c", 1)]
+        [InlineData(new string[] { "b", "c", "d", "e" }, "d", 2)]
+        [InlineData(new string[] { "b", "c", "d", "e" }, "e", 3)]
+        [InlineData(new string[] { "b", "c", "d", "e" }, "f", -5)]
+        public static void BinarySearch_String(string[] a, string value, int expectedIndex)
+        {
+            var span = new ReadOnlySpan<string>(a);
+
+            var index = span.BinarySearch(value);
+
+            Assert.Equal(expectedIndex, index);
         }
 
         //[Fact]
