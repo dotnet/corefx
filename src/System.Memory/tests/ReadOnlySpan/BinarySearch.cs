471f1f71c41c42eab03a98eb97ad1562b6c5efe6
ntr <ntr@ihfood.dk>
2017-12-07 18:59:37 +0100

    refactor tests.

--- src/System.Memory/tests/ReadOnlySpan/BinarySearch.cs
+++ src/System.Memory/tests/ReadOnlySpan/BinarySearch.cs
@@ -8,22 +8,57 @@ namespace System.SpanTests
 {
     public static partial class ReadOnlySpanTests
     {
-        [Theory]
-        [InlineData(new uint[] { }, 0u, -1)]
-        [InlineData(new uint[] { 1u, 2u, 4u, 5u }, 0u, -1)]
-        [InlineData(new uint[] { 1u, 2u, 4u, 5u }, 1u, 0)]
-        [InlineData(new uint[] { 1u, 2u, 4u, 5u }, 2u, 1)]
-        [InlineData(new uint[] { 1u, 2u, 4u, 5u }, 3u, -3)]
-        [InlineData(new uint[] { 1u, 2u, 4u, 5u }, 4u, 2)]
-        [InlineData(new uint[] { 1u, 2u, 4u, 5u }, 5u, 3)]
-        [InlineData(new uint[] { 1u, 2u, 4u, 5u }, 6u, -5)]
-        public static void BinarySearch_UInt(uint[] a, uint value, int expectedIndex)
+        public static TheoryData<(uint[] Array, uint Value, int ExpectedIndex)> UIntCases =>
+            new TheoryData<(uint[] Array, uint Value, int ExpectedIndex)> {
+                (new uint[] { }, 0u, -1),
+                (new uint[] { 1u, 2u, 4u, 5u }, 0u, -1),
+                (new uint[] { 1u, 2u, 4u, 5u }, 1u, 0),
+                (new uint[] { 1u, 2u, 4u, 5u }, 2u, 1),
+                (new uint[] { 1u, 2u, 4u, 5u }, 3u, -3),
+                (new uint[] { 1u, 2u, 4u, 5u }, 4u, 2),
+                (new uint[] { 1u, 2u, 4u, 5u }, 5u, 3),
+                (new uint[] { 1u, 2u, 4u, 5u }, 6u, -5),
+            };
+        public static TheoryData<(double[] Array, double Value, int ExpectedIndex)> DoubleCases =>
+            new TheoryData<(double[] Array, double Value, int ExpectedIndex)> {
+                (new double[] { }, 0u, -1),
+                (new double[] { 1u, 2u, 4u, 5u }, 0u, -1),
+                (new double[] { 1u, 2u, 4u, 5u }, 1u, 0),
+                (new double[] { 1u, 2u, 4u, 5u }, 2u, 1),
+                (new double[] { 1u, 2u, 4u, 5u }, 3u, -3),
+                (new double[] { 1u, 2u, 4u, 5u }, 4u, 2),
+                (new double[] { 1u, 2u, 4u, 5u }, 5u, 3),
+                (new double[] { 1u, 2u, 4u, 5u }, 6u, -5),
+            };
+
+        [Theory, MemberData(nameof(UIntCases))]
+        public static void BinarySearch_UInt_Span(
+            (uint[] Array, uint Value, int ExpectedIndex) c)
         {
-            var span = new ReadOnlySpan<uint>(a);
+            var index = new Span<uint>(c.Array).BinarySearch(c.Value);
+            Assert.Equal(c.ExpectedIndex, index);
+        }
+        [Theory, MemberData(nameof(UIntCases))]
+        public static void BinarySearch_UInt_ReadOnlySpan(
+            (uint[] Array, uint Value, int ExpectedIndex) c)
+        {
+            var index = new ReadOnlySpan<uint>(c.Array).BinarySearch(c.Value);
+            Assert.Equal(c.ExpectedIndex, index);
+        }
 
-            var index = span.BinarySearch(value);
-
-            Assert.Equal(expectedIndex, index);
+        [Theory, MemberData(nameof(DoubleCases))]
+        public static void BinarySearch_Double_Span(
+            (double[] Array, double Value, int ExpectedIndex) c)
+        {
+            var index = new Span<double>(c.Array).BinarySearch(c.Value);
+            Assert.Equal(c.ExpectedIndex, index);
+        }
+        [Theory, MemberData(nameof(DoubleCases))]
+        public static void BinarySearch_Double_ReadOnlySpan(
+            (double[] Array, double Value, int ExpectedIndex) c)
+        {
+            var index = new ReadOnlySpan<double>(c.Array).BinarySearch(c.Value);
+            Assert.Equal(c.ExpectedIndex, index);
         }
 
         [Theory]
@@ -37,7 +72,8 @@ namespace System.SpanTests
         [InlineData(new double[] { 1, 2, 4, 5 }, 6, -5)]
         public static void BinarySearch_Double(double[] a, double value, int expectedIndex)
         {
-            var span = new ReadOnlySpan<double>(a);
+            // Implicitly tests ReadOnlySpan
+            var span = new Span<double>(a);
 
             var index = span.BinarySearch(value);
 
@@ -55,7 +91,8 @@ namespace System.SpanTests
         [InlineData(new string[] { "b", "c", "e", "f" }, "g", -5)]
         public static void BinarySearch_String(string[] a, string value, int expectedIndex)
         {
-            var span = new ReadOnlySpan<string>(a);
+            // Implicitly tests ReadOnlySpan
+            var span = new Span<string>(a);
 
             var index = span.BinarySearch(value);
 
