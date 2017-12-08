c4d3097541d54d447822957a7c5ce7b6c3bd3228
Jose Perez Rodriguez <joperezr@microsoft.com>
2017-12-08 11:05:02 -0800

    Adding all 12 assemblies to the typeforward list

--- src/netfx.force.conflict/src/TypeForwards.471.cs
+++ src/netfx.force.conflict/src/TypeForwards.471.cs
@@ -4,11 +4,28 @@
 
 using System.Runtime.CompilerServices;
 
+// System.Data.Common
+[assembly: TypeForwardedTo(typeof(System.Data.Common.DbColumn))]
+// System.Diagnostics.StackTrace
+[assembly: TypeForwardedTo(typeof(System.Diagnostics.StackTrace))]
 // System.Diagnostics.Tracing
 [assembly: TypeForwardedTo(typeof(System.Diagnostics.Tracing.EventSource))]
+// System.Globalization.Extensions
+[assembly: TypeForwardedTo(typeof(System.Globalization.GlobalizationExtensions))]
 // System.IO.Compression
 [assembly: TypeForwardedTo(typeof(System.IO.Compression.GZipStream))]
-//  System.Net.Http
+// System.Net.Http
 [assembly: TypeForwardedTo(typeof(System.Net.Http.HttpClient))]
-//  System.Runtime.Serialization.Primitives
+// System.Net.Sockets
+[assembly: TypeForwardedTo(typeof(System.Net.Sockets.Socket))]
+// System.Runtime.Serialization.Primitives
 [assembly: TypeForwardedTo(typeof(System.Runtime.Serialization.DataContractAttribute))]
+// System.Security.Cryptography.Algorithms
+[assembly: TypeForwardedTo(typeof(System.Security.Cryptography.Aes))]
+// System.Security.SecureString
+[assembly: TypeForwardedTo(typeof(System.Security.SecureString))]
+// System.Threading.Overlapped
+[assembly: TypeForwardedTo(typeof(System.Threading.PreAllocatedOverlapped))]
+// System.Xml.XPath.XDocument
+[assembly: TypeForwardedTo(typeof(System.Xml.XPath.Extensions))]
+
