8cb65a5cd1c94c1279b862fb63a17264b592379a
Max Kerr <rikerr@microsoft.com>
2017-12-08 14:28:22 -0800

    Port changes from framework as-is. Fix does not appear to work.

--- src/System.Private.Uri/tests/FunctionalTests/IriTest.cs
+++ src/System.Private.Uri/tests/FunctionalTests/IriTest.cs
@@ -521,5 +521,26 @@ namespace System.PrivateUri.Tests
             catch (FormatException)
             { }
         }
+
+        [Fact]
+        public void Iri_FileUriUncFallback_DoesSupportUnicodeHost()
+        {
+            string[] authorities = { "\u00E8", "_\u00E8", "_" };
+            foreach (string authority in authorities)
+            {
+                Uri fileTwoSlashes = new Uri("file://" + authority);
+                Uri fileThreeSlashes = new Uri("file:///" + authority);
+                Uri fileFourSlashes = new Uri("file:////" + authority);
+                Assert.Equal(authority, fileTwoSlashes.Authority); // Two slashes must be followed by an authority
+                Assert.Equal(String.Empty, fileThreeSlashes.Authority); // Three slashes indicates an empty authority
+                Assert.Equal(authority, fileFourSlashes.Authority); // More than three slashes looks like a UNC share
+            }
+        }
+
+        [Fact]
+        public void Iri_FileUriUncFallback_DoesNotSupportPercentEncodedHost()
+        {
+            Assert.Throws<UriFormatException>(() => { var invalidUri = new Uri("file://_%C3%A8"); });
+        }
     }
 }
