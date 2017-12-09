4d9265c1ca5dc9d9a86465f47fa6d37db3187638
Jose Perez Rodriguez <joperezr@microsoft.com>
2017-12-08 16:50:01 -0800

    Adding netfx.force.conflicts.dll to 471 Support package (#25786)
    * Adding netfx.force.conflicts.dll to 471 Support package
    
    * Adding all 12 assemblies to the typeforward list

---
+++ src/netfx.force.conflict/src/TypeForwards.471.cs
@@ -0,0 +1,31 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.CompilerServices;
+
+// System.Data.Common
+[assembly: TypeForwardedTo(typeof(System.Data.Common.DbColumn))]
+// System.Diagnostics.StackTrace
+[assembly: TypeForwardedTo(typeof(System.Diagnostics.StackTrace))]
+// System.Diagnostics.Tracing
+[assembly: TypeForwardedTo(typeof(System.Diagnostics.Tracing.EventSource))]
+// System.Globalization.Extensions
+[assembly: TypeForwardedTo(typeof(System.Globalization.GlobalizationExtensions))]
+// System.IO.Compression
+[assembly: TypeForwardedTo(typeof(System.IO.Compression.GZipStream))]
+// System.Net.Http
+[assembly: TypeForwardedTo(typeof(System.Net.Http.HttpClient))]
+// System.Net.Sockets
+[assembly: TypeForwardedTo(typeof(System.Net.Sockets.Socket))]
+// System.Runtime.Serialization.Primitives
+[assembly: TypeForwardedTo(typeof(System.Runtime.Serialization.DataContractAttribute))]
+// System.Security.Cryptography.Algorithms
+[assembly: TypeForwardedTo(typeof(System.Security.Cryptography.Aes))]
+// System.Security.SecureString
+[assembly: TypeForwardedTo(typeof(System.Security.SecureString))]
+// System.Threading.Overlapped
+[assembly: TypeForwardedTo(typeof(System.Threading.PreAllocatedOverlapped))]
+// System.Xml.XPath.XDocument
+[assembly: TypeForwardedTo(typeof(System.Xml.XPath.Extensions))]
+
