687a93a8eb246ef3529b847c99e65d1d69ea1a63
ahsonkhan <ahson_ahmedk@yahoo.com>
2017-12-08 18:29:16 -0800

    Adding compiler services using directive to use Unsafe.

--- src/System.Memory/tests/Memory/CustomMemoryForTest.cs
+++ src/System.Memory/tests/Memory/CustomMemoryForTest.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Buffers;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
 
