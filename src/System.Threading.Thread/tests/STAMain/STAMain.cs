3d998818abc25b125627f0476f791d295d6198ea
Anipik <anirudhagnihotry098@gmail.com>
2017-12-14 18:20:32 -0800

    Tests For STA\MTA Main Thread Attribute

---
+++ src/System.Threading.Thread/tests/STAMain/STAMain.cs
@@ -0,0 +1,43 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Threading;
+
+namespace STAMain
+{
+    internal static class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            string mode = args[0];
+            int retValue = 1;
+            Thread curThread = Thread.CurrentThread;
+            
+            if (mode == "GetApartmentState")
+            {
+                if (curThread.GetApartmentState() == ApartmentState.STA)
+                {
+                    curThread.SetApartmentState(ApartmentState.STA);
+                    retValue = 0;
+                }    
+            }
+            else
+            {
+                try
+                {
+                    curThread.SetApartmentState(ApartmentState.MTA);
+                }
+                catch (InvalidOperationException)
+                {
+                    retValue = 0;
+                }
+                catch (PlatformNotSupportedException) {}
+            }
+
+            return retValue;
+        }
+    }
+}
