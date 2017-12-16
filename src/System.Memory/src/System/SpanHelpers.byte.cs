0c0bd3048a33683f4c1cb5fb6e8f9b6ec1b7986c
ahsonkhan <ahson_ahmedk@yahoo.com>
2017-12-15 17:32:20 -0800

    Use explicit type instead of var where type can't be inferred easily.

--- src/System.Memory/src/System/SpanHelpers.byte.cs
+++ src/System.Memory/src/System/SpanHelpers.byte.cs
@@ -437,7 +437,7 @@ namespace System
 
                 while ((byte*)nLength > (byte*)index)
                 {
-                    var vData = Unsafe.ReadUnaligned<Vector<byte>>(ref Unsafe.AddByteOffset(ref searchSpace, index));
+                    Vector<byte> vData = Unsafe.ReadUnaligned<Vector<byte>>(ref Unsafe.AddByteOffset(ref searchSpace, index));
                     var vMatches = Vector.BitwiseOr(
                                     Vector.Equals(vData, values0),
                                     Vector.Equals(vData, values1));
@@ -572,7 +572,7 @@ namespace System
                 Vector<byte> values2 = GetVector(value2);
                 while ((byte*)nLength > (byte*)index)
                 {
-                    var vData = Unsafe.ReadUnaligned<Vector<byte>>(ref Unsafe.AddByteOffset(ref searchSpace, index));
+                    Vector<byte> vData = Unsafe.ReadUnaligned<Vector<byte>>(ref Unsafe.AddByteOffset(ref searchSpace, index));
 
                     var vMatches = Vector.BitwiseOr(
                                     Vector.BitwiseOr(
@@ -707,7 +707,7 @@ namespace System
 
                 while ((byte*)nLength > (byte*)(Vector<byte>.Count - 1))
                 {
-                    var vData = Unsafe.ReadUnaligned<Vector<byte>>(ref Unsafe.AddByteOffset(ref searchSpace, index - Vector<byte>.Count));
+                    Vector<byte> vData = Unsafe.ReadUnaligned<Vector<byte>>(ref Unsafe.AddByteOffset(ref searchSpace, index - Vector<byte>.Count));
                     var vMatches = Vector.BitwiseOr(
                                     Vector.Equals(vData, values0),
                                     Vector.Equals(vData, values1));
@@ -837,7 +837,7 @@ namespace System
                 Vector<byte> values2 = GetVector(value2);
                 while ((byte*)nLength > (byte*)(Vector<byte>.Count - 1))
                 {
-                    var vData = Unsafe.ReadUnaligned<Vector<byte>>(ref Unsafe.AddByteOffset(ref searchSpace, index - Vector<byte>.Count));
+                    Vector<byte> vData = Unsafe.ReadUnaligned<Vector<byte>>(ref Unsafe.AddByteOffset(ref searchSpace, index - Vector<byte>.Count));
 
                     var vMatches = Vector.BitwiseOr(
                                     Vector.BitwiseOr(
