df4097a5e1ce0a91e3b2c68ff71f3f543a413018
wtgodbe <wigodbe@microsoft.com>
2017-12-18 13:41:55 -0800

    Casing

--- src/System.Diagnostics.FileVersionInfo/src/System/Diagnostics/FileVersionInfo.cs
+++ src/System.Diagnostics.FileVersionInfo/src/System/Diagnostics/FileVersionInfo.cs
@@ -271,7 +271,7 @@ namespace System.Diagnostics
                 throw new FileNotFoundException(fileName);
             }
             // Check if fileName is a full path.
-            if(!Path.isPathRooted(fileName))
+            if(!Path.IsPathRooted(fileName))
             {
                 throw new ArgumentException("Absolute path information is required.");
             }
