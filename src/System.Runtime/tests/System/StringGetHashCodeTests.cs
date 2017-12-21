812153d86ad19b76cb8b256f1af38921614b21f0
Maryam Ariyan <maryam.ariyan@microsoft.com>
2017-12-21 11:28:00 -0500

    Adding string random hashing tests for string comparers
    Moving Dictionary hash collision test to Dictionary test class
    
    Fixing test failure on netfx
    
    Problems:
    - Dictionary exposes internal type comparer
    
    - netfx used randomized comparer by default and netcoreapp used non-randomized and switches to randomized after reaching collision threshold
    
    - StringComparer.OrdinalIgnoreCase does not produce randomized hash codes in netcoreapp

--- src/System.Runtime/tests/System/StringGetHashCodeTests.cs
+++ src/System.Runtime/tests/System/StringGetHashCodeTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -12,6 +13,26 @@ namespace System.Tests
     public class StringGetHashCodeTests : RemoteExecutorTestBase
     {
         /// <summary>
+        /// Ensure that StringComparer.OrdinalIgnoreCase performs a case-insensitive ordinal string comparison
+        /// </summary>
+        [Fact]
+        public void GetHashCode_UsingOrdinalIgnoreCaseComparer_SameHashCodeCaseInsensitive()
+        {
+            var ordinalIgnoreCase = StringComparer.OrdinalIgnoreCase;
+            Assert.Equal(ordinalIgnoreCase.GetHashCode("ABC"), ordinalIgnoreCase.GetHashCode("abc"));
+        }
+
+        /// <summary>
+        /// Ensure that StringComparer.Ordinal performs a case-sensitive ordinal string comparison
+        /// </summary>
+        [Fact]
+        public void GetHashCode_UsingOrdinalComparer_SameHashCodeCaseSensitive()
+        {
+            var ordinal = StringComparer.Ordinal;
+            Assert.NotEqual(ordinal.GetHashCode("ABC"), ordinal.GetHashCode("abc"));
+        }
+
+        /// <summary>
         /// Ensure that string hash codes are randomized by getting the hash code for the same string in two processes
         /// and confirming it is different (modulo possible values of int).
         /// If the legacy hash codes are being returned, it will not be different.
@@ -20,44 +41,89 @@ namespace System.Tests
         public void GetHashCode_UseSameStringInTwoProcesses_ReturnsDifferentHashCodes()
         {
             const string abc = "same string in different processes";
-            int childHashCode, parentHashCode = abc.GetHashCode(), timesTried = 0;
+            Func<int> ComputeHash = () => abc.GetHashCode();
 
-            Func<int> GetChildHashCode = () =>
+            int parentHashCode = ComputeHash();
+            int childHashCode = GetChildHashCode(ComputeHash, parentHashCode);
+            Assert.NotEqual(parentHashCode, childHashCode);
+        }
+
+        /// <summary>
+        /// Ensure that string comparer hash codes are randomized by getting the hash in two processes
+        /// and confirming it is different (modulo possible values of int).
+        /// If the legacy hash codes are being returned, it will not be different.
+        /// </summary>
+        [Fact]
+        public void GetHashCodeWithStringComparer_UseSameStringInTwoProcesses_ReturnsDifferentHashCodes()
+        {
+            int parentHashCode, childHashCode;
+            IEnumerable<Func<int>> StringCompareHashCodeComputers = GetStringCompareHashCodeComputers();
+
+            foreach (var ComputeHashCode in StringCompareHashCodeComputers)
             {
-                using (RemoteInvokeHandle handle = RemoteInvoke(() => abc.GetHashCode(), new RemoteInvokeOptions { CheckExitCode = false }))
+                parentHashCode = ComputeHashCode();
+                childHashCode = GetChildHashCode(ComputeHashCode, parentHashCode);
+                Assert.NotEqual(parentHashCode, childHashCode);
+            }
+        }
+
+        private IEnumerable<Func<int>> GetStringCompareHashCodeComputers()
+        {
+            yield return () =>
+            {
+                return StringComparer.CurrentCulture.GetHashCode("abc");
+            };
+
+            yield return () =>
+            {
+                return StringComparer.CurrentCultureIgnoreCase.GetHashCode("abc");
+            };
+
+            yield return () =>
+            {
+                return StringComparer.InvariantCulture.GetHashCode("abc");
+            };
+
+            yield return () =>
+            {
+                return StringComparer.InvariantCultureIgnoreCase.GetHashCode("abc");
+            };
+
+            yield return () =>
+            {
+                return StringComparer.Ordinal.GetHashCode("abc");
+            };
+
+            // OrdinalIgnoreCase not working on netcoreapp
+            if (PlatformDetection.IsFullFramework)
+            {
+                yield return () =>
+                {
+                    return StringComparer.OrdinalIgnoreCase.GetHashCode("abc");
+                };
+            }
+        }
+
+        private int GetChildHashCode(Func<int> computeHash, int parentHashCode)
+        {
+            Func<int> computeHashRemote = () =>
+            {
+                using (RemoteInvokeHandle handle = RemoteInvoke(computeHash, new RemoteInvokeOptions { CheckExitCode = false }))
                 {
                     handle.Process.WaitForExit();
                     return handle.Process.ExitCode;
                 }
             };
 
+            int childHashCode, timesTried = 0;
             do
             {
                 // very small chance the child and parent hashcode are the same. To further reduce chance of collision we try up to 3 times
-                childHashCode = GetChildHashCode();
+                childHashCode = computeHashRemote();
                 timesTried++;
             } while (parentHashCode == childHashCode && timesTried < 3);
-            Assert.NotEqual(parentHashCode, childHashCode);
-        }
 
-        /// <summary>
-        /// Ensure that dictionary changes its comparer to randomized after it encounters more than 100 collisions on a single hash bucket
-        /// </summary>
-        [Fact]
-        public void AddToDictionaryWithNonRandomizedStringComparer_PassHashCollisionThreshold_SwitchesComparerToRandomized()
-        {
-            var dict = new Dictionary<string, int>();
-            Assert.NotSame(EqualityComparer<string>.Default, dict.Comparer);
-            var stringData = new StringsMatchingNonRandomizedHashCode().Data;
-
-            foreach (var s in stringData.Take(101))
-            {
-                dict.Add(s, 0);
-            } 
-
-            Assert.NotSame(EqualityComparer<string>.Default, dict.Comparer);
-            dict.Add(stringData.ElementAt(101), 0);
-            Assert.Same(EqualityComparer<string>.Default, dict.Comparer);
+            return childHashCode;
         }
     }
 }
