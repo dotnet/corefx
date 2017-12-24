2d111fb7fbf92d9985e560ed4021cc6d5f1ecc4b
michael <the.dwyer@gmail.com>
2017-12-24 10:43:29 -0800

    Increase code coverage System.Net.Security

---
+++ src/System.Net.Security/tests/UnitTests/System/Security/Authentication/AuthenticationExceptionTest.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace System.Security.Authentication
+{
+    public class AuthenticationExceptionTest
+    {
+        [Fact]
+        public void Constructor()
+        {
+            AuthenticationException authenticationException = new AuthenticationException();
+
+            Assert.Equal("System error.", authenticationException.Message);
+        }
+
+        [Fact]
+        public void Constructor_String()
+        {
+            AuthenticationException authenticationException = new AuthenticationException("base was called");
+
+            Assert.Equal("base was called", authenticationException.Message);
+        }
+
+        [Fact]
+        public void Constructor_String_Exception()
+        {
+            AuthenticationException authenticationException = new AuthenticationException("base was called", new Exception("this is the inner exception message"));
+
+            Assert.Equal("base was called", authenticationException.Message);
+            Assert.Equal("this is the inner exception message", authenticationException.InnerException.Message);
+        }
+    }
+}
---
+++ src/System.Net.Security/tests/UnitTests/System/Security/Authentication/InvalidCredentialExceptionTest.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace System.Security.Authentication
+{
+    public class InvalidCredentialExceptionTest
+    {
+        [Fact]
+        public void Constructor()
+        {
+            InvalidCredentialException invalidCredentialException = new InvalidCredentialException();
+
+            Assert.Equal("System error.", invalidCredentialException.Message);
+        }
+
+        [Fact]
+        public void Constructor_String()
+        {
+            InvalidCredentialException invalidCredentialException = new InvalidCredentialException("base was called");
+
+            Assert.Equal("base was called", invalidCredentialException.Message);
+        }
+
+        [Fact]
+        public void Constructor_String_Exception()
+        {
+            InvalidCredentialException invalidCredentialException = new InvalidCredentialException("base was called", new Exception("this is the inner exception message"));
+
+            Assert.Equal("base was called", invalidCredentialException.Message);
+            Assert.Equal("this is the inner exception message", invalidCredentialException.InnerException.Message);
+        }
+    }
+}
