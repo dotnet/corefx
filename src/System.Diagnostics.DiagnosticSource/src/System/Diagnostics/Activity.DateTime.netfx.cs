566c44e7510488ad82cc8bbac8bbe4691b45f5c2
Ben Adams <thundercat@illyriad.co.uk>
2017-12-27 01:13:54 +0000

    Activity shouldn't capture AsyncLocals into its Timer

--- src/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/Activity.DateTime.netfx.cs
+++ src/System.Diagnostics.DiagnosticSource/src/System/Diagnostics/Activity.DateTime.netfx.cs
@@ -42,6 +42,16 @@ namespace System.Diagnostics
         private static TimeSync timeSync = new TimeSync();
 
         // sync DateTime and Stopwatch ticks every 2 hours
-        private static Timer syncTimeUpdater = new Timer(s => { Sync(); }, null, 0, 7200000);
+        private static Timer syncTimeUpdater = InitalizeSyncTimer();
+
+        private static Timer InitalizeSyncTimer()
+        {
+            // Don't capture the current ExecutionContext and its AsyncLocals onto the timer causing them to live forever
+            ExecutionContext.SuppressFlow();
+            var timer = new Timer(s => { Sync(); }, null, 0, 7200000);
+            // Restore the current ExecutionContext
+            ExecutionContext.RestoreFlow();
+            return timer;
+        }
     }
 }
