4c53ff65b0dc16244fe4580162b16e7ab7d7f046
William Godbe <wigodbe@microsoft.com>
2018-01-03 10:42:37 -0800

    Print Inner Exceptions in ReflectionTypeLoadException ToString (#15688)
    * Print Inner Exceptions in ReflectionTypeLoadException ToString
    
    * Simplify formatting
    
    * Remove tab
    
    * Fix newlines
    
    * Use AppendLine
    
    * Use foreach
    
    * Remove ToString
    
    * Update string & message
    
    * Modify Message
    
    * Remove parens
    
    Signed-off-by: dotnet-bot-corefx-mirror <dotnet-bot@microsoft.com>

--- src/Common/src/System.Private.CoreLib/shared/System/Reflection/ReflectionTypeLoadException.cs
+++ src/Common/src/System.Private.CoreLib/shared/System/Reflection/ReflectionTypeLoadException.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Runtime.Serialization;
+using System.Text;
 
 namespace System.Reflection
 {
@@ -37,6 +38,38 @@ namespace System.Reflection
             base.GetObjectData(info, context);
             info.AddValue("Types", null, typeof(Type[]));
             info.AddValue("Exceptions", LoaderExceptions, typeof(Exception[]));
+        }
+
+        public override string Message
+        {
+            get
+            {
+                if (LoaderExceptions.Length == 0)
+                {
+                    return base.Message;
+                }
+
+                StringBuilder text = new StringBuilder();
+                text.AppendLine(base.Message);
+                foreach (Exception e in LoaderExceptions)
+                {
+                    text.AppendLine(e.Message);
+                }
+                return text.ToString();
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder text = new StringBuilder();
+            text.AppendLine(SR.ReflectionTypeLoad_LoadFailed);
+
+            foreach (Exception e in LoaderExceptions)
+            {
+                text.AppendLine(e.ToString());
+            }
+
+            return text.ToString();
         }
 
         public Type[] Types { get; }
