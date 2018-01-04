a1d4a7a6f3a12065dc8184c54695ad19b397f4a6
wtgodbe <wigodbe@microsoft.com>
2018-01-04 11:42:40 -0800

    Add ReflectionTypeLoadException tests

---
+++ src/System.Runtime/tests/System/Reflection/ReflectionTypeLoadExceptionTests.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Xunit;
+
+[assembly: TypeForwardedTo(typeof(string))]
+[assembly: TypeForwardedTo(typeof(TypeInForwardedAssembly))]
+
+namespace System.Reflection.Tests
+{
+    public static class ReflectionTypeLoadExceptionTests
+    {
+        [Fact]
+        public static void NullExceptionsNoNullPointerException()
+        {
+            bool foundRtleException = false;
+            try
+            {
+                Type[] Typo = new Type[1];
+                Exception[] Excepto = new Exception[1];
+                throw new ReflectionTypeLoadException(Typo, Excepto, "Null elements in Exceptions array");
+
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                foundRtleException = true;
+            }
+            Assert.True(foundRtleException);
+        }
+
+        [Fact]
+        public static void NullArgumentsNoNullPointerException()
+        {
+            bool foundRtleException = false;
+            try
+            {
+                throw new ReflectionTypeLoadException(null, null, "Null arguments");
+
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                foundRtleException = true;
+            }
+            Assert.True(foundRtleException);
+        }
+    }
+}
