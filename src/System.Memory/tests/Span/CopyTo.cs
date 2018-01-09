f3f914121504fd7d592c90444f8b141217ac435e
Atsushi Kanamori <AtsushiKan@users.noreply.github.com>
2018-01-09 14:16:17 -0800

    Consolidate the file creation and MMF file creation by passing a capacity.

--- src/System.Memory/tests/Span/CopyTo.cs
+++ src/System.Memory/tests/Span/CopyTo.cs
@@ -34,62 +34,6 @@ namespace System.SpanTests
                 return;
             }
 
-            int GuidCount = (int)(bufferSize / Unsafe.SizeOf<Guid>());
-            bool allocatedFirst = false;
-            bool allocatedSecond = false;
-            IntPtr memBlockFirst = IntPtr.Zero;
-            IntPtr memBlockSecond = IntPtr.Zero;
-
-            unsafe
-            {
-                try
-                {
-                    allocatedFirst = AllocationHelper.TryAllocNative((IntPtr)bufferSize, out memBlockFirst);
-                    allocatedSecond = AllocationHelper.TryAllocNative((IntPtr)bufferSize, out memBlockSecond);
-
-                    if (allocatedFirst && allocatedSecond)
-                    {
-                        ref Guid memoryFirst = ref Unsafe.AsRef<Guid>(memBlockFirst.ToPointer());
-                        var spanFirst = new Span<Guid>(memBlockFirst.ToPointer(), GuidCount);
-
-                        ref Guid memorySecond = ref Unsafe.AsRef<Guid>(memBlockSecond.ToPointer());
-                        var spanSecond = new Span<Guid>(memBlockSecond.ToPointer(), GuidCount);
-
-                        Guid theGuid = Guid.Parse("900DBAD9-00DB-AD90-00DB-AD900DBADBAD");
-                        for (int count = 0; count < GuidCount; ++count)
-                        {
-                            Unsafe.Add(ref memoryFirst, count) = theGuid;
-                        }
-
-                        spanFirst.CopyTo(spanSecond);
-
-                        for (int count = 0; count < GuidCount; ++count)
-                        {
-                            Guid guidfirst = Unsafe.Add(ref memoryFirst, count);
-                            Guid guidSecond = Unsafe.Add(ref memorySecond, count);
-                            Assert.Equal(guidfirst, guidSecond);
-                        }
-                    }
-                }
-                finally
-                {
-                    if (allocatedFirst)
-                        AllocationHelper.ReleaseNative(ref memBlockFirst);
-                    if (allocatedSecond)
-                        AllocationHelper.ReleaseNative(ref memBlockSecond);
-                }
-            }
-        }
-
-
-        public static void _CopyToLargeSizeTest(long bufferSize)
-        {
-            // If this test is run in a 32-bit process, the large allocation will fail.
-            if (Unsafe.SizeOf<IntPtr>() != sizeof(long))
-            {
-                return;
-            }
-
             if (AllocationHelper.TryAllocNative((IntPtr)1, out IntPtr pMemory))  // Synchronize with other big-memory tests to reduce stress on machine. The memory itself isn't used.
             {
                 try
@@ -99,48 +43,44 @@ namespace System.SpanTests
                     using (CopyToLargeSizeTestFileCleanup fileCleanup = new CopyToLargeSizeTestFileCleanup())
                     {
                         string testFilePath = fileCleanup.TestFilePath;
-                        using (FileStream fs = File.Create(testFilePath))
+                        try
                         {
-                            try
+                            long capacity = checked(bufferSize * 2);
+                            using (MemoryMappedFile memoryMappedFile = MemoryMappedFile.CreateFromFile(testFilePath, FileMode.Create, null, capacity, MemoryMappedFileAccess.ReadWrite))
+                            using (MemoryMappedViewAccessor viewAccessor = memoryMappedFile.CreateViewAccessor())
+                            using (SafeHandle viewAccessorSafeHandle = viewAccessor.SafeMemoryMappedViewHandle)
                             {
-                                fs.SetLength(checked(bufferSize * 2));
-                            }
-                            catch (IOException)
-                            {
-                                return; // Test machine does not have enough space for a 8gb file.
+                                unsafe
+                                {
+                                    byte* memBlockFirst = (byte*)viewAccessorSafeHandle.DangerousGetHandle();
+                                    byte* memBlockSecond = memBlockFirst + bufferSize;
+
+                                    ref Guid memoryFirst = ref Unsafe.AsRef<Guid>(memBlockFirst);
+                                    var spanFirst = new Span<Guid>(memBlockFirst, GuidCount);
+
+                                    ref Guid memorySecond = ref Unsafe.AsRef<Guid>(memBlockSecond);
+                                    var spanSecond = new Span<Guid>(memBlockSecond, GuidCount);
+
+                                    Guid theGuid = Guid.Parse("900DBAD9-00DB-AD90-00DB-AD900DBADBAD");
+                                    for (int count = 0; count < GuidCount; ++count)
+                                    {
+                                        Unsafe.Add(ref memoryFirst, count) = theGuid;
+                                    }
+
+                                    spanFirst.CopyTo(spanSecond);
+
+                                    for (int count = 0; count < GuidCount; ++count)
+                                    {
+                                        Guid guidfirst = Unsafe.Add(ref memoryFirst, count);
+                                        Guid guidSecond = Unsafe.Add(ref memorySecond, count);
+                                        Assert.Equal(guidfirst, guidSecond);
+                                    }
+                                }
                             }
                         }
-
-                        using (MemoryMappedFile memoryMappedFile = MemoryMappedFile.CreateFromFile(testFilePath))
-                        using (MemoryMappedViewAccessor viewAccessor = memoryMappedFile.CreateViewAccessor())
-                        using (SafeHandle viewAccessorSafeHandle = viewAccessor.SafeMemoryMappedViewHandle)
+                        catch (IOException)
                         {
-                            unsafe
-                            {
-                                byte* memBlockFirst = (byte*)viewAccessorSafeHandle.DangerousGetHandle();
-                                byte* memBlockSecond = memBlockFirst + bufferSize;
-
-                                ref Guid memoryFirst = ref Unsafe.AsRef<Guid>(memBlockFirst);
-                                var spanFirst = new Span<Guid>(memBlockFirst, GuidCount);
-
-                                ref Guid memorySecond = ref Unsafe.AsRef<Guid>(memBlockSecond);
-                                var spanSecond = new Span<Guid>(memBlockSecond, GuidCount);
-
-                                Guid theGuid = Guid.Parse("900DBAD9-00DB-AD90-00DB-AD900DBADBAD");
-                                for (int count = 0; count < GuidCount; ++count)
-                                {
-                                    Unsafe.Add(ref memoryFirst, count) = theGuid;
-                                }
-
-                                spanFirst.CopyTo(spanSecond);
-
-                                for (int count = 0; count < GuidCount; ++count)
-                                {
-                                    Guid guidfirst = Unsafe.Add(ref memoryFirst, count);
-                                    Guid guidSecond = Unsafe.Add(ref memorySecond, count);
-                                    Assert.Equal(guidfirst, guidSecond);
-                                }
-                            }
+                            return; // Test machine does not have enough space for a 8gb file.
                         }
                     }
                 }
