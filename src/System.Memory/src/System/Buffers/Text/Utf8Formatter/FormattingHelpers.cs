e30a8cca02b26b7116fc85f7d4e47bcfab1fe514
Levi Broderick <levib@microsoft.com>
2018-01-11 18:36:40 -0800

    Remove unsafe code from Guid formatter Also optimize code: on Win10 amd64 this results in +46% throughput (GUIDs formatted per second)

--- src/System.Memory/src/System/Buffers/Text/Utf8Formatter/FormattingHelpers.cs
+++ src/System.Memory/src/System/Buffers/Text/Utf8Formatter/FormattingHelpers.cs
@@ -25,15 +25,119 @@ namespace System.Buffers.Text
         // A simple lookup table for converting numbers to hex.
         private const string HexTable = "0123456789abcdef";
 
-        #region UTF-8 Helper methods
+        // TODO: Where should the below method live? Ideally it should be publicly exposed
+        // since having blittable access to structs would be convenient.
+        // Should it be on BinaryPrimitives?
 
+        /// <summary>
+        /// Given a reference to a blittable value type, returns a Span that allows
+        /// access to the binary representation of the value.
+        /// </summary>
+        /// <remarks>
+        /// No copy is performed as part of this method's logic.
+        /// This is intended to be a "safe" API even though it's implemented in terms of unsafe casts.
+        /// </remarks>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void WriteHexByte(byte value, ref byte buffer, int index)
+        public static Span<byte> GetSpanForBlittable<T>(ref T value) where T : struct
         {
-            Unsafe.Add(ref buffer, index) = (byte)HexTable[value >> 4];
-            Unsafe.Add(ref buffer, index + 1) = (byte)HexTable[value & 0xF];
+#if netstandard
+            if (SpanHelpers.IsReferenceOrContainsReferences<T>())
+            {
+                ThrowHelper.ThrowArgumentException_InvalidTypeWithPointersNotSupported(typeof(T));
+            }
+#else
+            if (RuntimeHelpers.IsReferenceOrContainsReferences<T>())
+            {
+                ThrowHelper.ThrowArgumentException_InvalidTypeWithPointersNotSupported(typeof(T));
+            }
+#endif
+            return Span<byte>.DangerousCreate(null, ref Unsafe.As<T, byte>(ref value), Unsafe.SizeOf<T>());
         }
 
+        /// <summary>
+        /// Returns the symbol contained within the standard format. If the standard format
+        /// has not been initialized, returns the provided fallback symbol.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static char GetSymbolOrDefault(in StandardFormat format, char defaultSymbol)
+        {
+            // This is equivalent to the line below, but it is written in such a way
+            // that the JIT is able to perform more optimizations.
+            //
+            // return (format.IsDefault) ? defaultSymbol : format.Symbol;
+
+            var symbol = format.Symbol;
+            if (symbol == default && format.Precision == default)
+            {
+                symbol = defaultSymbol;
+            }
+            return symbol;
+        }
+
+        #region UTF-8 Helper methods
+
+        public enum HexCasing : uint
+        {
+            // Output [ '0' .. '9' ] and [ 'A' .. 'F' ].
+            Uppercase = 0,
+
+            // Output [ '0' .. '9' ] and [ 'a' .. 'f' ].
+            // This works because values in the range [ 0x30 .. 0x39 ] ([ '0' .. '9' ])
+            // already have the 0x20 bit set, so ORing them with 0x20 is a no-op,
+            // while outputs in the range [ 0x41 .. 0x46 ] ([ 'A' .. 'F' ])
+            // don't have the 0x20 bit set, so ORing them maps to
+            // [ 0x61 .. 0x66 ] ([ 'a' .. 'f' ]), which is what we want.
+            Lowercase = 0x2020U,
+        }
+
+        // The JIT can elide bounds checks if 'startingIndex' is constant and if the caller is
+        // writing to a span of known length (or the caller has already checked the bounds of the
+        // furthest access).
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WriteHexByte(byte value, Span<byte> buffer, int startingIndex = 0, HexCasing casing = HexCasing.Uppercase)
+        {
+            // We want to pack the incoming byte into a single integer [ 0000 HHHH 0000 LLLL ],
+            // where HHHH and LLLL are the high and low nibbles of the incoming byte. Then
+            // subtract this integer from a constant minuend as shown below.
+            //
+            //   [ 1000 1001 1000 1001 ]
+            // - [ 0000 HHHH 0000 LLLL ]
+            // =========================
+            //   [ *YYY **** *ZZZ **** ]
+            //
+            // The end result of this is that YYY is 0b000 if HHHH <= 9, and YYY is 0b111 if HHHH >= 10.
+            // Similarly, ZZZ is 0b000 if LLLL <= 9, and ZZZ is 0b111 if LLLL >= 10.
+            // (We don't care about the value of asterisked bits.)
+            //
+            // To turn a nibble in the range [ 0 .. 9 ] into hex, we calculate hex := nibble + 48 (ascii '0').
+            // To turn a nibble in the range [ 10 .. 15 ] into hex, we calculate hex := nibble - 10 + 65 (ascii 'A').
+            //                                                                => hex := nibble + 55.
+            // The difference in the starting ASCII offset is (55 - 48) = 7, depending on whether the nibble is <= 9 or >= 10.
+            // Since 7 is 0b111, this conveniently matches the YYY or ZZZ value computed during the earlier subtraction.
+
+            // The commented out code below is code that directly implements the logic described above.
+
+            //uint packedOriginalValues = (((uint)value & 0xF0U) << 4) + ((uint)value & 0x0FU);
+            //uint difference = 0x8989U - packedOriginalValues;
+            //uint add7Mask = (difference & 0x7070U) >> 4; // line YYY and ZZZ back up with the packed values
+            //uint packedResult = packedOriginalValues + add7Mask + 0x3030U /* ascii '0' */;
+
+            // The code below is equivalent to the commented out code above but has been tweaked
+            // to allow codegen to make some extra optimizations.
+
+            uint difference = (((uint)value & 0xF0U) << 4) + ((uint)value & 0x0FU) - 0x8989U;
+            uint packedResult = ((((uint)(-(int)difference) & 0x7070U) >> 4) + difference + 0xB9B9U) | (uint)casing;
+
+            // The low byte of the packed result contains the hex representation of the incoming byte's low nibble.
+            // The adjacent byte of the packed result contains the hex representation of the incoming byte's high nibble.
+
+            // Finally, write to the output buffer starting with the *highest* index so that codegen can
+            // elide all but the first bounds check. (This only works if 'startingIndex' is a compile-time constant.)
+
+            buffer[startingIndex + 1] = (byte)(packedResult);
+            buffer[startingIndex] = (byte)(packedResult >> 8);
+        }
+        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void WriteFractionDigits(long value, int digitCount, ref byte buffer, int index)
         {
@@ -229,11 +333,11 @@ namespace System.Buffers.Text
                 digits += 5;
             }
 
-            if (value < 10) 
-            { 
+            if (value < 10)
+            {
                 // no-op
             }
-            else if (value < 100) 
+            else if (value < 100)
             {
                 digits += 1;
             }
