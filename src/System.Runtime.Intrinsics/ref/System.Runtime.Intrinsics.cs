2cdb8eb02ed7f8de8a6c7336763e727d287c1599
Tanner Gooding <tagoo@outlook.com>
2018-01-18 15:58:40 -0800

    Adding the `Vector64<T>` type to the S.R.Intrinsics assembly

--- src/System.Runtime.Intrinsics/ref/System.Runtime.Intrinsics.cs
+++ src/System.Runtime.Intrinsics/ref/System.Runtime.Intrinsics.cs
@@ -9,12 +9,22 @@ using System.Runtime.InteropServices;
 
 namespace System.Runtime.Intrinsics
 {
+    [StructLayout(LayoutKind.Sequential, Size = 8)]
+    public struct Vector64<T> where T : struct
+    {
+         private int _dummy;
+    }
     [StructLayout(LayoutKind.Sequential, Size = 16)]
-    public struct Vector128<T> where T : struct {}
+    public struct Vector128<T> where T : struct
+    {
+         private int _dummy;
+    }
     [StructLayout(LayoutKind.Sequential, Size = 32)]
-    public struct Vector256<T> where T : struct {}
+    public struct Vector256<T> where T : struct
+    {
+         private int _dummy;
+    }
 }
-
 namespace System.Runtime.Intrinsics.X86
 {
     public static class Aes 
