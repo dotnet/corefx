37c3794eafe2a9e455f6aa92bdd37167243c95d4
ahsonkhan <ahson_ahmedk@yahoo.com>
2018-01-19 12:01:55 -0800

    Remove some redundant tests and the big memory test.

--- src/System.Memory/tests/Span/Reverse.cs
+++ src/System.Memory/tests/Span/Reverse.cs
@@ -43,16 +43,6 @@ namespace System.SpanTests
         [Fact]
         public static void ReverseByte()
         {
-            byte[] actual = { 1, 2, 3, 4, 5 };
-            byte[] expected = { 5, 4, 3, 2, 1 };
-            Span<byte> span = actual;
-            span.Reverse();
-            Assert.Equal<byte>(expected, actual);
-        }
-
-        [Fact]
-        public static void ReverseByteLonger()
-        {
             for(int length = 0; length < byte.MaxValue; length++)
             {
                 var actual = new byte[length];
@@ -108,34 +98,6 @@ namespace System.SpanTests
         }
 
         [Fact]
-        public unsafe static void ReverseByteUnalignedFixed()
-        {
-            const int length = 32;
-            const int offset = 1;
-            var actualFull = new byte[length];
-            for (int i = 0; i < length; i++)
-            {
-                actualFull[i] = (byte)i;
-            }
-            byte[] expectedFull = new byte[length];
-            Array.Copy(actualFull, expectedFull, length);
-            Array.Reverse(expectedFull, offset, length - offset - 1);
-
-            var expectedSpan = new Span<byte>(expectedFull, offset, length - offset - 1);
-            fixed (byte* p = actualFull)
-            {
-                var actualSpan = new Span<byte>(p + offset, length - offset - 1);
-                actualSpan.Reverse();
-
-                byte[] actual = actualSpan.ToArray();
-                byte[] expected = expectedSpan.ToArray();
-                Assert.Equal<byte>(expected, actual);
-                Assert.Equal(expectedFull[0], actualFull[0]);
-                Assert.Equal(expectedFull[length - 1], actualFull[length - 1]);
-            }
-        }
-
-        [Fact]
         public static void ReverseIntPtrOffset()
         {
             const int length = 32;
@@ -162,17 +124,6 @@ namespace System.SpanTests
 
         [Fact]
         public static void ReverseValueTypeWithoutReferences()
-        {
-            int[] actual = { 1, 2, 3 };
-            int[] expected = { 3, 2, 1 };
-
-            var span = new Span<int>(actual);
-            span.Reverse();
-            Assert.Equal<int>(expected, actual);
-        }
-
-        [Fact]
-        public static void ReverseValueTypeWithoutReferencesLonger()
         {
             const int length = 2048;
             int[] actual = new int[length];
@@ -210,8 +161,15 @@ namespace System.SpanTests
         [Fact]
         public static void ReverseReferenceType()
         {
-            string[] actual = { "a1", "b2", "c3" };
-            string[] expected = { "c3", "b2", "a1" };
+            const int length = 2048;
+            string[] actual = new string[length];
+            for (int i = 0; i < length; i++)
+            {
+                actual[i] = i.ToString();
+            }
+            string[] expected = new string[length];
+            Array.Copy(actual, expected, length);
+            Array.Reverse(expected);
 
             var span = new Span<string>(actual);
             span.Reverse();
@@ -226,24 +184,6 @@ namespace System.SpanTests
 
             var span = new Span<string>(actual);
             span.Reverse();
-            span.Reverse();
-            Assert.Equal<string>(expected, actual);
-        }
-
-        [Fact]
-        public static void ReverseReferenceTypeLonger()
-        {
-            const int length = 2048;
-            string[] actual = new string[length];
-            for (int i = 0; i < length; i++)
-            {
-                actual[i] = i.ToString();
-            }
-            string[] expected = new string[length];
-            Array.Copy(actual, expected, length);
-            Array.Reverse(expected);
-
-            var span = new Span<string>(actual);
             span.Reverse();
             Assert.Equal<string>(expected, actual);
         }
@@ -274,58 +214,6 @@ namespace System.SpanTests
             var span = new Span<TestValueTypeWithReference>(actual);
             span.Reverse();
             Assert.Equal<TestValueTypeWithReference>(expected, actual);
-        }
-
-        // NOTE: ReverseLongerThanUintMaxValueBytes test is constrained to run on Windows and MacOSX because it causes
-        //       problems on Linux due to the way deferred memory allocation works. On Linux, the allocation can
-        //       succeed even if there is not enough memory but then the test may get killed by the OOM killer at the
-        //       time the memory is accessed which triggers the full memory allocation.
-        [Fact]
-        [OuterLoop]
-        [PlatformSpecific(TestPlatforms.Windows | TestPlatforms.OSX)]
-        unsafe static void ReverseLongerThanUintMaxValueBytes()
-        {
-            if (sizeof(IntPtr) == sizeof(long))
-            {
-                // Arrange
-                IntPtr bytes = (IntPtr)(((long)int.MaxValue) * sizeof(int));
-                int length = (int)(((long)bytes) / sizeof(int));
-
-                if (!AllocationHelper.TryAllocNative(bytes, out IntPtr memory))
-                {
-                    Console.WriteLine($"Span.Reverse test {nameof(ReverseLongerThanUintMaxValueBytes)} skipped (could not alloc memory).");
-                    return;
-                }
-
-                try
-                {
-                    ref int data = ref Unsafe.AsRef<int>(memory.ToPointer());
-
-                    for (int i = 0; i < length; i++)
-                    {
-                        Unsafe.Add(ref data, i) = i;
-                    }
-
-                    Span<int> span = new Span<int>(memory.ToPointer(), length);
-
-                    // Act
-                    span.Reverse();
-
-                    // Assert using custom code for perf and to avoid allocating extra memory
-                    for (int i = 0; i < length; i++)
-                    {
-                        var actual = Unsafe.Add(ref data, i);
-                        if (actual != length - i - 1)
-                        {
-                            Assert.Equal(length - i - 1, actual);
-                        }
-                    }
-                }
-                finally
-                {
-                    AllocationHelper.ReleaseNative(ref memory);
-                }
-            }
         }
     }
 }
