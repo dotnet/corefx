979777b790033add1462e5f15a46c3ca44b2c90e
Krzysztof Wicher <mordotymoja@gmail.com>
2018-01-18 16:58:15 -0800

    Do not throw IndexOutOfBounds when AM/PM designator is empty (#15904)
    * do not throw IndexOutOfBounds when AM/PM designator is empty
    
    * store am/pm designator in local
    
    Signed-off-by: dotnet-bot-corefx-mirror <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/Globalization/DateTimeParse.cs
+++ src/Common/src/CoreLib/System/Globalization/DateTimeParse.cs
@@ -3607,15 +3607,18 @@ new DS[] { DS.ERROR, DS.TX_NNN,  DS.TX_NNN,  DS.TX_NNN,  DS.ERROR,   DS.ERROR,  
             // change the code below.
             if (str.GetNext())
             {
-                if (str.GetChar() == dtfi.AMDesignator[0])
+                string amDesignator = dtfi.AMDesignator;
+                if (amDesignator.Length > 0 && str.GetChar() == amDesignator[0])
                 {
                     result = TM.AM;
-                    return (true);
+                    return true;
                 }
-                if (str.GetChar() == dtfi.PMDesignator[0])
+
+                string pmDesignator = dtfi.PMDesignator;
+                if (pmDesignator.Length > 0 && str.GetChar() == pmDesignator[0])
                 {
                     result = TM.PM;
-                    return (true);
+                    return true;
                 }
             }
             return false;
