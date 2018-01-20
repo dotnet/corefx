509e2ea42eb17f7f17b73e118bc84f1446d3b1dc
Shimmy Weitzhandler <weitzhandlerATgmailDOTcom>
2018-01-20 21:11:00 +0200

    Removed unused namespace.

--- src/System.ObjectModel/src/System/Collections/ObjectModel/ObservableCollection.cs
+++ src/System.ObjectModel/src/System/Collections/ObjectModel/ObservableCollection.cs
@@ -1,8 +1,6 @@
-using System.Security;
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
