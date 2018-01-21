964bb725e66854d16be82f9f7de2c38fe2c32876
Jon Hanna <jon@hackcraft.net>
2018-01-21 00:38:27 +0000

    Move init into ctor and make fields readonly

--- src/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/Semantics/SubstitutionContext.cs
+++ src/Microsoft.CSharp/src/Microsoft/CSharp/RuntimeBinder/Semantics/SubstitutionContext.cs
@@ -16,53 +16,13 @@ namespace Microsoft.CSharp.RuntimeBinder.Semantics
 
     internal sealed class SubstContext
     {
-        public CType[] prgtypeCls;
-        public int ctypeCls;
-        public CType[] prgtypeMeth;
-        public int ctypeMeth;
-        public SubstTypeFlags grfst;
+        public readonly CType[] prgtypeCls;
+        public readonly int ctypeCls;
+        public readonly CType[] prgtypeMeth;
+        public readonly int ctypeMeth;
+        public readonly SubstTypeFlags grfst;
 
         public SubstContext(TypeArray typeArgsCls, TypeArray typeArgsMeth, SubstTypeFlags grfst)
-        {
-            Init(typeArgsCls, typeArgsMeth, grfst);
-        }
-
-        public SubstContext(AggregateType type)
-            : this(type, null, SubstTypeFlags.NormNone)
-        {
-        }
-
-        public SubstContext(AggregateType type, TypeArray typeArgsMeth)
-            : this(type, typeArgsMeth, SubstTypeFlags.NormNone)
-        {
-        }
-
-        private SubstContext(AggregateType type, TypeArray typeArgsMeth, SubstTypeFlags grfst)
-        {
-            Init(type?.TypeArgsAll, typeArgsMeth, grfst);
-        }
-
-        public SubstContext(CType[] prgtypeCls, int ctypeCls, CType[] prgtypeMeth, int ctypeMeth)
-            : this(prgtypeCls, ctypeCls, prgtypeMeth, ctypeMeth, SubstTypeFlags.NormNone)
-        {
-        }
-
-        private SubstContext(CType[] prgtypeCls, int ctypeCls, CType[] prgtypeMeth, int ctypeMeth, SubstTypeFlags grfst)
-        {
-            this.prgtypeCls = prgtypeCls;
-            this.ctypeCls = ctypeCls;
-            this.prgtypeMeth = prgtypeMeth;
-            this.ctypeMeth = ctypeMeth;
-            this.grfst = grfst;
-        }
-
-        public bool FNop()
-        {
-            return 0 == ctypeCls && 0 == ctypeMeth;
-        }
-
-        // Initializes a substitution context. Returns false iff no substitutions will ever be performed.
-        private void Init(TypeArray typeArgsCls, TypeArray typeArgsMeth, SubstTypeFlags grfst)
         {
             if (typeArgsCls != null)
             {
@@ -89,6 +49,40 @@ namespace Microsoft.CSharp.RuntimeBinder.Semantics
             }
 
             this.grfst = grfst;
+        }
+
+        public SubstContext(AggregateType type)
+            : this(type, null, SubstTypeFlags.NormNone)
+        {
+        }
+
+        public SubstContext(AggregateType type, TypeArray typeArgsMeth)
+            : this(type, typeArgsMeth, SubstTypeFlags.NormNone)
+        {
+        }
+
+        private SubstContext(AggregateType type, TypeArray typeArgsMeth, SubstTypeFlags grfst)
+            : this(type?.TypeArgsAll, typeArgsMeth, grfst)
+        {
+        }
+
+        public SubstContext(CType[] prgtypeCls, int ctypeCls, CType[] prgtypeMeth, int ctypeMeth)
+            : this(prgtypeCls, ctypeCls, prgtypeMeth, ctypeMeth, SubstTypeFlags.NormNone)
+        {
+        }
+
+        private SubstContext(CType[] prgtypeCls, int ctypeCls, CType[] prgtypeMeth, int ctypeMeth, SubstTypeFlags grfst)
+        {
+            this.prgtypeCls = prgtypeCls;
+            this.ctypeCls = ctypeCls;
+            this.prgtypeMeth = prgtypeMeth;
+            this.ctypeMeth = ctypeMeth;
+            this.grfst = grfst;
+        }
+
+        public bool FNop()
+        {
+            return 0 == ctypeCls && 0 == ctypeMeth;
         }
     }
 }
