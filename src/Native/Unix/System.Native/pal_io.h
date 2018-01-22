09b3cc02d85c5c963af50567cb89b6d67beebc49
Dan Moseley <danmose@microsoft.com>
2018-01-22 11:00:13 -0800

    Remove old stat struct (#26383)
    * Remove old struct; rename as possible to remove 2

--- src/Native/Unix/System.Native/pal_io.h
+++ src/Native/Unix/System.Native/pal_io.h
@@ -26,24 +26,6 @@ struct FileStatus
     uint32_t Gid;      // group ID of owner
     int64_t Size;      // total size, in bytes
     int64_t ATime;     // time of last access
-    int64_t MTime;     // time of last modification
-    int64_t CTime;     // time of last status change
-    int64_t BirthTime; // time the file was created
-    int64_t Dev;       // ID of the device containing the file
-    int64_t Ino;       // inode number of the file
-};
-
-/**
- * File status returned by Stat or FStat.
- */
-struct FileStatus2
-{
-    int32_t Flags;     // flags for testing if some members are present (see FileStatusFlags)
-    int32_t Mode;      // file mode (see S_I* constants above for bit values)
-    uint32_t Uid;      // user ID of owner
-    uint32_t Gid;      // group ID of owner
-    int64_t Size;      // total size, in bytes
-    int64_t ATime;     // time of last access
     int64_t ATimeNsec; //     nanosecond part
     int64_t MTime;     // time of last modification
     int64_t MTimeNsec; //     nanosecond part
