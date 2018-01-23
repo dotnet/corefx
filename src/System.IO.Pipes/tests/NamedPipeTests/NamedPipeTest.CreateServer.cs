47bf74ef17a7d1f5eac7019eec9cf0ae17b114f1
Santiago Fernandez <safern@microsoft.com>
2018-01-23 10:14:05 -0800

    PR Feedback

--- src/System.IO.Pipes/tests/NamedPipeTests/NamedPipeTest.CreateServer.cs
+++ src/System.IO.Pipes/tests/NamedPipeTests/NamedPipeTest.CreateServer.cs
@@ -3,8 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.Win32.SafeHandles;
-using System.Security.Principal;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace System.IO.Pipes.Tests
