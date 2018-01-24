956aefebe8ad325a7d4357480bfa327c91d8eb8c
artkpv <w1ld@inbox.ru>
2018-01-24 20:41:51 +0300

    Command line tokenization reconciling (#25857)
    * Command line tokenization reconciling
    
    Using PasteArguments from Environment to avoid code duplication while
    getting command line.
    
    Issue #21267. Command Line tokenization is done all over the place and inconsistently.
    
    * Fixing no-op bug. Clean up (style)
    
    * Paste arguments cross platform fix: avoiding arg[0] check on Unix. refs #25857
    
    * Fixing code style. Adding unit tests. Refs #25857
    
    * (UWP CoreCLR x64 build fix)

---
+++ src/Common/tests/Tests/System/PasteArgumentsTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Xunit;
+
+namespace Tests.System
+{
+    public class PasteArgumentsTests
+    {
+        [Theory]
+        [InlineData(@"app.exe arg1 arg2", new[] {"app.exe", "arg1", "arg2"})]
+        [InlineData(@"""app name.exe"" arg1 arg2", new[] {"app name.exe", "arg1", "arg2"})]
+        [InlineData(@"app.exe \\ arg2", new[] {"app.exe", @"\\", "arg2"})] 
+        [InlineData(@"app.exe ""\"""" arg2", new[] {"app.exe", @"""", "arg2"})]  // literal double quotation mark character
+        [InlineData(@"app.exe ""\\\"""" arg2", new[] {"app.exe", @"\""", "arg2"})]    // 2N+1 backslashes before quote rule 
+        [InlineData(@"app.exe ""\\\\\"""" arg2", new[] {"app.exe", @"\\""", "arg2"})]  // 2N backslashes before quote rule 
+        public void Pastes(string pasteExpected, string[] arguments)
+        {
+            Assert.Equal(pasteExpected, PasteArguments.Paste(arguments, pasteFirstArgumentUsingArgV0Rules: true));
+        }
+
+        [Theory]
+        [InlineData(@"""dir/app\""name.exe""", new[] {@"dir/app""name.exe"})]  // no throwing on quotes, escaping quotes
+        [InlineData(@"""dir/app\\\""name.exe""", new[] {@"dir/app\""name.exe"})]  // escaping a backslash
+        [InlineData(@"""dir/app\\\\\""name.exe""", new[] {@"dir/app\\""name.exe"})]  // escaping backslashes
+        [PlatformSpecific(TestPlatforms.AnyUnix)]
+        public void Paste_Argv0Rules_Ignored_onUnix(string pasteExpected, string[] arguments)
+        {
+            Assert.Equal(pasteExpected, PasteArguments.Paste(arguments, pasteFirstArgumentUsingArgV0Rules: true));
+        }
+
+        [Theory]
+        [InlineData(@"dir/app""name.exe")]  // throws
+        [InlineData(@"dir/app\""name.exe")]  // throws and ignores a backslash
+        [InlineData(@"dir/app\\""name.exe")]  // throws and ignores backslashes
+        [PlatformSpecific(TestPlatforms.Windows)]
+        public void Paste_Argv0Rules_ThrowsIfQuotes_OnWindows(string argv0)
+        {
+            Assert.Throws<ApplicationException>(() => PasteArguments.Paste(new []{argv0}, pasteFirstArgumentUsingArgV0Rules: true));
+        }
+    }
+}
