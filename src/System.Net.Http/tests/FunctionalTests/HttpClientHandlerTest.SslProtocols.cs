838322a4a0557e080c7c01a6f517adc513218dda
Paulo Janotti <pauloja@microsoft.com>
2018-01-31 13:16:08 -0800

    WIP: Investigating test failures 26186
    This is expected to fail at least in some Windows 7 configurations.

--- src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.SslProtocols.cs
+++ src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.SslProtocols.cs
@@ -85,7 +85,7 @@ namespace System.Net.Http.Functional.Tests
             }
         }
 
-        [OuterLoop] // TODO: Issue #11345
+        // Temporarily in inner loop for CI run [OuterLoop] // TODO: Issue #11345
         [Theory]
         [InlineData(SslProtocols.Tls, false)]
         [InlineData(SslProtocols.Tls, true)]
@@ -97,12 +97,6 @@ namespace System.Net.Http.Functional.Tests
         {
             if (!BackendSupportsSslConfiguration)
             {
-                return;
-            }
-
-            if (UseManagedHandler)
-            {
-                // TODO #26186: The managed handler is failing on some OSes.
                 return;
             }
 
@@ -136,17 +130,11 @@ namespace System.Net.Http.Functional.Tests
         // instead of local ones.  We're keeping it for now (as outerloop) because it helps
         // to validate against another SSL implementation that what we mean by a particular
         // TLS version matches that other implementation.
-        [OuterLoop("Avoid www.ssllabs.com dependency in innerloop.")]
+        // Temporarily in inner loop for CI run [OuterLoop("Avoid www.ssllabs.com dependency in innerloop.")]
         [Theory]
         [MemberData(nameof(SupportedSSLVersionServers))]
         public async Task GetAsync_SupportedSSLVersion_Succeeds(SslProtocols sslProtocols, string url)
         {
-            if (UseManagedHandler)
-            {
-                // TODO #26186: The managed handler is failing on some OSes.
-                return;
-            }
-
             using (HttpClientHandler handler = CreateHttpClientHandler())
             {
                 if (PlatformDetection.IsRedHatFamily7)
