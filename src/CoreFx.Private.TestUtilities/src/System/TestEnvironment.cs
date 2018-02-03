9e53aee3d6400434be708191f01b48e8659b558d
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2018-02-02 17:17:43 -0800

    Enable Stress mode environment variable
    To write any stress test, you may mark the stress test case with one of the attributes:
    
    [ConditionalFact(typeof(TestEnvironment), nameof(TestEnvironment.IsStressModeEnabled))]
    [ConditionalTheory(typeof(TestEnvironment), nameof(TestEnvironment.IsStressModeEnabled))]
    
    and set the value of the environment variable COREFX_STRESS to 1 or true

---
+++ src/CoreFx.Private.TestUtilities/src/System/TestEnvironment.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+using Xunit;
+
+namespace System
+{
+    public static partial class TestEnvironment
+    {
+        /// <summary>
+        /// Check if the stress enabling mode environment variable is set.
+        /// </summary>
+        /// <returns> true if the environment variable set to '1' or 'true'. returns false otherwise</returns>
+        public static bool IsStressModeEnabled
+        {
+            get
+            {
+                string value = Environment.GetEnvironmentVariable("COREFX_STRESS");
+                return value != null && (value == "1" || value.Equals("true", StringComparison.OrdinalIgnoreCase));
+            }
+        }
+    }
+}
