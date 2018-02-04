d0f8c02db38d6f6ccf56312e9de0aa882e1e0ffb
danmosemsft <danmose@microsoft.com>
2018-02-03 20:40:05 -0800

    Negative test

--- src/System.Text.RegularExpressions/tests/Regex.Groups.Tests.cs
+++ src/System.Text.RegularExpressions/tests/Regex.Groups.Tests.cs
@@ -396,6 +396,7 @@ namespace System.Text.RegularExpressions.Tests
             yield return new object[] { @"(cat)(\cz*)(dog)", "asdlkcat\u001adogiwod", RegexOptions.None, new string[] { "cat\u001adog", "cat", "\u001a", "dog" } };
 
             yield return new object[] { @"(cat)(\c[*)(dog)", "asdlkcat\u001bdogiwod", RegexOptions.None, new string[] { "cat\u001bdog", "cat", "\u001b", "dog" } };
+            yield return new object[] { @"(cat)(\c[*)(dog)", "asdlkcat\u001Bdogiwod", RegexOptions.None, new string[] { "cat\u001Bdog", "cat", "\u001B", "dog" } };
 
             // Atomic Zero-Width Assertions \A \Z \z \G \b \B
             //\A
