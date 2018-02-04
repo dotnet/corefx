af18a5fe0d4aab25d8fcb6900fcffa8c99befb60
Kassem Sandarusi <kassemsandarusi@gmail.com>
2018-02-04 15:26:16 -0600

    TypeDefinition.IsNested Property (#25963)
    * Adding API TypeDefinition.IsNested. Closes #5377
    
    * Adding related tests.
    
    * fixing backward asserts.
    
    * Switching to expression-bodied syntax.
    
    * Adding doc comment. Switching to an assembly that actually has nested types.
    
    * Fixing xml doc comment to align with msdn comment style.
    
    * Update TypeDefinition.cs
    
    Fixing bad grammar in comments.

---
+++ src/System.Reflection.Metadata/tests/Metadata/TypeSystem/TypeDefinitionTests.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace System.Reflection.Metadata.Tests
+{
+    public class TypeDefinitionTests
+    {
+        [Fact]
+        public void ValidateTypeDefinitionIsNestedNoProjection()
+        {
+            var reader = MetadataReaderTests.GetMetadataReader(Namespace.NamespaceTests, options: MetadataReaderOptions.None);
+
+            foreach (var typeDefHandle in reader.TypeDefinitions)
+            {
+                var typeDef = reader.GetTypeDefinition(typeDefHandle);
+
+                Assert.Equal(typeDef.Attributes.IsNested(), typeDef.IsNested);
+            }
+        }
+
+        [Fact]
+        public void ValidateTypeDefinitionIsNestedWindowsProjection()
+        {
+            var reader = MetadataReaderTests.GetMetadataReader(Namespace.NamespaceTests, options: MetadataReaderOptions.ApplyWindowsRuntimeProjections);
+
+            foreach (var typeDefHandle in reader.TypeDefinitions)
+            {
+                var typeDef = reader.GetTypeDefinition(typeDefHandle);
+
+                Assert.Equal(typeDef.Attributes.IsNested(), typeDef.IsNested);
+            }
+        }
+    }
+}
