473f5b8826091922746636e0b7759a30cc689b4e
Max Kerr <rikerr@microsoft.com>
2018-02-05 10:06:40 -0800

    Add a fix for issue #26426 and a rough test that should probably be improved and moved.

--- src/System.Net.Http.WinHttpHandler/src/System/Net/Http/WinHttpRequestCallback.cs
+++ src/System.Net.Http.WinHttpHandler/src/System/Net/Http/WinHttpRequestCallback.cs
@@ -214,6 +214,10 @@ namespace System.Net.Http
             {
                 state.ServerCredentials = null;
             }
+
+            // Similarly, we need to clear any Auth headers that were added to the request manually or
+            // through the default headers.
+            //ResetAuthRequestHeaders(state);
         }
         
         private static void OnRequestSendingRequest(WinHttpRequestState state)
@@ -402,6 +406,27 @@ namespace System.Net.Http
                         "OnRequestError: Result (" + asyncResult.dwResult + ") is not expected.",
                         "Error code: " + asyncResult.dwError + " (" + innerException.Message + ")");
                     break;
+            }
+        }
+
+        private static void ResetAuthRequestHeaders(WinHttpRequestState state)
+        {
+            const string AuthHeaderNameWithColon = "Authorization:";
+            SafeWinHttpHandle requestHandle = state.RequestHandle;
+            //state.RequestMessage.Headers.Authorization = null;
+            
+            // Clear auth headers.
+            if (!Interop.WinHttp.WinHttpAddRequestHeaders(
+                requestHandle,
+                AuthHeaderNameWithColon,
+                (uint)AuthHeaderNameWithColon.Length,
+                Interop.WinHttp.WINHTTP_ADDREQ_FLAG_REPLACE))
+            {
+                int lastError = Marshal.GetLastWin32Error();
+                if (lastError != Interop.WinHttp.ERROR_WINHTTP_HEADER_NOT_FOUND)
+                {
+                    throw WinHttpException.CreateExceptionUsingError(lastError);
+                }
             }
         }
     }
--- src/System.Net.Http.WinHttpHandler/tests/FunctionalTests/WinHttpHandlerTest.cs
+++ src/System.Net.Http.WinHttpHandler/tests/FunctionalTests/WinHttpHandlerTest.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Net.Test.Common;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Text;
 
 using Xunit;
 using Xunit.Abstractions;
@@ -45,6 +46,26 @@ namespace System.Net.Http.WinHttpHandlerFunctional.Tests
                 _output.WriteLine(responseContent);
             }
         }
+
+        /*[Fact]
+        public void RedirectClearsAuth()
+        {
+            var output = Fetch(new Uri("http://httpbin.org//redirect-to?url=http://httpbin.org/get")).Result;
+            Console.Write(output);
+            Assert.False(output.Contains(Convert.ToBase64String(Encoding.UTF8.GetBytes("testaccount:notarealpassword"))));
+        }
+
+        static public async Task<string> Fetch(Uri dropUri)
+        {
+            using (var client = new HttpClient())
+            {
+                var base64EncodedString = Convert.ToBase64String(Encoding.UTF8.GetBytes("testaccount:notarealpassword"));
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64EncodedString);
+                var manifestresponse = await client.GetAsync(dropUri);
+                manifestresponse.EnsureSuccessStatusCode();
+                return await manifestresponse.Content.ReadAsStringAsync();
+            }
+        }*/
 
         [OuterLoop] // TODO: Issue #11345
         [Theory]
