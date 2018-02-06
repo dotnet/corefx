03ac3c10ec4f0289ddc3ab36c5b04058c3c41ca7
Caesar Chen <shizchen@microsoft.com>
2018-02-06 15:37:03 -0800

    add one missing data

--- src/System.Net.Http/tests/FunctionalTests/HttpProtocolTests.cs
+++ src/System.Net.Http/tests/FunctionalTests/HttpProtocolTests.cs
@@ -442,6 +442,7 @@ namespace System.Net.Http.Functional.Tests
                 yield return "HTTP/1.1 200 O\nK";
                 yield return "HTTP/1.1 200OK";
                 yield return "HTTP/1.1 20c";
+                yield return "HTTP/1.1 23";
                 yield return "HTTP/1.1 2bc";
             }
 
