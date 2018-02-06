4eec7c1e365f49e2aa1839f52862de5a5795bd13
Brian Robbins <brianrob@microsoft.com>
2018-02-05 16:15:55 -0800

    Remove FEATURE_ACTIVITYSAMPLING and associated code. (dotnet/coreclr#16185)
    Signed-off-by: dotnet-bot-corefx-mirror <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/Diagnostics/Tracing/EventProvider.cs
+++ src/Common/src/CoreLib/System/Diagnostics/Tracing/EventProvider.cs
@@ -259,12 +259,6 @@ namespace System.Diagnostics.Tracing
                     m_anyKeywordMask = anyKeyword;
                     m_allKeywordMask = allKeyword;
 
-                    // ES_SESSION_INFO is a marker for additional places we #ifdeffed out to remove
-                    // references to EnumerateTraceGuidsEx.  This symbol is actually not used because
-                    // today we use FEATURE_ACTIVITYSAMPLING to determine if this code is there or not.
-                    // However we put it in the #if so that we don't lose the fact that this feature
-                    // switch is at least partially independent of FEATURE_ACTIVITYSAMPLING
-
                     List<Tuple<SessionInfo, bool>> sessionsChanged = GetSessions();
                     foreach (var session in sessionsChanged)
                     {
