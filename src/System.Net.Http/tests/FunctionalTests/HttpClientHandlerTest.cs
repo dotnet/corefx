ea66d9aa4adb3ae5834e6fda6cb2ac63854fa008
Paulo Janotti <pauloja@microsoft.com>
2018-02-05 16:52:29 -0800

    Conditionally skip test causing assert in WSL.

--- src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.cs
+++ src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.cs
@@ -331,7 +331,7 @@ namespace System.Net.Http.Functional.Tests
 
         [ActiveIssue(22158, TargetFrameworkMonikers.Uap)]
         [OuterLoop] // TODO: Issue #11345
-        [Fact]
+        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsNotWindowsSubsystemForLinux))] // TODO: make unconditional after #26813 and #26476 are fixed
         public async Task GetAsync_IPv6LinkLocalAddressUri_Success()
         {
             using (HttpClient client = CreateHttpClient())
