8c947b77660ddb4f50b105d2292c33125cf13abd
Jan Kotas <jkotas@microsoft.com>
2018-02-05 18:29:34 -0800

    Add temporary logging to help us trace down root cause for dotnet/coreclr#16063 (#26866)

--- run.sh
+++ run.sh
@@ -15,4 +15,10 @@ __dotnet=$__toolRuntime/dotnetcli/dotnet
 
 cd $__scriptpath
 $__dotnet $__toolRuntime/run.exe $__scriptpath/config.json "$@"
-exit $?
+if [ $? -ne 0 ]; then
+    local exit=$?
+    echo "run.sh: dotnet run failed with exit code $exit"
+    exit $exit
+fi
+echo "run.sh: dotnet run succeeded"
+exit 0
