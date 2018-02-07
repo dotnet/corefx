e35507a84f817d535e37a2e7e31650b966940a46
Pavel Krymets <pavel@krymets.com>
2018-02-06 18:10:26 -0800

    Add ReadOnlySequence API (#26694)
    * Add ReadOnlyBuffer API
    
    * Add tests
    
    * Copyrights
    
    * PR Comments
    
    * Fix test
    
    * Add ToString test, remove extra members
    
    * Move more methods to extension class
    
    * Revert ref changes
    
    * Test changes
    
    * Set 1 of PR comments
    
    * Set 2 of PR comments
    
    * Set 2 of PR comments
    
    * Another set of style changes
    
    * Add tests, remove ToString
    
    * More tests
    
    * Add OM ctor back
    
    * PR Comments and coverege
    
    * More comments
    
    * Fix tests
    
    * Add owned memory back
    
    * ReadOnlySequence
    
    * Tests
    
    * Add array

---
+++ src/System.Memory/src/System/SequencePosition.cs
@@ -0,0 +1,59 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Numerics.Hashing;
+using System.ComponentModel;
+
+namespace System
+{
+    /// <summary>
+    /// Represents position in non-contiguous set of memory.
+    /// Properties of this type should not be interpreted by anything but the type that created it.
+    /// </summary>
+    public readonly struct SequencePosition : IEquatable<SequencePosition>
+    {
+        /// <summary>
+        /// Creates new <see cref="SequencePosition"/>
+        /// </summary>
+        public SequencePosition(object segment, int index)
+        {
+            Segment = segment;
+            Index = index;
+        }
+
+        /// <summary>
+        /// Segment of memory this <see cref="SequencePosition"/> points to.
+        /// </summary>
+        public object Segment { get; }
+
+        /// <summary>
+        /// Index inside segment of memory this <see cref="SequencePosition"/> points to.
+        /// </summary>
+        public int Index { get; }
+
+        /// <summary>
+        /// Returns true if left and right point at the same segment and have the same index.
+        /// </summary>
+        public static bool operator ==(SequencePosition left, SequencePosition right) => left.Index == right.Index && left.Segment == right.Segment;
+
+        /// <summary>
+        /// Returns true if left and right do not point at the same segment and have the same index.
+        /// </summary>
+        public static bool operator !=(SequencePosition left, SequencePosition right) => !(left == right);
+
+        /// <inheritdoc />
+        public bool Equals(SequencePosition position) => this == position;
+
+        /// <inheritdoc />
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool Equals(object obj) => obj is SequencePosition other && this == other;
+
+        /// <inheritdoc />
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override int GetHashCode() => HashHelpers.Combine(Segment?.GetHashCode() ?? 0, Index);
+
+        /// <inheritdoc />
+        public override string ToString() => this == default ? "(default)" : Segment == null ? Index.ToString(): $"{Segment}[{Index}]";
+    }
+}
