d7cbcdebc0db78c719a677125a3c80c6a3cf9c56
Santiago Fernandez Madero <safern@microsoft.com>
2018-02-07 18:50:42 -0600

    Add more tests

--- src/System.IO.Pipes/tests/NamedPipeTests/NamedPipeTest.CurrentUserOnly.netcoreapp.cs
+++ src/System.IO.Pipes/tests/NamedPipeTests/NamedPipeTest.CurrentUserOnly.netcoreapp.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace System.IO.Pipes.Tests
@@ -37,6 +39,50 @@ namespace System.IO.Pipes.Tests
                     client.Connect();
                 }
             }
+        }
+
+        [Fact]
+        public static void CreateMultipleServers_ConnectMultipleClients()
+        {
+            var name1 = GetUniquePipeName();
+            var name2 = GetUniquePipeName();
+            var name3 = GetUniquePipeName();
+            using (var server1 = new NamedPipeServerStream(name1, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.CurrentUserOnly))
+            using (var server2 = new NamedPipeServerStream(name2, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.CurrentUserOnly))
+            using (var server3 = new NamedPipeServerStream(name3, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.CurrentUserOnly))
+            {
+                using (var client1 = new NamedPipeClientStream(".", name1, PipeDirection.InOut, PipeOptions.CurrentUserOnly))
+                using (var client2 = new NamedPipeClientStream(".", name2, PipeDirection.InOut, PipeOptions.CurrentUserOnly))
+                using (var client3 = new NamedPipeClientStream(".", name3, PipeDirection.InOut, PipeOptions.CurrentUserOnly))
+                {
+                    client1.Connect();
+                    client2.Connect();
+                    client3.Connect();
+                }
+            }
+        }
+
+        [Fact]
+        public static void CreateMultipleServers_ConnectMultipleClients_MultipleThreads()
+        {
+            List<Task> tasks = new List<Task>();
+            for (int i = 0; i < 3; i++)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    var name = GetUniquePipeName();
+                    using (var server = new NamedPipeServerStream(name, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.CurrentUserOnly))
+                    {
+                        using (var client = new NamedPipeClientStream(".", name, PipeDirection.InOut, PipeOptions.CurrentUserOnly))
+                        {
+                            // Should not fail to connect since both, the server and client have the same owner.
+                            client.Connect();
+                        }
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
         }
     }
 }
