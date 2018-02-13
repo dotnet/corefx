12f4dacb21c0a996a4994e2616a11ce7bc02c255
Anipik <anirudhagnihotry098@gmail.com>
2018-02-13 11:34:09 -0800

    Removed duplicated Impleementatuin

--- src/System.Globalization.Extensions/src/System/Globalization/Extensions.cs
+++ src/System.Globalization.Extensions/src/System/Globalization/Extensions.cs
@@ -26,71 +26,7 @@ namespace System.Globalization
                 return StringComparer.OrdinalIgnoreCase;
             }
 
-            if ((options & CultureAwareComparer.ValidCompareMaskOffFlags) != 0)
-            {
-                throw new ArgumentException(SR.Argument_InvalidFlag, nameof(options));
-            }
-
             return new CultureAwareComparer(compareInfo, options);
-        }
-    }
-
-    internal sealed class CultureAwareComparer : StringComparer
-    {
-        internal const CompareOptions ValidCompareMaskOffFlags =
-            ~(CompareOptions.IgnoreCase | CompareOptions.IgnoreSymbols | CompareOptions.IgnoreNonSpace |
-              CompareOptions.IgnoreWidth | CompareOptions.IgnoreKanaType | CompareOptions.StringSort);
-
-        private readonly CompareInfo _compareInfo;
-        private readonly CompareOptions _options;
-
-        internal CultureAwareComparer(CompareInfo compareInfo, CompareOptions options)
-        {
-            Debug.Assert((options & ValidCompareMaskOffFlags) == 0);
-            _compareInfo = compareInfo;
-            _options = options;
-        }
-
-        public override int Compare(string x, string y)
-        {
-            if (Object.ReferenceEquals(x, y)) return 0;
-            if (x == null) return -1;
-            if (y == null) return 1;
-            return _compareInfo.Compare(x, y, _options);
-        }
-
-        public override bool Equals(string x, string y)
-        {
-            if (Object.ReferenceEquals(x, y)) return true;
-            if (x == null || y == null) return false;
-
-            return (_compareInfo.Compare(x, y, _options) == 0);
-        }
-
-        public override int GetHashCode(string obj)
-        {
-            if (obj == null)
-            {
-                throw new ArgumentNullException(nameof(obj));
-            }
-
-            // StringSort used in compare operation and not with the hashing
-            return _compareInfo.GetHashCode(obj, _options & (~CompareOptions.StringSort));
-        }
-
-        // Equals method for the comparer itself. 
-        public override bool Equals(object obj)
-        {
-            CultureAwareComparer comparer = obj as CultureAwareComparer;
-            return
-                comparer != null &&
-                _options == comparer._options &&
-                _compareInfo.Equals(comparer._compareInfo);
-        }
-
-        public override int GetHashCode()
-        {
-            return _compareInfo.GetHashCode() ^ ((int)_options & 0x7FFFFFFF);
         }
     }
 }
