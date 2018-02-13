8c9a21580cfc14d199189bca698294f4e77e3e35
Marco Rossignoli <boss@darkside.fs2pp1vdsxkehmdivj0oonl4vg.ax.internal.cloudapp.net>
2018-02-13 14:25:34 +0000

    clean GetHostByAddr unix

--- src/Native/Unix/System.Native/pal_networking.h
+++ src/Native/Unix/System.Native/pal_networking.h
@@ -299,8 +299,6 @@ struct SocketEvent
 
 int32_t SystemNative_GetHostEntryForName(const uint8_t* address, struct HostEntry* entry);
 
-//int32_t SystemNative_GetHostByAddress(const struct IPAddress* address, struct HostEntry* entry);
-
 int32_t SystemNative_GetNextIPAddress(const struct HostEntry* entry, void** addressListHandle, struct IPAddress* endPoint);
 
 void SystemNative_FreeHostEntry(struct HostEntry* entry);
