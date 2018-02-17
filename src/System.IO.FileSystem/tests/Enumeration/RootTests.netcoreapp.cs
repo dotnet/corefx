98ab83b6e8ee5d31161960e202c6ae4e137252be
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2018-02-16 19:11:34 -0800

    Fix enumerating the root directory.
    Shouldn't trim the trailing separator when at the root. On Windows that makes
    the path drive relative- which breaks most of the logic.
    
    Add a test to catch this case.

---
+++ src/System.IO.FileSystem/tests/Enumeration/RootTests.netcoreapp.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.IO.Enumeration;
+using System.Linq;
+using Xunit;
+
+namespace System.IO.Tests.Enumeration
+{
+    public class RootTests
+    {
+        private class DirectoryRecursed : FileSystemEnumerator<string>
+        {
+            public string LastDirectory { get; private set; }
+
+            public DirectoryRecursed(string directory, EnumerationOptions options)
+                : base(directory, options)
+            {
+            }
+
+            protected override string TransformEntry(ref FileSystemEntry entry)
+                => entry.ToFullPath();
+
+            protected override bool ShouldRecurseIntoEntry(ref FileSystemEntry entry)
+            {
+                LastDirectory = new string(entry.Directory); 
+                return false;
+            }
+        }
+
+        [Fact]
+        public void CanRecurseFromRoot()
+        {
+            string root = Path.GetPathRoot(Path.GetTempPath());
+            using (var recursed = new DirectoryRecursed(root, new EnumerationOptions { AttributesToSkip = FileAttributes.System, RecurseSubdirectories = true }))
+            {
+                while (recursed.MoveNext())
+                {
+                    if (recursed.LastDirectory != null)
+                    {
+                        Assert.Equal(root, recursed.LastDirectory);
+                        return;
+                    }
+
+                    // Should not get back a full path without a single separator (C:\foo.txt or /foo.txt)
+                    Assert.Equal(Path.DirectorySeparatorChar, recursed.Current.SingleOrDefault(c => c == Path.DirectorySeparatorChar));
+                }
+
+                Assert.NotNull(recursed.LastDirectory);
+            }
+        }
+    }
+}
