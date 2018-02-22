6bfdd9cd41780b277cfd011d768c59655860cbec
huanwu <huanwu@microsoft.com>
2018-02-22 13:37:53 -0800

    Catch the FileNotFoundException when to get the obsolete attribute for type.

--- src/Microsoft.XmlSerializer.Generator/src/Sgen.cs
+++ src/Microsoft.XmlSerializer.Generator/src/Sgen.cs
@@ -232,21 +232,33 @@ namespace Microsoft.XmlSerializer.Generator
             {
                 Type type = types[i];
 
-                if (type != null)
+                try
                 {
-                    bool isObsolete = false;
-                    object[] obsoleteAttributes = type.GetCustomAttributes(typeof(ObsoleteAttribute), false);
-                    foreach (object attribute in obsoleteAttributes)
+                    if (type != null)
                     {
-                        if (((ObsoleteAttribute)attribute).IsError)
+                        bool isObsolete = false;
+                        object[] obsoleteAttributes = type.GetCustomAttributes(typeof(ObsoleteAttribute), false);
+                        foreach (object attribute in obsoleteAttributes)
                         {
-                            isObsolete = true;
-                            break;
+                            if (((ObsoleteAttribute)attribute).IsError)
+                            {
+                                isObsolete = true;
+                                break;
+                            }
+                        }
+
+                        if (isObsolete)
+                        {
+                            continue;
                         }
                     }
-
-                    if (isObsolete)
+                }
+                catch (FileNotFoundException e)
+                {
+                    if (warnings)
                     {
+                        Console.Out.WriteLine(FormatMessage(parsableerrors, true, SR.Format(SR.InfoIgnoreType, type.FullName)));
+                        WriteWarning(e, parsableerrors);
                         continue;
                     }
                 }
