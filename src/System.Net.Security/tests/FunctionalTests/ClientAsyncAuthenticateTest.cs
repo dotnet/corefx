6025829dba1385e757e3c68d21f0f183fefd668a
Caesar Chen <shizchen@microsoft.com>
2018-02-22 14:42:19 -0800

    fix comment

--- src/System.Net.Security/tests/FunctionalTests/ClientAsyncAuthenticateTest.cs
+++ src/System.Net.Security/tests/FunctionalTests/ClientAsyncAuthenticateTest.cs
@@ -25,7 +25,7 @@ namespace System.Net.Security.Tests
         }
 
         [Fact]
-        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework, "CI uses old Framework version, which doesn't support SystemDefault")]
+        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework, "CI uses old Framework version, which doesn't support using SslProtocols.None for SystemDefaultTlsVersions behavior")]
         public async Task ClientAsyncAuthenticate_SslStreamClientServerNone_UseStrongCryptoSet()
         {
             SslProtocols protocol = SslProtocols.None;
