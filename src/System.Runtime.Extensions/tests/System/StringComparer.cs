889579d51e3a167a39baf16b2810af8fd991b163
Anipik <anirudhagnihotry098@gmail.com>
2018-02-22 14:52:59 -0800

    minor change

--- src/System.Runtime.Extensions/tests/System/StringComparer.cs
+++ src/System.Runtime.Extensions/tests/System/StringComparer.cs
@@ -5,9 +5,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
 using Xunit;
 
 namespace System.Tests
