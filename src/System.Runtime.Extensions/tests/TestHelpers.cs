276c992e4b1a24243b4d0591221bd4af343588ca
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2018-02-25 10:39:38 -0800

    Address feedback

--- src/System.Runtime.Extensions/tests/TestHelpers.cs
+++ src/System.Runtime.Extensions/tests/TestHelpers.cs
@@ -2,18 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace System
 {
     public static class TestHelpers
     {
-
         public static TheoryData<T> Concat<T>(this IEnumerable<T> first, TheoryData<T> second)
         {
             TheoryData<T> data = new TheoryData<T>();
