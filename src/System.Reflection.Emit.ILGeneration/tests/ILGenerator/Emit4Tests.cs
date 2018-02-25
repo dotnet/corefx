eb4ee13afda2360aab3ec3a0fbb5a2e60be2f4f0
Petar Trifonov Petrov <petarpetrovt@gmail.com>
2018-02-25 14:19:52 +0200

    Improve TestEmitCalliStdCall test method

--- src/System.Reflection.Emit.ILGeneration/tests/ILGenerator/Emit4Tests.cs
+++ src/System.Reflection.Emit.ILGeneration/tests/ILGenerator/Emit4Tests.cs
@@ -9,31 +9,42 @@ namespace System.Reflection.Emit.Tests
 {
     public class ILGeneratorEmit4
     {
-        [Fact]
-        public void TestEmitCalliWithNullReturnType()
+        [Theory]
+        [InlineData(1, 1, 2)]
+        public void TestEmitCalliStdCall(int a, int b, int result)
         {
             ModuleBuilder moduleBuilder = Helpers.DynamicModule();
             TypeBuilder typeBuilder = moduleBuilder.DefineType("T", TypeAttributes.Public);
+            Type returnType = typeof(int);
 
             MethodBuilder methodBuilder = typeBuilder.DefineMethod("F",
-                MethodAttributes.Public | MethodAttributes.Static, null, new Type[] { typeof(IntPtr) });
+                MethodAttributes.Public | MethodAttributes.Static, returnType, new Type[] { typeof(IntPtr), typeof(int), typeof(int) });
             methodBuilder.SetImplementationFlags(MethodImplAttributes.NoInlining);
 
             ILGenerator il = methodBuilder.GetILGenerator();
+            il.Emit(OpCodes.Ldarg_1);
+            il.Emit(OpCodes.Ldarg_2);
             il.Emit(OpCodes.Ldarg_0);
-            il.EmitCalli(OpCodes.Calli, CallingConvention.StdCall, null, Type.EmptyTypes);
+            il.EmitCalli(OpCodes.Calli, CallingConvention.StdCall, returnType, new Type[] { typeof(int), typeof(int) });
             il.Emit(OpCodes.Ret);
 
             Type dynamicType = typeBuilder.CreateType();
-            dynamicType
+            IntPtr funcPtr = Marshal.GetFunctionPointerForDelegate(new FooFoo(Foo));
+
+            object resultValue = dynamicType
                 .GetMethod("F", BindingFlags.Public | BindingFlags.Static)
-                .Invoke(null, new object[] { Marshal.GetFunctionPointerForDelegate(new FooFoo(Foo)) });
+                .Invoke(null, new object[] { funcPtr, a, b });
+
+            Assert.IsType(returnType, resultValue);
+            Assert.Equal(result, resultValue);
         }
 
-        delegate void FooFoo();
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate int FooFoo(int a, int b);
 
-        static void Foo()
+        public static int Foo(int a, int b)
         {
+            return a + b;
         }
     }
 }
