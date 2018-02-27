656baafa2c3899082b6d91e89114230f11b66f9e
Jeremy Barton <jbarton@microsoft.com>
2018-02-27 14:03:03 -0800

    Make Decrypt_512_NoData_FixedValue pass on netfx

--- src/System.Security.Cryptography.Pkcs/tests/EnvelopedCms/DecryptTests.cs
+++ src/System.Security.Cryptography.Pkcs/tests/EnvelopedCms/DecryptTests.cs
@@ -117,6 +117,13 @@ namespace System.Security.Cryptography.Pkcs.EnvelopedCmsTests.Tests
                 "650304012A0410280AC7A629BFC9FD6FB24F8A42F094B4"
             ).HexToByteArray();
 
+            if (PlatformDetection.IsFullFramework)
+            {
+                // On NetFx when Array.Empty should be returned an array of 6 zeros is
+                // returned instead.
+                content = new byte[6];
+            }
+
             VerifySimpleDecrypt(message, Certificates.RSASha512KeyTransfer1, new ContentInfo(content));
         }
 
