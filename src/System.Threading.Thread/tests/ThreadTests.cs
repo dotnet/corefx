4ccc1b821c1c76bea642650caf436bedaeaba7cb
Jan Kotas <jkotas@microsoft.com>
2018-02-27 23:22:40 -0800

    Check that the id is no negative

--- src/System.Threading.Thread/tests/ThreadTests.cs
+++ src/System.Threading.Thread/tests/ThreadTests.cs
@@ -1013,7 +1013,8 @@ namespace System.Threading.Threads.Tests
             Thread.SpinWait(0);
             Thread.SpinWait(1);
             Thread.Yield();
-            Thread.GetCurrentProcessorId();
+
+            Assert.True(Thread.GetCurrentProcessorId() >= 0);
         }
     }
 }
