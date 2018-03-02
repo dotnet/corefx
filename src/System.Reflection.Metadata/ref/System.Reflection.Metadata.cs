be5d687741c4339cc25cf8e414d4bf76d08ae651
Tomas Matousek <tomat@microsoft.com>
2018-03-01 18:06:05 -0800

    Add ifdefs back

--- src/System.Reflection.Metadata/ref/System.Reflection.Metadata.cs
+++ src/System.Reflection.Metadata/ref/System.Reflection.Metadata.cs
@@ -99,7 +99,9 @@ namespace System.Reflection.Metadata
         public System.Reflection.Metadata.StringHandle Name { get { throw null; } }
         public System.Reflection.Metadata.BlobHandle PublicKey { get { throw null; } }
         public System.Version Version { get { throw null; } }
+#if !NETSTANDARD11
         public System.Reflection.AssemblyName GetAssemblyName() { throw null; }
+#endif
         public System.Reflection.Metadata.CustomAttributeHandleCollection GetCustomAttributes() { throw null; }
         public System.Reflection.Metadata.DeclarativeSecurityAttributeHandleCollection GetDeclarativeSecurityAttributes() { throw null; }
     }
@@ -165,7 +167,9 @@ namespace System.Reflection.Metadata
         public System.Reflection.Metadata.StringHandle Name { get { throw null; } }
         public System.Reflection.Metadata.BlobHandle PublicKeyOrToken { get { throw null; } }
         public System.Version Version { get { throw null; } }
+#if !NETSTANDARD11
         public System.Reflection.AssemblyName GetAssemblyName() { throw null; }
+#endif
         public System.Reflection.Metadata.CustomAttributeHandleCollection GetCustomAttributes() { throw null; }
     }
     public readonly partial struct AssemblyReferenceHandle : System.IEquatable<System.Reflection.Metadata.AssemblyReferenceHandle>
