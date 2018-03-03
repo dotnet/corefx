36f0c12ee626ba6bb0a6edd3dea02b32836d2c14
Anirudh Agnihotry <anagniho@microsoft.com>
2018-03-02 17:20:51 -0800

    enable failing tests due to paths

--- src/System.IO.FileSystem/tests/DirectoryInfo/Root.cs
+++ src/System.IO.FileSystem/tests/DirectoryInfo/Root.cs
@@ -31,7 +31,6 @@ namespace System.IO.Tests
             Assert.Equal(root, test.FullName);
         }
 
-        [ActiveIssue(27552)]
         [Fact]
         [PlatformSpecific(TestPlatforms.Windows)]  // UNC shares
         public void UNCShares()
