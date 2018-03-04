5276f30a77ef15f019504c9559632b6c0d4faf16
David Fowler <davidfowl@gmail.com>
2018-03-04 07:41:11 -0800

    Small changes to pipelines (#27704)
    - Moved CompletionData to another file and not as a nested type of Pipe
    - Follow the file name guidelines for tfm specific implementations

--- src/System.IO.Pipelines/src/System/IO/Pipelines/Pipe.cs
+++ src/System.IO.Pipelines/src/System/IO/Pipelines/Pipe.cs
@@ -845,21 +845,5 @@ namespace System.IO.Pipelines
                 ResetState();
             }
         }
-
-        internal readonly struct CompletionData
-        {
-            public Action<object> Completion { get; }
-            public object CompletionState { get; }
-            public ExecutionContext ExecutionContext { get; }
-            public SynchronizationContext SynchronizationContext { get; }
-
-            public CompletionData(Action<object> completion, object completionState, ExecutionContext executionContext, SynchronizationContext synchronizationContext)
-            {
-                Completion = completion;
-                CompletionState = completionState;
-                ExecutionContext = executionContext;
-                SynchronizationContext = synchronizationContext;
-            }
-        }
     }
 }
