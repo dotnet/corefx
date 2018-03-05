5c756842b56023259f1bf111e5f9b5e3e4f47d28
David Shulman <david.shulman@microsoft.com>
2018-03-05 15:18:15 -0800

    Fix SocketsNetHttpHandler TLS client cert handling
    SocketNetHttpHandler was not filtering down the client certificates when sending in
    "Manual" mode. This caused a client certificate to be sent to a server when it
    didn't have the proper EKU OID for "Client Authentication".
    
    Changed this so that both "Manual" and "Automatic" modes will use the CertificateHelper
    GetEligibleCertificate() API.
    
    Fixes #23128

--- src/System.Net.Http/src/System/Net/Http/HttpClientHandler.Unix.cs
+++ src/System.Net.Http/src/System/Net/Http/HttpClientHandler.Unix.cs
@@ -17,6 +17,7 @@ namespace System.Net.Http
         private readonly CurlHandler _curlHandler;
         private readonly SocketsHttpHandler _socketsHttpHandler;
         private readonly DiagnosticsHandler _diagnosticsHandler;
+        private ClientCertificateOption _clientCertificateOptions;
 
         public HttpClientHandler() : this(UseSocketsHttpHandler) { }
 
@@ -26,6 +27,7 @@ namespace System.Net.Http
             {
                 _socketsHttpHandler = new SocketsHttpHandler();
                 _diagnosticsHandler = new DiagnosticsHandler(_socketsHttpHandler);
+                ClientCertificateOptions = ClientCertificateOption.Manual;
             }
             else
             {
@@ -91,9 +93,7 @@ namespace System.Net.Http
                 }
                 else
                 {
-                    return _socketsHttpHandler.SslOptions.LocalCertificateSelectionCallback != null ?
-                        ClientCertificateOption.Automatic :
-                        ClientCertificateOption.Manual;
+                    return _clientCertificateOptions;
                 }
             }
             set
@@ -108,11 +108,13 @@ namespace System.Net.Http
                     {
                         case ClientCertificateOption.Manual:
                             ThrowForModifiedManagedSslOptionsIfStarted();
-                            _socketsHttpHandler.SslOptions.LocalCertificateSelectionCallback = null;
+                            _clientCertificateOptions = value;
+                            _socketsHttpHandler.SslOptions.LocalCertificateSelectionCallback = (sender, targetHost, localCertificates, remoteCertificate, acceptableIssuers) => CertificateHelper.GetEligibleClientCertificate(ClientCertificates);
                             break;
 
                         case ClientCertificateOption.Automatic:
                             ThrowForModifiedManagedSslOptionsIfStarted();
+                            _clientCertificateOptions = value;
                             _socketsHttpHandler.SslOptions.LocalCertificateSelectionCallback = (sender, targetHost, localCertificates, remoteCertificate, acceptableIssuers) => CertificateHelper.GetEligibleClientCertificate();
                             break;
 
