892f7f7e212f04aa86c8484a2df012bd2640f2e5
Jan Kotas <jkotas@microsoft.com>
2018-03-07 15:27:00 -0800

    Add back method needed by CoreRT
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>
    Signed-off-by: dotnet-bot-corefx-mirror <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/Number.Parsing.cs
+++ src/Common/src/CoreLib/System/Number.Parsing.cs
@@ -956,6 +956,9 @@ namespace System
 
             public static unsafe ulong Mantissa(double d) =>
                 *((ulong*)&d) & 0x000fffffffffffff;
+
+            public static unsafe bool Sign(double d) =>
+                (*((uint*)&d + 1) >> 31) != 0;
         }
     }
 }
