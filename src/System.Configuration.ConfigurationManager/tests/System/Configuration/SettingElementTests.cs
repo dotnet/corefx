7e6436467cebb844bdad1a54b8f9877460a25ec5
Jlalond <jjjlalonde@gmail.com>
2018-03-08 12:14:02 -0500

    Added Tests for Setting Element (#27807)
    * Added Tests for Setting Element
    
    * Cleaning as per the corefx standards
    
    * including .net license

---
+++ src/System.Configuration.ConfigurationManager/tests/System/Configuration/SettingElementTests.cs
@@ -0,0 +1,71 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace System.Configuration.Tests
+{
+    public class SettingElementTests
+    {
+        [Fact]
+        public void TestForInequality()
+        {
+            var ElementOne = new SettingElement("NotEqualOne", SettingsSerializeAs.String);
+            var ElementTwo = new SettingElement("NotEqualTwo", SettingsSerializeAs.String);
+            Assert.False(ElementOne.Equals(ElementTwo));
+        }
+
+        [Fact]
+        public void TestForEquality()
+        {
+            var ElementOne = new SettingElement("TheExactSameName", SettingsSerializeAs.String);
+            var ElementTwo = new SettingElement("TheExactSameName", SettingsSerializeAs.String);
+            Assert.True(ElementOne.Equals(ElementTwo));
+        }
+
+        [Fact]
+        public void DefaultSettingSerializationIsString()
+        {
+            var Element = new SettingElement();
+            Assert.Equal(SettingsSerializeAs.String, Element.SerializeAs);
+        }
+
+        [Fact]
+        public void DefaultNameIsEmptyString()
+        {
+            var Element = new SettingElement();
+            Assert.Equal(string.Empty, Element.Name);
+        }
+
+        [Fact]
+        public void DefaultValueIsNull()
+        {
+            var Element = new SettingElement();
+            Assert.Equal(null, Element.Value.CurrentConfiguration);
+        }
+    
+        [Fact]
+        public void DefaultSettingElementExceptionForGetHashCode()
+        {
+            var Element = new SettingElement();
+            Assert.Throws<NullReferenceException>(() => Element.GetHashCode());
+            //likely a bug
+        }
+
+        [Fact]
+        public void NonDefaultValueHasNonNullHashCode()
+        {
+            var Element = new SettingElement("Test", SettingsSerializeAs.Xml)
+            {
+                Value = new SettingValueElement
+                {
+                    ValueXml = new ConfigXmlDocument
+                    {
+                    }
+                }
+            };
+            Assert.NotNull(Element.GetHashCode());
+        }
+    }
+}
