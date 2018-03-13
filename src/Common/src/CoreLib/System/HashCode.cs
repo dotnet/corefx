41bc99176f503e3be23a3c25f253ab596c748cc9
Jonathan Dickinson <jonathanD@k2.com>
2018-03-12 21:48:01 -0700

    Fix GetHashCode

--- src/Common/src/CoreLib/System/HashCode.cs
+++ src/Common/src/CoreLib/System/HashCode.cs
@@ -422,13 +422,15 @@ namespace System
         //   implementation has to change in the future we don't want to worry
         //   about people who might have incorrectly used this type.
 
-        // These aren't applied for HASHCODE_INTERNAL as EditorBrowsable is not available
+        // These attributes aren't applied for HASHCODE_INTERNAL as EditorBrowsable is not available
         // everywhere.
 #if !HASHCODE_INTERNAL
         [Obsolete("HashCode is a mutable struct and should not be compared with other HashCodes. Use ToHashCode to retrieve the computed hash code.", error: true)]
         [EditorBrowsable(EditorBrowsableState.Never)]
+#endif
         public override int GetHashCode() => throw new NotSupportedException(SR.HashCode_HashCodeNotSupported);
 
+#if !HASHCODE_INTERNAL
         [Obsolete("HashCode is a mutable struct and should not be compared with other HashCodes.", error: true)]
         [EditorBrowsable(EditorBrowsableState.Never)]
 #endif
