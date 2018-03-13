4b6ef4d5f451e79d1fe3cae3899b9f7a1111dfe4
Anipik <anirudhagnihotry098@gmail.com>
2018-03-13 15:07:26 -0700

    Tests Added for ServiceProcess Description

---
+++ src/System.ServiceProcess.ServiceController/tests/ServiceProcessDescriptionAttributeTests.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace System.ServiceProcess.Tests
+{
+    public class ServiceProcessDescriptionAttributeTests
+    {
+        public static TheoryData<string> Ctor_Data => new TheoryData<string>
+        {
+            { string.Empty},
+            { null },
+            { "hello" }
+        };
+
+        [Theory,
+            MemberData(nameof(Ctor_Data))]
+        public void Ctor_EmptyString(string input) => Assert.Equal(input, new ServiceProcessDescriptionAttribute(input).Description);
+    }
+}
