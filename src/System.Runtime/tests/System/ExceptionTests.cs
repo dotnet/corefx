0cf9fb1e7702da2a00f813f498e25b9c7ecc2c3e
Alexandre Teoi <ateoi@microsoft.com>
2018-03-14 14:28:18 -0300

    Added tests to verify the reported call stacks for rethrown exceptions, as specified by dotnet/coreclr#15780.
    1. Exception rethrown in a method different than the method where it was originally thrown: the stack trace contains both the location in the method where the exception was originally thrown, and the location where the method that threw the exception was called.
    
    2. Exception is thrown and later rethrown in the same method: the stack trace only contains the location where the exception was originally thrown and does not include the location where the exception was rethrown.

--- src/System.Runtime/tests/System/ExceptionTests.cs
+++ src/System.Runtime/tests/System/ExceptionTests.cs
@@ -2,9 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using Xunit;
 
 namespace System.Tests
@@ -56,6 +58,103 @@ namespace System.Tests
             }
 
             Assert.True(caught);
+        }
+
+        [Fact]
+        public static void ThrowStatementDoesNotResetExceptionStackLineSameMethod()
+        {
+            var callStack = new Stack<(string, string, int)>();
+
+            try
+            {
+                // Keep the two statements below together
+                callStack.Push(GetSourceInformation());
+                ThrowAndRethrowSameMethod(callStack);
+            }
+            catch (Exception ex)
+            {
+                VerifyCallStack(callStack, ex.StackTrace);
+            }
+        }
+
+        private static void ThrowAndRethrowSameMethod(Stack<(string, string, int)> callStack)
+        {
+            try
+            {
+                // Keep the two statements below together
+                callStack.Push(GetSourceInformation());
+                throw new Exception("Boom!");
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        [Fact]
+        public static void ThrowStatementDoesNotResetExceptionStackLineOtherMethod()
+        {
+            var callStack = new Stack<(string, string, int)>();
+
+            try
+            {
+                // Keep the two statements below together
+                callStack.Push(GetSourceInformation());
+                ThrowAndRethrowOtherMethod(callStack);
+            }
+            catch (Exception ex)
+            {
+                VerifyCallStack(callStack, ex.StackTrace);
+            }
+        }
+
+        private static void ThrowAndRethrowOtherMethod(Stack<(string, string, int)> callStack)
+        {
+            try
+            {
+                // Keep the two statements below together
+                callStack.Push(GetSourceInformation());
+                ThrowException(callStack);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private static void ThrowException(Stack<(string, string, int)> callStack)
+        {
+            // Keep the two statements below together
+            callStack.Push(GetSourceInformation());
+            throw new Exception("Boom!");
+        }
+
+        private static void VerifyCallStack(
+            Stack<(string CallerMemberName, string SourceFilePath, int SourceLineNumber)> expectedCallStack, string reportedCallStack)
+        {
+            const string FrameParserRegex = @"\s+at\s.+\.(?<memberName>[^(]+)\([^)]*\)\sin\s(?<filePath>.*)\:line\s(?<lineNumber>[\d]+)";
+
+            using (var sr = new StringReader(reportedCallStack))
+            {
+                string frame;
+                while (!string.IsNullOrEmpty(frame = sr.ReadLine()))
+                {
+                    var reportedFrame = expectedCallStack.Pop();
+                    var match = Regex.Match(frame, FrameParserRegex);
+                    Assert.True(match.Success);
+                    Assert.Equal(reportedFrame.CallerMemberName, match.Groups["memberName"].Value);
+                    Assert.Equal(reportedFrame.SourceFilePath, match.Groups["filePath"].Value);
+                    Assert.Equal(reportedFrame.SourceLineNumber + 1, Convert.ToInt32(match.Groups["lineNumber"].Value));
+                }
+            }
+        }
+
+        private static (string, string, int) GetSourceInformation(
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            return (memberName, sourceFilePath, sourceLineNumber);
         }
     }
 
