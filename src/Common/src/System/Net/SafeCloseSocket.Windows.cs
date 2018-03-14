72623a60455aa52da352873c7129752be7efcdf3
Tomas Weinfurt <tweinfurt@yahoo.com>
2018-03-14 15:29:14 -0700

    add comment to empty  UnblockSocket() on Windows

--- src/Common/src/System/Net/SafeCloseSocket.Windows.cs
+++ src/Common/src/System/Net/SafeCloseSocket.Windows.cs
@@ -123,6 +123,8 @@ namespace System.Net.Sockets
             }
         }
 
+        // On Unix some blocking operations can be stuck forever and this callback tries
+        // to wake them up. That is not necessary so far on Windows.
         private void UnblockSocket(InnerSafeCloseSocket innerSocket) {}
 
         internal sealed partial class InnerSafeCloseSocket : SafeHandleMinusOneIsInvalid
