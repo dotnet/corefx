9e6b5463aa0d93324084750e6173faf8f26cdf17
Günther Foidl <gue@korporal.at>
2018-03-18 23:13:07 +0100

    Added missing namespace

--- src/System.Collections.Immutable/src/System/Collections/Immutable/ImmutableArray_1.Builder.cs
+++ src/System.Collections.Immutable/src/System/Collections/Immutable/ImmutableArray_1.Builder.cs
@@ -5,6 +5,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
 
 namespace System.Collections.Immutable
 {
