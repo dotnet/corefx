af1761992a8629148b3d2297a443b046e4bef190
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2018-03-20 21:43:47 -0700

    Use FileSystemName in FileSystemWatcher (#28220)
    * Use FileSystemName in FileSystemWatcher
    
    - Uses newer spanified version in FileSystemName
    - Doesn't allow extended match chars `<`, `>`, `"`  as these are legal in Unix
    
    * Plumb through the case sensitivity
    
    * Update tests
    
    * Tweak test for netfx
    
    * Tweak the comment to be a bit more readable

--- src/System.IO.FileSystem.Watcher/tests/FileSystemWatcher.unit.cs
+++ src/System.IO.FileSystem.Watcher/tests/FileSystemWatcher.unit.cs
@@ -67,8 +67,8 @@ namespace System.IO.Tests
         [Fact]
         public void FileSystemWatcher_ctor()
         {
-            string path = String.Empty;
-            string pattern = "*.*";
+            string path = string.Empty;
+            string pattern = PlatformDetection.IsFullFramework ? "*.*" : "*";
             using (FileSystemWatcher watcher = new FileSystemWatcher())
                 ValidateDefaults(watcher, path, pattern);
         }
@@ -77,7 +77,7 @@ namespace System.IO.Tests
         public void FileSystemWatcher_ctor_path()
         {
             string path = @".";
-            string pattern = "*.*";
+            string pattern = PlatformDetection.IsFullFramework ? "*.*" : "*";
             using (FileSystemWatcher watcher = new FileSystemWatcher(path))
                 ValidateDefaults(watcher, path, pattern);
         }
@@ -216,14 +216,14 @@ namespace System.IO.Tests
         {
             FileSystemWatcher watcher = new FileSystemWatcher();
 
-            Assert.Equal("*.*", watcher.Filter);
+            Assert.Equal(PlatformDetection.IsFullFramework ? "*.*" : "*", watcher.Filter);
 
-            // Null and empty should be mapped to "*.*"
+            // Null and empty should be mapped to "*"
             watcher.Filter = null;
-            Assert.Equal("*.*", watcher.Filter);
+            Assert.Equal(PlatformDetection.IsFullFramework ? "*.*" : "*", watcher.Filter);
 
-            watcher.Filter = String.Empty;
-            Assert.Equal("*.*", watcher.Filter);
+            watcher.Filter = string.Empty;
+            Assert.Equal(PlatformDetection.IsFullFramework ? "*.*" : "*", watcher.Filter);
 
             watcher.Filter = " ";
             Assert.Equal(" ", watcher.Filter);
@@ -241,7 +241,7 @@ namespace System.IO.Tests
                 RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
             {
                 // expect no change for OrdinalIgnoreCase-equal strings
-                // it's unclear why desktop does this but preserve it for compat        
+                // it's unclear why desktop does this but preserve it for compat
                 watcher.Filter = "ABC.DLL";
                 Assert.Equal("abc.dll", watcher.Filter);
             }
