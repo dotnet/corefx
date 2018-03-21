4cda87ce3cab29bdf72f666f6200a9b10b3fec86
Maryam Ariyan <Maryam.Ariyan@microsoft.com>
2018-03-21 16:00:38 -0700

    Changing test method to accept MemberData. Returning 0 or -1 as exit code

--- src/System.Runtime/tests/System/StringGetHashCodeTests.cs
+++ src/System.Runtime/tests/System/StringGetHashCodeTests.cs
@@ -16,54 +16,51 @@ namespace System.Tests
         /// and confirming it is different (modulo possible values of int).
         /// If the legacy hash codes are being returned, it will not be different.
         /// </summary>
-        [Fact]
-        public void GetHashCodeWithStringComparer_UseSameStringInTwoProcesses_ReturnsDifferentHashCodes()
+        [Theory]
+        [MemberData(nameof(GetHashCode_TestData))]
+        public void GetHashCodeWithStringComparer_UseSameStringInTwoProcesses_ReturnsDifferentHashCodes(int getHashCodeIndex)
         {
-            foreach (Func<int> ComputeHashCode in HashCodeComputers())
-            {
-                int parentHashCode = ComputeHashCode();
-                int childHashCode = GetChildHashCode(ComputeHashCode, parentHashCode);
-                Assert.NotEqual(parentHashCode, childHashCode);
-            }
-        }
-
-        private static IEnumerable<Func<int>> HashCodeComputers()
-        {
-            return new Func<int>[]
-            {
-                () => { return StringComparer.CurrentCulture.GetHashCode("abc"); },
-                () => { return StringComparer.CurrentCultureIgnoreCase.GetHashCode("abc"); },
-                () => { return StringComparer.InvariantCulture.GetHashCode("abc"); },
-                () => { return StringComparer.InvariantCultureIgnoreCase.GetHashCode("abc"); },
-                () => { return StringComparer.Ordinal.GetHashCode("abc"); },
-                () => { return StringComparer.OrdinalIgnoreCase.GetHashCode("abc"); },
-                () => { return "abc".GetHashCode(); },
-                () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.IgnoreCase); },
-                () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.IgnoreKanaType); },
-                () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.IgnoreNonSpace); },
-                () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.IgnoreSymbols); },
-                () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.IgnoreWidth); },
-                () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.None); },
-                () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.Ordinal); },
-                () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.OrdinalIgnoreCase); }
-            };
-        }
-
-        private int GetChildHashCode(Func<int> computeHash, int parentHashCode)
-        {
-            int childHashCode, timesTried = 0;
+            Func<string, string, int> method = (parentHash, i) => { return int.Parse(parentHash) != s_GetHashCodes[int.Parse(i)]() ? 0 : -1; };
+            int parentHashCode = s_GetHashCodes[getHashCodeIndex]();
+            int exitCode, timesTried = 0;
             do
             {
                 // very small chance the child and parent hashcode are the same. To further reduce chance of collision we try up to 3 times
-                using (RemoteInvokeHandle handle = RemoteInvoke(computeHash, new RemoteInvokeOptions { CheckExitCode = false }))
+                using (RemoteInvokeHandle handle = RemoteInvoke(method, parentHashCode.ToString(), getHashCodeIndex.ToString(), new RemoteInvokeOptions { CheckExitCode = false }))
                 {
                     handle.Process.WaitForExit();
-                    childHashCode = handle.Process.ExitCode;
+                    exitCode = handle.Process.ExitCode;
                 }
                 timesTried++;
-            } while (parentHashCode == childHashCode && timesTried < 3);
+            } while (exitCode != 0 && timesTried < 3);
 
-            return childHashCode;
+            Assert.Equal(0, exitCode);
         }
+
+        public static IEnumerable<object[]> GetHashCode_TestData()
+        {
+            for (int i = 0; i < s_GetHashCodes.Length; i++)
+            {
+                yield return new object[] { i };
+            }
+        }
+
+        private static readonly Func<int>[] s_GetHashCodes = {
+            () => { return StringComparer.CurrentCulture.GetHashCode("abc"); },
+            () => { return StringComparer.CurrentCultureIgnoreCase.GetHashCode("abc"); },
+            () => { return StringComparer.InvariantCulture.GetHashCode("abc"); },
+            () => { return StringComparer.InvariantCultureIgnoreCase.GetHashCode("abc"); },
+            () => { return StringComparer.Ordinal.GetHashCode("abc"); },
+            () => { return StringComparer.OrdinalIgnoreCase.GetHashCode("abc"); },
+            () => { return "abc".GetHashCode(); },
+            () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.IgnoreCase); },
+            () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.IgnoreKanaType); },
+            () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.IgnoreNonSpace); },
+            () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.IgnoreSymbols); },
+            () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.IgnoreWidth); },
+            () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.None); },
+            () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.Ordinal); },
+            () => { return CultureInfo.CurrentCulture.CompareInfo.GetHashCode("abc", CompareOptions.OrdinalIgnoreCase); }
+        };
     }
 }
