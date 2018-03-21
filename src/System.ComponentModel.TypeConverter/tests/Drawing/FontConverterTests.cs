93ddaaadf5be8691bec8715f20ec93e8db7cd897
Stano Peťko <stano@stanopetko.eu>
2018-03-21 20:29:02 +0100

    Fixed namespace in FontConverterTests.

--- src/System.ComponentModel.TypeConverter/tests/Drawing/FontConverterTests.cs
+++ src/System.ComponentModel.TypeConverter/tests/Drawing/FontConverterTests.cs
@@ -2,11 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Drawing;
 using Xunit;
 
-namespace MonoTests.System.Drawing
+namespace System.ComponentModel.TypeConverterTests
 {
     public class FontNameConverterTest
     {
