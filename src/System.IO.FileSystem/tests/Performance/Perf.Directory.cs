8145c2b74470ece8c04f8710b958b2b427bc0fae
Anirudh Agnihotry <anirudhagnihotry098@gmail.com>
2018-03-23 00:52:52 -0700

    perf tests for create and delete directory (#28381)

--- src/System.IO.FileSystem/tests/Performance/Perf.Directory.cs
+++ src/System.IO.FileSystem/tests/Performance/Perf.Directory.cs
@@ -2,7 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Text;
 using Microsoft.Xunit.Performance;
+using Xunit;
 
 namespace System.IO.Tests
 {
@@ -54,6 +56,80 @@ namespace System.IO.Tests
 
             // Teardown
             Directory.Delete(testFile);
+        }
+
+        [Benchmark]
+        [InlineData(10, 100)]
+        [InlineData(100, 10)]
+        [InlineData(1000, 1)]
+        [OuterLoop("Takes a lot of time to finish")]
+        public void RecursiveCreateDirectory(int depth, int times)
+        {
+            if (!PlatformDetection.IsWindows && depth == 1000)
+                return;
+            
+            // Setup
+            string rootDirectory = GetTestFilePath();
+            StringBuilder sb = new StringBuilder(5000);
+            for (int i = 0; i < depth; i++)
+            {
+                sb.Append(@"\a");
+            }
+
+            string path = sb.ToString();
+
+            foreach (var iteration in Benchmark.Iterations)
+            {
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < times; i++)
+                    {
+                        Directory.CreateDirectory(rootDirectory + @"\" + i + path);
+                    }
+                }
+                // TearDown For each iteration
+                Directory.Delete(rootDirectory, recursive: true);
+            }
+        }
+
+        [Benchmark]
+        [InlineData(10, 100)]
+        [InlineData(100, 10)]
+        [InlineData(1000, 1)]
+        [OuterLoop("Takes a lot of time to finish")]
+        public void RecursiveDeleteDirectory(int depth, int times)
+        {
+            if (!PlatformDetection.IsWindows && depth == 1000)
+                return;
+
+            // Setup
+            string rootDirectory = GetTestFilePath();
+            StringBuilder sb = new StringBuilder(5000);
+            for (int i = 0; i < depth; i++)
+            {
+                sb.Append(@"\a");
+            }
+
+            string path = sb.ToString();
+
+            foreach (var iteration in Benchmark.Iterations)
+            {
+                // Setup For each Iteration
+                for (int i = 0; i < times; i++)
+                {
+                    Directory.CreateDirectory(rootDirectory + Path.DirectorySeparatorChar + i + path);
+                }
+
+                using (iteration.StartMeasurement())
+                {
+                    for (int i = 0; i < times; i++)
+                    {
+                        Directory.Delete(rootDirectory + Path.DirectorySeparatorChar + i, recursive: true);
+                    }
+                }
+            }
+            // TearDown
+            Directory.Delete(rootDirectory, recursive: true);
         }
     }
 }
