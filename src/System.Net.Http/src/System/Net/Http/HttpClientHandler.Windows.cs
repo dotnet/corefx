8bee87e89e5edda40acd7238ea7954ecc522f97d
wtgodbe <wigodbe@microsoft.com>
2018-03-26 14:01:08 -0700

    Resolve ApiCompat

--- src/System.Net.Http/src/System/Net/Http/HttpClientHandler.Windows.cs
+++ src/System.Net.Http/src/System/Net/Http/HttpClientHandler.Windows.cs
@@ -172,10 +172,30 @@ namespace System.Net.Http
             set { _winHttpHandler.MaxConnectionsPerServer = value; }
         }
 
+        public long MaxRequestContentBufferSize
+        {
+            // This property has been deprecated. In the .NET Desktop it was only used when the handler needed to 
+            // automatically buffer the request content. That only happened if neither 'Content-Length' nor 
+            // 'Transfer-Encoding: chunked' request headers were specified. So, the handler thus needed to buffer
+            // in the request content to determine its length and then would choose 'Content-Length' semantics when
+            // POST'ing. In CoreCLR and .NETNative, the handler will resolve the ambiguity by always choosing
+            // 'Transfer-Encoding: chunked'. The handler will never automatically buffer in the request content.
+            get { return 0; }
+            
+            // TODO (#7879): Add message/link to exception explaining the deprecation. 
+            // Update corresponding exception in HttpClientHandler.Unix.cs if/when this is updated.
+            set { throw new PlatformNotSupportedException(); }
+        }
+
         public int MaxResponseHeadersLength
         {
             get { return _winHttpHandler.MaxResponseHeadersLength; }
             set { _winHttpHandler.MaxResponseHeadersLength = value; }
+        }
+
+        public X509CertificateCollection ClientCertificates
+        {
+            get { return _winHttpHandler.ClientCertificates; }
         }
 
         public static Func<HttpRequestMessage, X509Certificate2, X509Chain, SslPolicyErrors, bool> DangerousAcceptAnyServerCertificateValidator { get; } = delegate { return true; };
