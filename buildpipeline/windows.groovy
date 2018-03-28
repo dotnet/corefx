5096f83973c96ac5bcc1e7d32b2e3a9d03680aae
Santiago Fernandez <safern@microsoft.com>
2018-03-28 16:35:51 -0700

    Update netfx queues to use rs3 machines with .net471 installed

--- buildpipeline/windows.groovy
+++ buildpipeline/windows.groovy
@@ -8,7 +8,7 @@
 // TestOuter - If true, runs outerloop, if false runs just innerloop
 
 def submittedHelixJson = null
-def submitToHelix = (params.TGroup == 'netcoreapp')
+def submitToHelix = (params.TGroup == 'netcoreapp' || params.TGroup == 'netfx')
 
 simpleNode('Windows_NT','latest') {
     stage ('Checkout source') {
@@ -78,6 +78,8 @@ simpleNode('Windows_NT','latest') {
                     }
                 } else if (params.TGroup == 'uap') {
                     targetHelixQueues = ['Windows.10.Amd64.ClientRS2.Open']
+                } else if (params.TGroup == 'netfx') {
+                    targetHelixQueues = ['Windows.10.Amd64.ClientRS3']
                 }
 
                 bat "\"%VS140COMNTOOLS%\\VsDevCmd.bat\" && msbuild src\\upload-tests.proj /p:TargetGroup=${params.TGroup} /p:ArchGroup=${params.AGroup} /p:ConfigurationGroup=${params.CGroup} /p:TestProduct=corefx /p:TimeoutInSeconds=1200 /p:TargetOS=Windows_NT /p:HelixJobType=test/functional/cli/ /p:HelixSource=${helixSource} /p:BuildMoniker=${helixBuild} /p:HelixCreator=${helixCreator} /p:CloudDropAccountName=dotnetbuilddrops /p:CloudResultsAccountName=dotnetjobresults /p:CloudDropAccessToken=%CloudDropAccessToken% /p:CloudResultsAccessToken=%OutputCloudResultsAccessToken% /p:HelixApiEndpoint=https://helix.dot.net/api/2017-04-14/jobs /p:TargetQueues=\"${targetHelixQueues.join(',')}\" /p:HelixLogFolder= /p:HelixLogFolder=${WORKSPACE}\\${logFolder}\\ /p:HelixCorrelationInfoFileName=SubmittedHelixRuns.txt"
