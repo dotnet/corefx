fe63206856998c223adb0240a1e57201b6cfd467
Tomas Weinfurt <tweinfurt@yahoo.com>
2018-03-29 10:10:23 -0700

    enable tcp keep-alive on curl handler (#28588)

--- src/Common/src/Interop/Unix/System.Net.Http.Native/Interop.Easy.cs
+++ src/Common/src/Interop/Unix/System.Net.Http.Native/Interop.Easy.cs
@@ -111,6 +111,7 @@ internal static partial class Interop
             CURLOPT_PROXYTYPE = CurlOptionLongBase + 101,
             CURLOPT_HTTPAUTH = CurlOptionLongBase + 107,
             CURLOPT_TCP_NODELAY = CurlOptionLongBase + 121,
+            CURLOPT_TCP_KEEPALIVE = CurlOptionLongBase + 213,
             CURLOPT_CONNECTTIMEOUT_MS = CurlOptionLongBase + 156,
             CURLOPT_ADDRESS_SCOPE = CurlOptionLongBase + 171,
             CURLOPT_PROTOCOLS = CurlOptionLongBase + 181,
