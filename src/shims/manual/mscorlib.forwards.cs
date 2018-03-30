4b85bd03e9071a8855435ddd9ca5ba68c3818950
wtgodbe <wigodbe@microsoft.com>
2018-03-30 11:24:07 -0700

    Delete SerializationHolder

--- src/shims/manual/mscorlib.forwards.cs
+++ src/shims/manual/mscorlib.forwards.cs
@@ -27,7 +27,6 @@
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Collections.Generic.ShortEnumEqualityComparer<>))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Collections.Generic.LongEnumEqualityComparer<>))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Collections.ListDictionaryInternal))]
-[assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.UnitySerializationHolder))]
 [assembly:System.Runtime.CompilerServices.TypeForwardedTo(typeof(System.Diagnostics.Contracts.ContractException))]
 
 // This is temporary as we are building the mscorlib shim against netfx461 which doesn't contain ValueTuples.
