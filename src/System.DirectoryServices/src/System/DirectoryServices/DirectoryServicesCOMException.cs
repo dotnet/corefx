010a6bd354ffafcf64bb3c064afe88b71dc59d21
wtgodbe <wigodbe@microsoft.com>
2018-03-30 11:02:14 -0700

    Fix ExtendedError

--- src/System.DirectoryServices/src/System/DirectoryServices/DirectoryServicesCOMException.cs
+++ src/System.DirectoryServices/src/System/DirectoryServices/DirectoryServicesCOMException.cs
@@ -1,11 +1,7 @@
+
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-
-using System.Text;
-using System.Runtime.InteropServices;
-using System.Runtime.Serialization;
-using System.DirectoryServices.Interop;
 
 namespace System.DirectoryServices
 {
@@ -21,10 +17,11 @@ namespace System.DirectoryServices
     [System.Runtime.CompilerServices.TypeForwardedFrom("System.DirectoryServices, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
     public class DirectoryServicesCOMException : COMException, ISerializable
     {
+        private int _extendederror = 0;
+        private string _extendedmessage = "";
+
         public DirectoryServicesCOMException() { }
-
         public DirectoryServicesCOMException(string message) : base(message) { }
-
         public DirectoryServicesCOMException(string message, Exception inner) : base(message, inner) { }
         protected DirectoryServicesCOMException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
@@ -32,8 +29,16 @@ namespace System.DirectoryServices
 
         internal DirectoryServicesCOMException(string extendedMessage, int extendedError, COMException e) : base(e.Message, e.ErrorCode)
         {
-            ExtendedError = extendedError;
-            ExtendedErrorMessage = extendedMessage;
+            _extendederror = extendedError;
+            _extendedmessage = extendedMessage;
+        }
+
+        public int ExtendedError
+        {
+            get
+            {
+                return _extendederror;
+            }
         }
 
         public string ExtendedErrorMessage
@@ -66,7 +71,7 @@ namespace System.DirectoryServices
             }
             else
             {
-                errorMsg = SR.Format(SR.DSUnknown , Convert.ToString(hr, 16));
+                errorMsg = String.Format(CultureInfo.CurrentCulture, SR.DSUnknown , Convert.ToString(hr, 16));
             }
 
             return CreateFormattedComException(new COMException(errorMsg, hr));
