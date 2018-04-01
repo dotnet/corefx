5b291797ee32cb78ec73702fb1efd4855a96812b
Paulo Janotti <pauloja@microsoft.com>
2018-03-31 18:26:34 -0700

    Fix intermittent crash in FileSystemEnumerator for Unix

--- src/System.Private.Xml/tests/XmlSchema/XmlSchemaValidatorApi/ValidateMisc.cs
+++ src/System.Private.Xml/tests/XmlSchema/XmlSchemaValidatorApi/ValidateMisc.cs
@@ -893,7 +893,6 @@ namespace System.Xml.Tests
         }
 
         //TFS_538324
-        [ActiveIssue(27740, TestPlatforms.AnyUnix)]
         [Fact]
         public void XSDValidationGeneratesInvalidError_1()
         {
@@ -920,7 +919,6 @@ namespace System.Xml.Tests
         }
 
         //TFS_538324
-        [ActiveIssue(27740, TestPlatforms.AnyUnix)]
         [Fact]
         public void XSDValidationGeneratesInvalidError_2()
         {
