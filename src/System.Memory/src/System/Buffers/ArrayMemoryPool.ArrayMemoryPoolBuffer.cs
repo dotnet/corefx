d67ea510c68dbce13e735bc18b7356822a0df235
Ben Adams <thundercat@illyriad.co.uk>
2018-04-02 19:23:14 +0100

    ArrayMemoryPoolBuffer should only implement IMemoryOwner<T> (#28690)
    * ArrayMemoryPoolBuffer should only implement IMemoryOwner<T>
    
    * React pipelines to changes

--- src/System.Memory/src/System/Buffers/ArrayMemoryPool.ArrayMemoryPoolBuffer.cs
+++ src/System.Memory/src/System/Buffers/ArrayMemoryPool.ArrayMemoryPoolBuffer.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Runtime.InteropServices;
-using System.Threading;
 
 #if !netstandard
 using Internal.Runtime.CompilerServices;
@@ -15,102 +14,36 @@ namespace System.Buffers
 {
     internal sealed partial class ArrayMemoryPool<T> : MemoryPool<T>
     {
-        private sealed class ArrayMemoryPoolBuffer : MemoryManager<T>
+        private sealed class ArrayMemoryPoolBuffer : IMemoryOwner<T>
         {
             private T[] _array;
-            private int _refCount;
 
             public ArrayMemoryPoolBuffer(int size)
             {
                 _array = ArrayPool<T>.Shared.Rent(size);
-                _refCount = 1;
             }
 
-            public sealed override int Length => _array.Length;
-
-            public bool IsDisposed => _array == null;
-
-            public bool IsRetained => Volatile.Read(ref _refCount) > 0;
-
-            public sealed override Span<T> GetSpan()
+            public Memory<T> Memory
             {
-                if (IsDisposed)
-                    ThrowHelper.ThrowObjectDisposedException_ArrayMemoryPoolBuffer();
-
-                return _array;
-            }
-
-            protected sealed override void Dispose(bool disposing)
-            {
-                if (_array != null)
+                get
                 {
-                    ArrayPool<T>.Shared.Return(_array);
-                    _array = null;
-                }
-            }
-
-            // TryGetArray is exposed as "protected internal". Normally, the rules of C# dictate we override it as "protected" because the base class is
-            // in a different assembly. Except in the netstandard config where the base class is in the same assembly.
-            protected
-#if netstandard  
-            internal
-#endif
-            sealed override bool TryGetArray(out ArraySegment<T> segment)
-            {
-                if (IsDisposed)
-                    ThrowHelper.ThrowObjectDisposedException_ArrayMemoryPoolBuffer();
-
-                segment = new ArraySegment<T>(_array);
-                return true;
-            }
-
-            public sealed override MemoryHandle Pin(int elementIndex = 0)
-            {
-                unsafe
-                {
-                    while (true)
+                    T[] array = _array;
+                    if (array == null)
                     {
-                        int currentCount = Volatile.Read(ref _refCount);
-                        if (currentCount <= 0)
-                            ThrowHelper.ThrowObjectDisposedException_ArrayMemoryPoolBuffer();
-                        if (Interlocked.CompareExchange(ref _refCount, currentCount + 1, currentCount) == currentCount)
-                            break;
-                    }
-
-                    try
-                    {
-                        if ((uint)elementIndex > (uint)_array.Length)
-                        {
-                            ThrowHelper.ThrowArgumentOutOfRangeException(ExceptionArgument.elementIndex);
-                        }
-
-                        GCHandle handle = GCHandle.Alloc(_array, GCHandleType.Pinned);
-
-                        return new MemoryHandle(Unsafe.Add<T>(((void*)handle.AddrOfPinnedObject()), elementIndex), handle, this);
-                    }
-                    catch
-                    {
-                        Unpin();
-                        throw;
-                    }
-                }
-            }
-
-            public sealed override void Unpin()
-            {
-                while (true)
-                {
-                    int currentCount = Volatile.Read(ref _refCount);
-                    if (currentCount <= 0)
                         ThrowHelper.ThrowObjectDisposedException_ArrayMemoryPoolBuffer();
-                    if (Interlocked.CompareExchange(ref _refCount, currentCount - 1, currentCount) == currentCount)
-                    {
-                        if (currentCount == 1)
-                        {
-                            Dispose(true);
-                        }
-                        break;
                     }
+
+                    return new Memory<T>(array);
+                }
+            }
+
+            public void Dispose()
+            {
+                T[] array = _array;
+                if (array != null)
+                {
+                    _array = null;
+                    ArrayPool<T>.Shared.Return(array);
                 }
             }
         }
