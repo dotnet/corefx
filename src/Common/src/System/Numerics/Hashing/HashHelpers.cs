71a9dbc8262d01eb1e04f640c0357fd7f02be834
Ahson Khan <ahkha@microsoft.com>
2018-04-05 20:07:18 -0700

    C# Naming rule violation: Missing prefix: 's_'

--- src/Common/src/System/Numerics/Hashing/HashHelpers.cs
+++ src/Common/src/System/Numerics/Hashing/HashHelpers.cs
@@ -6,7 +6,7 @@ namespace System.Numerics.Hashing
 {
     internal static class HashHelpers
     {
-        public static readonly int RandomSeed = Guid.NewGuid().GetHashCode();
+        public static readonly int s_randomSeed = Guid.NewGuid().GetHashCode();
 
         public static int Combine(int h1, int h2)
         {
