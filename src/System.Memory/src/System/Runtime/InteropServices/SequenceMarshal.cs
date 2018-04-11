124c68c4067f19fb010fcacc3fb3703cbd6303f6
Ahson Khan <ahkha@microsoft.com>
2018-04-10 18:30:08 -0700

    Removing SequenceMarshal.TryGetMemoryManager for ReadOnlySequence (#28990)

--- src/System.Memory/src/System/Runtime/InteropServices/SequenceMarshal.cs
+++ src/System.Memory/src/System/Runtime/InteropServices/SequenceMarshal.cs
@@ -34,15 +34,6 @@ namespace System.Runtime.InteropServices
         }
 
         /// <summary>
-        /// Get <see cref="MemoryManager{T}"/> from the underlying <see cref="ReadOnlySequence{T}"/>.
-        /// If unable to get the <see cref="MemoryManager{T}"/>, return false.
-        /// </summary>
-        public static bool TryGetMemoryManager<T>(ReadOnlySequence<T> sequence, out MemoryManager<T> manager, out int start, out int length)
-        {
-            return sequence.TryGetMemoryManager(out manager, out start, out length);
-        }
-
-        /// <summary>
         /// Get <see cref="ReadOnlyMemory{T}"/> from the underlying <see cref="ReadOnlySequence{T}"/>.
         /// If unable to get the <see cref="ReadOnlyMemory{T}"/>, return false.
         /// </summary>
