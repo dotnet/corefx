69737872e69172695201449ca311bed78441304e
Marco Rossignoli <marco.rossignoli@gmail.com>
2018-04-17 15:35:56 +0200

    address PR feedback

--- src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.cs
+++ src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.cs
@@ -2146,7 +2146,7 @@ namespace System.Net.Http.Functional.Tests
             using (var client = new HttpClient(handler))
             {
                 // international version of the Starbucks website
-                // punnycode: xn--oy2b35ckwhba574atvuzkc.com                
+                // punycode: xn--oy2b35ckwhba574atvuzkc.com                
                 string server = "http://\uc2a4\ud0c0\ubc85\uc2a4\ucf54\ub9ac\uc544.com";
                 using (HttpResponseMessage response = await client.GetAsync(server))
                 {
