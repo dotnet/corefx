3bc11f84553f4c1936492c7950d05cfaa9cded53
Jan Kotas <jkotas@microsoft.com>
2018-04-18 20:02:04 -0700

    Tweak order of interface declarations on string (dotnet/coreclr#17660)
    Fixes https://github.com/dotnet/corefx/issues/29158
    
    Signed-off-by: dotnet-bot-corefx-mirror <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/String.cs
+++ src/Common/src/CoreLib/System/String.cs
@@ -21,7 +21,7 @@ namespace System
 
     [Serializable]
     [System.Runtime.CompilerServices.TypeForwardedFrom("mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
-    public sealed partial class String : IComparable, IEnumerable, IEnumerable<char>, IComparable<String>, IEquatable<String>, IConvertible, ICloneable
+    public sealed partial class String : IComparable, IEnumerable, IConvertible, IEnumerable<char>, IComparable<String>, IEquatable<String>, ICloneable
     {
         // String constructors
         // These are special. The implementation methods for these have a different signature from the
