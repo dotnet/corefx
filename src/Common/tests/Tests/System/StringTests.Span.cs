c8f53c26457d292706e05e382bfa9f9a56d4f91e
Marco Rossignoli <marco.rossignoli@gmail.com>
2018-04-27 21:53:11 +0200

    create tests source in common and link to projects

---
+++ src/Common/tests/Tests/System/StringTests.Span.cs
@@ -0,0 +1,22 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace System.Tests
+{
+    public partial class StringTestsSpan : RemoteExecutorTestBase
+    {
+
+    }
+}
