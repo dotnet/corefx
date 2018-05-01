1575aa998615cfb8c2c5bd0d14fd33fcc7e7813d
Marco Rossignoli <marco.rossignoli@gmail.com>
2018-05-01 19:45:59 +0200

    remove duplicates
    Signed-off-by: dotnet-bot-corefx-mirror <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/Buffers/Utilities.cs
+++ src/Common/src/CoreLib/System/Buffers/Utilities.cs
@@ -12,14 +12,16 @@ namespace System.Buffers
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static int SelectBucketIndex(int bufferSize)
         {
+            Debug.Assert(bufferSize > 0);
+
             uint bitsRemaining = ((uint)bufferSize - 1) >> 4;
 
             int poolIndex = 0;
             if (bitsRemaining > 0xFFFF) { bitsRemaining >>= 16; poolIndex = 16; }
-            if (bitsRemaining > 0xFF) { bitsRemaining >>= 8; poolIndex += 8; }
-            if (bitsRemaining > 0xF) { bitsRemaining >>= 4; poolIndex += 4; }
-            if (bitsRemaining > 0x3) { bitsRemaining >>= 2; poolIndex += 2; }
-            if (bitsRemaining > 0x1) { bitsRemaining >>= 1; poolIndex += 1; }
+            if (bitsRemaining > 0xFF)   { bitsRemaining >>= 8;  poolIndex += 8; }
+            if (bitsRemaining > 0xF)    { bitsRemaining >>= 4;  poolIndex += 4; }
+            if (bitsRemaining > 0x3)    { bitsRemaining >>= 2;  poolIndex += 2; }
+            if (bitsRemaining > 0x1)    { bitsRemaining >>= 1;  poolIndex += 1; }
 
             return poolIndex + (int)bitsRemaining;
         }
