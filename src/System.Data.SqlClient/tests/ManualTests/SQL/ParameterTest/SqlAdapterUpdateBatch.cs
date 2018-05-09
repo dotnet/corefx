7ab8d4119b13b4aa49fe87e20caa1d661f9e965c
Afsaneh Rafighi <v-afrafi@microsoft.com>
2018-05-09 14:46:31 -0700

    added license header file and formatted code

--- src/System.Data.SqlClient/tests/ManualTests/SQL/ParameterTest/SqlAdapterUpdateBatch.cs
+++ src/System.Data.SqlClient/tests/ManualTests/SQL/ParameterTest/SqlAdapterUpdateBatch.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace System.Data.SqlClient.ManualTesting.Tests
 {
     public class SqlAdapterUpdateBatch
-    { 
+    {
         private static string tcpConnStr = DataTestUtility.TcpConnStr;
         private static string tableName = "BatchDemoTable";
         private static List<EventInfo> entities;
@@ -17,7 +17,7 @@ namespace System.Data.SqlClient.ManualTesting.Tests
         public void SqlAdapterTest()
         {
             try
-            {          
+            {
                 var createTableQuery = "IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='BatchDemoTable' AND xtype='U')" +
                     " CREATE TABLE [dbo].[" + tableName + "]([TransactionNumber][int] IDENTITY(1, 1) NOT NULL,[Level] [nvarchar] (50) NOT NULL," +
                     "[Message] [nvarchar] (500) NOT NULL,[EventTime] [datetime]NOT NULL,CONSTRAINT[PK_BatchDemoTable] " +
@@ -25,8 +25,8 @@ namespace System.Data.SqlClient.ManualTesting.Tests
                     "IGNORE_DUP_KEY = OFF,ALLOW_ROW_LOCKS = ON,ALLOW_PAGE_LOCKS = ON,FILLFACTOR = 90) ON[PRIMARY]) ON[PRIMARY]";
 
                 using (var connection = new SqlConnection(tcpConnStr))
+                using (var cmd = new SqlCommand(createTableQuery, connection))
                 {
-                    var cmd = new SqlCommand(createTableQuery, connection);
                     connection.Open();
                     cmd.ExecuteNonQuery();
                 }
@@ -39,15 +39,16 @@ namespace System.Data.SqlClient.ManualTesting.Tests
             }
             finally
             {
+                var dropTableQuery = "DROP TABLE IF EXISTS " + tableName;
                 using (var connection = new SqlConnection(tcpConnStr))
+                using (var cmd = new SqlCommand(dropTableQuery, connection))
                 {
-                    var dropTableQuery = "DROP TABLE IF EXISTS " + tableName;
-                    var cmd = new SqlCommand(dropTableQuery, connection);
                     connection.Open();
                     cmd.ExecuteNonQuery();
                 }
             }
         }
+
 
         public class EventInfo
         {
@@ -64,19 +65,18 @@ namespace System.Data.SqlClient.ManualTesting.Tests
         public static void ExecuteNonQueries()
         {
             entities = new List<EventInfo>
-        {
-            new EventInfo {Level = "L1", Message = "Message 1"},
-            new EventInfo {Level = "L2", Message = "Message 2"},
-            new EventInfo {Level = "L3", Message = "Message 3"},
-            new EventInfo {Level = "L4", Message = "Message 4"},
-        };
+            {
+                new EventInfo {Level = "L1", Message = "Message 1"},
+                new EventInfo {Level = "L2", Message = "Message 2"},
+                new EventInfo {Level = "L3", Message = "Message 3"},
+                new EventInfo {Level = "L4", Message = "Message 4"},
+            };
 
             var sql = "INSERT INTO BatchDemoTable(Level, Message, EventTime)  VALUES(@Level, @Message, @EventTime)";
-
             using (var connection = new SqlConnection(tcpConnStr))
+            using (var adapter = new SqlDataAdapter())
+            using (var cmd = new SqlCommand(sql, connection))
             {
-                var adapter = new SqlDataAdapter();
-                var cmd = new SqlCommand(sql, connection);
                 cmd.Parameters.Add(new SqlParameter("@Level", System.Data.SqlDbType.NVarChar, 50, "Level"));
                 cmd.Parameters.Add(new SqlParameter("@Message", SqlDbType.NVarChar, 500, "Message"));
                 cmd.Parameters.Add(new SqlParameter("@EventTime", SqlDbType.DateTime, 0, "EventTime"));
@@ -104,7 +104,6 @@ namespace System.Data.SqlClient.ManualTesting.Tests
                 row["EventTime"] = entity.EventTime;
                 table.Rows.Add(row);
             }
-
             return table;
         }
     }
