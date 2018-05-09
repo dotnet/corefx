043c48139ef73b4ce037bc24315e60820b39d1b8
Bruno Juchli <bruno.juchli@innocreative.ch>
2018-05-09 16:40:39 +0200

    ConcurrentDictionary: improve documentation (#29599)
    * ConcurrentDictionary: improve documentation
    
    Improve the summary of ConcurrentyDictionary.TryUpdate. The summary mentions parameters in a different order and with different names than they are represented in the method signature. This is confusing.
    To clear things up, I've replaced the "verbal" parameter references by `<paramref name="XYZ"/>`.
    
    * ConcurrentDictionary: improve documentation
    
    Improving summary of ConcurrentDictionary.TryUpdate by mentioning parameters in the same order as they occur in the method signature.

--- src/System.Collections.Concurrent/src/System/Collections/Concurrent/ConcurrentDictionary.cs
+++ src/System.Collections.Concurrent/src/System/Collections/Concurrent/ConcurrentDictionary.cs
@@ -458,8 +458,8 @@ namespace System.Collections.Concurrent
         }
 
         /// <summary>
-        /// Compares the existing value for the specified key with a specified value, and if they're equal,
-        /// updates the key with a third value.
+        /// Updates the value associated with <paramref name="key"/> to <paramref name="newValue"/> if the existing value is equal
+        /// to <paramref name="comparisonValue"/>.
         /// </summary>
         /// <param name="key">The key whose value is compared with <paramref name="comparisonValue"/> and
         /// possibly replaced.</param>
@@ -479,8 +479,8 @@ namespace System.Collections.Concurrent
         }
 
         /// <summary>
-        /// Compares the existing value for the specified key with a specified value, and if they're equal,
-        /// updates the key with a third value.
+        /// Updates the value associated with <paramref name="key"/> to <paramref name="newValue"/> if the existing value is equal
+        /// to <paramref name="comparisonValue"/>.
         /// </summary>
         /// <param name="key">The key whose value is compared with <paramref name="comparisonValue"/> and
         /// possibly replaced.</param>
