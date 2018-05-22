ae43e6ed728de54b6ec9642d31da0a327774f3b0
Caesar Chen <caesar1995@users.noreply.github.com>
2018-05-22 15:31:24 -0700

    Disable HttpListener tests on Nano (#29839)
    * disable HttpListener tests on Nano
    
    * disable one more test class
    
    * add comment

--- src/System.Net.HttpListener/tests/HttpListenerAuthenticationTests.cs
+++ src/System.Net.HttpListener/tests/HttpListenerAuthenticationTests.cs
@@ -13,6 +13,7 @@ using Xunit;
 
 namespace System.Net.Tests
 {
+    [ConditionalClass(typeof(PlatformDetection), nameof(PlatformDetection.IsNotWindowsNanoServer))] // httpsys component missing in Nano.
     public class HttpListenerAuthenticationTests : IDisposable
     {
         private const string Basic = "Basic";
