51fedd274c4d8b1b206c6cc58775e499b10532a3
Keerat Singh <v-kesin@microsoft.com>
2018-05-23 15:53:49 -0700

    Added headers to SqlFileStreamTest.cs

--- src/System.Data.SqlClient/tests/ManualTests/SQL/SqlFileStreamTest/SqlFileStreamTest.cs
+++ src/System.Data.SqlClient/tests/ManualTests/SQL/SqlFileStreamTest/SqlFileStreamTest.cs
@@ -1,4 +1,8 @@
-﻿using System.Data.SqlTypes;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Data.SqlTypes;
 using System.IO;
 using Xunit;
 
