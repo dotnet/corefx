64c32360758091a432b49d6986bf35275854c7c3
Calvin <calvin@cmpct.info>
2018-05-31 13:00:31 -0400

    Forgot to include where AIX deviates from the rest of the world on poll

--- src/Native/Unix/System.Native/pal_io.h
+++ src/Native/Unix/System.Native/pal_io.h
@@ -273,12 +273,22 @@ enum SysConfName
  */
 enum PollEvents
 {
+#if defined(_AIX)
+/* of course AIX is different */
+    PAL_POLLIN = 0x0001,   /* non-urgent readable data available */
+    PAL_POLLPRI = 0x0004,  /* urgent readable data available */
+    PAL_POLLOUT = 0x0002,  /* data can be written without blocked */
+    PAL_POLLERR = 0x4000,  /* an error occurred */
+    PAL_POLLHUP = 0x2000,  /* the file descriptor hung up */
+    PAL_POLLNVAL = 0x8000, /* the requested events were invalid */
+#else
     PAL_POLLIN = 0x0001,   /* non-urgent readable data available */
     PAL_POLLPRI = 0x0002,  /* urgent readable data available */
     PAL_POLLOUT = 0x0004,  /* data can be written without blocked */
     PAL_POLLERR = 0x0008,  /* an error occurred */
     PAL_POLLHUP = 0x0010,  /* the file descriptor hung up */
     PAL_POLLNVAL = 0x0020, /* the requested events were invalid */
+#endif
 };
 
 /**
