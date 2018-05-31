045683df3e8a1ca98aec629d0c1906fcb98d48b0
Koundinya Veluri <kouvel@microsoft.com>
2018-05-30 17:45:01 -0700

    Change 0 to -1 to disable Sleep(1)

--- src/System.Threading/tests/SpinWaitTests.netcoreapp.cs
+++ src/System.Threading/tests/SpinWaitTests.netcoreapp.cs
@@ -13,20 +13,22 @@ namespace System.Threading.Tests
         {
             SpinWait spinner = new SpinWait();
 
-            AssertExtensions.Throws<ArgumentOutOfRangeException>("sleep1Threshold", () => spinner.SpinOnce(-1));
+            AssertExtensions.Throws<ArgumentOutOfRangeException>("sleep1Threshold", () => spinner.SpinOnce(-2));
             AssertExtensions.Throws<ArgumentOutOfRangeException>("sleep1Threshold", () => spinner.SpinOnce(int.MinValue));
             Assert.Equal(0, spinner.Count);
 
-            spinner.SpinOnce(sleep1Threshold: 0);
+            spinner.SpinOnce(sleep1Threshold: -1);
             Assert.Equal(1, spinner.Count);
-            spinner.SpinOnce(sleep1Threshold: 1);
+            spinner.SpinOnce(sleep1Threshold: 0);
             Assert.Equal(2, spinner.Count);
-            spinner.SpinOnce(sleep1Threshold: int.MaxValue);
+            spinner.SpinOnce(sleep1Threshold: 1);
             Assert.Equal(3, spinner.Count);
-            int i = 4;
+            spinner.SpinOnce(sleep1Threshold: int.MaxValue);
+            Assert.Equal(4, spinner.Count);
+            int i = 5;
             for (; i < 10; ++i)
             {
-                spinner.SpinOnce(sleep1Threshold: 0);
+                spinner.SpinOnce(sleep1Threshold: -1);
                 Assert.Equal(i, spinner.Count);
             }
             for (; i < 20; ++i)
