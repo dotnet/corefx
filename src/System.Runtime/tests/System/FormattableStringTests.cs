9b3384fc2002bdc7d03d070f59b98bcbfc110f6a
Mikhail Shilkov <shilkov@gmail.com>
2018-06-02 16:52:09 +0200

    Revert wrong commit

--- src/System.Runtime/tests/System/FormattableStringTests.cs
+++ src/System.Runtime/tests/System/FormattableStringTests.cs
@@ -41,7 +41,6 @@ namespace System.Tests
                     return SuccessExitCode;
                 }).Dispose();
         }
-
         [Fact]
         public static void Invariant_Null_ThrowsArgumentNullException()
         {
@@ -57,7 +56,7 @@ namespace System.Tests
                     CultureInfo.CurrentCulture = new CultureInfo("nl"); // would be 123,456 in Dutch
                     double d = 123.456;
                     string expected = string.Format(CultureInfo.InvariantCulture, "Invariant culture is used {0}", d);
-                    string actual = FormattableString.CurrentCulture($"Invariant culture is used {d}");
+                    string actual = FormattableString.Invariant($"Invariant culture is used {d}");
                     Assert.Equal(expected, actual);
 
                     return SuccessExitCode;
