3f1188d5f559832b3ac8a3b0e85ede3de7320cf8
Mikhail Shilkov <shilkov@gmail.com>
2018-06-02 16:34:55 +0200

    Add tests for old Invariant extension

--- src/System.Runtime/tests/System/FormattableStringTests.cs
+++ src/System.Runtime/tests/System/FormattableStringTests.cs
@@ -41,5 +41,27 @@ namespace System.Tests
                     return SuccessExitCode;
                 }).Dispose();
         }
+        [Fact]
+        public static void Invariant_Null_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => FormattableString.Invariant(null));
+        }
+
+        [Fact]
+        public static void Invariant_DutchCulture_FormatsDoubleBasedOnInvariantCulture()
+        {
+            RemoteInvoke(
+                () =>
+                {
+                    CultureInfo.CurrentCulture = new CultureInfo("nl"); // would be 123,456 in Dutch
+                    double d = 123.456;
+                    string expected = string.Format(CultureInfo.InvariantCulture, "Invariant culture is used {0}", d);
+                    string actual = FormattableString.CurrentCulture($"Invariant culture is used {d}");
+                    Assert.Equal(expected, actual);
+
+                    return SuccessExitCode;
+                }).Dispose();
+        }
+
     }
 }
