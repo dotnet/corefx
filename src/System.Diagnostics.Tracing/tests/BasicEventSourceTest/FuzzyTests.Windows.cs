f5aeb0788b7f1658cfd79021332237e623214ef2
s. galiamov <sgalyamov@gmail.com>
2018-06-02 14:27:38 +0200

    Splitted files for specific classes.

---
+++ src/System.Diagnostics.Tracing/tests/BasicEventSourceTest/FuzzyTests.Windows.cs
@@ -0,0 +1,35 @@
+﻿#if USE_ETW
+using Microsoft.Diagnostics.Tracing.Session;
+#endif
+
+using System;
+using Xunit;
+
+namespace BasicEventSourceTests
+{
+    public partial class FuzzyTests
+    {
+        /// <summary>
+        /// Tests the EventSource.Write[T] method (can only use the self-describing mechanism) for Windows platform.
+        /// </summary>
+        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsWindows))]
+        public void Test_Write_Fuzzy_IsWindows()
+        {
+            TestFixture((logger, tests) =>
+            {
+                if (TestUtilities.IsProcessElevated)
+                {
+                    using (var listener = new EtwListener())
+                    {
+                        EventTestHarness.RunTests(tests, listener, logger);
+                    }
+                }
+
+                using (var listener = new EventListenerListener())
+                {
+                    EventTestHarness.RunTests(tests, listener, logger);
+                }
+            });
+        }
+    }
+}
--- src/System.Diagnostics.Tracing/tests/BasicEventSourceTest/FuzzyTests.cs
+++ src/System.Diagnostics.Tracing/tests/BasicEventSourceTest/FuzzyTests.cs
@@ -10,13 +10,10 @@ using Microsoft.Diagnostics.Tracing;
 using System.Diagnostics.Tracing;
 #endif
 using Xunit;
-#if USE_ETW
-using Microsoft.Diagnostics.Tracing.Session;
-#endif
 
 namespace BasicEventSourceTests
 {
-    public class FuzzyTests
+    public partial class FuzzyTests
     {
         /// <summary>
         /// Tests the EventSource.Write[T] method (can only use the self-describing mechanism) for not Windows Nano server.
@@ -26,29 +23,6 @@ namespace BasicEventSourceTests
         {
             TestFixture((logger, tests) =>
             {
-                using (var listener = new EventListenerListener())
-                {
-                    EventTestHarness.RunTests(tests, listener, logger);
-                }
-            });
-        }
-
-        /// <summary>
-        /// Tests the EventSource.Write[T] method (can only use the self-describing mechanism) for Windows platform.
-        /// </summary>
-        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsWindows))]
-        public void Test_Write_Fuzzy_IsWindows()
-        {
-            TestFixture((logger, tests) =>
-            {
-                if (TestUtilities.IsProcessElevated)
-                {
-                    using (var listener = new EtwListener())
-                    {
-                        EventTestHarness.RunTests(tests, listener, logger);
-                    }
-                }
-
                 using (var listener = new EventListenerListener())
                 {
                     EventTestHarness.RunTests(tests, listener, logger);
