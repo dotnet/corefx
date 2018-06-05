ca9e22430e60ca5f0d28ab2a8d65e1a2e9647bb6
Tom Deseyn <tom.deseyn@gmail.com>
2018-06-05 20:27:09 +0200

    Skip certificates we can't read when populating machine store. (#29973)
    * Skip certificates we can't read when populating machine store.
    
    This is a regression in 2.1.0 (https://github.com/dotnet/corefx/pull/29351).
    
    Fixes https://github.com/dotnet/corefx/issues/29942.
    
    * Add test
    
    * Assert chmod returns 0
    
    * Skip test on OSX
    
    * Add valid content to the unreadable cert file

--- src/System.Security.Cryptography.X509Certificates/tests/X509StoreTests.cs
+++ src/System.Security.Cryptography.X509Certificates/tests/X509StoreTests.cs
@@ -2,11 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
 using Xunit;
 
 namespace System.Security.Cryptography.X509Certificates.Tests
 {
-    public class X509StoreTests
+    public class X509StoreTests : RemoteExecutorTestBase
     {
         [Fact]
         public static void OpenMyStore()
@@ -496,5 +499,48 @@ namespace System.Security.Cryptography.X509Certificates.Tests
                 Assert.Equal(0, store.Certificates.Count);
             }
         }
+
+        [Fact]
+        [PlatformSpecific(TestPlatforms.Linux)] // Windows/OSX doesn't use SSL_CERT_{DIR,FILE}.
+        private void X509Store_MachineStoreLoadSkipsInvalidFiles()
+        {
+            // We create a folder for our machine store and use it by setting SSL_CERT_{DIR,FILE}.
+            // In the store we'll add some invalid files, but we start and finish with a valid file.
+            // This is to account for the order in which the store is populated.
+            string sslCertDir = GetTestFilePath();
+            Directory.CreateDirectory(sslCertDir);
+
+            // Valid file.
+            File.WriteAllBytes(Path.Combine(sslCertDir, "0.pem"), TestData.SelfSigned1PemBytes);
+
+            // File with invalid content.
+            File.WriteAllText(Path.Combine(sslCertDir, "1.pem"), "This is not a valid cert");
+
+            // File which is not readable by the current user.
+            string unreadableFileName = Path.Combine(sslCertDir, "2.pem");
+            File.WriteAllBytes(unreadableFileName, TestData.SelfSigned2PemBytes);
+            Assert.Equal(0, chmod(unreadableFileName, 0));
+
+            // Valid file.
+            File.WriteAllBytes(Path.Combine(sslCertDir, "3.pem"), TestData.SelfSigned3PemBytes);
+
+            var psi = new ProcessStartInfo();
+            psi.Environment.Add("SSL_CERT_DIR", sslCertDir);
+            psi.Environment.Add("SSL_CERT_FILE", "/nonexisting");
+            RemoteInvoke(() =>
+            {
+                using (var store = new X509Store(StoreName.Root, StoreLocation.LocalMachine))
+                {
+                    store.Open(OpenFlags.OpenExistingOnly);
+
+                    // Check nr of certificates in store.
+                    Assert.Equal(2, store.Certificates.Count);
+                }
+                return SuccessExitCode;
+            }, new RemoteInvokeOptions { StartInfo = psi }).Dispose();
+        }
+
+        [DllImport("libc")]
+        private static extern int chmod(string path, int mode);
     }
 }
