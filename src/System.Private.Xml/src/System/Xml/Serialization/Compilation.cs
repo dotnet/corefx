389a7d04d8d7acd9d8335a6efe4d636c38b6d5f9
yujayee <yujayee@gmail.com>
2018-06-08 16:19:46 -0700

    revert adding log

--- src/System.Private.Xml/src/System/Xml/Serialization/Compilation.cs
+++ src/System.Private.Xml/src/System/Xml/Serialization/Compilation.cs
@@ -23,7 +23,6 @@ namespace System.Xml.Serialization
     using System.Xml.Extensions;
     using System.Linq;
     using System.Xml.Serialization;
-    using System.Security.Permissions;
 
     internal class TempAssembly
     {
@@ -194,7 +193,6 @@ namespace System.Xml.Serialization
                     {
                         throw;
                     }
-                    Log(e.Message, EventLogEntryType.Information);
                     byte[] token = name.GetPublicKeyToken();
                     if (token != null && token.Length > 0)
                     {
@@ -209,14 +207,13 @@ namespace System.Xml.Serialization
                     {
                         throw new Exception(SR.Format(SR.FailLoadAssemblyUnderPregenMode, serializerName));
                     }
-                    Log(SR.Format(SR.Format(SR.XmlPregenCannotLoad, serializerName)), EventLogEntryType.Information);
+
                     return null;
                 }
 
 #if !FEATURE_SERIALIZATION_UAPAOT
                 if (!IsSerializerVersionMatch(serializer, type, defaultNamespace))
                 {
-                    Log(SR.Format(SR.XmlSerializerExpiredDetails, serializerName, type.FullName), EventLogEntryType.Error);
                     return null;
                 }
 #endif
@@ -254,14 +251,8 @@ namespace System.Xml.Serialization
             contract = (XmlSerializerImplementation)Activator.CreateInstance(contractType);
             if (contract.CanSerialize(type))
                 return serializer;
-            Log(SR.Format(SR.XmlSerializerExpiredDetails, serializerName, type.FullName), EventLogEntryType.Error);
-            return null;
-        }
 
-        private static void Log(string message, EventLogEntryType type)
-        {
-            new EventLogPermission(PermissionState.Unrestricted).Assert();
-            EventLog.WriteEntry("XmlSerializer", message, type);
+            return null;
         }
 
 #if !FEATURE_SERIALIZATION_UAPAOT
