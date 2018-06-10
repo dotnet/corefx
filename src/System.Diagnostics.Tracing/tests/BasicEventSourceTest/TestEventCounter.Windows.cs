154feed1d09689fbe4fb4213b0472428823f66e8
s. galiamov <sgalyamov@gmail.com>
2018-06-10 12:48:04 +0200

    try use ConditionalFact only

--- src/System.Diagnostics.Tracing/tests/BasicEventSourceTest/TestEventCounter.Windows.cs
+++ src/System.Diagnostics.Tracing/tests/BasicEventSourceTest/TestEventCounter.Windows.cs
@@ -10,7 +10,7 @@ namespace BasicEventSourceTests
     public partial class TestEventCounter
     {
         // Specifies whether the process is elevated or not.
-        private static readonly Lazy<bool> s_isElevated = new Lazy<bool>(() => AdminHelpers.IsProcessElevated());
+        private static readonly Lazy<bool> s_isElevated = new Lazy<bool>(AdminHelpers.IsProcessElevated);
         private static bool IsProcessElevated => s_isElevated.Value;
 
         [ConditionalFact(nameof(IsProcessElevated))]
