b0edc960e855e159fb9d329df08d47904963d75e
Hugh Bellamy <hughbellars@gmail.com>
2018-06-13 16:50:15 +0100

    Add Syndication tests

---
+++ src/System.ServiceModel.Syndication/tests/System/ServiceModel/Syndication/SyndicationVersionsTests.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace System.ServiceModel.Syndication.Tests
+{
+    public class SyndicationVersionsTests
+    {
+        [Fact]
+        public void Atom10_Get_ReturnsExpected()
+        {
+            Assert.Equal("Atom10", SyndicationVersions.Atom10);
+        }
+        [Fact]
+        public void Rss20_Get_ReturnsExpected()
+        {
+            Assert.Equal("Rss20", SyndicationVersions.Rss20);
+        }
+    }
+}
