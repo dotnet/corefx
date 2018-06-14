0433806c7675db07cf8ede44805d7f25cf5e920e
danmosemsft <danmose@microsoft.com>
2018-06-13 17:24:55 -0700

    Move #pragma warning disable BCL0015 to baseline files

--- src/System.Reflection.Metadata/src/System/Reflection/Internal/Utilities/FileStreamReadLightUp.cs
+++ src/System.Reflection.Metadata/src/System/Reflection/Internal/Utilities/FileStreamReadLightUp.cs
@@ -79,7 +79,6 @@ namespace System.Reflection.Internal
             return true;
         }
 
-#pragma warning disable BCL0015 // Disable Pinvoke analyzer errors.
         [DllImport(@"kernel32.dll", EntryPoint = "ReadFile", ExactSpelling = true, SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static unsafe extern bool ReadFile(
@@ -89,6 +88,5 @@ namespace System.Reflection.Internal
              out int bytesRead,
              IntPtr overlapped
         );
-#pragma warning restore BCL0015
     }
 }
