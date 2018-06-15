976e3daf0ef7990229a048e95940c2b2fffd0e24
rossslaney <ross@slaney.org>
2018-06-14 19:41:04 -0700

    ported majority of non-redundant overflow tests from corefxlab

--- src/System.Memory/tests/Span/Overflow.cs
+++ src/System.Memory/tests/Span/Overflow.cs
@@ -60,11 +60,229 @@ namespace System.SpanTests
             }
         }
 
+
+        [Fact]
+        public static void SliceStartInt32Overflow()
+        {
+            IntPtr huge;
+            try
+            {
+                huge = Marshal.AllocHGlobal(new IntPtr(ThreeGiB));
+            }
+            catch (Exception)
+            {
+                return;  // It's not implausible to believe that a 3gb allocation will fail - if so, skip this test to avoid unnecessary test flakiness.
+            }
+
+            try
+            {
+                unsafe
+                {
+                    int
+                        GuidThreeGiBLimit = (int)(ThreeGiB / sizeof(Guid)),
+                        GuidTwoGiBLimit = (int)(TwoGiB / sizeof(Guid)),
+                        GuidOneGiBLimit = (int)(OneGiB / sizeof(Guid));
+
+
+                    var span = new Span<Guid>((void*)huge, GuidThreeGiBLimit);
+                    Guid guid = Guid.NewGuid();
+                    var slice = span.Slice(GuidTwoGiBLimit + 1);
+                    slice[0] = guid;
+
+                    slice = span.Slice(GuidOneGiBLimit).Slice(1).Slice(GuidOneGiBLimit);
+                    Assert.Equal(guid, slice[0]);
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(huge);
+            }
+        }
+
+        [Fact]
+        public static void SliceStartUInt32Overflow()
+        {
+            IntPtr huge;
+            try
+            {
+                huge = Marshal.AllocHGlobal(new IntPtr(ThreeGiB));
+            }
+            catch (Exception)
+            {
+                return;  // It's not implausible to believe that a 3gb allocation will fail - if so, skip this test to avoid unnecessary test flakiness.
+            }
+
+            try
+            {
+                unsafe
+                {
+                    int
+                        GuidThreeGiBLimit = (int)(ThreeGiB / sizeof(Guid)),
+                        GuidTwoGiBLimit = (int)(TwoGiB / sizeof(Guid)),
+                        GuidOneGiBLimit = (int)(OneGiB / sizeof(Guid));
+                    var span = new Span<Guid>((void*)huge, GuidThreeGiBLimit);
+                    Guid guid = Guid.NewGuid();
+                    var slice = span.Slice((GuidTwoGiBLimit + 1));
+                    slice[0] = guid;
+
+                    slice = span.Slice(GuidOneGiBLimit).Slice(1).Slice(GuidOneGiBLimit);
+                    Assert.Equal(guid, slice[0]);
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(huge);
+            }
+        }
+
+        [Fact]
+        public static void ArrayCtorStartOverflow()
+        {
+            var arr = new Guid[20];
+
+            var slice = arr.AsSpan().Slice(2);
+            Guid guid = Guid.NewGuid();
+            slice[1] = guid;
+
+            Assert.Equal(guid, arr[3]);
+        }
+
+        [Fact]
+        public static void ReadOnlySliceStartInt32Overflow()
+        {
+            IntPtr huge;
+            try
+            {
+                huge = Marshal.AllocHGlobal(new IntPtr(ThreeGiB));
+            }
+            catch (Exception)
+            {
+                return;  // It's not implausible to believe that a 3gb allocation will fail - if so, skip this test to avoid unnecessary test flakiness.
+            }
+
+            try
+            {
+                unsafe
+                {
+                    int
+                        GuidThreeGiBLimit = (int)(ThreeGiB / sizeof(Guid)),
+                        GuidTwoGiBLimit = (int)(TwoGiB / sizeof(Guid)),
+                        GuidOneGiBLimit = (int)(OneGiB / sizeof(Guid));
+                    var mutable = new Span<Guid>((void*)huge, GuidThreeGiBLimit);
+                    var span = new ReadOnlySpan<Guid>((void*)huge, GuidThreeGiBLimit);
+                    Guid guid = Guid.NewGuid();
+                    var slice = span.Slice(GuidTwoGiBLimit + 1);
+                    mutable[GuidTwoGiBLimit + 1] = guid;
+                    Assert.Equal(guid, slice[0]);
+
+                    slice = span.Slice(GuidOneGiBLimit).Slice(1).Slice(GuidOneGiBLimit);
+                    Assert.Equal(guid, slice[0]);
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(huge);
+            }
+        }
+
+        [Fact]
+        public static void ReadOnlySliceStartUInt32Overflow()
+        {
+            IntPtr huge;
+            try
+            {
+                huge = Marshal.AllocHGlobal(new IntPtr(ThreeGiB));
+            }
+            catch (Exception)
+            {
+                return;  // It's not implausible to believe that a 3gb allocation will fail - if so, skip this test to avoid unnecessary test flakiness.
+            }
+
+            try
+            {
+                unsafe
+                {
+                    int
+                        GuidThreeGiBLimit = (int)(ThreeGiB / sizeof(Guid)),
+                        GuidTwoGiBLimit = (int)(TwoGiB / sizeof(Guid)),
+                        GuidOneGiBLimit = (int)(OneGiB / sizeof(Guid));
+
+                    var mutable = new Span<Guid>((void*)huge, GuidThreeGiBLimit);
+                    var span = new ReadOnlySpan<Guid>((void*)huge, GuidThreeGiBLimit);
+                    Guid guid = Guid.NewGuid();
+                    var slice = span.Slice((GuidTwoGiBLimit + 1));
+                    mutable[GuidTwoGiBLimit + 1] = guid;
+                    Assert.Equal(guid, slice[0]);
+
+                    slice = span.Slice(GuidOneGiBLimit).Slice(1).Slice(GuidOneGiBLimit);
+                    Assert.Equal(guid, slice[0]);
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(huge);
+            }
+        }
+
+        [Fact]
+        public static void ReadOnlySliceStartLengthInt32Overflow()
+        {
+            IntPtr huge;
+            try
+            {
+                huge = Marshal.AllocHGlobal(new IntPtr(ThreeGiB));
+            }
+            catch (Exception)
+            {
+                return;  // It's not implausible to believe that a 3gb allocation will fail - if so, skip this test to avoid unnecessary test flakiness.
+            }
+
+            try
+            {
+                unsafe
+                {
+                    int
+                        GuidThreeGiBLimit = (int)(ThreeGiB / sizeof(Guid)),
+                        GuidTwoGiBLimit = (int)(TwoGiB / sizeof(Guid)),
+                        GuidOneGiBLimit = (int)(OneGiB / sizeof(Guid));
+
+                    var mutable = new Span<Guid>((void*)huge, GuidThreeGiBLimit);
+                    var span = new ReadOnlySpan<Guid>((void*)huge, GuidThreeGiBLimit);
+                    Guid guid = Guid.NewGuid();
+                    var slice = span.Slice(GuidTwoGiBLimit + 1, 20);
+                    mutable[GuidTwoGiBLimit + 1] = guid;
+                    Assert.Equal(guid, slice[0]);
+
+                    slice = span.Slice(GuidOneGiBLimit).Slice(1).Slice(GuidOneGiBLimit);
+                    Assert.Equal(guid, slice[0]);
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(huge);
+            }
+        }
+
+        [Fact]
+        public static void ReadOnlyArrayCtorStartLengthOverflow()
+        {
+            var arr = new Guid[20];
+
+            var slice = new ReadOnlySpan<Guid>(arr, 2, 2);
+            Guid guid = Guid.NewGuid();
+            arr[3] = guid;
+
+            Assert.Equal(guid, slice[1]);
+        }
+
         private const long ThreeGiB = 3L * 1024L * 1024L * 1024L;
         private const long TwoGiB = 2L * 1024L * 1024L * 1024L;
         private const long OneGiB = 1L * 1024L * 1024L * 1024L;
 
+        
+
         private static readonly int s_guidThreeGiBLimit = (int)(ThreeGiB / Unsafe.SizeOf<Guid>());  // sizeof(Guid) requires unsafe keyword and I don't want to mark the entire class unsafe.
         private static readonly int s_guidTwoGiBLimit = (int)(TwoGiB / Unsafe.SizeOf<Guid>());
     }
+
 }
