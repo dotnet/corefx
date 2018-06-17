7a27c0d696659b35ada8cc8428b1948ff55a9de3
jswolf19 <jswolf19@hotmail.com>
2018-06-17 12:43:56 +0900

    added tests to illustrate behavior of CallerArgumentExpressionAttribute (final behavior may differ)

--- src/System.Runtime/tests/System/Runtime/CompilerServices/CallerArgumentExpressionAttributeTests.cs
+++ src/System.Runtime/tests/System/Runtime/CompilerServices/CallerArgumentExpressionAttributeTests.cs
@@ -80,5 +80,129 @@ namespace System.Runtime.CompilerServices.Tests
         {
             return expr;
         }
+
+        [Fact(Skip = "Not yet implemented in compiler. Final behavior may differ.")]
+        [ActiveIssue("https://github.com/dotnet/roslyn/issues/19605", TestPlatforms.Any)]
+        public static void SimpleExpression()
+        {
+            int notVal = 0;
+
+            Assert.Equal("notVal", IntParamMethod(notVal));
+        }
+
+        [Fact(Skip = "Not yet implemented in compiler. Final behavior may differ.")]
+        [ActiveIssue("https://github.com/dotnet/roslyn/issues/19605", TestPlatforms.Any)]
+        public static void ComplexExpression()
+        {
+            int x = 5;
+
+            Assert.Equal("Math.Min(x + 20, x * x)",
+                IntParamMethod(Math.Min(x + 20, x * x)));
+        }
+
+        [Fact(Skip = "Not yet implemented in compiler. Final behavior may differ.")]
+        [ActiveIssue("https://github.com/dotnet/roslyn/issues/19605", TestPlatforms.Any)]
+        public static void SurroundingWhitespaceHandling()
+        {
+            int notVal = 0;
+
+            Assert.Equal("notVal", IntParamMethod(notVal));
+        }
+
+        [Fact(Skip = "Not yet implemented in compiler. Final behavior may differ.")]
+        [ActiveIssue("https://github.com/dotnet/roslyn/issues/19605", TestPlatforms.Any)]
+        public static void InternalWhitespaceHandling()
+        {
+            int notVal = 0;
+
+            Assert.Equal("notVal  + 20", IntParamMethod(notVal + 20));
+
+            Assert.Equal(@"Math.Min(notVal * 2,
+                    notVal + 20)",
+                IntParamMethod(Math.Min(notVal * 2,
+                    notVal + 20)));
+        }
+
+        [Fact(Skip = "Not yet implemented in compiler. Final behavior may differ.")]
+        [ActiveIssue("https://github.com/dotnet/roslyn/issues/19605", TestPlatforms.Any)]
+        public static void InternalCommentHandling()
+        {
+            int notVal = 0;
+
+            Assert.Equal("notVal + /*comment*/20", IntParamMethod(notVal + /*comment*/20));
+            Assert.Equal("notVal + 20 //comment",
+                IntParamMethod(notVal + 20 //comment
+                ));
+        }
+
+        [Fact(Skip = "Not yet implemented in compiler. Final behavior may differ.")]
+        [ActiveIssue("https://github.com/dotnet/roslyn/issues/19605", TestPlatforms.Any)]
+        public static void OptionalParameterHandling()
+        {
+            string suppliedVal = "supplied value";
+            Assert.Equal("suppliedVal", OptionalParamMethod(suppliedVal));
+            Assert.Equal("suppliedVal", OptionalParamMethod(val: suppliedVal));
+
+            Assert.Equal("StringConst + \" string literal\"", OptionalParamMethod());
+
+            Assert.Equal("\"no file\"", CompilerSuppliedParamMethod());
+        }
+
+        private const string StringConst = "hello";
+        private static string OptionalParamMethod(string val = StringConst + " string literal", [CallerArgumentExpression("val")] string expr = null)
+        {
+            return expr;
+        }
+
+        private static string CompilerSuppliedParamMethod([CallerFilePath] string val = "no file", [CallerArgumentExpression("val")] string expr = null)
+        {
+            return expr;
+        }
+
+        [Fact(Skip = "Not yet implemented in compiler. Final behavior may differ.")]
+        [ActiveIssue("https://github.com/dotnet/roslyn/issues/19605", TestPlatforms.Any)]
+        public static void ExtensionMethodThisParameterHandling()
+        {
+            int notVal = 0;
+
+            Assert.Equal("notVal", notVal.ExtensionMethod());
+        }
+
+        private static string ExtensionMethod(this int val, [CallerArgumentExpression("val")] string expr = null)
+        {
+            return expr;
+        }
+
+        [Fact(Skip = "Not yet implemented in compiler. Final behavior may differ.")]
+        [ActiveIssue("https://github.com/dotnet/roslyn/issues/19605", TestPlatforms.Any)]
+        public static void InstanceMethodThisHandling()
+        {
+            var instance = new InstanceTest();
+
+            Assert.Equal("instance", instance.Method());
+            Assert.Equal("new InstanceTest()", new InstanceTest().Method());
+            Assert.Equal("(instance ?? new InstanceTest())", (instance ?? new InstanceTest()).Method());
+
+            Assert.Equal("", instance.NoThisMethodCaller());
+            Assert.Equal("this", instance.ThisMethodCaller());
+        }
+
+        private class InstanceTest
+        {
+            public string NoThisMethodCaller()
+            {
+                return Method();
+            }
+
+            public string ThisMethodCaller()
+            {
+                return this.Method();
+            }
+
+            public string Method([CallerArgumentExpression("this")] string expr = null)
+            {
+                return expr;
+            }
+        }
     }
 }
