e2ad0ad2433062a5315596af176a02028c4be5ee
Vance Morrison <vancemorrison@comcast.net>
2018-06-18 09:18:53 -0700

    Expose and Add tests for StringBuilder overloads of TextWriter
    Add tests for implementation in https://github.com/dotnet/coreclr/pull/18281
    This supports the issue https://github.com/dotnet/corefx/issues/30048

--- src/System.IO/tests/TextWriter/TextWriterTests.netcoreapp.cs
+++ src/System.IO/tests/TextWriter/TextWriterTests.netcoreapp.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -50,6 +52,63 @@ namespace System.IO.Tests
                 var rs = new Memory<char>(TestDataProvider.CharData, 4, 6);
                 await tw.WriteLineAsync(rs);
                 Assert.Equal(new string(rs.Span) + tw.NewLine, tw.Text);
+            }
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void WriteStringBuilderTest(bool isSynchronized)
+        {
+            using (CharArrayTextWriter ctw = NewTextWriter)
+            {
+                TextWriter tw = isSynchronized ? TextWriter.Synchronized(ctw) : ctw;
+                tw.Write(new StringBuilder(new string(TestDataProvider.CharData)));
+                tw.Flush();
+                Assert.Equal(new string(TestDataProvider.CharData), ctw.Text);
+            }
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void WriteLineStringBuilderTest(bool isSynchronized)
+        {
+            using (CharArrayTextWriter ctw = NewTextWriter)
+            {
+                TextWriter tw = isSynchronized ? TextWriter.Synchronized(ctw) : ctw;
+                tw.Write(new StringBuilder(new string(TestDataProvider.CharData)));
+                tw.Flush();
+                Assert.Equal(new string(TestDataProvider.CharData), ctw.Text);
+            }
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+
+        public async void WriteAsyncStringBuilderTest(bool isSynchronized)
+        {
+            using (CharArrayTextWriter ctw = NewTextWriter)
+            {
+                TextWriter tw = isSynchronized ? TextWriter.Synchronized(ctw) : ctw;
+                await tw.WriteAsync(new StringBuilder(new string(TestDataProvider.CharData)));
+                tw.Flush();
+                Assert.Equal(new string(TestDataProvider.CharData), ctw.Text);
+            }
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async void WriteLineAsyncStringBuilderTest(bool isSynchronized)
+        {
+            using (CharArrayTextWriter ctw = NewTextWriter)
+            {
+                TextWriter tw = isSynchronized ? TextWriter.Synchronized(ctw) : ctw;
+                await tw.WriteLineAsync(new StringBuilder(new string(TestDataProvider.CharData)));
+                tw.Flush();
+                Assert.Equal(new string(TestDataProvider.CharData) + tw.NewLine, ctw.Text);
             }
         }
     }
