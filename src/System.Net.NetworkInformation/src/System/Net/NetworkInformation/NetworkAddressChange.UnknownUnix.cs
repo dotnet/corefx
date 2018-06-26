f77687746393b245516efcaa13b0b62463cd9595
wfurt <tweinfurt@yahoo.com>
2018-06-26 10:22:02 -0700

    feedback from review. move RegisterNetworkChange to common file

--- src/System.Net.NetworkInformation/src/System/Net/NetworkInformation/NetworkAddressChange.UnknownUnix.cs
+++ src/System.Net.NetworkInformation/src/System/Net/NetworkInformation/NetworkAddressChange.UnknownUnix.cs
@@ -6,15 +6,12 @@ namespace System.Net.NetworkInformation
 {
     public partial class NetworkChange
     {
-        // Introduced for supporting design-time loading of System.Windows.dll
-       [Obsolete("This API supports the .NET Framework infrastructure and is not intended to be used directly from your code.", true)]
-       public static void RegisterNetworkChange(NetworkChange nc) { }
-
         public static event NetworkAddressChangedEventHandler NetworkAddressChanged
         {
             add { throw new PlatformNotSupportedException(); }
             remove { throw new PlatformNotSupportedException(); }
         }
+
         public static event NetworkAvailabilityChangedEventHandler NetworkAvailabilityChanged
         {
             add { throw new PlatformNotSupportedException(); }
