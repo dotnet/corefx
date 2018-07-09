2d086520b29b5763cb4640508d4455c340e7043c
Anipik <anirudhagnihotry098@gmail.com>
2018-07-09 16:01:23 -0700

    Porting IMarshal

---
+++ src/System.Runtime.InteropServices/src/Microsoft/Win32/IMarshal.cs
@@ -0,0 +1,27 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Win32
+{
+    [ComImport, Guid("00000003-0000-0000-C000-000000000046"),
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IMarshal
+    {
+        int GetUnmarshalClass(ref Guid riid, IntPtr pv, int dwDestContext, IntPtr pvDestContext, int mshlflags, out Guid pCid);
+        int GetMarshalSizeMax(ref Guid riid, IntPtr pv, int dwDestContext, IntPtr pvDestContext, int mshlflags, out int pSize);
+        int MarshalInterface(IntPtr pStm, ref Guid riid, IntPtr pv, int dwDestContext, IntPtr pvDestContext, int mshlflags);
+        int UnmarshalInterface(IntPtr pStm, ref Guid riid, out IntPtr ppv);
+        int ReleaseMarshalData(IntPtr pStm);
+        int DisconnectObject(int dwReserved);
+    }
+
+    internal static class NativeMethods
+    {
+        public const int S_OK = 0x0;
+        public const int E_NOTIMPL = unchecked((int)0x80004001);
+    }
+}
