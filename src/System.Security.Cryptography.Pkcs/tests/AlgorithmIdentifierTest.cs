5998add068a753985085f7ee65ed6064d17d56e6
Maxim Lipnin <mlipnin@gmail.com>
2018-07-10 00:12:03 +0300

    AlgorithmIdentifierTest: add not-null check for Parameters property; use helper method from Test.Cryptography.

--- src/System.Security.Cryptography.Pkcs/tests/AlgorithmIdentifierTest.cs
+++ src/System.Security.Cryptography.Pkcs/tests/AlgorithmIdentifierTest.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Test.Cryptography;
 using Xunit;
 
 namespace System.Security.Cryptography.Pkcs.Tests
@@ -14,7 +15,7 @@ namespace System.Security.Cryptography.Pkcs.Tests
             AlgorithmIdentifier ai = new AlgorithmIdentifier();
             Assert.Equal(0, ai.KeyLength);
             Assert.Equal(Oids.TripleDesCbc, ai.Oid.Value);
-            Assert.Equal(typeof(byte), ai.Parameters.GetType().GetElementType());
+            Assert.NotNull(ai.Parameters);
             Assert.Equal(0, ai.Parameters.Length);
         }
 
@@ -25,7 +26,7 @@ namespace System.Security.Cryptography.Pkcs.Tests
             AlgorithmIdentifier ai = new AlgorithmIdentifier(o);
             Assert.Equal(0, ai.KeyLength);
             Assert.Equal(Oids.Rsa, ai.Oid.Value);
-            Assert.Equal(typeof(byte), ai.Parameters.GetType().GetElementType());
+            Assert.NotNull(ai.Parameters);
             Assert.Equal(0, ai.Parameters.Length);
         }
 
@@ -35,7 +36,7 @@ namespace System.Security.Cryptography.Pkcs.Tests
             AlgorithmIdentifier ai = new AlgorithmIdentifier(null);
             Assert.Null(ai.Oid);
             Assert.Equal(0, ai.KeyLength);
-            Assert.Equal(typeof(byte), ai.Parameters.GetType().GetElementType());
+            Assert.NotNull(ai.Parameters);
             Assert.Equal(0, ai.Parameters.Length);
         }
 
@@ -46,7 +47,7 @@ namespace System.Security.Cryptography.Pkcs.Tests
             AlgorithmIdentifier ai = new AlgorithmIdentifier(o, 128);
             Assert.Equal(128, ai.KeyLength);
             Assert.Equal(Oids.Rsa, ai.Oid.Value);
-            Assert.Equal(typeof(byte), ai.Parameters.GetType().GetElementType());
+            Assert.NotNull(ai.Parameters);
             Assert.Equal(0, ai.Parameters.Length);
         }
 
@@ -56,7 +57,7 @@ namespace System.Security.Cryptography.Pkcs.Tests
             AlgorithmIdentifier ai = new AlgorithmIdentifier(null, 128);
             Assert.Null(ai.Oid);
             Assert.Equal(128, ai.KeyLength);
-            Assert.Equal(typeof(byte), ai.Parameters.GetType().GetElementType());
+            Assert.NotNull(ai.Parameters);
             Assert.Equal(0, ai.Parameters.Length);
         }
 
@@ -67,7 +68,7 @@ namespace System.Security.Cryptography.Pkcs.Tests
             AlgorithmIdentifier ai = new AlgorithmIdentifier(o, -1);
             Assert.Equal(-1, ai.KeyLength);
             Assert.Equal(Oids.Rsa, ai.Oid.Value);
-            Assert.Equal(typeof(byte), ai.Parameters.GetType().GetElementType());
+            Assert.NotNull(ai.Parameters);
             Assert.Equal(0, ai.Parameters.Length);
         }
 
@@ -104,7 +105,7 @@ namespace System.Security.Cryptography.Pkcs.Tests
             {
                 Parameters = new byte[2] { 0x05, 0x00 } // ASN.1 NULL
             };
-            Assert.Equal("05-00", BitConverter.ToString(ai.Parameters));
+            Assert.Equal("0500", ai.Parameters.ByteArrayToHex());
             ai.Parameters = null;
             Assert.Null(ai.Parameters);
         }
