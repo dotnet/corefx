e2fab38451c21b77de5d096d4519cd4be29ca4dd
Stephen Toub <stoub@microsoft.com>
2018-07-11 18:32:14 -0400

    Factor out large generic additions in System.Linq
    New generic types in System.Linq are causing significant size-on-disk increases in AOT builds.  This change addresses some of them:
    
    - The many implementations of IPartition and IIListProvider are causing significant binary size increases for AOT.  I've factored out the majority of those large changes to only be used in a netcoreapp build, not in a uap build.  There are still code paths that test whether an object is IPartition or IIListProvider, but there are no types now in the UAP build that implement them.
    - As part of this, I replaced several places where `EmptyPartition<TResult>.Instance` was being used directly to implement an `IEnumerable<T>`, changed them to use `Enumerable.Empty<T>`, and then changed `Enumerable.Empty<T>` so that on netcoreapp it returns `EmptyPartition<TResult>.Instance`, and on uap it returns `Array.Empty<T>`.  This also means now that code paths lighting-up on partitions will also light-up on `Enumerable.Empty<T>`.  It also means that I changed the empty partition's `IEnumerator.Reset` method to be a nop instead of throwing, to match the same behavior as array's `IEnumerator.Reset`.
    - LargeArrayBuilder uses T[][], which introduces lots of new array types and corresponding interface instantiations, helper types, etc.  I've specialized LargeArrayBuilder in the UAP build to just be a thin wrapper around ArrayBuilder; we still end up with unnecessary instantiations of LargeArrayBuilder, but they're relatively small, and it saves everything to do with those T[][] instantiations.

---
+++ src/System.Linq/src/System/Linq/IIListProvider.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+    /// <summary>
+    /// An iterator that can produce an array or <see cref="List{TElement}"/> through an optimized path.
+    /// </summary>
+    internal interface IIListProvider<TElement> : IEnumerable<TElement>
+    {
+        /// <summary>
+        /// Produce an array of the sequence through an optimized path.
+        /// </summary>
+        /// <returns>The array.</returns>
+        TElement[] ToArray();
+
+        /// <summary>
+        /// Produce a <see cref="List{TElement}"/> of the sequence through an optimized path.
+        /// </summary>
+        /// <returns>The <see cref="List{TElement}"/>.</returns>
+        List<TElement> ToList();
+
+        /// <summary>
+        /// Returns the count of elements in the sequence.
+        /// </summary>
+        /// <param name="onlyIfCheap">If true then the count should only be calculated if doing
+        /// so is quick (sure or likely to be constant time), otherwise -1 should be returned.</param>
+        /// <returns>The number of elements.</returns>
+        int GetCount(bool onlyIfCheap);
+    }
+}
---
+++ src/System.Linq/src/System/Linq/Repeat.netcoreapp.cs
@@ -0,0 +1,85 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+    public static partial class Enumerable
+    {
+        private sealed partial class RepeatIterator<TResult> : IPartition<TResult>
+        {
+            public override IEnumerable<TResult2> Select<TResult2>(Func<TResult, TResult2> selector) =>
+                new SelectIPartitionIterator<TResult, TResult2>(this, selector);
+
+            public TResult[] ToArray()
+            {
+                TResult[] array = new TResult[_count];
+                if (_current != null)
+                {
+                    Array.Fill(array, _current);
+                }
+
+                return array;
+            }
+
+            public List<TResult> ToList()
+            {
+                List<TResult> list = new List<TResult>(_count);
+                for (int i = 0; i != _count; ++i)
+                {
+                    list.Add(_current);
+                }
+
+                return list;
+            }
+
+            public int GetCount(bool onlyIfCheap) => _count;
+
+            public IPartition<TResult> Skip(int count)
+            {
+                if (count >= _count)
+                {
+                    return EmptyPartition<TResult>.Instance;
+                }
+
+                return new RepeatIterator<TResult>(_current, _count - count);
+            }
+
+            public IPartition<TResult> Take(int count)
+            {
+                if (count >= _count)
+                {
+                    return this;
+                }
+
+                return new RepeatIterator<TResult>(_current, count);
+            }
+
+            public TResult TryGetElementAt(int index, out bool found)
+            {
+                if ((uint)index < (uint)_count)
+                {
+                    found = true;
+                    return _current;
+                }
+
+                found = false;
+                return default(TResult);
+            }
+
+            public TResult TryGetFirst(out bool found)
+            {
+                found = true;
+                return _current;
+            }
+
+            public TResult TryGetLast(out bool found)
+            {
+                found = true;
+                return _current;
+            }
+        }
+    }
+}
