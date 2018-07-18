da036834fa4733bd4d7a410451927b39bd1aa6fa
Hugh Bellamy <hughbellars@gmail.com>
2018-07-18 23:41:58 +0100

    Address PR feedback

--- src/System.Runtime.InteropServices/tests/System/Runtime/InteropServices/Marshal/ZeroFreeGlobalAllocUnicodeTests.cs
+++ src/System.Runtime.InteropServices/tests/System/Runtime/InteropServices/Marshal/ZeroFreeGlobalAllocUnicodeTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Security;
 using Xunit;
 
 namespace System.Runtime.InteropServices.Tests
@@ -11,14 +12,28 @@ namespace System.Runtime.InteropServices.Tests
         [Fact]
         public void ZeroFreeGlobalAllocUnicode_ValidPointer_Success()
         {
-            IntPtr ptr = Marshal.StringToHGlobalUni("hello");
-            Marshal.ZeroFreeGlobalAllocUnicode(ptr);
+            using (SecureString secureString = ToSecureString("hello"))
+            {
+                IntPtr ptr = Marshal.SecureStringToGlobalAllocUnicode(secureString);
+                Marshal.ZeroFreeGlobalAllocUnicode(ptr);
+            }
         }
 
         [Fact]
         public void ZeroFreeGlobalAllocUnicode_Zero_Nop()
         {
             Marshal.ZeroFreeGlobalAllocUnicode(IntPtr.Zero);
+        }
+
+        private static SecureString ToSecureString(string data)
+        {
+            var str = new SecureString();
+            foreach (char c in data)
+            {
+                str.AppendChar(c);
+            }
+            str.MakeReadOnly();
+            return str;
         }
     }
 }
