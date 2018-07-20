2f8e2bb0c5f435ab34ae9715703af0b1f47e395e
Jeremy Barton <jbarton@microsoft.com>
2018-07-20 15:20:07 -0700

    Allow SignedCms to verify documents whose signer didn't sort attributes.
    The CMS RFC says that when computing the digest value for a SignerInfo
    which contains attributes that the digest should be over the DER encoded
    SET OF attributes.  DER rules for SET OF include applying a data sort.
    
    Signers exist in the wild which do not appropriately sort their attributes before
    computing the signature, and Windows has a compatibility fallback to allow
    these signers' signatures to be considered valid.  This change adds a similar
    fallback to the managed implementation of SignedCms so that documents
    do not go from valid to invalid when transitioning from .NET Framework to
    .NET Core.

--- src/System.Security.Cryptography.Pkcs/tests/Rfc3161/TimestampTokenTests.cs
+++ src/System.Security.Cryptography.Pkcs/tests/Rfc3161/TimestampTokenTests.cs
@@ -14,6 +14,7 @@ namespace System.Security.Cryptography.Pkcs.Tests
         [Theory]
         [InlineData(nameof(TimestampTokenTestData.FreeTsaDotOrg1))]
         [InlineData(nameof(TimestampTokenTestData.Symantec1))]
+        [InlineData(nameof(TimestampTokenTestData.DigiCert1))]
         public static void ParseDocument(string testDataName)
         {
             TimestampTokenTestData testData = TimestampTokenTestData.GetTestData(testDataName);
@@ -24,6 +25,7 @@ namespace System.Security.Cryptography.Pkcs.Tests
         [Theory]
         [InlineData(nameof(TimestampTokenTestData.FreeTsaDotOrg1))]
         [InlineData(nameof(TimestampTokenTestData.Symantec1))]
+        [InlineData(nameof(TimestampTokenTestData.DigiCert1))]
         public static void ParseDocument_ExcessData(string testDataName)
         {
             TimestampTokenTestData testData = TimestampTokenTestData.GetTestData(testDataName);
