1c854f9a245bc64d6f71efcca2cd9cbaabda086f
Hugh Bellamy <hughbellars@gmail.com>
2018-07-22 15:46:37 +0100

    Cleanup System.Runtime.Serialization.Primitives code

--- src/System.Runtime.Serialization.Primitives/src/System/Runtime/Serialization/DataMemberAttribute.cs
+++ src/System.Runtime.Serialization.Primitives/src/System/Runtime/Serialization/DataMemberAttribute.cs
@@ -10,8 +10,6 @@ namespace System.Runtime.Serialization
         private string _name;
         private bool _isNameSetExplicitly;
         private int _order = -1;
-        private bool _isRequired;
-        private bool _emitDefaultValue = true;
 
         public DataMemberAttribute()
         {
@@ -19,36 +17,32 @@ namespace System.Runtime.Serialization
 
         public string Name
         {
-            get { return _name; }
-            set { _name = value; _isNameSetExplicitly = true; }
+            get => _name;
+            set
+            {
+                _name = value;
+                _isNameSetExplicitly = true;
+            }
         }
 
-        public bool IsNameSetExplicitly
-        {
-            get { return _isNameSetExplicitly; }
-        }
+        public bool IsNameSetExplicitly => _isNameSetExplicitly;
 
         public int Order
         {
-            get { return _order; }
+            get => _order;
             set
             {
                 if (value < 0)
+                {
                     throw new InvalidDataContractException(SR.OrderCannotBeNegative);
+                }
+
                 _order = value;
             }
         }
 
-        public bool IsRequired
-        {
-            get { return _isRequired; }
-            set { _isRequired = value; }
-        }
+        public bool IsRequired { get; set; }
 
-        public bool EmitDefaultValue
-        {
-            get { return _emitDefaultValue; }
-            set { _emitDefaultValue = value; }
-        }
+        public bool EmitDefaultValue { get; set; } = true;
     }
 }
--- src/System.Runtime.Serialization.Primitives/src/System/Runtime/Serialization/InvalidDataContractException.cs
+++ src/System.Runtime.Serialization.Primitives/src/System/Runtime/Serialization/InvalidDataContractException.cs
@@ -3,11 +3,12 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Runtime.CompilerServices;
 
 namespace System.Runtime.Serialization
 {
     [Serializable]
-    [System.Runtime.CompilerServices.TypeForwardedFrom("System.Runtime.Serialization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
+    [TypeForwardedFrom("System.Runtime.Serialization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089")]
     public class InvalidDataContractException : Exception
     {
         public InvalidDataContractException()
