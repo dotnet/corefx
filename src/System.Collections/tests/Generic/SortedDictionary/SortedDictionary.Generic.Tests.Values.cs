4703e3f1cedbc6b36b4486cc6f58d39be6bf66e1
Wraith2 <wraith2@gmail.com>
2018-07-22 20:02:07 +0100

    fix serialization of SortedDictionary.ValueCollection being marked possible

--- src/System.Collections/tests/Generic/SortedDictionary/SortedDictionary.Generic.Tests.Values.cs
+++ src/System.Collections/tests/Generic/SortedDictionary/SortedDictionary.Generic.Tests.Values.cs
@@ -13,6 +13,7 @@ namespace System.Collections.Tests
         protected override bool DefaultValueAllowed => true;
         protected override bool DuplicateValuesAllowed => true;
         protected override bool IsReadOnly => true;
+
         protected override IEnumerable<ModifyEnumerable> GetModifyEnumerables(ModifyOperation operations) => new List<ModifyEnumerable>();
 
         protected override ICollection<string> GenericICollectionFactory()
@@ -64,7 +65,7 @@ namespace System.Collections.Tests
         protected override bool IsReadOnly => true;
         protected override bool Enumerator_Current_UndefinedOperation_Throws => true;
         protected override IEnumerable<ModifyEnumerable> GetModifyEnumerables(ModifyOperation operations) => new List<ModifyEnumerable>();
-        protected override bool SupportsSerialization => true;
+        protected override bool SupportsSerialization => false;
 
         protected override ICollection NonGenericICollectionFactory()
         {
