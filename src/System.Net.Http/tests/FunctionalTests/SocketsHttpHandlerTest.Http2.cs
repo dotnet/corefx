bec8679e311ace74efb11dcc394cee9a916ace37
Max Kerr <rikerr@microsoft.com>
2018-07-31 13:19:41 -0700

    Add test, fix formatting, set appcontext switch.

--- src/System.Net.Http/tests/FunctionalTests/SocketsHttpHandlerTest.Http2.cs
+++ src/System.Net.Http/tests/FunctionalTests/SocketsHttpHandlerTest.Http2.cs
@@ -17,8 +17,6 @@ namespace System.Net.Http.Functional.Tests
 {
     using Configuration = System.Net.Test.Common.Configuration;
 
-    // Note:  Disposing the HttpClient object automatically disposes the handler within. So, it is not necessary
-    // to separately Dispose (or have a 'using' statement) for the handler.
     public sealed class SocketsHttpHandler_HttpClientHandler_Http2_Test : HttpClientTestBase
     {
         protected override bool UseSocketsHttpHandler => true;
@@ -56,7 +54,6 @@ namespace System.Net.Http.Functional.Tests
 
                 // Send a frame despite not having sent the server connection preface.
                 DataFrame invalidFrame = new DataFrame(new byte[10], FrameFlags.Padded, 10, 1);
-                Console.WriteLine("\nServer Sending:\n"+invalidFrame);
                 await server.WriteFrameAsync(invalidFrame);
 
                 // This currently throws an Http2ProtocolException, but that type is not public.
@@ -81,15 +78,12 @@ namespace System.Net.Http.Functional.Tests
 
                 // Receive the initial settings frame from the server.
                 Frame receivedFrame = await server.ReadFrameAsync(TimeSpan.FromSeconds(30));
-                Console.WriteLine("\nServer Recieved:\n"+receivedFrame);
 
                 // Receive the request header frame.
                 receivedFrame = await server.ReadFrameAsync(TimeSpan.FromSeconds(30));
-                Console.WriteLine("\nServer Recieved:\n"+receivedFrame);
 
                 // Send a reset stream frame so that stream 1 moves to a terminal state.
                 RstStreamFrame resetStream = new RstStreamFrame(FrameFlags.Padded, 0x1, 1);
-                Console.WriteLine("\nServer Sending:\n"+resetStream);
                 await server.WriteFrameAsync(resetStream);
 
                 // This currently throws an IOException.
@@ -114,15 +108,12 @@ namespace System.Net.Http.Functional.Tests
 
                 // Receive the initial settings frame from the server.
                 Frame receivedFrame = await server.ReadFrameAsync(TimeSpan.FromSeconds(30));
-                Console.WriteLine("\n"+receivedFrame);
 
                 // Receive the request header frame.
                 receivedFrame = await server.ReadFrameAsync(TimeSpan.FromSeconds(30));
-                Console.WriteLine("\n"+receivedFrame);
 
                 // Send a malformed frame.
                 DataFrame invalidFrame = new DataFrame(new byte[10], FrameFlags.None, 0, 0);
-                Console.WriteLine("\n"+invalidFrame);
                 await server.WriteFrameAsync(invalidFrame);
 
                 // As this is a connection level error, the client should see the request fail.
@@ -130,13 +121,46 @@ namespace System.Net.Http.Functional.Tests
 
                 // The server should receive a GOAWAY frame.
                 receivedFrame = await server.ReadFrameAsync(TimeSpan.FromSeconds(30));
-                Console.WriteLine("\n"+receivedFrame);
                 Assert.Equal(FrameType.GoAway, receivedFrame.Type);
             }
         }
 
         [Fact]
-        //[ActiveIssue(0)] // TODO: Open an issue to track the fact that we never handle malformed frames.
+        [ActiveIssue(31520)]
+        public async Task DataFrame_TooLong_ConnectionError()
+        {
+            HttpClientHandler handler = CreateHttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = TestHelper.AllowAllCertificates;
+
+            using (var server = new Http2LoopbackServer(new Http2Options()))
+            using (var client = new HttpClient(handler))
+            {
+                Task sendTask = client.GetAsync(server.CreateServer());
+
+                await server.AcceptConnectionAsync();
+                await server.SendConnectionPrefaceAsync();
+
+                // Receive the initial settings frame from the server.
+                Frame receivedFrame = await server.ReadFrameAsync(TimeSpan.FromSeconds(30));
+
+                // Receive the request header frame.
+                receivedFrame = await server.ReadFrameAsync(TimeSpan.FromSeconds(30));
+
+                // Send a malformed frame.
+                DataFrame invalidFrame = new DataFrame(new byte[Frame.MaxFrameLength + 1], FrameFlags.None, 0, 0);
+                await server.WriteFrameAsync(invalidFrame);
+
+                // As this is a connection level error, the client should see the request fail.
+                await Assert.ThrowsAsync<HttpRequestException>(async () => await sendTask);
+
+                // The server should receive a GOAWAY frame.
+                receivedFrame = await server.ReadFrameAsync(TimeSpan.FromSeconds(30));
+                Assert.Equal(FrameType.GoAway, receivedFrame.Type);
+            }
+        }
+
+        [Fact]
+        [ActiveIssue(31315)]
         public async Task DataFrame_PaddingOnly_ResetsStream()
         {
             HttpClientHandler handler = CreateHttpClientHandler();
@@ -152,22 +176,18 @@ namespace System.Net.Http.Functional.Tests
 
                 // Receive the initial settings frame from the server.
                 Frame receivedFrame = await server.ReadFrameAsync(TimeSpan.FromSeconds(30));
-                Console.WriteLine("\nServer Recieved:\n"+receivedFrame);
 
                 // Receive the request header frame.
                 receivedFrame = await server.ReadFrameAsync(TimeSpan.FromSeconds(30));
-                Console.WriteLine("\nServer Recieved:\n"+receivedFrame);
 
                 // Send a malformed frame.
                 DataFrame invalidFrame = new DataFrame(new byte[0], FrameFlags.Padded, 10, 1);
-                Console.WriteLine("\nServer Sending:\n"+invalidFrame);
                 await server.WriteFrameAsync(invalidFrame);
 
                 await Assert.ThrowsAsync<HttpRequestException>(async () => await sendTask);
 
                 // Receive a RST_STREAM frame.
                 receivedFrame = await server.ReadFrameAsync(TimeSpan.FromSeconds(30));
-                Console.WriteLine("\nServer Recieved:\n"+receivedFrame);
                 Assert.Equal(FrameType.RstStream, receivedFrame.Type);
             }
         }
@@ -189,29 +209,35 @@ namespace System.Net.Http.Functional.Tests
 
                 // Receive the initial settings frame from the server.
                 Frame receivedFrame = await server.ReadFrameAsync(TimeSpan.FromSeconds(30));
-                Console.WriteLine("\nServer Recieved:\n"+receivedFrame);
 
                 // Receive the request header frame.
                 receivedFrame = await server.ReadFrameAsync(TimeSpan.FromSeconds(30));
-                Console.WriteLine("\nServer Recieved:\n"+receivedFrame);
 
                 // Send a reset stream frame so that stream 1 moves to a terminal state.
                 RstStreamFrame resetStream = new RstStreamFrame(FrameFlags.Padded, 0x1, 1);
-                Console.WriteLine("\nServer Sending:\n"+resetStream);
                 await server.WriteFrameAsync(resetStream);
 
                 // Send a frame on the now-closed stream.
                 DataFrame invalidFrame = new DataFrame(new byte[10], FrameFlags.Padded, 10, 1);
-                Console.WriteLine("\nServer Sending:\n"+invalidFrame);
                 await server.WriteFrameAsync(invalidFrame);
 
                 // Receive a RST_STREAM frame.
                 receivedFrame = await server.ReadFrameAsync(TimeSpan.FromSeconds(30));
-                Console.WriteLine("\nServer Recieved:\n"+receivedFrame);
                 Assert.Equal(FrameType.RstStream, receivedFrame.Type);
 
                 await Assert.ThrowsAsync<HttpRequestException>(async () => await sendTask);
             }
+        }
+
+        public SocketsHttpHandler_HttpClientHandler_Http2_Test()
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2Support", true);
+        }
+
+        public new void Dispose()
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2Support", false);
+            base.Dispose();
         }
     }
 }
