b8153acd8eb8d40682b607cb27d2eec813b1ede0
Zbginiew Dobras <zbagi@desti.no>
2018-07-31 14:12:41 +0200

    Added option to determine if empty collection should be allowed

--- src/System.ComponentModel.Annotations/src/System/ComponentModel/DataAnnotations/RequiredAttribute.cs
+++ src/System.ComponentModel.Annotations/src/System/ComponentModel/DataAnnotations/RequiredAttribute.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
+using System.Collections;
 
 namespace System.ComponentModel.DataAnnotations
 {
@@ -29,12 +30,18 @@ namespace System.ComponentModel.DataAnnotations
         public bool AllowEmptyStrings { get; set; }
 
         /// <summary>
+        ///     Gets or sets a flag indicating whether the attribute should allow empty collections.
+        /// </summary>
+        public bool AllowEmptyCollections { get; set; }
+
+        /// <summary>
         ///     Override of <see cref="ValidationAttribute.IsValid(object)" />
         /// </summary>
         /// <param name="value">The value to test</param>
         /// <returns>
-        ///     <c>false</c> if the <paramref name="value" /> is null or an empty string. If
-        ///     <see cref="RequiredAttribute.AllowEmptyStrings" />
+        ///     <c>false</c> if the <paramref name="value" /> is null, an empty string or an empty collection.
+        ///     Unless <see cref="RequiredAttribute.AllowEmptyStrings" /> or 
+        ///     <see cref="RequiredAttribute.AllowEmptyCollections"/>
         ///     then <c>false</c> is returned only if <paramref name="value" /> is null.
         /// </returns>
         public override bool IsValid(object value)
@@ -44,8 +51,17 @@ namespace System.ComponentModel.DataAnnotations
                 return false;
             }
 
-            // only check string length if empty strings are not allowed
-            return AllowEmptyStrings || !(value is string stringValue) || stringValue.Trim().Length != 0;
+            if (value is string stringValue)
+            {
+                return AllowEmptyStrings || stringValue.Trim().Length != 0;
+            }
+
+            if (value is ICollection collectionValue)
+            {
+                return AllowEmptyCollections || collectionValue.Count() != 0;
+            }
+
+            return true;
         }
     }
 }
