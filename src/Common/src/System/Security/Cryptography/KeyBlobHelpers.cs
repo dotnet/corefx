f6198718119c230ccd13924a84e86b9e12315877
Filip Navara <navara@emclient.com>
2018-08-06 00:06:08 +0200

    Remove now unused helper functions.

--- src/Common/src/System/Security/Cryptography/KeyBlobHelpers.cs
+++ src/Common/src/System/Security/Cryptography/KeyBlobHelpers.cs
@@ -62,48 +62,5 @@ namespace System.Security.Cryptography
 
             writer.WriteIntegerUnsigned(integer);
         }
-        
-        internal static byte[] TrimPaddingByte(byte[] data)
-        {
-            byte[] dataLocal = data;
-            TrimPaddingByte(ref dataLocal);
-            return dataLocal;
-        }
-
-        internal static void TrimPaddingByte(ref byte[] data)
-        {
-            if (data[0] != 0)
-                return;
-
-            byte[] newData = new byte[data.Length - 1];
-            Buffer.BlockCopy(data, 1, newData, 0, newData.Length);
-            data = newData;
-        }
-
-        internal static byte[] PadOrTrim(byte[] data, int length)
-        {
-            byte[] dataLocal = data;
-            PadOrTrim(ref dataLocal, length);
-            return dataLocal;
-        }
-
-        internal static void PadOrTrim(ref byte[] data, int length)
-        {
-            if (data.Length == length)
-                return;
-
-            // Need to skip the sign-padding byte.
-            if (data.Length == length + 1 && data[0] == 0)
-            {
-                TrimPaddingByte(ref data);
-                return;
-            }
-
-            int offset = length - data.Length;
-
-            byte[] newData = new byte[length];
-            Buffer.BlockCopy(data, 0, newData, offset, data.Length);
-            data = newData;
-        }
     }
 }
