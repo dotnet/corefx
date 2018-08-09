17696af2337060a0a2ba07cd8097f2e9372abcd2
Anirudh Agnihotry <anirudhagnihotry098@gmail.com>
2018-08-08 20:08:38 -0700

    Move methodbody and exceptionHandlingClause to shared (#19364)
    * Changing names and making runtime files
    
    * Movel methodbody and exceptionHandlingClause to shared
    
    * Fixing build error
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

---
+++ src/Common/src/CoreLib/System/Reflection/ExceptionHandlingClause.cs
@@ -0,0 +1,28 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Globalization;
+
+namespace System.Reflection
+{
+    public class ExceptionHandlingClause
+    {
+        protected ExceptionHandlingClause() { }
+        public virtual ExceptionHandlingClauseOptions Flags => default;
+        public virtual int TryOffset => 0;
+        public virtual int TryLength => 0;
+        public virtual int HandlerOffset => 0;
+        public virtual int HandlerLength => 0;
+        public virtual int FilterOffset => throw new InvalidOperationException(SR.Arg_EHClauseNotFilter);
+        public virtual Type CatchType => null;
+
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.CurrentUICulture,
+                "Flags={0}, TryOffset={1}, TryLength={2}, HandlerOffset={3}, HandlerLength={4}, CatchType={5}",
+                Flags, TryOffset, TryLength, HandlerOffset, HandlerLength, CatchType);
+        }
+    }
+}
+
