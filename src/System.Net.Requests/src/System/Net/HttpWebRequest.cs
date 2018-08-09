f602b2117a099a8355c239c54c7689f2d7fcb842
David Shulman <david.shulman@microsoft.com>
2018-08-09 11:44:17 -0700

    Fix HttpWebRequest when using system proxy settings (#31123)
    While investigating other HttpClient/HttpWebRequest proxy-related bugs, I discovered that
    HttpWebRequest was not honoring system proxy settings as defined on Windows with IE
    settings or on Linux using environment variables.
    
    The problem is due to how HttpClient and HttpWebRequest differ in how they represent
    the default behavior of using system proxy settings with the various properties. Fixed
    HttpWebRequest so that it will translate the system proxy settings to the internal
    HttpClient/HttpClientHandler objects.
    
    I also removed an invalid Assert in HttpConnection. This assert was firing when using a proxy
    that was defined on the loopback adapter using IPv6 literal "[::1]".  Due to issue #28863 with Uri,
    the Uri.IdnHost property doesn't have the brackets for IPv6 literals. So, the Assert was
    occuring.
    
    I did not add any new CI tests because it is currently not possible to test system proxy settings
    in CI since it involves changing machine configuration. But I ran manual tests.

--- src/System.Net.Requests/src/System/Net/HttpWebRequest.cs
+++ src/System.Net.Requests/src/System/Net/HttpWebRequest.cs
@@ -1138,11 +1138,22 @@ namespace System.Net
 
                 Debug.Assert(handler.UseProxy); // Default of handler.UseProxy is true.
                 Debug.Assert(handler.Proxy == null); // Default of handler.Proxy is null.
+
+                // HttpClientHandler default is to use a proxy which is the system proxy.
+                // This is indicated by the properties 'UseProxy == true' and 'Proxy == null'.
+                //
+                // However, HttpWebRequest doesn't have a separate 'UseProxy' property. Instead,
+                // the default of the 'Proxy' property is a non-null IWebProxy object which is the
+                // system default proxy object. If the 'Proxy' property were actually null, then
+                // that means don't use any proxy. 
+                //
+                // So, we need to map the desired HttpWebRequest proxy settings to equivalent
+                // HttpClientHandler settings.
                 if (_proxy == null)
                 {
                     handler.UseProxy = false;
                 }
-                else
+                else if (!object.ReferenceEquals(_proxy, WebRequest.GetSystemWebProxy()))
                 {
                     handler.Proxy = _proxy;
                 }
