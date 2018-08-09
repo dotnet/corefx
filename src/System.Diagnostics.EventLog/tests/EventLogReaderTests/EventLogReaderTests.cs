d6f6400f6ec7f7cb673295cd93e3f0809d6db166
Tomasz Heimowski <tomek.heimowski@gmail.com>
2018-08-09 10:25:40 +0200

    Nested Event Log tests

--- src/System.Diagnostics.EventLog/tests/EventLogReaderTests/EventLogReaderTests.cs
+++ src/System.Diagnostics.EventLog/tests/EventLogReaderTests/EventLogReaderTests.cs
@@ -2,10 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Globalization;
-using System.IO;
-using Xunit;
 using System.Diagnostics.Eventing.Reader;
+using Xunit;
 
 namespace System.Diagnostics.Tests
 {
@@ -14,20 +12,47 @@ namespace System.Diagnostics.Tests
         [ConditionalFact(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
         public void ApplicationEventLog_Record()
         {
-            var eventLog = new EventLogReader("Application");
-            var record = eventLog.ReadEvent();
-            Assert.NotNull(record);
-            Assert.Equal(record.LogName, "Application");
+            using (var eventLog = new EventLogReader("Application"))
+            {
+                var record = eventLog.ReadEvent();
+                Assert.NotNull(record);
+                Assert.Equal("Application", record.LogName);
+            }
         }
 
         [ConditionalFact(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
         public void ApplicationEventLogQuery_Record()
         {
             var query = new EventLogQuery("Application", PathType.LogName) { ReverseDirection = true };
-            var eventLog = new EventLogReader(query);
-            var record = eventLog.ReadEvent();
-            Assert.NotNull(record);
-            Assert.Equal(record.LogName, "Application");
+            using (var eventLog = new EventLogReader(query))
+            {
+                var record = eventLog.ReadEvent();
+                Assert.NotNull(record);
+                Assert.Equal("Application", record.LogName);
+            }
+        }
+
+        [ConditionalFact(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
+        public void NestedEventLog_Record()
+        {
+            using (var eventLog = new EventLogReader("Microsoft-Windows-PowerShell/Operational"))
+            {
+                var record = eventLog.ReadEvent();
+                Assert.NotNull(record);
+                Assert.Equal("Microsoft-Windows-PowerShell/Operational", record.LogName);
+            }
+        }
+
+        [ConditionalFact(typeof(Helpers), nameof(Helpers.SupportsEventLogs))]
+        public void NestedEventLogQuery_Record()
+        {
+            var query = new EventLogQuery("Microsoft-Windows-PowerShell/Operational", PathType.LogName) { ReverseDirection = true };
+            using (var eventLog = new EventLogReader(query))
+            {
+                var record = eventLog.ReadEvent();
+                Assert.NotNull(record);
+                Assert.Equal("Microsoft-Windows-PowerShell/Operational", record.LogName);
+            }
         }
     }
 }
