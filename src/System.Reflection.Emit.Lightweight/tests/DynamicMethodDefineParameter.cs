0a8f62bedef77db9eca1d9b06ef9b73647ca0559
Maryam Ariyan <maryam.ariyan@microsoft.com>
2018-08-13 18:18:29 -0700

    Improving test

--- src/System.Reflection.Emit.Lightweight/tests/DynamicMethodDefineParameter.cs
+++ src/System.Reflection.Emit.Lightweight/tests/DynamicMethodDefineParameter.cs
@@ -10,7 +10,7 @@ namespace System.Reflection.Emit.Tests
     public class DynamicMethodDefineParameter
     {
         [Fact]
-        public void DefineParameter_SetsParameterName()
+        public void DefineParameter_SetsParameterCorrectly()
         {
             Type[] mathArgs = { typeof(double), typeof(double) };
 
@@ -24,21 +24,25 @@ namespace System.Reflection.Emit.Tests
             il.Emit(OpCodes.Ldarg_1);
             il.Emit(OpCodes.Call, typeof(Math).GetMethod("Pow"));
             il.Emit(OpCodes.Ret);
-
-            var paramNames = new string[] { "param1", "param2" };
-            for (int i = 0; i < paramNames.Length; i++)
-            {
-                powerOf.DefineParameter(i+1, ParameterAttributes.In, paramNames[i]);
-            }
+            
+            powerOf.DefineParameter(1, ParameterAttributes.In, "base");
+            powerOf.DefineParameter(2, ParameterAttributes.Out, "exponent");
             
             object[] invokeArgs = { 2, 5 };
             object objRet = powerOf.Invoke(null, BindingFlags.ExactBinding, null, invokeArgs, new CultureInfo("en-us"));
 
             ParameterInfo[] parameters = powerOf.GetParameters();
 
-            Assert.Equal(paramNames.Length, parameters.Length);
-            Assert.Equal("param1", parameters[0].Name);
-            Assert.Equal("param2", parameters[1].Name);
+            Assert.Equal(32.0, objRet);
+
+            Assert.Equal("base", parameters[0].Name);
+            Assert.Equal("exponent", parameters[1].Name);
+
+            Assert.Equal(typeof(double), parameters[0].ParameterType);
+            Assert.Equal(typeof(double), parameters[1].ParameterType);
+
+            Assert.Equal(ParameterAttributes.In, parameters[0].Attributes);
+            Assert.Equal(ParameterAttributes.Out, parameters[1].Attributes);
         }
     }
 }
