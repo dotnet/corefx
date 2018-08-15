95c8fab37fafd4a0fd6c5535db58fd44d9887832
EgorBo <egorbo@gmail.com>
2018-08-16 01:27:58 +0300

    simplify Equals

--- src/System.Numerics.Vectors/src/System/Numerics/Matrix4x4.cs
+++ src/System.Numerics.Vectors/src/System/Numerics/Matrix4x4.cs
@@ -2196,15 +2196,7 @@ namespace System.Numerics
         /// </summary>
         /// <param name="obj">The Object to compare against.</param>
         /// <returns>True if the Object is equal to this matrix; False otherwise.</returns>
-        public override bool Equals(object obj)
-        {
-            if (obj is Matrix4x4 m)
-            {
-                return this == m;
-            }
-
-            return false;
-        }
+        public override bool Equals(object obj) => (obj is Matrix4x4 other) && (this == other);
 
         /// <summary>
         /// Returns a String representing this matrix instance.
