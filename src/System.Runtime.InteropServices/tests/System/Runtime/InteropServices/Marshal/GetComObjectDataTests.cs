ca630ef6a80c49d228b8b0cc098e51e0b32a6f27
Hugh Bellamy <hughbellars@gmail.com>
2018-08-15 23:04:59 +0200

    Add Get/SetComObjectData tests (#31153)

--- src/System.Runtime.InteropServices/tests/System/Runtime/InteropServices/Marshal/GetComObjectDataTests.cs
+++ src/System.Runtime.InteropServices/tests/System/Runtime/InteropServices/Marshal/GetComObjectDataTests.cs
@@ -6,46 +6,32 @@ using Xunit;
 
 namespace System.Runtime.InteropServices.Tests
 {
-    public class GetComObjectDataTests
+    public partial class GetComObjectDataTests
     {
         [Fact]
-        [SkipOnTargetFramework(~TargetFrameworkMonikers.NetFramework, "Marshal.GetComObjectData is not implemented in .NET Core.")]
-        public void GetComObjectData_NetFramework_ReturnsExpected()
-        {
-            Type type = Type.GetTypeFromCLSID(new Guid("927971f5-0939-11d1-8be1-00c04fd8d503"));
-            object comObject = Activator.CreateInstance(type);
 
-            Assert.Null(Marshal.GetComObjectData(comObject, "key"));
-
-            Marshal.SetComObjectData(comObject, "key", 1);
-            Assert.Equal(1, Marshal.GetComObjectData(comObject, "key"));
-            Assert.Null(Marshal.GetComObjectData(comObject, "noSuchKey"));
-        }
-
-        [Fact]
-        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework, "Marshal.GetComObjectData is not implemented in .NET Core.")]
-        [ActiveIssue(31214)]
-        public void GetComObjectData_NetCore_ThrowsPlatformNotSupportedException()
+        [PlatformSpecific(TestPlatforms.AnyUnix)]
+        public void GetComObjectData_Unix_ThrowsPlatformNotSupportedException()
         {
             Assert.Throws<PlatformNotSupportedException>(() => Marshal.GetComObjectData(null, null));
         }
 
         [Fact]
-        [SkipOnTargetFramework(~TargetFrameworkMonikers.NetFramework, "Marshal.GetComObjectData is not implemented in .NET Core.")]
+        [PlatformSpecific(TestPlatforms.Windows)]
         public void GetComObjectData_NullObj_ThrowsArgumentNullException()
         {
             AssertExtensions.Throws<ArgumentNullException>("obj", () => Marshal.GetComObjectData(null, new object()));
         }
 
         [Fact]
-        [SkipOnTargetFramework(~TargetFrameworkMonikers.NetFramework, "Marshal.GetComObjectData is not implemented in .NET Core.")]
+        [PlatformSpecific(TestPlatforms.Windows)]
         public void GetComObjectData_NullKey_ThrowsArgumentNullException()
         {
             AssertExtensions.Throws<ArgumentNullException>("key", () => Marshal.GetComObjectData(new object(), null));
         }
 
         [Fact]
-        [SkipOnTargetFramework(~TargetFrameworkMonikers.NetFramework, "Marshal.GetComObjectData is not implemented in .NET Core.")]
+        [PlatformSpecific(TestPlatforms.Windows)]
         public void GetComObjectData_NonComObjectObj_ThrowsArgumentNullException()
         {
             AssertExtensions.Throws<ArgumentException>("obj", () => Marshal.GetComObjectData(1, 2));
