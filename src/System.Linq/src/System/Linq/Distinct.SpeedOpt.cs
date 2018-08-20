8136b3dd606b08acc749645401fdf719b7c5e05c
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2018-08-21 00:50:21 +0200

    Uwp6.2: Factor out large generic additions in System.Linq for uap build (#31694)
    * Factor out large generic additions in System.Linq for uap build (#31025)
    
    * Factor out large generic additions in System.Linq
    
    New generic types in System.Linq are causing significant size-on-disk increases in AOT builds.  This change addresses some of them:
    
    - The many implementations of IPartition and IIListProvider are causing significant binary size increases for AOT.  I've factored out the majority of those large changes to only be used in a netcoreapp build, not in a uap build.  There are still code paths that test whether an object is IPartition or IIListProvider, but there are no types now in the UAP build that implement them.
    - As part of this, I replaced several places where `EmptyPartition<TResult>.Instance` was being used directly to implement an `IEnumerable<T>`, changed them to use `Enumerable.Empty<T>`, and then changed `Enumerable.Empty<T>` so that on netcoreapp it returns `EmptyPartition<TResult>.Instance`, and on uap it returns `Array.Empty<T>`.  This also means now that code paths lighting-up on partitions will also light-up on `Enumerable.Empty<T>`.  It also means that I changed the empty partition's `IEnumerator.Reset` method to be a nop instead of throwing, to match the same behavior as array's `IEnumerator.Reset`.
    - LargeArrayBuilder uses T[][], which introduces lots of new array types and corresponding interface instantiations, helper types, etc.  I've specialized LargeArrayBuilder in the UAP build to just be a thin wrapper around ArrayBuilder; we still end up with unnecessary instantiations of LargeArrayBuilder, but they're relatively small, and it saves everything to do with those T[][] instantiations.
    
    * Rename files from netcoreapp/uap to SpeedOpt/SizeOpt
    
    * Fix tests for Reset-becoming-nop change
    
    * Fix build breaks

---
+++ src/System.Linq/src/System/Linq/Distinct.SpeedOpt.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+    public static partial class Enumerable
+    {
+        private sealed partial class DistinctIterator<TSource> : IIListProvider<TSource>
+        {
+            private Set<TSource> FillSet()
+            {
+                var set = new Set<TSource>(_comparer);
+                set.UnionWith(_source);
+                return set;
+            }
+
+            public TSource[] ToArray() => FillSet().ToArray();
+
+            public List<TSource> ToList() => FillSet().ToList();
+
+            public int GetCount(bool onlyIfCheap) => onlyIfCheap ? -1 : FillSet().Count;
+        }
+    }
+}
---
+++ src/System.Linq/src/System/Linq/Where.SpeedOpt.cs
@@ -0,0 +1,365 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+    public static partial class Enumerable
+    {
+        private sealed partial class WhereEnumerableIterator<TSource> : IIListProvider<TSource>
+        {
+            public int GetCount(bool onlyIfCheap)
+            {
+                if (onlyIfCheap)
+                {
+                    return -1;
+                }
+
+                int count = 0;
+
+                foreach (TSource item in _source)
+                {
+                    if (_predicate(item))
+                    {
+                        checked
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                return count;
+            }
+
+            public TSource[] ToArray()
+            {
+                var builder = new LargeArrayBuilder<TSource>(initialize: true);
+
+                foreach (TSource item in _source)
+                {
+                    if (_predicate(item))
+                    {
+                        builder.Add(item);
+                    }
+                }
+
+                return builder.ToArray();
+            }
+
+            public List<TSource> ToList()
+            {
+                var list = new List<TSource>();
+
+                foreach (TSource item in _source)
+                {
+                    if (_predicate(item))
+                    {
+                        list.Add(item);
+                    }
+                }
+
+                return list;
+            }
+        }
+
+        internal sealed partial class WhereArrayIterator<TSource> : IIListProvider<TSource>
+        {
+            public int GetCount(bool onlyIfCheap)
+            {
+                if (onlyIfCheap)
+                {
+                    return -1;
+                }
+
+                int count = 0;
+
+                foreach (TSource item in _source)
+                {
+                    if (_predicate(item))
+                    {
+                        checked
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                return count;
+            }
+
+            public TSource[] ToArray()
+            {
+                var builder = new LargeArrayBuilder<TSource>(_source.Length);
+
+                foreach (TSource item in _source)
+                {
+                    if (_predicate(item))
+                    {
+                        builder.Add(item);
+                    }
+                }
+
+                return builder.ToArray();
+            }
+
+            public List<TSource> ToList()
+            {
+                var list = new List<TSource>();
+
+                foreach (TSource item in _source)
+                {
+                    if (_predicate(item))
+                    {
+                        list.Add(item);
+                    }
+                }
+
+                return list;
+            }
+        }
+
+        private sealed partial class WhereListIterator<TSource> : Iterator<TSource>, IIListProvider<TSource>
+        {
+            public int GetCount(bool onlyIfCheap)
+            {
+                if (onlyIfCheap)
+                {
+                    return -1;
+                }
+
+                int count = 0;
+
+                for (int i = 0; i < _source.Count; i++)
+                {
+                    TSource item = _source[i];
+                    if (_predicate(item))
+                    {
+                        checked
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                return count;
+            }
+
+            public TSource[] ToArray()
+            {
+                var builder = new LargeArrayBuilder<TSource>(_source.Count);
+
+                for (int i = 0; i < _source.Count; i++)
+                {
+                    TSource item = _source[i];
+                    if (_predicate(item))
+                    {
+                        builder.Add(item);
+                    }
+                }
+
+                return builder.ToArray();
+            }
+
+            public List<TSource> ToList()
+            {
+                var list = new List<TSource>();
+
+                for (int i = 0; i < _source.Count; i++)
+                {
+                    TSource item = _source[i];
+                    if (_predicate(item))
+                    {
+                        list.Add(item);
+                    }
+                }
+
+                return list;
+            }
+        }
+
+        private sealed partial class WhereSelectArrayIterator<TSource, TResult> : IIListProvider<TResult>
+        {
+            public int GetCount(bool onlyIfCheap)
+            {
+                // In case someone uses Count() to force evaluation of
+                // the selector, run it provided `onlyIfCheap` is false.
+
+                if (onlyIfCheap)
+                {
+                    return -1;
+                }
+
+                int count = 0;
+
+                foreach (TSource item in _source)
+                {
+                    if (_predicate(item))
+                    {
+                        _selector(item);
+                        checked
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                return count;
+            }
+
+            public TResult[] ToArray()
+            {
+                var builder = new LargeArrayBuilder<TResult>(_source.Length);
+
+                foreach (TSource item in _source)
+                {
+                    if (_predicate(item))
+                    {
+                        builder.Add(_selector(item));
+                    }
+                }
+
+                return builder.ToArray();
+            }
+
+            public List<TResult> ToList()
+            {
+                var list = new List<TResult>();
+
+                foreach (TSource item in _source)
+                {
+                    if (_predicate(item))
+                    {
+                        list.Add(_selector(item));
+                    }
+                }
+
+                return list;
+            }
+        }
+
+        private sealed partial class WhereSelectListIterator<TSource, TResult> : IIListProvider<TResult>
+        {
+            public int GetCount(bool onlyIfCheap)
+            {
+                // In case someone uses Count() to force evaluation of
+                // the selector, run it provided `onlyIfCheap` is false.
+
+                if (onlyIfCheap)
+                {
+                    return -1;
+                }
+
+                int count = 0;
+
+                for (int i = 0; i < _source.Count; i++)
+                {
+                    TSource item = _source[i];
+                    if (_predicate(item))
+                    {
+                        _selector(item);
+                        checked
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                return count;
+            }
+
+            public TResult[] ToArray()
+            {
+                var builder = new LargeArrayBuilder<TResult>(_source.Count);
+
+                for (int i = 0; i < _source.Count; i++)
+                {
+                    TSource item = _source[i];
+                    if (_predicate(item))
+                    {
+                        builder.Add(_selector(item));
+                    }
+                }
+
+                return builder.ToArray();
+            }
+
+            public List<TResult> ToList()
+            {
+                var list = new List<TResult>();
+
+                for (int i = 0; i < _source.Count; i++)
+                {
+                    TSource item = _source[i];
+                    if (_predicate(item))
+                    {
+                        list.Add(_selector(item));
+                    }
+                }
+
+                return list;
+            }
+        }
+
+        private sealed partial class WhereSelectEnumerableIterator<TSource, TResult> : IIListProvider<TResult>
+        {
+            public int GetCount(bool onlyIfCheap)
+            {
+                // In case someone uses Count() to force evaluation of
+                // the selector, run it provided `onlyIfCheap` is false.
+
+                if (onlyIfCheap)
+                {
+                    return -1;
+                }
+
+                int count = 0;
+
+                foreach (TSource item in _source)
+                {
+                    if (_predicate(item))
+                    {
+                        _selector(item);
+                        checked
+                        {
+                            count++;
+                        }
+                    }
+                }
+
+                return count;
+            }
+
+            public TResult[] ToArray()
+            {
+                var builder = new LargeArrayBuilder<TResult>(initialize: true);
+
+                foreach (TSource item in _source)
+                {
+                    if (_predicate(item))
+                    {
+                        builder.Add(_selector(item));
+                    }
+                }
+
+                return builder.ToArray();
+            }
+
+            public List<TResult> ToList()
+            {
+                var list = new List<TResult>();
+
+                foreach (TSource item in _source)
+                {
+                    if (_predicate(item))
+                    {
+                        list.Add(_selector(item));
+                    }
+                }
+
+                return list;
+            }
+        }
+    }
+}
