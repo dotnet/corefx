e32b8fa16f8eb233bc8a0ff0d2120f3c7ce76fed
Ben Gribaudo <ben@bengribaudo.com>
2018-08-24 15:49:45 -0500

    Typo fix (missing return signature)

--- src/System.Diagnostics.Process/src/System/Diagnostics/Process.Uap.cs
+++ src/System.Diagnostics.Process/src/System/Diagnostics/Process.Uap.cs
@@ -23,6 +23,6 @@ namespace System.Diagnostics
 
         private bool WaitForInputIdleCore(int milliseconds) => throw new InvalidOperationException(SR.InputIdleUnkownError);
 
-        public Kill(bool entireProcessTree) { }
+        public void Kill(bool entireProcessTree) { }
     }
 }
