36528eb9a2711dd96e593206a5d5180434172c41
Levi Broderick <levib@microsoft.com>
2018-08-28 16:22:09 -0700

    Add Utf8String ctor tests

--- src/System.Runtime/tests/System/Utf8StringTests.cs
+++ src/System.Runtime/tests/System/Utf8StringTests.cs
@@ -16,6 +16,201 @@ namespace System.Tests
         private const string AllWhitespaceChars = "\u0009\u000A\u000B\u000C\u000D\u0020\u0085\u00A0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u2028\u2029\u202F\u205F\u3000";
 
         [Fact]
+        public static void Ctor_ByteArrayOffset_Empty_ReturnsEmpty()
+        {
+            byte[] inputData = new byte[] { (byte)'H', (byte)'e', (byte)'l', (byte)'l', (byte)'o' };
+            Assert.Same(Utf8String.Empty, new Utf8String(inputData, 3, 0));
+        }
+
+        [Fact]
+        public static void Ctor_ByteArrayOffset_ValidData_ReturnsOriginalContents()
+        {
+            byte[] inputData = new byte[] { (byte)'x', (byte)'H', (byte)'e', (byte)'l', (byte)'l', (byte)'o', (byte)'x' };
+            Utf8String expected = Utf8String.Literal("Hello");
+
+            var actual = new Utf8String(inputData, 1, 5);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public static void Ctor_ByteArrayOffset_InvalidData_FixesUpData()
+        {
+            byte[] inputData = new byte[] { (byte)'x', (byte)'H', (byte)'e', (byte)0xFF, (byte)'l', (byte)'o', (byte)'x' };
+            Utf8String expected = Utf8String.Literal("He\uFFFDlo");
+
+            var actual = new Utf8String(inputData, 1, 5);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public static void Ctor_BytePointer_NullOrEmpty_ReturnsEmpty()
+        {
+            byte nullByte = 0;
+
+            Assert.Same(Utf8String.Empty, new Utf8String((byte*)null));
+            Assert.Same(Utf8String.Empty, new Utf8String(&nullByte));
+        }
+
+        [Fact]
+        public static void Ctor_BytePointer_ValidData_ReturnsOriginalContents()
+        {
+            byte[] inputData = new byte[] { (byte)'H', (byte)'e', (byte)'l', (byte)'l', (byte)'o', (byte)'\0' };
+            Utf8String expected = Utf8String.Literal("Hello");
+
+            fixed (byte* pData = inputData)
+            {
+                var actual = new Utf8String(pData);
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public static void Ctor_BytePointer_InvalidData_FixesUpData()
+        {
+            byte[] inputData = new byte[] { (byte)'H', (byte)'e', (byte)0xFF, (byte)'l', (byte)'o', (byte)'\0' };
+            Utf8String expected = Utf8String.Literal("He\uFFFDlo");
+
+            fixed (byte* pData = inputData)
+            {
+                var actual = new Utf8String(pData);
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public static void Ctor_ByteSpan_Empty_ReturnsEmpty()
+        {
+            Assert.Same(Utf8String.Empty, new Utf8String(ReadOnlySpan<byte>.Empty));
+        }
+
+        [Fact]
+        public static void Ctor_ByteSpan_ValidData_ReturnsOriginalContents()
+        {
+            byte[] inputData = new byte[] { (byte)'H', (byte)'e', (byte)'l', (byte)'l', (byte)'o' };
+            Utf8String expected = Utf8String.Literal("Hello");
+
+            var actual = new Utf8String(inputData.AsSpan());
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public static void Ctor_ByteSpan_InvalidData_FixesUpData()
+        {
+            byte[] inputData = new byte[] { (byte)'H', (byte)'e', (byte)0xFF, (byte)'l', (byte)'o' };
+            Utf8String expected = Utf8String.Literal("He\uFFFDlo");
+
+            var actual = new Utf8String(inputData.AsSpan());
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public static void Ctor_CharArrayOffset_Empty_ReturnsEmpty()
+        {
+            char[] inputData = "Hello".ToCharArray();
+            Assert.Same(Utf8String.Empty, new Utf8String(inputData, 3, 0));
+        }
+
+        [Fact]
+        public static void Ctor_CharArrayOffset_ValidData_ReturnsOriginalContents()
+        {
+            char[] inputData = "xHellox".ToCharArray();
+            Utf8String expected = Utf8String.Literal("Hello");
+
+            var actual = new Utf8String(inputData, 1, 5);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public static void Ctor_CharArrayOffset_InvalidData_FixesUpData()
+        {
+            char[] inputData = new char[] { 'x', 'H', 'e', '\uD800', 'l', 'o', 'x' };
+            Utf8String expected = Utf8String.Literal("He\uFFFDlo");
+
+            var actual = new Utf8String(inputData, 1, 5);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public static void Ctor_CharPointer_NullOrEmpty_ReturnsEmpty()
+        {
+            char nullChar = '\0';
+
+            Assert.Same(Utf8String.Empty, new Utf8String((char*)null));
+            Assert.Same(Utf8String.Empty, new Utf8String(&nullChar));
+        }
+
+        [Fact]
+        public static void Ctor_CharPointer_ValidData_ReturnsOriginalContents()
+        {
+            const string inputData = "Hello";
+            Utf8String expected = Utf8String.Literal("Hello");
+
+            fixed (char* pData = inputData)
+            {
+                var actual = new Utf8String(pData);
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public static void Ctor_CharPointer_InvalidData_FixesUpData()
+        {
+            char[] inputData = new char[] { 'H', 'e', '\uD800', 'l', 'o', '\0' };
+            Utf8String expected = Utf8String.Literal("He\uFFFDlo");
+
+            fixed (char* pData = inputData)
+            {
+                var actual = new Utf8String(pData);
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        [Fact]
+        public static void Ctor_CharSpan_Empty_ReturnsEmpty()
+        {
+            Assert.Same(Utf8String.Empty, new Utf8String(ReadOnlySpan<char>.Empty));
+        }
+
+        [Fact]
+        public static void Ctor_CharSpan_ValidData_ReturnsOriginalContents()
+        {
+            char[] inputData = "Hello".ToCharArray();
+            Utf8String expected = Utf8String.Literal("Hello");
+
+            var actual = new Utf8String(inputData.AsSpan());
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public static void Ctor_CharSpan_InvalidData_FixesUpData()
+        {
+            char[] inputData = new char[] { 'H', 'e', '\uD800', 'l', 'o' };
+            Utf8String expected = Utf8String.Literal("He\uFFFDlo");
+
+            var actual = new Utf8String(inputData.AsSpan());
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public static void Ctor_String_NullOrEmpty_ReturnsEmpty()
+        {
+            Assert.Same(Utf8String.Empty, new Utf8String((string)null));
+            Assert.Same(Utf8String.Empty, new Utf8String(string.Empty));
+        }
+
+        [Fact]
+        public static void Ctor_String_ValidData_ReturnsOriginalContents()
+        {
+            Assert.Equal(Utf8String.Literal("Hello"), new Utf8String("Hello"));
+        }
+
+        [Fact]
+        public static void Ctor_String_InvalidData_FixesUpData()
+        {
+            Assert.Equal(Utf8String.Literal("He\uFFFDlo"), new Utf8String("He\uD800lo"));
+        }
+
+        [Fact]
         public static void Empty_HasLengthZero()
         {
             Assert.Equal(0, Utf8String.Empty.Length);
