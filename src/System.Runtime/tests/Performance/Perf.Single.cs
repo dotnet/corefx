f4451168f6fde8f34ea7cd8a798951c1286aa058
Tanner Gooding <tagoo@outlook.com>
2018-09-18 09:22:14 -0700

    Adding Perf.Single and splitting out Perf.Decimal

--- src/System.Runtime/tests/Performance/Perf.Single.cs
+++ src/System.Runtime/tests/Performance/Perf.Single.cs
@@ -9,19 +9,19 @@ using Xunit;
 
 namespace System.Tests
 {
-    public class Perf_Double
+    public class Perf_Single
     {
         private volatile string _string;
 
         [Benchmark]
-        [InlineData(104234.343, 1_000_000)]
-        [InlineData(double.MaxValue, 100_000)]
-        [InlineData(double.MinValue, 100_000)]
-        [InlineData(double.MinValue / 2, 100_000)]
-        [InlineData(double.NaN, 10_000_000)]
-        [InlineData(double.PositiveInfinity, 10_000_000)]
-        [InlineData(2.2250738585072009E-308, 100_000)]
-        public void DefaultToString(double number, int innerIterations)
+        [InlineData(104234.343f, 1_000_000)]
+        [InlineData(float.MaxValue, 100_000)]
+        [InlineData(float.MinValue, 100_000)]
+        [InlineData(float.MinValue / 2, 100_000)]
+        [InlineData(float.NaN, 10_000_000)]
+        [InlineData(float.PositiveInfinity, 10_000_000)]
+        [InlineData(float.Epsilon, 100_000)]
+        public void DefaultToString(float number, int innerIterations)
         {
             foreach (var iteration in Benchmark.Iterations)
             {
@@ -36,13 +36,13 @@ namespace System.Tests
         }
 
         [Benchmark]
-        [InlineData("zh", 104234.343, 1_000_000)]
-        [InlineData("zh", double.MaxValue, 100_000)]
-        [InlineData("zh", double.MinValue, 100_000)]
-        [InlineData("zh", double.NaN, 20_000_000)]
-        [InlineData("zh", double.PositiveInfinity, 20_000_000)]
+        [InlineData("zh", 104234.343f, 1_000_000)]
+        [InlineData("zh", float.MaxValue, 100_000)]
+        [InlineData("zh", float.MinValue, 100_000)]
+        [InlineData("zh", float.NaN, 20_000_000)]
+        [InlineData("zh", float.PositiveInfinity, 20_000_000)]
         [InlineData("zh", 0.0, 4_000_000)]
-        public void ToStringWithCultureInfo(string cultureName, double number, int innerIterations)
+        public void ToStringWithCultureInfo(string cultureName, float number, int innerIterations)
         {
             CultureInfo cultureInfo = new CultureInfo(cultureName);
             foreach (var iteration in Benchmark.Iterations)
@@ -63,44 +63,44 @@ namespace System.Tests
             {
                 "R",
                 "G",
-                "G17",
+                "G9",
                 "E",
                 "F50"
             };
 
-            double[] normalTestValues =
+            float[] normalTestValues =
             {
-                0.0,
-                250.0,
+                0.0f,
+                250.0f,
             };
 
-            double[] edgeTestValues =
+            float[] edgeTestValues =
             {
-                double.MaxValue,
-                double.MinValue,
-                double.Epsilon,
+                float.MaxValue,
+                float.MinValue,
+                float.Epsilon,
             };
 
             foreach (string format in formats)
             {
-                foreach (double testValue in normalTestValues)
+                foreach (float testValue in normalTestValues)
                 {
                     yield return new object[] { format, testValue, 2_000_000 };
                 }
 
-                foreach (double testValue in edgeTestValues)
+                foreach (float testValue in edgeTestValues)
                 {
                     yield return new object[] { format, testValue, 100_000 };
                 }
             }
 
-            yield return new object[] { "G", double.PositiveInfinity, 20_000_000 };
-            yield return new object[] { "G", double.NaN, 20_000_000 };
+            yield return new object[] { "G", float.PositiveInfinity, 20_000_000 };
+            yield return new object[] { "G", float.NaN, 20_000_000 };
         }
 
         [Benchmark]
         [MemberData(nameof(ToStringWithFormat_TestData))]
-        public void ToStringWithFormat(string format, double number, int innerIterations)
+        public void ToStringWithFormat(string format, float number, int innerIterations)
         {
             foreach (var iteration in Benchmark.Iterations)
             {
