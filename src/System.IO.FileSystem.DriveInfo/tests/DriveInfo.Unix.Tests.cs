c024b7343ef565b14842e1f9cc562e68be0d16a6
Anipik <anirudhagnihotry098@gmail.com>
2018-09-20 16:48:01 -0700

    rootfs added

--- src/System.IO.FileSystem.DriveInfo/tests/DriveInfo.Unix.Tests.cs
+++ src/System.IO.FileSystem.DriveInfo/tests/DriveInfo.Unix.Tests.cs
@@ -84,5 +84,13 @@ namespace System.IO.FileSystem.DriveInfoTests
             var root = new DriveInfo("/");
             Assert.Throws<PlatformNotSupportedException>(() => root.VolumeLabel = root.Name);
         }
+
+        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsWindowsSubsystemForLinux))]
+        public void DriveFormatOnWSl()
+        {
+            DriveInfo di = new DriveInfo(Path.GetTempPath());
+            Assert.Equal("rootfs", di.DriveFormat);
+            Assert.Equal(DriveType.Ram, di.DriveType);
+        }
     }
 }
