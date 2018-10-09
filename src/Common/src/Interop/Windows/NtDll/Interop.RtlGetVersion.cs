7690380df4b0f2174f35f416a82c61fa0de4c007
Nathan White <nawhit@microsoft.com>
2018-10-09 09:36:16 -0700

    Replaced string interpolation with StringBuilder

--- src/Common/src/Interop/Windows/NtDll/Interop.RtlGetVersion.cs
+++ src/Common/src/Interop/Windows/NtDll/Interop.RtlGetVersion.cs
@@ -2,9 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Runtime.InteropServices;
-using System.Security;
+using System.Text;
 
 internal partial class Interop
 {
@@ -20,8 +19,19 @@ internal partial class Interop
             const string version = "Microsoft Windows";
             if (RtlGetVersion(ref osvi) == 0)
             {
-                return $"{version} {osvi.dwMajorVersion}.{osvi.dwMinorVersion}.{osvi.dwBuildNumber}" +
-                       $"{(osvi.szCSDVersion[0] == '\0' ? string.Empty : " " + new string(&(osvi.szCSDVersion[0])))}";
+                var stringBuilder = new StringBuilder();
+
+                stringBuilder.Append(version);
+                stringBuilder.Append(" ");
+                stringBuilder.AppendFormat("{0}.{1}.{2}", osvi.dwMajorVersion, osvi.dwMinorVersion, osvi.dwBuildNumber);
+
+                if (osvi.szCSDVersion[0] != '\0')
+                {
+                    stringBuilder.Append(" ");
+                    stringBuilder.Append(new string(&(osvi.szCSDVersion[0])));
+                }
+
+                return stringBuilder.ToString();
             }
             else
             {
