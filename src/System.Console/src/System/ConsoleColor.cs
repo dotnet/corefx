635a558ed7bae532f06f1d963b245ccefccbff7e
Yago dos Santos Rocha <yagorocha1@icloud.com>
2018-10-27 14:52:45 -0300

    Remove unneeded enumerator index

--- src/System.Console/src/System/ConsoleColor.cs
+++ src/System.Console/src/System/ConsoleColor.cs
@@ -9,21 +9,21 @@ namespace System
 
     public enum ConsoleColor
     {
-        Black = 0,
-        DarkBlue = 1,
-        DarkGreen = 2,
-        DarkCyan = 3,
-        DarkRed = 4,
-        DarkMagenta = 5,
-        DarkYellow = 6,
-        Gray = 7,
-        DarkGray = 8,
-        Blue = 9,
-        Green = 10,
-        Cyan = 11,
-        Red = 12,
-        Magenta = 13,
-        Yellow = 14,
-        White = 15
+        Black,
+        DarkBlue,
+        DarkGreen,
+        DarkCyan,
+        DarkRed,
+        DarkMagenta,
+        DarkYellow,
+        Gray,
+        DarkGray,
+        Blue,
+        Green,
+        Cyan,
+        Red,
+        Magenta,
+        Yellow,
+        White
     }
 }
