229a41e2f09739f0a529ac07ffebbd233272959e
Paul M Cohen <paul1956@users.noreply.github.com>
2018-10-31 22:29:54 -0700

    Move more MsgBox artifacts to VB/Tests/Interactive.Windows.vb

--- src/Microsoft.VisualBasic/tests/VB/Interaction.Windows.vb
+++ src/Microsoft.VisualBasic/tests/VB/Interaction.Windows.vb
@@ -39,6 +39,49 @@ Namespace Microsoft.VisualBasic
             Return Title
         End Function
 
+        Public Enum MsgBoxResult
+            Ok = 1
+            Cancel = 2
+            Abort = 3
+            Retry = 4
+            Ignore = 5
+            Yes = 6
+            No = 7
+        End Enum
+
+        <Flags()>
+        Public Enum MsgBoxStyle
+            'You may BitOr one value from each group
+            'Button group: Lower 4 bits, &H00F
+            OkOnly = &H0I
+            OkCancel = &H1I
+            AbortRetryIgnore = &H2I
+            YesNoCancel = &H3I
+            YesNo = &H4I
+            RetryCancel = &H5I
+
+            'Icon Group: Middle 4 bits &H0F0
+            Critical = &H10I     'Same as Windows.Forms.MessageBox.IconError
+            Question = &H20I     'Same As Windows.MessageBox.IconQuestion
+            Exclamation = &H30I  'Same As Windows.MessageBox.IconExclamation
+            Information = &H40I  'Same As Windows.MessageBox.IconInformation
+
+            'Default Group: High 4 bits &HF00
+            DefaultButton1 = 0
+            DefaultButton2 = &H100I
+            DefaultButton3 = &H200I
+            'UNSUPPORTED IN VB7
+            'DefaultButton4 = &H300I
+
+            ApplicationModal = &H0I
+            SystemModal = &H1000I
+
+            MsgBoxHelp = &H4000I
+            MsgBoxRight = &H80000I
+            MsgBoxRtlReading = &H100000I
+            MsgBoxSetForeground = &H10000I
+        End Enum
+
         Public Function MsgBox(ByVal Prompt As Object, Optional ByVal Buttons As MsgBoxStyle = MsgBoxStyle.OkOnly, Optional ByVal Title As Object = Nothing) As MsgBoxResult
             Dim sPrompt As String = Nothing
             Dim sTitle As String
