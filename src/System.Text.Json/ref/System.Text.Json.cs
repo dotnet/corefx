1a95dbea0ea55f5b78ad1623b27d987a9ca5c1ad
Ahson Khan <ahkha@microsoft.com>
2018-11-01 11:47:36 -0700

    Auto-generate the reference assembly

--- src/System.Text.Json/ref/System.Text.Json.cs
+++ src/System.Text.Json/ref/System.Text.Json.cs
@@ -9,17 +9,17 @@ namespace System.Text.Json
 {
     public enum JsonTokenType : byte
     {
-        None,
-        StartObject,
-        EndObject,
-        StartArray,
-        EndArray,
-        PropertyName,
-        String,
-        Number,
-        True,
-        False,
-        Null,
-        Comment,
+        Comment = (byte)11,
+        EndArray = (byte)4,
+        EndObject = (byte)2,
+        False = (byte)9,
+        None = (byte)0,
+        Null = (byte)10,
+        Number = (byte)7,
+        PropertyName = (byte)5,
+        StartArray = (byte)3,
+        StartObject = (byte)1,
+        String = (byte)6,
+        True = (byte)8,
     }
 }
