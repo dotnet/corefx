ad6178123b4fad2e17e43e094aa5e8bd315d02c1
Paul M Cohen <paul1956@users.noreply.github.com>
2018-11-02 00:16:08 -0700

    Restore file to Commit 7cad8486 before I made any changes

--- src/Microsoft.VisualBasic/src/Microsoft/VisualBasic/Interaction.vb
+++ src/Microsoft.VisualBasic/src/Microsoft/VisualBasic/Interaction.vb
@@ -2,12 +2,17 @@
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
 Namespace Microsoft.VisualBasic
-    Public Module Interaction
-        Public Function IIf(Of T)(ByVal condition As Boolean, ByVal truePart As T, ByVal falsePart As T) As T
+
+    Friend Module Interaction
+
+        Friend Function IIf(Of T)(ByVal condition As Boolean, ByVal truePart As T, ByVal falsePart As T) As T
             If condition Then
                 Return truePart
             End If
             Return falsePart
+
         End Function
+
     End Module
+
 End Namespace
