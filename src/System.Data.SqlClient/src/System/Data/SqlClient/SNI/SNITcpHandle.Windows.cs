261eb30fa64766cf36e2e1d2601f46b20d6731a5
Afsaneh Rafighi <v-afrafi@microsoft.com>
2018-11-01 17:16:53 -0700

    set KeepAlive values in unix only

---
+++ src/System.Data.SqlClient/src/System/Data/SqlClient/SNI/SNITcpHandle.Windows.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace System.Data.SqlClient.SNI
+{
+    internal partial class SNITcpHandle
+    {
+        internal static void SetKeepAliveValues(ref Socket socket)
+        {
+        }
+    }
+}
