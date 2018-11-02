1add330b945e465be63a87e7a8effca196ac7d95
Ahson Khan <ahkha@microsoft.com>
2018-11-01 22:05:52 -0700

    Add comments to the JsonTokenType enum

--- src/System.Text.Json/src/System/Text/Json/JsonTokenType.cs
+++ src/System.Text.Json/src/System/Text/Json/JsonTokenType.cs
@@ -4,6 +4,13 @@
 
 namespace System.Text.Json
 {
+    /// <summary>
+    /// This enum defines the various JSON tokens that make up a JSON text and is used by
+    /// the <see cref="JsonUtf8Reader"/> when moving from one token to the next.
+    /// The <see cref="JsonUtf8Reader"/> starts at 'None' by default. The 'Comment' enum value
+    /// is only ever reached in a specific <see cref="JsonUtf8Reader"/> mode and is not
+    /// reachable by default.
+    /// </summary>
     public enum JsonTokenType : byte
     {
         None,
