5d9b296dcc652ffe8a08f44eb7f36689d8672968
Egor Bogatov <egorbo@gmail.com>
2018-11-02 18:24:40 +0300

    Update TimeZoneInfoTests.cs

--- src/System.Runtime/tests/System/TimeZoneInfoTests.cs
+++ src/System.Runtime/tests/System/TimeZoneInfoTests.cs
@@ -3,7 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
