924c5639732d71d7bbdb1fd1cc4a58f9c2b4300b
Paul M Cohen <paul1956@users.noreply.github.com>
2018-11-01 23:38:54 -0700

    Address PR Feedback

--- src/Microsoft.VisualBasic/src/Microsoft/VisualBasic/Interaction.vb
+++ src/Microsoft.VisualBasic/src/Microsoft/VisualBasic/Interaction.vb
@@ -1,19 +1,13 @@
 ' Licensed to the .NET Foundation under one or more agreements.
 ' The .NET Foundation licenses this file to you under the MIT license.
 ' See the LICENSE file in the project root for more information.
-Option Explicit On
-Option Strict On
-
 Namespace Microsoft.VisualBasic
-
-    Partial Public Module Interaction
-        Friend Function IIf(Of T)(ByVal condition As Boolean, ByVal truePart As T, ByVal falsePart As T) As T
+    Public Module Interaction
+        Public Function IIf(Of T)(ByVal condition As Boolean, ByVal truePart As T, ByVal falsePart As T) As T
             If condition Then
                 Return truePart
             End If
-
             Return falsePart
         End Function
     End Module
 End Namespace
-
