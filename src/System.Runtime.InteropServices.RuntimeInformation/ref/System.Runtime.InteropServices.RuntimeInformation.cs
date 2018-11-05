3f763487f688dd826ed56905899082d8d1d68382
Tomas Weinfurt <tweinfurt@yahoo.com>
2018-11-05 10:54:00 -0800

    add aproved FreeBSD to OSPlatform (#33178)
    * add aproved FreeBSD to OSPlatform
    
    * add test for new property and add missing entry to ref assembly
    
    * add one more freebsd on other platforms

--- src/System.Runtime.InteropServices.RuntimeInformation/ref/System.Runtime.InteropServices.RuntimeInformation.cs
+++ src/System.Runtime.InteropServices.RuntimeInformation/ref/System.Runtime.InteropServices.RuntimeInformation.cs
@@ -17,6 +17,7 @@ namespace System.Runtime.InteropServices
     public readonly partial struct OSPlatform : System.IEquatable<System.Runtime.InteropServices.OSPlatform>
     {
         private readonly object _dummy;
+        public static System.Runtime.InteropServices.OSPlatform FreeBSD { get { throw null; } }
         public static System.Runtime.InteropServices.OSPlatform Linux { get { throw null; } }
         public static System.Runtime.InteropServices.OSPlatform OSX { get { throw null; } }
         public static System.Runtime.InteropServices.OSPlatform Windows { get { throw null; } }
