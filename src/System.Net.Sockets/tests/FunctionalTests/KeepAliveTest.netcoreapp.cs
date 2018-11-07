88a2b199ffddc2171c65b583025d7f2acd3224b9
Luigi Berrettini <luigi.berrettini@gmail.com>
2018-11-07 06:09:57 +0100

    Use *SocketOption for keep-alive on Win < 10 v1709
    Rely on defaults and a ConditionalWeakTable to allow:
     - setting time and/or interval via IOControl
     - getting time or interval

--- src/System.Net.Sockets/tests/FunctionalTests/KeepAliveTest.netcoreapp.cs
+++ src/System.Net.Sockets/tests/FunctionalTests/KeepAliveTest.netcoreapp.cs
@@ -12,15 +12,14 @@ namespace System.Net.Sockets.Tests
         private const int RetryCount = 60;
         private const int Time = 5;
         private const int Interval = 2;
+        private const int WindowsDefaultTime = 7200;
+        private const int WindowsDefaultInterval = 1;
 
         private static bool IsUnixOrWindowsAtLeast1703 =>
             !PlatformDetection.IsWindows || PlatformDetection.IsWindows10Version1703OrGreater;
 
         private static bool IsWindowsBelow1703 =>
             PlatformDetection.IsWindows && !PlatformDetection.IsWindows10Version1703OrGreater;
-
-        private static bool IsUnixOrWindowsAtLeast1709 =>
-            !PlatformDetection.IsWindows || PlatformDetection.IsWindows10Version1709OrGreater;
 
         private static bool IsWindowsBelow1709 =>
             PlatformDetection.IsWindows && !PlatformDetection.IsWindows10Version1709OrGreater;
@@ -45,7 +44,7 @@ namespace System.Net.Sockets.Tests
         }
 
         [ConditionalFact(typeof(KeepAliveTest), nameof(IsUnixOrWindowsAtLeast1703))]
-        public void Socket_Set_KeepAlive_RetryCount_Success()
+        public void Socket_KeepAlive_RetryCount_Success()
         {
             using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
             {
@@ -55,7 +54,7 @@ namespace System.Net.Sockets.Tests
         }
 
         [ConditionalFact(typeof(KeepAliveTest), nameof(IsWindowsBelow1703))]
-        public void Socket_Set_KeepAlive_RetryCount_Failure()
+        public void Socket_KeepAlive_RetryCount_Failure()
         {
             using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
             {
@@ -65,7 +64,7 @@ namespace System.Net.Sockets.Tests
         }
 
         [Fact]
-        public void Socket_Set_KeepAlive_Time_Success()
+        public void Socket_KeepAlive_Time()
         {
             using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
             {
@@ -74,8 +73,8 @@ namespace System.Net.Sockets.Tests
             }
         }
 
-        [ConditionalFact(typeof(KeepAliveTest), nameof(IsUnixOrWindowsAtLeast1709))]
-        public void Socket_Set_KeepAlive_Interval_Success()
+        [Fact]
+        public void Socket_KeepAlive_Interval()
         {
             using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
             {
@@ -85,12 +84,117 @@ namespace System.Net.Sockets.Tests
         }
 
         [ConditionalFact(typeof(KeepAliveTest), nameof(IsWindowsBelow1709))]
-        public void Socket_Set_KeepAlive_Interval_Failure()
+        public void Socket_KeepAlive_Time_And_Interval()
         {
             using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
             {
-                Assert.Throws<SocketException>(() => socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveInterval, Interval));
-                Assert.Throws<SocketException>(() => socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveInterval));
+                Assert.Equal<int>(WindowsDefaultTime, (int)socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime));
+                Assert.Equal<int>(WindowsDefaultInterval, (int)socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveInterval));
+
+                socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, Time);
+                Assert.Equal<int>(Time, (int)socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime));
+                Assert.Equal<int>(WindowsDefaultInterval, (int)socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveInterval));
+        
+                socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveInterval, Interval);
+                Assert.Equal<int>(Time, (int)socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime));
+                Assert.Equal<int>(Interval, (int)socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveInterval));
+            }
+        }
+        
+        [ConditionalFact(typeof(KeepAliveTest), nameof(IsWindowsBelow1709))]
+        public void Socket_KeepAlive_Interval_And_Time()
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                Assert.Equal<int>(WindowsDefaultTime, (int)socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime));
+                Assert.Equal<int>(WindowsDefaultInterval, (int)socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveInterval));
+
+                socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveInterval, Interval);
+                Assert.Equal<int>(WindowsDefaultTime, (int)socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime));
+                Assert.Equal<int>(Interval, (int)socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveInterval));
+
+                socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, Time);
+                Assert.Equal<int>(Time, (int)socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime));
+                Assert.Equal<int>(Interval, (int)socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveInterval));
+            }
+        }
+
+        [Fact]
+        public void Socket_Get_KeepAlive_Time_AsByteArray_OptionLengthZero_Failure()
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                Assert.Throws<SocketException>(() => socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, 0));
+            }
+        }
+
+        [Fact]
+        public void Socket_Get_KeepAlive_Time_AsByteArray_BufferNull_Failure()
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                byte[] bufferNull = null;
+                Assert.Throws<SocketException>(() => socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, bufferNull));
+            }
+        }
+
+        [Fact]
+        public void Socket_Get_KeepAlive_Time_AsByteArray_BufferLengthZero_Failure()
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                byte[] bufferLenghtZero = new byte[0];
+                Assert.Throws<SocketException>(() => socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, bufferLenghtZero));
+            }
+        }
+
+        [Fact]
+        public void Socket_Get_KeepAlive_Time_AsByteArray_BufferShort_Failure()
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                byte[] bufferShort = new byte[1];
+                Assert.Throws<SocketException>(() => socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, bufferShort));
+            }
+        }
+
+        [Fact]
+        public void Socket_Set_KeepAlive_Time_AsByteArray_BufferNull_Failure()
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                byte[] bufferNull = null;
+                Assert.Throws<SocketException>(() => socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, bufferNull));
+            }
+        }
+
+        [Fact]
+        public void Socket_Set_KeepAlive_Time_AsByteArray_BufferLengthZero_Failure()
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                byte[] bufferLenghtZero = new byte[0];
+                Assert.Throws<SocketException>(() => socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, bufferLenghtZero));
+            }
+        }
+
+        [Fact]
+        public void Socket_Set_KeepAlive_Time_AsByteArray_BufferShort_Failure()
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                byte[] bufferShort = new byte[1];
+                Assert.Throws<SocketException>(() => socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, bufferShort));
+            }
+        }
+
+        [Fact]
+        public void Socket_KeepAlive_Time_AsByteArray_Success()
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, BitConverter.GetBytes(Time));
+                Assert.Equal<int>(Time, BitConverter.ToInt32(socket.GetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, sizeof(int)), 0));
             }
         }
     }
