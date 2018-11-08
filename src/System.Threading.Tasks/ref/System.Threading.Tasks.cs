f81db407ee3ee0f7de850cfebc5312d0ab50f11b
Stephen Toub <stoub@microsoft.com>
2018-11-08 13:09:03 -0500

    Expose\test IAsyncEnumerable.ConfigureAwait (#33337)

--- src/System.Threading.Tasks/ref/System.Threading.Tasks.cs
+++ src/System.Threading.Tasks/ref/System.Threading.Tasks.cs
@@ -87,6 +87,18 @@ namespace System.Runtime.CompilerServices
         public void SetStateMachine(System.Runtime.CompilerServices.IAsyncStateMachine stateMachine) { }
         public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : System.Runtime.CompilerServices.IAsyncStateMachine { }
     }
+    public readonly struct ConfiguredAsyncEnumerable<T>
+    {
+        private readonly object _dummy;
+        public Enumerator GetAsyncEnumerator() { throw null; }
+        public readonly struct Enumerator
+        {
+            private readonly object _dummy;
+            public ConfiguredValueTaskAwaitable<bool> MoveNextAsync() { throw null; }
+            public T Current { get { throw null; } }
+            public ConfiguredValueTaskAwaitable DisposeAsync() { throw null; }
+        }
+    }
 }
 namespace System.Threading
 {
@@ -149,6 +161,7 @@ namespace System.Threading.Tasks
     }
     public static partial class TaskExtensions
     {
+        public static System.Runtime.CompilerServices.ConfiguredAsyncEnumerable<T> ConfigureAwait<T>(this System.Collections.Generic.IAsyncEnumerable<T> source, bool continueOnCapturedContext) { throw null; }
         public static System.Threading.Tasks.Task Unwrap(this System.Threading.Tasks.Task<System.Threading.Tasks.Task> task) { throw null; }
         public static System.Threading.Tasks.Task<TResult> Unwrap<TResult>(this System.Threading.Tasks.Task<System.Threading.Tasks.Task<TResult>> task) { throw null; }
     }
