f9ac77c7b7db7de01ee6dfd56b3a10dc7ff58749
Ahson Khan <ahkha@microsoft.com>
2018-11-09 07:01:55 -0800

    Add tests for custom bit array and add details to debug assert

--- src/System.Text.Json/src/System/Text/Json/CustomUncheckedBitArray.cs
+++ src/System.Text.Json/src/System/Text/Json/CustomUncheckedBitArray.cs
@@ -18,9 +18,9 @@ namespace System.Text.Json
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public CustomUncheckedBitArray(int bitLength, int integerLength)
         {
-            Debug.Assert(bitLength > 0);
-            Debug.Assert(integerLength > 0 && integerLength < int.MaxValue / 32);
-            Debug.Assert(integerLength * 32 == bitLength);
+            Debug.Assert(bitLength > 0, $"bitLength: {bitLength}, integerLength: {integerLength}");
+            Debug.Assert(integerLength > 0 && integerLength <= int.MaxValue / 32 + 1, $"bitLength: {bitLength}, integerLength: {integerLength}");
+            Debug.Assert(bitLength <= (long)integerLength * 32, $"bitLength: {bitLength}, integerLength: {integerLength}");
 
             _array = new int[integerLength];
             Length = bitLength;
@@ -31,18 +31,18 @@ namespace System.Text.Json
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get
             {
-                Debug.Assert(index >= 0 && index <= Length);
+                Debug.Assert(index >= 0 && index < Length, $"index: {index}, Length: {Length}");
 
                 int quotient = Div32Rem(index, out int remainder);
 
-                Debug.Assert(quotient < _array.Length);
+                Debug.Assert(quotient < _array.Length, $"index: {index}, Length: {Length}, quotient: {quotient}, arrayLength: {_array.Length}, remainder: {remainder}");
 
                 return (_array[quotient] & (1 << remainder)) != 0;
             }
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             set
             {
-                Debug.Assert(index >= 0);
+                Debug.Assert(index >= 0, $"index: {index}, Length: {Length}");
 
                 // Grow the array when setting a bit if it isn't big enough
                 // This way the caller doesn't have to check.
@@ -53,7 +53,7 @@ namespace System.Text.Json
 
                 int quotient = Div32Rem(index, out int remainder);
 
-                Debug.Assert(quotient < _array.Length);
+                Debug.Assert(quotient < _array.Length, $"index: {index}, Length: {Length}, quotient: {quotient}, arrayLength: {_array.Length}, remainder: {remainder}");
 
                 if (value)
                 {
@@ -69,7 +69,7 @@ namespace System.Text.Json
         [MethodImpl(MethodImplOptions.NoInlining)]
         private void Grow(int index)
         {
-            Debug.Assert(index >= Length);
+            Debug.Assert(index >= Length, $"index: {index}, Length: {Length}");
             int newints = ((index - 1) >> 5) + 1;   // (value + 31) / 32 without overflow
 
             // If index is a multiple of 32
@@ -78,12 +78,29 @@ namespace System.Text.Json
                 newints++;
             }
 
+            index++;
+
             if (newints > _array.Length)
             {
-                int[] newArray = new int[newints];
+                var newArray = new int[newints];
                 _array.AsSpan().CopyTo(newArray);
                 _array = newArray;
             }
+
+            if (index > Length)
+            {
+                // clear high bit values in the last int
+                int last = (Length - 1) >> 5;
+                Div32Rem(Length, out int bits);
+                if (bits > 0)
+                {
+                    _array[last] &= (1 << bits) - 1;
+                }
+
+                // clear remaining int values
+                _array.AsSpan(last + 1, newints - last - 1).Clear();
+            }
+
             Length = index;
         }
 
