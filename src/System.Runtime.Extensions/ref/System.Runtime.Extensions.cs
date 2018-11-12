7b36fb91c3c1b43805af3b24ce86b0786a118240
Stephen Toub <stoub@microsoft.com>
2018-11-12 11:26:25 -0500

    Add BufferedStream.DisposeAsync override

--- src/System.Runtime.Extensions/ref/System.Runtime.Extensions.cs
+++ src/System.Runtime.Extensions/ref/System.Runtime.Extensions.cs
@@ -1344,6 +1344,7 @@ namespace System.IO
         public override System.IAsyncResult BeginRead(byte[] buffer, int offset, int count, System.AsyncCallback callback, object state) { throw null; }
         public override System.IAsyncResult BeginWrite(byte[] buffer, int offset, int count, System.AsyncCallback callback, object state) { throw null; }
         protected override void Dispose(bool disposing) { }
+        public override System.Threading.Tasks.ValueTask DisposeAsync() { throw null; }
         public override int EndRead(System.IAsyncResult asyncResult) { throw null; }
         public override void EndWrite(System.IAsyncResult asyncResult) { throw null; }
         public override void Flush() { }
