93be5dd7e2bc05c852317a736fc0466d0f0f3e3e
Santiago Fernandez Madero <safern@microsoft.com>
2018-11-12 15:09:15 -0800

    Add WPF and Winforms to the catalog drop (#33444)

--- pkg/Microsoft.Windows.Compatibility/tests/publishcompatibilityassets.ps1
+++ pkg/Microsoft.Windows.Compatibility/tests/publishcompatibilityassets.ps1
@@ -47,7 +47,7 @@ if (!(Test-Path $localPackageSourcePath))
 	}
 }
 
-$restoreSources = -join("https://dotnet.myget.org/F/aspnetcore-dev/api/v3/index.json;https://api.nuget.org/v3/index.json;https://dotnet.myget.org/F/dotnet-core/api/v3/index.json;", $localPackageSourcePath)
+$restoreSources = -join("https://dotnet.myget.org/F/aspnetcore-dev/api/v3/index.json;https://api.nuget.org/v3/index.json;https://dotnet.myget.org/F/dotnet-core/api/v3/index.json;https://dotnetfeed.blob.core.windows.net/dotnet-core/index.json;", $localPackageSourcePath)
 
 $compatPackageVersion = _getPackageVersion "Microsoft.Windows.Compatibility"
 $privatePackageVersion = _getPackageVersion "Microsoft.Private.CoreFx.NETCoreApp"
