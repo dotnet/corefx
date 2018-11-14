bda3fe28f187ee47790d4cf3239f5846cb562c9a
Stano Peťko <stano@stanopetko.eu>
2018-11-14 22:02:25 +0100

    Test MultipartContentTest.ReadAsStringAsync_TwoSubContents_MatchesExpected

--- src/System.Net.Http/tests/FunctionalTests/MultipartContentTest.cs
+++ src/System.Net.Http/tests/FunctionalTests/MultipartContentTest.cs
@@ -180,7 +180,6 @@ namespace System.Net.Http.Functional.Tests
                 "This is a ByteArrayContent\r\n" +
                 "--theBoundary\r\n" +
                 "Content-Type: text/plain; charset=utf-8\r\n" +
-                "Content-Length: 26\r\n" +
                 "\r\n" +
                 "This is a StringContent\r\n" +
                 "--theBoundary--\r\n",
