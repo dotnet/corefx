25ee3f072c394a8580cc8827e470e2f394782b27
Maryam Ariyan <maryam.ariyan@microsoft.com>
2018-11-14 12:44:41 -0800

    Adds test for ArgIterator, related to: #32898

--- src/System.Runtime/tests/System/ArgIteratorTests.netcoreapp.cs
+++ src/System.Runtime/tests/System/ArgIteratorTests.netcoreapp.cs
@@ -10,6 +10,34 @@ namespace System.Tests
 {
     public static class ArgIteratorTests
     {
+        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsArgIteratorSupported))]
+        public static void ArgIterator_GetRemainingCount_GetNextArg()
+        {
+            object[] result = UseArgIterator("a", "r", "g", "s", __arglist(1, 2, 3));
+            Assert.Equal(new object[] {"a", "r", "g", "s", 1, 2, 3}, result);
+        }
+
+        private static object[] UseArgIterator(Object arg0, Object arg1, Object arg2, Object arg3, __arglist) 
+        {
+            ArgIterator args = new ArgIterator(__arglist);
+            int argCount = args.GetRemainingCount() + 4;
+            object[] objArgs = new Object[argCount];
+
+            // Handle the hard-coded arguments
+            objArgs[0] = arg0;
+            objArgs[1] = arg1;
+            objArgs[2] = arg2;
+            objArgs[3] = arg3;
+            
+            // Walk all of the args in the variable part of the argument list.
+            for (int i = 4; i < argCount; i++)
+            {
+                objArgs[i] = TypedReference.ToObject(args.GetNextArg());
+            }
+
+            return objArgs;
+        }
+
         [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsArgIteratorNotSupported))]
         public unsafe static void ArgIterator_Throws_PlatformNotSupportedException()
         {
