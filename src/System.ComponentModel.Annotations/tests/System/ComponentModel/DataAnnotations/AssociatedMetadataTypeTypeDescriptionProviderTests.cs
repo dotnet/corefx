7878f3ed95ce8036980817f290daff5eaf9282aa
Yaser Moradi <ysmoradi@outlook.com>
2018-12-01 21:52:51 +0330

    Added AssociatedMetadataTypeTypeDescriptionProvider & MetadataTypeAttribute to S.CM.DA

---
+++ src/System.ComponentModel.Annotations/tests/System/ComponentModel/DataAnnotations/AssociatedMetadataTypeTypeDescriptionProviderTests.cs
@@ -0,0 +1,41 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace System.ComponentModel.DataAnnotations.Tests
+{
+    public class AssociatedMetadataTypeTypeDescriptionProviderTests
+    {
+        [Fact]
+        public static void Constructor_NullType_ThrowsArgumentNullException()
+        {
+            AssertExtensions.Throws<ArgumentNullException>("type", () => new AssociatedMetadataTypeTypeDescriptionProvider(null));
+        }
+
+        [Fact]
+        public static void Constructor_NullAssociatedMetadataType_ThrowsArgumentNullException()
+        {
+            AssertExtensions.Throws<ArgumentNullException>("associatedMetadataType", () => new AssociatedMetadataTypeTypeDescriptionProvider(typeof(string), null));
+        }
+
+        [Fact]
+        public static void Validate_GetTypeDescriptor_ReturnsTypeDescriptor_ForTypeWithCustomDescription()
+        {
+            var provider = new AssociatedMetadataTypeTypeDescriptionProvider(typeof(SomeClassWithMetadata));
+            ICustomTypeDescriptor typeDescriptor = provider.GetTypeDescriptor(typeof(SomeClassWithMetadata), null);
+            PropertyDescriptorCollection props = typeDescriptor.GetProperties();
+            PropertyDescriptor firstNameProp = props[nameof(SomeClassWithMetadata.FirstName)];
+            string displayName = firstNameProp.DisplayName;
+
+            Assert.Equal("First name", displayName);
+        }
+
+        class SomeClassWithMetadata
+        {
+            [DisplayName("First name")]
+            public string FirstName { get; set; }
+        }
+    }
+}
