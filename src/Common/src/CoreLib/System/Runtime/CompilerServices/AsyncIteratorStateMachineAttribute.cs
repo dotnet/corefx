b581df2bb268f36c06957a9d91cb850885d98daa
Stephen Toub <stoub@microsoft.com>
2018-11-30 23:31:11 -0500

    Address PR feedback
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/Runtime/CompilerServices/AsyncIteratorStateMachineAttribute.cs
+++ src/Common/src/CoreLib/System/Runtime/CompilerServices/AsyncIteratorStateMachineAttribute.cs
@@ -4,9 +4,12 @@
 
 namespace System.Runtime.CompilerServices
 {
+    /// <summary>Indicates whether a method is an asynchronous iterator.</summary>
     [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
     public sealed class AsyncIteratorStateMachineAttribute : StateMachineAttribute
     {
+        /// <summary>Initializes a new instance of the <see cref="AsyncIteratorStateMachineAttribute"/> class.</summary>
+        /// <param name="stateMachineType">The type object for the underlying state machine type that's used to implement a state machine method.</param>
         public AsyncIteratorStateMachineAttribute(Type stateMachineType)
             : base(stateMachineType)
         {
