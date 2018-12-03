84fd227f5ec55a1a29e0ff438f5de1081f99bbef
Viktor <viktor.hofer@microsoft.com>
2018-12-04 00:57:57 +0100

    Consume CoreFxTesting arcade package and fixes

--- src/System.ComponentModel.TypeConverter/tests/AddingNewEventArgsTests.cs
+++ src/System.ComponentModel.TypeConverter/tests/AddingNewEventArgsTests.cs
@@ -9,6 +9,12 @@ namespace System.ComponentModel.Tests
     public class AddingNewEventArgsTests
     {
         [Fact]
+        public void C()
+        {
+            var x = TypeDescriptor.GetConverter(typeof(Nullable<Boolean>));
+        }
+
+        [Fact]
         public void Ctor_Default()
         {
             var args = new AddingNewEventArgs();
