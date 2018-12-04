6f22d14ac0285ec1e4c7100753603033f6f199b2
Roma Marusyk <romamarusyk@gmail.com>
2018-12-05 01:04:51 +0200

    System.Diagnostics.EventLog . Refactor DllImports into Common\src\Interop

---
+++ src/Common/src/Interop/Windows/advapi32/Interop.ReadEventLog.cs
@@ -0,0 +1,22 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.InteropServices;
+
+internal partial class Interop
+{
+    internal partial class Advapi32
+    {
+        [DllImport(Libraries.Advapi32, CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool ReadEventLog(
+            SafeHandle hEventLog,
+            int dwReadFlags,
+            int dwRecordOffset,
+            byte[] buffer,
+            int numberOfBytesToRead,
+            out int bytesRead,
+            out int minNumOfBytesNeeded);
+    }
+}
