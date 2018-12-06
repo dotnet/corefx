ae38b7cc3762d832fdcebe27a0aab7b22ab2f3fa
Viktor Hofer <viktor.hofer@microsoft.com>
2018-12-06 02:56:11 +0100

    Consume Microsoft.DotNet.CoreFxTesting package (#33423)
    * Consume Microsoft.DotNet.CoreFxTesting package
    
    * Remove code coverage dedicated runtime dir config files and switch to Coverlet
    
    * Add global tool support in corefx
    
    * Update code coverage docs

--- src/System.Runtime.Extensions/tests/System/AppDomainTests.cs
+++ src/System.Runtime.Extensions/tests/System/AppDomainTests.cs
@@ -591,12 +591,12 @@ namespace System.Tests
             RemoteInvoke(() => {
                 ResolveEventHandler handler = (sender, e) =>
                 {
-                    return Assembly.LoadFile(Path.Combine(Environment.CurrentDirectory, "AssemblyResolveTests.exe"));
+                    return Assembly.LoadFile(Path.Combine(Environment.CurrentDirectory, "AssemblyResolveTestApp.dll"));
                 };
 
                 AppDomain.CurrentDomain.AssemblyResolve += handler;
 
-                Type t = Type.GetType("AssemblyResolveTests.Class1, AssemblyResolveTests", true);
+                Type t = Type.GetType("AssemblyResolveTestApp.Class1, AssemblyResolveTestApp", true);
                 Assert.NotNull(t);
                 return SuccessExitCode;
             }).Dispose();
@@ -614,7 +614,7 @@ namespace System.Tests
                 ResolveEventHandler handler = (sender, e) =>
                 {
                     Assert.Equal(e.RequestingAssembly, a);
-                    return Assembly.LoadFile(Path.Combine(Environment.CurrentDirectory, "AssemblyResolveTests.exe"));
+                    return Assembly.LoadFile(Path.Combine(Environment.CurrentDirectory, "AssemblyResolveTestApp.dll"));
                 };
 
                 AppDomain.CurrentDomain.AssemblyResolve += handler;
@@ -732,11 +732,11 @@ namespace System.Tests
 
         private void CopyTestAssemblies()
         {
-            string destTestAssemblyPath = Path.Combine(Environment.CurrentDirectory, "AssemblyResolveTests", "AssemblyResolveTests.dll");
-            if (!File.Exists(destTestAssemblyPath) && File.Exists("AssemblyResolveTests.dll"))
+            string destTestAssemblyPath = Path.Combine(Environment.CurrentDirectory, "AssemblyResolveTestApp", "AssemblyResolveTestApp.dll");
+            if (!File.Exists(destTestAssemblyPath) && File.Exists("AssemblyResolveTestApp.dll"))
             {
                 Directory.CreateDirectory(Path.GetDirectoryName(destTestAssemblyPath));
-                File.Copy("AssemblyResolveTests.dll", destTestAssemblyPath, false);
+                File.Copy("AssemblyResolveTestApp.dll", destTestAssemblyPath, false);
             }
 
             destTestAssemblyPath = Path.Combine(Environment.CurrentDirectory, "TestAppOutsideOfTPA", "TestAppOutsideOfTPA.exe");
