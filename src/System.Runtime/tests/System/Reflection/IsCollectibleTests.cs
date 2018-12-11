2674d1037773f05cfe5b0190bbf24a38234073dc
John Salem <josalem@microsoft.com>
2018-12-11 10:33:10 -0800

    Adding tests for generics with type parameters from collectible assembly

--- src/System.Runtime/tests/System/Reflection/IsCollectibleTests.cs
+++ src/System.Runtime/tests/System/Reflection/IsCollectibleTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -16,30 +17,13 @@ namespace System.Reflection.Tests
         private readonly string _assemblyPath;
 
         public TestAssemblyLoadContext(string assemblyName, string assemblyPath)
+            : base(true)
         {
             _assemblyName = assemblyName;
             _assemblyPath = assemblyPath;
         }
-        protected override Assembly Load(AssemblyName assemblyName)
-        {
-            return assemblyName.Name == _assemblyName ? Assembly.LoadFrom(_assemblyPath) : null;
-        }
-    }
-
-    // These types are recreated in TestCollectibleAssembly.cs
-    public class MyTestClass
-    {
-        public bool MyField = false;
-        public int MyProperty => 1;
-        public int MyMethod() { return 1 * 1; }
-    }
-
-    public class MyGenericTestClass<T>
-    {
-        public T MyGenericField;
-        public T MyGenericProperty { get; set; }
-        public T MyMethod(T arg1) { return arg1; }
-        public T MyGenericMethod<V>(T arg1, V arg2) { return arg1; }
+        protected override Assembly Load(AssemblyName assemblyName) =>
+            assemblyName.Name == _assemblyName ? LoadFromAssemblyPath(_assemblyPath) : null;
     }
 
     public class MemberInfoIsCollectibleTests : RemoteExecutorTestBase
@@ -49,11 +33,46 @@ namespace System.Reflection.Tests
 
         static public Func<AssemblyName, Assembly> assemblyResolver = (asmName) => 
             asmName.Name == asmNameString ? Assembly.LoadFrom(asmPath) : null;
+        
+        static public Func<AssemblyName, Assembly> collectibleAssemblyResolver(AssemblyLoadContext acl) => 
+            (asmName) => 
+                asmName.Name == asmNameString ? acl.LoadFromAssemblyPath(asmPath) : null;
 
         static public Func<Assembly, string, bool, Type> typeResolver(bool shouldThrowIfNotFound) => 
             (asm, simpleTypeName, isCaseSensitive) => asm == null ? 
                 Type.GetType(simpleTypeName, shouldThrowIfNotFound, isCaseSensitive) : 
                 asm.GetType(simpleTypeName, shouldThrowIfNotFound, isCaseSensitive);
+
+        [Fact]
+        public void Assembly_IsCollectibleFalse_WhenUsingAssemblyLoad()
+        {
+            RemoteInvoke(() => {
+                Assembly asm = Assembly.LoadFrom(asmPath);
+
+                Assert.NotNull(asm);
+                
+                Assert.False(asm.IsCollectible);
+
+                return SuccessExitCode;
+            });
+        }
+
+        [Fact]
+        // TODO: IsCollectible is returning false... fix it
+        public void Assembly_IsCollectibleTrue_WhenUsingAssemblyLoadContext()
+        {
+            RemoteInvoke(() => {
+                AssemblyLoadContext acl = new TestAssemblyLoadContext(asmNameString, asmPath);
+
+                Assembly asm = acl.LoadFromAssemblyName(new AssemblyName(asmNameString));
+
+                Assert.NotNull(asm);
+                
+                Assert.True(asm.IsCollectible);
+
+                return SuccessExitCode;
+            }).Dispose();
+        }
 
         [Theory]
         [InlineData("MyField")]
@@ -109,6 +128,59 @@ namespace System.Reflection.Tests
 
                 return SuccessExitCode;
             }, memberName).Dispose();
+        }
+
+        [Theory]
+        [InlineData("MyGenericField")]
+        [InlineData("MyGenericProperty")]
+        [InlineData("MyGenericMethod")]
+        [SkipOnTargetFramework(TargetFrameworkMonikers.UapAot, "Assembly.LoadFrom() is not supported on UapAot")]
+        public void MemberInfoGeneric_IsCollectibleTrue_WhenUsingAssemblyLoadContext(string memberName)
+        {
+            RemoteInvoke((marshalledName) => 
+            {
+                AssemblyLoadContext acl = new TestAssemblyLoadContext(asmNameString, asmPath);
+
+                Type t1 = Type.GetType(
+                    "TestCollectibleAssembly.MyGenericTestClass`1[System.Int32], TestCollectibleAssembly, Version=1.0.0.0", 
+                    collectibleAssemblyResolver(acl), 
+                    typeResolver(false), 
+                    true
+                );
+
+                Assert.NotNull(t1);
+
+                var member = t1.GetMember(marshalledName).FirstOrDefault();
+
+                Assert.NotNull(member);
+
+                Assert.True(member.IsCollectible);
+
+                return SuccessExitCode;
+            }, memberName).Dispose();
+        }
+
+        [Fact]
+        [SkipOnTargetFramework(TargetFrameworkMonikers.UapAot, "Assembly.LoadFrom() is not supported on UapAot")]
+        public void GenericWithCollectibleTypeParameter_IsCollectibleTrue_WhenUsingAssemblyLoadContext()
+        {
+            RemoteInvoke(() => 
+            {
+                AssemblyLoadContext acl = new TestAssemblyLoadContext(asmNameString, asmPath);
+
+                Type t1 = Type.GetType(
+                    "System.Collections.Generic.Dictionary`2[[System.Int32],[TestCollectibleAssembly.MyTestClass, TestCollectibleAssembly, Version=1.0.0.0]]", 
+                    collectibleAssemblyResolver(acl), 
+                    typeResolver(false), 
+                    true
+                );
+
+                Assert.NotNull(t1);
+
+                Assert.True(t1.IsCollectible);
+
+                return SuccessExitCode;
+            }).Dispose();
         }
     }
 }
