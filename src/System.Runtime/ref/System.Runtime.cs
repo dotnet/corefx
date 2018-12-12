d38db578d8a611f28a62de1005a9299d199bf5a9
John Salem <josalem@microsoft.com>
2018-12-12 14:32:17 -0800

    Expose `Iscollectible` property on Assembly and MemberInfo (#33979)
    * Expose IsCollectible on MemberInfo
    
    * Adding skeleton for testing IsCollectible property on MemberInfo/MethodInfo
    
    * Add test for generic tests being loaded using Assembly.LoadFrom
    
    * Expose IsCollectible on Systme.Reflection.Assembly
    
    * Adding tests for generics with type parameters from collectible assembly
    
    * Rename test class to be more general
    
    * Removing unnecessary comments
    
    * Add test skips for uapaot
    
    * Add missing Dispose() to tests
    
    * Fix typos and add framework exclusion for NetFx
    
    * Simplify TestAssemblyLoadContext.Load() per feedback
    
    * Finish simplification of TestAssemblyLoadContext and added statics to tests
    
    * Prevent IsCollectible tests from compiling on netfx

--- src/System.Runtime/ref/System.Runtime.cs
+++ src/System.Runtime/ref/System.Runtime.cs
@@ -5273,6 +5273,7 @@ namespace System.Reflection
         public virtual System.Collections.Generic.IEnumerable<System.Reflection.Module> Modules { get { throw null; } }
         public virtual bool ReflectionOnly { get { throw null; } }
         public virtual System.Security.SecurityRuleSet SecurityRuleSet { get { throw null; } }
+        public virtual bool IsCollectible { get { throw null; } }
         public virtual event System.Reflection.ModuleResolveEventHandler ModuleResolve { add { } remove { } }
         public object CreateInstance(string typeName) { throw null; }
         public object CreateInstance(string typeName, bool ignoreCase) { throw null; }
@@ -5856,6 +5857,7 @@ namespace System.Reflection
         public virtual System.Reflection.Module Module { get { throw null; } }
         public abstract string Name { get; }
         public abstract System.Type ReflectedType { get; }
+        public virtual bool IsCollectible { get { throw null; } }
         public override bool Equals(object obj) { throw null; }
         public abstract object[] GetCustomAttributes(bool inherit);
         public abstract object[] GetCustomAttributes(System.Type attributeType, bool inherit);
