3474d3629dfa265438a2171b102061db526a134f
Ben Adams <thundercat@illyriad.co.uk>
2018-12-14 22:08:31 +0000

    Fast-path inline Pipe TrySchedule

--- src/System.IO.Pipelines/src/System/IO/Pipelines/Pipe.cs
+++ src/System.IO.Pipelines/src/System/IO/Pipelines/Pipe.cs
@@ -23,7 +23,6 @@ namespace System.IO.Pipelines
         private static readonly Action<object> s_invokeCompletionCallbacks = state => ((PipeCompletionCallbacks)state).Execute();
 
         // These callbacks all point to the same methods but are different delegate types
-        private static readonly ContextCallback s_executionContextCallback = ExecuteWithExecutionContext;
         private static readonly ContextCallback s_executionContextRawCallback = ExecuteWithoutExecutionContext;
         private static readonly SendOrPostCallback s_syncContextExecutionContextCallback = ExecuteWithExecutionContext;
         private static readonly SendOrPostCallback s_syncContextExecuteWithoutExecutionContextCallback = ExecuteWithoutExecutionContext;
@@ -632,6 +631,7 @@ namespace System.IO.Pipelines
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static void TrySchedule(PipeScheduler scheduler, Action<object> action, object state)
         {
             if (action != null)
@@ -640,10 +640,12 @@ namespace System.IO.Pipelines
             }
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static void TrySchedule(PipeScheduler scheduler, in CompletionData completionData)
         {
+            Action<object> completion = completionData.Completion;
             // Nothing to do
-            if (completionData.Completion == null)
+            if (completion is null)
             {
                 return;
             }
@@ -653,24 +655,31 @@ namespace System.IO.Pipelines
             // 2. The scheduler with a delegate
             // That delegate and state will either be the action passed in directly
             // or it will be that specified delegate wrapped in ExecutionContext.Run
-
-            if (completionData.SynchronizationContext == null)
+            if (completionData.SynchronizationContext is null && completionData.ExecutionContext is null)
             {
-                // We don't have a SynchronizationContext so execute on the specified scheduler
-                if (completionData.ExecutionContext == null)
-                {
-                    // We can run directly, this should be the default fast path
-                    scheduler.UnsafeSchedule(completionData.Completion, completionData.CompletionState);
-                    return;
-                }
+                // Common fast-path
+                scheduler.UnsafeSchedule(completion, completionData.CompletionState);
+            }
+            else
+            {
+                ScheduleWithContext(scheduler, in completionData);
+            }
+        }
 
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void ScheduleWithContext(PipeScheduler scheduler, in CompletionData completionData)
+        {
+            Debug.Assert(completionData.SynchronizationContext != null || completionData.ExecutionContext != null);
+
+            if (completionData.SynchronizationContext is null)
+            {
                 // We also have to run on the specified execution context so run the scheduler and execute the
                 // delegate on the execution context
                 scheduler.UnsafeSchedule(s_scheduleWithExecutionContextCallback, completionData);
             }
             else
             {
-                if (completionData.ExecutionContext == null)
+                if (completionData.ExecutionContext is null)
                 {
                     // We need to box the struct here since there's no generic overload for state
                     completionData.SynchronizationContext.Post(s_syncContextExecuteWithoutExecutionContextCallback, completionData);
