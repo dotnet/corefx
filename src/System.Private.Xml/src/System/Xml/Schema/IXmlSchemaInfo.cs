f8b29482efcb22056873aa2078e443fe9d1ad1ae
Stanislav Ushakov <sushakov@dataart.com>
2018-12-21 16:30:33 +0300

    Fixed CS1589 for System.Private.Xml. Removed references to non-existing '*.uex' files.

--- src/System.Private.Xml/src/System/Xml/Schema/IXmlSchemaInfo.cs
+++ src/System.Private.Xml/src/System/Xml/Schema/IXmlSchemaInfo.cs
@@ -7,28 +7,20 @@ using System.Collections;
 
 namespace System.Xml.Schema
 {
-    /// <include file='doc\IXmlSchemaInfo.uex' path='docs/doc[@for="IXmlSchemaInfo"]/*' />
     public interface IXmlSchemaInfo
     {
-        /// <include file='doc\IXmlSchemaInfo.uex' path='docs/doc[@for="IXmlSchemaInfo.Validity"]/*' />
         XmlSchemaValidity Validity { get; }
 
-        /// <include file='doc\IXmlSchemaInfo.uex' path='docs/doc[@for="IXmlSchemaInfo.IsDefault"]/*' />
         bool IsDefault { get; }
 
-        /// <include file='doc\IXmlSchemaInfo.uex' path='docs/doc[@for="IXmlSchemaInfo.IsNil"]/*' />
         bool IsNil { get; }
 
-        /// <include file='doc\IXmlSchemaInfo.uex' path='docs/doc[@for="IXmlSchemaInfo.MemberType"]/*' />
         XmlSchemaSimpleType MemberType { get; }
 
-        /// <include file='doc\IXmlSchemaInfo.uex' path='docs/doc[@for="IXmlSchemaInfo.SchemaType"]/*' />
         XmlSchemaType SchemaType { get; }
 
-        /// <include file='doc\IXmlSchemaInfo.uex' path='docs/doc[@for="IXmlSchemaInfo.SchemaElement"]/*' />
         XmlSchemaElement SchemaElement { get; }
 
-        /// <include file='doc\IXmlSchemaInfo.uex' path='docs/doc[@for="IXmlSchemaInfo.SchemaAttribute"]/*' />
         XmlSchemaAttribute SchemaAttribute { get; }
     }
 }
--- src/System.Private.Xml/src/System/Xml/Schema/XmlSchemaComplexContent.cs
+++ src/System.Private.Xml/src/System/Xml/Schema/XmlSchemaComplexContent.cs
@@ -6,31 +6,19 @@ namespace System.Xml.Schema
 {
     using System.Xml.Serialization;
 
-    /// <include file='doc\XmlSchemaComplexContent.uex' path='docs/doc[@for="XmlSchemaComplexContent"]/*' />
-    /// <devdoc>
-    ///    <para>[To be supplied.]</para>
-    /// </devdoc>
     public class XmlSchemaComplexContent : XmlSchemaContentModel
     {
         private XmlSchemaContent _content;
         private bool _isMixed;
         private bool _hasMixedAttribute;
 
-        /// <include file='doc\XmlSchemaComplexContent.uex' path='docs/doc[@for="XmlSchemaComplexContent.IsMixed"]/*' />
-        /// <devdoc>
-        ///    <para>[To be supplied.]</para>
-        /// </devdoc>
-        [XmlAttribute("mixed")]
+         [XmlAttribute("mixed")]
         public bool IsMixed
         {
             get { return _isMixed; }
             set { _isMixed = value; _hasMixedAttribute = true; }
         }
 
-        /// <include file='doc\XmlSchemaComplexContent.uex' path='docs/doc[@for="XmlSchemaComplexContent.Content"]/*' />
-        /// <devdoc>
-        ///    <para>[To be supplied.]</para>
-        /// </devdoc>
         [XmlElement("restriction", typeof(XmlSchemaComplexContentRestriction)),
          XmlElement("extension", typeof(XmlSchemaComplexContentExtension))]
         public override XmlSchemaContent Content
