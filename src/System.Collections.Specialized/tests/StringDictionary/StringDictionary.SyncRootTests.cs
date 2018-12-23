63c8021acc043eb2e394a7e1b9a641509476410c
danmosemsft <danmose@microsoft.com>
2018-12-22 22:17:23 -0800

    Disable test for NETFX

--- src/System.Collections.Specialized/tests/StringDictionary/StringDictionary.SyncRootTests.cs
+++ src/System.Collections.Specialized/tests/StringDictionary/StringDictionary.SyncRootTests.cs
@@ -11,6 +11,7 @@ namespace System.Collections.Specialized.Tests
         [Theory]
         [InlineData(0)]
         [InlineData(5)]
+        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework)] // Different implementation
         public void SyncRoot(int count)
         {
             StringDictionary stringDictionary1 = Helpers.CreateStringDictionary(count);
