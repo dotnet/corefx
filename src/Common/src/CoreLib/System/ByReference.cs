e215305796bdb73de5b67776f28d591fab74cdaa
Michal Strehovský <MichalStrehovsky@users.noreply.github.com>
2018-12-23 17:09:39 +0100

    Fix ProjectN regression in `ByReference<T>` (dotnet/corert#6713)
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/ByReference.cs
+++ src/Common/src/CoreLib/System/ByReference.cs
@@ -11,7 +11,11 @@ namespace System
     // around lack of first class support for byref fields in C# and IL. The JIT and 
     // type loader has special handling for it that turns it into a thin wrapper around ref T.
     [NonVersionable]
-    internal readonly ref struct ByReference<T>
+    internal
+#if !PROJECTN // readonly breaks codegen contract and asserts UTC
+    readonly
+#endif
+    ref struct ByReference<T>
     {
         // CS0169: The private field '{blah}' is never used
 #pragma warning disable 169
