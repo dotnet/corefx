5720176d924ad2aa016856db021c1a87ff4cacac
Paul Westcott <p.westcott@gmail.com>
2019-01-07 17:22:21 +1100

    Added ToDictionary

---
+++ src/System.Linq/src/System/Linq/ChainLinq/Consumer/ToDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq.ChainLinq.Consumer
+{
+    sealed class ToDictionary<TSource, TKey> : Consumer<TSource, Dictionary<TKey, TSource>>
+    {
+        private readonly Func<TSource, TKey> _keySelector;
+
+        public ToDictionary(Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
+            : base(new Dictionary<TKey, TSource>(comparer)) =>
+        _keySelector = keySelector;
+
+        public ToDictionary(Func<TSource, TKey> keySelector, int capacity, IEqualityComparer<TKey> comparer)
+            : base(new Dictionary<TKey, TSource>(capacity, comparer)) =>
+        _keySelector = keySelector;
+
+        public override ChainStatus ProcessNext(TSource input)
+        {
+            Result.Add(_keySelector(input), input);
+
+            return ChainStatus.Flow;
+        }
+    }
+
+    sealed class ToDictionary<TSource, TKey, TElement> : Consumer<TSource, Dictionary<TKey, TElement>>
+    {
+        private readonly Func<TSource, TKey> _keySelector;
+        private readonly Func<TSource, TElement> _elementSelector;
+
+        public ToDictionary(Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, IEqualityComparer<TKey> comparer)
+            : base(new Dictionary<TKey, TElement>(comparer)) =>
+        (_keySelector, _elementSelector) = (keySelector, elementSelector);
+
+        public ToDictionary(Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, int capacity, IEqualityComparer<TKey> comparer)
+            : base(new Dictionary<TKey, TElement>(capacity, comparer)) =>
+        (_keySelector, _elementSelector) = (keySelector, elementSelector);
+
+        public override ChainStatus ProcessNext(TSource input)
+        {
+            Result.Add(_keySelector(input), _elementSelector(input));
+
+            return ChainStatus.Flow;
+        }
+    }
+}
--- src/System.Linq/src/System/Linq/ToCollection.cs
+++ src/System.Linq/src/System/Linq/ToCollection.cs
@@ -71,55 +71,18 @@ namespace System.Linq
                 throw Error.ArgumentNull(nameof(keySelector));
             }
 
-            int capacity = 0;
-            if (source is ICollection<TSource> collection)
+            var consumable = ChainLinq.Utils.AsConsumable(source);
+
+            if (consumable is ChainLinq.Optimizations.ICountOnConsumable counter)
             {
-                capacity = collection.Count;
-                if (capacity == 0)
+                var count = counter.GetCount(true);
+                if (count >= 0)
                 {
-                    return new Dictionary<TKey, TSource>(comparer);
-                }
-
-                if (collection is TSource[] array)
-                {
-                    return ToDictionary(array, keySelector, comparer);
-                }
-
-                if (collection is List<TSource> list)
-                {
-                    return ToDictionary(list, keySelector, comparer);
+                    consumable.Consume(new ChainLinq.Consumer.ToDictionary<TSource, TKey>(keySelector, count, comparer));
                 }
             }
 
-            Dictionary<TKey, TSource> d = new Dictionary<TKey, TSource>(capacity, comparer);
-            foreach (TSource element in source)
-            {
-                d.Add(keySelector(element), element);
-            }
-
-            return d;
-        }
-
-        private static Dictionary<TKey, TSource> ToDictionary<TSource, TKey>(TSource[] source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
-        {
-            Dictionary<TKey, TSource> d = new Dictionary<TKey, TSource>(source.Length, comparer);
-            for (int i = 0; i < source.Length; i++)
-            {
-                d.Add(keySelector(source[i]), source[i]);
-            }
-
-            return d;
-        }
-
-        private static Dictionary<TKey, TSource> ToDictionary<TSource, TKey>(List<TSource> source, Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
-        {
-            Dictionary<TKey, TSource> d = new Dictionary<TKey, TSource>(source.Count, comparer);
-            foreach (TSource element in source)
-            {
-                d.Add(keySelector(element), element);
-            }
-
-            return d;
+            return consumable.Consume(new ChainLinq.Consumer.ToDictionary<TSource, TKey>(keySelector, comparer));
         }
 
         public static Dictionary<TKey, TElement> ToDictionary<TSource, TKey, TElement>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector) =>
@@ -142,55 +105,18 @@ namespace System.Linq
                 throw Error.ArgumentNull(nameof(elementSelector));
             }
 
-            int capacity = 0;
-            if (source is ICollection<TSource> collection)
+            var consumable = ChainLinq.Utils.AsConsumable(source);
+
+            if (consumable is ChainLinq.Optimizations.ICountOnConsumable counter)
             {
-                capacity = collection.Count;
-                if (capacity == 0)
+                var count = counter.GetCount(true);
+                if (count >= 0)
                 {
-                    return new Dictionary<TKey, TElement>(comparer);
-                }
-
-                if (collection is TSource[] array)
-                {
-                    return ToDictionary(array, keySelector, elementSelector, comparer);
-                }
-
-                if (collection is List<TSource> list)
-                {
-                    return ToDictionary(list, keySelector, elementSelector, comparer);
+                    consumable.Consume(new ChainLinq.Consumer.ToDictionary<TSource, TKey, TElement>(keySelector, elementSelector, count, comparer));
                 }
             }
 
-            Dictionary<TKey, TElement> d = new Dictionary<TKey, TElement>(capacity, comparer);
-            foreach (TSource element in source)
-            {
-                d.Add(keySelector(element), elementSelector(element));
-            }
-
-            return d;
-        }
-
-        private static Dictionary<TKey, TElement> ToDictionary<TSource, TKey, TElement>(TSource[] source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, IEqualityComparer<TKey> comparer)
-        {
-            Dictionary<TKey, TElement> d = new Dictionary<TKey, TElement>(source.Length, comparer);
-            for (int i = 0; i < source.Length; i++)
-            {
-                d.Add(keySelector(source[i]), elementSelector(source[i]));
-            }
-
-            return d;
-        }
-
-        private static Dictionary<TKey, TElement> ToDictionary<TSource, TKey, TElement>(List<TSource> source, Func<TSource, TKey> keySelector, Func<TSource, TElement> elementSelector, IEqualityComparer<TKey> comparer)
-        {
-            Dictionary<TKey, TElement> d = new Dictionary<TKey, TElement>(source.Count, comparer);
-            foreach (TSource element in source)
-            {
-                d.Add(keySelector(element), elementSelector(element));
-            }
-
-            return d;
+            return consumable.Consume(new ChainLinq.Consumer.ToDictionary<TSource, TKey, TElement>(keySelector, elementSelector, comparer));
         }
 
         public static HashSet<TSource> ToHashSet<TSource>(this IEnumerable<TSource> source) => source.ToHashSet(comparer: null);
