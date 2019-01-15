bc34e6a2e01fc4b1768ed24135aa8715eb670c2d
Maryam Ariyan <maryam.ariyan@microsoft.com>
2019-01-15 01:29:08 -0800

    Add more tests for Set APIs on DynamicILInfo

--- src/System.Reflection.Emit.Lightweight/tests/DynamicILInfoTests.cs
+++ src/System.Reflection.Emit.Lightweight/tests/DynamicILInfoTests.cs
@@ -2,13 +2,86 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Runtime.InteropServices;
 using Xunit;
 
 namespace System.Reflection.Emit.Tests
 {
     public class DynamicILInfoTests
     {
-        private const string FieldName = "_id";
+        private const string LocalSignature = "LocalSignature";
+        private const string Exceptions = "Exceptions";
+        private const string Code = "Code";
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public unsafe void SetX_ArrayNull_SetsToEmptyArray(bool skipVisibility)
+        {
+            DynamicMethod dynamicMethod = GetDynamicMethod(skipVisibility);
+            DynamicILInfo dynamicILInfo = dynamicMethod.GetDynamicILInfo();
+
+            dynamicILInfo.SetLocalSignature(null);
+            Assert.Equal(0, GetPropertyValue(dynamicILInfo, LocalSignature).Length);
+            dynamicILInfo.SetExceptions(null);
+            Assert.Equal(0, GetPropertyValue(dynamicILInfo, Exceptions).Length);
+            dynamicILInfo.SetCode(null, 2);
+            Assert.Equal(0, GetPropertyValue(dynamicILInfo, Code).Length);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public unsafe void SetX_ArrayEmpty_SetsToEmptyArray(bool skipVisibility)
+        {
+            DynamicMethod dynamicMethod = GetDynamicMethod(skipVisibility);
+			DynamicILInfo dynamicILInfo = dynamicMethod.GetDynamicILInfo();
+
+			dynamicILInfo.SetLocalSignature(new Byte[] { });
+            Assert.Equal(0, GetPropertyValue(dynamicILInfo, LocalSignature).Length);
+			dynamicILInfo.SetExceptions(new Byte[] { });
+            Assert.Equal(0, GetPropertyValue(dynamicILInfo, Exceptions).Length);
+            dynamicILInfo.SetCode(new Byte[] { }, 2);
+            Assert.Equal(0, GetPropertyValue(dynamicILInfo, Code).Length);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public unsafe void SetX_ArrayNotNullOrEmpty_Success(bool skipVisibility)
+        {
+            Byte[] brCode = new Byte[] { 35, 26, 98, 0, 17, 128, 9, 23, 69, 12, 07, 84, 4, 43 };
+            DynamicMethod dynamicMethod = GetDynamicMethod(skipVisibility);
+			DynamicILInfo dynamicILInfo = dynamicMethod.GetDynamicILInfo();
+
+			dynamicILInfo.SetLocalSignature(brCode);
+            Assert.Equal(brCode, GetPropertyValue(dynamicILInfo, LocalSignature));
+			dynamicILInfo.SetExceptions(brCode);
+            Assert.Equal(brCode, GetPropertyValue(dynamicILInfo, Exceptions));
+            dynamicILInfo.SetCode(brCode, 1000004);
+            Assert.Equal(brCode, GetPropertyValue(dynamicILInfo, Code));
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public unsafe void SetX_LengthIsZero_SetsToEmptyArray(bool skipVisibility)
+        {
+            DynamicMethod dynamicMethod = GetDynamicMethod(skipVisibility);
+            Byte[] brCode = new Byte[] { };
+			// Get the pointer to the byte array.
+		    GCHandle hmem = GCHandle.Alloc((Object) brCode, GCHandleType.Pinned);
+			IntPtr addr = hmem.AddrOfPinnedObject();
+			DynamicILInfo dynamicILInfo = dynamicMethod.GetDynamicILInfo();
+
+			dynamicILInfo.SetLocalSignature((byte*)addr, brCode.Length);
+            Assert.Equal(0, GetPropertyValue(dynamicILInfo, LocalSignature).Length);
+			dynamicILInfo.SetExceptions((byte*)addr, brCode.Length);
+            Assert.Equal(0, GetPropertyValue(dynamicILInfo, Exceptions).Length);
+			dynamicILInfo.SetCode((byte*)addr, brCode.Length, 4);
+            Assert.Equal(0, GetPropertyValue(dynamicILInfo, Code).Length);
+			hmem.Free();
+        }
 
         [Theory]
         [InlineData(true)]
@@ -37,10 +110,29 @@ namespace System.Reflection.Emit.Tests
             Assert.Throws<ArgumentOutOfRangeException>(() => {fixed (byte* bytesPtr = bytes) { dynamicILInfo.SetLocalSignature(bytesPtr, -1); }});
         }
 
+        [Fact]
+        public unsafe void SetX_LengthNotZero_Success()
+        {
+            DynamicMethod dynamicMethod = new DynamicMethod("DynamicMethod5", typeof(void), null, typeof(Int32), true);
+            Byte[] brCode = new Byte[] { 0x03, 0x30, 0x0A, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x17, 0x2a };
+            // Get the pointer to the byte array.
+            GCHandle hmem = GCHandle.Alloc((Object) brCode, GCHandleType.Pinned);
+            IntPtr addr = hmem.AddrOfPinnedObject();
+			DynamicILInfo dynamicILInfo = dynamicMethod.GetDynamicILInfo();
+
+			dynamicILInfo.SetLocalSignature((byte*)addr, brCode.Length);
+            Assert.Equal(brCode, GetPropertyValue(dynamicILInfo, LocalSignature));
+			dynamicILInfo.SetExceptions((byte*)addr, brCode.Length);
+            Assert.Equal(brCode, GetPropertyValue(dynamicILInfo, Exceptions));
+			dynamicILInfo.SetCode((byte*)addr, brCode.Length, 198);
+            Assert.Equal(brCode, GetPropertyValue(dynamicILInfo, Code));
+            hmem.Free();
+        }
+
         [Theory]
         [InlineData(true)]
         [InlineData(false)]
-        public void GetDynamicILInfo_IsUniqueNotNull(bool skipVisibility)
+        public void GetDynamicILInfo_IsNotUniqueNotNull(bool skipVisibility)
         {
             DynamicMethod method = GetDynamicMethod(skipVisibility);
             DynamicILInfo dynamicILInfo = method.GetDynamicILInfo();
@@ -52,11 +144,18 @@ namespace System.Reflection.Emit.Tests
         
         private DynamicMethod GetDynamicMethod(bool skipVisibility)
         {
-            IDClass target = new IDClass();
-            FieldInfo field = typeof(IDClass).GetField(FieldName, BindingFlags.Instance | BindingFlags.NonPublic);
-            Type[] paramTypes = new Type[] { typeof(IDClass), typeof(int) };
-            DynamicMethod method = new DynamicMethod("Method", typeof(int), paramTypes, typeof(IDClass), skipVisibility);
-            return method;
+            return new DynamicMethod("DynamicMethod", typeof(void), 
+                new Type[] { typeof(object), typeof(int), typeof(string) },
+                typeof(Object),
+                skipVisibility);
+        }
+
+        private static Byte[] GetPropertyValue(DynamicILInfo dynamicInfo, string property)
+        {
+            // Get property value for verification.
+            PropertyInfo pinfo = typeof(DynamicILInfo).GetProperty(property, BindingFlags.NonPublic | BindingFlags.Instance);
+            Assert.NotNull(pinfo);
+            return (Byte[])pinfo.GetValue(dynamicInfo, BindingFlags.NonPublic | BindingFlags.Instance, null, null, null);
         }
     }
 }
