827f91c666c2b40e078c3f1df7a7e5ab13781eaa
Stephen Toub <stoub@microsoft.com>
2019-01-17 17:03:41 -0500

    Use RemoteInvoke with tracing tests (#34645)
    Various test projects validate that EventSource data is being appropriately generated.  Since these tests can run concurrently with others in the process, they should be done as out-of-process such that they don't interfere with each other, but some test projects aren't doing that.  This fixes that, ensuring we're using RemoteInvoke everywhere we should be.

--- src/System.Collections.Concurrent/tests/EtwTests.cs
+++ src/System.Collections.Concurrent/tests/EtwTests.cs
@@ -2,49 +2,53 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics;
 using System.Diagnostics.Tracing;
 using System.Linq;
 using Xunit;
 
 namespace System.Collections.Concurrent.Tests
 {
-    public class EtwTests
+    public class EtwTests : RemoteExecutorTestBase
     {
         [Fact]
         public void TestEtw()
         {
-            using (var listener = new TestEventListener("System.Collections.Concurrent.ConcurrentCollectionsEventSource", EventLevel.Verbose))
+            RemoteInvoke(() =>
             {
-                var events = new ConcurrentQueue<int>();
-
-                const int AcquiringAllLocksEventId = 3;
-                Clear(events);
-                listener.RunWithCallback(ev => events.Enqueue(ev.EventId), () =>
+                using (var listener = new TestEventListener("System.Collections.Concurrent.ConcurrentCollectionsEventSource", EventLevel.Verbose))
                 {
-                    var cd = new ConcurrentDictionary<int, int>();
-                    cd.TryAdd(1, 1);
-                    cd.Clear();
-                });
-                Assert.True(events.Count(i => i == AcquiringAllLocksEventId) > 0);
+                    var events = new ConcurrentQueue<int>();
 
-                const int TryTakeStealsEventId = 4;
-                const int TryPeekStealsEventId = 5;
-                Clear(events);
-                listener.RunWithCallback(ev => events.Enqueue(ev.EventId), () =>
-                {
-                    var cb = new ConcurrentBag<int>();
-                    int item;
-                    cb.TryPeek(out item);
-                    cb.TryTake(out item);
-                });
-                Assert.True(events.Count(i => i == TryPeekStealsEventId) > 0);
-                Assert.True(events.Count(i => i == TryTakeStealsEventId) > 0);
+                    const int AcquiringAllLocksEventId = 3;
+                    Clear(events);
+                    listener.RunWithCallback(ev => events.Enqueue(ev.EventId), () =>
+                    {
+                        var cd = new ConcurrentDictionary<int, int>();
+                        cd.TryAdd(1, 1);
+                        cd.Clear();
+                    });
+                    Assert.True(events.Count(i => i == AcquiringAllLocksEventId) > 0);
 
-                // No tests for:
-                //      CONCURRENTSTACK_FASTPUSHFAILED_ID
-                //      CONCURRENTSTACK_FASTPOPFAILED_ID
-                // These require certain race condition interleavings in order to fire.
-            }
+                    const int TryTakeStealsEventId = 4;
+                    const int TryPeekStealsEventId = 5;
+                    Clear(events);
+                    listener.RunWithCallback(ev => events.Enqueue(ev.EventId), () =>
+                    {
+                        var cb = new ConcurrentBag<int>();
+                        int item;
+                        cb.TryPeek(out item);
+                        cb.TryTake(out item);
+                    });
+                    Assert.True(events.Count(i => i == TryPeekStealsEventId) > 0);
+                    Assert.True(events.Count(i => i == TryTakeStealsEventId) > 0);
+
+                    // No tests for:
+                    //      CONCURRENTSTACK_FASTPUSHFAILED_ID
+                    //      CONCURRENTSTACK_FASTPOPFAILED_ID
+                    // These require certain race condition interleavings in order to fire.
+                }
+            }).Dispose();
         }
 
         private static void Clear<T>(ConcurrentQueue<T> queue)
