1e225a3ae0902bfa4e1c4b398aa8d06640f39a50
Paul Westcott <p.westcott@gmail.com>
2019-01-24 16:55:34 +1100

    Added WhereSelect

--- src/System.Linq/src/System/Linq/ChainLinq/Consumables/Where.SpeedOpt.cs
+++ src/System.Linq/src/System/Linq/ChainLinq/Consumables/Where.SpeedOpt.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections.Generic;
-using System.Linq.ChainLinq.Optimizations;
+﻿using System.Linq.ChainLinq.Optimizations;
 
 namespace System.Linq.ChainLinq.Consumables
 {
     sealed partial class WhereArray<T> : IMergeSelect<T>, IMergeWhere<T>
     {
         Consumable<V> IMergeSelect<T>.MergeSelect<V>(ConsumableForMerging<T> _, Func<T, V> u2v) =>
-            new Array<T, V>(Underlying, new Links.WhereSelect<T, V>(Predicate, u2v));
+            new WhereSelectArray<T, V>(Underlying, Predicate, u2v);
 
         Consumable<T> IMergeWhere<T>.MergeWhere(ConsumableForMerging<T> _, Func<T, bool> predicate) =>
             new WhereArray<T>(Underlying, t => Predicate(t) && predicate(t));
@@ -15,7 +14,7 @@ namespace System.Linq.ChainLinq.Consumables
     sealed partial class WhereList<T> : IMergeSelect<T>, IMergeWhere<T>
     {
         Consumable<V> IMergeSelect<T>.MergeSelect<V>(ConsumableForMerging<T> _, Func<T, V> u2v) =>
-            new List<T, V>(Underlying, new Links.WhereSelect<T, V>(Predicate, u2v));
+            new WhereSelectList<T, V>(Underlying, Predicate, u2v);
 
         Consumable<T> IMergeWhere<T>.MergeWhere(ConsumableForMerging<T> consumable, Func<T, bool> predicate) =>
             new WhereList<T>(Underlying, t => Predicate(t) && predicate(t));
@@ -24,7 +23,7 @@ namespace System.Linq.ChainLinq.Consumables
     sealed partial class WhereEnumerable<T> : IMergeSelect<T>, IMergeWhere<T>
     {
         Consumable<V> IMergeSelect<T>.MergeSelect<V>(ConsumableForMerging<T> consumable, Func<T, V> u2v) =>
-            new Enumerable<T, V>(Underlying, new Links.WhereSelect<T,V>(Predicate, u2v));
+            new WhereSelectEnumerable<T, V>(Underlying, Predicate, u2v);
 
         Consumable<T> IMergeWhere<T>.MergeWhere(ConsumableForMerging<T> consumable, Func<T, bool> predicate) =>
             new WhereEnumerable<T>(Underlying, t => Predicate(t) && predicate(t));
