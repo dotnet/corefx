a0f36904809e68d3e57b6b838564981e12747376
Marek Safar <marek.safar@gmail.com>
2019-01-25 02:00:33 +0100

    Move Marshal to shared partition (dotnet/coreclr#22146)
    * Move Marshal to shared partition
    
    * Moved code optimizations
    
    * Cosmetic changes
    
    * Avoid duplicate arguments checks bys shipping Encoding.UTF8.GetString
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

---
+++ src/Common/src/CoreLib/System/Runtime/InteropServices/Marshal.Windows.cs
@@ -0,0 +1,23 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Runtime.InteropServices
+{
+    public static partial class Marshal
+    {
+        // Win32 has the concept of Atoms, where a pointer can either be a pointer
+        // or an int.  If it's less than 64K, this is guaranteed to NOT be a
+        // pointer since the bottom 64K bytes are reserved in a process' page table.
+        // We should be careful about deallocating this stuff.  Extracted to
+        // a function to avoid C# problems with lack of support for IntPtr.
+        // We have 2 of these methods for slightly different semantics for NULL.
+        private static bool IsWin32Atom(IntPtr ptr)
+        {
+            const long HIWORDMASK = unchecked((long)0xffffffffffff0000L);
+
+            long lPtr = (long)ptr;
+            return 0 == (lPtr & HIWORDMASK);
+        }
+    }
+}
