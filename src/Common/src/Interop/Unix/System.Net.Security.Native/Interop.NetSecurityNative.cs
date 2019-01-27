eec001d96a68376c0e504eb7635c8edec196f90f
David Shulman <david.shulman@microsoft.com>
2019-01-27 15:41:53 -0800

    Fix GSSAPI error handling (#34883)
    This PR addresses 3 issues in GSSAPI error handling:
    
    * Fixes an uninitialized variable when calling gss_display_status().
    This was causing the major statuscode error message to always return
    'An invalid status code was supplied'.
    
    * If the minor statuscode is 0 (GSS_S_COMPLETE), we shouldn't be calling
    for the minor statuscode error message text. That is why we were always getting
    'Unknown error' for that text.
    
    * Added more text (help text) to certain errors returned from GSSAPI
    such as when the NTLM plugin is not installed.
    
    No new tests were added to this PR since it requires machine changes
    to test. However, I manually tested various combinations of errors
    including when 'gss-ntlmssp' is installed or not.
    
    In terms of #34877, the exception message goes from:
    
    >GSSAPI operation failed with error - An invalid status code was supplied (Unknown error).
    
    to
    
    >GSSAPI operation failed with error - An unsupported mechanism was requested. NTLM authentication requires the GSSAPI plugin 'gss-ntlmssp'.
    
    Fixes #34877

--- src/Common/src/Interop/Unix/System.Net.Security.Native/Interop.NetSecurityNative.cs
+++ src/Common/src/Interop/Unix/System.Net.Security.Native/Interop.NetSecurityNative.cs
@@ -148,7 +148,8 @@ internal static partial class Interop
         internal enum Status : uint
         {
             GSS_S_COMPLETE = 0,
-            GSS_S_CONTINUE_NEEDED = 1
+            GSS_S_CONTINUE_NEEDED = 1,
+            GSS_S_BAD_MECH = 65536
         }
 
         [Flags]
