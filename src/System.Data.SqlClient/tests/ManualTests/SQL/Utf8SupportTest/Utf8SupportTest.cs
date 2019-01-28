652f5c1cfcdb108305f7df5b4a6b1ecd1a3ae6fd
Wraith2 <wraith2@gmail.com>
2019-01-28 22:44:40 +0000

    re-add UTF8 test using guard condition

---
+++ src/System.Data.SqlClient/tests/ManualTests/SQL/Utf8SupportTest/Utf8SupportTest.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace System.Data.SqlClient.ManualTesting.Tests
+{
+    public static class Utf8SupportTest
+    {
+        [ConditionalFact(typeof(DataTestUtility), nameof(DataTestUtility.AreConnStringsSetup), nameof(DataTestUtility.IsUTF8Supported))]
+        public static void CheckSupportUtf8ConnectionProperty()
+        {
+            using (SqlConnection connection = new SqlConnection(DataTestUtility.TcpConnStr))
+            using (SqlCommand command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = "SELECT CONNECTIONPROPERTY('SUPPORT_UTF8')";
+                connection.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Assert.Equal(1, reader.GetInt32(0));
+                    }
+                }
+            }
+        }
+    }
+}
