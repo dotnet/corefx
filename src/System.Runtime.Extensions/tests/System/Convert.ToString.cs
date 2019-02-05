17c969e59f6de5c2487e84bb54ec0b653e99148f
danmosemsft <danmose@microsoft.com>
2019-02-04 16:57:54 -0800

    Another

--- src/System.Runtime.Extensions/tests/System/Convert.ToString.cs
+++ src/System.Runtime.Extensions/tests/System/Convert.ToString.cs
@@ -181,7 +181,7 @@ namespace System.Tests
         }
 
         [Fact]
-        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework)]
+        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework | TargetFrameworkMonikers.UapAot)]
         public static void FromBoxedObject_NotNetFramework()
         {
             object[] testValues =
