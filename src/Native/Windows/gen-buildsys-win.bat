b1ddfa7b33eb8f0e47627f815da57fc3074be2ad
Santiago Fernandez Madero <safern@microsoft.com>
2019-02-08 20:02:48 -0800

    Support building corefx in VS2019

--- src/Native/Windows/gen-buildsys-win.bat
+++ src/Native/Windows/gen-buildsys-win.bat
@@ -8,18 +8,29 @@ for %%x in (%*) do Set /A argC+=1
 if NOT %argC%==3 GOTO :USAGE
 if %1=="/?" GOTO :USAGE
 
-setlocal
+setlocal EnableDelayedExpansion
 set __sourceDir=%~dp0
 set __ExtraCmakeParams=
 
-:: VS 2017 is required to build native assets,
-:: once CMake supports 2019 as the project generator we can use that if installed in the local system
-set __VSString=15 2017
+:: VS 2017 is the minimum supported toolset
+if "%__VSVersion%" == "vs2019" (
+  set __VSString=16 2019
 
-:: Set the target architecture to a format cmake understands. ANYCPU defaults to x64
-if /i "%3" == "x86"     (set __VSString=%__VSString%)
-if /i "%3" == "x64"     (set __VSString=%__VSString% Win64)
-if /i "%3" == "arm"     (set __VSString=%__VSString% ARM)
+  :: VS 2019 generator is not per arch, and the arch needs to be passed with -A option
+  :: x86 is the default arch selected by cmake.
+  :: CMAKE 3.14 or later is required to use VS2019
+  if /i "%3" == "x64"     (set __ExtraCmakeParams=%__ExtraCmakeParams% -A x64)
+  if /i "%3" == "arm"     (set __ExtraCmakeParams=%__ExtraCmakeParams% -A ARM)
+) else (
+  set __VSString=15 2017
+
+  :: VS2017 generator needs to be specified with the arch as part of its name.
+  :: x86 is the default arch selected by cmake.
+  if /i "%3" == "x64"     (set __VSString=!__VSString! Win64)
+  if /i "%3" == "arm"     (set __VSString=!__VSString! ARM)
+)
+
+:: arm64 doesn't have a specific generator so we need to pass it through -A option.
 if /i "%3" == "arm64"   (set __ExtraCmakeParams=%__ExtraCmakeParams% -A ARM64)
 if /i "%3" == "wasm"    (set __sourceDir=%~dp0..\Unix && goto DoGen)
 
@@ -35,7 +46,7 @@ popd
 if "%3" == "wasm" (
     emcmake cmake "-DEMSCRIPTEN_GENERATE_BITCODE_STATIC_LIBRARIES=1" "-DCMAKE_TOOLCHAIN_FILE=%EMSCRIPTEN%/cmake/Modules/Platform/Emscripten.cmake" "-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE%" -G "NMake Makefiles" %__sourceDir%
 ) else (
-    "%CMakePath%" %__SDKVersion% "-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE%" "-DCMAKE_INSTALL_PREFIX=%__CMakeBinDir%" -G "Visual Studio %__VSString%" -B. -H%1 %__ExtraCmakeParams% 
+    "%CMakePath%" %__SDKVersion% "-DCMAKE_BUILD_TYPE=%CMAKE_BUILD_TYPE%" "-DCMAKE_INSTALL_PREFIX=%__CMakeBinDir%" -G "Visual Studio %__VSString%" -B. -H%1 %__ExtraCmakeParams%
 )
 endlocal
 GOTO :DONE
