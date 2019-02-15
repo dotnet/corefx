b3445a53358f65c2b6e16c3e23d01f2f9f3d4e9d
Ahson Khan <ahkha@microsoft.com>
2019-02-15 02:15:53 -0800

    Add obsolete attributes to S.Collections.Specialized

--- src/System.Collections.Specialized/ref/System.Collections.Specialized.cs
+++ src/System.Collections.Specialized/ref/System.Collections.Specialized.cs
@@ -90,9 +90,11 @@ namespace System.Collections.Specialized
     {
         protected NameObjectCollectionBase() { }
         protected NameObjectCollectionBase(System.Collections.IEqualityComparer equalityComparer) { }
+        [System.ObsoleteAttribute("Please use NameObjectCollectionBase(IEqualityComparer) instead.")]
         protected NameObjectCollectionBase(System.Collections.IHashCodeProvider hashProvider, System.Collections.IComparer comparer) { }
         protected NameObjectCollectionBase(int capacity) { }
         protected NameObjectCollectionBase(int capacity, System.Collections.IEqualityComparer equalityComparer) { }
+        [System.ObsoleteAttribute("Please use NameObjectCollectionBase(Int32, IEqualityComparer) instead.")]
         protected NameObjectCollectionBase(int capacity, System.Collections.IHashCodeProvider hashProvider, System.Collections.IComparer comparer) { }
         protected NameObjectCollectionBase(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) { }
         public virtual int Count { get { throw null; } }
@@ -133,10 +135,12 @@ namespace System.Collections.Specialized
     {
         public NameValueCollection() { }
         public NameValueCollection(System.Collections.IEqualityComparer equalityComparer) { }
+        [System.ObsoleteAttribute("Please use NameValueCollection(IEqualityComparer) instead.")]
         public NameValueCollection(System.Collections.IHashCodeProvider hashProvider, System.Collections.IComparer comparer) { }
         public NameValueCollection(System.Collections.Specialized.NameValueCollection col) { }
         public NameValueCollection(int capacity) { }
         public NameValueCollection(int capacity, System.Collections.IEqualityComparer equalityComparer) { }
+        [System.ObsoleteAttribute("Please use NameValueCollection(Int32, IEqualityComparer) instead.")]
         public NameValueCollection(int capacity, System.Collections.IHashCodeProvider hashProvider, System.Collections.IComparer comparer) { }
         public NameValueCollection(int capacity, System.Collections.Specialized.NameValueCollection col) { }
         protected NameValueCollection(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) { }
