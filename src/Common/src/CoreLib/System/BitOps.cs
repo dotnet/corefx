c463b0d22a35a877ea7806c11003eac9638afd5d
Tanner Gooding <tagoo@outlook.com>
2019-02-15 15:17:52 -0800

    Updating some local functions to be 'static' (captureless). (#22635)
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/BitOps.cs
+++ src/Common/src/CoreLib/System/BitOps.cs
@@ -306,18 +306,18 @@ namespace System
 
             return SoftwareFallback(value);
 
-            int SoftwareFallback(uint v)
+            static int SoftwareFallback(uint value)
             {
                 const uint c1 = 0x_55555555u;
                 const uint c2 = 0x_33333333u;
                 const uint c3 = 0x_0F0F0F0Fu;
                 const uint c4 = 0x_01010101u;
 
-                v = v - ((v >> 1) & c1);
-                v = (v & c2) + ((v >> 2) & c2);
-                v = (((v + (v >> 4)) & c3) * c4) >> 24;
+                value = value - ((value >> 1) & c1);
+                value = (value & c2) + ((value >> 2) & c2);
+                value = (((value + (value >> 4)) & c3) * c4) >> 24;
 
-                return (int)v;
+                return (int)value;
             }
         }
 
@@ -340,18 +340,18 @@ namespace System
 #else
             return SoftwareFallback(value);
 
-            int SoftwareFallback(ulong v) 
+            static int SoftwareFallback(ulong value) 
             {
                 const ulong c1 = 0x_55555555_55555555ul;
                 const ulong c2 = 0x_33333333_33333333ul;
                 const ulong c3 = 0x_0F0F0F0F_0F0F0F0Ful;
                 const ulong c4 = 0x_01010101_01010101ul;
 
-                v = v - ((v >> 1) & c1);
-                v = (v & c2) + ((v >> 2) & c2);
-                v = (((v + (v >> 4)) & c3) * c4) >> 56;
+                value = value - ((value >> 1) & c1);
+                value = (value & c2) + ((value >> 2) & c2);
+                value = (((value + (value >> 4)) & c3) * c4) >> 56;
 
-                return (int)v;
+                return (int)value;
             }
 #endif
         }
