610e6ece1eda3aedca87ba8aca3f57a785c394a6
Tomas Weinfurt <toweinfurt@microsoft.com>
2019-02-14 20:22:50 -0800

    fix itteration

--- src/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/HPack/HPackEncoder.cs
+++ src/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/HPack/HPackEncoder.cs
@@ -175,11 +175,11 @@ namespace System.Net.Http.HPack
             int i = 0;
             for (int j = 0; j < s.Length; j++)
             {
-                if ((c & 0xFF80) != 0)
+                if ((s[j] & 0xFF80) != 0)
                 {
                     throw new HttpRequestException(SR.net_http_request_invalid_char_encoding);
                 }
-                buffer[i++] = (byte)c[j];
+                buffer[i++] = (byte)s[j];
             }
 
             encodedLength = i;
