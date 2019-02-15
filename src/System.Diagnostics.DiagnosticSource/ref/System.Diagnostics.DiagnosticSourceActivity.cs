4973729e58aa3825505f433d722c27bbcdbc20e3
Ahson Khan <ahkha@microsoft.com>
2019-02-15 03:21:57 -0800

    Revert changes to S.Diag.DiagSource and modify DiagSourceActivity instead

--- src/System.Diagnostics.DiagnosticSource/ref/System.Diagnostics.DiagnosticSourceActivity.cs
+++ src/System.Diagnostics.DiagnosticSource/ref/System.Diagnostics.DiagnosticSourceActivity.cs
@@ -7,41 +7,41 @@
 
 namespace System.Diagnostics 
 {
-   public partial class Activity
-   {
-        public Activity(string operationName) { }
-        public System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string, string>> Baggage { get { throw null; } }
-        public static Activity Current
-        {
+  public partial class Activity
+  {
+      public Activity(string operationName) { }
+      public System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string, string>> Baggage { get { throw null; } }
+      public static System.Diagnostics.Activity Current
+      {
 #if ALLOW_PARTIALLY_TRUSTED_CALLERS
         [System.Security.SecuritySafeCriticalAttribute]
 #endif
-            get { throw null; }
+        get { throw null; }
 #if ALLOW_PARTIALLY_TRUSTED_CALLERS
        [System.Security.SecuritySafeCriticalAttribute]
 #endif
-            set { }
-        }
-        public System.TimeSpan Duration { get { throw null; } }
-        public string Id { get { throw null; } }
-        public string OperationName { get { throw null; } }
-        public System.Diagnostics.Activity Parent { get { throw null; } }
-        public string ParentId { get { throw null; } }
-        public string RootId { get { throw null; } }
-        public System.DateTime StartTimeUtc { get { throw null; } }
-        public System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string, string>> Tags { get { throw null; } }
-        public System.Diagnostics.Activity AddBaggage(string key, string value) { throw null; }
-        public System.Diagnostics.Activity AddTag(string key, string value) { throw null; }
-        public string GetBaggageItem(string key) { throw null; }
-        public System.Diagnostics.Activity SetEndTime(System.DateTime endTimeUtc) { throw null; }
-        public System.Diagnostics.Activity SetParentId(string parentId) { throw null; }
-        public System.Diagnostics.Activity SetStartTime(System.DateTime startTimeUtc) { throw null; }
-        public System.Diagnostics.Activity Start() { throw null; }
-        public void Stop() { }
+        set { }
+      }
+      public System.TimeSpan Duration { get { throw null; } }
+      public string Id { get { throw null; } }
+      public string OperationName { get { throw null; } }
+      public System.Diagnostics.Activity Parent { get { throw null; } }
+      public string ParentId { get { throw null; } }
+      public string RootId { get { throw null; } }
+      public System.DateTime StartTimeUtc { get { throw null; } }
+      public System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string, string>> Tags { get { throw null; } }
+      public System.Diagnostics.Activity AddBaggage(string key, string value) { throw null; }
+      public System.Diagnostics.Activity AddTag(string key, string value) { throw null; }
+      public string GetBaggageItem(string key) { throw null; }
+      public System.Diagnostics.Activity SetEndTime(System.DateTime endTimeUtc) { throw null; }
+      public System.Diagnostics.Activity SetParentId(string parentId) { throw null; }
+      public System.Diagnostics.Activity SetStartTime(System.DateTime startTimeUtc) { throw null; }
+      public System.Diagnostics.Activity Start() { throw null; }
+      public void Stop() { }
   }
   public abstract partial class DiagnosticSource 
   {
-    public Activity StartActivity(Activity activity, object args) {throw null;}
-    public void StopActivity(Activity activity, object args) {}
+    public System.Diagnostics.Activity StartActivity(System.Diagnostics.Activity activity, object args) { throw null; }
+    public void StopActivity(System.Diagnostics.Activity activity, object args) { }
   }
 }
