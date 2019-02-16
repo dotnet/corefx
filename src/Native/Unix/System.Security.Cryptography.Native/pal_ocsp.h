8de68b67f11307ad55baa3694723e1093b168ff7
Jeremy Barton <jbarton@microsoft.com>
2019-02-15 16:05:32 -0800

    Add comments to new exports

--- src/Native/Unix/System.Security.Cryptography.Native/pal_ocsp.h
+++ src/Native/Unix/System.Security.Cryptography.Native/pal_ocsp.h
@@ -6,12 +6,27 @@
 #include "pal_compiler.h"
 #include "opensslshim.h"
 
+/*
+Direct shim to OCSP_REQUEST_free
+*/
 DLLEXPORT void CryptoNative_OcspRequestDestroy(OCSP_REQUEST* request);
 
+/*
+Returns the number of bytes required to encode an OCSP_REQUEST
+*/
 DLLEXPORT int32_t CryptoNative_GetOcspRequestDerSize(OCSP_REQUEST* req);
 
+/*
+Encodes the OCSP_REQUEST req into the destination buffer, returning the number of bytes written.
+*/
 DLLEXPORT int32_t CryptoNative_EncodeOcspRequest(OCSP_REQUEST* req, uint8_t* buf);
 
+/*
+Direct shim to d2i_OCSP_RESPONSE
+*/
 DLLEXPORT OCSP_RESPONSE* CryptoNative_DecodeOcspResponse(const uint8_t* buf, int32_t len);
 
+/*
+Direct shim to OCSP_RESPONSE_free
+*/
 DLLEXPORT void CryptoNative_OcspResponseDestroy(OCSP_RESPONSE* response);
--- src/Native/Unix/System.Security.Cryptography.Native/pal_x509.h
+++ src/Native/Unix/System.Security.Cryptography.Native/pal_x509.h
@@ -245,6 +245,9 @@ Shims the X509_STORE_CTX_get1_chain method.
 */
 DLLEXPORT X509Stack* CryptoNative_X509StoreCtxGetChain(X509_STORE_CTX* ctx);
 
+/*
+Shims the X509_STORE_CTX_get_current_cert function.
+*/
 DLLEXPORT X509* CryptoNative_X509StoreCtxGetCurrentCert(X509_STORE_CTX* ctx);
 
 /*
@@ -262,6 +265,10 @@ Shims the X509_STORE_CTX_get_error method.
 */
 DLLEXPORT X509VerifyStatusCode CryptoNative_X509StoreCtxGetError(X509_STORE_CTX* ctx);
 
+/*
+Resets ctx to before the chain was built, preserving the target cert, trust store, extra cert context,
+and verify parameters.
+*/
 DLLEXPORT int32_t CryptoNative_X509StoreCtxReset(X509_STORE_CTX* ctx);
 
 /*
@@ -327,14 +334,36 @@ in userTrustPath to be trusted
 */
 DLLEXPORT X509_STORE* CryptoNative_X509ChainNew(X509Stack* systemTrust, const char* userTrustPath);
 
+/*
+Adds all of the simple certificates from null-or-empty-password PFX files in storePath to stack.
+*/
 DLLEXPORT int32_t CryptoNative_X509StackAddDirectoryStore(X509Stack* stack, char* storePath);
 
+/*
+Adds all of the certificates in src to dest and increases their reference count.
+*/
 DLLEXPORT int32_t CryptoNative_X509StackAddMultiple(X509Stack* dest, X509Stack* src);
 
+/*
+Removes any untrusted/extra certificates from the unstrusted collection that are not part of
+the current chain to make chain builds after Reset faster.
+*/
 DLLEXPORT int32_t CryptoNative_X509StoreCtxCommitToChain(X509_STORE_CTX* storeCtx);
 
+/*
+Look for a cached OCSP response appropriate to the end-entity certificate using the issuer as
+determined by the chain in storeCtx.
+*/
 DLLEXPORT X509VerifyStatusCode CryptoNative_X509ChainGetCachedOcspStatus(X509_STORE_CTX* storeCtx, char* cachePath);
 
+/*
+Build an OCSP request appropriate for the end-entity certificate using the issuer (and trust) as
+determined by the chain in storeCtx.
+*/
 DLLEXPORT OCSP_REQUEST* CryptoNative_X509ChainBuildOcspRequest(X509_STORE_CTX* storeCtx);
 
+/*
+Determine if the OCSP response is acceptable, and if acceptable report the status and
+cache the result (if appropriate)
+*/
 DLLEXPORT X509VerifyStatusCode CryptoNative_X509ChainVerifyOcsp(X509_STORE_CTX* storeCtx, OCSP_REQUEST* req, OCSP_RESPONSE* resp, char* cachePath);
