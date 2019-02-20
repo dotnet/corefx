7c4d96971f3f6a508a38080795547be11a724585
Viktor <viktor.hofer@microsoft.com>
2019-02-20 22:30:22 +0100

    Fix unix arguments

--- eng/build.sh
+++ eng/build.sh
@@ -17,13 +17,13 @@ usage()
   echo "Default if no actions are passed in: --restore --build"
   echo ""
   echo "CoreFX specific settings:"
-  echo "  -buildtests             Build test projects. Can be used as a target or as an option"
-  echo "  -framework              The target group assemblies are built for (short: -f)"
-  echo "  -os                     The operating system assemblies are built for"
-  echo "  -allconfigurations      Build packages for all build configurations"
-  echo "  -coverage               Collect code coverage when testing"
-  echo "  -outerloop              Include tests which are marked as OuterLoop"
-  echo "  -arch                   The architecture group"
+  echo "  --buildtests             Build test projects. Can be used as a target or as an option"
+  echo "  --framework              The target group assemblies are built for (short: -f)"
+  echo "  --os                     The operating system assemblies are built for"
+  echo "  --allconfigurations      Build packages for all build configurations"
+  echo "  --coverage               Collect code coverage when testing"
+  echo "  --outerloop              Include tests which are marked as OuterLoop"
+  echo "  --arch                   The architecture group (short: -a)"
   echo ""
 }
 
@@ -45,7 +45,7 @@ while (($# > 0)); do
       usage
       exit 0
       ;;
-     -arch)
+     --arch|-a)
       arguments="$arguments /p:ArchGroup=$2"
       shift 2
       ;;
@@ -53,32 +53,32 @@ while (($# > 0)); do
       arguments="$arguments /p:ConfigurationGroup=$2 --configuration $2"
       shift 2
       ;;
-     -framework|-f)
+     --framework|-f)
       val="$(echo "$2" | awk '{print tolower($0)}')"
       arguments="$arguments /p:TargetGroup=$val"
       shift 2
       ;;
-     -os)
+     --os)
       arguments="$arguments /p:OSGroup=$2"
       shift 2
       ;;
-     -allconfigurations)
+     --allconfigurations)
       arguments="$arguments /p:BuildAllConfigurations=true"
       shift 1
       ;;
-     -buildtests)
+     --buildtests)
       arguments="$arguments /p:BuildTests=true"
       shift 1
       ;;
-     -outerloop)
+     --outerloop)
       arguments="$arguments /p:OuterLoop=true"
       shift 1
       ;;
-     -coverage)
+     --coverage)
       arguments="$arguments /p:Coverage=true"
       shift 1
       ;;
-     -stripsymbols)
+     --stripsymbols)
       arguments="$arguments /p:BuildNativeStripSymbols=true"
       shift 1
       ;;
