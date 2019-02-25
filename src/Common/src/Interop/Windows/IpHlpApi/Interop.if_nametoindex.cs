30554396188be78978b2f7df90c34ee9c71a909b
EgorBo <egorbo@gmail.com>
2019-02-25 12:07:36 +0300

    remove "Vista" comment from Interop.if_nametoindex.cs

--- src/Common/src/Interop/Windows/IpHlpApi/Interop.if_nametoindex.cs
+++ src/Common/src/Interop/Windows/IpHlpApi/Interop.if_nametoindex.cs
@@ -9,7 +9,6 @@ internal static partial class Interop
 {
     internal static partial class IpHlpApi
     {
-        // Available since Windows Vista and Windows Server 2008
         [DllImport(Interop.Libraries.IpHlpApi, SetLastError = true)]
         internal extern static uint if_nametoindex(string name);
     }
