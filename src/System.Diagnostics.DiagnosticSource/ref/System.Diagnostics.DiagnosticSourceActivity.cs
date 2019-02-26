fa07e4f0a8e564f33ef8e2de86dc9087a53bdf26
Vance Morrison <vancem@microsoft.com>
2019-02-26 07:51:19 -0800

    Changes to support W3C style IDs and propagation (#33207)
    * First set of changes to support W3C style IDs and propagation
    
    see https://w3c.github.io/trace-context
    
    This is mostly for discussion purposes.
    
    * Changed UseW3CFormat to be DefaultIdFormat
    
    * Review feedback
    
    * More comments.  Small Renames
    
    * Added Sampling Support, Review feedback.
    
    * Added placeholder for SetRecordingDesired
    
    * Separated out the Sampling support into its own PR.
    
    * Remove more sampling support
    
    * Fix IsWC3Id -> IdFormat
    
    * Added Sampling Support, Review feedback.
    
    * Separated out the Sampling support into its own PR.
    
    * Add ForceW3C option.
    
    * Introduce ForceDefaultIdFormat
    
    * Adding SpanId and TraceId support
    
    * Change ulong->long in SpanID (probably temporary)
    
    * Remove undesired file changes
    
    * Fix up reference assembly.
    
    Note complete because of questions about Span<byte>, but closer now.
    
    * Turn on code that used Span<byte>
    
    * Defer setting IDFormat until start.
    
    This insures that the IDFormat property is either unknown or a given value (that never changes from there).
    
    * More implementation, made the interface more uniform.
    
    * Support to avoid using strings whenever possible
    
    Basically Id, SpanId TraceId properties are set lazily and only converted lazily.
    
    * Added some tests
    
    * First round of testing (and bugfixes)
    
    * More testing
    
    * Added Equality operators
    
    * Rename SpanId -> ActivitySpanId TraceId ->ActivityTraceId
    
    * Add Comments
    
    * Fix bad XML comment
    
    * Review feedback
    
    * Change AsBytes -> CopyTo
    
    Lifetime issues prevent returning Span<T> (which is what AsBytes does.   Reverting to CopyTo instead.
    Also added System.Memory ref in attempt to resolve build errors (that don't reproduce locally).
    
    * Attempt to fix build break in netfx build
    
    * Deal with Verification errors.
    
    * Provide full namespace for SecuritySafeCriticalAttribute
    
    * More securitySafeCritical annotations to fix test failures on desktop
    
    * Another attempt on setting SecuritySafeCritical
    
    * Remove the readonly ref (to avoid perf issue)
    
    * Fix most  -buildAllConfigurations issues
    
    * Tentative change to see if testing works if we ignore the older netstandard configs.
    
    * Workaround package cycle involving DiagnosticSource and Memory
    
    * Satisfy dangling System.Memory dangling reference in package tests
    
    * Add notes to remove the workarounds when Unsafe is fixed.
    
    * Review feedback
    
    Uses Utf8 helpers where possible.

--- src/System.Diagnostics.DiagnosticSource/ref/System.Diagnostics.DiagnosticSourceActivity.cs
+++ src/System.Diagnostics.DiagnosticSource/ref/System.Diagnostics.DiagnosticSourceActivity.cs
@@ -5,10 +5,10 @@
 // Changes to this file must follow the http://aka.ms/api-review process.
 // ------------------------------------------------------------------------------
 
-namespace System.Diagnostics 
+namespace System.Diagnostics
 {
-   public partial class Activity
-   {
+    public partial class Activity
+    {
         public Activity(string operationName) { }
         public System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string, string>> Baggage { get { throw null; } }
         public static Activity Current
@@ -22,26 +22,80 @@ namespace System.Diagnostics
 #endif
             set { }
         }
+        public static System.Diagnostics.ActivityIdFormat DefaultIdFormat { get { throw null; } set { } }
         public System.TimeSpan Duration { get { throw null; } }
+        public static bool ForceDefaultIdFormat { get { throw null; } set { } }
         public string Id { get { throw null; } }
+        public System.Diagnostics.ActivityIdFormat IdFormat { get { throw null; } }
         public string OperationName { get { throw null; } }
         public System.Diagnostics.Activity Parent { get { throw null; } }
         public string ParentId { get { throw null; } }
+        public ref System.Diagnostics.ActivitySpanId ParentSpanId { get { throw null; } }
         public string RootId { get { throw null; } }
+        public ref System.Diagnostics.ActivitySpanId SpanId { get { throw null; } }
         public System.DateTime StartTimeUtc { get { throw null; } }
         public System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<string, string>> Tags { get { throw null; } }
+        public ref System.Diagnostics.ActivityTraceId TraceId { get { throw null; } }
+        public string TraceStateString { get { throw null; } set { } }
         public System.Diagnostics.Activity AddBaggage(string key, string value) { throw null; }
         public System.Diagnostics.Activity AddTag(string key, string value) { throw null; }
         public string GetBaggageItem(string key) { throw null; }
         public System.Diagnostics.Activity SetEndTime(System.DateTime endTimeUtc) { throw null; }
+        public System.Diagnostics.Activity SetParentId(in System.Diagnostics.ActivityTraceId traceId, in System.Diagnostics.ActivitySpanId spanId) { throw null; }
         public System.Diagnostics.Activity SetParentId(string parentId) { throw null; }
         public System.Diagnostics.Activity SetStartTime(System.DateTime startTimeUtc) { throw null; }
         public System.Diagnostics.Activity Start() { throw null; }
         public void Stop() { }
-  }
-  public abstract partial class DiagnosticSource 
-  {
-    public Activity StartActivity(Activity activity, object args) {throw null;}
-    public void StopActivity(Activity activity, object args) {}
-  }
+    }
+    public enum ActivityIdFormat : byte
+    {
+        Hierarchical = (byte)1,
+        Unknown = (byte)0,
+        W3C = (byte)2,
+    }
+
+#if ALLOW_PARTIALLY_TRUSTED_CALLERS
+        [System.Security.SecuritySafeCriticalAttribute]
+#endif
+    public partial struct ActivitySpanId : IEquatable<ActivitySpanId>
+    {
+        private object _dummy;
+        private int _dummyPrimitive;
+        public ActivitySpanId(System.ReadOnlySpan<byte> idData, bool isUtf8Chars = false) { throw null; }
+        public ActivitySpanId(System.ReadOnlySpan<char> idData) { throw null; }
+        public string AsHexString { get { throw null; } }
+        public void CopyTo(System.Span<byte> destination) { }
+        public bool Equals(System.Diagnostics.ActivitySpanId spanId) { throw null; }
+        public override bool Equals(object obj) { throw null; }
+        public override int GetHashCode() { throw null; }
+        public static System.Diagnostics.ActivitySpanId NewSpanId() { throw null; }
+        public static bool operator ==(in System.Diagnostics.ActivitySpanId spanId1, in System.Diagnostics.ActivitySpanId spandId2) { throw null; }
+        public static bool operator !=(in System.Diagnostics.ActivitySpanId spanId1, in System.Diagnostics.ActivitySpanId spandId2) { throw null; }
+        public override string ToString() { throw null; }
+    }
+
+#if ALLOW_PARTIALLY_TRUSTED_CALLERS
+        [System.Security.SecuritySafeCriticalAttribute]
+#endif
+    public partial struct ActivityTraceId : IEquatable<ActivityTraceId>
+    {
+        private object _dummy;
+        private int _dummyPrimitive;
+        public ActivityTraceId(System.ReadOnlySpan<byte> idData, bool isUtf8Chars = false) { throw null; }
+        public ActivityTraceId(System.ReadOnlySpan<char> idData) { throw null; }
+        public string AsHexString { get { throw null; } }
+        public void CopyTo(System.Span<byte> destination) { }
+        public bool Equals(System.Diagnostics.ActivityTraceId traceId) { throw null; }
+        public override bool Equals(object obj) { throw null; }
+        public override int GetHashCode() { throw null; }
+        public static System.Diagnostics.ActivityTraceId NewTraceId() { throw null; }
+        public static bool operator ==(in System.Diagnostics.ActivityTraceId traceId1, in System.Diagnostics.ActivityTraceId traceId2) { throw null; }
+        public static bool operator !=(in System.Diagnostics.ActivityTraceId traceId1, in System.Diagnostics.ActivityTraceId traceId2) { throw null; }
+        public override string ToString() { throw null; }
+    }
+    public abstract partial class DiagnosticSource
+    {
+        public Activity StartActivity(Activity activity, object args) { throw null; }
+        public void StopActivity(Activity activity, object args) { }
+    }
 }
