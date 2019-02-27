e2badc789a3d96c46bc7fdebdef3150ddeefb9f8
Stephen Toub <stoub@microsoft.com>
2019-02-27 14:51:50 -0500

    Fix Thread-related breaking changes
    - Thread.ExecutionContext needs to return ExecutionContext.Capture() to public callers.
    - Fix restoring of culture to be inside of the ExecutionContext callback where applicable
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/Runtime/CompilerServices/AsyncMethodBuilder.cs
+++ src/Common/src/CoreLib/System/Runtime/CompilerServices/AsyncMethodBuilder.cs
@@ -1011,7 +1011,7 @@ namespace System.Runtime.CompilerServices
             // Capture references to Thread Contexts
             Thread currentThread0 = Thread.CurrentThread;
             Thread currentThread = currentThread0;
-            ExecutionContext previousExecutionCtx0 = currentThread0.ExecutionContext;
+            ExecutionContext previousExecutionCtx0 = currentThread0.m_ExecutionContext;
 
             // Store current ExecutionContext and SynchronizationContext as "previousXxx".
             // This allows us to restore them and undo any Context changes made in stateMachine.MoveNext
@@ -1036,7 +1036,7 @@ namespace System.Runtime.CompilerServices
                 }
 
                 ExecutionContext previousExecutionCtx1 = previousExecutionCtx;
-                ExecutionContext currentExecutionCtx1 = currentThread1.ExecutionContext;
+                ExecutionContext currentExecutionCtx1 = currentThread1.m_ExecutionContext;
                 if (previousExecutionCtx1 != currentExecutionCtx1)
                 {
                     ExecutionContext.RestoreChangedContextToThread(currentThread1, previousExecutionCtx1, currentExecutionCtx1);
