7f3112a0cd0fd04025d2dd402d05839bd3e274cf
Tim Seaward <seawardtim@gmail.com>
2019-03-11 14:28:20 +0000

    Clean Fakes

--- src/System.Net.Security/tests/UnitTests/Fakes/FakeSslStream.Implementation.cs
+++ src/System.Net.Security/tests/UnitTests/Fakes/FakeSslStream.Implementation.cs
@@ -2,19 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.IO;
-using System.Security.Authentication;
 using System.Security.Authentication.ExtendedProtection;
 using System.Security.Cryptography.X509Certificates;
-using System.Threading;
 using System.Threading.Tasks;
-using static System.Net.Security.SslStream;
 
 namespace System.Net.Security
 {
     public partial class SslStream
     {
-        internal void ValidateCreateContext(SslClientAuthenticationOptions sslClientAuthenticationOptions, RemoteCertValidationCallback remoteCallback, LocalCertSelectionCallback localCallback)
+        private void ValidateCreateContext(SslClientAuthenticationOptions sslClientAuthenticationOptions, RemoteCertValidationCallback remoteCallback, LocalCertSelectionCallback localCallback)
         {
             if(_shutdown == true && _nestedWrite == 0)
             {
@@ -28,57 +24,57 @@ namespace System.Net.Security
             _nestedWrite = 0;
         }
 
-        internal void ValidateParameters(byte[] buffer, int offset, int count)
+        private void ValidateParameters(byte[] buffer, int offset, int count)
         {
         }
 
-        internal void ValidateCreateContext(SslAuthenticationOptions sslAuthenticationOptions)
+        private void ValidateCreateContext(SslAuthenticationOptions sslAuthenticationOptions)
         {
         }
 
-        internal ValueTask WriteAsyncInternal<TWriteAdapter>(TWriteAdapter writeAdapter, ReadOnlyMemory<byte> buffer)
+        private ValueTask WriteAsyncInternal<TWriteAdapter>(TWriteAdapter writeAdapter, ReadOnlyMemory<byte> buffer)
             where TWriteAdapter : struct, ISslWriteAdapter => default;
 
-        internal ValueTask<int> ReadAsyncInternal<TReadAdapter>(TReadAdapter adapter, Memory<byte> buffer) => default;
+        private ValueTask<int> ReadAsyncInternal<TReadAdapter>(TReadAdapter adapter, Memory<byte> buffer) => default;
 
-        internal Task CheckEnqueueWriteAsync() => default;
+        private Task CheckEnqueueWriteAsync() => default;
 
-        internal void CheckEnqueueWrite()
+        private void CheckEnqueueWrite()
         {
         }
 
-        internal ValueTask<int> CheckEnqueueReadAsync(Memory<byte> buffer) => default;
+        private ValueTask<int> CheckEnqueueReadAsync(Memory<byte> buffer) => default;
 
-        internal int CheckEnqueueRead(Memory<byte> buffer) => default;
+        private int CheckEnqueueRead(Memory<byte> buffer) => default;
 
-        internal bool RemoteCertRequired => default;
+        private bool RemoteCertRequired => default;
 
         private X509Certificate InternalLocalCertificate => default;
 
-        internal bool HandshakeCompleted => default;
+        private bool HandshakeCompleted => default;
 
-        public bool IsShutdown { get; internal set; }
+        private bool IsShutdown { get; set; }
 
-        internal void CheckThrow(bool authSuccessCheck, bool shutdownCheck = false)
+        private void CheckThrow(bool authSuccessCheck, bool shutdownCheck = false)
         {
         }
 
-        internal void CloseInternal()
+        private void CloseInternal()
         {
         }
         //
         // This method assumes that a SSPI context is already in a good shape.
         // For example it is either a fresh context or already authenticated context that needs renegotiation.
         //
-        internal void ProcessAuthentication(LazyAsyncResult lazyResult)
+        private void ProcessAuthentication(LazyAsyncResult lazyResult)
         {
         }
 
-        internal void EndProcessAuthentication(IAsyncResult result)
+        private void EndProcessAuthentication(IAsyncResult result)
         {
         }
 
-        internal void ReturnReadBufferIfEmpty()
+        private void ReturnReadBufferIfEmpty()
         {
         }
     }
