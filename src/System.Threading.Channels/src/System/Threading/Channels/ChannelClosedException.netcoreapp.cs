9187219ad62356da3d5b820c6b63deba0549f79f
Stephen Toub <stoub@microsoft.com>
2019-03-11 13:32:14 -0400

    Add missing deserialization ctor to ChannelClosedException (#35952)

---
+++ src/System.Threading.Channels/src/System/Threading/Channels/ChannelClosedException.netcoreapp.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.Serialization;
+
+namespace System.Threading.Channels
+{
+    /// <summary>Exception thrown when a channel is used after it's been closed.</summary>
+    [Serializable]
+    public partial class ChannelClosedException : InvalidOperationException
+    {
+        /// <summary>Initializes a new instance of the <see cref="ChannelClosedException"/> class with serialized data.</summary>
+        /// <param name="info">The object that holds the serialized object data.</param>
+        /// <param name="context">The contextual information about the source or destination.</param>
+        protected ChannelClosedException(SerializationInfo info, StreamingContext context) :
+            base(info, context)
+        {
+        }
+    }
+}
