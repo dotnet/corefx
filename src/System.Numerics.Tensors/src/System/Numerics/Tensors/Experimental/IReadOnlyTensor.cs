03f609da62537a604b336808f21244905f2177b1
Tanner Gooding <tagoo@outlook.com>
2019-03-13 11:27:08 -0700

    Adding the ITensor interfaces SxS with the existing types

---
+++ src/System.Numerics.Tensors/src/System/Numerics/Tensors/Experimental/IReadOnlyTensor.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Numerics.Tensors
+{
+    public interface IReadOnlyTensor<T>
+    {
+        long Rank { get; }
+
+        T this[params long[] indices] { get; }
+        T this[ReadOnlySpan<long> indices] { get; }
+
+        long GetDimensionLength(long dimension);
+        IReadOnlyTensor<T> Slice(params (long start, long length)[] ranges);
+        IReadOnlyTensor<T> Slice(ReadOnlySpan<(long start, long length)> ranges);
+        IReadOnlyTensor<T> Reshape(params long[] dimensions);
+        IReadOnlyTensor<T> Reshape(ReadOnlySpan<long> dimensions);
+    }
+}
---
+++ src/System.Numerics.Tensors/src/System/Numerics/Tensors/Experimental/ITensor.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Numerics.Tensors
+{
+    public interface ITensor<T> : IReadOnlyTensor<T>
+    {
+        new T this[params long[] indices] { get; set; }
+        new T this[ReadOnlySpan<long> indices] { get; set; }
+    }
+}
