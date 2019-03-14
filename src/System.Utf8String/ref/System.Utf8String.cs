ff549d3f33006ae1d35be461486b6e35b3a1aa7a
Levi Broderick <levib@microsoft.com>
2019-03-13 17:34:46 -0700

    Add Utf8StringContent

--- src/System.Utf8String/ref/System.Utf8String.cs
+++ src/System.Utf8String/ref/System.Utf8String.cs
@@ -104,3 +104,14 @@ namespace System
         public override string ToString() => throw null;
     }
 }
+namespace System.Net.Http
+{
+    public sealed partial class Utf8StringContent : System.Net.Http.HttpContent
+    {
+        public Utf8StringContent(Utf8String content) { }
+        public Utf8StringContent(Utf8String content, string mediaType) { }
+        protected override System.Threading.Tasks.Task<System.IO.Stream> CreateContentReadStreamAsync() => throw null;
+        protected override System.Threading.Tasks.Task SerializeToStreamAsync(System.IO.Stream stream, System.Net.TransportContext context) => throw null;
+        protected override bool TryComputeLength(out long length) => throw null;
+    }
+}
