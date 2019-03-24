b9452817bfdbc03b5da3e8326fd166e8b6626112
Dan Moseley <danmose@microsoft.com>
2019-03-23 17:23:27 -0700

    space

--- src/System.Data.OleDb/src/AssemblyTemplate_BID.cs
+++ src/System.Data.OleDb/src/AssemblyTemplate_BID.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
+
 //// <summary>
 //// This is intended to be a template for starting a new component with Bid.
 //// </summary>
