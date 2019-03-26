77b51c7e5102ac03d24dfbc85bbf4a159a1b33e9
ryans610 <ryans610@gmail.com>
2019-03-26 21:17:42 +0800

    add new MediaTypeNames

--- src/System.Net.Mail/ref/System.Net.Mail.cs
+++ src/System.Net.Mail/ref/System.Net.Mail.cs
@@ -303,26 +303,91 @@ namespace System.Net.Mime
     {
         public static partial class Application
         {
+            public const string EcmaScript = "application/ecmascript";
+            public const string Epub = "application/epub+zip";
+            public const string Exe = "application/vnd.microsoft.portable-executable";
+            public const string Gzip = "application/gzip";
+            public const string Jar = "application/java-archive";
+            public const string JavaScript = "application/javascript";
             public const string Json = "application/json";
+            public const string JsonPatch = "application/json-patch+json";
+            public const string MicrosoftExcel = "application/vnd.ms-excel";
+            public const string MicrosoftExcelOpenXml = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            public const string MicrosoftPowerPoint = "application/vnd.ms-powerpoint";
+            public const string MicrosoftPowerPointOpenXml = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+            public const string MicrosoftWord = "application/msword";
+            public const string MicrosoftWordOpenXml = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
             public const string Octet = "application/octet-stream";
+            public const string Ogg = "application/ogg";
             public const string Pdf = "application/pdf";
+            public const string PostScript = "application/postscript";
+            public const string Rar = "application/x-rar-compressed";
+            public const string Rss = "application/rss+xml";
             public const string Rtf = "application/rtf";
+            public const string SevenZip = "application/x-7z-compressed";
             public const string Soap = "application/soap+xml";
+            public const string Swf = "application/x-shockwave-flash";
+            public const string Xhtml = "application/xhtml+xml";
             public const string Xml = "application/xml";
+            public const string XmlDtd = "application/xml-dtd";
+            public const string XWwwFormUrlEncoded = "application/x-www-form-urlencoded";
             public const string Zip = "application/zip";
+        }
+        public static partial class Audio
+        {
+            public const string Aac = "audio/aac";
+            public const string Midi = "audio/midi";
+            public const string Mp4 = "audio/mp4";
+            public const string Mpeg = "audio/mpeg";
+            public const string Ogg = "audio/ogg";
+            public const string Wav = "audio/x-wav";
+            public const string Webm = "audio/webm";
+        }
+        public static partial class Font
+        {
+            public const string Otf = "font/otf";
+            public const string Sfnt = "font/sfnt";
+            public const string Ttf = "font/ttf";
+            public const string Woff = "font/woff";
+            public const string Woff2 = "font/woff2";
         }
         public static partial class Image
         {
+            public const string Bmp = "image/bmp";
             public const string Gif = "image/gif";
+            public const string Icon = "image/x-icon";
             public const string Jpeg = "image/jpeg";
+            public const string Png = "image/png";
+            public const string Svg = "image/svg+xml";
             public const string Tiff = "image/tiff";
+            public const string Webp = "image/webp";
+        }
+        public static partial class Multipart
+        {
+            public const string ByteRanges = "multipart/byteranges";
+            public const string FormData = "multipart/form-data";
         }
         public static partial class Text
         {
+            public const string Css = "text/css";
+            public const string Csv = "text/csv";
+            public const string EcmaScript = "text/ecmascript";
             public const string Html = "text/html";
+            public const string JavaScript = "text/javascript";
             public const string Plain = "text/plain";
             public const string RichText = "text/richtext";
             public const string Xml = "text/xml";
+        }
+        public static partial class Video
+        {
+            public const string Avi = "video/x-msvideo";
+            public const string Flv = "video/x-flv";
+            public const string Mp4 = "video/mp4";
+            public const string Mpeg = "video/mpeg";
+            public const string Ogg = "video/ogg";
+            public const string QuickTime = "video/quicktime";
+            public const string Webm = "video/webm";
+            public const string Wmv = "video/x-ms-wmv";
         }
     }
     public enum TransferEncoding
--- src/System.Net.Mail/src/System/Net/Mime/MediaTypeNames.cs
+++ src/System.Net.Mail/src/System/Net/Mime/MediaTypeNames.cs
@@ -12,6 +12,10 @@ namespace System.Net.Mime
             public const string Html = "text/html";
             public const string Xml = "text/xml";
             public const string RichText = "text/richtext";
+            public const string Css = "text/css";
+            public const string Csv = "text/csv";
+            public const string EcmaScript = "text/ecmascript";
+            public const string JavaScript = "text/javascript";
         }
 
         public static class Application
@@ -23,6 +27,28 @@ namespace System.Net.Mime
             public const string Zip = "application/zip";
             public const string Json = "application/json";
             public const string Xml = "application/xml";
+            public const string EcmaScript = "application/ecmascript";
+            public const string Epub = "application/epub+zip";
+            public const string Exe = "application/vnd.microsoft.portable-executable";
+            public const string Gzip = "application/gzip";
+            public const string Jar = "application/java-archive";
+            public const string JavaScript = "application/javascript";
+            public const string JsonPatch = "application/json-patch+json";
+            public const string MicrosoftExcel = "application/vnd.ms-excel";
+            public const string MicrosoftExcelOpenXml = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            public const string MicrosoftPowerPoint = "application/vnd.ms-powerpoint";
+            public const string MicrosoftPowerPointOpenXml = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+            public const string MicrosoftWord = "application/msword";
+            public const string MicrosoftWordOpenXml = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+            public const string Ogg = "application/ogg";
+            public const string PostScript = "application/postscript";
+            public const string Rar = "application/x-rar-compressed";
+            public const string Rss = "application/rss+xml";
+            public const string SevenZip = "application/x-7z-compressed";
+            public const string Swf = "application/x-shockwave-flash";
+            public const string Xhtml = "application/xhtml+xml";
+            public const string XmlDtd = "application/xml-dtd";
+            public const string XWwwFormUrlEncoded = "application/x-www-form-urlencoded";
         }
 
         public static class Image
@@ -30,6 +56,49 @@ namespace System.Net.Mime
             public const string Gif = "image/gif";
             public const string Tiff = "image/tiff";
             public const string Jpeg = "image/jpeg";
+            public const string Bmp = "image/bmp";
+            public const string Icon = "image/x-icon";
+            public const string Png = "image/png";
+            public const string Svg = "image/svg+xml";
+            public const string Webp = "image/webp";
+        }
+
+        public static class Audio
+        {
+            public const string Aac = "audio/aac";
+            public const string Midi = "audio/midi";
+            public const string Mp4 = "audio/mp4";
+            public const string Mpeg = "audio/mpeg";
+            public const string Ogg = "audio/ogg";
+            public const string Wav = "audio/x-wav";
+            public const string Webm = "audio/webm";
+        }
+
+        public static class Font
+        {
+            public const string Otf = "font/otf";
+            public const string Sfnt = "font/sfnt";
+            public const string Ttf = "font/ttf";
+            public const string Woff = "font/woff";
+            public const string Woff2 = "font/woff2";
+        }
+
+        public static class Multipart
+        {
+            public const string ByteRanges = "multipart/byteranges";
+            public const string FormData = "multipart/form-data";
+        }
+
+        public static class Video
+        {
+            public const string Avi = "video/x-msvideo";
+            public const string Flv = "video/x-flv";
+            public const string Mp4 = "video/mp4";
+            public const string Mpeg = "video/mpeg";
+            public const string Ogg = "video/ogg";
+            public const string QuickTime = "video/quicktime";
+            public const string Webm = "video/webm";
+            public const string Wmv = "video/x-ms-wmv";
         }
     }
 }
