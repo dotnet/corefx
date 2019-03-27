cde6ea222258d96f578b04d24546526ca78f3361
Krzysztof Wicher <mordotymoja@gmail.com>
2019-03-27 11:25:12 -0700

    Nullable: all calendars (dotnet/coreclr#23469)
    * calendar
    
    * nullable: all calendars
    
    * fix likely corert error
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/Globalization/HijriCalendar.cs
+++ src/Common/src/CoreLib/System/Globalization/HijriCalendar.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
 namespace System.Globalization
 {
     /// <remarks>
@@ -353,7 +354,7 @@ namespace System.Globalization
             return HijriEra;
         }
 
-        public override int[] Eras => new int[] { HijriEra };
+        public override int[]? Eras => new int[] { HijriEra };
 
         public override int GetMonth(DateTime time)
         {
