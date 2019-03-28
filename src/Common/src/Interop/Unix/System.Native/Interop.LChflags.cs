73ba61211ce6b9ab3a2b41db3d5d7dedb7a4ffdd
Stephen Toub <stoub@microsoft.com>
2019-03-28 09:54:56 -0400

    Add missing newline

--- src/Common/src/Interop/Unix/System.Native/Interop.LChflags.cs
+++ src/Common/src/Interop/Unix/System.Native/Interop.LChflags.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 using System;
 using System.Runtime.InteropServices;
+
 internal static partial class Interop
 {
     internal static partial class Sys
