3db3261f39945567d818b10a562acf86d334e07a
Steve MacLean <Steve.MacLean@Microsoft.com>
2019-03-28 17:09:50 -0400

    Amend ALC tests
    Add simple assertions to test:
    
    Test new public constructor
    Test instantiating AssemblyLoadContext as a concrete type
    Test All
    Test Name
    Test ToString
    Test Assemblies

--- src/System.Runtime.Loader/tests/DefaultContext/DefaultLoadContextTest.cs
+++ src/System.Runtime.Loader/tests/DefaultContext/DefaultLoadContextTest.cs
@@ -96,12 +96,15 @@ namespace System.Runtime.Loader.Tests
             // Create a secondary load context and wireup its resolving event
             SecondaryLoadContext slc = new SecondaryLoadContext();
             slc.Resolving += ResolveAssembly;
-            
+
+            Assert.Contains(slc, AssemblyLoadContext.All);
+
             // Attempt to load the assembly in secondary load context
             var slcLoadedAssembly = slc.LoadFromAssemblyName(assemblyName);
-            
+
             // We should have successfully loaded the assembly in secondary load context.
             Assert.NotNull(slcLoadedAssembly);
+            Assert.Contains(slcLoadedAssembly, slc.Assemblies);
 
             // And make sure the simple name matches
             Assert.Equal(TestAssemblyName, slcLoadedAssembly.GetName().Name);
@@ -118,12 +121,13 @@ namespace System.Runtime.Loader.Tests
             AssemblyLoadContext.Default.Resolving += ResolveNullAssembly;
             AssemblyLoadContext.Default.Resolving += ResolveAssembly;
             AssemblyLoadContext.Default.Resolving += ResolveAssemblyAgain;
-            
+
             // This will invoke the resolution via VM requiring to bind using the TPA binder
             var assemblyExpectedFromLoad = Assembly.Load(assemblyName);
 
             // We should have successfully loaded the assembly in default context.
             Assert.NotNull(assemblyExpectedFromLoad);
+            Assert.Contains(assemblyExpectedFromLoad, AssemblyLoadContext.Default.Assemblies);
 
             // We should have only invoked non-Null returning handler once
             Assert.Equal(1, _numNonNullResolutions);
