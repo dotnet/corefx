2125c502fb25c3eeb7c12dce6d6ec6d60e7aa508
Santiago Fernandez Madero <safern@microsoft.com>
2019-04-01 14:10:13 -0700

    Nullable: Annotate remaining System.*Exception (dotnet/coreclr#23585)
    * Nullable: Annotate remaining System.*Exception
    
    * PR feedback
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/ArrayTypeMismatchException.cs
+++ src/Common/src/CoreLib/System/ArrayTypeMismatchException.cs
@@ -11,6 +11,7 @@
 **
 =============================================================================*/
 
+#nullable enable
 using System.Runtime.Serialization;
 
 namespace System
@@ -34,13 +35,13 @@ namespace System
         // message, its HRESULT set to COR_E_ARRAYTYPEMISMATCH, 
         // and its ExceptionInfo reference set to null. 
         // 
-        public ArrayTypeMismatchException(string message)
+        public ArrayTypeMismatchException(string? message)
             : base(message)
         {
             HResult = HResults.COR_E_ARRAYTYPEMISMATCH;
         }
 
-        public ArrayTypeMismatchException(string message, Exception innerException)
+        public ArrayTypeMismatchException(string? message, Exception? innerException)
             : base(message, innerException)
         {
             HResult = HResults.COR_E_ARRAYTYPEMISMATCH;
