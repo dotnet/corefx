e94501d4d685f5c5a5e22a520dbc95045b3d413e
Eric St. John <ericstj@microsoft.com>
2019-04-01 16:01:42 -0700

    Add license to LinqDataView.cs

--- src/System.Data.Common/src/System/Data/LinqDataView.cs
+++ src/System.Data.Common/src/System/Data/LinqDataView.cs
@@ -1,18 +1,13 @@
-//------------------------------------------------------------------------------
-// <copyright file="LinqDataView.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
-// <owner current="true" primary="true">sthaker</owner>
-// <owner current="true" primary="false">aconrad</owner>
-//------------------------------------------------------------------------------
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Diagnostics;
 using System.ComponentModel;
 using System.Data;
-using System.Text;
+using System.Diagnostics;
 
 namespace System.Data
 {
