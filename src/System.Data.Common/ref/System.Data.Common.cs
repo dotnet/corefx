b2c6141fdbf19d3cb541a7e3538243b717178779
Eric St. John <ericstj@microsoft.com>
2019-04-02 13:04:33 -0700

    Move remaining types from DataSetExtensions to Common

--- src/System.Data.Common/ref/System.Data.Common.cs
+++ src/System.Data.Common/ref/System.Data.Common.cs
@@ -383,6 +383,29 @@ namespace System.Data
         public void Remove(System.Data.DataRow row) { }
         public void RemoveAt(int index) { }
     }
+    public static partial class DataRowComparer
+    {
+        public static System.Data.DataRowComparer<System.Data.DataRow> Default { get { throw null; } }
+    }
+    public sealed partial class DataRowComparer<TRow> : System.Collections.Generic.IEqualityComparer<TRow> where TRow : System.Data.DataRow
+    {
+        internal DataRowComparer() { }
+        public static System.Data.DataRowComparer<TRow> Default { get { throw null; } }
+        public bool Equals(TRow leftRow, TRow rightRow) { throw null; }
+        public int GetHashCode(TRow row) { throw null; }
+    }
+    public static partial class DataRowExtensions
+    {
+        public static T Field<T>(this System.Data.DataRow row, System.Data.DataColumn column) { throw null; }
+        public static T Field<T>(this System.Data.DataRow row, System.Data.DataColumn column, System.Data.DataRowVersion version) { throw null; }
+        public static T Field<T>(this System.Data.DataRow row, int columnIndex) { throw null; }
+        public static T Field<T>(this System.Data.DataRow row, int columnIndex, System.Data.DataRowVersion version) { throw null; }
+        public static T Field<T>(this System.Data.DataRow row, string columnName) { throw null; }
+        public static T Field<T>(this System.Data.DataRow row, string columnName, System.Data.DataRowVersion version) { throw null; }
+        public static void SetField<T>(this System.Data.DataRow row, System.Data.DataColumn column, T value) { }
+        public static void SetField<T>(this System.Data.DataRow row, int columnIndex, T value) { }
+        public static void SetField<T>(this System.Data.DataRow row, string columnName, T value) { }
+    }
     [System.FlagsAttribute]
     public enum DataRowState
     {
