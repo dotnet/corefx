1673b977bf35eee72d6bac63be4c34e2507c7640
Santiago Fernandez Madero <safern@microsoft.com>
2019-04-03 11:50:58 -0700

    Nullable: All remaining exceptions (dotnet/coreclr#23676)
    * Nullable: All remaining exceptions
    
    * Fix build break and PR Feedback
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/Collections/Generic/KeyNotFoundException.cs
+++ src/Common/src/CoreLib/System/Collections/Generic/KeyNotFoundException.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
+#nullable enable
 using System.Runtime.Serialization;
 
 namespace System.Collections.Generic
@@ -17,13 +17,13 @@ namespace System.Collections.Generic
             HResult = HResults.COR_E_KEYNOTFOUND;
         }
 
-        public KeyNotFoundException(string message)
+        public KeyNotFoundException(string? message)
             : base(message)
         {
             HResult = HResults.COR_E_KEYNOTFOUND;
         }
 
-        public KeyNotFoundException(string message, Exception innerException)
+        public KeyNotFoundException(string? message, Exception? innerException)
             : base(message, innerException)
         {
             HResult = HResults.COR_E_KEYNOTFOUND;
