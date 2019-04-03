9daf1d4a7dca429e875e4ac5e6ea2071c3cb50a5
Paito Anderson <pj.paito@gmail.com>
2019-04-03 15:11:36 -0400

    Added supporting HttpClient functional cookie test

--- src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.Cookies.cs
+++ src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.Cookies.cs
@@ -627,6 +627,7 @@ namespace System.Net.Http.Functional.Tests
                 yield return new object[] { "ABC", "123", useCookies };
                 yield return new object[] { "Hello", "World", useCookies };
                 yield return new object[] { "foo", "bar", useCookies };
+                yield return new object[] { "Hello World", "value", useCookies };
 
                 yield return new object[] { ".AspNetCore.Session", "RAExEmXpoCbueP_QYM", useCookies };
 
