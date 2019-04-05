9b4b59f42e7449694b6996ec34cf51bcf62706f3
Paito Anderson <pj.paito@gmail.com>
2019-04-05 09:25:56 -0400

    Skip test on .net framework

--- src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.Cookies.cs
+++ src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.Cookies.cs
@@ -627,7 +627,10 @@ namespace System.Net.Http.Functional.Tests
                 yield return new object[] { "ABC", "123", useCookies };
                 yield return new object[] { "Hello", "World", useCookies };
                 yield return new object[] { "foo", "bar", useCookies };
-                yield return new object[] { "Hello World", "value", useCookies };
+
+                if (!PlatformDetection.IsFullFramework) {
+                    yield return new object[] { "Hello World", "value", useCookies };
+                }
 
                 yield return new object[] { ".AspNetCore.Session", "RAExEmXpoCbueP_QYM", useCookies };
 
