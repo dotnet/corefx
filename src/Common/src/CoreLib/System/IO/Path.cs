f8907ee806dbd991c46d302828ee253126a389b7
Stephen Toub <stoub@microsoft.com>
2019-04-09 12:10:28 -0400

    Nullable: System.IO (dotnet/coreclr#23732)
    * Nullable: System.IO
    
    Everything exception StreamReader, StreamWriter, BinaryReader, BinaryWriter, which are waiting for some changes in master to propagate over to the feature branch.
    
    * Address PR feedback
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/IO/Path.cs
+++ src/Common/src/CoreLib/System/IO/Path.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -45,7 +46,7 @@ namespace System.IO
         // returns null. If path does not contain a file extension,
         // the new file extension is appended to the path. If extension
         // is null, any existing extension is removed from path.
-        public static string ChangeExtension(string path, string extension)
+        public static string? ChangeExtension(string? path, string? extension) // TODO-NULLABLE: https://github.com/dotnet/roslyn/issues/26761
         {
             if (path == null)
                 return null;
@@ -98,7 +99,7 @@ namespace System.IO
         /// <remarks>
         /// Directory separators are normalized in the returned string.
         /// </remarks>
-        public static string GetDirectoryName(string path)
+        public static string? GetDirectoryName(string? path) // TODO-NULLABLE: https://github.com/dotnet/roslyn/issues/26761
         {
             if (path == null || PathInternal.IsEffectivelyEmpty(path.AsSpan()))
                 return null;
@@ -146,7 +147,7 @@ namespace System.IO
         /// The returned value is null if the given path is null or empty if the given path does not include an
         /// extension.
         /// </summary>
-        public static string GetExtension(string path)
+        public static string? GetExtension(string? path) // TODO-NULLABLE: https://github.com/dotnet/roslyn/issues/26761
         {
             if (path == null)
                 return null;
@@ -185,7 +186,7 @@ namespace System.IO
         /// the characters of path that follow the last separator in path. The resulting string is
         /// null if path is null.
         /// </summary>
-        public static string GetFileName(string path)
+        public static string? GetFileName(string? path)
         {
             if (path == null)
                 return null;
@@ -216,7 +217,7 @@ namespace System.IO
             return path;
         }
 
-        public static string GetFileNameWithoutExtension(string path)
+        public static string? GetFileNameWithoutExtension(string? path) // TODO-NULLABLE: https://github.com/dotnet/roslyn/issues/26761
         {
             if (path == null)
                 return null;
@@ -290,7 +291,7 @@ namespace System.IO
         /// Tests if a path's file name includes a file extension. A trailing period
         /// is not considered an extension.
         /// </summary>
-        public static bool HasExtension(string path)
+        public static bool HasExtension(string? path)
         {
             if (path != null)
             {
@@ -433,12 +434,12 @@ namespace System.IO
             return JoinInternal(path1, path2, path3);
         }
 
-        public static string Join(string path1, string path2)
+        public static string Join(string? path1, string? path2)
         {
             return Join(path1.AsSpan(), path2.AsSpan());
         }
 
-        public static string Join(string path1, string path2, string path3)
+        public static string Join(string? path1, string? path2, string? path3)
         {
             return Join(path1.AsSpan(), path2.AsSpan(), path3.AsSpan());
         }
--- src/Common/src/CoreLib/System/IO/PathHelper.Windows.cs
+++ src/Common/src/CoreLib/System/IO/PathHelper.Windows.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -126,7 +127,7 @@ namespace System.IO
             }
         }
 
-        internal static string TryExpandShortFileName(ref ValueStringBuilder outputBuilder, string originalPath)
+        internal static string TryExpandShortFileName(ref ValueStringBuilder outputBuilder, string? originalPath)
         {
             // We guarantee we'll expand short names for paths that only partially exist. As such, we need to find the part of the path that actually does exist. To
             // avoid allocating like crazy we'll create only one input array and modify the contents with embedded nulls.
