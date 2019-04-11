0c5424e03f7e126ab9c42d018919e3c0e043ca0b
Krzysztof Wicher <mordotymoja@gmail.com>
2019-04-11 12:20:40 -0700

    Nullable: Interop: Kernel32 and few others (dotnet/coreclr#23880)
    * Nullable: Interop: Kernel32 and few others
    
    * fix compile errors (new compiler)
    
    * address feedback
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/Interop/Windows/OleAut32/Interop.SysAllocStringByteLen.cs
+++ src/Common/src/CoreLib/Interop/Windows/OleAut32/Interop.SysAllocStringByteLen.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
 using System;
 using System.Runtime.InteropServices;
 
@@ -10,6 +11,6 @@ internal partial class Interop
     internal partial class OleAut32
     {
         [DllImport(Libraries.OleAut32)]
-        internal static extern IntPtr SysAllocStringByteLen(byte[] str, uint len);
+        internal static extern IntPtr SysAllocStringByteLen(byte[]? str, uint len);
     }
 }
--- src/Common/src/CoreLib/Interop/Windows/OleAut32/Interop.SysAllocStringLen.cs
+++ src/Common/src/CoreLib/Interop/Windows/OleAut32/Interop.SysAllocStringLen.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
 using System;
 using System.Runtime.InteropServices;
 using System.Security;
@@ -14,6 +15,6 @@ internal partial class Interop
         internal static extern SafeBSTRHandle SysAllocStringLen(IntPtr src, uint len);
 
         [DllImport(Libraries.OleAut32, CharSet = CharSet.Unicode)]
-        internal static extern IntPtr SysAllocStringLen(string src, int len);
+        internal static extern IntPtr SysAllocStringLen(string? src, int len);
     }
 }
