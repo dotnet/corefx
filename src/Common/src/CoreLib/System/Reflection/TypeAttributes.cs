652ed9fc553004af49b4ca3541f0da9170aba746
buyaa-n <buyankhishig.namnan@microsoft.com>
2019-04-12 17:00:35 -0700

    Nullable: Enums, Structures from Reflection folder (dotnet/coreclr#23933)
    * Nullable: Enums, Structures from Reflection
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/Reflection/TypeAttributes.cs
+++ src/Common/src/CoreLib/System/Reflection/TypeAttributes.cs
@@ -2,8 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Runtime.InteropServices;
-
+#nullable enable
 namespace System.Reflection
 {
     // This Enum matchs the CorTypeAttr defined in CorHdr.h
