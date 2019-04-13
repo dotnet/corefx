6ed3f58637514ffb919071c42c61a53589b62a18
buyaa-n <buyankhishig.namnan@microsoft.com>
2019-04-12 17:00:35 -0700

    Nullable: Enums, Structures from Reflection folder (dotnet/coreclr#23933)
    * Nullable: Enums, Structures from Reflection
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/Reflection/Emit/SignatureToken.cs
+++ src/Common/src/CoreLib/System/Reflection/Emit/SignatureToken.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
 namespace System.Reflection.Emit
 {
     public struct SignatureToken
@@ -17,7 +18,7 @@ namespace System.Reflection.Emit
 
         public override int GetHashCode() => Token;
 
-        public override bool Equals(object obj) => obj is SignatureToken st && Equals(st);
+        public override bool Equals(object? obj) => obj is SignatureToken st && Equals(st);
 
         public bool Equals(SignatureToken obj) => obj.Token == Token;
 
