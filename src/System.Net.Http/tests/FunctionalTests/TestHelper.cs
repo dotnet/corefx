27cd1591df676781291c8bf40f588f3bbfabf7c5
Tomas Weinfurt <tweinfurt@yahoo.com>
2019-04-16 18:07:01 -0700

    fix tests with http/2

--- src/System.Net.Http/tests/FunctionalTests/TestHelper.cs
+++ src/System.Net.Http/tests/FunctionalTests/TestHelper.cs
@@ -118,7 +118,7 @@ namespace System.Net.Http.Functional.Tests
                 .Where(a => a.IsIPv6LinkLocal)
                 .FirstOrDefault();
 
-        public static void EnsureHttp2Feature(HttpClientHandler handler)
+        public static void EnsureHttp2Feature(HttpClientHandler handler, bool useHttp2LoopbackServer = true)
         {
             // All .NET Core implementations of HttpClientHandler have HTTP/2 enabled by default except when using
             // SocketsHttpHandler. Right now, the HTTP/2 feature is disabled on SocketsHttpHandler unless certain
@@ -156,11 +156,14 @@ namespace System.Net.Http.Functional.Tests
                 BindingFlags.NonPublic | BindingFlags.Instance);
             field_maxHttpVersion.SetValue(_settings, new Version(2, 0));
 
-            // Allow HTTP/2.0 via unencrypted socket if ALPN is not supported on platform.
-            FieldInfo field_allowPlainHttp2 = type_HttpConnectionSettings.GetField(
-                "_allowUnencryptedHttp2",
-                BindingFlags.NonPublic | BindingFlags.Instance);
-            field_allowPlainHttp2.SetValue(_settings, !PlatformDetection.SupportsAlpn);
+            if (useHttp2LoopbackServer && !PlatformDetection.SupportsAlpn)
+            {
+                // Allow HTTP/2.0 via unencrypted socket if ALPN is not supported on platform.
+                FieldInfo field_allowPlainHttp2 = type_HttpConnectionSettings.GetField(
+                    "_allowUnencryptedHttp2",
+                    BindingFlags.NonPublic | BindingFlags.Instance);
+                field_allowPlainHttp2.SetValue(_settings, !PlatformDetection.SupportsAlpn);
+            }
         }
 
         public static bool NativeHandlerSupportsSslConfiguration()
