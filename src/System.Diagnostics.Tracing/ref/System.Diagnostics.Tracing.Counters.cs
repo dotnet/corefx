c29c7b4a5630b896bbbb73f18f0de2dea9e0d123
Sung Yoon Whang <suwhang@microsoft.com>
2019-04-18 16:39:08 -0700

    try to exclude new counters for uap

---
+++ src/System.Diagnostics.Tracing/ref/System.Diagnostics.Tracing.Counters.cs
@@ -0,0 +1,27 @@
+namespace System.Diagnostics.Tracing
+{
+    public abstract partial class DiagnosticCounter : System.IDisposable
+    {
+        public DiagnosticCounter(string name, System.Diagnostics.Tracing.EventSource eventSource) { }
+        public void AddMetadata(string key, string value) { }
+        public void Dispose() { }
+        public string DisplayName { get { throw null; } set { } }
+        public string Name { get { throw null; } }
+        public System.Diagnostics.Tracing.EventSource EventSource { get { throw null; } }
+    }
+    public partial class PollingCounter : System.Diagnostics.Tracing.DiagnosticCounter
+    {
+        public PollingCounter(string name, System.Diagnostics.Tracing.EventSource eventSource, Func<double> metricProvider) : base(name, eventSource) { }
+    }
+    public partial class IncrementingEventCounter : System.Diagnostics.Tracing.DiagnosticCounter
+    {
+        public IncrementingEventCounter(string name, System.Diagnostics.Tracing.EventSource eventSource) : base(name, eventSource) { }
+        public void Increment(double increment = 1) { }
+        public TimeSpan DisplayRateTimeScale { get { throw null; } set { } }
+    }
+    public partial class IncrementingPollingCounter : System.Diagnostics.Tracing.DiagnosticCounter
+    {
+        public IncrementingPollingCounter(string name, System.Diagnostics.Tracing.EventSource eventSource, Func<double> totalValueProvider) : base(name, eventSource) { }
+        public TimeSpan DisplayRateTimeScale { get { throw null; } set { } }
+    }
+}
