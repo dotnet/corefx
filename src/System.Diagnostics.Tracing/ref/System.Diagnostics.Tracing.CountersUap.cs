c150a145990a662258e685ea204a8e83a7333111
Sung Yoon Whang <suwhang@microsoft.com>
2019-04-18 18:04:39 -0700

    Add missing method

--- src/System.Diagnostics.Tracing/ref/System.Diagnostics.Tracing.CountersUap.cs
+++ src/System.Diagnostics.Tracing/ref/System.Diagnostics.Tracing.CountersUap.cs
@@ -3,6 +3,7 @@ namespace System.Diagnostics.Tracing
     public partial class EventCounter : System.IDisposable
     {
         public EventCounter(string name, System.Diagnostics.Tracing.EventSource eventSource) { }
+        public void Dispose() { }
         public void WriteMetric(float value) { }
     }
 }
