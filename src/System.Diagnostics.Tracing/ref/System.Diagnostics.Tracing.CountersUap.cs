d64827a410909d18d90c0fa2b25c52f65ce59e2c
Sung Yoon Whang <suwhang@microsoft.com>
2019-04-18 17:56:07 -0700

    I'm stupid

--- src/System.Diagnostics.Tracing/ref/System.Diagnostics.Tracing.CountersUap.cs
+++ src/System.Diagnostics.Tracing/ref/System.Diagnostics.Tracing.CountersUap.cs
@@ -1,6 +1,6 @@
 namespace System.Diagnostics.Tracing
 {
-    public partial class EventCounter : System.Diagnostics.Tracing.IDisposable
+    public partial class EventCounter : System.IDisposable
     {
         public EventCounter(string name, System.Diagnostics.Tracing.EventSource eventSource) { }
         public void WriteMetric(float value) { }
