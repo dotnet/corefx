7578e8757a1684f2fbef8bfeafde73e23a34205a
Kevin Jones <kevin@vcsjones.com>
2019-04-23 12:41:33 -0400

    Support RSAES_OAEP certificates for EnvelopedCms

--- src/Common/src/System/Security/Cryptography/Oids.cs
+++ src/Common/src/System/Security/Cryptography/Oids.cs
@@ -26,6 +26,7 @@ namespace System.Security.Cryptography
         internal const string RsaPkcs1Sha512 = "1.2.840.113549.1.1.13";
         internal const string Esdh = "1.2.840.113549.1.9.16.3.5";
         internal const string EcDiffieHellman = "1.3.132.1.12";
+        internal const string X942dh = "1.2.840.10046.2.1";
 
         // Cryptographic Attribute Types
         internal const string SigningTime = "1.2.840.113549.1.9.5";
