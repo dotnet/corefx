48cc70c649b7876ca43155451cafba5499107c07
Charles Stoner <chucks@microsoft.com>
2019-04-23 18:11:11 -0700

    Port remaining members of Microsoft.VisualBasic.Strings (#37123)

--- src/Microsoft.VisualBasic.Core/src/Microsoft/VisualBasic/Helpers/UnsafeNativeMethods.vb
+++ src/Microsoft.VisualBasic.Core/src/Microsoft/VisualBasic/Helpers/UnsafeNativeMethods.vb
@@ -4,11 +4,25 @@
 
 Imports System
 Imports System.Runtime.InteropServices
+Imports System.Runtime.Versioning
 
 Namespace Microsoft.VisualBasic.CompilerServices
 
     <ComVisible(False)>
     Friend NotInheritable Class UnsafeNativeMethods
+
+        <ResourceExposure(ResourceScope.Machine)>
+        <PreserveSig()>
+        Friend Declare Ansi Function LCMapStringA _
+                Lib "kernel32" Alias "LCMapStringA" (ByVal Locale As Integer, ByVal dwMapFlags As Integer,
+                    <MarshalAs(UnmanagedType.LPArray)> ByVal lpSrcStr As Byte(), ByVal cchSrc As Integer, <MarshalAs(UnmanagedType.LPArray)> ByVal lpDestStr As Byte(), ByVal cchDest As Integer) As Integer
+
+        <ResourceExposure(ResourceScope.Machine)>
+        <PreserveSig()>
+        Friend Declare Auto Function LCMapString _
+                Lib "kernel32" (ByVal Locale As Integer, ByVal dwMapFlags As Integer,
+                    ByVal lpSrcStr As String, ByVal cchSrc As Integer, ByVal lpDestStr As String, ByVal cchDest As Integer) As Integer
+
         ''' <summary>
         ''' Frees memory allocated from the local heap. i.e. frees memory allocated
         ''' by LocalAlloc or LocalReAlloc.n
