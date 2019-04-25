1638836a4e5311f23eaa0d24ace031216f94aa18
Stephen Toub <stoub@microsoft.com>
2019-04-25 10:06:02 -0400

    Nullable: Contract.cs and more interop (dotnet/coreclr#24193)
    * Nullable: Contract.cs and more interop
    
    * Address PR feedback
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/Runtime/InteropServices/HandleRef.cs
+++ src/Common/src/CoreLib/System/Runtime/InteropServices/HandleRef.cs
@@ -2,23 +2,24 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable enable
 namespace System.Runtime.InteropServices
 {
     public readonly struct HandleRef
     {
         // ! Do not add or rearrange fields as the EE depends on this layout.
         //------------------------------------------------------------------
-        private readonly object _wrapper;
+        private readonly object? _wrapper;
         private readonly IntPtr _handle;
         //------------------------------------------------------------------
 
-        public HandleRef(object wrapper, IntPtr handle)
+        public HandleRef(object? wrapper, IntPtr handle)
         {
             _wrapper = wrapper;
             _handle = handle;
         }
 
-        public object Wrapper
+        public object? Wrapper
         {
             get
             {
