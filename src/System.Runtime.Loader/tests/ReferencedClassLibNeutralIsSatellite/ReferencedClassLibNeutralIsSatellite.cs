f57901a0f0daca2f0c384000e908947ddfe3fe33
Steve MacLean <Steve.MacLean@Microsoft.com>
2019-04-25 19:50:08 -0400

    ALC SatelliteAssemblies tests

---
+++ src/System.Runtime.Loader/tests/ReferencedClassLibNeutralIsSatellite/ReferencedClassLibNeutralIsSatellite.cs
@@ -0,0 +1,34 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+using System;
+using System.Resources;
+using System.Globalization;
+
+[assembly:NeutralResourcesLanguage("es", UltimateResourceFallbackLocation.Satellite)]
+
+namespace ReferencedClassLibNeutralIsSatellite
+{
+    public class Program
+    {
+        static public string Describe(string lang)
+        {
+            try
+            {
+                ResourceManager rm = new ResourceManager(typeof(ReferencedStrings));
+    
+                CultureInfo ci = CultureInfo.CreateSpecificCulture(lang);
+        
+                return rm.GetString("Describe", ci);
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+    }
+    
+    class ReferencedStrings
+    {
+    }
+}
