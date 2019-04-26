3990e952a90b16165a5995483e42a2147a96d0c6
Kaya Dávid <david.kaya@ysoft.com>
2019-04-26 15:59:29 +0200

    Added more test cases for encoding detection

--- src/System.IO/tests/StreamReader/StreamReader.CtorTests.cs
+++ src/System.IO/tests/StreamReader/StreamReader.CtorTests.cs
@@ -52,29 +52,89 @@ namespace System.IO.Tests
             sr2.Dispose();
         }
 
-        public static IEnumerable<object[]> EncodingsToTestDetection()
+        public static IEnumerable<object[]> EncodingsWithEncodedDataWithPreamble()
         {
-            yield return new object[] { Encoding.BigEndianUnicode };
-            yield return new object[] { Encoding.Unicode };
-            yield return new object[] { Encoding.UTF32 };
-            yield return new object[] { Encoding.UTF8 };
+            var stringInputs = new[] { string.Empty, "ABCD" };
+            var encodingInputs = new[] { Encoding.BigEndianUnicode, Encoding.Unicode, Encoding.UTF8, Encoding.UTF32, new UTF32Encoding(bigEndian: true, byteOrderMark: true) };
 
-            var bigEndianUTF32 = new UTF32Encoding(bigEndian: true, byteOrderMark: true);
-            yield return new object[] { bigEndianUTF32 };
+            foreach (var encoding in encodingInputs)
+            {
+                foreach (var stringInput in stringInputs)
+                {
+                    yield return new object[] { encoding, GetEncodedDataWithPreamble(encoding.GetBytes(stringInput), encoding), stringInput };
+                }
+            }
+
+            byte[] GetEncodedDataWithPreamble(byte[] data, Encoding encoding)
+                => encoding.GetPreamble().Concat(data).ToArray();
         }
 
         [Theory]
-        [MemberData(nameof(EncodingsToTestDetection))]
-        public static void CreationFromMemoryStreamWithEncodingTrueDetectsCorrectEncoding(Encoding expectedEncoding)
+        [MemberData(nameof(EncodingsWithEncodedDataWithPreamble))]
+        public static void CreationFromMemoryStreamWithEncodingTrueDetectsCorrectEncodingWhenPreambleAvailable(Encoding expectedEncoding, byte[] encodedData, string expectedOutput)
         {
             var ms2 = new MemoryStream();
-            var encodedBytesWithPreamble = expectedEncoding.GetPreamble().Concat(expectedEncoding.GetBytes("ABCD")).ToArray();
-            ms2.Write(encodedBytesWithPreamble);
+            ms2.Write(encodedData);
             ms2.Position = 0;
             var sr2 = new StreamReader(ms2, true);
             var streamContent = sr2.ReadToEnd();
 
-            Assert.Equal("ABCD", streamContent);
+            Assert.Equal(expectedOutput, streamContent);
+            Assert.Equal(expectedEncoding, sr2.CurrentEncoding);
+            sr2.Dispose();
+        }
+
+        public static IEnumerable<object[]> EncodingsWithSlightlyOffPatterns()
+        {
+            yield return new object[] { Encoding.Unicode, new byte[] { 0xFF, 0xFE, 0x00, 0xBB } }; // Possible UTF-32LE, first 2 bytes are the same
+            yield return new object[] { Encoding.Unicode, new byte[] { 0xFF, 0xFE, 0xBB, 0x00 } }; // Possible UTF-32LE, first 2 bytes are the same
+            yield return new object[] { Encoding.Unicode, new byte[] { 0xFF, 0xFE, 0xFE, 0xFF } }; // Possible UTF-32LE and UTF-32BE, first (last) 2 bytes are the same
+            yield return new object[] { Encoding.BigEndianUnicode, new byte[] { 0xFE, 0xFF, 0x00, 0x00 } }; // Possible UTF-32LE, last 2 bytes are the same
+            yield return new object[] { Encoding.BigEndianUnicode, new byte[] { 0xFE, 0xFF, 0x00, 0x00, 0xFE, 0xFF } }; // Possible UTF-32BE, last 4 bytes are the preamble of UTF-32BE
+            yield return new object[] { Encoding.BigEndianUnicode, new byte[] { 0xFE, 0xFF, 0xFE, 0xFF } }; // Possible UTF-32BE, last 2 bytes are the same
+            yield return new object[] { Encoding.UTF32, new byte[] { 0xFF, 0xFE, 0x00, 0x00, 0xFE, 0xFF } }; // Possible UTF-32BE, last 4 bytes are the preamble of UTF-32BE
+            yield return new object[] { Encoding.UTF32, new byte[] { 0xFF, 0xFE, 0x00, 0x00, 0xEF, 0xBB, 0xBF } }; // Possible UTF-8, last 3 bytes are the preamble of UTF-8
+            yield return new object[] { new UTF32Encoding(bigEndian: true, byteOrderMark: true), new byte[] { 0x00, 0x00, 0xFE, 0xFF } }; // Possible UnicodeBE, last 2 bytes are UnicodeBE
+        }
+
+        [Theory]
+        [MemberData(nameof(EncodingsWithSlightlyOffPatterns))]
+        public static void CreationFromMemoryStreamWithEncodingTrueDetectsCorrectEncodingWithOffPatterns(Encoding expectedEncoding, byte[] encodedData)
+        {
+            var ms2 = new MemoryStream();
+            ms2.Write(encodedData);
+            ms2.Position = 0;
+            var sr2 = new StreamReader(ms2, true);
+            var streamContent = sr2.ReadToEnd();
+
+            Assert.Equal(expectedEncoding, sr2.CurrentEncoding);
+            sr2.Dispose();
+        }
+
+        public static IEnumerable<object[]> EncodingsWithSmallInputs()
+        {
+            var byteArrayInputs = new byte[][] { new byte[] { }, new byte[] { 0x00 }, new byte[] { 0xEF }, new byte[] { 0xFE }, new byte[] { 0xFF } };
+            var encodingInputs = new[] { Encoding.BigEndianUnicode, Encoding.Unicode, Encoding.UTF8, Encoding.UTF32, new UTF32Encoding(bigEndian: true, byteOrderMark: true) };
+
+            foreach (var encoding in encodingInputs)
+            {
+                foreach (var byteArrayInput in byteArrayInputs)
+                {
+                    yield return new object[] { encoding, byteArrayInput };
+                }
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(EncodingsWithSmallInputs))]
+        public static void CreationFromMemoryStreamWithEncodingTrueDoesNotFailOnSmallInputs(Encoding expectedEncoding, byte[] data)
+        {
+            var ms2 = new MemoryStream();
+            ms2.Write(data);
+            ms2.Position = 0;
+            var sr2 = new StreamReader(ms2, encoding: expectedEncoding, detectEncodingFromByteOrderMarks: true);
+            var streamContent = sr2.ReadToEnd();
+
             Assert.Equal(expectedEncoding, sr2.CurrentEncoding);
             sr2.Dispose();
         }
