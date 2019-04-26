faa3cffab27b011d6ddc97fb230a4e22fc9406c9
Anipik <anirudhagnihotry098@gmail.com>
2019-04-26 14:53:10 -0700

    add precision
    precision tests added and precision increased in current tests
    
    fixing max value test

--- src/System.Runtime/tests/System/TimeSpanTests.netcoreapp.cs
+++ src/System.Runtime/tests/System/TimeSpanTests.netcoreapp.cs
@@ -16,8 +16,8 @@ namespace System.Tests
             yield return new object[] {new TimeSpan(14, 2, 30, 0), 192.0, TimeSpan.FromDays(2708)};
             yield return new object[] {TimeSpan.FromDays(366), Math.PI, new TimeSpan(993446995288779)};
             yield return new object[] {TimeSpan.FromDays(366), -Math.E, new TimeSpan(-859585952922633)};
-            yield return new object[] {TimeSpan.FromDays(29.530587981), 13.0, TimeSpan.FromDays(383.897643819444)};
-            yield return new object[] {TimeSpan.FromDays(-29.530587981), -12.0, TimeSpan.FromDays(354.367055833333)};
+            yield return new object[] {TimeSpan.FromDays(29.530587981), 13.0, TimeSpan.FromDays(29.530587981 * 13.0) };
+            yield return new object[] {TimeSpan.FromDays(-29.530587981), -12.0, TimeSpan.FromDays(-29.530587981 * -12.0) };
             yield return new object[] {TimeSpan.FromDays(-29.530587981), 0.0, TimeSpan.Zero};
             yield return new object[] {TimeSpan.MaxValue, 0.5, TimeSpan.FromTicks((long)(long.MaxValue * 0.5))};
         }
