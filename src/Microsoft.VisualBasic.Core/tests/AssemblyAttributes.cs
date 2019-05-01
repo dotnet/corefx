7ec574ba7e4bf2b10ae69534972e4a3f3f027229
Charles Stoner <chucks@microsoft.com>
2019-05-01 14:17:50 -0700

    Run Microsoft.VisualBasic.Core.Tests sequentially

---
+++ src/Microsoft.VisualBasic.Core/tests/AssemblyAttributes.cs
@@ -0,0 +1,9 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+// Tests that use ProjectData or AssemblyData rely on shared state
+// and should not be run in parallel.
+[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true, MaxParallelThreads = 1)]
