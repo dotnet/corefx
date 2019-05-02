73dca013c68965ab040588bfdb5a5256b93c026a
Stephen Toub <stoub@microsoft.com>
2019-05-02 18:18:29 -0400

    Log exceptions from asynchronously failing Expect: 100-continue sends
    When Expect: 100-continue is used, the sending of the request body content is allowed to run concurrently with the receipt of the response headers.  If the processing of the response headers encounters an exception, we currently fail to ever join with that send task, which results in a TaskScheduler.UnobservedTaskException event.  This PR changes to observe the exception in such cases and log it.
    
    (In doing so, I also came across an async void method, and changed all of the remaining ones in the assembly to be async Task instead.  The current implementation of async void isn't any cheaper than async Task, and is actually more expensive in certain ways, plus it unnecessarily interacts with any SynchronizationContext that may exist.)

--- src/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/Http2Connection.cs
+++ src/System.Net.Http/src/System/Net/Http/SocketsHttpHandler/Http2Connection.cs
@@ -139,7 +139,7 @@ namespace System.Net.Http
 
             _expectingSettingsAck = true;
 
-            ProcessIncomingFrames();
+            _ = ProcessIncomingFrames();
         }
 
         private async Task EnsureIncomingBytesAsync(int minReadBytes)
@@ -199,7 +199,7 @@ namespace System.Net.Http
             return frameHeader;
         }
 
-        private async void ProcessIncomingFrames()
+        private async Task ProcessIncomingFrames()
         {
             try
             {
