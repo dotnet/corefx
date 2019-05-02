88288cf16d1bce29353ce8baff20e506e08595af
Tarek Mahmoud Sayed <tarekms@microsoft.com>
2019-05-02 14:46:36 -0700

    Fix Macedonian display name in ComponentModel CultureInfo converter

--- src/System.ComponentModel.TypeConverter/src/System/ComponentModel/CultureInfoConverter.cs
+++ src/System.ComponentModel.TypeConverter/src/System/ComponentModel/CultureInfoConverter.cs
@@ -58,7 +58,7 @@ namespace System.ComponentModel
         /// </summary>
         public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
         {
-            // Hack, Only when GetCultureName returns culture.Name, we use CultureInfoMapper 
+            // Hack, Only when GetCultureName returns culture.Name, we use CultureInfoMapper
             // (Since CultureInfoMapper will transfer Culture.DisplayName to Culture.Name).
             // Otherwise, we just keep the value unchange.
             if (value is string text)
@@ -434,7 +434,7 @@ namespace System.ComponentModel
                     {"Lower Sorbian (Germany)", "dsb-DE"},
                     {"Luxembourgish (Luxembourg)", "lb-LU"},
                     {"Macedonian", "mk"},
-                    {"Macedonian (Former Yugoslav Republic of Macedonia)", "mk-MK"},
+                    {"Macedonian (North Macedonia)", "mk-MK"},
                     {"Malay", "ms"},
                     {"Malay (Brunei Darussalam)", "ms-BN"},
                     {"Malay (Malaysia)", "ms-MY"},
