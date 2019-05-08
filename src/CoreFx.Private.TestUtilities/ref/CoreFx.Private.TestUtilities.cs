e1a9ee2c0c3ca040f2058c4e99a4cbd5fbde013a
Eric StJohn <ericstj@microsoft.com>
2019-05-07 19:46:58 -0700

    React to APICompat changes enabling reverse APICompat for facades (#37375)
    * React to APICompat changes enabling reverse APICompat for facades
    
    * Fix UAP and NETFX reverse APICompat issues
    
    * Fix reverse APICompat baseline for allconfigurations build
    
    * Manually update to latest APICompat
    
    * Manually update to latest CoreCLR and fix issues
    
    * Baseline System.Threading.Tasks
    
    * Revert "Manually update to latest CoreCLR and fix issues"
    
    This partially reverts commit ee800617b6a52cc66ac3214717f34b8896320ef9.
    
    I'm keeping the baseline changes which will be needed when we get a new update.
    
    * Temporarily baseline DiagnosticCounter API gaps
    
    This can be removed when get a new CoreCLR
    
    That's currently blocked due to regressions in tests.

--- src/CoreFx.Private.TestUtilities/ref/CoreFx.Private.TestUtilities.cs
+++ src/CoreFx.Private.TestUtilities/ref/CoreFx.Private.TestUtilities.cs
@@ -27,6 +27,7 @@ namespace System
         public static void ThrowsAny<TFirstExceptionType, TSecondExceptionType, TThirdExceptionType>(System.Action action) where TFirstExceptionType : System.Exception where TSecondExceptionType : System.Exception where TThirdExceptionType : System.Exception { }
         public static System.Threading.Tasks.Task<T> ThrowsAsync<T>(string paramName, System.Func<System.Threading.Tasks.Task> testCode) where T : System.ArgumentException { throw null; }
         public static void ThrowsIf<T>(bool condition, System.Action action) where T : System.Exception { }
+        public static T Throws<T>(System.Action action) where T : System.Exception { throw null; }
         public static void Throws<T>(System.Action action, string message) where T : System.Exception { }
         public static T Throws<T>(string paramName, System.Action action) where T : System.ArgumentException { throw null; }
         public static T Throws<T>(string paramName, System.Func<object> testCode) where T : System.ArgumentException { throw null; }
@@ -42,13 +43,13 @@ namespace System
         public static bool ClientWebSocketPartialMessagesSupported { get { throw null; } }
         public static bool HasWindowsShell { get { throw null; } }
         public static System.Version ICUVersion { get { throw null; } }
+        public static bool Is32BitProcess { get { throw null; } }
         public static bool IsAlpine { get { throw null; } }
         public static bool IsArgIteratorNotSupported { get { throw null; } }
         public static bool IsArgIteratorSupported { get { throw null; } }
         public static bool IsArm64Process { get { throw null; } }
         public static bool IsArmOrArm64Process { get { throw null; } }
         public static bool IsArmProcess { get { throw null; } }
-        public static bool Is32BitProcess { get { throw null; } }
         public static bool IsCentos6 { get { throw null; } }
         public static bool IsDebian { get { throw null; } }
         public static bool IsDebian8 { get { throw null; } }
@@ -156,6 +157,7 @@ namespace System.IO
     public abstract partial class FileCleanupTestBase : System.IDisposable
     {
         protected FileCleanupTestBase() { }
+        protected static bool IsProcessElevated { get { throw null; } }
         protected string TestDirectory { get { throw null; } }
         public void Dispose() { }
         protected virtual void Dispose(bool disposing) { }
