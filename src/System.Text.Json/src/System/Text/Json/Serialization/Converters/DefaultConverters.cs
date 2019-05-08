1cbea0840488e9272d2abeb4217e0df658f98bb6
Jeremy Kuhne <jkuhne@microsoft.com>
2019-05-08 15:31:27 -0700

    Add Guid serialization support.
    Add non IConvertable converters to dictionary for easier extensibility.

--- src/System.Text.Json/src/System/Text/Json/Serialization/Converters/DefaultConverters.cs
+++ src/System.Text.Json/src/System/Text/Json/Serialization/Converters/DefaultConverters.cs
@@ -2,12 +2,27 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Concurrent;
 using System.Reflection;
 
 namespace System.Text.Json.Serialization.Converters
 {
     internal static class DefaultConverters
     {
+        private static readonly ConcurrentDictionary<Type, object> _valueConverters = new ConcurrentDictionary<Type, object>();
+
+        static DefaultConverters()
+        {
+            _valueConverters.TryAdd(typeof(DateTimeOffset), new JsonValueConverterDateTimeOffset());
+            _valueConverters.TryAdd(typeof(Guid), new JsonValueConverterGuid());
+            _valueConverters.TryAdd(typeof(JsonElement), new JsonValueConverterJsonElement());
+        }
+
+        internal static bool IsValueConvertable(Type type)
+        {
+            return typeof(IConvertible).IsAssignableFrom(type) || _valueConverters.ContainsKey(type);
+        }
+
         internal static object Create(Type type)
         {
             if (type.IsEnum)
@@ -54,19 +69,16 @@ namespace System.Text.Json.Serialization.Converters
                     return new JsonValueConverterString();
             }
 
-            if (type == typeof(DateTimeOffset))
+            if (_valueConverters.TryGetValue(type, out object value))
             {
-                return new JsonValueConverterDateTimeOffset();
+                return value;
             }
-            if (type == typeof(JsonElement))
-            {
-                return new JsonValueConverterJsonElement();
-            }
+
             if (type == typeof(object))
             {
                 return new JsonValueConverterObject();
             }
-            
+
             return null;
         }
     }
