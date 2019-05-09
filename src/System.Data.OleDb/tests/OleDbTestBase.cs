431e0cc2ffff4850fc1238dd6a9da6510b199b3c
Maryam Ariyan <maryam.ariyan@microsoft.com>
2019-05-09 06:04:34 -0700

    Fix OleDbTestBase Implementation

--- src/System.Data.OleDb/tests/OleDbTestBase.cs
+++ src/System.Data.OleDb/tests/OleDbTestBase.cs
@@ -3,15 +3,16 @@
 // See the LICENSE file in the project root for more information.
 
 using System.IO;
+using System.Threading;
 using Xunit;
 
 namespace System.Data.OleDb.Tests
 {
     public abstract class OleDbTestBase : FileCleanupTestBase, IDisposable
     {
-        protected readonly OleDbConnection connection;
-        protected readonly OleDbTransaction transaction;
-        protected readonly OleDbCommand command;
+        protected OleDbConnection connection;
+        protected OleDbTransaction transaction;
+        protected OleDbCommand command;
         private bool _disposed;
         
 
@@ -19,7 +20,22 @@ namespace System.Data.OleDb.Tests
         {
             _disposed = false;
             connection = new OleDbConnection(ConnectionString);
-            connection.Open();
+            // Make 3 attempts
+            string failure = string.Empty;
+            for (int i = 0; i <= 2; i++)
+            {
+                try
+                {
+                    connection.Open();
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    failure += ex.ToString() + Environment.NewLine;
+                    Thread.Sleep(10); // Give a transient condition like antivirus/indexing a chance to go away
+                }
+            }
+            Assert.True(ConnectionState.Open == connection.State, $"{nameof(OleDbTestBase)} failed to open {nameof(OleDbConnection)}. {failure}");
             transaction = connection.BeginTransaction();
             command = connection.CreateCommand();
             command.Transaction = transaction;
@@ -27,13 +43,26 @@ namespace System.Data.OleDb.Tests
 
         protected override void Dispose(bool disposing)
         {
-            if (!_disposed)
+            if (_disposed)
+                return;
+
+            if (disposing)
             {
-                if (disposing)
+                if (command != null)
                 {
                     command.Dispose();
+                    command = null;
+                }
+                if (transaction != null)
+                {
                     transaction.Dispose();
+                    transaction = null;
+                }
+                if (connection != null)
+                {
+                    connection.Close();
                     connection.Dispose();
+                    connection = null;
                 }
                 _disposed = true;
             }
