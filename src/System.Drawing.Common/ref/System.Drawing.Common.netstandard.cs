e5c1f3f5d459b5e4d70057540523514badd31e5a
Anipik <anirudhagnihotry098@gmail.com>
2019-05-10 10:24:34 -0700

    removing partial facades from netcoreapp\uap reference assembly build

---
+++ src/System.Drawing.Common/ref/System.Drawing.Common.netstandard.cs
@@ -0,0 +1,43 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Drawing
+{    
+    public static partial class SystemColors
+    {
+        public static System.Drawing.Color ActiveBorder { get { throw null; } }
+        public static System.Drawing.Color ActiveCaption { get { throw null; } }
+        public static System.Drawing.Color ActiveCaptionText { get { throw null; } }
+        public static System.Drawing.Color AppWorkspace { get { throw null; } }
+        public static System.Drawing.Color ButtonFace { get { throw null; } }
+        public static System.Drawing.Color ButtonHighlight { get { throw null; } }
+        public static System.Drawing.Color ButtonShadow { get { throw null; } }
+        public static System.Drawing.Color Control { get { throw null; } }
+        public static System.Drawing.Color ControlDark { get { throw null; } }
+        public static System.Drawing.Color ControlDarkDark { get { throw null; } }
+        public static System.Drawing.Color ControlLight { get { throw null; } }
+        public static System.Drawing.Color ControlLightLight { get { throw null; } }
+        public static System.Drawing.Color ControlText { get { throw null; } }
+        public static System.Drawing.Color Desktop { get { throw null; } }
+        public static System.Drawing.Color GradientActiveCaption { get { throw null; } }
+        public static System.Drawing.Color GradientInactiveCaption { get { throw null; } }
+        public static System.Drawing.Color GrayText { get { throw null; } }
+        public static System.Drawing.Color Highlight { get { throw null; } }
+        public static System.Drawing.Color HighlightText { get { throw null; } }
+        public static System.Drawing.Color HotTrack { get { throw null; } }
+        public static System.Drawing.Color InactiveBorder { get { throw null; } }
+        public static System.Drawing.Color InactiveCaption { get { throw null; } }
+        public static System.Drawing.Color InactiveCaptionText { get { throw null; } }
+        public static System.Drawing.Color Info { get { throw null; } }
+        public static System.Drawing.Color InfoText { get { throw null; } }
+        public static System.Drawing.Color Menu { get { throw null; } }
+        public static System.Drawing.Color MenuBar { get { throw null; } }
+        public static System.Drawing.Color MenuHighlight { get { throw null; } }
+        public static System.Drawing.Color MenuText { get { throw null; } }
+        public static System.Drawing.Color ScrollBar { get { throw null; } }
+        public static System.Drawing.Color Window { get { throw null; } }
+        public static System.Drawing.Color WindowFrame { get { throw null; } }
+        public static System.Drawing.Color WindowText { get { throw null; } }
+    }
+}
