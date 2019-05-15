ba0e6e52b93e63af14da77dd968b972e3fa4a4fe
Tom Deseyn <tom.deseyn@gmail.com>
2019-05-15 13:47:20 +0200

    TcpReceiveSendGetsCanceledByDispose: fix test for OSX

--- src/System.Net.Sockets/tests/FunctionalTests/SendReceive.cs
+++ src/System.Net.Sockets/tests/FunctionalTests/SendReceive.cs
@@ -1060,7 +1060,7 @@ namespace System.Net.Sockets.Tests
 
                 // On OSX, we're unable to unblock the on-going socket operations and
                 // perform an abortive close.
-                if (PlatformDetection.IsOSX)
+                if (UsesSync && PlatformDetection.IsOSX)
                 {
                     Assert.Equal(SocketError.Success, peerObservedSocketError);
 
