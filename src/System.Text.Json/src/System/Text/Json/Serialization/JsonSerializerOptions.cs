f68bcab529aeac983e022d6593435412fbdfbd58
Krzysztof Wicher <mordotymoja@gmail.com>
2019-05-23 20:58:14 -0700

    'Note:' -> '<remarks>'

--- src/System.Text.Json/src/System/Text/Json/Serialization/JsonSerializerOptions.cs
+++ src/System.Text.Json/src/System/Text/Json/Serialization/JsonSerializerOptions.cs
@@ -125,9 +125,11 @@ namespace System.Text.Json.Serialization
         /// <summary>
         /// Determines whether read-only properties are ignored during serialization.
         /// A property is read-only if it contains a public getter but not a public setter.
-        /// Note: Read-only properties will not get deserialized regardless of this setting.
         /// The default value is false.
         /// </summary>
+        /// <remarks>
+        /// Read-only properties will not get deserialized regardless of this setting.
+        /// </remarks>
         /// <exception cref="InvalidOperationException">
         /// Thrown if this property is set after serialization or deserialization has occurred.
         /// </exception>
