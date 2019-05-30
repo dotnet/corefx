023a82cd5f7b407d1377e7b4435668050ab85e98
wtgodbe <wigodbe@microsoft.com>
2019-05-30 14:56:45 -0700

    Fix tests to account for NetFx runs

--- src/System.Private.Uri/tests/FunctionalTests/UriIpHostTest.cs
+++ src/System.Private.Uri/tests/FunctionalTests/UriIpHostTest.cs
@@ -321,7 +321,6 @@ namespace System.PrivateUri.Tests
         [InlineData(":1:2:3:4:5")] // leading single colon
         [InlineData(":1:2:3:4:5:6")] // leading single colon
         [InlineData(":1:2:3:4:5:6:7")] // leading single colon
-        [InlineData(":1:2:3:4:5:6:7:8")] // leading single colon
         [InlineData(":1:2:3:4:5:6:7:8:9")] // leading single colon
         [InlineData("::1:2:3:4:5:6:7:8")] // compressor with too many number groups
         [InlineData("1::2:3:4:5:6:7:8")] // compressor with too many number groups
@@ -351,6 +350,15 @@ namespace System.PrivateUri.Tests
         {
             ParseBadIPv6Address(address);
         }
+
+        [Theory]
+        [InlineData(":1:2:3:4:5:6:7:8")] // leading single colon
+        [SkipOnTargetFramework(TargetFrameworkMonikers.NetFramework, "Full framework machines are not yet patched with the fix for this")]
+        public void UriIPv6Host_BadAddress_SkipOnFramework(string address)
+        {
+            ParseBadIPv6Address(address);
+        }
+
 
         #region Helpers
 
