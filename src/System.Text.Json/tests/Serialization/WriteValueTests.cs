7ab0cb53de4bab0fe4c152f330eca8066569e589
Ahson Khan <ahkha@microsoft.com>
2019-05-30 22:15:12 -0700

    Add the new files that were missed in previous commit.

---
+++ src/System.Text.Json/tests/Serialization/WriteValueTests.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Buffers;
+using Xunit;
+
+namespace System.Text.Json.Serialization.Tests
+{
+    public static partial class WriteValueTests
+    {
+        [Fact]
+        public static void WriteInvalid()
+        {
+            {
+                var abw = new ArrayBufferWriter<byte>();
+                var writer = new Utf8JsonWriter(abw);
+
+                JsonSerializer.WriteValue(writer, 1, typeof(int));
+                Assert.Throws<InvalidOperationException>(() => JsonSerializer.WriteValue(writer, 1, typeof(int)));
+                Assert.Throws<InvalidOperationException>(() => JsonSerializer.WriteValue(writer, 1));
+            }
+
+            {
+                var abw = new ArrayBufferWriter<byte>();
+                var writer = new Utf8JsonWriter(abw);
+
+                writer.WriteStartObject();
+                Assert.Throws<InvalidOperationException>(() => JsonSerializer.WriteValue(writer, 1, typeof(int)));
+                Assert.Throws<InvalidOperationException>(() => JsonSerializer.WriteValue(writer, 1));
+            }
+
+            {
+                var abw = new ArrayBufferWriter<byte>();
+                var writer = new Utf8JsonWriter(abw);
+
+                writer.WriteStartArray();
+                writer.WriteEndArray();
+                Assert.Throws<InvalidOperationException>(() => JsonSerializer.WriteValue(writer, 1, typeof(int)));
+                Assert.Throws<InvalidOperationException>(() => JsonSerializer.WriteValue(writer, 1));
+            }
+
+            {
+                var abw = new ArrayBufferWriter<byte>();
+                var writer = new Utf8JsonWriter(abw);
+
+                writer.WriteStartObject();
+                writer.WriteEndObject();
+                Assert.Throws<InvalidOperationException>(() => JsonSerializer.WriteValue(writer, 1, typeof(int)));
+                Assert.Throws<InvalidOperationException>(() => JsonSerializer.WriteValue(writer, 1));
+            }
+        }
+    }
+}
