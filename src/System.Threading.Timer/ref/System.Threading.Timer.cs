d4a7d5241e11588897cd9e6ee4c83db95c5cdcff
Santiago Fernandez Madero <safern@microsoft.com>
2019-06-05 16:38:28 -0700

    Add nullable annotations to System.Threading.Timer ref assembly

--- src/System.Threading.Timer/ref/System.Threading.Timer.cs
+++ src/System.Threading.Timer/ref/System.Threading.Timer.cs
@@ -10,20 +10,20 @@ namespace System.Threading
     public sealed partial class Timer : System.MarshalByRefObject, System.IAsyncDisposable, System.IDisposable
     {
         public Timer(System.Threading.TimerCallback callback) { }
-        public Timer(System.Threading.TimerCallback callback, object state, int dueTime, int period) { }
-        public Timer(System.Threading.TimerCallback callback, object state, long dueTime, long period) { }
-        public Timer(System.Threading.TimerCallback callback, object state, System.TimeSpan dueTime, System.TimeSpan period) { }
+        public Timer(System.Threading.TimerCallback callback, object? state, int dueTime, int period) { }
+        public Timer(System.Threading.TimerCallback callback, object? state, long dueTime, long period) { }
+        public Timer(System.Threading.TimerCallback callback, object? state, System.TimeSpan dueTime, System.TimeSpan period) { }
         [System.CLSCompliantAttribute(false)]
-        public Timer(System.Threading.TimerCallback callback, object state, uint dueTime, uint period) { }
+        public Timer(System.Threading.TimerCallback callback, object? state, uint dueTime, uint period) { }
+        public static long ActiveCount { get { throw null; } }
         public bool Change(int dueTime, int period) { throw null; }
         public bool Change(long dueTime, long period) { throw null; }
         public bool Change(System.TimeSpan dueTime, System.TimeSpan period) { throw null; }
         [System.CLSCompliantAttribute(false)]
         public bool Change(uint dueTime, uint period) { throw null; }
-        public static long ActiveCount { get { throw null; } }
         public void Dispose() { }
         public bool Dispose(System.Threading.WaitHandle notifyObject) { throw null; }
         public System.Threading.Tasks.ValueTask DisposeAsync() { throw null; }
     }
-    public delegate void TimerCallback(object state);
+    public delegate void TimerCallback(object? state);
 }
