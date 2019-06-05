d6a3b78f3fa2b636621f6030fc3f8b99e08474bb
Dustin Campbell <dustin@teamcampbell.org>
2019-06-05 07:58:02 -0700

    Enable code path in TypeDescriptor.GetAssociation(...) to use IDesignerHost (#38244)
    * Enable code path in TypeDescriptor.GetAssociation(...) to use IDesignerHost
    
    Fixes #38241
    
    TypeDescriptor.GetAssociation(...) is supposed to have a fallback in the case
    that the primary object passed to it is a sited IComponent in design-mode. In
    that case, it tries to acquire an IDesignerHost from the component site and
    calls IDesignerHost.GetDesigner(...). Unfortunately, this code path has been
    disabled since TypeDescriptor was introduced in the corefx code base 3 years
    ago.
    
    This change enables that code path and adds a test for the scenario.
    
    This is currently blocking WinForms Designer work.
    
    * Code review feedback for TypeConverterTests.cs

---
+++ src/System.ComponentModel.TypeConverter/tests/Mocks/MockDesigner.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel.Design;
+
+namespace System.ComponentModel.Tests
+{
+    internal class MockDesigner : IDesigner
+    {
+        public IComponent Component => throw new NotImplementedException();
+
+        public DesignerVerbCollection Verbs => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoDefaultAction()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(IComponent component)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
