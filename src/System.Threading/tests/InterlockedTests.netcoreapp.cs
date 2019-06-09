021424e194a2cab93ac8a65beb3d477cca64b4a5
vsadov <vsadov@microsoft.com>
2019-06-09 11:41:21 -0700

    Enable test for MemoryBarrierProcessWide on ARM64 since corresponding bug is closed.
    Re:https://github.com/dotnet/coreclr/issues/20215

--- src/System.Threading/tests/InterlockedTests.netcoreapp.cs
+++ src/System.Threading/tests/InterlockedTests.netcoreapp.cs
@@ -98,7 +98,6 @@ namespace System.Threading.Tests
             }
         }
 
-        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsNotArm64Process))] // issue: https://github.com/dotnet/coreclr/issues/20215
         public void MemoryBarrierProcessWide()
         {
             // Stress MemoryBarrierProcessWide correctness using a simple AsymmetricLock
