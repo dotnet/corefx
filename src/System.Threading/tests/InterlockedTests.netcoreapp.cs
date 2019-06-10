8135319caa7e457ed61053ca1418313b88057b51
Stephen Toub <stoub@microsoft.com>
2019-06-10 11:17:56 -0400

    Revert "Enable test for MemoryBarrierProcessWide on ARM64." (#38401)
    * Revert "Enable test for MemoryBarrierProcessWide on ARM64. (#38386)"
    
    This reverts commit 7e56d8bd8b224d834b782a98bfd7035f71c3f547.
    
    * Update comment for new issue number

--- src/System.Threading/tests/InterlockedTests.netcoreapp.cs
+++ src/System.Threading/tests/InterlockedTests.netcoreapp.cs
@@ -98,7 +98,7 @@ namespace System.Threading.Tests
             }
         }
 
-        [Fact]
+        [ConditionalFact(typeof(PlatformDetection), nameof(PlatformDetection.IsNotArm64Process))] // [ActiveIssue(38400)]
         public void MemoryBarrierProcessWide()
         {
             // Stress MemoryBarrierProcessWide correctness using a simple AsymmetricLock
