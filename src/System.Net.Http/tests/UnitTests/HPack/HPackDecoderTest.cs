9dfd715f033ba61238cda463bc372e7b5500e0d4
Cory Nelson <conelso@microsoft.com>
2019-06-11 13:34:10 -0700

    Fix licensing to use ASP.NET's licensing. Add a TPN.

--- src/System.Net.Http/tests/UnitTests/HPack/HPackDecoderTest.cs
+++ src/System.Net.Http/tests/UnitTests/HPack/HPackDecoderTest.cs
@@ -1,6 +1,6 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0.
+// See THIRD-PARTY-NOTICES.TXT in the project root for license information.
 
 // Note: this is largely a copy of tests from ASP.NET, with minimal changes
 // needed to support API differences. As such, it has some stylistic
