5f537a2d66271385a9087144ac1dc4c92e0c4133
Marco Rossignoli <marco.rossignoli@gmail.com>
2019-06-12 22:11:59 +0200

    address PR feedback

--- src/System.Text.Json/src/System/Text/Json/Serialization/Converters/JsonValueConverterEnum.cs
+++ src/System.Text.Json/src/System/Text/Json/Serialization/Converters/JsonValueConverterEnum.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Runtime.CompilerServices;
 using System.Text.Json.Serialization.Policies;
 
 namespace System.Text.Json.Serialization.Converters
@@ -9,17 +10,7 @@ namespace System.Text.Json.Serialization.Converters
     internal sealed class JsonValueConverterEnum<TValue> : JsonValueConverter<TValue>
         where TValue : struct, Enum
     {
-        private static readonly bool s_isUInt64 = Enum.GetUnderlyingType(typeof(TValue)) == typeof(ulong);
-        private static readonly bool s_isInt64 = Enum.GetUnderlyingType(typeof(TValue)) == typeof(long);
-
-        private static readonly bool s_isUInt32 = Enum.GetUnderlyingType(typeof(TValue)) == typeof(uint);
-        private static readonly bool s_isInt32 = Enum.GetUnderlyingType(typeof(TValue)) == typeof(int);
-
-        private static readonly bool s_isUInt16 = Enum.GetUnderlyingType(typeof(TValue)) == typeof(ushort);
-        private static readonly bool s_isInt16 = Enum.GetUnderlyingType(typeof(TValue)) == typeof(short);
-
-        private static readonly bool s_isByte = Enum.GetUnderlyingType(typeof(TValue)) == typeof(byte);
-        private static readonly bool s_isSByte = Enum.GetUnderlyingType(typeof(TValue)) == typeof(sbyte);
+        private static readonly TypeCode s_enumTypeCode = Type.GetTypeCode(Enum.GetUnderlyingType(typeof(TValue)));
 
         public bool TreatAsString { get; private set; }
 
@@ -49,90 +40,96 @@ namespace System.Text.Json.Serialization.Converters
                 return false;
             }
 
-            if (s_isInt32)
-            {
-                if (reader.TryGetInt32(out int int32))
-                {
-                    value = (TValue)Enum.ToObject(valueType, int32);
-                    return true;
-                }
-                goto False;
-            }
-
-            if (s_isByte)
-            {
-                if (reader.TryGetUInt32(out uint ubyte8) && JsonHelpers.IsInRangeInclusive(ubyte8, byte.MinValue, byte.MaxValue))
-                {
-                    value = (TValue)Enum.ToObject(valueType, ubyte8);
-                    return true;
-                }
-                goto False;
-            }
-
-            if (s_isUInt64)
-            {
-                if (reader.TryGetUInt64(out ulong uint64))
-                {
-                    value = (TValue)Enum.ToObject(valueType, uint64);
-                    return true;
-                }
-                goto False;
-            }
-
-            if (s_isInt64)
-            {
-                if (reader.TryGetInt64(out long int64))
-                {
-                    value = (TValue)Enum.ToObject(valueType, int64);
-                    return true;
-                }
-                goto False;
-            }
-
-            if (s_isUInt32)
-            {
-                if (reader.TryGetUInt32(out uint uint32))
-                {
-                    value = (TValue)Enum.ToObject(valueType, uint32);
-                    return true;
-                }
-                goto False;
-            }
-
             // When utf8reader/writer will support all primitive types we should remove custom bound checks
             // https://github.com/dotnet/corefx/issues/36125
 
-            if (s_isUInt16)
+            switch (s_enumTypeCode)
             {
-                if (reader.TryGetUInt32(out uint uint16) && JsonHelpers.IsInRangeInclusive(uint16, ushort.MinValue, ushort.MaxValue))
-                {
-                    value = (TValue)Enum.ToObject(valueType, uint16);
-                    return true;
-                }
-                goto False;
+                case TypeCode.Empty:
+                case TypeCode.Object:
+                case TypeCode.DBNull:
+                case TypeCode.Boolean:
+                case TypeCode.Char:
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.DateTime:
+                case TypeCode.String:
+                case TypeCode.Single:
+                    break;
+                case TypeCode.SByte:
+                    {
+                        if (reader.TryGetInt32(out int byte8) && JsonHelpers.IsInRangeInclusive(byte8, sbyte.MinValue, sbyte.MaxValue))
+                        {
+                            value = (TValue)Enum.ToObject(valueType, byte8);
+                            return true;
+                        }
+                        break;
+                    }
+                case TypeCode.Byte:
+                    {
+                        if (reader.TryGetUInt32(out uint ubyte8) && JsonHelpers.IsInRangeInclusive(ubyte8, byte.MinValue, byte.MaxValue))
+                        {
+                            value = (TValue)Enum.ToObject(valueType, ubyte8);
+                            return true;
+                        }
+                        break;
+                    }
+                case TypeCode.Int16:
+                    {
+                        if (reader.TryGetInt32(out int int16) && JsonHelpers.IsInRangeInclusive(int16, short.MinValue, short.MaxValue))
+                        {
+                            value = (TValue)Enum.ToObject(valueType, int16);
+                            return true;
+                        }
+                        break;
+                    }
+                case TypeCode.UInt16:
+                    {
+                        if (reader.TryGetUInt32(out uint uint16) && JsonHelpers.IsInRangeInclusive(uint16, ushort.MinValue, ushort.MaxValue))
+                        {
+                            value = (TValue)Enum.ToObject(valueType, uint16);
+                            return true;
+                        }
+                        break;
+                    }
+                case TypeCode.Int32:
+                    {
+                        if (reader.TryGetInt32(out int int32))
+                        {
+                            value = Unsafe.As<int, TValue>(ref int32);
+                            return true;
+                        }
+                        break;
+                    }
+                case TypeCode.UInt32:
+                    {
+                        if (reader.TryGetUInt32(out uint uint32))
+                        {
+                            value = Unsafe.As<uint, TValue>(ref uint32);
+                            return true;
+                        }
+                        break;
+                    }
+                case TypeCode.Int64:
+                    {
+                        if (reader.TryGetInt64(out long int64))
+                        {
+                            value = Unsafe.As<long, TValue>(ref int64);
+                            return true;
+                        }
+                        break;
+                    }
+                case TypeCode.UInt64:
+                    {
+                        if (reader.TryGetUInt64(out ulong uint64))
+                        {
+                            value = Unsafe.As<ulong, TValue>(ref uint64);
+                            return true;
+                        }
+                        break;
+                    }
             }
 
-            if (s_isInt16)
-            {
-                if (reader.TryGetInt32(out int int16) && JsonHelpers.IsInRangeInclusive(int16, short.MinValue, short.MaxValue))
-                {
-                    value = (TValue)Enum.ToObject(valueType, int16);
-                    return true;
-                }
-                goto False;
-            }
-
-            if (s_isSByte)
-            {
-                if (reader.TryGetInt32(out int byte8) && JsonHelpers.IsInRangeInclusive(byte8, sbyte.MinValue, sbyte.MaxValue))
-                {
-                    value = (TValue)Enum.ToObject(valueType, byte8);
-                    return true;
-                }
-                goto False;
-            }
-
-        False:
             value = default;
             return false;
         }
@@ -143,7 +140,7 @@ namespace System.Text.Json.Serialization.Converters
             {
                 writer.WriteStringValue(value.ToString());
             }
-            else if (s_isUInt64)
+            else if (s_enumTypeCode == TypeCode.UInt64)
             {
                 // Use the ulong converter to prevent conversion into a signed\long value.
                 ulong ulongValue = Convert.ToUInt64(value);
@@ -163,7 +160,7 @@ namespace System.Text.Json.Serialization.Converters
             {
                 writer.WriteString(propertyName, value.ToString());
             }
-            else if (s_isUInt64)
+            else if (s_enumTypeCode == TypeCode.UInt64)
             {
                 // Use the ulong converter to prevent conversion into a signed\long value.
                 ulong ulongValue = Convert.ToUInt64(value);
