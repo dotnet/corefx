b794c569372353f1db25e88a063c4e68fe3af198
Santiago Fernandez Madero <safern@microsoft.com>
2019-06-12 15:30:33 -0700

    Use builder as the param name instead

--- src/System.Collections.Immutable/src/System/Collections/Immutable/ImmutableSortedDictionary.cs
+++ src/System.Collections.Immutable/src/System/Collections/Immutable/ImmutableSortedDictionary.cs
@@ -164,14 +164,14 @@ namespace System.Collections.Immutable
         /// <summary>
         /// Returns an immutable copy of the current contents of the builder's collection.
         /// </summary>
-        /// <param name="source">The builder to create the immutable map from.</param>
+        /// <param name="builder">The builder to create the immutable map from.</param>
         /// <returns>An immutable map.</returns>
         [Pure]
-        public static ImmutableSortedDictionary<TKey, TValue> ToImmutableSortedDictionary<TKey, TValue>(this ImmutableSortedDictionary<TKey, TValue>.Builder source)
+        public static ImmutableSortedDictionary<TKey, TValue> ToImmutableSortedDictionary<TKey, TValue>(this ImmutableSortedDictionary<TKey, TValue>.Builder builder)
         {
-            Requires.NotNull(source, nameof(source));
+            Requires.NotNull(builder, nameof(builder));
 
-            return source.ToImmutable();
+            return builder.ToImmutable();
         }
 
         /// <summary>
