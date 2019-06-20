514e50a6bfbcaa13fcc4a34fc3bfbc6a99a0f3f7
Levi Broderick <levib@microsoft.com>
2019-06-20 13:58:29 -0700

    Remove Serialization Guard public APIs - Also refactors reflection helpers into separate file

--- src/System.Runtime.Serialization.Formatters/tests/SerializationGuardTests.cs
+++ src/System.Runtime.Serialization.Formatters/tests/SerializationGuardTests.cs
@@ -73,7 +73,7 @@ namespace System.Runtime.Serialization.Formatters.Tests
             ms.Position = 0;
 
             BinaryFormatter reader = new BinaryFormatter();
-            Assert.Throws<DeserializationBlockedException>(() => reader.Deserialize(ms));
+            Assert.Throws(SerializationGuard.DeserializationBlockedExceptionType, () => reader.Deserialize(ms));
         }
     }
 
