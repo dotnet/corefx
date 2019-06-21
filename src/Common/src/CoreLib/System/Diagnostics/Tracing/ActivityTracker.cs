cfc22b33ff723221141a750f73b8383c2ff339e3
Stephen Toub <stoub@microsoft.com>
2019-06-21 11:56:13 -0400

    Remove several unnecessary string allocations (dotnet/coreclr#25305)
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/Diagnostics/Tracing/ActivityTracker.cs
+++ src/Common/src/CoreLib/System/Diagnostics/Tracing/ActivityTracker.cs
@@ -269,15 +269,24 @@ namespace System.Diagnostics.Tracing
         /// </summary>
         private string NormalizeActivityName(string providerName, string activityName, int task)
         {
-            if (activityName.EndsWith(EventSource.s_ActivityStartSuffix, StringComparison.Ordinal))
-                activityName = activityName.Substring(0, activityName.Length - EventSource.s_ActivityStartSuffix.Length);
-            else if (activityName.EndsWith(EventSource.s_ActivityStopSuffix, StringComparison.Ordinal))
-                activityName = activityName.Substring(0, activityName.Length - EventSource.s_ActivityStopSuffix.Length);
-            else if (task != 0)
-                activityName = "task" + task.ToString();
-
             // We use provider name to distinguish between activities from different providers.
-            return providerName + activityName;
+
+            if (activityName.EndsWith(EventSource.s_ActivityStartSuffix, StringComparison.Ordinal))
+            {
+                return string.Concat(providerName, activityName.AsSpan(0, activityName.Length - EventSource.s_ActivityStartSuffix.Length));
+            }
+            else if (activityName.EndsWith(EventSource.s_ActivityStopSuffix, StringComparison.Ordinal))
+            {
+                return string.Concat(providerName, activityName.AsSpan(0, activityName.Length - EventSource.s_ActivityStopSuffix.Length));
+            }
+            else if (task != 0)
+            {
+                return providerName + "task" + task.ToString();
+            }
+            else
+            {
+                return providerName + activityName;
+            }
         }
 
         // *******************************************************************************
