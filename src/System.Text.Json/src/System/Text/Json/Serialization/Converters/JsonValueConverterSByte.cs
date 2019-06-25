be68ab2410401f96867126db91e6c1a8e0f4c4fd
WinCPP <mandards@gmail.com>
2019-06-26 03:07:47 +0530

    CoreFx #38894: Use (s)byte and (u)int16 get apis

--- src/System.Text.Json/src/System/Text/Json/Serialization/Converters/JsonValueConverterSByte.cs
+++ src/System.Text.Json/src/System/Text/Json/Serialization/Converters/JsonValueConverterSByte.cs
@@ -8,15 +8,12 @@ namespace System.Text.Json.Serialization.Converters
     {
         public override sbyte Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            if (reader.TokenType != JsonTokenType.Number ||
-                !reader.TryGetInt32(out int rawValue) ||
-                !JsonHelpers.IsInRangeInclusive(rawValue, sbyte.MinValue, sbyte.MaxValue))
+            if (reader.TokenType != JsonTokenType.Number)
             {
                 ThrowHelper.ThrowJsonException();
-                return default;
             }
 
-            return (sbyte)reader.GetInt32();
+            return reader.GetSByte();
         }
 
         public override void Write(Utf8JsonWriter writer, sbyte value, JsonSerializerOptions options)
