fd1430864549f9635729862d79ad58606d63b44b
Dan Moseley <danmose@microsoft.com>
2019-06-24 20:46:49 -0700

    typo

--- src/CoreFx.Private.TestUtilities/src/System/PlatformApis.Unix.cs
+++ src/CoreFx.Private.TestUtilities/src/System/PlatformApis.Unix.cs
@@ -238,7 +238,7 @@ namespace System
 
             // In some distros/versions we cannot discover the distro version; return something valid.
             // Pick a high version number, since this seems to happen on newer distros.
-            if (String.IsNullOrEmpty(_distroInfo.Value?.VersionId)
+            if (String.IsNullOrEmpty(_distroInfo.Value?.VersionId))
             {
                 distroInfo.VersionId = (new Version(Int32.MaxValue, Int32.MaxValue)).ToString();
             }
