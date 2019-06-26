1ed17d7c049792686ffa653831ae3766d99bf491
Vatsan Madhavan <vatsan.madhavan@microsoft.com>
2019-06-26 01:09:51 -0700

    Limit  to netcoreapp/windows

--- src/System.Windows.Extensions/tests/XamlAccessLevelTests.cs
+++ src/System.Windows.Extensions/tests/XamlAccessLevelTests.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Reflection;
-using System.Xaml.Permissions;
 using Xunit;
 
 namespace System.Security.Permissions.Tests
