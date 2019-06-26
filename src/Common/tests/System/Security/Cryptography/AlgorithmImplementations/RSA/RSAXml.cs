5590a9172a65be8cd6f93fdd76f547e63006c837
Kevin Jones <kevin@vcsjones.com>
2019-06-26 10:03:10 -0400

    Validate RSA parameters when importing to OpenSSL
    This imports a key explicitly into the OpenSSL default software RSA implementation to call 
    `RSA_check_key` to determine that the key parameters are consistent.
    
    After the key consistency is determined, then it imports into a second new key handle, using the
    configured default (which may also be the default software RSA implementation), and returns that one.

--- src/Common/tests/System/Security/Cryptography/AlgorithmImplementations/RSA/RSAXml.cs
+++ src/Common/tests/System/Security/Cryptography/AlgorithmImplementations/RSA/RSAXml.cs
@@ -1340,7 +1340,7 @@ zM=
         }
 
         [Fact]
-        [ActiveIssue(37595, TestPlatforms.AnyUnix)]
+        [ActiveIssue(37595, TestPlatforms.OSX)]
         public static void FromNonsenseXml()
         {
             // This is DiminishedDPParameters XML, but with a P that is way too long.
