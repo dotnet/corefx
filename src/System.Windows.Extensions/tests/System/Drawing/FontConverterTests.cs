06f5035b13bd25c05d1d2810de8db81403e86e4f
Anirudh Agnihotry <anirudhagnihotry098@gmail.com>
2019-07-10 17:27:24 -0700

    checking if empty string is an installed font (#39309)
    * checking if empty string is an installed font
    
    * Not running font converter tests for netfx and disposing the installed FOnts object

--- src/System.Windows.Extensions/tests/System/Drawing/FontConverterTests.cs
+++ src/System.Windows.Extensions/tests/System/Drawing/FontConverterTests.cs
@@ -3,7 +3,9 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Drawing;
+using System.Drawing.Text;
 using System.Globalization;
+using System.Linq;
 using Xunit;
 using static System.Drawing.FontConverter;
 
@@ -97,22 +99,33 @@ namespace System.ComponentModel.TypeConverterTests
                 { $"Arial{s_Separator} 10{s_Separator}", "Arial", 10f, GraphicsUnit.Point, FontStyle.Regular },
                 { $"Arial{s_Separator}", "Arial", 8.25f, GraphicsUnit.Point, FontStyle.Regular },
                 { $"Arial{s_Separator} 10{s_Separator} style=12", "Arial", 10f, GraphicsUnit.Point, FontStyle.Underline | FontStyle.Strikeout },
+                { $"Courier New{s_Separator} Style=Bold", "Courier New", 8.25f, GraphicsUnit.Point, FontStyle.Bold }, // FullFramework style keyword is case sensitive.
+                { $"11px{s_Separator} Style=Bold", "Microsoft Sans Serif", 8.25f, GraphicsUnit.Point, FontStyle.Bold}
             };
 
-            if (!PlatformDetection.IsFullFramework)
+            // FullFramework disregards all arguments if the font name is an empty string.
+            // Empty string is not an installed font on Windows 7, windows 8 and some versions of windows 10.
+            if (EmptyFontPresent)
             {
-                // FullFramework style keyword is case sensitive.
-                data.Add($"Courier New{s_Separator} Style=Bold", "Courier New", 8.25f, GraphicsUnit.Point, FontStyle.Bold);
-                data.Add($"11px{s_Separator} Style=Bold", "Microsoft Sans Serif", 8.25f, GraphicsUnit.Point, FontStyle.Bold);
-
-                // FullFramework disregards all arguments if the font name is an empty string.
-                if (PlatformDetection.IsWindows10Version1607OrGreater)
-                {
-                    data.Add($"{s_Separator} 10{s_Separator} style=bold", "", 10f, GraphicsUnit.Point, FontStyle.Bold); // empty string is not a installed font on Windows 7 and windows 8.
-                }
+                data.Add($"{s_Separator} 10{s_Separator} style=bold", "", 10f, GraphicsUnit.Point, FontStyle.Bold);
+            }
+            else
+            {
+                data.Add($"{s_Separator} 10{s_Separator} style=bold", "Microsoft Sans Serif", 10f, GraphicsUnit.Point, FontStyle.Bold);
             }
 
             return data;
+        }
+
+        private static bool EmptyFontPresent
+        {
+            get
+            {
+                using (var installedFonts = new InstalledFontCollection())
+                {
+                    return installedFonts.Families.Select(t => t.Name).Contains(string.Empty);
+                }
+            }
         }
 
         public static TheoryData<string, string, string> ArgumentExceptionFontConverterData() => new TheoryData<string, string, string>()
