2034e2cd9b801a06d62fbf23db30c18ab5dcad69
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2019-07-13 11:11:46 -0700

    Remove unnecessary using

--- src/System.Text.Json/tests/Serialization/EnumConverterTests.cs
+++ src/System.Text.Json/tests/Serialization/EnumConverterTests.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.IO;
-using System.Runtime.InteropServices.ComTypes;
 using Xunit;
 
 namespace System.Text.Json.Serialization.Tests
