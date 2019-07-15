8af2b531bdd0ea41d3507870fadf9a26d663e700
Steve Harter <steveharter@users.noreply.github.com>
2019-07-15 10:29:03 -0700

    Address nits from other PRs (#39482)

--- src/System.Text.Json/tests/Serialization/DictionaryTests.KeyPolicy.cs
+++ src/System.Text.Json/tests/Serialization/DictionaryTests.KeyPolicy.cs
@@ -3,8 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
-using System.Runtime.Serialization;
-using Newtonsoft.Json;
 using Xunit;
 
 namespace System.Text.Json.Serialization.Tests
