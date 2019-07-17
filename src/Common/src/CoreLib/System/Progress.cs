b31d5feabf0462e8f38fa81d0de915ea86fcb56b
Santiago Fernandez Madero <safern@microsoft.com>
2019-07-16 17:22:54 -0700

    Fix nullability warnings from compiler update
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/Progress.cs
+++ src/Common/src/CoreLib/System/Progress.cs
@@ -56,7 +56,7 @@ namespace System
         /// Handlers registered with this event will be invoked on the 
         /// <see cref="System.Threading.SynchronizationContext"/> captured when the instance was constructed.
         /// </remarks>
-        public event EventHandler<T> ProgressChanged;
+        public event EventHandler<T> ProgressChanged = null!;
 
         /// <summary>Reports a progress change.</summary>
         /// <param name="value">The value of the updated progress.</param>
