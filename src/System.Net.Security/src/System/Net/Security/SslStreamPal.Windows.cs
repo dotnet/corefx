4c645e36207349fab1bf22b5418d58180c347ae1
Krzysztof Wicher <kwicher@microsoft.com>
2019-07-18 16:12:37 -0700

    Fix Windows 8.1 (product issue)

--- src/System.Net.Security/src/System/Net/Security/SslStreamPal.Windows.cs
+++ src/System.Net.Security/src/System/Net/Security/SslStreamPal.Windows.cs
@@ -377,6 +377,16 @@ namespace System.Net.Security
 
         private static int GetProtocolFlagsFromSslProtocols(SslProtocols protocols, bool isServer)
         {
+            if (protocols.HasFlag(SslProtocols.Tls12) || protocols.HasFlag(SslProtocols.Tls13))
+            {
+#pragma warning disable 0618
+                // SSL2 is mutually exclusive with >= TLS1.2
+                // On Windows10 SSL2 flag has no effect but on earlier versions of the OS
+                // opting into both SSL2 and >= TLS1.2 causes negotiation to always fail.
+                protocols &= ~SslProtocols.Ssl2;
+#pragma warning restore 0618
+            }
+
             int protocolFlags = (int)protocols;
 
             if (isServer)
