681ce39a7064dd9cd1389eb320662bb1489aefbb
Katarzyna Bulat <t-kabul@microsoft.com>
2019-08-06 14:20:29 -0700

    JsonString implementation started

---
+++ src/System.Text.Json/tests/JsonStringTests.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace System.Text.Json.Tests
+{
+    public static class JsonStringTests
+    {
+        [Fact]
+        public static void TestDefaultCtor()
+        {
+            var jsonString = new JsonString();
+            Assert.Equal("", jsonString.Value);
+        }
+
+        [Fact]
+        public static void TestValueCtor()
+        {
+            var jsonString = new JsonString("property value");
+            Assert.Equal("property value", jsonString.Value);
+        }
+
+        [Fact]
+        public static void TestChangingValue()
+        {
+            var jsonString = new JsonString();
+            jsonString.Value = "property value";
+            Assert.Equal("property value", jsonString.Value);
+            jsonString.Value = "different property value";
+            Assert.Equal("different property value", jsonString.Value);
+        }
+    }
+}
