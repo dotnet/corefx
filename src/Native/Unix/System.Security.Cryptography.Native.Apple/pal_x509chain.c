6e1a3b2d7c146162d00584e13d5cbfde427dd63a
buyaa-n <bunamnan@microsoft.com>
2019-08-07 09:11:17 -0700

    [release/3.0] For Mac Catalina build, suppress warnings porting to 3.0 (#39984)
    * For Mac Catalina build, suppress warnings  (#39938)
    
    * Applying feedback

--- src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_x509chain.c
+++ src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_x509chain.c
@@ -63,7 +63,10 @@ int32_t AppleCryptoNative_X509ChainEvaluate(SecTrustRef chain,
     }
 
     SecTrustResultType trustResult;
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
     *pOSStatus = SecTrustEvaluate(chain, &trustResult);
+#pragma clang diagnostic pop
 
     // If any error is reported from the function or the trust result value indicates that
     // otherwise was a failed chain build (vs an untrusted chain, etc) return failure and
