da219ed0aee5d2b6c5af1bf25a3b37aa3aeb48a5
Buyaa <bunamnan@microsoft.com>
2019-08-08 09:56:50 -0700

    Using system a.m. string for test instead of hard coded one

--- src/Common/tests/System/Runtime/Serialization/Utils.cs
+++ src/Common/tests/System/Runtime/Serialization/Utils.cs
@@ -73,7 +73,6 @@ internal static class Utils
             return new CompareResult { ErrorMessage = "actual was null, but was expecting:\n" + expected };
         }
 
-        actual = actual.Replace('\u00A0', ' '); // normalize non breaking space to space; varies by OS
         int commonLength = Min(actual.Length, expected.Length);
 
         for (int currentIndex = 0; currentIndex < commonLength; ++currentIndex)
