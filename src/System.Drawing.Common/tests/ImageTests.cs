20506244eb715f729475f5c558b046c1945eb373
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-08-09 14:31:27 +0200

    Add unit tests

--- src/System.Drawing.Common/tests/ImageTests.cs
+++ src/System.Drawing.Common/tests/ImageTests.cs
@@ -2,7 +2,9 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace System.Drawing.Tests
@@ -102,6 +104,92 @@ namespace System.Drawing.Tests
             AssertExtensions.Throws<ArgumentNullException, ArgumentException>("stream", null, () => Image.FromStream(null));
             AssertExtensions.Throws<ArgumentNullException, ArgumentException>("stream", null, () => Image.FromStream(null, useEmbeddedColorManagement: true));
             AssertExtensions.Throws<ArgumentNullException, ArgumentException>("stream", null, () => Image.FromStream(null, useEmbeddedColorManagement: true, validateImageData: true));
+        }
+
+        [Theory]
+        [InlineData(PixelFormat.Format1bppIndexed, 1)]
+        [InlineData(PixelFormat.Format4bppIndexed, 4)]
+        [InlineData(PixelFormat.Format8bppIndexed, 8)]
+        [InlineData(PixelFormat.Format16bppArgb1555, 16)]
+        [InlineData(PixelFormat.Format16bppGrayScale, 16)]
+        [InlineData(PixelFormat.Format16bppRgb555, 16)]
+        [InlineData(PixelFormat.Format16bppRgb565, 16)]
+        [InlineData(PixelFormat.Format24bppRgb, 24)]
+        [InlineData(PixelFormat.Format32bppArgb, 32)]
+        [InlineData(PixelFormat.Format32bppPArgb, 32)]
+        [InlineData(PixelFormat.Format32bppRgb, 32)]
+        [InlineData(PixelFormat.Format48bppRgb, 48)]
+        [InlineData(PixelFormat.Format64bppArgb, 64)]
+        [InlineData(PixelFormat.Format64bppPArgb, 64)]
+        public void GetPixelFormatSize_ReturnsExpected(PixelFormat format, int expectedSize)
+        {
+            Assert.Equal(expectedSize, Image.GetPixelFormatSize(format));
+        }
+
+        [Theory]
+        [InlineData(PixelFormat.Format16bppArgb1555, true)]
+        [InlineData(PixelFormat.Format32bppArgb, true)]
+        [InlineData(PixelFormat.Format32bppPArgb, true)]
+        [InlineData(PixelFormat.Format64bppArgb, true)]
+        [InlineData(PixelFormat.Format64bppPArgb, true)]
+        [InlineData(PixelFormat.Format16bppGrayScale, false)]
+        [InlineData(PixelFormat.Format16bppRgb555, false)]
+        [InlineData(PixelFormat.Format16bppRgb565, false)]
+        [InlineData(PixelFormat.Format1bppIndexed, false)]
+        [InlineData(PixelFormat.Format24bppRgb, false)]
+        [InlineData(PixelFormat.Format32bppRgb, false)]
+        [InlineData(PixelFormat.Format48bppRgb, false)]
+        [InlineData(PixelFormat.Format4bppIndexed, false)]
+        [InlineData(PixelFormat.Format8bppIndexed, false)]
+        public void IsAlphaPixelFormat_ReturnsExpected(PixelFormat format, bool expected)
+        {
+            Assert.Equal(expected, Image.IsAlphaPixelFormat(format));
+        }
+
+        public static IEnumerable<object[]> GetEncoderParameterList_ReturnsExpected_TestData()
+        {
+            yield return new object[]
+            {
+                ImageFormat.Tiff,
+                new Guid[]
+                {
+                    Encoder.Compression.Guid,
+                    Encoder.ColorDepth.Guid,
+                    Encoder.SaveFlag.Guid,
+                    Encoder.SaveAsCmyk.Guid
+                }
+            };
+
+            yield return new object[]
+            {
+                ImageFormat.Jpeg,
+                new Guid[]
+                {
+                    Encoder.Transformation.Guid,
+                    Encoder.Quality.Guid,
+                    Encoder.LuminanceTable.Guid,
+                    Encoder.ChrominanceTable.Guid,
+                    Encoder.ImageItems.Guid
+                }
+            };
+        }
+
+        [ActiveIssue(20884, TestPlatforms.AnyUnix)]
+        [ConditionalTheory(Helpers.IsDrawingSupported)]
+        [MemberData(nameof(GetEncoderParameterList_ReturnsExpected_TestData))]
+        public void GetEncoderParameterList_ReturnsExpected(ImageFormat format, Guid[] expectedParameters)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+            ImageCodecInfo codec = codecs.Single(c => c.FormatID == format.Guid);
+
+            using (var bitmap = new Bitmap(1, 1))
+            {
+                EncoderParameters paramList = bitmap.GetEncoderParameterList(codec.Clsid);
+
+                Assert.Equal(
+                    expectedParameters,
+                    paramList.Param.Select(p => p.Encoder.Guid));
+            }
         }
     }
 }
