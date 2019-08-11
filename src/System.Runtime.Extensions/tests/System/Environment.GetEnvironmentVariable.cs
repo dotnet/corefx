61c5e96b70fd14621ec71aee356b1b520696a3c9
Viktor Hofer <viktor.hofer@microsoft.com>
2019-08-11 14:19:01 +0200

    Upgrade Windows Nano Server to 1809 (#39734)
    * Upgrade Windows Nano Server to 1809
    
    * Handle ERROR_CALL_NOT_IMPLEMENTED in PlatformDetection.IsInAppContainer
    
    * Disable TextInfo test on Nano server
    
    * Disable EnvironmentVariable tests on user scope

--- src/System.Runtime.Extensions/tests/System/Environment.GetEnvironmentVariable.cs
+++ src/System.Runtime.Extensions/tests/System/Environment.GetEnvironmentVariable.cs
@@ -220,6 +220,13 @@ namespace System.Tests
         public void EnumerateEnvironmentVariables(EnvironmentVariableTarget target)
         {
             bool lookForSetValue = (target == EnvironmentVariableTarget.Process) || PlatformDetection.IsWindowsAndElevated;
+            
+            // [ActiveIssue(40226)]
+            if (PlatformDetection.IsWindowsNanoServer && target == EnvironmentVariableTarget.User)
+            {
+                lookForSetValue = false;
+            }
+
             string key = $"EnumerateEnvironmentVariables ({target})";
             string value = Path.GetRandomFileName();
 
