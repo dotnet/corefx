7f0e5e3ba86fff73e59f59870957b010dfc7ff07
Santiago Fernandez Madero <safern@microsoft.com>
2019-08-13 11:59:24 -0700

    As a to as for text

--- src/System.Text.Json/src/System/Text/Json/Writer/Utf8JsonWriter.WriteValues.Bytes.cs
+++ src/System.Text.Json/src/System/Text/Json/Writer/Utf8JsonWriter.WriteValues.Bytes.cs
@@ -10,9 +10,9 @@ namespace System.Text.Json
     public sealed partial class Utf8JsonWriter
     {
         /// <summary>
-        /// Writes the raw bytes value as Base64 encoded JSON string as an element of a JSON array.
+        /// Writes the raw bytes value as a Base64 encoded JSON string as an element of a JSON array.
         /// </summary>
-        /// <param name="bytes">The binary data to write as a Base64 encoded text.</param>
+        /// <param name="bytes">The binary data to write as Base64 encoded text.</param>
         /// <exception cref="ArgumentException">
         /// Thrown when the specified value is too large.
         /// </exception>
