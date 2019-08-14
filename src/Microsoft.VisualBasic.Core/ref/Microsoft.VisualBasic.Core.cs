8b9844a07d51777a3ec674be173308976b9d34fa
Charles Stoner <chucks@microsoft.com>
2019-08-14 14:39:19 -0700

    Add missing members for ErrObject

--- src/Microsoft.VisualBasic.Core/ref/Microsoft.VisualBasic.Core.cs
+++ src/Microsoft.VisualBasic.Core/ref/Microsoft.VisualBasic.Core.cs
@@ -306,8 +306,11 @@ namespace Microsoft.VisualBasic
         internal ErrObject() { }
         public string Description { get { throw null; } set { } }
         public int Erl { get { throw null; } }
+        public int HelpContext { get { throw null; } set { } }
+        public string HelpFile { get { throw null; } set { } }
         public int LastDllError { get { throw null; } }
         public int Number { get { throw null; } set { } }
+        public string Source { get { throw null; } set { } }
         public void Clear() { }
         public System.Exception GetException() { throw null; }
         public void Raise(int Number, object Source = null, object Description = null, object HelpFile = null, object HelpContext = null) { }
@@ -458,6 +461,7 @@ namespace Microsoft.VisualBasic
     public sealed partial class Information
     {
         internal Information() { }
+        public static int Erl() { throw null; }
         public static Microsoft.VisualBasic.ErrObject Err() { throw null; }
         public static bool IsArray(object VarName) { throw null; }
         public static bool IsDate(object Expression) { throw null; }
