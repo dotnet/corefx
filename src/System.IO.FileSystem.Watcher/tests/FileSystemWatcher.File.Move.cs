8ee5d4fe5aec1d76d4c3cf4a4fdd7a4a8b917486
Buyaa <bunamnan@microsoft.com>
2019-08-15 14:00:40 -0700

    Formatting update

--- src/System.IO.FileSystem.Watcher/tests/FileSystemWatcher.File.Move.cs
+++ src/System.IO.FileSystem.Watcher/tests/FileSystemWatcher.File.Move.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
+
 using System.Runtime.InteropServices;
 using Xunit;
 
@@ -52,6 +53,7 @@ namespace System.IO.Tests
         [Fact]
         public void File_Move_From_Unwatched_To_Watched()
         {
+            // TODO remove OS version check after https://github.com/dotnet/corefx/issues/40034 fixed
             if (!RuntimeInformation.IsOSPlatform(OSPlatform.OSX) || Environment.OSVersion.Version.Major < 19)
             {
                 FileMove_FromUnwatchedToWatched(WatcherChangeTypes.Created);
