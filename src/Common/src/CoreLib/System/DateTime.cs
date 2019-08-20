0180195d72842f7b82d3436971be8b3b29cd7658
Stephen Toub <stoub@microsoft.com>
2019-08-20 09:14:01 -0400

    Fix StyleCop warning SA1013 (closing brace spacing)
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/System/DateTime.cs
+++ src/Common/src/CoreLib/System/DateTime.cs
@@ -99,9 +99,9 @@ namespace System
         private const int DatePartDay = 3;
 
         private static readonly int[] s_daysToMonth365 = {
-            0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365};
+            0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 };
         private static readonly int[] s_daysToMonth366 = {
-            0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366};
+            0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366 };
 
         public static readonly DateTime MinValue = new DateTime(MinTicks, DateTimeKind.Unspecified);
         public static readonly DateTime MaxValue = new DateTime(MaxTicks, DateTimeKind.Unspecified);
