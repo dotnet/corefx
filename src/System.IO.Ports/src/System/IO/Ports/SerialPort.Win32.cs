b916df38d5f6e4f7e90a5c5015ed8d5f4608cb94
Paul <paul.farrand@primetss.com.au>
2019-08-21 13:25:27 +0930

    Update src/System.IO.Ports/src/System/IO/Ports/SerialPort.Win32.cs
    Co-Authored-By: Krzysztof Wicher <mordotymoja@gmail.com>

--- src/System.IO.Ports/src/System/IO/Ports/SerialPort.Win32.cs
+++ src/System.IO.Ports/src/System/IO/Ports/SerialPort.Win32.cs
@@ -15,7 +15,7 @@ namespace System.IO.Ports
         {
             // Hitting the registry for this isn't the only way to get the ports.
             //
-            // WMI: https://msdn.microsoft.com/en-us/library/aa394413.aspx [But Requires .NetFramework !]
+            // WMI: https://msdn.microsoft.com/en-us/library/aa394413.aspx [requires .NetFramework]
             // QueryDosDevice: https://msdn.microsoft.com/en-us/library/windows/desktop/aa365461.aspx [Can produces duplicates eg COM4 also listed as DosDeviceName \\USB#VID_1B4F&PID_214F..]
             //
             // QueryDosDevice involves finding any ports that map to \Device\Serialx (call with null to get all, then iterate to get the actual device name) [Typically maps to COMx value]
