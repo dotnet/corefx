ecc963eb834cb42d8d66bd605bc33ff6fbb9be68
Stephen Toub <stoub@microsoft.com>
2019-08-27 11:07:34 -0400

    Remove stray "#nullable enable" from Pkcs
    This assembly hasn't been reviewed yet for nullability annotations.  We don't want to be mislead by this one file saying it has been.

--- src/System.Security.Cryptography.Pkcs/src/System/Security/Cryptography/Pkcs/CmsRecipient.cs
+++ src/System.Security.Cryptography.Pkcs/src/System/Security/Cryptography/Pkcs/CmsRecipient.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable enable
 using System;
 using System.Diagnostics;
 using System.Security.Cryptography.X509Certificates;
@@ -67,7 +66,7 @@ namespace System.Security.Cryptography.Pkcs
 #else
         public
 #endif
-        RSAEncryptionPadding? RSAEncryptionPadding { get; }
+        RSAEncryptionPadding RSAEncryptionPadding { get; }
         public SubjectIdentifierType RecipientIdentifierType { get; }
         public X509Certificate2 Certificate { get; }
 
