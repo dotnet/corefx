6598e521045e9d2671fb0c234d771a433392a1ea
Steve Harter <steveharter@users.noreply.github.com>
2019-08-29 12:43:09 -0500

    [release/3.0] Fix issue where dictionary properties that immediately follow a skipped dictionary property were skipped. (#40624)
    * Fix issue where dictionary properties that immediately follow a skipped dictionary property were skipped(#40598)
    
    * Fix additional issue of dictionary properties being skipped (#40629)

--- src/System.Text.Json/src/System/Text/Json/Serialization/JsonSerializer.Read.HandleNull.cs
+++ src/System.Text.Json/src/System/Text/Json/Serialization/JsonSerializer.Read.HandleNull.cs
@@ -12,6 +12,10 @@ namespace System.Text.Json
         {
             if (state.Current.SkipProperty)
             {
+                // Clear the current property in case it is a dictionary, since dictionaries must have EndProperty() called when completed.
+                // A non-dictionary property can also have EndProperty() called when completed, although it is redundant.
+                state.Current.EndProperty();
+
                 return false;
             }
 
