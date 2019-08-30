70d2b62dcfd328228f8ea6754faae0ed3b69ff37
Stephen Toub <stoub@microsoft.com>
2019-08-30 15:23:38 -0400

    Annotate Microsoft.Win32.Primitives for nullable reference types (#40652)

--- src/Microsoft.Win32.Primitives/ref/Microsoft.Win32.Primitives.cs
+++ src/Microsoft.Win32.Primitives/ref/Microsoft.Win32.Primitives.cs
@@ -11,10 +11,10 @@ namespace System.ComponentModel
     {
         public Win32Exception() { }
         public Win32Exception(int error) { }
-        public Win32Exception(int error, string message) { }
+        public Win32Exception(int error, string? message) { }
         protected Win32Exception(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) { }
-        public Win32Exception(string message) { }
-        public Win32Exception(string message, System.Exception innerException) { }
+        public Win32Exception(string? message) { }
+        public Win32Exception(string? message, System.Exception? innerException) { }
         public int NativeErrorCode { get { throw null; } }
         public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) { }
         public override string ToString() { throw null; }
