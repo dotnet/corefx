a3416e730a22b79a8be2d22bf1f80cb01a2569c0
Stephen Toub <stoub@microsoft.com>
2019-08-30 14:47:06 -0400

    Add System.Linq tests to validate enumerator lifecycle

--- src/System.Linq/tests/EnumerableTests.cs
+++ src/System.Linq/tests/EnumerableTests.cs
@@ -345,25 +345,5 @@ namespace System.Linq.Tests
 
             void IDisposable.Dispose() => _dispose();
         }
-
-        protected sealed class DisposeTrackingEnumerable<T> : IEnumerable<T>
-        {
-            public bool EnumeratorDisposed { get; private set; }
-
-
-            public IEnumerator<T> GetEnumerator() => new Enumerator(this);
-            IEnumerator IEnumerable.GetEnumerator() => new Enumerator(this);
-
-            private sealed class Enumerator : IEnumerator<T>
-            {
-                private readonly DisposeTrackingEnumerable<T> _enumerable;
-                public Enumerator(DisposeTrackingEnumerable<T> enumerable) => _enumerable = enumerable;
-                public void Dispose() => _enumerable.EnumeratorDisposed = true;
-                public T Current => default;
-                object IEnumerator.Current => default;
-                public bool MoveNext() => false;
-                public void Reset() { }
-            }
-        }
     }
 }
