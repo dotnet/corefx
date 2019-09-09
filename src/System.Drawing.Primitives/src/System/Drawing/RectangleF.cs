5bc243aa2040a1dbf63df1dd099124ade64ed5ce
Hugh Bellamy <hughbellars@gmail.com>
2019-09-09 03:18:22 +0100

    Cleanup drawing primitives (#40019)

--- src/System.Drawing.Primitives/src/System/Drawing/RectangleF.cs
+++ src/System.Drawing.Primitives/src/System/Drawing/RectangleF.cs
@@ -8,17 +8,14 @@ using System.Numerics.Hashing;
 namespace System.Drawing
 {
     /// <summary>
-    ///    <para>
-    ///       Stores the location and size of a rectangular region.
-    ///    </para>
+    /// Stores the location and size of a rectangular region.
     /// </summary>
     [Serializable]
     [System.Runtime.CompilerServices.TypeForwardedFrom("System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
     public struct RectangleF : IEquatable<RectangleF>
     {
         /// <summary>
-        ///    Initializes a new instance of the <see cref='System.Drawing.RectangleF'/>
-        ///    class.
+        /// Initializes a new instance of the <see cref='System.Drawing.RectangleF'/> class.
         /// </summary>
         public static readonly RectangleF Empty = new RectangleF();
 
@@ -28,10 +25,8 @@ namespace System.Drawing
         private float height; // Do not rename (binary serialization)
 
         /// <summary>
-        ///    <para>
-        ///       Initializes a new instance of the <see cref='System.Drawing.RectangleF'/>
-        ///       class with the specified location and size.
-        ///    </para>
+        /// Initializes a new instance of the <see cref='System.Drawing.RectangleF'/> class with the specified location
+        /// and size.
         /// </summary>
         public RectangleF(float x, float y, float width, float height)
         {
@@ -42,11 +37,8 @@ namespace System.Drawing
         }
 
         /// <summary>
-        ///    <para>
-        ///       Initializes a new instance of the <see cref='System.Drawing.RectangleF'/>
-        ///       class with the specified location
-        ///       and size.
-        ///    </para>
+        /// Initializes a new instance of the <see cref='System.Drawing.RectangleF'/> class with the specified location
+        /// and size.
         /// </summary>
         public RectangleF(PointF location, SizeF size)
         {
@@ -57,24 +49,19 @@ namespace System.Drawing
         }
 
         /// <summary>
-        ///    <para>
-        ///       Creates a new <see cref='System.Drawing.RectangleF'/> with
-        ///       the specified location and size.
-        ///    </para>
+        /// Creates a new <see cref='System.Drawing.RectangleF'/> with the specified location and size.
         /// </summary>
         public static RectangleF FromLTRB(float left, float top, float right, float bottom) =>
             new RectangleF(left, top, right - left, bottom - top);
 
         /// <summary>
-        ///    <para>
-        ///       Gets or sets the coordinates of the upper-left corner of
-        ///       the rectangular region represented by this <see cref='System.Drawing.RectangleF'/>.
-        ///    </para>
+        /// Gets or sets the coordinates of the upper-left corner of the rectangular region represented by this
+        /// <see cref='System.Drawing.RectangleF'/>.
         /// </summary>
         [Browsable(false)]
         public PointF Location
         {
-            get { return new PointF(X, Y); }
+            get => new PointF(X, Y);
             set
             {
                 X = value.X;
@@ -83,14 +70,12 @@ namespace System.Drawing
         }
 
         /// <summary>
-        ///    <para>
-        ///       Gets or sets the size of this <see cref='System.Drawing.RectangleF'/>.
-        ///    </para>
+        /// Gets or sets the size of this <see cref='System.Drawing.RectangleF'/>.
         /// </summary>
         [Browsable(false)]
         public SizeF Size
         {
-            get { return new SizeF(Width, Height); }
+            get => new SizeF(Width, Height);
             set
             {
                 Width = value.Width;
@@ -99,163 +84,122 @@ namespace System.Drawing
         }
 
         /// <summary>
-        ///    <para>
-        ///       Gets or sets the x-coordinate of the
-        ///       upper-left corner of the rectangular region defined by this <see cref='System.Drawing.RectangleF'/>.
-        ///    </para>
+        /// Gets or sets the x-coordinate of the upper-left corner of the rectangular region defined by this
+        /// <see cref='System.Drawing.RectangleF'/>.
         /// </summary>
         public float X
         {
-            get { return x; }
-            set { x = value; }
+            get => x;
+            set => x = value;
         }
 
         /// <summary>
-        ///    <para>
-        ///       Gets or sets the y-coordinate of the
-        ///       upper-left corner of the rectangular region defined by this <see cref='System.Drawing.RectangleF'/>.
-        ///    </para>
+        /// Gets or sets the y-coordinate of the upper-left corner of the rectangular region defined by this
+        /// <see cref='System.Drawing.RectangleF'/>.
         /// </summary>
         public float Y
         {
-            get { return y; }
-            set { y = value; }
+            get => y;
+            set => y = value;
         }
 
         /// <summary>
-        ///    <para>
-        ///       Gets or sets the width of the rectangular
-        ///       region defined by this <see cref='System.Drawing.RectangleF'/>.
-        ///    </para>
+        /// Gets or sets the width of the rectangular region defined by this <see cref='System.Drawing.RectangleF'/>.
         /// </summary>
         public float Width
         {
-            get { return width; }
-            set { width = value; }
+            get => width;
+            set => width = value;
         }
 
         /// <summary>
-        ///    <para>
-        ///       Gets or sets the height of the
-        ///       rectangular region defined by this <see cref='System.Drawing.RectangleF'/>.
-        ///    </para>
+        /// Gets or sets the height of the rectangular region defined by this <see cref='System.Drawing.RectangleF'/>.
         /// </summary>
         public float Height
         {
-            get { return height; }
-            set { height = value; }
+            get => height;
+            set => height = value;
         }
 
         /// <summary>
-        ///    <para>
-        ///       Gets the x-coordinate of the upper-left corner of the
-        ///       rectangular region defined by this <see cref='System.Drawing.RectangleF'/> .
-        ///    </para>
+        /// Gets the x-coordinate of the upper-left corner of the rectangular region defined by this
+        /// <see cref='System.Drawing.RectangleF'/> .
         /// </summary>
         [Browsable(false)]
         public float Left => X;
 
         /// <summary>
-        ///    <para>
-        ///       Gets the y-coordinate of the upper-left corner of the
-        ///       rectangular region defined by this <see cref='System.Drawing.RectangleF'/>.
-        ///    </para>
+        /// Gets the y-coordinate of the upper-left corner of the rectangular region defined by this
+        /// <see cref='System.Drawing.RectangleF'/>.
         /// </summary>
         [Browsable(false)]
         public float Top => Y;
 
         /// <summary>
-        ///    <para>
-        ///       Gets the x-coordinate of the lower-right corner of the
-        ///       rectangular region defined by this <see cref='System.Drawing.RectangleF'/>.
-        ///    </para>
+        /// Gets the x-coordinate of the lower-right corner of the rectangular region defined by this
+        /// <see cref='System.Drawing.RectangleF'/>.
         /// </summary>
         [Browsable(false)]
         public float Right => X + Width;
 
         /// <summary>
-        ///    <para>
-        ///       Gets the y-coordinate of the lower-right corner of the
-        ///       rectangular region defined by this <see cref='System.Drawing.RectangleF'/>.
-        ///    </para>
+        /// Gets the y-coordinate of the lower-right corner of the rectangular region defined by this
+        /// <see cref='System.Drawing.RectangleF'/>.
         /// </summary>
         [Browsable(false)]
         public float Bottom => Y + Height;
 
         /// <summary>
-        ///    <para>
-        ///       Tests whether this <see cref='System.Drawing.RectangleF'/> has a <see cref='System.Drawing.RectangleF.Width'/> or a <see cref='System.Drawing.RectangleF.Height'/> of 0.
-        ///    </para>
+        /// Tests whether this <see cref='System.Drawing.RectangleF'/> has a <see cref='System.Drawing.RectangleF.Width'/> or a <see cref='System.Drawing.RectangleF.Height'/> of 0.
         /// </summary>
         [Browsable(false)]
         public bool IsEmpty => (Width <= 0) || (Height <= 0);
 
         /// <summary>
-        ///    <para>
-        ///       Tests whether <paramref name="obj"/> is a <see cref='System.Drawing.RectangleF'/> with the same location and size of this
-        ///    <see cref='System.Drawing.RectangleF'/>.
-        ///    </para>
+        /// Tests whether <paramref name="obj"/> is a <see cref='System.Drawing.RectangleF'/> with the same location and
+        /// size of this <see cref='System.Drawing.RectangleF'/>.
         /// </summary>
         public override bool Equals(object obj) => obj is RectangleF && Equals((RectangleF)obj);
 
         public bool Equals(RectangleF other) => this == other;
 
         /// <summary>
-        ///    <para>
-        ///       Tests whether two <see cref='System.Drawing.RectangleF'/>
-        ///       objects have equal location and size.
-        ///    </para>
+        /// Tests whether two <see cref='System.Drawing.RectangleF'/> objects have equal location and size.
         /// </summary>
         public static bool operator ==(RectangleF left, RectangleF right) =>
             left.X == right.X && left.Y == right.Y && left.Width == right.Width && left.Height == right.Height;
 
         /// <summary>
-        ///    <para>
-        ///       Tests whether two <see cref='System.Drawing.RectangleF'/>
-        ///       objects differ in location or size.
-        ///    </para>
+        /// Tests whether two <see cref='System.Drawing.RectangleF'/> objects differ in location or size.
         /// </summary>
         public static bool operator !=(RectangleF left, RectangleF right) => !(left == right);
 
         /// <summary>
-        ///    <para>
-        ///       Determines if the specified point is contained within the
-        ///       rectangular region defined by this <see cref='System.Drawing.Rectangle'/> .
-        ///    </para>
+        /// Determines if the specified point is contained within the rectangular region defined by this
+        /// <see cref='System.Drawing.Rectangle'/> .
         /// </summary>
         public bool Contains(float x, float y) => X <= x && x < X + Width && Y <= y && y < Y + Height;
 
         /// <summary>
-        ///    <para>
-        ///       Determines if the specified point is contained within the
-        ///       rectangular region defined by this <see cref='System.Drawing.Rectangle'/> .
-        ///    </para>
+        /// Determines if the specified point is contained within the rectangular region defined by this
+        /// <see cref='System.Drawing.Rectangle'/> .
         /// </summary>
         public bool Contains(PointF pt) => Contains(pt.X, pt.Y);
 
         /// <summary>
-        ///    <para>
-        ///       Determines if the rectangular region represented by
-        ///    <paramref name="rect"/> is entirely contained within the rectangular region represented by
-        ///       this <see cref='System.Drawing.Rectangle'/> .
-        ///    </para>
+        /// Determines if the rectangular region represented by <paramref name="rect"/> is entirely contained within
+        /// the rectangular region represented by this <see cref='System.Drawing.Rectangle'/> .
         /// </summary>
         public bool Contains(RectangleF rect) =>
             (X <= rect.X) && (rect.X + rect.Width <= X + Width) && (Y <= rect.Y) && (rect.Y + rect.Height <= Y + Height);
 
         /// <summary>
-        ///    Gets the hash code for this <see cref='System.Drawing.RectangleF'/>.
+        /// Gets the hash code for this <see cref='System.Drawing.RectangleF'/>.
         /// </summary>
-        public override int GetHashCode() =>
-            HashHelpers.Combine(
-                HashHelpers.Combine(HashHelpers.Combine(X.GetHashCode(), Y.GetHashCode()), Width.GetHashCode()),
-                Height.GetHashCode());
+        public override int GetHashCode() => HashCode.Combine(X, Y, Width, Height);
 
         /// <summary>
-        ///    <para>
-        ///       Inflates this <see cref='System.Drawing.Rectangle'/>
-        ///       by the specified amount.
-        ///    </para>
+        /// Inflates this <see cref='System.Drawing.Rectangle'/> by the specified amount.
         /// </summary>
         public void Inflate(float x, float y)
         {
@@ -266,15 +210,12 @@ namespace System.Drawing
         }
 
         /// <summary>
-        ///    Inflates this <see cref='System.Drawing.Rectangle'/> by the specified amount.
+        /// Inflates this <see cref='System.Drawing.Rectangle'/> by the specified amount.
         /// </summary>
         public void Inflate(SizeF size) => Inflate(size.Width, size.Height);
 
         /// <summary>
-        ///    <para>
-        ///       Creates a <see cref='System.Drawing.Rectangle'/>
-        ///       that is inflated by the specified amount.
-        ///    </para>
+        /// Creates a <see cref='System.Drawing.Rectangle'/> that is inflated by the specified amount.
         /// </summary>
         public static RectangleF Inflate(RectangleF rect, float x, float y)
         {
@@ -283,7 +224,8 @@ namespace System.Drawing
             return r;
         }
 
-        /// <summary> Creates a Rectangle that represents the intersection between this Rectangle and rect.
+        /// <summary>
+        /// Creates a Rectangle that represents the intersection between this Rectangle and rect.
         /// </summary>
         public void Intersect(RectangleF rect)
         {
@@ -296,8 +238,8 @@ namespace System.Drawing
         }
 
         /// <summary>
-        ///    Creates a rectangle that represents the intersection between a and
-        ///    b. If there is no intersection, null is returned.
+        /// Creates a rectangle that represents the intersection between a and b. If there is no intersection, an
+        /// empty rectangle is returned.
         /// </summary>
         public static RectangleF Intersect(RectangleF a, RectangleF b)
         {
@@ -315,14 +257,13 @@ namespace System.Drawing
         }
 
         /// <summary>
-        ///    Determines if this rectangle intersects with rect.
+        /// Determines if this rectangle intersects with rect.
         /// </summary>
         public bool IntersectsWith(RectangleF rect) =>
             (rect.X < X + Width) && (X < rect.X + rect.Width) && (rect.Y < Y + Height) && (Y < rect.Y + rect.Height);
 
         /// <summary>
-        ///    Creates a rectangle that represents the union between a and
-        ///    b.
+        /// Creates a rectangle that represents the union between a and b.
         /// </summary>
         public static RectangleF Union(RectangleF a, RectangleF b)
         {
@@ -335,12 +276,12 @@ namespace System.Drawing
         }
 
         /// <summary>
-        ///    Adjusts the location of this rectangle by the specified amount.
+        /// Adjusts the location of this rectangle by the specified amount.
         /// </summary>
         public void Offset(PointF pos) => Offset(pos.X, pos.Y);
 
         /// <summary>
-        ///    Adjusts the location of this rectangle by the specified amount.
+        /// Adjusts the location of this rectangle by the specified amount.
         /// </summary>
         public void Offset(float x, float y)
         {
@@ -349,14 +290,14 @@ namespace System.Drawing
         }
 
         /// <summary>
-        ///    Converts the specified <see cref='System.Drawing.Rectangle'/> to a
+        /// Converts the specified <see cref='System.Drawing.Rectangle'/> to a
         /// <see cref='System.Drawing.RectangleF'/>.
         /// </summary>
         public static implicit operator RectangleF(Rectangle r) => new RectangleF(r.X, r.Y, r.Width, r.Height);
 
         /// <summary>
-        ///    Converts the <see cref='System.Drawing.RectangleF.Location'/> and <see cref='System.Drawing.RectangleF.Size'/> of this <see cref='System.Drawing.RectangleF'/> to a
-        ///    human-readable string.
+        /// Converts the <see cref='System.Drawing.RectangleF.Location'/> and <see cref='System.Drawing.RectangleF.Size'/>
+        /// of this <see cref='System.Drawing.RectangleF'/> to a human-readable string.
         /// </summary>
         public override string ToString() =>
             "{X=" + X.ToString() + ",Y=" + Y.ToString() +
--- src/System.Drawing.Primitives/src/System/Drawing/Size.cs
+++ src/System.Drawing.Primitives/src/System/Drawing/Size.cs
@@ -7,33 +7,24 @@ using System.Numerics.Hashing;
 
 namespace System.Drawing
 {
-    /**
-     * Represents a dimension in 2D coordinate space
-     */
     /// <summary>
-    ///    Represents the size of a rectangular region
-    ///    with an ordered pair of width and height.
+    /// Represents the size of a rectangular region with an ordered pair of width and height.
     /// </summary>
     [Serializable]
     [System.Runtime.CompilerServices.TypeForwardedFrom("System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
     public struct Size : IEquatable<Size>
     {
         /// <summary>
-        ///    Initializes a new instance of the <see cref='System.Drawing.Size'/> class.
+        /// Initializes a new instance of the <see cref='System.Drawing.Size'/> class.
         /// </summary>
         public static readonly Size Empty = new Size();
 
         private int width; // Do not rename (binary serialization)
         private int height; // Do not rename (binary serialization)
 
-        /**
-         * Create a new Size object from a point
-         */
         /// <summary>
-        ///    <para>
-        ///       Initializes a new instance of the <see cref='System.Drawing.Size'/> class from
-        ///       the specified <see cref='System.Drawing.Point'/>.
-        ///    </para>
+        /// Initializes a new instance of the <see cref='System.Drawing.Size'/> class from the specified
+        /// <see cref='System.Drawing.Point'/>.
         /// </summary>
         public Size(Point pt)
         {
@@ -41,12 +32,8 @@ namespace System.Drawing
             height = pt.Y;
         }
 
-        /**
-         * Create a new Size object of the specified dimension
-         */
         /// <summary>
-        ///    Initializes a new instance of the <see cref='System.Drawing.Size'/> class from
-        ///    the specified dimensions.
+        /// Initializes a new instance of the <see cref='System.Drawing.Size'/> class from the specified dimensions.
         /// </summary>
         public Size(int width, int height)
         {
@@ -55,22 +42,17 @@ namespace System.Drawing
         }
 
         /// <summary>
-        ///    Converts the specified <see cref='System.Drawing.Size'/> to a
-        /// <see cref='System.Drawing.SizeF'/>.
+        /// Converts the specified <see cref='System.Drawing.Size'/> to a <see cref='System.Drawing.SizeF'/>.
         /// </summary>
         public static implicit operator SizeF(Size p) => new SizeF(p.Width, p.Height);
 
         /// <summary>
-        ///    <para>
-        ///       Performs vector addition of two <see cref='System.Drawing.Size'/> objects.
-        ///    </para>
+        /// Performs vector addition of two <see cref='System.Drawing.Size'/> objects.
         /// </summary>
         public static Size operator +(Size sz1, Size sz2) => Add(sz1, sz2);
 
         /// <summary>
-        ///    <para>
-        ///       Contracts a <see cref='System.Drawing.Size'/> by another <see cref='System.Drawing.Size'/>
-        ///    </para>
+        /// Contracts a <see cref='System.Drawing.Size'/> by another <see cref='System.Drawing.Size'/>
         /// </summary>
         public static Size operator -(Size sz1, Size sz2) => Subtract(sz1, sz2);
 
@@ -124,120 +106,88 @@ namespace System.Drawing
             => new SizeF(left.width / right, left.height / right);
 
         /// <summary>
-        ///    Tests whether two <see cref='System.Drawing.Size'/> objects
-        ///    are identical.
+        /// Tests whether two <see cref='System.Drawing.Size'/> objects are identical.
         /// </summary>
         public static bool operator ==(Size sz1, Size sz2) => sz1.Width == sz2.Width && sz1.Height == sz2.Height;
 
         /// <summary>
-        ///    <para>
-        ///       Tests whether two <see cref='System.Drawing.Size'/> objects are different.
-        ///    </para>
+        /// Tests whether two <see cref='System.Drawing.Size'/> objects are different.
         /// </summary>
         public static bool operator !=(Size sz1, Size sz2) => !(sz1 == sz2);
 
         /// <summary>
-        ///    Converts the specified <see cref='System.Drawing.Size'/> to a
-        /// <see cref='System.Drawing.Point'/>.
+        /// Converts the specified <see cref='System.Drawing.Size'/> to a <see cref='System.Drawing.Point'/>.
         /// </summary>
         public static explicit operator Point(Size size) => new Point(size.Width, size.Height);
 
         /// <summary>
-        ///    Tests whether this <see cref='System.Drawing.Size'/> has zero
-        ///    width and height.
+        /// Tests whether this <see cref='System.Drawing.Size'/> has zero width and height.
         /// </summary>
         [Browsable(false)]
         public bool IsEmpty => width == 0 && height == 0;
 
-        /**
-         * Horizontal dimension
-         */
-
         /// <summary>
-        ///    <para>
-        ///       Represents the horizontal component of this
-        ///    <see cref='System.Drawing.Size'/>.
-        ///    </para>
+        /// Represents the horizontal component of this <see cref='System.Drawing.Size'/>.
         /// </summary>
         public int Width
         {
-            get { return width; }
-            set { width = value; }
+            get => width;
+            set => width = value;
         }
 
-        /**
-         * Vertical dimension
-         */
-
         /// <summary>
-        ///    Represents the vertical component of this
-        /// <see cref='System.Drawing.Size'/>.
+        /// Represents the vertical component of this <see cref='System.Drawing.Size'/>.
         /// </summary>
         public int Height
         {
-            get { return height; }
-            set { height = value; }
+            get => height;
+            set => height = value;
         }
 
         /// <summary>
-        ///    <para>
-        ///       Performs vector addition of two <see cref='System.Drawing.Size'/> objects.
-        ///    </para>
+        /// Performs vector addition of two <see cref='System.Drawing.Size'/> objects.
         /// </summary>
         public static Size Add(Size sz1, Size sz2) =>
             new Size(unchecked(sz1.Width + sz2.Width), unchecked(sz1.Height + sz2.Height));
 
         /// <summary>
-        ///   Converts a SizeF to a Size by performing a ceiling operation on
-        ///   all the coordinates.
+        /// Converts a SizeF to a Size by performing a ceiling operation on all the coordinates.
         /// </summary>
         public static Size Ceiling(SizeF value) =>
             new Size(unchecked((int)Math.Ceiling(value.Width)), unchecked((int)Math.Ceiling(value.Height)));
 
         /// <summary>
-        ///    <para>
-        ///       Contracts a <see cref='System.Drawing.Size'/> by another <see cref='System.Drawing.Size'/> .
-        ///    </para>
+        /// Contracts a <see cref='System.Drawing.Size'/> by another <see cref='System.Drawing.Size'/> .
         /// </summary>
         public static Size Subtract(Size sz1, Size sz2) =>
             new Size(unchecked(sz1.Width - sz2.Width), unchecked(sz1.Height - sz2.Height));
 
         /// <summary>
-        ///   Converts a SizeF to a Size by performing a truncate operation on
-        ///   all the coordinates.
+        /// Converts a SizeF to a Size by performing a truncate operation on all the coordinates.
         /// </summary>
         public static Size Truncate(SizeF value) => new Size(unchecked((int)value.Width), unchecked((int)value.Height));
 
         /// <summary>
-        ///   Converts a SizeF to a Size by performing a round operation on
-        ///   all the coordinates.
+        /// Converts a SizeF to a Size by performing a round operation on all the coordinates.
         /// </summary>
         public static Size Round(SizeF value) =>
             new Size(unchecked((int)Math.Round(value.Width)), unchecked((int)Math.Round(value.Height)));
 
         /// <summary>
-        ///    <para>
-        ///       Tests to see whether the specified object is a
-        ///    <see cref='System.Drawing.Size'/>
-        ///    with the same dimensions as this <see cref='System.Drawing.Size'/>.
-        /// </para>
+        /// Tests to see whether the specified object is a <see cref='System.Drawing.Size'/>  with the same dimensions
+        /// as this <see cref='System.Drawing.Size'/>.
         /// </summary>
         public override bool Equals(object obj) => obj is Size && Equals((Size)obj);
 
         public bool Equals(Size other) => this == other;
 
         /// <summary>
-        ///    <para>
-        ///       Returns a hash code.
-        ///    </para>
+        /// Returns a hash code.
         /// </summary>
-        public override int GetHashCode() => HashHelpers.Combine(Width, Height);
+        public override int GetHashCode() => HashCode.Combine(Width, Height);
 
         /// <summary>
-        ///    <para>
-        ///       Creates a human-readable string that represents this
-        ///    <see cref='System.Drawing.Size'/>.
-        ///    </para>
+        /// Creates a human-readable string that represents this <see cref='System.Drawing.Size'/>.
         /// </summary>
         public override string ToString() => "{Width=" + width.ToString() + ", Height=" + height.ToString() + "}";
 
