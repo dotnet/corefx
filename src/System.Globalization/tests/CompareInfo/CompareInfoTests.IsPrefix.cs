c929ba0be3ebb70b3bfa0a84af388626fd494a2e
Adam Sitnik <adam.sitnik@gmail.com>
2019-09-11 09:24:08 +0200

    add InvariantCulture.CompareInfo.IsPrefix("o\u0308", "o") edge case

--- src/System.Globalization/tests/CompareInfo/CompareInfoTests.IsPrefix.cs
+++ src/System.Globalization/tests/CompareInfo/CompareInfoTests.IsPrefix.cs
@@ -51,6 +51,7 @@ namespace System.Globalization.Tests
             yield return new object[] { s_invariantCompare, "\u00C0nimal", "a\u0300", CompareOptions.OrdinalIgnoreCase, false };
             yield return new object[] { s_invariantCompare, "FooBar", "Foo\u0400Bar", CompareOptions.Ordinal, false };
             yield return new object[] { s_invariantCompare, "FooBA\u0300R", "FooB\u00C0R", CompareOptions.IgnoreNonSpace, true };
+            yield return new object[] { s_invariantCompare, "o\u0308", "o", CompareOptions.None, false };
 
             // Ignore symbols
             yield return new object[] { s_invariantCompare, "Test's can be interesting", "Tests", CompareOptions.IgnoreSymbols, true };
