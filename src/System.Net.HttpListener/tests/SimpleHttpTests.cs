e48fc39b94a96fcf357085b39cb2ad9d0ab592a7
Cory Nelson <conelso@microsoft.com>
2019-09-18 21:51:02 -0700

    re-enable tests.

--- src/System.Net.HttpListener/tests/SimpleHttpTests.cs
+++ src/System.Net.HttpListener/tests/SimpleHttpTests.cs
@@ -168,7 +168,6 @@ namespace System.Net.Tests
         }
 
         [Fact]
-        [ActiveIssue(39552)]
         public void ListenerRestart_BeginGetContext_Success()
         {
             using (HttpListenerFactory factory = new HttpListenerFactory())
@@ -182,7 +181,6 @@ namespace System.Net.Tests
         }
 
         [ConditionalFact]
-        [ActiveIssue(39552)]
         public async Task ListenerRestart_GetContext_Success()
         {
             const string Content = "ListenerRestart_GetContext_Success";
