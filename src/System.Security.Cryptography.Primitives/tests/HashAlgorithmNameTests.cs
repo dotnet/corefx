80aca86f5163ca638a12647578d926d21937a285
Kevin Jones <kevin@vcsjones.com>
2019-09-20 15:03:25 -0400

    Implement FromOid and TryFromOid.

---
+++ src/System.Security.Cryptography.Primitives/tests/HashAlgorithmNameTests.cs
@@ -0,0 +1,59 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+using Xunit;
+
+namespace System.Security.Cryptography.Primitives.Tests
+{
+    public static class HashAlgorithmNameTests
+    {
+        [Theory]
+        [MemberData(nameof(ValidInputs))]
+        public static void FromOid_ValidInput(string oid, HashAlgorithmName expected)
+        {
+            Assert.Equal(expected, HashAlgorithmName.FromOid(oid));
+        }
+
+        [Fact]
+        public static void TryFromOid_ThrowsForNullInput()
+        {
+            Assert.Throws<ArgumentNullException>(() => HashAlgorithmName.FromOid(null));
+        }
+
+        [Fact]
+        public static void TryFromOid_ThrowsForInvalidInput()
+        {
+            Assert.Throws<CryptographicException>(() => HashAlgorithmName.FromOid("1.2.3.4"));
+        }
+
+        [Theory]
+        [MemberData(nameof(ValidInputs))]
+        public static void TryFromOid_ValidInput(string oid, HashAlgorithmName expected)
+        {
+            Assert.True(HashAlgorithmName.TryFromOid(oid, out HashAlgorithmName actual));
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("1.2.3.4")]
+        public static void TryFromOid_ReturnsFalseForInvalidInput(string oidValue)
+        {
+            Assert.False(HashAlgorithmName.TryFromOid(oidValue, out _));
+        }
+
+        public static IEnumerable<object[]> ValidInputs
+        {
+            get
+            {
+                yield return new object[] { "1.2.840.113549.2.5", HashAlgorithmName.MD5 };
+                yield return new object[] { "1.3.14.3.2.26", HashAlgorithmName.SHA1 };
+                yield return new object[] { "2.16.840.1.101.3.4.2.1", HashAlgorithmName.SHA256 };
+                yield return new object[] { "2.16.840.1.101.3.4.2.2", HashAlgorithmName.SHA384 };
+                yield return new object[] { "2.16.840.1.101.3.4.2.3", HashAlgorithmName.SHA512 };
+            }
+        }
+    }
+}
