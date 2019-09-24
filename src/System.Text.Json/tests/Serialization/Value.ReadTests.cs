26d64f0e6438b85d677e3cf3afd5ae8aec565344
Steve Harter <steveharter@users.noreply.github.com>
2019-09-24 14:44:46 -0500

    Netfx build issue

--- src/System.Text.Json/tests/Serialization/Value.ReadTests.cs
+++ src/System.Text.Json/tests/Serialization/Value.ReadTests.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Globalization;
+using System.Runtime.CompilerServices;
 using Xunit;
 
 namespace System.Text.Json.Serialization.Tests
