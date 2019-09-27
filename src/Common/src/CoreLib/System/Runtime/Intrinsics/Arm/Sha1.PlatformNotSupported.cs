3da4a87fdaf0ac337f2613e05a5eea2b6d4b9e35
Tanner Gooding <tagoo@outlook.com>
2019-09-27 05:06:24 -0700

    Refactoring the ARM Hardware Intrinsics based on the latest design decisions. (#26895)
    * Moving the Arm64 intrinsic files into the Arm folder.
    
    * Refactoring the ARM Hardware Intrinsics based on the new design.
    
    * Adding support for AdvSimd.Add
    
    * Adding LoadVector64 and LoadVector128 APIs to Arm.AdvSimd
    
    * Marking the LoadVector64 and LoadVector128 methods as unsafe
    
    * Removing the ARM64 HWIntrinsic tests
    
    * Renaming Arm.Base to Arm.ArmBase, as per the design
    
    * Fixing Base.cs to ArmBase.cs in the shared projitems
    
    * Fixing the doc comments for the AdvSimd.LoadVector methods
    
    * Marking various IsSupported methods as new, now that they inherit from ArmBase
    
    * Fixing a malformed doc comment that was missed
    
    * Marking AdvSimd.Arm64 as [Intrinsic] and new
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

---
+++ src/Common/src/CoreLib/System/Runtime/Intrinsics/Arm/Sha1.PlatformNotSupported.cs
@@ -0,0 +1,62 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#pragma warning disable IDE0060 // unused parameters
+using System.Runtime.CompilerServices;
+
+namespace System.Runtime.Intrinsics.Arm
+{
+    /// <summary>
+    /// This class provides access to the ARM SHA1 hardware instructions via intrinsics
+    /// </summary>
+    [CLSCompliant(false)]
+    public abstract class Sha1 : ArmBase
+    {
+        internal Sha1() { }
+
+        public static new bool IsSupported { [Intrinsic] get => false; }
+
+        /// <summary>
+        /// uint32_t vsha1h_u32 (uint32_t hash_e)
+        ///   A32: SHA1H.32 Qd, Qm
+        ///   A64: SHA1H Sd, Sn
+        /// </summary>
+        public static uint FixedRotate(uint hash_e) { throw new PlatformNotSupportedException(); }
+
+        /// <summary>
+        /// uint32x4_t vsha1cq_u32 (uint32x4_t hash_abcd, uint32_t hash_e, uint32x4_t wk)
+        ///   A32: SHA1C.32 Qd, Qn, Qm
+        ///   A64: SHA1C Qd, Sn, Vm.4S
+        /// </summary>
+        public static Vector128<uint> HashUpdateChoose(Vector128<uint> hash_abcd, uint hash_e, Vector128<uint> wk) { throw new PlatformNotSupportedException(); }
+
+        /// <summary>
+        /// uint32x4_t vsha1mq_u32 (uint32x4_t hash_abcd, uint32_t hash_e, uint32x4_t wk)
+        ///   A32: SHA1M.32 Qd, Qn, Qm
+        ///   A64: SHA1M Qd, Sn, Vm.4S
+        /// </summary>
+        public static Vector128<uint> HashUpdateMajority(Vector128<uint> hash_abcd, uint hash_e, Vector128<uint> wk) { throw new PlatformNotSupportedException(); }
+
+        /// <summary>
+        /// uint32x4_t vsha1pq_u32 (uint32x4_t hash_abcd, uint32_t hash_e, uint32x4_t wk)
+        ///   A32: SHA1P.32 Qd, Qn, Qm
+        ///   A64: SHA1P Qd, Sn, Vm.4S
+        /// </summary>
+        public static Vector128<uint> HashUpdateParity(Vector128<uint> hash_abcd, uint hash_e, Vector128<uint> wk) { throw new PlatformNotSupportedException(); }
+
+        /// <summary>
+        /// uint32x4_t vsha1su0q_u32 (uint32x4_t w0_3, uint32x4_t w4_7, uint32x4_t w8_11)
+        ///   A32: SHA1SU0.32 Qd, Qn, Qm
+        ///   A64: SHA1SU0 Vd.4S, Vn.4S, Vm.4S
+        /// </summary>
+        public static Vector128<uint> ScheduleUpdate0(Vector128<uint> w0_3, Vector128<uint> w4_7, Vector128<uint> w8_11) { throw new PlatformNotSupportedException(); }
+
+        /// <summary>
+        /// uint32x4_t vsha1su1q_u32 (uint32x4_t tw0_3, uint32x4_t w12_15)
+        ///   A32: SHA1SU1.32 Qd, Qm
+        ///   A64: SHA1SU1 Vd.4S, Vn.4S
+        /// </summary>
+        public static Vector128<uint> ScheduleUpdate1(Vector128<uint> tw0_3, Vector128<uint> w12_15) { throw new PlatformNotSupportedException(); }
+    }
+}
