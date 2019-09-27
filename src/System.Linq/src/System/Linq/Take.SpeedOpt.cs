e1f7ff7953684ed55ee7a2fb9143e9397e251432
TimAndy <xuchonglei@126.com>
2019-09-27 13:46:35 +0800

    Revert "Convert to conditional expression"
    This reverts commit 7c7908fb

--- src/System.Linq/src/System/Linq/Take.SpeedOpt.cs
+++ src/System.Linq/src/System/Linq/Take.SpeedOpt.cs
@@ -31,9 +31,14 @@ namespace System.Linq
             {
                 int sourceCount = sourceList.Count;
 
-                return sourceCount > count
-                    ? new ListPartition<TSource>(sourceList, sourceCount - count, sourceCount)
-                    : new ListPartition<TSource>(sourceList, 0, sourceCount);
+                if (sourceCount > count)
+                {
+                    return new ListPartition<TSource>(sourceList, sourceCount - count, sourceCount);
+                }
+                else
+                {
+                    return new ListPartition<TSource>(sourceList, 0, sourceCount);
+                }
             }
 
             return TakeLastIterator<TSource>(source, count);
