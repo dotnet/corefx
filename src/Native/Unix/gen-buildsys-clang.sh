f66aeba93309d3c4e448ce3e7f0e01a2c21adca8
Zoltan Varga <vargaz@gmail.com>
2019-10-09 14:23:10 -0400

    Add support for wasm to gen-buildsys-clang.sh

--- src/Native/Unix/gen-buildsys-clang.sh
+++ src/Native/Unix/gen-buildsys-clang.sh
@@ -44,6 +44,7 @@ else
   buildtype="$5"
 fi
 
+cmake_cmd=cmake
 cmake_extra_defines="-DCMAKE_BUILD_TYPE=$buildtype"
 if [[ -n "$CROSSCOMPILE" ]]; then
     if ! [[ -n "$ROOTFS_DIR" ]]; then
@@ -60,6 +61,11 @@ fi
 if [ "$build_arch" == "armel" ]; then
     cmake_extra_defines="$cmake_extra_defines -DARM_SOFTFP=1"
 fi
+if [ $"build_arch" == "wasm" ] then
+   source $(BUILD_BINARIESDIRECTORY)/emsdk/emsdk_env.sh
+   cmake_cmd="emcmake cmake"
+   cmake_extra_defines="$cmake_extra_defines -DCMAKE_TOOLCHAIN_FILE=$(BUILD_BINARIESDIRECTORY)/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake -DEMSCRIPTEN_GENERATE_BITCODE_STATIC_LIBRARIES=1"
+fi
 
 __UnprocessedCMakeArgs=""
 if [ -z "$6" ]; then
@@ -68,6 +74,6 @@ else
     __UnprocessedCMakeArgs="$6"
 fi
 
-cmake $cmake_extra_defines \
+$cmake_cmd $cmake_extra_defines \
     $__UnprocessedCMakeArgs \
     $1
