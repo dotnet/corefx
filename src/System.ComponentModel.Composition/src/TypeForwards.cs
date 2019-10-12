a72fa5c3955336f62f8be4a6447aa36f0217cf6c
Stephen Toub <stoub@microsoft.com>
2019-10-12 12:23:00 -0400

    Update TypeForwards.cs

--- src/System.ComponentModel.Composition/src/TypeForwards.cs
+++ src/System.ComponentModel.Composition/src/TypeForwards.cs
@@ -2,4 +2,5 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+// Test
 [assembly: System.Runtime.CompilerServices.TypeForwardedTo(destination: typeof(System.Lazy<,>))]
