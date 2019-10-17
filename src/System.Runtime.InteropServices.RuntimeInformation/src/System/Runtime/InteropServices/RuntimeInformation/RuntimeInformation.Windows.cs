fd6714a359ff68127e25f1aecbfbdf4be09524a7
Stephen Toub <stoub@microsoft.com>
2019-10-17 13:23:20 -0400

    Annotate System.Runtime.InteropServices.RuntimeInformation for nullable reference types (#41856)

--- src/System.Runtime.InteropServices.RuntimeInformation/src/System/Runtime/InteropServices/RuntimeInformation/RuntimeInformation.Windows.cs
+++ src/System.Runtime.InteropServices.RuntimeInformation/src/System/Runtime/InteropServices/RuntimeInformation/RuntimeInformation.Windows.cs
@@ -8,7 +8,7 @@ namespace System.Runtime.InteropServices
 {
     public static partial class RuntimeInformation
     {
-        private static string s_osDescription = null;
+        private static string? s_osDescription = null;
         private static readonly object s_osLock = new object();
         private static readonly object s_processLock = new object();
         private static Architecture? s_osArch = null;
@@ -19,18 +19,7 @@ namespace System.Runtime.InteropServices
             return OSPlatform.Windows == osPlatform;
         }
 
-        public static string OSDescription
-        {
-            get
-            {
-                if (null == s_osDescription)
-                {
-                    s_osDescription = Interop.NtDll.RtlGetVersion();
-                }
-
-                return s_osDescription;
-            }
-        }
+        public static string OSDescription => s_osDescription ??= Interop.NtDll.RtlGetVersion();
 
         public static Architecture OSArchitecture
         {
