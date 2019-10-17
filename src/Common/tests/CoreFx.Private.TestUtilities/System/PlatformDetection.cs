e881f63f189a0af216a86b1dd46fe8e794e153fe
Viktor Hofer <viktor.hofer@microsoft.com>
2019-10-17 11:03:10 +0200

    Fix Xml UI tests using statements

--- src/Common/tests/CoreFx.Private.TestUtilities/System/PlatformDetection.cs
+++ src/Common/tests/CoreFx.Private.TestUtilities/System/PlatformDetection.cs
@@ -2,14 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Diagnostics;
 using System.IO;
 using System.Security;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Runtime.CompilerServices;
 using Microsoft.Win32;
-using Xunit;
 
 namespace System
 {
