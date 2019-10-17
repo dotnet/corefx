3174a68838cea61e137c8c4f61134401bcf47b6b
carlossanlop <calope@microsoft.com>
2019-10-17 12:35:06 -0700

    Initial commit

---
+++ src/System.Threading.AccessControl/src/System/Threading/EventWaitHandleAcl.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Security.AccessControl;
+
+namespace System.Threading.AccessControl.System.Threading
+{
+    public static class EventWaitHandleAcl
+    {
+        public static EventWaitHandle Create(
+            bool initialState,
+            EventResetMode mode,
+            string name,
+            out bool createdNew,
+            EventWaitHandleSecurity eventSecurity)
+        {
+            throw null;
+        }
+    }
+}
---
+++ src/System.Threading.AccessControl/src/System/Threading/MutexAcl.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Security.AccessControl;
+
+namespace System.Threading.AccessControl.System.Threading
+{
+    public static class MutexAcl
+    {
+        public static Mutex Create(
+            bool initiallyOwned,
+            string name,
+            out bool createdNew,
+            MutexSecurity mutexSecurity)
+        {
+            throw null;
+        }
+    }
+}
