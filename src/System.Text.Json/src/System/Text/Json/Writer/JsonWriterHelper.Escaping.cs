9cba98a0cfcc536d353fa6bfad5b8c46768d4609
Günther Foidl <gue@korporal.at>
2019-10-21 15:10:26 +0200

    Added comments for the bit-mask

--- src/System.Text.Json/src/System/Text/Json/Writer/JsonWriterHelper.Escaping.cs
+++ src/System.Text.Json/src/System/Text/Json/Writer/JsonWriterHelper.Escaping.cs
@@ -97,41 +97,76 @@ namespace System.Text.Json
         private static readonly Vector128<sbyte> s_bitPosLookup = Unsafe.ReadUnaligned<Vector128<sbyte>>(ref MemoryMarshal.GetReference(BitPosLookup));
         private static readonly Vector128<sbyte> s_zero128 = Vector128<sbyte>.Zero;
 
-        // for each lower nibble a bitmask for the higher nibble
-        // only the 0..7 (one byte) is considered here
-        // 1 -> needs escaping
+        // See comment below in method CreateEscapingMaskSsse3 for description of the bit-mask.
         private static ReadOnlySpan<byte> Bitmask => new byte[16]
         {
-            0b_01000011,        // lower nibble 0
-            0b_00000011,        // lower nibble 1
-            0b_00000111,        // lower nibble 2
-            0b_00000011,        // lower nibble 3
-            0b_00000011,        // lower nibble 4
-            0b_00000111,        // lower nibble 5
-            0b_00000111,        // lower nibble 6
-            0b_00000111,        // lower nibble 7
-            0b_00000011,        // lower nibble 8
-            0b_00000011,        // lower nibble 9
-            0b_00000011,        // lower nibble A
-            0b_00000111,        // lower nibble B
-            0b_00101011,        // lower nibble C
-            0b_00000011,        // lower nibble D
-            0b_00001011,        // lower nibble E
-            0b_10000011,        // lower nibble F
+            0b_01000011,        // low-nibble 0
+            0b_00000011,        // low-nibble 1
+            0b_00000111,        // low-nibble 2
+            0b_00000011,        // low-nibble 3
+            0b_00000011,        // low-nibble 4
+            0b_00000111,        // low-nibble 5
+            0b_00000111,        // low-nibble 6
+            0b_00000111,        // low-nibble 7
+            0b_00000011,        // low-nibble 8
+            0b_00000011,        // low-nibble 9
+            0b_00000011,        // low-nibble A
+            0b_00000111,        // low-nibble B
+            0b_00101011,        // low-nibble C
+            0b_00000011,        // low-nibble D
+            0b_00001011,        // low-nibble E
+            0b_10000011,        // low-nibble F
         };
 
+        // To check if a bit in a bitmask from the Bitmask is set, in a sequential code
+        // we would do ((1 << bitIndex) & bitmask) != 0
+        // As there is no hardware instrinic for such a shift, we use lookup that
+        // stores the shifted bitpositions.
+        // So (1 << bitIndex) becomes BitPosLook[bitIndex], which is simd-friendly.
         private static ReadOnlySpan<byte> BitPosLookup => new byte[16]
         {
             0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80,
 
-            // To avoid a check for value outside of 0..7 (see above),
-            // we use a bitpos, that always results in escaping
+            // A bitmask from the Bitmask (above) is created only for value 0..7 (one byte)
+            // so to avoid a explicit check for values outside 0..7, i.e.
+            // high nibbles 8..F, we use a bitpos that always results in escaping.
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
         };
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static Vector128<sbyte> CreateEscapingMaskSsse3(Vector128<sbyte> sourceValue)
         {
+            // To check if an input byte needs to be escaped or not, we create bit-mask.
+            // Therefore we split the input byte into the low- and high-nibble, which will get
+            // the row-/column-index in the bit-mask.
+            // The bit-mask-matrix looks like
+            //                                     high-nibble
+            // low-nibble  0   1   2   3   4   5   6   7   8   9   A   B   C   D   E   F
+            //         0   1   1   0   0   0   0   1   0   1   1   1   1   1   1   1   1
+            //         1   1   1   0   0   0   0   0   0   1   1   1   1   1   1   1   1
+            //         2   1   1   1   0   0   0   0   0   1   1   1   1   1   1   1   1
+            //         3   1   1   0   0   0   0   0   0   1   1   1   1   1   1   1   1
+            //         4   1   1   0   0   0   0   0   0   1   1   1   1   1   1   1   1
+            //         5   1   1   0   0   0   0   0   0   1   1   1   1   1   1   1   1
+            //         6   1   1   1   0   0   0   0   0   1   1   1   1   1   1   1   1
+            //         7   1   1   1   0   0   0   0   0   1   1   1   1   1   1   1   1
+            //         8   1   1   0   0   0   0   0   0   1   1   1   1   1   1   1   1
+            //         9   1   1   0   0   0   0   0   0   1   1   1   1   1   1   1   1
+            //         A   1   1   0   0   0   0   0   0   1   1   1   1   1   1   1   1
+            //         B   1   1   1   0   0   0   0   0   1   1   1   1   1   1   1   1
+            //         C   1   1   0   1   0   1   0   0   1   1   1   1   1   1   1   1
+            //         D   1   1   0   0   0   0   0   0   1   1   1   1   1   1   1   1
+            //         E   1   1   0   1   0   0   0   0   1   1   1   1   1   1   1   1
+            //         F   1   1   0   0   0   0   0   1   1   1   1   1   1   1   1   1
+            //
+            // where 1 denotes the neeed for escaping, while 0 means no escaping needed.
+            // For high-nibbles in the range 8..F every input needs to be escaped, so we
+            // can omit them in the bit-mask, thus only high-nibbles in the range 0..7 need
+            // to be considered, hence the entries in the bit-mask can be of type byte.
+            //
+            // In the Bitmask (see above) for each row (= low-nibble) a bit-mask for the
+            // high-nibbles (= columns) is created.
+
             Debug.Assert(Ssse3.IsSupported);
 
             Vector128<sbyte> highNibbles = Sse2.And(Sse2.ShiftRightLogical(sourceValue.AsInt32(), 4).AsSByte(), s_mask_SByte_0xF);
