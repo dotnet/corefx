69fc7edb110a69345253060bb611deb3d4bec417
Viktor Hofer <viktor.hofer@microsoft.com>
2019-10-21 15:06:57 +0200

    Disabling TaskCanceledQuickly timing out test (#41941)

--- src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.Cancellation.cs
+++ src/System.Net.Http/tests/FunctionalTests/HttpClientHandlerTest.Cancellation.cs
@@ -128,6 +128,7 @@ namespace System.Net.Http.Functional.Tests
         }
 
         [Theory]
+        [ActiveIssue(28805)]
         [MemberData(nameof(TwoBoolsAndCancellationMode))]
         public async Task GetAsync_CancelDuringResponseBodyReceived_Buffered_TaskCanceledQuickly(bool chunkedTransfer, bool connectionClose, CancellationMode mode)
         {
