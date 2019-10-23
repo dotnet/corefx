28a5bea6877c69bf2c3d53c7bb76cd868efcbb09
Ahson Khan <ahkha@microsoft.com>
2019-10-23 01:55:00 -0700

    Add necessary using directive in tests.

--- src/System.Text.Encodings.Web/tests/JavaScriptStringEncoderTests.cs
+++ src/System.Text.Encodings.Web/tests/JavaScriptStringEncoderTests.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
 using Xunit;
