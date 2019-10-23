51a837f58b97002846aa2608d2c74e2c5dd2b6a0
Ahson Khan <ahkha@microsoft.com>
2019-10-23 13:19:10 -0700

    Fix up the using directives that got removed during merge conflict resolution.

--- src/System.Text.Encodings.Web/tests/JavaScriptStringEncoderTests.cs
+++ src/System.Text.Encodings.Web/tests/JavaScriptStringEncoderTests.cs
@@ -9,6 +9,8 @@ using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.Internal;
 using System.Text.Unicode;
 using Xunit;
 
