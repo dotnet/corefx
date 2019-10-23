599ddc9eab91dde1ac6ad9c6bddd7469b4da7694
wfurt <tweinfurt@yahoo.com>
2019-10-22 21:39:19 -0700

    remove extra comment

--- src/System.Net.Security/src/System/Net/Security/SslStream.Implementation.cs
+++ src/System.Net.Security/src/System/Net/Security/SslStream.Implementation.cs
@@ -501,7 +501,6 @@ namespace System.Net.Security
             if (message.Failed)
             {
                 StartSendAuthResetSignal(null, asyncRequest, ExceptionDispatchInfo.Capture(new AuthenticationException(SR.net_auth_SSPI, message.GetException())));
-                //throw new AuthenticationException(SR.net_auth_SSPI, message.GetException());
                 return;
             }
             else if (message.Done && !_pendingReHandshake)
