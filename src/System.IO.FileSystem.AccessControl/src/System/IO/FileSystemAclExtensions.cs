e05874e222878bc6e2dbf56817696c6b70f6d25e
carlossanlop <calope@microsoft.com>
2019-10-23 11:37:08 -0700

    Revert commits accidentally pushed to release/3.1 directly

--- src/System.IO.FileSystem.AccessControl/src/System/IO/FileSystemAclExtensions.cs
+++ src/System.IO.FileSystem.AccessControl/src/System/IO/FileSystemAclExtensions.cs
@@ -2,12 +2,13 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Security.AccessControl;
 using Microsoft.Win32.SafeHandles;
+using System.Security.AccessControl;
+using System;
 
 namespace System.IO
 {
-    public static partial class FileSystemAclExtensions
+    public static class FileSystemAclExtensions
     {
         public static DirectorySecurity GetAccessControl(this DirectoryInfo directoryInfo)
         {
