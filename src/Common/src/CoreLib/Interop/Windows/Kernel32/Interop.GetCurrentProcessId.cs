73ca64dccd0db8905e25e7c6262505ff4caa931d
Aaron Robinson <arobins@microsoft.com>
2019-10-22 22:37:24 -0700

    Apply SuppressGCAttribute to some SPCL functions. (dotnet/coreclr#27369)
    Apply SuppressGCAttribute to some SPCL functions.
    
    Only insert GC_POLL in first morph call.
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/Interop/Windows/Kernel32/Interop.GetCurrentProcessId.cs
+++ src/Common/src/CoreLib/Interop/Windows/Kernel32/Interop.GetCurrentProcessId.cs
@@ -9,6 +9,7 @@ internal static partial class Interop
     internal static partial class Kernel32
     {
         [DllImport(Libraries.Kernel32)]
+        [SuppressGCTransition]
         internal static extern uint GetCurrentProcessId();
     }
 
