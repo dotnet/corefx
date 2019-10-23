3d73b196fd8c1c4c903ceece9eb44e8350ebf792
Steve Harter <steveharter@users.noreply.github.com>
2019-10-23 16:01:53 -0500

    Cache polymorphic properties (#41753) (#42057)

--- src/System.Text.Json/src/System/Text/Json/Serialization/JsonClassInfo.AddProperty.cs
+++ src/System.Text.Json/src/System/Text/Json/Serialization/JsonClassInfo.AddProperty.cs
@@ -2,9 +2,11 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Reflection;
 using System.Text.Json.Serialization;
+using System.Threading;
 
 namespace System.Text.Json
 {
@@ -208,19 +210,31 @@ namespace System.Text.Json
                 options: options);
         }
 
-        internal JsonPropertyInfo CreatePolymorphicProperty(JsonPropertyInfo property, Type runtimePropertyType, JsonSerializerOptions options)
+        internal JsonPropertyInfo GetOrAddPolymorphicProperty(JsonPropertyInfo property, Type runtimePropertyType, JsonSerializerOptions options)
         {
-            JsonPropertyInfo runtimeProperty = CreateProperty(
-                property.DeclaredPropertyType,
-                runtimePropertyType,
-                property.ImplementedPropertyType,
-                property.PropertyInfo,
-                parentClassType: Type,
-                converter: null,
-                options: options);
-            property.CopyRuntimeSettingsTo(runtimeProperty);
+            static JsonPropertyInfo CreateRuntimeProperty((JsonPropertyInfo property, Type runtimePropertyType) key, (JsonSerializerOptions options, Type classType) arg)
+            {
+                JsonPropertyInfo runtimeProperty = CreateProperty(
+                    key.property.DeclaredPropertyType,
+                    key.runtimePropertyType,
+                    key.property.ImplementedPropertyType,
+                    key.property.PropertyInfo,
+                    parentClassType: arg.classType,
+                    converter : null,
+                    options: arg.options);
 
-            return runtimeProperty;
+                key.property.CopyRuntimeSettingsTo(runtimeProperty);
+
+                return runtimeProperty;
+            }
+
+            ConcurrentDictionary<(JsonPropertyInfo, Type), JsonPropertyInfo> cache =
+                LazyInitializer.EnsureInitialized(ref RuntimePropertyCache, () => new ConcurrentDictionary<(JsonPropertyInfo, Type), JsonPropertyInfo>());
+#if BUILDING_INBOX_LIBRARY
+            return cache.GetOrAdd((property, runtimePropertyType), (key, arg) => CreateRuntimeProperty(key, arg), (options, Type));
+#else
+            return cache.GetOrAdd((property, runtimePropertyType), key => CreateRuntimeProperty(key, (options, Type)));
+#endif
         }
     }
 }
