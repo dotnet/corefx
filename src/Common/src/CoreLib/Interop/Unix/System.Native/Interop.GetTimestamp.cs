fc72133f81795161a0eeb59a20dc9c57cf7c5f6f
Aaron Robinson <arobins@microsoft.com>
2019-10-29 15:13:46 -0700

    Revert removal of SuppressGCTransition from SystemNative_GetTimestamp() (#27473)
    * Revert removal of SuppressGCTransition from SystemNative_GetTimestamp()
    
    * Insert GC_POLL before statement with unmanaged call.
    
    * JIT test for insertion of GCPoll
    
    Signed-off-by: dotnet-bot <dotnet-bot@microsoft.com>

--- src/Common/src/CoreLib/Interop/Unix/System.Native/Interop.GetTimestamp.cs
+++ src/Common/src/CoreLib/Interop/Unix/System.Native/Interop.GetTimestamp.cs
@@ -12,7 +12,7 @@ internal static partial class Interop
         internal static extern ulong GetTimestampResolution();
 
         [DllImport(Libraries.SystemNative, EntryPoint = "SystemNative_GetTimestamp", ExactSpelling = true)]
-        // [SuppressGCTransition] // https://github.com/dotnet/coreclr/issues/27465
+        [SuppressGCTransition]
         internal static extern ulong GetTimestamp();
     }
 }
