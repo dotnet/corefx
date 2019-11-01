273d46ae3306f4da9928bdb3a39672a9676d5e9b
Eric StJohn <ericstj@microsoft.com>
2019-11-01 16:16:21 -0700

    Add desktop build of System.Runtime.CompilerServices.Unsafe (#42297)

---
+++ src/System.Runtime.CompilerServices.Unsafe/src/include/netfx/coreassembly.h
@@ -0,0 +1,9 @@
+#define CORE_ASSEMBLY "mscorlib"
+#define net45
+
+// Metadata version: v4.0.30319
+.assembly extern CORE_ASSEMBLY
+{
+  .publickeytoken = (B7 7A 5C 56 19 34 E0 89 )                         // .z\V.4..
+  .ver 4:0:0:0
+}
