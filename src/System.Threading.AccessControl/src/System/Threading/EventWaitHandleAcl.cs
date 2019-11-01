e0cf7ccff52e10bac93dc3b6d7ed3b7d997c29b9
carlossanlop <calope@microsoft.com>
2019-11-01 11:36:28 -0700

    Apply suggestion of creating an EWH, then replacing its SWH.

--- src/System.Threading.AccessControl/src/System/Threading/EventWaitHandleAcl.cs
+++ src/System.Threading.AccessControl/src/System/Threading/EventWaitHandleAcl.cs
@@ -13,10 +13,10 @@ namespace System.Threading
 {
     public static class EventWaitHandleAcl
     {
-        /// <summary>Creates a new named <see cref="EventWaitHandle" /> instance, ensuring it is created with the specified event security.</summary>
-        /// <param name="initialState"><see langword="true" /> to set the initial state to signaled if the named event is created as a result of this call; <see langword="false" /> to set it to nonsignaled.</param>
+        /// <summary>Creates a new <see cref="EventWaitHandle" /> instance, ensuring it is created with the specified event security.</summary>
+        /// <param name="initialState"><see langword="true" /> to set the initial state to signaled if the named event is created as a result of this call; <see langword="false" /> to set it to non-signaled.</param>
         /// <param name="mode">One of the enum values that determines whether the event resets automatically or manually.</param>
-        /// <param name="name">The name of a system-wide synchronization event.</param>
+        /// <param name="name">The name, if the event is a system-wide synchronization event; otherwise, <see langword="null" /> or an empty string.</param>
         /// <param name="createdNew">When this method returns, it is set to <see langword="true" /> if a local event was created (that is, if name is <see langword="null" /> or an empty string) or if the specified named system event was created; <see langword="false" /> if the specified named system event already existed. This parameter is passed uninitialized.</param>
         /// <param name="eventSecurity">The Windows access control security to apply.</param>
         /// <returns>An object that represents the named event wait handle.</returns>
@@ -24,15 +24,11 @@ namespace System.Threading
         /// <exception cref="ArgumentNullException"><paramref name="eventSecurity" /> is <see langword="null" />.</exception>
         /// <exception cref="ArgumentOutOfRangeException">The <paramref name="mode" /> enum value was out of legal range.</exception>
         /// <exception cref="DirectoryNotFoundException">Could not find a part of the path specified in <paramref name="name" />.</exception>
-        /// <exception cref="WaitHandleCannotBeOpenedException">An event with the provided <paramref name="name" /> was not found.
+        /// <exception cref="WaitHandleCannotBeOpenedException">A system-wide synchronization event with the provided <paramref name="name" /> was not found.
         /// -or-
         /// An <see cref="EventWaitHandle" /> with system-wide name <paramref name="name" /> cannot be created. An <see cref="EventWaitHandle" /> of a different type might have the same name.</exception>
         public static unsafe EventWaitHandle Create(bool initialState, EventResetMode mode, string name, out bool createdNew, EventWaitHandleSecurity eventSecurity)
         {
-            if (string.IsNullOrEmpty(name))
-            {
-                throw new ArgumentException(SR.Format(SR.Argument_CannotBeNullOrEmpty), nameof(name));
-            }
             if (name.Length > Interop.Kernel32.MAX_PATH)
             {
                 throw new ArgumentException(SR.Format(SR.Argument_WaitHandleNameTooLong, name), nameof(name));
@@ -42,12 +38,12 @@ namespace System.Threading
                 throw new ArgumentNullException(nameof(eventSecurity));
             }
 
-            uint eventFlags = initialState ? Interop.Kernel32.CREATE_EVENT_INITIAL_SET : 0;
-
-            if (mode == EventResetMode.ManualReset)
+            bool isManualReset = mode switch
             {
-                eventFlags |= (uint)Interop.Kernel32.CREATE_EVENT_MANUAL_RESET;
-            }
+                EventResetMode.ManualReset => true,
+                EventResetMode.AutoReset => false,
+                _ => throw new ArgumentOutOfRangeException(nameof(mode), SR.Format(SR.ArgumentOutOfRange_Enum))
+            };
 
             fixed (byte* pSecurityDescriptor = eventSecurity.GetSecurityDescriptorBinaryForm())
             {
@@ -57,19 +53,16 @@ namespace System.Threading
                     lpSecurityDescriptor = (IntPtr)pSecurityDescriptor
                 };
 
-                using SafeWaitHandle handle = Interop.Kernel32.CreateEvent(ref secAttrs, name, eventFlags, (uint)EventWaitHandleRights.FullControl);
+                using SafeWaitHandle handle = Interop.Kernel32.CreateEvent(ref secAttrs, isManualReset, initialState, name);
 
                 ValidateHandle(handle, name, out createdNew);
 
-                try
-                {
-                    return EventWaitHandle.OpenExisting(name);
-                }
-                finally
-                {
-                    // Close our handle as the EventWaitHandle will have it's own.
-                    handle.Dispose();
-                }
+                var ewh = new EventWaitHandle(initialState, mode);
+                var old = ewh.SafeWaitHandle;
+                ewh.SafeWaitHandle = handle;
+                old.Dispose();
+
+                return ewh;
             }
         }
 
