bd7229ba41b6f45c3926a33a78dfca6c6d570486
carlossanlop <calope@microsoft.com>
2019-11-04 16:01:46 -0800

    Remove exception checks in netfx version

--- src/System.Threading.AccessControl/src/System/Threading/MutexAcl.net46.cs
+++ src/System.Threading.AccessControl/src/System/Threading/MutexAcl.net46.cs
@@ -10,19 +10,6 @@ namespace System.Threading
     {
         public static Mutex Create(bool initiallyOwned, string name, out bool createdNew, MutexSecurity mutexSecurity)
         {
-            if (string.IsNullOrEmpty(name))
-            {
-                throw new ArgumentException(SR.Format(SR.Argument_CannotBeNullOrEmpty), nameof(name));
-            }
-            if (name.Length > Interop.Kernel32.MAX_PATH)
-            {
-                throw new ArgumentException(SR.Format(SR.Argument_WaitHandleNameTooLong, name), nameof(name));
-            }
-            if (mutexSecurity == null)
-            {
-                throw new ArgumentNullException(nameof(mutexSecurity));
-            }
-
             return new Mutex(initiallyOwned, name, out createdNew, mutexSecurity);
         }
     }
