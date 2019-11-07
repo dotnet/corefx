f9eb7fdbdfbde3bfc525c3be3a67713b3f5a75a1
Viktor Hofer <viktor.hofer@microsoft.com>
2019-11-07 11:32:29 +0100

    Remove root cross folder in favor of eng/common/cross (#42434)
    * Manual darc update from build '20191106.10'
    
    * Remove root cross folder in favor of common/cross

--- src/Native/Unix/gen-buildsys-gcc.sh
+++ src/Native/Unix/gen-buildsys-gcc.sh
@@ -139,7 +139,7 @@ if [ "$CROSSCOMPILE" = "1" ]; then
         exit 1
     fi
     if [ -z "$CONFIG_DIR" ]; then
-        CONFIG_DIR="$2/cross"
+        CONFIG_DIR="$repo_root/eng/common/cross"
     fi
     export TARGET_BUILD_ARCH=$build_arch
     cmake_extra_defines="$cmake_extra_defines -C $scriptroot/tryrun.cmake"
