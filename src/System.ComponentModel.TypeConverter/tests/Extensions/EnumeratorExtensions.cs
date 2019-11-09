6025514f2abaaff7af375785bf5566e8550b2a86
twogenies <dev@twogenies.ru>
2019-11-09 18:14:58 +0300

    Code review issues are fixed

---
+++ src/System.ComponentModel.TypeConverter/tests/Extensions/EnumeratorExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace System.ComponentModel.Tests
+{
+    public static class EnumeratorExtensions
+    {
+        public static IEnumerable<T> ToEnumerable<T>(this IEnumerator<T> enumerator)
+        {
+            while(enumerator.MoveNext())
+                yield return enumerator.Current;
+        }
+
+        public static IEnumerator<T> Cast<T>(this IEnumerator iterator)
+        {
+            while (iterator.MoveNext())
+            {
+                yield return (T) iterator.Current;
+            }
+        }
+    }
+}
