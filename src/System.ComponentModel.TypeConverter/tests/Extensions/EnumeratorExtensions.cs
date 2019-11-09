587cd3d2bef83005a5cc10cf5b0762d8c45cc662
twogenies <dev@twogenies.ru>
2019-11-09 18:14:58 +0300

    add MaskedTextProviderTests(#7758)

---
+++ src/System.ComponentModel.TypeConverter/tests/Extensions/EnumeratorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Extensions
+{
+    public static class EnumeratorExtensions
+    {
+        public static IEnumerable<T> ToEnumerable<T>(this IEnumerator<T> enumerator)
+        {
+            while(enumerator.MoveNext())
+                yield return enumerator.Current;
+        }
+
+        public static IEnumerator<T> Cast<T>(this IEnumerator iterator)
+        {
+            while (iterator.MoveNext())
+            {
+                yield return (T) iterator.Current;
+            }
+        }
+    }
+}
