9c8dd6bb73426f5ddfd4237a305f9f0c1d26800e
YohDeadfall <yoh.deadfall@hotmail.com>
2019-11-14 16:46:31 +0300

    Added support for surogates

--- src/System.Text.Json/src/System/Text/Json/Serialization/JsonSnakeCaseNamingPolicy.cs
+++ src/System.Text.Json/src/System/Text/Json/Serialization/JsonSnakeCaseNamingPolicy.cs
@@ -56,6 +56,9 @@ namespace System.Text.Json
                         }
                         break;
 
+                    case UnicodeCategory.Surrogate:
+                        break;
+
                     default:
                         if (previousCategory != null)
                         {
