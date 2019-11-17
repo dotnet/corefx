856bd576ae7e968cb026abdfbdc77bcbfd0af3a6
Miha Markič <miha@rthand.com>
2019-11-17 17:54:45 +0100

    Adds tests for not calling Stream.Flush and calling Stream.FlushAsync instead on an async XmlWriter #37457

---
+++ src/System.Private.Xml/tests/XmlWriter/CloseOnAsync.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace System.Xml.Tests
+{
+    public class CloseOnAsync
+    {
+        public class ExtendedMemoryStream : MemoryStream
+        {
+            public bool FlushCalled { get; private set; }
+            public bool FlushAsyncCalled { get; private set; }
+
+            public override void Flush()
+            {
+                FlushCalled = true;
+                base.Flush();
+            }
+
+            public override Task FlushAsync(CancellationToken cancellationToken)
+            {
+                FlushAsyncCalled = true;
+                return base.FlushAsync(cancellationToken);
+            }
+        }
+
+        [Fact]
+        public void AsyncWriter_DoesNotCall_Flush_On_Close()
+        {
+            var stream = new ExtendedMemoryStream();
+            var writer = XmlWriter.Create(stream, new XmlWriterSettings { Async = true });
+            writer.Close();
+
+            Assert.False(stream.FlushCalled);
+        }
+
+        [Fact]
+        public void AsyncWriter_DoesCall_FlushAsync_On_Close()
+        {
+            var stream = new ExtendedMemoryStream();
+            var writer = XmlWriter.Create(stream, new XmlWriterSettings { Async = true });
+            writer.Close();
+
+            Assert.True(stream.FlushAsyncCalled);
+        }
+    }
+}
