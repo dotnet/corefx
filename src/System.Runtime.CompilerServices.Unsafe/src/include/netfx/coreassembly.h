e5c05ddd6ba7b56f617962aa8f7e953c81f0f1ff
Jose Perez Rodriguez <joperezr@microsoft.com>
2020-02-11 13:48:26 -0800

    Fix comment on header file

--- src/System.Runtime.CompilerServices.Unsafe/src/include/netfx/coreassembly.h
+++ src/System.Runtime.CompilerServices.Unsafe/src/include/netfx/coreassembly.h
@@ -3,6 +3,6 @@
 // Metadata version: v4.0.30319
 .assembly extern CORE_ASSEMBLY
 {
-  .publickeytoken = (B7 7A 5C 56 19 34 E0 89 )                         // .?_....:
+  .publickeytoken = (B7 7A 5C 56 19 34 E0 89 )                         // .z\V.4..
   .ver 4:0:0:0
 }
