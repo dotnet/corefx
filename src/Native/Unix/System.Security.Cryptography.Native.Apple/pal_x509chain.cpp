5440ef48d39d9279b6346bb486fed5768e5d09d8
buyaa-n <buyankhishig.namnan@microsoft.com>
2020-05-01 19:13:43 -0400

    Handle `UnparseableExtension` status code when building X509Chain on OSX

--- src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_x509chain.cpp
+++ src/Native/Unix/System.Security.Cryptography.Native.Apple/pal_x509chain.cpp
@@ -175,6 +175,10 @@ static void MergeStatusCodes(CFTypeRef key, CFTypeRef value, void* context)
         *pStatus |= PAL_X509ChainPartialChain;
     else if (CFEqual(keyString, CFSTR("CriticalExtensions")))
         *pStatus |= PAL_X509ChainHasNotSupportedCriticalExtension;
+    else if (CFEqual(keyString, CFSTR("UnparseableExtension")))
+    {
+        // 10.15 introduced new status code value which is not reported by Windows. Ignoring for now.
+    }
     else if (CFEqual(keyString, CFSTR("WeakLeaf")) || CFEqual(keyString, CFSTR("WeakIntermediates")) ||
              CFEqual(keyString, CFSTR("WeakRoot")) || CFEqual(keyString, CFSTR("WeakKeySize")) ||
              CFEqual(keyString, CFSTR("WeakSignature")))
