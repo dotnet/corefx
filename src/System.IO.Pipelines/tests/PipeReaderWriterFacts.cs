cb86234335a54e1f17071f8ce97bb5dc96d551fe
BrennanConroy <brecon@microsoft.com>
2020-10-28 13:48:21 -0700

    fb

--- src/System.IO.Pipelines/tests/PipeReaderWriterFacts.cs
+++ src/System.IO.Pipelines/tests/PipeReaderWriterFacts.cs
@@ -5,6 +5,7 @@
 using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
