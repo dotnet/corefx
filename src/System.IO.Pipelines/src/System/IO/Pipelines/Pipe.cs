214727042a799f7e9ce77418d0e25ce20593e6c0
Brennan <brecon@microsoft.com>
2020-11-13 15:36:19 -0800

    [Backport] Throw when reader is completed with an exception (#43002)
    * [Backport] Throw when reader is completed with an exception
    
    * fb

--- src/System.IO.Pipelines/src/System/IO/Pipelines/Pipe.cs
+++ src/System.IO.Pipelines/src/System/IO/Pipelines/Pipe.cs
@@ -955,7 +955,7 @@ namespace System.IO.Pipelines
                 ThrowHelper.ThrowInvalidOperationException_NoWritingAllowed();
             }
 
-            if (_readerCompletion.IsCompleted)
+            if (_readerCompletion.IsCompletedOrThrow())
             {
                 return new ValueTask<FlushResult>(new FlushResult(isCanceled: false, isCompleted: true));
             }
