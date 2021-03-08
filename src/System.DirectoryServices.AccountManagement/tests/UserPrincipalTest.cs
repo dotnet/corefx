72d4f6cad0ad29ab5864ed8d7f443af5788b49a4
Jan Jahoda <jajahoda@microsoft.com>
2021-03-08 17:25:00 +0100

    Get pwd from env variable as in runtime repo

--- src/System.DirectoryServices.AccountManagement/tests/UserPrincipalTest.cs
+++ src/System.DirectoryServices.AccountManagement/tests/UserPrincipalTest.cs
@@ -33,7 +33,7 @@ namespace System.DirectoryServices.AccountManagement.Tests
         public void ComputedUACCheck()
         {
             string username = "Administrator";
-            string password = "PLACEHOLDER";
+            string password = Environment.GetEnvironmentVariable("TESTPASSWORD");
             //TODO: don't assume it exists, create it if its not
             string OU = "TestNull";
             string baseDomain =WindowsIdentity.GetCurrent().Name.Split(new char[] { '\\' })[1] + "-TEST";
