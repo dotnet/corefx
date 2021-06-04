backend-devloper/runtime

Use EVP_PKEY for RSA Decrypt

src/libraries/Native/Unix/System.Security.Cryptography.Native/pal_evp_pkey.h
@@ -24,6 +24,11 @@ Always succeeds.
 PALEXPORT void CryptoNative_EvpPkeyDestroy(EVP_PKEY* pkey);
 
 /*
+Returns the maximum size, in bytes, of an operation with the provided key.
+*/
+PALEXPORT int32_t CryptoNative_EvpPKeySize(EVP_PKEY* pkey);
+
+/*
 Used by System.Security.Cryptography.X509Certificates' OpenSslX509CertificateReader when
 duplicating a private key context as part of duplicating the Pal object.
 

src/libraries/Native/Unix/System.Security.Cryptography.Native/pal_evp_pkey_rsa.h
@@ -1,14 +1,37 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#include "pal_types.h"
-#include "pal_compiler.h"
 #include "opensslshim.h"
+#include "pal_compiler.h"
+#include "pal_types.h"
+
+/*
+Padding options for RSA.
+Matches RSAEncryptionPaddingMode / RSASignaturePaddingMode.
+*/
+typedef enum
+{
+    RsaPaddingPkcs1,
+    RsaPaddingOaepOrPss,
+} RsaPaddingMode;
 
 /*
 Creates an RSA key of the requested size.
 */
 PALEXPORT EVP_PKEY* CryptoNative_RsaGenerateKey(int32_t keySize);
+
+/*
+Decrypt source into destination using the specified RSA key (wrapped in an EVP_PKEY) and padding/digest options.
+
+Returns the number of bytes written to destination, -1 on error.
+*/
+PALEXPORT int32_t CryptoNative_RsaDecrypt(EVP_PKEY* pkey,
+                                          const uint8_t* source,
+                                          int32_t sourceLen,
+                                          RsaPaddingMode padding,
+                                          const EVP_MD* digest,
+                                          uint8_t* destination,
+                                          int32_t destinationLen);
 
 /*
 Shims the EVP_PKEY_get1_RSA method.

dotnet/corefx

src/Native/Unix/System.Security.Cryptography.Native/pal_evp_pkey.h
@@ -25,6 +25,11 @@ Always succeeds.
 DLLEXPORT void CryptoNative_EvpPkeyDestroy(EVP_PKEY* pkey);
 
 /*
+Returns the maximum size, in bytes, of an operation with the provided key.
+*/
+DLLEXPORT int32_t CryptoNative_EvpPKeySize(EVP_PKEY* pkey);
+
+/*
 Used by System.Security.Cryptography.X509Certificates' OpenSslX509CertificateReader when
 duplicating a private key context as part of duplicating the Pal object.
 

src/Native/Unix/System.Security.Cryptography.Native/pal_evp_pkey_rsa.h
@@ -2,14 +2,37 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#include "pal_types.h"
-#include "pal_compiler.h"
 #include "opensslshim.h"
+#include "pal_compiler.h"
+#include "pal_types.h"
+
+/*
+Padding options for RSA.
+Matches RSAEncryptionPaddingMode / RSASignaturePaddingMode.
+*/
+typedef enum
+{
+    RsaPaddingPkcs1,
+    RsaPaddingOaepOrPss,
+} RsaPaddingMode;
 
 /*
 Creates an RSA key of the requested size.
 */
 DLLEXPORT EVP_PKEY* CryptoNative_RsaGenerateKey(int32_t keySize);
+
+/*
+Decrypt source into destination using the specified RSA key (wrapped in an EVP_PKEY) and padding/digest options.
+
+Returns the number of bytes written to destination, -1 on error.
+*/
+DLLEXPORT int32_t CryptoNative_RsaDecrypt(EVP_PKEY* pkey,
+                                          const uint8_t* source,
+                                          int32_t sourceLen,
+                                          RsaPaddingMode padding,
+                                          const EVP_MD* digest,
+                                          uint8_t* destination,
+                                          int32_t destinationLen);
 
 /*
 Shims the EVP_PKEY_get1_RSA method.