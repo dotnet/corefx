dotnet/runtime

Use EVP_PKEY for RSA key generation

src/libraries/Native/Unix/System.Security.Cryptography.Native/pal_evp_pkey_rsa.h
@@ -6,6 +6,11 @@
 #include "opensslshim.h"
 
 /*
+Creates an RSA key of the requested size.
+*/
+PALEXPORT EVP_PKEY* CryptoNative_RsaGenerateKey(int32_t keySize);
+
+/*
 Shims the EVP_PKEY_get1_RSA method.
 
 Returns the RSA instance for the EVP_PKEY.

dotnet/corefx

src/Native/Unix/System.Security.Cryptography.Native/pal_evp_pkey_rsa.h
@@ -7,6 +7,11 @@
 #include "opensslshim.h"
 
 /*
+Creates an RSA key of the requested size.
+*/
+DLLEXPORT EVP_PKEY* CryptoNative_RsaGenerateKey(int32_t keySize);
+
+/*
 Shims the EVP_PKEY_get1_RSA method.
 
 Returns the RSA instance for the EVP_PKEY.