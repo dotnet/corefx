dotnet/runtime

OpenSSL3: Register legacy algorithms when needed

src/libraries/Native/Unix/System.Security.Cryptography.Native/openssl.h
@@ -72,3 +72,5 @@ PALEXPORT int32_t CryptoNative_LookupFriendlyNameByOid(const char* oidValue, con
 PALEXPORT int32_t CryptoNative_EnsureOpenSslInitialized(void);
 
 PALEXPORT int64_t CryptoNative_OpenSslVersionNumber(void);
+
+PALEXPORT void CryptoNative_RegisterLegacyAlgorithms(void);

dotnet/corefx

src/Native/Unix/System.Security.Cryptography.Native/openssl.h
@@ -73,3 +73,5 @@ DLLEXPORT int32_t CryptoNative_LookupFriendlyNameByOid(const char* oidValue, con
 DLLEXPORT int32_t CryptoNative_EnsureOpenSslInitialized(void);
 
 DLLEXPORT int64_t CryptoNative_OpenSslVersionNumber(void);
+
+DLLEXPORT void CryptoNative_RegisterLegacyAlgorithms(void);