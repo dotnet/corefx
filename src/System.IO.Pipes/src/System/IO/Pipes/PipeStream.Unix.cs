849a8b37d4c7ca8e127db5fe599be78886314ae1
Mark Haines <mjark@negativecurvature.net>
2022-06-15 23:19:19 +0100

    Fix writing large arrays to anonymous pipes on linux
    Increment the `bufPtr` when we can't write the array in a single syscall. Should fix https://github.com/dotnet/runtime/issues/70798

--- src/System.IO.Pipes/src/System/IO/Pipes/PipeStream.Unix.cs
+++ src/System.IO.Pipes/src/System/IO/Pipes/PipeStream.Unix.cs
@@ -165,9 +165,9 @@ namespace System.IO.Pipes
             }
 
             // For anonymous pipes, write the file descriptor.
-            fixed (byte* bufPtr = &MemoryMarshal.GetReference(buffer))
+            while (buffer.Length > 0)
             {
-                while (buffer.Length > 0)
+                fixed (byte* bufPtr = &MemoryMarshal.GetReference(buffer))
                 {
                     int bytesWritten = CheckPipeCall(Interop.Sys.Write(_handle, bufPtr, buffer.Length));
                     buffer = buffer.Slice(bytesWritten);
